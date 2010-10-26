056a4dd7acce5e3a411c90aa17cc7c6d7003bcd2
highslide-software <support@highslide.com>
2010-10-26 11:25:15 +0200

    Optimized live server example.

--- studies/live-server-data.php
+++ studies/live-server-data.php
@@ -1,9 +1,13 @@
 <?php 
+// Set the JSON header
+header("Content-type: text/json");
+
 // The x value is the current JavaScript time, which is the Unix time multiplied by 1000.
 $x = time() * 1000;
 // The y value is a random number
 $y = rand(0, 100);
 
-// Return it in the form of a JavaScript array
-echo "[$x, $y]";
+// Create a PHP array and echo it as JSON
+$ret = array($x, $y);
+echo json_encode($ret);
 ?>
