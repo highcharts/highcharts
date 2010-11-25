d38466ed11f40da344e638a3ef4c6bfec9aaa2ec
highslide-software <support@highslide.com>
2010-11-25 10:31:47 +0100

    Fixed infinite loop issue with low custom tickIntervals

--- js/highcharts.src.js
+++ js/highcharts.src.js
@@ -4612,8 +4612,12 @@ function Chart (options, callback) {
 		 * @param {Number} num
 		 */
 		function correctFloat(num) {
-			var invMag = (magnitude < 1 ? mathRound(1 / magnitude) : 1) * 10;
-			return mathRound(num * invMag) / invMag;
+			var invMag, ret = num;
+			if (defined(magnitude)) {
+				invMag = (magnitude < 1 ? mathRound(1 / magnitude) : 1) * 10;
+				ret = mathRound(num * invMag) / invMag;
+			}
+			return ret;
 		}
 				
 		/**
