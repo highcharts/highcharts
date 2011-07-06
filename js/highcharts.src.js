7e4600c603313ea255993cd5620082800bcaad0e
highslide-software <support@highslide.com>
2011-07-06 11:59:05 +0200

    Fixed support for lineHeight option in tooltips and more. Closes #293.

--- js/highcharts.src.js
+++ js/highcharts.src.js
@@ -2303,7 +2303,7 @@ SVGRenderer.prototype = {
 							// Webkit and opera sometimes return 'normal' as the line height. In that
 							// case, webkit uses offsetHeight, while Opera falls back to 18
 							lineHeight = win[GET_COMPUTED_STYLE] &&
-								win[GET_COMPUTED_STYLE](lastLine, null).getPropertyValue('line-height');
+								pInt(win[GET_COMPUTED_STYLE](lastLine, null).getPropertyValue('line-height'));
 							
 							if (!lineHeight || isNaN(lineHeight)) {
 								lineHeight = textLineHeight || lastLine.offsetHeight || 18;
