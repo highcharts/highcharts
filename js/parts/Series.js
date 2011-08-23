16fce710ea2150c04d2679dd28f6c42f18791e09
highslide-software <support@highslide.com>
2011-08-23 14:36:59 +0200

    Fixed issue with stray points staying behind after panning the chart

--- js/parts/Series.js
+++ js/parts/Series.js
@@ -84,7 +84,6 @@ Point.prototype = {
 		if (point === series.chart.hoverPoint) {
 			point.onMouseOut();
 		}
-		series.chart.hoverPoints = null; // remove reference
 
 		// remove all events
 		if (point.graphic || point.dataLabel) { // removeEvent and destroyElements are performance expensive
