51a7d7578564ef64f0153e6794e1804699299d63
Erik Olsson <erik@highsoft.com>
2011-09-05 13:51:26 +0200

    Avoided recursion in removeEvents

--- js/adapters/mootools-adapter.src.js
+++ js/adapters/mootools-adapter.src.js
@@ -204,8 +204,11 @@ win.HighchartsAdapter = {
 	},
 
 	removeEvent: function (el, type, fn) {
+		if (typeof el === 'string') {
+			// el.removeEvents below apperantly calls this method again. Do not quite understand why, so for now just bail out.
+			return;
+		}
 		win.HighchartsAdapter.extendWithEvents(el);
-
 		if (type) {
 			if (type === 'unload') { // Moo self destructs before custom unload events
 				type = 'beforeunload';
