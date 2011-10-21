08a29e41571fc44aabab0820cc21c89e68766ea2
highslide-software <support@highslide.com>
2011-10-21 10:18:19 +0200

    Changed and added demos for series.compare

--- samples/stock/demo/multiple-series/demo.js
+++ samples/stock/demo/multiple-series/demo.js
@@ -2,7 +2,7 @@ $(function() {
 	var seriesOptions = [],
 		yAxisOptions = [],
 		seriesCounter = 0,
-		names = ['ADBE', 'GOOG', 'MSFT'],
+		names = ['MSFT', 'AAPL', 'GOOG'],
 		colors = Highcharts.getOptions().colors;
 
 	$.each(names, function(i, name) {
@@ -11,25 +11,7 @@ $(function() {
 
 			seriesOptions[i] = {
 				name: name,
-				data: data,
-				yAxis: i
-			};
-
-			// create one y axis for each series in order to be able to compare them
-			yAxisOptions[i] = {
-				alternateGridColor: null,
-				gridLineWidth: i ? 0 : 1, // only grid lines for the first series
-				opposite: i ? true : false,
-				minorGridLineWidth: 0,
-				title: {
-					text: name,
-					style: {
-						color: colors[i]
-					}
-				},
-				lineWidth: 2,
-				lineColor: colors[i],
-				offset: [0, 0, 70][i]
+				data: data
 			};
 
 			// As we're loading the data asynchronously, we don't know what order it will arrive. So
@@ -49,26 +31,33 @@ $(function() {
 
 		chart = new Highcharts.StockChart({
 		    chart: {
-		        renderTo: 'container',
-		        alignTicks: false,
-		        marginLeft: 20,
-		        marginRight: 130
+		        renderTo: 'container'
 		    },
 
 		    rangeSelector: {
-		        selected: 1
+		        selected: 4
 		    },
 
-		    title: {
-		        text: null
+		    yAxis: {
+		    	labels: {
+		    		formatter: function() {
+		    			return (this.value > 0 ? '+' : '') + this.value + '%';
+		    		}
+		    	}
+		    },
+		    
+		    plotOptions: {
+		    	series: {
+		    		compare: 'percent'
+		    	}
 		    },
 		    
 		    tooltip: {
+		    	pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+		    	changeDecimals: 2,
 		    	yDecimals: 2
 		    },
 		    
-		    yAxis: yAxisOptions,
-
 		    series: seriesOptions
 		});
 	}
