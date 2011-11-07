5441d47cfb0d1178786c66905bbb01ece06908ef
highslide-software <support@highslide.com>
2011-11-07 15:37:52 +0100

    Edited samples

--- samples/view.php
+++ samples/view.php
@@ -4,7 +4,8 @@ if (!preg_match('/^[a-z]+\/[a-z]+\/[a-z\-]+$/', $path)) {
 	die ('Invalid sample path input');
 }
 
-$next = (int)$_GET['next'];
+$i = (int)$_GET['i'];
+$next = $i + 1;
 ?><!DOCTYPE HTML>
 <html>
 	<head>
@@ -19,18 +20,44 @@ $next = (int)$_GET['next'];
 		<script type="text/javascript">
 			$(function() {
 				
-				
-				
-				if (window.parent.frames[0] && window.parent.frames[0].document.getElementById('i<?php echo $next ?>')) {
-					function goNext () {
-						window.location.href = 
-							window.parent.frames[0].document.getElementById('i<?php echo $next ?>').href;
+				if (window.parent.frames[0]) {
+					var contentDoc = window.parent.frames[0].document;
+					
+					// Highlight the current sample in the left
+					var li = contentDoc.getElementById('li<?php echo $i ?>');
+					if (li) {
+						// previous
+						if (contentDoc.currentLi) {
+							$(contentDoc.currentLi).removeClass('hilighted');
+							$(contentDoc.currentLi).addClass('visited');
+						}
+						
+						$(li).addClass('hilighted');
+						contentDoc.currentLi = li;
 					}
 					
-					$('#next').click(function() {
-						goNext();
+					// add the next button
+					if (contentDoc.getElementById('i<?php echo $next ?>')) {
+						function goNext () {
+							window.location.href = 
+								window.parent.frames[0].document.getElementById('i<?php echo $next ?>').href;
+						}
+						
+						$('#next').click(function() {
+							goNext();
+						});
+						$('#next')[0].disabled = false;
+					}
+					
+					// the reload button
+					$('#reload').click(function() {
+						location.reload();
 					});
-					$('#next')[0].disabled = false;
+					
+					// the jsfiddle button
+					$('#jsfiddle').click(function() {
+						location.href = 'http://jsfiddle.net/gh/get/jquery/1.6/highslide-software/highcharts.com/tree/stock/samples/<?php echo $path ?>/';
+					});
 				}
 			});
 		</script>
@@ -48,7 +75,8 @@ $next = (int)$_GET['next'];
 		
 		<div style="text-align: center">
 			<button id="next" disabled="disabled">Next</button>
-			<a target="_blank" href="http://jsfiddle.net/gh/get/jquery/1.6/highslide-software/highcharts.com/tree/stock/samples/<?php echo $path ?>/">jsFiddle</a>
+			<button id="reload">Reload</button>
+			<button id="jsfiddle">jsFiddle</button>
 		</div>
 	</body>
 </html>
