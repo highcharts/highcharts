97153f751f3f71fb4a6b2d5d9a3242e707c464a6
highslide-software <support@highslide.com>
2011-12-19 13:37:48 +0100

    Worked around issue in Chrome 16 where graphs were incompletely drawn due to malfunction in while loops. Closes #654.

--- js/highstock.src.js
+++ js/highstock.src.js
@@ -15354,18 +15354,20 @@ Point.prototype.tooltipFormatter = function (pointFormat) {
 						
 					// first look for an exact match in the ordinalpositions array
 					i = ordinalLength;
-					while (ordinalIndex === UNDEFINED && i--) {
+					while (i--) {
 						if (ordinalPositions[i] === val) {
 							ordinalIndex = i;
+							break;
 						}
 					}
 					
 					// if that failed, find the intermediate position between the two nearest values
 					i = ordinalLength - 1;
-					while (ordinalIndex === UNDEFINED && i--) {
+					while (i--) {
 						if (val > ordinalPositions[i]) { // interpolate
 							distance = (val - ordinalPositions[i]) / (ordinalPositions[i + 1] - ordinalPositions[i]); // something between 0 and 1
 							ordinalIndex = i + distance;
+							break;
 						}
 					}
 					return toIndex ?
