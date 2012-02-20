c23ea201d90cc0f495a0baefd7004f0ce9e98446
Erik Olsson <erik@highsoft.com>
2012-02-20 18:12:11 +0100

    Moved StackItem to its own file

--- js/parts/Chart.js
+++ js/parts/Chart.js
@@ -627,99 +627,6 @@ function Chart(options, callback) {
 		};
 
 		/**
-		 * The class for stack items
-		 */
-		function StackItem(options, isNegative, x, stackOption) {
-			var stackItem = this;
-
-			// Tells if the stack is negative
-			stackItem.isNegative = isNegative;
-
-			// Save the options to be able to style the label
-			stackItem.options = options;
-
-			// Save the x value to be able to position the label later
-			stackItem.x = x;
-
-			// Save the stack option on the series configuration object
-			stackItem.stack = stackOption;
-
-			// The align options and text align varies on whether the stack is negative and
-			// if the chart is inverted or not.
-			// First test the user supplied value, then use the dynamic.
-			stackItem.alignOptions = {
-				align: options.align || (inverted ? (isNegative ? 'left' : 'right') : 'center'),
-				verticalAlign: options.verticalAlign || (inverted ? 'middle' : (isNegative ? 'bottom' : 'top')),
-				y: pick(options.y, inverted ? 4 : (isNegative ? 14 : -6)),
-				x: pick(options.x, inverted ? (isNegative ? -6 : 6) : 0)
-			};
-
-			stackItem.textAlign = options.textAlign || (inverted ? (isNegative ? 'right' : 'left') : 'center');
-		}
-
-		StackItem.prototype = {
-			destroy: function () {
-				destroyObjectProperties(this);
-			},
-
-			/**
-			 * Sets the total of this stack. Should be called when a serie is hidden or shown
-			 * since that will affect the total of other stacks.
-			 */
-			setTotal: function (total) {
-				this.total = total;
-				this.cum = total;
-			},
-
-			/**
-			 * Renders the stack total label and adds it to the stack label group.
-			 */
-			render: function (group) {
-				var stackItem = this,									// aliased this
-					str = stackItem.options.formatter.call(stackItem);  // format the text in the label
-
-				// Change the text to reflect the new total and set visibility to hidden in case the serie is hidden
-				if (stackItem.label) {
-					stackItem.label.attr({text: str, visibility: HIDDEN});
-				// Create new label
-				} else {
-					stackItem.label =
-						chart.renderer.text(str, 0, 0)				// dummy positions, actual position updated with setOffset method in columnseries
-							.css(stackItem.options.style)			// apply style
-							.attr({align: stackItem.textAlign,			// fix the text-anchor
-								rotation: stackItem.options.rotation,	// rotation
-								visibility: HIDDEN })					// hidden until setOffset is called
-							.add(group);							// add to the labels-group
-				}
-			},
-
-			/**
-			 * Sets the offset that the stack has from the x value and repositions the label.
-			 */
-			setOffset: function (xOffset, xWidth) {
-				var stackItem = this,										// aliased this
-					neg = stackItem.isNegative,								// special treatment is needed for negative stacks
-					y = axis.translate(stackItem.total, 0, 0, 0, 1),		// stack value translated mapped to chart coordinates
-					yZero = axis.translate(0),								// stack origin
-					h = mathAbs(y - yZero),									// stack height
-					x = chart.xAxis[0].translate(stackItem.x) + xOffset,	// stack x position
-					plotHeight = chart.plotHeight,
-					stackBox = {	// this is the box for the complete stack
-							x: inverted ? (neg ? y : y - h) : x,
-							y: inverted ? plotHeight - x - xWidth : (neg ? (plotHeight - y - h) : plotHeight - y),
-							width: inverted ? h : xWidth,
-							height: inverted ? xWidth : h
-					};
-
-				if (stackItem.label) {
-					stackItem.label
-						.align(stackItem.alignOptions, null, stackBox)	// align the label to the box
-						.attr({visibility: VISIBLE});					// set visibility
-				}
-			}
-		};
-
-		/**
 		 * Get the minimum and maximum for the series of each axis
 		 */
 		function getSeriesExtremes() {
@@ -833,7 +740,7 @@ function Chart(options, callback) {
 									// If the StackItem is there, just update the values,
 									// if not, create one first
 									if (!stacks[key][x]) {
-										stacks[key][x] = new StackItem(options.stackLabels, isNegative, x, stackOption);
+										stacks[key][x] = new StackItem(new StackItemContext(chart, inverted, axis), options.stackLabels, isNegative, x, stackOption);
 									}
 									stacks[key][x].setTotal(y);
 
