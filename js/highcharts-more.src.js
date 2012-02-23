55a3ca237036e6363ff088f9526d766b8c9c2dc1
highslide-software <support@highslide.com>
2012-02-23 11:25:20 +0100

    Added framework for building and running highcharts-more.js

---
+++ js/highcharts-more.src.js
@@ -0,0 +1,17 @@
+// ==ClosureCompiler==
+// @compilation_level SIMPLE_OPTIMIZATIONS
+
+/**
+ * @license Highcharts JS v2.2.0 (2012-02-16)
+ *
+ * (c) 2009-2011 Torstein Hønsi
+ *
+ * License: www.highcharts.com/license
+ */
+
+// JSLint options:
+/*global Highcharts, document, window, navigator, setInterval, clearInterval, clearTimeout, setTimeout, location, jQuery, $, console */
+
+(function () {
+// This is the area range series file
+}());
---
+++ js/parts-more/Intro.js
@@ -0,0 +1,15 @@
+// ==ClosureCompiler==
+// @compilation_level SIMPLE_OPTIMIZATIONS
+
+/**
+ * @license @product.name@ JS v@product.version@ (@product.date@)
+ *
+ * (c) 2009-2011 Torstein Hønsi
+ *
+ * License: www.highcharts.com/license
+ */
+
+// JSLint options:
+/*global Highcharts, document, window, navigator, setInterval, clearInterval, clearTimeout, setTimeout, location, jQuery, $, console */
+
+(function () {
