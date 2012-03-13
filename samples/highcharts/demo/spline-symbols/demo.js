e7b92478653753763017e413a976eb9a85b4592b
highslide-software <support@highslide.com>
2012-03-13 15:56:00 +0100

    Updated demos

--- samples/highcharts/demo/spline-symbols/demo.js
+++ samples/highcharts/demo/spline-symbols/demo.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
 			data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, {
 				y: 26.5,
 				marker: {
-					symbol: 'url(../../graphics/sun.png)'
+					symbol: 'url(http://www.highcharts.com/demo/gfx/sun.png)'
 				}
 			}, 23.3, 18.3, 13.9, 9.6]
 
@@ -58,7 +58,7 @@ $(document).ready(function() {
 			data: [{
 				y: 3.9,
 				marker: {
-					symbol: 'url(../../graphics/snow.png)'
+					symbol: 'url(http://www.highcharts.com/demo/gfx/snow.png)'
 				}
 			}, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
 		}]
