8cbe843a3b9b4f5b93f82bae5fc56e8f025b5d1d
highslide-software <support@highslide.com>
2012-03-15 11:36:06 +0100

    Replace line endings in demo.js

--- samples/highcharts/demo/area-negative/demo.js
+++ samples/highcharts/demo/area-negative/demo.js
@@ -1,34 +1,34 @@
-var chart;
-$(document).ready(function() {
-	chart = new Highcharts.Chart({
-		chart: {
-			renderTo: 'container',
-			type: 'area'
-		},
-		title: {
-			text: 'Area chart with negative values'
-		},
-		xAxis: {
-			categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
-		},
-		tooltip: {
-			formatter: function() {
-				return ''+
-					this.series.name +': '+ this.y +'';
-			}
-		},
-		credits: {
-			enabled: false
-		},
-		series: [{
-			name: 'John',
-			data: [5, 3, 4, 7, 2]
-		}, {
-			name: 'Jane',
-			data: [2, -2, -3, 2, 1]
-		}, {
-			name: 'Joe',
-			data: [3, 4, 4, -2, 5]
-		}]
-	});
-});
+var chart;
+$(document).ready(function() {
+	chart = new Highcharts.Chart({
+		chart: {
+			renderTo: 'container',
+			type: 'area'
+		},
+		title: {
+			text: 'Area chart with negative values'
+		},
+		xAxis: {
+			categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+		},
+		tooltip: {
+			formatter: function() {
+				return ''+
+					this.series.name +': '+ this.y +'';
+			}
+		},
+		credits: {
+			enabled: false
+		},
+		series: [{
+			name: 'John',
+			data: [5, 3, 4, 7, 2]
+		}, {
+			name: 'Jane',
+			data: [2, -2, -3, 2, 1]
+		}, {
+			name: 'Joe',
+			data: [3, 4, 4, -2, 5]
+		}]
+	});
+});
