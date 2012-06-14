cb780c6e4adfd01105bdf3042280ed3fe8e5c835
Torstein Hønsi <torstein@highsoft.com>
2012-06-14 20:07:17 +0200

    Reorganized the Data module into a prototype object that can be extended and overridden from outside.

--- js/modules/data.src.js
+++ js/modules/data.src.js
@@ -1,7 +1,7 @@
 /**
  * @license Data plugin for Highcharts v0.1
  *
- * (c) 2011 Torstein Hønsi
+ * (c) 2012 Torstein Hønsi
  *
  * License: www.highcharts.com/license
  */
@@ -10,61 +10,53 @@
  * Demo: http://jsfiddle.net/highcharts/SnLFj/
  */
 
-(function(Highcharts) {
+(function(Highcharts) {	
 	
-	function trim(str) {
-		return str.replace(/^\s+|\s+$/g, '');
-	}
+	// Utilities
+	var each = Highcharts.each;
 	
-	/**
-	 * Parse numeric cells in to number types and date types in to true dates.
-	 * @param {Object} columns
-	 */
-	function parseTypes(columns) {
-		var col = columns.length, 
-			row,
-			val,
-			floatVal,
-			trimVal,
-			dateVal;
-			
-		while (col--) {
-			row = columns[col].length;
-			while (row--) {
-				val = columns[col][row];
-				floatVal = parseFloat(val);
-				trimVal = trim(val);
-				if (trimVal == floatVal) { // is numeric
-					columns[col][row] = floatVal;
-					columns[col].isNumeric = true;					
-				
-				} else { // string, continue to determine if it is a date string or really a string
-					dateVal = Date.parse(val);
-					
-					if (typeof dateVal === 'number' && !isNaN(dateVal)) { // is date
-						columns[col][row] = dateVal;
-						columns[col].isDatetime = true;
-					
-					} else { // string
-						columns[col][row] = trimVal;
-					}
-				}
-				
-			}
-		}
+	
+	// The Data constructor
+	var Data = function (options) {
+		this.init(options);
+	};
+	
+	// Set the prototype properties
+	Highcharts.extend(Data.prototype, {
 		
-	}
-	
-	
+	/**
+	 * Initialize the Data object with the given options
+	 */
+	init: function (options) {
+		this.options = options;
+		this.columns = [];
+		
+        
+        // Parse a CSV if options.csv is given
+        this.parseCSV();
+        
+        // Interpret the values into right types
+        this.parseTypes();
+        
+        // Use first row for series names?
+        this.findHeaderRow();
+        
+        // Complete if a complete callback is given
+        this.complete();
+        
+    },
     
-    Highcharts.data = function(options) {
-        var columns = [],
-        	each = Highcharts.each,
-        	hasXData,
-        	firstRowIsHeader = true;
-        	
-        if (options.csv) {
-        	var lines = options.csv.split(options.lineDelimiter || '\n');
+    /**
+     * Parse a CSV input string
+     */
+    parseCSV: function () {
+    	var options = this.options,
+    		csv = options.csv,
+    		columns = this.columns,
+    		lines;
+    		
+    	if (csv) {
+        	lines = csv.split(options.lineDelimiter || '\n');
         	
         	each(lines, function (line, rowNo) {
         		var items = line.split(options.itemDelimiter || ',');
@@ -79,29 +71,100 @@
         		});
         	});
         }
-        
-        // Parse into right types
-        parseTypes(columns);
-        
-        // Use first row for series names?
-        each(columns, function (column) {
+    },
+    
+    /**
+     * Find the header row. For now, we just check whether the first row contains
+     * numbers or strings. Later we could loop down and find the first row with 
+     * numbers.
+     */
+    findHeaderRow: function () {
+    	var headerRow = 0;
+    	each(this.columns, function (column) {
         	if (typeof column[0] !== 'string') {
-        		firstRowIsHeader = false;
+        		headerRow = null;
         	}
         });
-        
-        
-    	// If a user defined complete function exists, parse the columns 
-    	// into a Highcharts options object.
+        this.headerRow = 0;        	
+    },
+    
+   	/**
+   	 * Trim a string from whitespace
+   	 */
+   	trim: function (str) {
+		return str.replace(/^\s+|\s+$/g, '');
+	},
+	
+	/**
+	 * Parse numeric cells in to number types and date types in to true dates.
+	 * @param {Object} columns
+	 */
+	parseTypes: function () {
+		var columns = this.columns,
+			col = columns.length, 
+			row,
+			val,
+			floatVal,
+			trimVal,
+			dateVal;
+			
+		while (col--) {
+			row = columns[col].length;
+			while (row--) {
+				val = columns[col][row];
+				floatVal = parseFloat(val);
+				trimVal = this.trim(val);
+				if (trimVal == floatVal) { // is numeric
+					columns[col][row] = floatVal;
+					
+					// If the number is greater than milliseconds in a year, assume datetime
+					if (floatVal > 365 * 24 * 3600 * 1000) {
+						columns[col].isDatetime = true;
+					} else {
+						columns[col].isNumeric = true;
+					}					
+				
+				} else { // string, continue to determine if it is a date string or really a string
+					dateVal = Date.parse(val);
+					
+					if (col === 0 && typeof dateVal === 'number' && !isNaN(dateVal)) { // is date
+						columns[col][row] = dateVal;
+						columns[col].isDatetime = true;
+					
+					} else { // string
+						columns[col][row] = trimVal;
+					}
+				}
+				
+			}
+		}		
+	},
+	
+	/**
+	 * If a complete callback function is provided in the options, interpret the 
+	 * columns into a Highcharts options object.
+	 */
+	complete: function () {
+		
+		var columns = this.columns,
+			hasXData,
+			categories,
+			firstCol,
+			type,
+			options = this.options,
+			series,
+			data,
+			name,
+			i,
+			j;
+			
+		
     	if (options.complete) {
-    		var categories,
-    			firstCol,
-    			type;
-            
+    	    
             // Use first column for X data or categories?
             if (columns.length > 1) {
 	        	firstCol = columns.shift();
-	        	if (firstRowIsHeader) {
+	        	if (this.headerRow === 0) {
 	        		firstCol.shift(); // remove the first cell
 	        	}
 	            
@@ -117,15 +180,13 @@
 	        }
             
             // Use the next columns for series
-            var series = [],
-                data,
-                name;
+            series = [];
             for (i = 0; i < columns.length; i++) {
-            	if (firstRowIsHeader) {
+            	if (this.headerRow === 0) {
                 	name = columns[i].shift();
                	}
                 data = [];
-                for (var j = 0; j < columns[i].length; j++) {
+                for (j = 0; j < columns[i].length; j++) {
                     data[j] = columns[i][j] !== undefined ?
                         (hasXData ?
                         	[firstCol[j], columns[i][j]] :
@@ -139,6 +200,7 @@
                 };
             }
             
+            // Do the callback
             options.complete({
                 xAxis: {
                     categories: categories,
@@ -147,5 +209,12 @@
                 series: series
             });
         }
-   };
+    }
+	});
+	
+	// Register the Data prototype and data function on Highcharts
+	Highcharts.Data = Data;
+	Highcharts.data = function (options) {
+		return new Data(options);
+	};
 })(Highcharts);
