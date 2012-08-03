30ee7b1d1966c14dce77290f34ee91da19efa98a
Torstein Hønsi <torstein@highsoft.com>
2012-08-03 10:19:39 +0200

    Fixed tooltip point format for ranges

--- js/highcharts-more.src.js
+++ js/highcharts-more.src.js
@@ -659,7 +659,7 @@ defaultPlotOptions.arearange = merge(defaultPlotOptions.area, {
 	marker: null,
 	threshold: null,
 	tooltip: {
-		pointFormat: '<span style="color:{series.color}">{series.name}</span>: {point.low} - {point.high}' 
+		pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.low}</b> - <b>{point.high}</b><br/>' 
 	},
 	trackByArea: true,
 	dataLabels: {
