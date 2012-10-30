5e7c1c6dcffe35b1073bba6cc267eb653e49b5c9
Torstein Hønsi <torstein@highsoft.com>
2012-10-30 13:40:11 +0100

    Fixed issue with useHTM, fixed width and word wrapping in Firefox. Closes #1254.

--- js/parts/SvgRenderer.js
+++ js/parts/SvgRenderer.js
@@ -591,7 +591,7 @@ SVGElement.prototype = {
 				height = pick(wrapper.elemHeight, elem.offsetHeight);
 
 				// update textWidth
-				if (width > textWidth && /[ \-]/.test(elem.innerText)) { // #983
+				if (width > textWidth && /[ \-]/.test(elem.textContent || elem.innerText)) { // #983, #1254
 					css(elem, {
 						width: textWidth + PX,
 						display: 'block',
