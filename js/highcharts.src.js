be010b96edd710d0b6a9562b095692afce897421
Torstein Hønsi <torstein@highsoft.com>
2013-01-03 13:10:52 +0100

    Fixed issue where tooltip didn't work on scatter series when tooltip was set to shared. Made the scatter tooltip ignore the tooltip.share option. Closes #1413.

--- js/highcharts.src.js
+++ js/highcharts.src.js
@@ -14435,6 +14435,7 @@ var ScatterSeries = extendClass(Series, {
 	type: 'scatter',
 	sorted: false,
 	requireSorting: false,
+	noSharedTooltip: true,
 	/**
 	 * Extend the base Series' translate method by adding shape type and
 	 * arguments for the point trackers
