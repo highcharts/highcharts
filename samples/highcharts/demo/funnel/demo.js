ba6415642764df2730700dc2e2360225711f0975
Torstein Hønsi <torstein@highsoft.com>
2013-03-23 13:11:31 +0100

    Added demos

---
+++ samples/highcharts/demo/funnel/demo.js
@@ -0,0 +1,42 @@
+$(function () {
+    
+    $('#container').highcharts({
+        chart: {
+            type: 'funnel',
+            marginRight: 100
+        },
+        title: {
+            text: 'Sales funnel',
+            x: -50
+        },
+        plotOptions: {
+            series: {
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b> ({point.y:,.0f})',
+                    color: 'black',
+                    softConnector: true
+                },
+                neckWidth: '30%',
+                neckHeight: '25%'
+                
+                //-- Other available options
+                // height: pixels or percent
+                // width: pixels or percent
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        series: [{
+            name: 'Unique users',
+            data: [
+                ['Website visits',   15654],
+                ['Downloads',       4064],
+                ['Requested price list', 1987],
+                ['Invoice sent',    976],
+                ['Finalized',    846]
+            ]
+        }]
+    });
+});
