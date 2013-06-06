b2b78abe584d7d0976f34c73ad087a1ebed237fd
Torstein Hønsi <torstein@highsoft.com>
2013-06-06 09:03:51 +0200

    Removed bad character from jQueryAdapter.js. Closes #1921.

--- js/parts/JQueryAdapter.js
+++ js/parts/JQueryAdapter.js
@@ -64,7 +64,7 @@
 
 			// Extend the opacity getter, needed for fading opacity with IE9 and jQuery 1.10+
 			wrap(opacityHook, 'get', function (proceed, elem, computed) {
-				return elem.attr ? (elem.opacity || 0) : proceed.call(this, elem, computed);
+				return elem.attr ? (elem.opacity || 0) : proceed.call(this, elem, computed);
 			});
 			
 			
