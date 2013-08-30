420036bdf97f52bc1c323f8d57659f5e2b4fb1d2
oysteinmoseng <oystein@highsoft.com>
2013-08-30 11:16:41 +0200

    Fixed issue with removing points in funnel charts. Closes #2206

--- js/highstock.src.js
+++ js/highstock.src.js
@@ -12480,6 +12480,7 @@ Point.prototype = {
 	remove: function (redraw, animation) {
 		var point = this,
 			series = point.series,
+			points = series.points,
 			chart = series.chart,
 			i,
 			data = series.data;
@@ -12492,6 +12493,9 @@ Point.prototype = {
 
 			// splice all the parallel arrays
 			i = inArray(point, data);
+			if (data.length === points.length) {
+				points.splice(i, 1);			
+			}
 			data.splice(i, 1);
 			series.options.data.splice(i, 1);
 			series.xData.splice(i, 1);
