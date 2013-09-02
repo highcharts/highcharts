b3c327c416accc5dc517c510f84db74b6664f251
oysteinmoseng <oystein@highsoft.com>
2013-09-02 10:06:28 +0200

    Added samples for no-data-to-display plugin

---
+++ samples/highcharts/no-data-to-display/no-data-line/demo.js
@@ -0,0 +1,48 @@
+$(function () {
+
+    var chart,
+        btnRemove = $('#remove'),
+        btnAdd = $('#add'),
+        btnShow = $('#showCustom');
+
+    btnAdd.click(function () {              
+        chart.series[0].addPoint(Math.floor(Math.random() * 10 + 1)); // Return random integer between 1 and 10. 
+    });
+
+    btnRemove.click(function () {     
+        if(chart.series[0].points[0]) {
+            chart.series[0].points[0].remove();
+        }
+    });
+
+    btnShow.click(function () {
+        if(!chart.hasData()) {
+            chart.hideNoData();
+            chart.showNoData("Your custom error message");
+        }
+    });
+
+    $('#container').highcharts({
+        title: {
+            text: 'No data in line chart - with custom options'
+        },
+        series: [{
+            type: 'line',
+            name: 'Random data',  
+            data: []     
+        }],        
+        lang: {
+            noData: "Nichts zu anzeigen"
+        },
+        noData: {
+            style: {    
+                fontWeight: 'bold',     
+                fontSize: '15px',
+                color: '#303030'        
+            }
+        }
+    });
+
+    chart = $('#container').highcharts();
+});
+    
---
+++ samples/highcharts/no-data-to-display/no-data-pie/demo.js
@@ -0,0 +1,35 @@
+$(function () {
+
+    var chart,
+        btnRemove = $('#remove'),
+        btnAdd = $('#add');
+
+    btnAdd.click(function () {       
+        chart.series[0].addPoint(Math.floor(Math.random() * 10 + 1)); // Return random integer between 1 and 10. 
+    });
+
+    btnRemove.click(function () {     
+        if(chart.series[0].points[0]) {
+            chart.series[0].points[0].remove();
+        }
+    });
+
+    $('#container').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: 'No data in pie chart'
+        },
+        series: [{
+            type: 'pie',
+            name: 'Random data',  
+            data: []     
+        }]
+    });
+
+    chart = $('#container').highcharts();
+});
+    
