37286943f4450b8f7ef5a9ef093b7c6df33707c2
Torstein Hønsi <torstein@highsoft.com>
2013-11-08 08:49:39 +0100

    Utils: Added visual Git branch tree to the issue-by-commit tool.

--- utils/issue-by-commit/commits.php
+++ utils/issue-by-commit/commits.php
@@ -6,8 +6,130 @@
 	
 	<head>
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+		<script type="text/javascript" src="http://code.highcharts.com/highcharts.js"></script>
 		
 		<script type="text/javascript">
+			var ren,
+				paths = [];
+			
+			// Draw the lines connecting the dots
+			function drawGraph() {
+
+				var h = $('#ul').height();
+				
+				// First time
+				if (!ren) {
+					ren = new Highcharts.Renderer(
+						$('#graph')[0],
+						300,
+						h
+					);
+				// Update
+				} else { 
+					$.each(paths, function (i, path) {
+						paths[i] = path.destroy();
+					});
+					paths.length = 0;
+				}
+				$('div.parents').each(function (i, item) {
+					var $dot = $(item),
+						doffset = $dot && $dot.offset(),
+						parents = $(item).data('parents');
+
+					$.each(parents, function (i, parent) {
+						var $parentDot = $('#dot-' + parent),
+							poffset = $parentDot && $parentDot.offset(),
+							path,
+							dLeft,
+							pLeft,
+							color;
+
+						if (doffset && poffset) {
+
+							dLeft = Math.round($dot.data('left')) + 9;
+							pLeft = Math.round($parentDot.data('left')) + 9;
+
+							// Straight line
+							if (dLeft === pLeft) {
+								path = ['M', dLeft, doffset.top,
+									'L', pLeft, poffset.top];
+								color = $dot.data('color');
+
+							// Curve, merge
+							} else if (dLeft < pLeft) {
+								path = ['M', dLeft, doffset.top,
+									'C', dLeft, doffset.top + 10,
+									pLeft, doffset.top + 10,
+									pLeft, doffset.top + 20,
+									'L',
+									pLeft, poffset.top];
+								color = $parentDot.data('color');
+
+							// Curve, fork
+							} else {
+								path = ['M', dLeft, doffset.top,
+									'L', dLeft, poffset.top - 20,
+									'C', dLeft, poffset.top - 10,
+									pLeft, poffset.top - 10,
+									pLeft, poffset.top];
+								color = $dot.data('color');
+							}
+
+							paths.push(ren.path(path)
+								.attr({
+									'stroke-width': 2,
+									stroke: color
+								})
+								.add());
+						}
+					});
+				});
+			};
+
+			/**
+			 * Indent the text to reveal the graph
+			 */
+			function indent() {
+				var $rightMost,
+					marginLeft;
+				$('div.parents').each(function (i, item) {
+					var $dot = $(item),
+						doffset = $dot && $dot.offset(),
+						parents = $(item).data('parents');
+
+					$.each(parents, function (i, parent) {
+						var $parentDot = $('#dot-' + parent),
+							poffset = $parentDot && $parentDot.offset(),
+							dLeft,
+							pLeft;
+
+						if ($rightMost === undefined) {
+							$rightMost = $dot;
+						}
+								
+							
+						if (doffset && poffset) {
+							dLeft = Math.round($rightMost.data('left'));
+							pLeft = Math.round($parentDot.data('left'));
+							
+							if (pLeft >= dLeft)  {
+								$rightMost = $parentDot;
+							}
+						}
+
+						marginLeft = $rightMost.data('left');
+						$dot.parent().find('a').css('marginLeft', marginLeft + 20);
+						$dot.parent().find('span.date').css('marginLeft', marginLeft + 20);
+
+
+						if ($rightMost[0] === $dot[0]) {
+							$rightMost = undefined;
+						}
+					});
+				});
+
+			}
+
 			$(function() {
 				var $active,
 					month,
@@ -15,6 +137,16 @@
 					parentHierarchy = {};
 
 				var colors = [
+					   '#2f7ed8', 
+					   '#0d233a', 
+					   '#8bbc21', 
+					   '#910000', 
+					   '#1aadce', 
+					   '#492970',
+					   '#f28f43', 
+					   '#77a1e5', 
+					   '#c42525', 
+					   '#a6c96a',
 					   '#2f7ed8', 
 					   '#0d233a', 
 					   '#8bbc21', 
@@ -63,16 +195,30 @@
 							} else {
 								branchI = branchCounter++;
 							}
-							parentHierarchy[parents[0]] = branchI;
+							if (parentHierarchy[parents[0]] !== undefined) {
+								branchCounter--;
+							} else {
+								parentHierarchy[parents[0]] = branchI;
+							}
 							if (parents[1]) {
 								parentHierarchy[parents[1]] = branchCounter++;
 							}
+							var marginLeft = branchI * 10;
 
 							var par = $('<div>')
 								.attr({
-									'class': 'parents'
+									'class': 'parents',
+									title: 'hash: '+ commit + ', parent: ' + parents.join(', '),
+									id: 'dot-' + commit
 								})
-								.html('<div class="disc" style="background-color: '+ colors[branchI] + '"></div>')
+								.data({
+									hash: commit,
+									parents: parents,
+									left: marginLeft,
+									color: colors[branchI]
+								})
+								.html('<div class="disc" style="background-color: '+ colors[branchI] + 
+									';margin-left:' + marginLeft + 'px"></div>')
 								.appendTo($li);
 
 
@@ -117,7 +263,13 @@
 						}
 							
 					});
+
+
+					indent();
+					drawGraph();
 				});
+
+				$(window).bind('resize', drawGraph);
 			});
 		</script>
 		
@@ -128,6 +280,7 @@
 			}
 			h3 {
 				font-size: 16pt;
+				background: white;
 			}
 			ul {
 				margin: 50px 1em 1em 1em;
@@ -137,15 +290,15 @@
 			
 			a {
 				text-decoration: none;
+				border-right: 10px solid white;
+				background: rgba(255,255,255,0.75);
 			}
-
 
 			a.visited {
 				color: silver;
 			}
 			a.active {
-				color: black;
-				font-weight: bold;
+				border-right-color: black;
 			}
 			
 			body {
@@ -154,6 +307,8 @@
 
 			ul {
 				list-style-type: none;
+				position: relative;
+
 			}
 
 			ul li {
@@ -164,19 +319,17 @@
 			.date {
 				color: gray;
 				display: block;
-				margin-left: 20px;
+				background: white;
 			}
 			.parents {
-				width: 20px;
 				position: absolute;
 			}
 			.parents .disc {
-				width: 14px;
-				height: 14px;
-				border-radius: 7px;
+				width: 10px;
+				height: 10px;
+				border-radius: 5px;
 			}
 			.message {
-				margin-left: 20px;
 				display: block;
 			}
 			.status {
@@ -199,6 +352,7 @@
 
 			#topnav {
 				position: fixed;
+				z-index: 2;
 				top: 0;
 				box-shadow: 5px 5px 5px #888;
 				background: white;
@@ -215,6 +369,12 @@
 			#topnav span {
 				padding-left: 5px;
 			}
+			#graph {
+				position: absolute;
+				width: 100%;
+				left: 20px;
+				top: 0;
+			}
 			
 		</style>
 	</head>
@@ -224,6 +384,7 @@
 			<span id="loaded"></span>
 			<a href="main.php" target="main">Set new test data</a>
 		</div>
+		<div id="graph"></div>
 		<ul id="ul"></ul>
 		
 	</body>
