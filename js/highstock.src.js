8026d7ee7b2c63c078f99d53e7757287e91b526c
Manuel Carrasco Moñino <manuel.carrasco.m@gmail.com>
2013-11-16 13:43:41 +0100

    set Highcharts var which is used several times without namespace, making highchars fail when it is initialized in a different context

--- js/highstock.src.js
+++ js/highstock.src.js
@@ -122,7 +122,7 @@ var UNDEFINED,
 	seriesTypes = {};
 
 // The Highcharts namespace
-win.Highcharts = win.Highcharts ? error(16, true) : {};
+var Highcharts = win.Highcharts = win.Highcharts ? error(16, true) : {};
 
 /**
  * Extend an object with the members of another
