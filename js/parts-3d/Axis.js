44e72d0ac190a253cbbba2c25eeae14117d4eeb5
stephanevanraes <stephane@highsoft.com>
2013-11-22 15:31:20 +0100

    3D: added tick marks and labels

--- js/parts-3d/Axis.js
+++ js/parts-3d/Axis.js
@@ -16,50 +16,79 @@ H.wrap(HA.prototype, 'render', function (proceed) {
 		z: chart.getTotalDepth()
 	};
 
+	var frame = options3d.frame || { x: this.lineWidth, y : [this.lineWidth, this.lineWidth] };
+
 	// AxisLines --> replace with flat cubes
-	if (options.lineWidth > 0) {
-		axis.axisLine.destroy();
+	if (axis.axisLine) axis.axisLine.destroy();
 
-		var x1 = this.left,
-			y1 = (this.horiz ? this.top + this.height : this.top),
-			z1 = 0,
-			h = (this.horiz ? options.lineWidth * 5 : this.height),
-			w = (this.horiz ? this.len : options.lineWidth * 5),
-			d = chart.getTotalDepth();
+	var x1 = this.left,
+		y1 = (this.horiz ? this.top + this.height : this.top),
+		z1 = 0,
+		h = (this.horiz ? frame.x : this.height),
+		w = (this.horiz ? [this.len] : frame.y),
+		d = chart.getTotalDepth();
 
-		var nstacks = chart.getNumberOfStacks();
+	var nstacks = chart.getNumberOfStacks();
 
-		if (this.horiz) {
-			axis.axisLine  = renderer.cube(x1, y1, z1, w, h, d, options3d)
-				.attr({
-					fill: options.lineColor,
-					zIndex: nstacks + 2
-				})
-				.add(axis.axisGroup);
-		} else {
-			var axisLineGroup = renderer.createElement3D().add(axis.axisGroup);
+	if (this.horiz) {
+		axis.axisLine  = renderer.cube(x1, y1, z1, w[0], h, d, options3d)
+			.attr({
+				fill: options.lineColor,
+				zIndex: nstacks + 2
+			})
+			.add(axis.axisGroup);
+	} else {
+		var axisLineGroup = renderer.createElement3D().add(axis.axisGroup);
+		
+		// back			
+		var back = renderer.cube(x1 - w[0], y1, z1 + d, this.width + w[0], h + options3d.frame.x, w[1], options3d)
+			.attr({
+				fill: options.lineColor,
+				zIndex: nstacks + 2
+			})
+			.add(axis.axisLineGroup);
+		axisLineGroup.children.push(back);
+		
+		// side
+		var side = renderer.cube(x1 - w[0], y1, z1, w[0], h + frame.x, d, options3d)
+			.attr({
+				fill: options.lineColor,
+				zIndex: nstacks + 2
+			})
+			.add(axis.axisLineGroup);
+		axisLineGroup.children.push(side);
 
-			// back			
-			var back = renderer.cube(x1 - w, y1, z1 + d, this.width + w, h + w, w, options3d)
-				.attr({
-					fill: options.lineColor,
-					zIndex: nstacks + 2
-				})
-				.add(axis.axisLineGroup);
-			axisLineGroup.children.push(back);
-			
-			// side
-			var side = renderer.cube(x1 - w, y1, z1, w, h + w, d, options3d)
-				.attr({
-					fill: options.lineColor,
-					zIndex: nstacks + 2
-				})
-				.add(axis.axisLineGroup);
-			axisLineGroup.children.push(side);
-
-			axis.axisLine = axisLineGroup;
-		}
+		axis.axisLine = axisLineGroup;
 	}
+	
+
+	H.each(axis.tickPositions, function (tickPos) {
+		var tick = axis.ticks[tickPos],
+		label = tick.label,
+		mark = tick.mark;
+
+		if (label) {
+			var xy = label.xy,
+			labelPos = perspective([{x: xy.x, y: xy.y, z: z1 }], options3d)[0];
+
+			label['animate']({
+				x: labelPos.x,
+				y: labelPos.y,
+				opacity: xy.opacity					
+			});
+		}
+
+		if (mark) {
+			var path = (mark.toD ? mark.toD : mark.d.split(' ')),
+			pArr = [ 
+			{x: path[1], y: path[2], z: z1 },
+			{x: path[4], y: path[5], z: z1 }
+			];
+			path = chart.renderer.toLinePath(perspective(pArr, options3d), false);
+			mark.animate({d: path, opacity: 1});
+		}
+	});
+
 });
 
 H.wrap(HA.prototype, 'getPlotLinePath', function (proceed) {
