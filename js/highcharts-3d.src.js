bfc6045a788bd8c7228be7ccdafeba20587155c6
stephanevanraes <stephane@highsoft.com>
2013-11-25 14:03:55 +0100

    3D: changed frame x/y to bottom/back/side

--- js/highcharts-3d.src.js
+++ js/highcharts-3d.src.js
@@ -211,8 +211,9 @@ H.wrap(HC.prototype, 'init', function (proceed, userOptions, callback) {
 				angle2: 0,
 				depth: 0,
 				frame: {
-					x: 0,
-					y: [0, 0]
+					bottom: 0,
+					side: 0,
+					back: 0
 				}
 			}
 		}
@@ -297,23 +298,23 @@ H.wrap(HA.prototype, 'render', function (proceed) {
 		z: chart.getTotalDepth()
 	};
 
-	// AxisLines --> replace with flat cubes
-	if (axis.axisLine) {
-		axis.axisLine.destroy();
-	}
-
 	var x1 = this.left,
 		y1 = (this.horiz ? this.top + this.height : this.top),
 		z1 = 0,
-		h = (this.horiz ? frame.x : this.height),
-		w = (this.horiz ? [this.len] : frame.y),
+		h = this.height,
+		w = this.len,
 		d = chart.getTotalDepth();
 
 	var nstacks = chart.getNumberOfStacks();
+	var fbottom = frame.bottom,
+		fside = frame.side,
+		fback = frame.back;
 
-
-	if (this.horiz && h !== 0) {
-		axis.axisLine  = renderer.cube(x1, y1, z1, w[0], h, d, options3d)
+	if (this.horiz && fbottom !== 0) {
+		if (axis.axisLine) { 
+				axis.axisLine.destroy();
+			}
+		axis.axisLine  = renderer.cube(x1, y1, z1, w, fbottom, d, options3d)
 			.attr({
 				fill: options.lineColor,
 				zIndex: nstacks + 2
@@ -321,10 +322,9 @@ H.wrap(HA.prototype, 'render', function (proceed) {
 			.add(axis.axisGroup);
 	} else if (!this.horiz) {
 		var axisLineGroup = renderer.createElement3D().add(axis.axisGroup);
-		
 		// back
-		if (w[1] !== 0) {
-			var back = renderer.cube(x1 - w[0], y1, z1 + d, this.width + w[0], h + frame.x, w[1], options3d)
+		if (fback) {
+			var back = renderer.cube(x1 - fside, y1, z1 + d, this.width + fside, h + fbottom, fback, options3d)
 				.attr({
 					fill: options.lineColor,
 					zIndex: nstacks + 2
@@ -333,14 +333,19 @@ H.wrap(HA.prototype, 'render', function (proceed) {
 			axisLineGroup.children.push(back);
 		}
 		// side
-		if (w[0] !== 0) {
-			var side = renderer.cube(x1 - w[0], y1, z1, w[0], h + frame.x, d, options3d)
+		if (fside) {
+			if (axis.axisLine) { 
+				axis.axisLine.destroy();
+			}
+			var side = renderer.cube(x1 - fside, y1, z1, fside, h + fbottom, d, options3d)
 				.attr({
 					fill: options.lineColor,
 					zIndex: nstacks + 2
 				})
 				.add(axis.axisLineGroup);
 			axisLineGroup.children.push(side);
+		} else {
+			axisLineGroup.children.push(axis.axisLine);
 		}
 		axis.axisLine = axisLineGroup;
 	}
