35950d57bac7ddc8d12a4ed03c8351773241c68b
Torstein Hønsi <torstein@highsoft.com>
2013-12-13 20:09:25 +0100

    New column drilldown demo using the drilldown module.

--- samples/highcharts/demo/column-drilldown/demo.js
+++ samples/highcharts/demo/column-drilldown/demo.js
@@ -1,134 +1,124 @@
 $(function () {
-    
-        var colors = Highcharts.getOptions().colors,
-            categories = ['MSIE', 'Firefox', 'Chrome', 'Safari', 'Opera'],
-            name = 'Browser brands',
-            data = [{
-                    y: 55.11,
-                    color: colors[0],
-                    drilldown: {
-                        name: 'MSIE versions',
-                        categories: ['MSIE 6.0', 'MSIE 7.0', 'MSIE 8.0', 'MSIE 9.0'],
-                        data: [10.85, 7.35, 33.06, 2.81],
-                        color: colors[0]
-                    }
-                }, {
-                    y: 21.63,
-                    color: colors[1],
-                    drilldown: {
-                        name: 'Firefox versions',
-                        categories: ['Firefox 2.0', 'Firefox 3.0', 'Firefox 3.5', 'Firefox 3.6', 'Firefox 4.0'],
-                        data: [0.20, 0.83, 1.58, 13.12, 5.43],
-                        color: colors[1]
-                    }
-                }, {
-                    y: 11.94,
-                    color: colors[2],
-                    drilldown: {
-                        name: 'Chrome versions',
-                        categories: ['Chrome 5.0', 'Chrome 6.0', 'Chrome 7.0', 'Chrome 8.0', 'Chrome 9.0',
-                            'Chrome 10.0', 'Chrome 11.0', 'Chrome 12.0'],
-                        data: [0.12, 0.19, 0.12, 0.36, 0.32, 9.91, 0.50, 0.22],
-                        color: colors[2]
-                    }
-                }, {
-                    y: 7.15,
-                    color: colors[3],
-                    drilldown: {
-                        name: 'Safari versions',
-                        categories: ['Safari 5.0', 'Safari 4.0', 'Safari Win 5.0', 'Safari 4.1', 'Safari/Maxthon',
-                            'Safari 3.1', 'Safari 4.1'],
-                        data: [4.55, 1.42, 0.23, 0.21, 0.20, 0.19, 0.14],
-                        color: colors[3]
-                    }
-                }, {
-                    y: 2.14,
-                    color: colors[4],
-                    drilldown: {
-                        name: 'Opera versions',
-                        categories: ['Opera 9.x', 'Opera 10.x', 'Opera 11.x'],
-                        data: [ 0.12, 0.37, 1.65],
-                        color: colors[4]
-                    }
-                }];
-    
-        function setChart(name, categories, data, color) {
-			chart.xAxis[0].setCategories(categories, false);
-			chart.series[0].remove(false);
-			chart.addSeries({
-				name: name,
-				data: data,
-				color: color || 'white'
-			}, false);
-			chart.redraw();
-        }
-    
-        var chart = $('#container').highcharts({
-            chart: {
-                type: 'column'
-            },
-            title: {
-                text: 'Browser market share, April, 2011'
-            },
-            subtitle: {
-                text: 'Click the columns to view versions. Click again to view brands.'
-            },
-            xAxis: {
-                categories: categories
-            },
-            yAxis: {
-                title: {
-                    text: 'Total percent market share'
+
+    Highcharts.data({
+        csv: document.getElementById('tsv').innerHTML,
+        itemDelimiter: '\t',
+        parsed: function (columns) {
+
+            var brands = {},
+                brandsData = [],
+                versions = {},
+                drilldownSeries = [];
+            
+            // Parse percentage strings
+            columns[1] = $.map(columns[1], function (value) {
+                if (value.indexOf('%') === value.length - 1) {
+                    value = parseFloat(value);
                 }
-            },
-            plotOptions: {
-                column: {
-                    cursor: 'pointer',
-                    point: {
-                        events: {
-                            click: function() {
-                                var drilldown = this.drilldown;
-                                if (drilldown) { // drill down
-                                    setChart(drilldown.name, drilldown.categories, drilldown.data, drilldown.color);
-                                } else { // restore
-                                    setChart(name, categories, data);
-                                }
-                            }
-                        }
-                    },
-                    dataLabels: {
-                        enabled: true,
-                        color: colors[0],
-                        style: {
-                            fontWeight: 'bold'
-                        },
-                        formatter: function() {
-                            return this.y +'%';
-                        }
+                return value;
+            });
+
+            $.each(columns[0], function (i, name) {
+                var brand,
+                    version;
+
+                if (i > 0) {
+
+                    // Remove special edition notes
+                    name = name.split(' -')[0];
+
+                    // Split into brand and version
+                    version = name.match(/([0-9]+[\.0-9x]*)/);
+                    if (version) {
+                        version = version[0];
                     }
-                }
-            },
-            tooltip: {
-                formatter: function() {
-                    var point = this.point,
-                        s = this.x +':<b>'+ this.y +'% market share</b><br/>';
-                    if (point.drilldown) {
-                        s += 'Click to view '+ point.category +' versions';
+                    brand = name.replace(version, '');
+
+                    // Create the main data
+                    if (!brands[brand]) {
+                        brands[brand] = columns[1][i];
                     } else {
-                        s += 'Click to return to browser brands';
+                        brands[brand] += columns[1][i];
                     }
-                    return s;
+
+                    // Create the version data
+                    if (version !== null) {
+                        if (!versions[brand]) {
+                            versions[brand] = [];
+                        }
+                        versions[brand].push(['v' + version, columns[1][i]]);
+                    }
                 }
-            },
-            series: [{
-                name: name,
-                data: data,
-                color: 'white'
-            }],
-            exporting: {
-                enabled: false
-            }
-        })
-        .highcharts(); // return chart
+                
+            });
+
+            $.each(brands, function (name, y) {
+                brandsData.push({ 
+                    name: name, 
+                    y: y,
+                    drilldown: versions[name] ? name : null
+                });
+            });
+            $.each(versions, function (key, value) {
+                drilldownSeries.push({
+                    name: key,
+                    id: key,
+                    data: value
+                });
+            });
+
+            // Create the chart
+            $('#container').highcharts({
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: 'Browser market shares. November, 2013'
+                },
+                subtitle: {
+                    text: 'Click the columns to view versions. Source: netmarketshare.com.'
+                },
+                xAxis: {
+                    type: 'category'
+                },
+                yAxis: {
+                    title: {
+                        text: 'Total percent market share'
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                plotOptions: {
+                    series: {
+                        borderWidth: 0,
+                        dataLabels: {
+                            enabled: true,
+                            //color: colors[0],
+                            style: {
+                                fontWeight: 'bold'
+                            },
+                            format: '{point.y:.1f}%'
+                        }
+                    }
+                },
+
+                tooltip: {
+                    headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+                }, 
+
+                series: [{
+                    name: 'Brands',
+                    colorByPoint: true,
+                    data: brandsData
+                }],
+                drilldown: {
+                    series: drilldownSeries
+                }
+            })
+
+        }
     });
+});
     
