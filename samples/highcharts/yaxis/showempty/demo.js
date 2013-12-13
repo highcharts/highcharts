c0afa02dbf622e9859c58379c6a0bb001c318f86
Torstein Hønsi <torstein@highsoft.com>
2013-12-13 13:56:01 +0100

    Added demo for axis.showEmpty option.

---
+++ samples/highcharts/yaxis/showempty/demo.js
@@ -0,0 +1,25 @@
+$(function () {
+    $('#container').highcharts({
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        
+        yAxis: [{
+            lineWidth: 2
+            // showEmpty: true // by default
+        }, {
+            lineWidth: 2,
+            opposite: true,
+            showEmpty: false
+        }],
+
+        series: [{
+            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+            yAxis: 0
+        }, {
+            data: [106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4, 29.9, 71.5],
+            yAxis: 1,
+            visible: false
+        }]
+    });
+});
