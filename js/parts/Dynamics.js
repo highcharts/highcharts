dad207dbfc4a7ee8160526c9245f581fc546e3e2
Torstein Hønsi <torstein@highsoft.com>
2014-02-07 08:34:48 +0100

    Fixed issue with Axis.update not removing all associated series. Closes #2687.

--- js/parts/Dynamics.js
+++ js/parts/Dynamics.js
@@ -434,12 +434,16 @@ extend(Axis.prototype, {
      */
 	remove: function (redraw) {
 		var chart = this.chart,
-			key = this.coll; // xAxis or yAxis
+			key = this.coll, // xAxis or yAxis
+			axisSeries = this.series,
+			i = axisSeries.length;
 
-		// Remove associated series
-		each(this.series, function (series) {
-			series.remove(false);
-		});
+		// Remove associated series (#2687)
+		while (i--) {
+			if (axisSeries[i]) {
+				axisSeries[i].remove(false);
+			}
+		}
 
 		// Remove the axis
 		erase(chart.axes, this);
