8d50a8def564c548098c43d69385290061ef527c
Torstein Hønsi <torstein@highsoft.com>
2014-02-11 15:58:00 +0100

    Worked on sparkline demo.

--- samples/highcharts/demo/sparkline/demo.js
+++ samples/highcharts/demo/sparkline/demo.js
@@ -1,143 +1,142 @@
 
 $(function () {
-/*
-===============================
-=== Creating random data to use
-===============================
-*
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-var regions = ["Alabama", "Alaska ", "Arizona ", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
-jQuery(regions).each(function () {
-    var income_q = '',
-        income_t = 0,
-        cost_q = '',
-        cost_t = 0,
-        result_q = '',
-        result_t = 0,
-        row = '',
-        seperator = ', ';
-    for (i = 0; i < 4; i++) {
-        if (i == 3) {
-            seperator = ' ';
-        }
-        income = getRandomInt(0, 100);
-        cost = getRandomInt(0, 100);
-        result = income - cost;
-        income_q += income + seperator;
-        income_t += income;
-        cost_q += cost + seperator;
-        cost_t += cost;
-        result_q += result + seperator;
-        result_t += result;
-    }
-    row = '<tr><th>' + this + '</th><td>' + income_t + '</td><td data-sparkline="' + income_q + '"></td><td>' + cost_t + '</td><td data-sparkline="' + cost_q + '"></td><td>' + result_t + '</td><td data-sparkline="' + result_q + '; column"></td></tr>';
-    jQuery("#table-sparkline").append(row);
-});
-return;
-*/
-/*
-===================================
-=== END Creating random data to use
-===================================
-*/
-/*
-==========================
-=== Sparkling Magic Begins
-==========================
-*/
-Highcharts.SparkLine = function (options, callback) {
-    var defaultOptions = {
-        chart: {
-            renderTo: this,
-            type: 'area',
-            margin: [0, 0, 0, 0]
-        },
-        title: {
-            text: ''
-        },
-        credits: {
-            enabled: false
-        },
-        xAxis: {
-            labels: {
-                enabled: false
-            },
-            title: {
-                text: null
-            }
-        },
-        yAxis: {
-            maxPadding: 0,
-            minPadding: 0,
-            endOnTick: false,
-            labels: {
-                enabled: false
-            },
-            title: {
-                text: null
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        tooltip: {
-            enabled: false
-        },
-        plotOptions: {
-            series: {
-                lineWidth: 1,
-                shadow: false,
-                states: {
-                    hover: {
-                        lineWidth: 1
-                    }
-                },
-                marker: {
-                    radius: 1,
-                    states: {
-                        hover: {
-                            radius: 2
-                        }
-                    }
+    /**
+     * Create a constructor for sparklines that takes some sensible defaults and merges in the individual 
+     * chart options. This function is also available from the jQuery plugin as $(element).highcharts('SparkLine').
+     */
+    Highcharts.SparkLine = function (options, callback) {
+        var defaultOptions = {
+            chart: {
+                renderTo: (options.chart && options.chart.renderTo) || this,
+                type: 'area',
+                margin: [2, 0, 2, 0],
+                width: 120,
+                height: 20,
+                style: {
+                    overflow: 'visible'
                 }
             },
-            column: {
-                negativeColor: 'red'
+            title: {
+                text: ''
+            },
+            credits: {
+                enabled: false
+            },
+            xAxis: {
+                labels: {
+                    enabled: false
+                },
+                title: {
+                    text: null
+                },
+                startOnTick: false,
+                endOnTick: false,
+                tickPositions: []
+            },
+            yAxis: {
+                endOnTick: false,
+                startOnTick: false,
+                labels: {
+                    enabled: false
+                },
+                title: {
+                    text: null
+                },
+                tickPositions: [0]
+            },
+            legend: {
+                enabled: false
+            },
+            tooltip: {
+                backgroundColor: null,
+                borderWidth: 0,
+                shadow: false,
+                useHTML: true,
+                hideDelay: 0,
+                shared: true,
+                padding: 0,
+                positioner: function (w, h, point) {
+                    return { x: point.plotX - w / 2, y: point.plotY - h};
+                }
+            },
+            plotOptions: {
+                series: {
+                    animation: false,
+                    lineWidth: 1,
+                    shadow: false,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    marker: {
+                        radius: 1,
+                        states: {
+                            hover: {
+                                radius: 2
+                            }
+                        }
+                    },
+                    fillOpacity: 0.25
+                },
+                column: {
+                    negativeColor: '#910000',
+                    borderColor: 'silver'
+                }
             }
-        },
-        series: [{
-            color: '#666',
-            fillColor: 'rgba(204,204,204,.25)'
-        }]
+        };
+        options = Highcharts.merge(defaultOptions, options);
+
+        // Speed optimization by not having to poll for offsetWidth
+        Highcharts.Chart.prototype.cloneRenderTo = false; 
+
+        return new Highcharts.Chart(options, callback);
     };
-    options = Highcharts.merge(defaultOptions, options);
-    return new Highcharts.Chart(options, callback);
-};
-console.time('sparkline');
-jQuery("td[data-sparkline]").each(function () {
-    stringdata = jQuery(this).data('sparkline');
-    arr = stringdata.split('; ');
-    data = jQuery.map(arr[0].split(', '), function (value) {
-        return parseFloat(value);
-    });
-    chart = {};
-    if (arr[1]) {
-        chart.type = arr[1];
+
+    var start = +new Date(),
+        $tds = $("td[data-sparkline]"),
+        len = $tds.length,
+        chunks,
+        i;
+
+    // Creating 153 sparkline charts is quite fast in modern browsers, but IE8 and mobile
+    // can take some seconds, so we split the input into chunks and apply them in timeouts
+    // in order avoid locking up the browser process and allow interaction.
+    chunks = [];
+    for (i = 0; i < len; i += 20) {
+        chunks.push($tds.slice(i, i + 20));
     }
-    $(this).highcharts('SparkLine', {
-        series: [{
-            data: data
-        }],
-        chart: chart
+
+    i = 0;
+    $.each(chunks, function () {
+        var chunk = this;
+        setTimeout(function () {
+            $.each(chunk, function () {
+                stringdata = $(this).data('sparkline');
+                arr = stringdata.split('; ');
+                data = $.map(arr[0].split(', '), parseFloat);
+                chart = {};
+                if (arr[1]) {
+                    chart.type = arr[1];
+                }
+                $(this).highcharts('SparkLine', {
+                    series: [{
+                        data: data,
+                        pointStart: 1
+                    }],
+                    tooltip: {
+                        headerFormat: 'Q{point.x}<br/>',
+                        pointFormat: '<b>{point.y}.000</b> USD'
+                    },
+                    chart: chart
+                });
+
+                i++;
+                if (i === len) {
+                    $('#result').html('Generated ' + $tds.length + ' sparklines in ' + (new Date() - start) + ' ms');
+                }
+            });
+        }, 0);
     });
-});
-console.timeEnd('sparkline');
-/*
-==============================
-=== END Sparkling magic begins
-==============================
-*/
+    
 });
