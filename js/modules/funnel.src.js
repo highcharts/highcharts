08532a4198f14decef677f6cf18002f1c1046c0f
stephanevanraes <stephane@highsoft.com>
2014-03-04 14:40:09 +0100

    Added 'pyramid' type of series

--- js/modules/funnel.src.js
+++ js/modules/funnel.src.js
@@ -289,5 +289,16 @@ seriesTypes.funnel = Highcharts.extendClass(seriesTypes.pie, {
 
 });
 
+/** PYRAMID SERIES
+	A pyramid series is a special type of funnel, without neck and reversed by default.
+**/
+defaultOptions.plotOptions.pyramid = Highcharts.merge(defaultOptions.plotOptions.funnel, {        
+	neckWidth: '0%',
+	neckHeight: '0%',
+	reversed: true
+});
+Highcharts.seriesTypes.pyramid = Highcharts.extendClass(Highcharts.seriesTypes.funnel, {
+	type: 'pyramid'
+});
 
 }(Highcharts));
