deef06184799f8213b763d6b100fffeda633e727
Torstein Hønsi <torstein@highsoft.com>
2014-03-12 13:34:49 +0100

    Fixed Highstock demo

--- samples/stock/demo/candlestick-and-volume/demo.js
+++ samples/stock/demo/candlestick-and-volume/demo.js
@@ -46,14 +46,14 @@ $(function() {
 		        title: {
 		            text: 'OHLC'
 		        },
-		        height: 200,
+		        height: 160,
 		        lineWidth: 2
 		    }, {
 		        title: {
 		            text: 'Volume'
 		        },
-		        top: 300,
-		        height: 100,
+		        top: 248,
+		        height: 60,
 		        offset: 0,
 		        lineWidth: 2
 		    }],
