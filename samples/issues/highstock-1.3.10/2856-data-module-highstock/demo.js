9ed32db57cc9c10fbe61ac12468bd76bb5bd83c2
Torstein Hønsi <torstein@highsoft.com>
2014-03-28 12:17:26 +0100

    Highstock: Fixed #2856 causing the Data module not to work without an empty series definition.

---
+++ samples/issues/highstock-1.3.10/2856-data-module-highstock/demo.js
@@ -0,0 +1,21 @@
+$(function () {
+    // Create the chart
+    $('#container').highcharts('StockChart',{
+
+
+        rangeSelector: {
+            selected: 1,
+            inputEnabled: $('#container').width() > 480
+        },
+
+        title: {
+            text: 'AAPL Stock Price'
+        },
+
+        data: {
+            googleSpreadsheetKey: '0Am-0HKnuNa4MdDNTNS1FYnd6ZDA3R0NvN29yYnVoZFE',
+            googleSpreadsheetWorksheet: 2
+        },
+
+    });
+});
