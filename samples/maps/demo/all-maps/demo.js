2b1442dc7ddac1b2b52b2a7844f87480232a9ec2
Torstein Hønsi <torstein@highsoft.com>
2014-05-09 14:42:39 +0200

    Worked onn all-maps demo

--- samples/maps/demo/all-maps/demo.js
+++ samples/maps/demo/all-maps/demo.js
@@ -3,7 +3,9 @@ $(function () {
 	// Base path to maps
 	var baseMapPath = "http://code.highcharts.com/mapdata/1.0.0/",
 		showDataLabels = true, // Switch for data labels enabled/disabled
-		mapOptions = '<option value="custom/world.js">Search maps</option>'; // default selected
+		mapCount = 0,
+		searchText,
+		mapOptions = '';
 
 	// Populate dropdown menus and turn into jQuery UI widgets
 	$.each(Highcharts.mapDataIndex, function (mapGroup, maps) {
@@ -11,8 +13,11 @@ $(function () {
 		mapOptions += '<option>--- ' + mapGroup + ' ---</option>';
 		$.each(maps, function (desc, path) {
             mapOptions += '<option value="' + path + '">' + desc + '</option>';
+            mapCount++;
 		});
 	});
+	searchText = 'Search ' + mapCount + ' maps';
+	mapOptions = '<option value="custom/world.js">' + searchText + '</option>' + mapOptions;
 	$("#mapDropdown").append(mapOptions).combobox();
 
 	// Change map when item selected in dropdown 
@@ -21,17 +26,32 @@ $(function () {
 			mapKey = this.value.slice(0, -3),
 			svgPath = baseMapPath + mapKey + '.svg',
 			geojsonPath = baseMapPath + mapKey + '.geo.json',
-			javascriptPath = baseMapPath + this.value;
+			javascriptPath = baseMapPath + this.value,
+			isHeader = mapDesc.indexOf('---') === 0;
 
-        // Hide default selected
-        if (mapDesc === 'Search maps') {
-            mapDesc = '';
+		// Dim or highlight search box
+		if (mapDesc === searchText || isHeader) {
+			$('.custom-combobox-input').removeClass('valid');
+		} else {
+			$('.custom-combobox-input').addClass('valid');
+		}
+
+		if (isHeader) {
+			return;
+		}
+
+        // Show loading 
+        if ($("#container").highcharts()) {
+        	$("#container").highcharts().showLoading('<i class="fa fa-spinner fa-spin fa-2x"></i>');
         }
 
 		// Get map from server
 		$.getScript(javascriptPath, function () {
+
 			var mapGeoJSON = Highcharts.maps[mapKey],
-				data = [];
+				data = [],
+				parent,
+				match;
 
 			// Update info box download links
 			$("#download").html('<a target="_blank" href="' + svgPath +
@@ -46,15 +66,41 @@ $(function () {
 				});
 			});
 
+			// Show arrows the first time a real map is shown
+			if (mapDesc !== searchText) {
+				$('.selector .prev-next').show();
+				$('#sideBox').show();
+			}
+
+			
+			// Is there a layer above this?
+			if (/^countries\/[a-z]{3}\/[a-z]{3}-all$/.test(mapKey)) { // country
+				parent = {
+					desc: 'World',
+					key: 'custom/world'
+				};
+			} else if (match = mapKey.match(/^(countries\/[a-z]{3}\/[a-z]{3})-[a-z0-9]+-all$/)) { // admin1
+				parent = {
+					desc: $('option[value="' + match[1] + '-all.js"]').text(),
+					key: match[1] + '-all'
+				};
+			}
+			$('#up').html('');
+			if (parent) {
+				$('#up').append(
+					$('<a><i class="fa fa-angle-up"></i> ' + parent.desc + '</a>')
+					.click(function () {
+						$('#mapDropdown').val(parent.key).change();
+					})
+				);
+			}
+
+
 			// Instantiate chart
 			$("#container").highcharts('Map', {
 
 				title: {
-					text: 'Map Choice Demo'
-				},
-
-				subtitle: {
-					text: mapDesc
+					text: null
 				},
 
 				mapNavigation: {
@@ -85,9 +131,23 @@ $(function () {
                                 bBox = this.point.graphic && this.point.graphic.getBBox();
 							
                             if (props && bBox.width > 30 && bBox.height > 20) {
-                                return props['postal-code'] && props['postal-code'] !== 'NULL' ? props['postal-code'] :
-								    props.code;
+                                /*return props['postal-code'] && props['postal-code'] !== 'NULL' ? props['postal-code'] :
+								    props.code;*/
+								return this.point.name && (this.point.name.substr(0, 3) + '.')
                             }
+						}
+					},
+					point: {
+						events: {
+							// On click, look for a detailed map
+							click: function () {
+								var code = this.code.toLowerCase();
+								$('#mapDropdown option').each(function (i) {
+									if (this.value === 'countries/' + code + '/' + code + '-all.js') {
+										$('#mapDropdown').val(this.value).change();
+									}
+								});
+							}
 						}
 					}
 				}, {
