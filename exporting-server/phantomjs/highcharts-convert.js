f5eb1b528eb52c5ae78a7921af31e0f43509e846
gvaartjes <gert@highsoft.com>
2014-05-12 13:41:13 +0200

    Export: Relative paths to images should throw an error. Move logic away from sandbox.

--- exporting-server/phantomjs/highcharts-convert.js
+++ exporting-server/phantomjs/highcharts-convert.js
@@ -101,27 +101,24 @@
 			renderSVG,
 			convert,
 			exit,
-			interval;
+			interval,
+            counter;
 
-		messages.imagesLoaded = 'Highcharts.images.loaded';
 		messages.optionsParsed = 'Highcharts.options.parsed';
 		messages.callbackParsed = 'Highcharts.cb.parsed';
-		window.imagesLoaded = false;
+		
 		window.optionsParsed = false;
 		window.callbackParsed = false;
-
+        
 		page.onConsoleMessage = function (msg) {
-			//console.log(msg);
-
+			console.log(msg);
+            
 			/*
 			 * Ugly hack, but only way to get messages out of the 'page.evaluate()'
 			 * sandbox. If any, please contribute with improvements on this!
 			 */
-
-			if (msg === messages.imagesLoaded) {
-				window.imagesLoaded = true;
-			}
-			/* more ugly hacks, to check options or callback are properly parsed */
+			
+			/* to check options or callback are properly parsed */
 			if (msg === messages.optionsParsed) {
 				window.optionsParsed = true;
 			}
@@ -202,12 +199,40 @@
 				exit(base64);
 			}
 		};
-
+        
+        function decrementImgCounter() {
+            counter -= 1;
+            if (counter < 1) {                
+                window.imagesLoaded = true;
+            }
+        }
+        
+        function loadImages(imgUrls) {
+            var i, img;
+            counter = imgUrls.length;
+            for (i = 0; i < imgUrls.length; i += 1) {                    
+                img = new Image();                    
+                /* onload decrements the counter, also when error (perhaps 404), don't wait for this image to be loaded */
+                img.onload = img.onerror = decrementImgCounter;                    
+                /* force loading of images by setting the src attr.*/                    
+                img.src = imgUrls[i];
+            }
+        }
+        
 		renderSVG = function (svg) {
 			var svgFile;
-			// From this point we have loaded/or created a SVG
+			// From this point we have 'loaded' or 'created' a SVG
+            
+            // Do we have to load images?
+            if (svg.imgUrls.length > 0) {
+                loadImages(svg.imgUrls);
+            } else  {
+                 // no images present, no loading, no waiting
+                window.imagesLoaded = true;
+            }
+            
 			try {
-				if (outType.toLowerCase() === 'svg') {
+				if (outType.toLowerCase() === 'svg'&& false) {
 					// output svg
 					svg = svg.html.replace(/<svg /, '<svg xmlns:xlink="http://www.w3.org/1999/xlink" ').replace(/ href=/g, ' xlink:href=').replace(/<\/svg>.*?$/, '</svg>');
 					// add xml doc type
@@ -229,7 +254,7 @@
 					if (!window.imagesLoaded) {
 						// render with interval, waiting for all images loaded
 						interval = window.setInterval(function () {
-							console.log('waiting');
+							
 							if (window.imagesLoaded) {
 								clearTimeout(timer);
 								clearInterval(interval);
@@ -252,44 +277,16 @@
 			}
 		};
 
-		loadChart = function (input, outputType, messages) {
-			var nodeIter, nodes, elem, opacity, counter, svgElem;
+		loadChart = function (input, outputType) {
+			var nodeIter, nodes, elem, opacity, svgElem, imgs, imgUrls, imgIndex;
 
 			document.body.style.margin = '0px';
 			document.body.innerHTML = input;
 
-			function decrementImgCounter() {
-				counter -= 1;
-				if (counter < 1) {
-					console.log(messages.imagesLoaded);
-				}
-			}
-
-			function loadImages() {
-				var images = document.getElementsByTagName('image'), i, img;
-
-				if (images.length > 0) {
-
-					counter = images.length;
-
-					for (i = 0; i < images.length; i += 1) {
-						img = new Image();
-						/* onload decremnts the counter, also when error (perhaps 404), then we wont wait for this image to be loaded */
-						img.onload = img.onerror = decrementImgCounter;
-						/* force loading of images by setting the src attr.*/
-						img.src = images[i].href.baseVal;
-					}
-				} else {
-					// no images set property to:imagesLoaded = true
-					console.log(messages.imagesLoaded);
-				}
-			}
-
 			if (outputType === 'jpeg') {
 				document.body.style.backgroundColor = 'white';
 			}
-
-
+            
 			nodes = document.querySelectorAll('*[stroke-opacity]');
 
 			for (nodeIter = 0; nodeIter < nodes.length; nodeIter += 1) {
@@ -299,56 +296,34 @@
 				elem.setAttribute('opacity', opacity);
 			}
 
-			// ensure all image are loaded
-			loadImages();
-
 			svgElem = document.getElementsByTagName('svg')[0];
-
+            
+            imgs = document.getElementsByTagName('image');
+            imgUrls = [];
+            
+            for (imgIndex = 0; imgIndex < imgs.length; imgIndex = imgIndex + 1) {
+                imgUrls.push(imgs[imgIndex].href.baseVal);
+            }           
+			
 			return {
 			    html: document.body.innerHTML,
 			    width: svgElem.getAttribute("width"),
-			    height: svgElem.getAttribute("height")
+			    height: svgElem.getAttribute("height"),
+                imgUrls: imgUrls
 			};
 		};
 
-		createChart = function (width, constr, input, globalOptionsArg, dataOptionsArg, customCodeArg, outputType, callback, messages) {
+		createChart = function (constr, input, globalOptionsArg, dataOptionsArg, customCodeArg, outputType, callback, messages) {
 
-			var $container, chart, nodes, nodeIter, elem, opacity, counter;
+			var $container, chart, nodes, nodeIter, elem, opacity, imgIndex, imgs, imgUrls;
 
-			// dynamic script insertion
+            // dynamic script insertion
 			function loadScript(varStr, codeStr) {
 				var $script = $('<script>').attr('type', 'text/javascript');
 				$script.html('var ' + varStr + ' = ' + codeStr);
 				document.getElementsByTagName("head")[0].appendChild($script[0]);
 				if (window[varStr] !== undefined) {
 					console.log('Highcharts.' + varStr + '.parsed');
-				}
-			}
-
-			function decrementImgCounter() {
-				counter -= 1;
-				if (counter < 1) {
-					console.log(messages.imagesLoaded);
-				}
-			}
-
-			function loadImages() {
-				var $images = $('svg image'), i, img;
-
-				if ($images.length > 0) {
-
-					counter = $images.length;
-
-					for (i = 0; i < $images.length; i += 1) {
-						img = new Image();
-						/* onload decremnts the counter, also when error (perhaps 404), then we wont wait for this image to be loaded */
-						img.onload = img.onerror = decrementImgCounter;
-						/* force loading of images by setting the src attr.*/
-						img.src = $images[i].getAttribute('href');
-					}
-				} else {
-					// no images set property to:imagesLoaded = true
-					console.log(messages.imagesLoaded);
 				}
 			}
 
@@ -439,14 +414,9 @@
 
 					chart = new Highcharts[constr](mergedOptions, cb);
 
-					// ensure images are all loaded
-					loadImages();
 				}, options, dataOptions);
 			} else {
-				chart = new Highcharts[constr](options, cb);
-
-				// ensure images are all loaded
-				loadImages();
+				chart = new Highcharts[constr](options, cb);				
 			}
 
 			/* remove stroke-opacity paths, used by mouse-trackers, they turn up as
@@ -460,12 +430,19 @@
 				elem.removeAttribute('stroke-opacity');
 				elem.setAttribute('opacity', opacity);
 			}
-
-			return {
-				//html: $container[0].firstChild.innerHTML,
+            
+            imgs = document.getElementsByTagName('image');
+            imgUrls = [];
+            
+            for (imgIndex = 0; imgIndex < imgs.length; imgIndex = imgIndex + 1) {
+                imgUrls.push(imgs[imgIndex].href.baseVal);
+            }
+            
+			return {				
 				html: $('div.highcharts-container')[0].innerHTML,
 				width: chart.chartWidth,
-				height: chart.chartHeight
+				height: chart.chartHeight,
+                imgUrls: imgUrls
 			};
 		};
 
@@ -500,7 +477,7 @@
 				if (input.substring(0, 4).toLowerCase() === "<svg" || input.substring(0, 5).toLowerCase() === "<?xml"
 					|| input.substring(0, 9).toLowerCase() === "<!doctype") {
 					//render page directly from svg file
-					svg = page.evaluate(loadChart, input, outType, messages);
+					svg = page.evaluate(loadChart, input, outType);
 					page.viewportSize = { width: svg.width, height: svg.height };
 					renderSVG(svg);
 				} else {
@@ -512,9 +489,9 @@
 							page.injectJs(config.files[jsfile]);	
 						}
 					}
-
+                    
 					// load chart in page and return svg height and width
-					svg = page.evaluate(createChart, width, constr, input, globalOptions, dataOptions, customCode, outType, callback, messages);
+					svg = page.evaluate(createChart, constr, input, globalOptions, dataOptions, customCode, outType, callback, messages);
 
 					if (!window.optionsParsed) {
 						exit('ERROR: the options variable was not available or couldn\'t be parsed, does the infile contain an syntax error? Input used:' + input);
