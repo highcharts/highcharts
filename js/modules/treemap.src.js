7085344763fe8af6feff70a16bb4b5814857a29f
jon-a-nygaard <jon.arild@highsoft.com>
2014-07-01 17:07:11 +0200

    Treemaps: added functionality for parents

--- js/modules/treemap.src.js
+++ js/modules/treemap.src.js
@@ -26,6 +26,7 @@
         this.centerX = this.x + (this.width/2);
         this.centerY = this.y + (this.height/2);
         this.totalArea = this.width * this.height;
+        this.direction = 0;
         this.totalValue = 0;
         this._plotW = this.width;
         this._plotH = this.height;
@@ -33,7 +34,7 @@
         this._plotY = this.y;
         // Calculates plotting width for a child point
         this.plotW = function (total, d) {
-            if (d == 0) {
+            if (this.direction == 0) {
                 val = total/this._plotH;
                 this._plotW -= val;
             } else {
@@ -43,7 +44,7 @@
         }
         // Calculates plotting height for a child point
         this.plotH = function (total, d) {
-            if (d == 1) {
+            if (this.direction == 1) {
                 val = total/this._plotW;
                 this._plotH -= val;
             } else {
@@ -54,7 +55,7 @@
         // Calculates x value for a child point
         this.plotX = function (w, d) {
             val = this._plotX;
-            if (d == 0) {
+            if (this.direction == 0) {
                 this._plotX += w;
             }
             return val;
@@ -62,49 +63,44 @@
         // Calculates y value for a child point
         this.plotY = function (h, d) {
             val = this._plotY;
-            if (d == 1) {
+            if (this.direction == 1) {
                 this._plotY += h;
             }
             return val;
         }
     }
 
-    // Compute sum of values in array of Points
-    function sumPointsValues(points) {
-        var totalValue = 0;
-        each(points, function (point) {
-            totalValue += point.value
-        });    
-        return totalValue;
-    }
-
-    function getSeriesArea(series) {
+    function getSeriesArea(series, totalValue) {
         var numSeries = series.chart.series.length,
             w = series.chart.plotWidth/numSeries,
             x = w * series._i,
             y = 0,
-            h = series.chart.plotHeight;
-        return seriesArea = new Area(x, y, w, h);
+            h = series.chart.plotHeight,
+            seriesArea = new Area(x, y, w, h)
+        seriesArea.totalValue = totalValue;
+        return seriesArea;
     }
-    
-    // Lay out tiles as stripes in one dimension
-    function stripes(points) {         
-        var startX = 0,
-            totalValue = sumPointsValues(points);
-        
-        each(points, function (point) {
-            point.shapeType = 'rect';
-            point.shapeArgs = {
-                x: startX,
-                y: 0,
-                width: point.value / totalValue * point.series.chart.plotWidth,
-                height: point.series.chart.plotHeight
-            };
-            point.plotX = point.shapeArgs.x + point.shapeArgs.width / 2;
-            point.plotY = point.shapeArgs.height / 2;            
-            startX += point.shapeArgs.width;
-        });
-        return points;
+    function getNodeTree(id, index, list, points) {
+        //index = list
+        var children = [],
+            totalValue = 0;
+        if (list[id] !== undefined) {
+            children = [];
+            each(list[id], function (index) {
+                node = getNodeTree(points[index].id, index, list, points);
+                totalValue += node.totalValue;
+                children.push(node);
+            });
+        } else {
+            totalValue = points[index].value;
+        }
+        nodeTree = {
+            id: id,
+            index: index,
+            children: children,
+            totalValue: totalValue
+        }
+        return nodeTree;
     }
 
     // Define default options
@@ -119,6 +115,9 @@
             formatter: function () { // #2945
                 return this.point.value;
             },
+        },
+        tooltip: {
+            pointFormat: 'id: <b>{point.id}</b><br/>parent: <b>{point.parent}</b><br/>value: <b>{point.value}</b><br/>'
         },
         layoutAlgorithm: 'sliceAndDice'
     });
@@ -147,14 +146,41 @@
         isCartesian: false,    
         trackerGroups: ['group', 'dataLabelsGroup'],
         handleLayout: function () {
-            seriesArea = getSeriesArea(this);
-            seriesArea.totalValue = sumPointsValues(this.points);
-            direction = 0;
-            layoutAlgorithm = this[this.options.layoutAlgorithm];
-
-            each(this.points, function (point) {
-                var pointArea = layoutAlgorithm(seriesArea, point.value, direction);                
- 
+            var series = this,
+                points = series.points,
+                tree = series.buildTree(points),
+                seriesArea = getSeriesArea(series, tree.totalValue)
+            series.calculateArea(seriesArea, tree);
+        },
+        buildTree: function () {
+            var points = this.points,
+                parentList = [],
+                index = 0
+            each(points, function (point) {
+                if (point.parent === undefined) {
+                    parent = 'root';
+                } else {
+                    parent = point.parent;
+                }
+                if (parentList[parent] === undefined) {
+                    parentList[parent] = [];
+                }
+                parentList[parent].push(index);
+                index++;
+            });
+            root = getNodeTree('root', -1, parentList, points);
+            return root;
+        },
+        calculateArea: function (parentArea, node) {
+            var series = this,
+                pointArea = series[series.options.layoutAlgorithm](parentArea, node.totalValue)
+            // If node is not a leaf, then call this method recursively             
+            if (node.children.length) {
+                each(node.children, function (childNode) {
+                    series.calculateArea(pointArea, childNode);
+                });
+            } else {
+                point = series.points[node.index];
                 // Set point values
                 point.shapeType = 'rect';
                 point.shapeArgs = {
@@ -165,19 +191,25 @@
                 };
                 point.plotX = pointArea.centerX;
                 point.plotY = pointArea.centerY;
-                direction = 1 - direction;
-            });            
+            }
+            
         },
-        stripes: function () {
-
+        sliceAndDice: function (parent, value) {
+            var pointTotal = parent.totalArea * (value / parent.totalValue),
+                pointW = parent.plotW(pointTotal),
+                pointH = parent.plotH(pointTotal),
+                pointX = parent.plotX(pointW),
+                pointY = parent.plotY(pointH),
+                pointArea = new Area(pointX, pointY, pointW, pointH)
+            pointArea.totalValue = value,
+            parent.direction = 1 - parent.direction;
+            return pointArea;
         },
-        sliceAndDice: function (parent, value, direction) {
-            pointTotal = parent.totalArea * (value / parent.totalValue);
-            pointW = parent.plotW(pointTotal, direction);
-            pointH = parent.plotH(pointTotal, direction);
-            pointX = parent.plotX(pointW, direction);
-            pointY = parent.plotY(pointH, direction);
-            pointArea = new Area(pointX, pointY, pointW, pointH);
+        stripes: function (parent, value) {
+            // Call sliceAndDice
+            var pointArea = this['sliceAndDice'](parent, value)
+            // Reset direction
+            parent.direction = 0;
             return pointArea;
         },
         translate: function () {
