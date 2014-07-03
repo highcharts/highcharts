b51f621788e85cc6c9f84b6499f6b5bcb8a95689
Torstein Hønsi <torstein@highsoft.com>
2014-07-03 10:45:49 +0200

    Fixed #2251, a regression since 3.0.8 causing text and graphic elements to become selected while zooming in old IE.

--- js/highmaps.src.js
+++ js/highmaps.src.js
@@ -9000,8 +9000,8 @@ Pointer.prototype = {
 
 		hoverChartIndex = chart.index;
 
-		// normalize
 		e = this.normalize(e);		
+		e.returnValue = false; // #2251, #3224
 		
 		if (chart.mouseIsDown === 'mousedown') {
 			this.drag(e);
