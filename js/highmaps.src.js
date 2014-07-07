1cd887f6663ce81c3ab3008ce821987c20f183b1
Torstein Hønsi <torstein@highsoft.com>
2014-07-07 14:31:17 +0200

    Highmaps: Fixed #3209, the first map area missing when joinBy is null.

--- js/highmaps.src.js
+++ js/highmaps.src.js
@@ -15850,7 +15850,7 @@ var MapAreaPoint = extendClass(Point, {
 			mapPoint;
 
 		if (series.mapData) {
-			mapPoint = point[joinBy[1]] && series.mapMap[point[joinBy[1]]];
+			mapPoint = point[joinBy[1]] !== undefined && series.mapMap[point[joinBy[1]]];
 			if (mapPoint) {
 				// This applies only to bubbles
 				if (series.xyFromShape) {
