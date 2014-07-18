ca7143c72a0ffa7bd80955dcc288c0bb4a05e5de
lagonnebula <lagonnebula2@gmail.com>
2014-07-18 16:36:27 +0200

    Add verification in numberFormat

--- js/parts/Utilities.js
+++ js/parts/Utilities.js
@@ -255,20 +255,24 @@ function extendClass(parent, members) {
  * @param {String} thousandsSep The thousands separator, defaults to the one given in the lang options
  */
 function numberFormat(number, decimals, decPoint, thousandsSep) {
-	var lang = defaultOptions.lang,
-		// http://kevin.vanzonneveld.net/techblog/article/javascript_equivalent_for_phps_number_format/
-		n = +number || 0,
-		c = decimals === -1 ?
-			(n.toString().split('.')[1] || '').length : // preserve decimals
-			(isNaN(decimals = mathAbs(decimals)) ? 2 : decimals),
-		d = decPoint === undefined ? lang.decimalPoint : decPoint,
-		t = thousandsSep === undefined ? lang.thousandsSep : thousandsSep,
-		s = n < 0 ? "-" : "",
-		i = String(pInt(n = mathAbs(n).toFixed(c))),
-		j = i.length > 3 ? i.length % 3 : 0;
+	if(isNaN(number)){
+		return number
+	}else{
+		var lang = defaultOptions.lang,
+			// http://kevin.vanzonneveld.net/techblog/article/javascript_equivalent_for_phps_number_format/
+			n = +number || 0,
+			c = decimals === -1 ?
+				(n.toString().split('.')[1] || '').length : // preserve decimals
+				(isNaN(decimals = mathAbs(decimals)) ? 2 : decimals),
+			d = decPoint === undefined ? lang.decimalPoint : decPoint,
+			t = thousandsSep === undefined ? lang.thousandsSep : thousandsSep,
+			s = n < 0 ? "-" : "",
+			i = String(pInt(n = mathAbs(n).toFixed(c))),
+			j = i.length > 3 ? i.length % 3 : 0;
 
-	return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) +
-		(c ? d + mathAbs(n - i).toFixed(c).slice(2) : "");
+		return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) +
+			(c ? d + mathAbs(n - i).toFixed(c).slice(2) : "");
+	}
 }
 
 /**
