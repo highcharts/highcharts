26352a0ec438c972e7c43e0bb52bda11543ec727
oysteinmoseng <oystein@highsoft.com>
2014-09-01 12:58:03 +0200

    #3203 Zoom buttons in maps revert styling on hover

--- js/modules/map.src.js
+++ js/modules/map.src.js
@@ -696,6 +696,7 @@ extend(Chart.prototype, {
 				if (buttons.hasOwnProperty(n)) {
 					buttonOptions = merge(options.buttonOptions, buttons[n]);
 					attr = buttonOptions.theme;
+					attr.style = merge(buttonOptions.theme.style, buttonOptions.style); // #3203
 					states = attr.states;
 					button = chart.renderer.button(
 							buttonOptions.text, 
@@ -713,8 +714,7 @@ extend(Chart.prototype, {
 							height: buttonOptions.height,
 							title: chart.options.lang[n],
 							zIndex: 5
-						})
-						.css(buttonOptions.style)
+						})					
 						.add();
 					button.handler = buttonOptions.onclick;
 					button.align(extend(buttonOptions, { width: button.width, height: 2 * button.height }), null, buttonOptions.alignTo);
