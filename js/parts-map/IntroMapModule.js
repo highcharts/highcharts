584f9cd325f1c1abb993d13ebc4c645a19de76ed
Torstein Hønsi <torstein@highsoft.com>
2014-09-03 15:28:51 +0200

    Changed comment in maps file

--- js/parts-map/IntroMapModule.js
+++ js/parts-map/IntroMapModule.js
@@ -1,6 +1,6 @@
 /**
  * @license @product.name@ JS v@product.version@ (@product.date@)
- * Highmaps as a plugin for Highcharts 4.0.1 or Highstock 2.0.1
+ * Highmaps as a plugin for Highcharts 4.0.x or Highstock 2.0.x (x being the patch version of this file)
  *
  * (c) 2011-2014 Torstein Honsi
  *
