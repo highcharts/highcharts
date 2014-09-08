5da8f0d3737f72b7317f877fbc4beddac85c69e8
Torstein Hønsi <torstein@highsoft.com>
2014-09-08 14:14:44 +0200

    Highmaps: Fixed #3190, reversed color axis displaying the wrong gradient.

--- js/highmaps.src.js
+++ js/highmaps.src.js
@@ -15540,9 +15540,10 @@ extend(ColorAxis.prototype, {
 	setLegendColor: function () {
 		var grad,
 			horiz = this.horiz,
-			options = this.options;
+			options = this.options,
+			reversed = this.reversed;
 
-		grad = horiz ? [0, 0, 1, 0] : [0, 0, 0, 1]; 
+		grad = horiz ? [+reversed, 0, +!reversed, 0] : [0, +!reversed, 0, +reversed]; // #3190
 		this.legendColor = {
 			linearGradient: { x1: grad[0], y1: grad[1], x2: grad[2], y2: grad[3] },
 			stops: options.stops || [
