d446f3879810aca38a9b02ee98ed3c6a681f0988
Torstein Hønsi <torstein@highsoft.com>
2014-09-16 10:07:29 +0200

    Updated numberFormat demo, related to #3284.

--- samples/highcharts/members/highcharts-numberformat/demo.js
+++ samples/highcharts/members/highcharts-numberformat/demo.js
@@ -6,28 +6,21 @@ $(function () {
             thousandsSeparator: '\u066C'
         }
     });
+    // English to Farsi
+    function digits_en2fa (number) {
+        return number.toString().replace(/\d/g, function(d){
+            return String.fromCharCode(d.charCodeAt(0) + 1728);
+        });
+    }
+    // English to Arabic
+    function digits_en2ar(number) {
+        return number.toString().replace(/\d/g, function(d){
+            return String.fromCharCode(d.charCodeAt(0) + 1584);
+        });
+    }
     Highcharts.wrap(Highcharts, 'numberFormat', function (proceed) {
-        var ret = proceed.apply(0, [].slice.call(arguments, 1)),
-            replacements = [
-                '\u0660',
-                '\u0661',
-                '\u0662',
-                '\u0663',
-                '\u0664',
-                '\u0665',
-                '\u0666',
-                '\u0667',
-                '\u0668',
-                '\u0669'
-            ],
-            i;
-
-        for (i = 0; i < 10; i++) {
-            ret = ret.replace(i, replacements[i]);
-        }
-
-
-        return ret;
+        var ret = proceed.apply(0, [].slice.call(arguments, 1));
+        return digits_en2ar(ret);
     });
 
     $('#container').highcharts({
@@ -44,7 +37,10 @@ $(function () {
             data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
             dataLabels: {
                 enabled: true,
-                format: '{y:.1f}'
+                format: '{y:.1f}',
+                style: {
+                    fontSize: '13px'
+                }
             }
         }]
     });
