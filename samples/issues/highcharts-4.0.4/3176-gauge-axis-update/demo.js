2eee512786d9d0deef48b275ad0130f58ccd0303
Torstein Hønsi <torstein@highsoft.com>
2014-09-19 12:33:03 +0200

    Fixed #3176, updating gauge axis caused background to disappear.

---
+++ samples/issues/highcharts-4.0.4/3176-gauge-axis-update/demo.js
@@ -0,0 +1,97 @@
+$(function () {
+    
+    $('#container').highcharts({
+    
+        chart: {
+            type: 'gauge',
+            plotBackgroundColor: null,
+            plotBackgroundImage: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+        
+        title: {
+            text: 'Speedometer'
+        },
+        
+        pane: {
+            startAngle: -150,
+            endAngle: 150,
+            background: [{
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                        [0, '#FFF'],
+                        [1, '#333']
+                    ]
+                },
+                borderWidth: 0,
+                outerRadius: '109%'
+            }, {
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                        [0, '#333'],
+                        [1, '#FFF']
+                    ]
+                },
+                borderWidth: 1,
+                outerRadius: '107%'
+            }, {
+                // default background
+            }, {
+                backgroundColor: '#DDD',
+                borderWidth: 0,
+                outerRadius: '105%',
+                innerRadius: '103%'
+            }]
+        },
+           
+        // the value axis
+        yAxis: [{
+            min: 0,
+            max: 200,
+            
+            minorTickInterval: 'auto',
+            minorTickWidth: 1,
+            minorTickLength: 10,
+            minorTickPosition: 'inside',
+            minorTickColor: '#666',
+    
+            tickPixelInterval: 30,
+            tickWidth: 2,
+            tickPosition: 'inside',
+            tickLength: 10,
+            tickColor: '#666',
+            labels: {
+                step: 2,
+                rotation: 'auto'
+            },
+            title: {
+                text: 'km/h'
+            }      
+        }],
+    
+        series: [{
+            name: 'Speed',
+            data: [80],
+            tooltip: {
+                valueSuffix: ' km/h'
+            }
+        }]
+    
+    }, 
+    // Add some life
+    function (chart) {
+        $('#btn').click(function(){
+        
+            chart.yAxis[0].update({
+            
+                min:0,
+                max:400
+            
+            });
+        
+        });
+    });
+});
