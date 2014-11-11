b462cf3ac995683082f13a26864daa481e51f978
Paulo Costa <me@paulo.costa.nom.br>
2014-11-11 06:14:16 -0800

    Use 'crispEdges' option when drawing heatmaps to prevent the tiny gaps between cells. The artifact used to be very visible in Chrome and sometimes visible in Firefox.

--- js/modules/heatmap.src.js
+++ js/modules/heatmap.src.js
@@ -661,7 +661,8 @@ seriesTypes.heatmap = extendClass(seriesTypes.scatter, merge(colorSeriesMixin, {
 				x: Math.min(x1, x2),
 				y: Math.min(y1, y2),
 				width: Math.abs(x2 - x1),
-				height: Math.abs(y2 - y1)
+				height: Math.abs(y2 - y1),
+                                'shape-rendering': 'crispEdges',
 			};
 		});
 		
