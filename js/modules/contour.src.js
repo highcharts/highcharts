20c742a6ea886af86796b6a37e37a5bc3d27ce7e
Paulo Costa <me@paulo.costa.nom.br>
2014-11-13 11:23:20 -0800

    Renamed "heatmap-surface" to "countour" and made big performance improvements.
    In particular, the performance gains come from:
    - Reusing triangles from previous rendering, instead of delete+recreate every time
    - Manually managing SVG gradients
    - Hacking the renderer to say isHidden() is false. Not sure why, but it thinks it is hidden (it is not!) and it causes a major performance hit thanks to `chart.cloneRenderTo()`.

--- js/modules/contour.src.js
+++ js/modules/contour.src.js
@@ -10,18 +10,20 @@
 (function (Highcharts) {
 
 
-var UNDEFINED,
-	defaultOptions = Highcharts.getOptions(),
+var defaultOptions = Highcharts.getOptions(),
 	each = Highcharts.each,
 	extendClass = Highcharts.extendClass,
 	merge = Highcharts.merge,
 	seriesTypes = Highcharts.seriesTypes,
-	eps = 0.0001;
+	eps = 0.0001,
+	SVG_NS = "http://www.w3.org/2000/svg";
+	XLINK_NS = "http://www.w3.org/1999/xlink",
+	gradient_id = 0;
 
 /**
  * Extend the default options with map options
  */
-defaultOptions.plotOptions.heatmap_surface = merge(defaultOptions.plotOptions.heatmap, {
+defaultOptions.plotOptions.contour = merge(defaultOptions.plotOptions.heatmap, {
 	marker: merge(
 		{}, {
 		states: {
@@ -33,10 +35,15 @@ defaultOptions.plotOptions.heatmap_surface = merge(defaultOptions.plotOptions.he
 });
 
 // The Heatmap series type
-seriesTypes.heatmap_surface = extendClass(seriesTypes.heatmap, {
-	type: 'heatmap_surface',
+seriesTypes.contour = extendClass(seriesTypes.heatmap, {
+	type: 'contour',
 	init: function () {
 		seriesTypes.heatmap.prototype.init.apply(this, arguments);
+		//FIXME: I have no idea why, but it believes my chart is always hidden!
+		//This causes a _HUGE_ slowdown while the whole thing is copied over on chart.cloneRenderTo()
+		this.chart.renderer.isHidden = function() {
+		  return false;
+		};
 	},
 	translateColors: function () {
 		//Hide the markers, so that the surface is visible instead.
@@ -45,12 +52,16 @@ seriesTypes.heatmap_surface = extendClass(seriesTypes.heatmap, {
 			point.color = 'rgba(0,0,0,0)';
 		});
 	},
-	drawTriangle: function (group, a,b,c) {
-		var attribs = {
-				'shape-rendering': 'crispEdges'
-			};
+	drawTriangle: function (group, triangle_index, a,b,c) {
+		var fill;
 		var colorKey = this.colorKey;
-
+		var renderer = this.chart.renderer;		
+		var triangle_data = this.triangles[triangle_index];
+		
+		if (!triangle_data) {
+			triangle_data = this.triangles[triangle_index] = {};
+		}
+		
 		//Normalized values of the vertexes
 		var values = [
 			this.colorAxis.toRelativePosition(a[colorKey]),
@@ -60,7 +71,7 @@ seriesTypes.heatmap_surface = extendClass(seriesTypes.heatmap, {
 		
 		//All vertexes have the same value/color
 		if (Math.abs(values[0] - values[1]) < eps && Math.abs(values[0] - values[2]) < eps) {
-			attribs.fill = this.colorAxis.toColor((a.value+b.value+c.value)/3);
+			fill = this.colorAxis.toColor((a.value+b.value+c.value)/3);
 		//Use a linear gradient to interpolate values/colors
 		} else {
 			//Find function where "Value = A*X + B*Y + C" at the 3 vertexes
@@ -82,22 +93,42 @@ seriesTypes.heatmap_surface = extendClass(seriesTypes.heatmap, {
 			// k = (Value - C) / (A² + B²)
 			var k0 = (0-C) / (A*A + B*B);
 			var k1 = (1-C) / (A*A + B*B);
+			var x1 = k0*A;
+			var y1 = k0*B;
+			var x2 = k1*A;
+			var y2 = k1*B;			
 			
 			// Assign a linear gradient that interpolates all 3 vertexes
-			attribs.fill = {
-				linearGradient: {
-					x1: k0*A, 
-					y1: k0*B, 
-					x2: k1*A, 
-					y2: k1*B, 
-					spreadMethod:'pad', 
-					gradientUnits:'userSpaceOnUse'
-				},
-				stops: this.colorAxis.options.stops || [
-					[0, this.colorAxis.options.minColor],
-					[1, this.colorAxis.options.maxColor]
-				]
-			};
+			if (renderer.isSVG) {
+				//SVGRenderer implementation of gradient is slow and leaks memory -- Lets do it ourselves
+				var gradient = triangle_data.gradient;
+				if (!gradient) {
+					var gradient = triangle_data.gradient = document.createElementNS(SVG_NS, "linearGradient");
+					gradient.setAttributeNS(XLINK_NS, "xlink:href", this.base_gradient_id);
+					gradient.setAttribute("id", "contour-gradient-id-" + (gradient_id++));
+					renderer.defs.element.appendChild(gradient);
+				}
+				gradient.setAttribute("x1", x1);
+				gradient.setAttribute("y1", y1);
+				gradient.setAttribute("x2", x2);
+				gradient.setAttribute("y2", y2);
+				fill = 'url(' + renderer.url + '#' + gradient.getAttribute('id') + ')';
+			} else {
+				fill = {
+					linearGradient: {
+						x1: x1,
+						y1: y1,
+						x2: x2,
+						y2: y2,
+						spreadMethod:'pad', 
+						gradientUnits:'userSpaceOnUse'
+					},
+					stops: this.colorAxis.options.stops || [
+						[0, this.colorAxis.options.minColor],
+						[1, this.colorAxis.options.maxColor]
+					]
+				};
+			}
 		}
 		
 		
@@ -110,36 +141,74 @@ seriesTypes.heatmap_surface = extendClass(seriesTypes.heatmap, {
 			c.plotX + ',' + c.plotY,
 			'Z'
 		];
-		this.chart.renderer.path(path)
-			.attr(attribs)
-			.add(group);
+		
+		if (triangle_data.shape) {
+			triangle_data.shape.attr({
+				d: path,
+				fill: fill,
+			});
+		} else {
+			triangle_data.shape = renderer.path(path)
+				.attr({
+					'shape-rendering': 'crispEdges',
+					fill: fill
+				})
+				.add(group);
+		}
+
 	},
 	drawGraph: function () {
 		var series = this,
 			i,j,
 			points = series.points,
 			options = this.options,
+			renderer = series.chart.renderer;
 			grid_width = options.grid_width;
+			
 		if (!series.surface_group) {
-			series.surface_group = this.chart.renderer.g().add(series.group);
+			series.surface_group = renderer.g().add(series.group);
+			series.triangles = [];
 		}
-		if (series.surface_current_group) {
-			series.surface_current_group.destroy();
-		}
-		var group = series.surface_current_group = this.chart.renderer.g().add(series.surface_group);
 		
+		//When creating a SVG, we create a "base" gradient with the right colors,
+		//And extend it on every triangle to define the orientation.
+		if (renderer.isSVG) {
+			var fake_rect = renderer.rect(0,0,1,1).attr({
+				fill: {
+					linearGradient: {
+						x1: 0,
+						y1: 0,
+						x2: 1,
+						y2: 0,
+						spreadMethod:'pad', 
+						gradientUnits:'userSpaceOnUse'
+					},
+					stops: this.colorAxis.options.stops || [
+						[0, this.colorAxis.options.minColor],
+						[1, this.colorAxis.options.maxColor]
+					]
+				}
+			});
+			this.base_gradient_id = /(#.*)[)]/.exec(fake_rect.attr('fill'))[1];
+			console.log(this.base_gradient_id);
+		}
+		
+		var group = series.surface_group;
+		var triangle_count = 0;
 		if (grid_width) {
 			//points are in a nice regular grid
 			for (i=1; i<points.length/grid_width; i++) {
 				for (j=1; j<options.grid_width; j++) {
 					this.drawTriangle(
 						group,
+						triangle_count++,
 						points[( i )*grid_width + (j-1)], 
 						points[(i-1)*grid_width + (j-1)], 
 						points[(i-1)*grid_width + ( j )]);
 					
 					this.drawTriangle(
 						group,
+						triangle_count++,
 						points[( i )*grid_width + ( j )], 
 						points[( i )*grid_width + (j-1)], 
 						points[(i-1)*grid_width + ( j )]);
@@ -154,11 +223,23 @@ seriesTypes.heatmap_surface = extendClass(seriesTypes.heatmap, {
 			for (i=0; i<triangles.length; i+=3) {
 				this.drawTriangle(
 					group,
+					triangle_count++,
 					points[triangles[i]], 
 					points[triangles[i+1]], 
 					points[triangles[i+2]]);
 			}
 		}
+		
+		// Remove extra unused triangles from previous rendering
+		for (i=triangle_count; i<series.triangles.length; i++) {
+			if (series.triangles[i].shape) {
+				series.triangles[i].shape.destroy();
+			}
+			if (series.triangles[i].gradient) {
+				series.triangles[i].gradient.parentNode.removeChild(series.triangles[i].gradient);
+			}
+		}
+		series.triangles.splice(triangle_count, series.triangles.length - triangle_count);
 	}
 });
 
