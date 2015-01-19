48a838255fb7a219304d3d1be75868c35d7b10df
Torstein Hønsi <torstein@highsoft.com>
2015-01-19 10:21:41 +0100

    Item chart proof of concept

---
+++ samples/highcharts/studies/item-series/demo.js
@@ -0,0 +1,98 @@
+$(function () {
+
+    /** 
+     * Proof of concept for a Highcharts item chart
+     *
+     * TODO:
+     * - Stacking
+     * - Check update, remove etc.
+     * - Custom icons like persons, carts etc. Either as images, font icons or Highcharts symbols.
+     */
+    (function (H, HA) {
+        var seriesTypes = H.seriesTypes,
+            extendClass = H.extendClass,
+            each = H.each,
+            stop = HA.stop;
+
+        seriesTypes.item = extendClass(seriesTypes.column, {
+            drawPoints: function () {
+                var series = this,
+                    graphics,
+                    renderer = series.chart.renderer;
+
+                each(this.points, function (point) {
+                    var i,
+                        attr,
+                        graphics,
+                        pointAttr;
+
+                    point.graphics = graphics = point.graphics || {};
+                    pointAttr = point.pointAttr[point.selected ? 'selected' : ''] || series.pointAttr[''];
+                    delete pointAttr.r;
+                    
+                    if (point.y !== null) {
+
+                        if (!point.graphic) {
+                            point.graphic = renderer.g().add(series.group);
+                        }
+
+                        for (i = 1; i <= point.y; i++) {
+                            attr = {
+                                x: point.plotX,
+                                y: series.yAxis.toPixels(i, true),
+                                r: Math.min(-series.pointXOffset, (series.yAxis.transA / 2) * (1 - (series.options.itemPadding || 0.2)))
+                            };
+                            if (graphics[i]) {
+                                stop(graphics[i]);
+                                graphics[i].attr(attr);
+                            } else {
+                                graphics[i] = renderer.circle(attr)
+                                    .attr(pointAttr)
+                                    .add(point.graphic);
+                            }
+                        }
+                    }
+                });
+                
+            }
+        });
+        
+    }(Highcharts, HighchartsAdapter));
+
+
+    $('#container').highcharts({
+        
+        chart: {
+            type: 'item'
+        },
+
+        title: {
+            text: 'Highcharts item chart'
+        },
+
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+
+        legend: {
+            enabled: false
+        },
+
+        yAxis: {
+            gridLineWidth: 0,
+            labels: {
+                enabled: false
+            },
+            title: {
+                text: null
+            }
+        },
+
+        series: [{
+            name: 'Items bought',
+            data: [5, 3, 4]
+        }]
+
+    });
+});
