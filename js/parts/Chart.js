30b55bf333b31ae6b13c8ddd6c891a8743a8be48
Jesse Gavin <jesse.gavin@gmail.com>
2015-02-02 16:51:40 -0600

    Fix for issue #2397

--- js/parts/Chart.js
+++ js/parts/Chart.js
@@ -501,7 +501,7 @@ Chart.prototype = {
 			subtitle
 				.css({ width: (subtitleOptions.width || autoWidth) + PX })
 				.align(extend({ 
-					y: titleOffset + (titleOptions.margin - 13) + renderer.fontMetrics(titleOptions.style.fontSize, subtitle).b 
+					y: titleOffset + renderer.fontMetrics(titleOptions.style.fontSize, subtitle).b 
 				}, subtitleOptions), false, 'spacingBox');
 			
 			if (!subtitleOptions.floating && !subtitleOptions.verticalAlign) {
