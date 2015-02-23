c344389e0a7b45f16b12fc730c097cf60329543b
Torstein Hønsi <torstein@highsoft.com>
2015-02-23 20:43:13 +0100

    Samples: Prepared unit tests for JSFiddle

--- samples/unit-tests/exporting/sanitizesvg/demo.js
+++ samples/unit-tests/exporting/sanitizesvg/demo.js
@@ -1,73 +1,69 @@
-// Proceeding so far:
-// .replace(/(text-shadow:[ 0-9a-z,\.\(\)]+),\s?[a-z][^;"]+([;"])/g, '$1$2') 
-
-
-
 // Implementation
 function sanitizeSVG(svg) {
     return Highcharts.Chart.prototype.sanitizeSVG.call(0, svg);
 }
 
+$(function () {
+
+    QUnit.test("RGB, Chrome style", function (assert) {
+        
+        assert.equal(
+            sanitizeSVG('text-shadow: rgb(255, 255, 255) 0px 0px 6px, rgb(255, 255, 255) 0px 0px 3px;'),
+            'text-shadow: rgb(255, 255, 255) 0px 0px 6px;',
+            "Basic rgb"
+        );
+        assert.equal(
+            sanitizeSVG('text-shadow: rgb(255, 255, 255) 0px 0px 6px;'),
+            'text-shadow: rgb(255, 255, 255) 0px 0px 6px;',
+            "Single"
+        );
+        assert.equal(
+            sanitizeSVG('"text-shadow: rgb(255, 255, 255) 0px 0px 6px,rgb(255, 255, 255) 0px 0px 3px"'),
+            '"text-shadow: rgb(255, 255, 255) 0px 0px 6px"',
+            "Quoted"
+        );
+    });
+
+    QUnit.test("RGBA, Chrome style", function (assert) {
+        
+        assert.equal(
+            sanitizeSVG('text-shadow: rgba(255, 255, 255, 0.5) 0px 0px 6px, rgb(255, 255, 255, 0.5) 0px 0px 3px;'),
+            'text-shadow: rgba(255, 255, 255, 0.5) 0px 0px 6px;',
+            "Basic rgba"
+        );
+    });
+
+    QUnit.test("RGB, Firefox style", function (assert) {
+        
+        assert.equal(
+            sanitizeSVG('text-shadow: 0px 0px 6px rgb(255, 255, 255), 0px 0px 3px rgb(255, 255, 255);'),
+            'text-shadow: 0px 0px 6px rgb(255, 255, 255);',
+            "Basic rgb"
+        );
+        
+    });
 
 
-QUnit.test("RGB, Chrome style", function (assert) {
-    
-    assert.equal(
-        sanitizeSVG('text-shadow: rgb(255, 255, 255) 0px 0px 6px, rgb(255, 255, 255) 0px 0px 3px;'),
-        'text-shadow: rgb(255, 255, 255) 0px 0px 6px;',
-        "Basic rgb"
-    );
-    assert.equal(
-        sanitizeSVG('text-shadow: rgb(255, 255, 255) 0px 0px 6px;'),
-        'text-shadow: rgb(255, 255, 255) 0px 0px 6px;',
-        "Single"
-    );
-    assert.equal(
-        sanitizeSVG('"text-shadow: rgb(255, 255, 255) 0px 0px 6px,rgb(255, 255, 255) 0px 0px 3px"'),
-        '"text-shadow: rgb(255, 255, 255) 0px 0px 6px"',
-        "Quoted"
-    );
+    QUnit.test("Named color", function (assert) {
+        
+        assert.equal(
+            sanitizeSVG('text-shadow:0 0 6px green, 0 0 3px green;'),
+            'text-shadow:0 0 6px green;',
+            "Named color"
+        );
+        
+    });
+
+    QUnit.test("Multiple replacements", function (assert) {
+
+        
+        assert.equal(
+            sanitizeSVG('<g style="text-shadow: rgba(255, 255, 255, 0.5) 0px 0px 6px, rgb(255, 255, 255, 0.5) 0px 0px 3px;"></g>' +     
+                '<g style="text-shadow: rgba(255, 255, 255, 0.5) 0px 0px 6px, rgb(255, 255, 255, 0.5) 0px 0px 3px;"></g>'),
+            '<g style="text-shadow: rgba(255, 255, 255, 0.5) 0px 0px 6px;"></g>' +     
+                '<g style="text-shadow: rgba(255, 255, 255, 0.5) 0px 0px 6px;"></g>',
+            "Multiple replacements"
+        );
+    });
 });
-
-QUnit.test("RGBA, Chrome style", function (assert) {
-    
-    assert.equal(
-        sanitizeSVG('text-shadow: rgba(255, 255, 255, 0.5) 0px 0px 6px, rgb(255, 255, 255, 0.5) 0px 0px 3px;'),
-        'text-shadow: rgba(255, 255, 255, 0.5) 0px 0px 6px;',
-        "Basic rgba"
-    );
-});
-
-QUnit.test("RGB, Firefox style", function (assert) {
-    
-    assert.equal(
-        sanitizeSVG('text-shadow: 0px 0px 6px rgb(255, 255, 255), 0px 0px 3px rgb(255, 255, 255);'),
-        'text-shadow: 0px 0px 6px rgb(255, 255, 255);',
-        "Basic rgb"
-    );
-    
-});
-
-
-QUnit.test("Named color", function (assert) {
-    
-    assert.equal(
-        sanitizeSVG('text-shadow:0 0 6px green, 0 0 3px green;'),
-        'text-shadow:0 0 6px green;',
-        "Named color"
-    );
-    
-});
-
-QUnit.test("Multiple replacements", function (assert) {
-
-    
-    assert.equal(
-        sanitizeSVG('<g style="text-shadow: rgba(255, 255, 255, 0.5) 0px 0px 6px, rgb(255, 255, 255, 0.5) 0px 0px 3px;"></g>' +     
-            '<g style="text-shadow: rgba(255, 255, 255, 0.5) 0px 0px 6px, rgb(255, 255, 255, 0.5) 0px 0px 3px;"></g>'),
-        '<g style="text-shadow: rgba(255, 255, 255, 0.5) 0px 0px 6px;"></g>' +     
-            '<g style="text-shadow: rgba(255, 255, 255, 0.5) 0px 0px 6px;"></g>',
-        "Multiple replacements"
-    );
-})
 
