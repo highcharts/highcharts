bad1ab72c994cc7d236122ce9f8f970ece8b80e9
Torstein Hønsi <torstein@highsoft.com>
2015-02-27 15:58:05 +0100

    Utils: Continued work on copy-release.js script.

--- grunt-tasks/copy-release.js
+++ grunt-tasks/copy-release.js
@@ -2,14 +2,26 @@
 (function () {
     "use strict";
 
+    // TODO: 
+    // - Use the products object and loop over products and their versions
+    // - Add procedure or task to commit, tag and push
+
     module.exports = function (grunt) {
 
         var product = 'highmaps',
-            version = '1.1.3';
+            version = '1.1.3',
+            products;
 
 
         grunt.loadNpmTasks('grunt-contrib-copy');
         grunt.loadNpmTasks('grunt-replace');
+
+        // Load the current products and versions
+        products = grunt.file.read('build/dist/products.js');
+        if (products) {
+            products = products.replace('var products = ', '');
+            products = JSON.parse(products);
+        }
 
         grunt.initConfig({
             copy: {
