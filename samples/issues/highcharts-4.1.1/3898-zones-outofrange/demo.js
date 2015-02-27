caf375cb7ce515c5524db3e48023046e3c6c599c
stephanevanraes <stephane@highsoft.com>
2015-02-27 15:08:10 +0100

    Added test for #3898

---
+++ samples/issues/highcharts-4.1.1/3898-zones-outofrange/demo.js
@@ -0,0 +1,17 @@
+$(function () {
+	$('#container').highcharts({
+		title: {
+			text: 'Zones were not applied correctly if they were out of range'
+		},
+		yAxis: {
+			tickPositioner: function () { return [-6, -4, -2]; }
+		},
+		series: [{
+			type: 'spline',
+			data: [-4, -3, -2, -3, -2, -4],
+			color: '#00F',
+			negativeColor: '#F00',
+			threshold: 0
+		}]
+	});
+});
