66c32ddb4929cb3cc25bb6e1a276cc6304b3631f
stephanevanraes <stephane@highsoft.com>
2015-02-27 08:50:18 +0100

    Closes #3895 replace called on a non-string value

--- js/parts/SvgRenderer.js
+++ js/parts/SvgRenderer.js
@@ -1054,7 +1054,7 @@ SVGElement.prototype = {
 			titleNode = doc.createElementNS(SVG_NS, 'title');
 			this.element.appendChild(titleNode);
 		}
-		titleNode.textContent = pick(value, '').replace(/<[^>]*>/g, ''); // #3276
+		titleNode.textContent = (String(pick(value), '')).replace(/<[^>]*>/g, ''); // #3276 #3895
 	},
 	textSetter: function (value) {
 		if (value !== this.textStr) {
