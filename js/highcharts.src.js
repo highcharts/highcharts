2496e33da4f358c8c0895ca25db53b3087bac850
Torstein Hønsi <torstein@highsoft.com>
2015-03-02 11:25:26 +0100

    Added originalEvent to wrapped event argument for point and series click. Closed #3913. (reverted from commit 7cc1a518dab8283a9d32bd1d1ac5d062cb70a1d0)

--- js/highcharts.src.js
+++ js/highcharts.src.js
@@ -9971,6 +9971,7 @@ Pointer.prototype = {
 			plotTop = chart.plotTop;
 		
 		e = this.normalize(e);
+		e.originalEvent = e; // #3913
 		e.cancelBubble = true; // IE specific
 
 		if (!chart.cancelClick) {
--- js/highmaps.src.js
+++ js/highmaps.src.js
@@ -9508,6 +9508,7 @@ Pointer.prototype = {
 			plotTop = chart.plotTop;
 		
 		e = this.normalize(e);
+		e.originalEvent = e; // #3913
 		e.cancelBubble = true; // IE specific
 
 		if (!chart.cancelClick) {
