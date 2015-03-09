07d06242754e3f98c9d5a910d34512c120998f59
Torstein Hønsi <torstein@highsoft.com>
2015-03-09 15:23:48 +0100

    Revert "Additional case and fix for #3944."
    This reverts commit 76de3c2c849896972b4aea61905abe98188bec95.

--- js/highmaps.src.js
+++ js/highmaps.src.js
@@ -12335,7 +12335,7 @@ Point.prototype = {
 		// Insert options for valueDecimals, valuePrefix, and valueSuffix
 		var series = this.series,
 			seriesTooltipOptions = series.tooltipOptions,
-			valueDecimals = seriesTooltipOptions.valueDecimals,
+			valueDecimals = pick(seriesTooltipOptions.valueDecimals, ''),
 			valuePrefix = seriesTooltipOptions.valuePrefix || '',
 			valueSuffix = seriesTooltipOptions.valueSuffix || '';
 
@@ -12347,7 +12347,7 @@ Point.prototype = {
 					return valuePrefix + $1 + valueSuffix;
 				});
 			}
-			if (typeof valueDecimals === 'number') { // Overrides format
+			if (valueDecimals) { // Overrides format
 				pointFormat = pointFormat.replace(key,  '$2:,.' + valueDecimals + 'f}');
 			}
 		});
