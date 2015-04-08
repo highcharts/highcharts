85dd9f33044b15be2b0e32f1c5f542fff5fbdb87
Torstein Hønsi <torstein@highsoft.com>
2015-04-08 11:24:23 +0200

    Fixed #4069, setVisible was very slow on pies with many legend items.
    A regression caused each label to be redrawn as many times as there were items in total, so for instance if there were 10 items, there would be 10*10 = 100 redraws.

--- js/parts/PieSeries.js
+++ js/parts/PieSeries.js
@@ -80,32 +80,36 @@ var PiePoint = extendClass(Point, {
 			chart = series.chart,
 			doRedraw = !series.isDirty && series.options.ignoreHiddenPoint;
 
-		// if called without an argument, toggle visibility
-		point.visible = point.options.visible = vis = vis === UNDEFINED ? !point.visible : vis;
-		series.options.data[inArray(point, series.data)] = point.options; // update userOptions.data
+		// Only if the value has changed
+		if (vis !== point.visible) {
+			
+			// If called without an argument, toggle visibility
+			point.visible = point.options.visible = vis = vis === UNDEFINED ? !point.visible : vis;
+			series.options.data[inArray(point, series.data)] = point.options; // update userOptions.data
 
-		// Show and hide associated elements
-		each(['graphic', 'dataLabel', 'connector', 'shadowGroup'], function (key) {
-			if (point[key]) {
-				point[key][vis ? 'show' : 'hide'](true);
-			}
-		});
-
-		if (point.legendItem) {
-			if (chart.hasRendered) {
-				series.updateTotals();
-				chart.legend.clearItems();
-				if (!doRedraw) {
-					chart.legend.render();
+			// Show and hide associated elements
+			each(['graphic', 'dataLabel', 'connector', 'shadowGroup'], function (key) {
+				if (point[key]) {
+					point[key][vis ? 'show' : 'hide'](true);
 				}
-			}
-			chart.legend.colorizeItem(point, vis);
-		}
+			});
 
-		// Handle ignore hidden slices
-		if (doRedraw) {
-			series.isDirty = true;
-			chart.redraw();
+			if (point.legendItem) {
+				if (chart.hasRendered) {
+					series.updateTotals();
+					chart.legend.clearItems();
+					if (!doRedraw) {
+						chart.legend.render();
+					}
+				}
+				chart.legend.colorizeItem(point, vis);
+			}
+
+			// Handle ignore hidden slices
+			if (doRedraw) {
+				series.isDirty = true;
+				chart.redraw();
+			}
 		}
 	},
 
@@ -443,9 +447,9 @@ var PieSeries = {
 					.shadow(shadow, shadowGroup);	
 			}
 
-			// detect point specific visibility (#2430)
-			if (point.visible !== undefined) {
-				point.setVisible(point.visible);
+			// Detect point specific visibility (#2430)
+			if (point.options.visible !== undefined) {
+				point.setVisible(point.options.visible);
 			}
 
 		});
