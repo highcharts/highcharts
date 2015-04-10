76ffd52be685da6a1bcc4f82d1ba5b18874821af
Torstein Hønsi <torstein@highsoft.com>
2015-04-10 15:52:33 +0200

    Automized demo

---
+++ samples/highcharts/drilldown/drillupbutton/unit-tests.js
@@ -0,0 +1,29 @@
+QUnit.test('Drill-up text', function (assert) {
+
+	var chart = Highcharts.charts[0];
+    chart.options.drilldown.animation = false;
+
+	assert.equal(
+        chart.series.length,
+        1,
+        'Chart created'
+    );
+
+	chart.series[0].points[0].doDrilldown();
+	assert.equal(
+        chart.drillUpButton.element.textContent,
+        '<< Terug naar Things',
+        'Button text'
+    );
+    assert.equal(
+        chart.drillUpButton.element.firstChild.getAttribute('fill'),
+        'white',
+        'Button fill'
+    );
+
+    assert.equal(
+        chart.drillUpButton.element.firstChild.getAttribute('stroke'),
+        'silver',
+        'Button stroke'
+    );
+});
