7b9d0d0592f1098bb4e8ceaf823a46eedfdda4d9
Torstein Hønsi <torstein@highsoft.com>
2015-04-14 12:43:58 +0200

    Improved heatmap-canvas demo

--- samples/highcharts/demo/heatmap-canvas/demo.js
+++ samples/highcharts/demo/heatmap-canvas/demo.js
@@ -8,30 +8,32 @@ $(function () {
      *   to capture mouseovers, we need another way of detecting hover points for the tooltip.
      */
     (function (H) {
-        var wrap = H.wrap,
+        var Series = H.Series,
+            each = H.each,
+            wrap = H.wrap,
             seriesTypes = H.seriesTypes;
 
         /**
-         * Get the canvas context for a series
+         * Create a hidden canvas to draw the graph on. The contents is later copied over 
+         * to an SVG image element.
          */
-        H.Series.prototype.getContext = function () {
-            var canvas;
-            if (!this.ctx) {
-                canvas = document.createElement('canvas');
-                canvas.setAttribute('width', this.chart.plotWidth);
-                canvas.setAttribute('height', this.chart.plotHeight);
-                canvas.style.position = 'absolute';
-                canvas.style.left = this.group.translateX + 'px';
-                canvas.style.top = this.group.translateY + 'px';
-                canvas.style.zIndex = 0;
-                canvas.style.cursor = 'crosshair';
-                this.chart.container.appendChild(canvas);
-                if (canvas.getContext) {
-                    this.ctx = canvas.getContext('2d');
-                }
+        Series.prototype.getContext = function () {
+            if (!this.canvas) {
+                this.canvas = document.createElement('canvas');
+                this.canvas.setAttribute('width', this.chart.chartWidth);
+                this.canvas.setAttribute('height', this.chart.chartHeight);
+                this.image = this.chart.renderer.image('', 0, 0, this.chart.chartWidth, this.chart.chartHeight).add(this.group);
+                this.ctx = this.canvas.getContext('2d');
             }
             return this.ctx;
-        }
+        };
+
+        /** 
+         * Draw the canvas image inside an SVG image
+         */ 
+        Series.prototype.canvasToSVG = function () {
+            this.image.attr({ href: this.canvas.toDataURL('image/png') });
+        };
 
         /**
          * Wrap the drawPoints method to draw the points in canvas instead of the slower SVG,
@@ -39,35 +41,31 @@ $(function () {
          */
         H.wrap(H.seriesTypes.heatmap.prototype, 'drawPoints', function (proceed) {
 
-            var ctx;
-            if (this.chart.renderer.forExport) {
-                // Run SVG shapes
-                proceed.call(this);
+            var ctx = this.getContext();
+            
+            if (ctx) {
+
+                // draw the columns
+                each(this.points, function (point) {
+                    var plotY = point.plotY,
+                        shapeArgs;
+
+                    if (plotY !== undefined && !isNaN(plotY) && point.y !== null) {
+                        shapeArgs = point.shapeArgs;
+
+                        ctx.fillStyle = point.pointAttr[''].fill;
+                        ctx.fillRect(shapeArgs.x, shapeArgs.y, shapeArgs.width, shapeArgs.height);
+                    }
+                });
+
+                this.canvasToSVG();
 
             } else {
+                this.chart.showLoading("Your browser doesn't support HTML5 canvas, <br>please use a modern browser");
 
-                if (ctx = this.getContext()) {
-
-                    // draw the columns
-                    H.each(this.points, function (point) {
-                        var plotY = point.plotY,
-                            shapeArgs;
-
-                        if (plotY !== undefined && !isNaN(plotY) && point.y !== null) {
-                            shapeArgs = point.shapeArgs;
-
-                            ctx.fillStyle = point.pointAttr[''].fill;
-                            ctx.fillRect(shapeArgs.x, shapeArgs.y, shapeArgs.width, shapeArgs.height);
-                        }
-                    });
-
-                } else {
-                    this.chart.showLoading("Your browser doesn't support HTML5 canvas, <br>please use a modern browser");
-
-                    // Uncomment this to provide low-level (slow) support in oldIE. It will cause script errors on
-                    // charts with more than a few thousand points.
-                    //proceed.call(this);
-                }
+                // Uncomment this to provide low-level (slow) support in oldIE. It will cause script errors on
+                // charts with more than a few thousand points.
+                //proceed.call(this);
             }
         });
     }(Highcharts));
@@ -101,18 +99,6 @@ $(function () {
             x: 40
         },
 
-        tooltip: {
-            backgroundColor: null,
-            borderWidth: 0,
-            distance: 10,
-            shadow: false,
-            useHTML: true,
-            style: {
-                padding: 0,
-                color: 'black'
-            }
-        },
-
         xAxis: {
             type: 'datetime',
             min: Date.UTC(2013, 0, 1),
@@ -120,6 +106,7 @@ $(function () {
             labels: {
                 align: 'left',
                 x: 5,
+                y: 14,
                 format: '{value:%B}' // long month
             },
             showLastLabel: false,
