557a34abaafd850e58844ce728084e46384945e7
jon-a-nygaard <jon.arild@highsoft.com>
2015-05-05 18:13:59 +0200

    AMD: corrected reference mistake with offset.

--- js/parts/Pointer.js
+++ js/parts/Pointer.js
@@ -77,7 +77,7 @@ Pointer.prototype = {
 
 		// Get mouse position
 		if (!chartPosition) {
-			this.chartPosition = chartPosition = Highcharts.offset(this.chart.container);
+			this.chartPosition = chartPosition = HighchartsAdapter.offset(this.chart.container);
 		}
 
 		// chartX and chartY
