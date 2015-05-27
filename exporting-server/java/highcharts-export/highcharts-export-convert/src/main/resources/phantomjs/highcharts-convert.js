8a7023ad8ca7074c260db7506db84c737c845c7a
gvaartjes <gert@highsoft.com>
2015-05-27 13:19:14 +0200

    Export: Copy updated highcharts-convert.js to the export-server

--- exporting-server/java/highcharts-export/highcharts-export-convert/src/main/resources/phantomjs/highcharts-convert.js
+++ exporting-server/java/highcharts-export/highcharts-export-convert/src/main/resources/phantomjs/highcharts-convert.js
@@ -20,7 +20,7 @@
 	var config = {
 			/* define locations of mandatory javascript files.
 			 * Depending on purchased license change the HIGHCHARTS property to 
-			 * Highcharts.js or Highstock.js or Highmaps.js
+			 * highcharts.js or highstock.js 
 			 */
 			files: {
 				// Highcharts serverside rendering config
@@ -42,39 +42,21 @@
 
 				// Highstock serverside rendering config
 				/*
-				JQUERY: 'jquery.1.9.1.min.js',
-				/* HIGHCHARTS: 'highstock.js',*/
-				HIGHCHARTS: 'highcharts.js',
-				HIGHCHARTS_MORE: 'highcharts-more.js',
-				HIGHCHARTS_DATA: 'data.js',
-				HIGHCHARTS_DRILLDOWN: 'drilldown.js',
-				HIGHCHARTS_FUNNEL: 'funnel.js',
-				HIGHCHARTS_HEATMAP: 'heatmap.js',
-				HIGHCHARTS_TREEMAP: 'treemap.js',
-				HIGHCHARTS_3D: 'highcharts-3d.js',
-				HIGHCHARTS_NODATA: 'no-data-to-display.js',
-				// Uncomment below if you have both Highcharts and Highmaps license
-				// HIGHCHARTS_MAP: 'map.js',
-				HIGHCHARTS_SOLID_GAUGE: 'solid-gauge.js',
-				BROKEN_AXIS: 'broken-axis.js'
-
-				// Highstock serverside rendering config
-				/*
-				JQUERY: 'jquery.1.9.1.min.js',
-				HIGHCHARTS: 'highstock.js',
-				HIGHCHARTS_MORE: 'highcharts-more.js',
-				HIGHCHARTS_DATA: 'data.js',
-				HIGHCHARTS_DRILLDOWN: 'drilldown.js',
-				HIGHCHARTS_FUNNEL: 'funnel.js',
-				HIGHCHARTS_HEATMAP: 'heatmap.js',
-				HIGHCHARTS_TREEMAP: 'treemap.js',
-				HIGHCHARTS_3D: 'highcharts-3d.js',
-				HIGHCHARTS_NODATA: 'no-data-to-display.js',
-				// Uncomment below if you have both Highstock and Highmaps license
-				// HIGHCHARTS_MAP: 'map.js',
-				HIGHCHARTS_SOLID_GAUGE: 'solid-gauge.js',
-				BROKEN_AXIS: 'broken-axis.js'
-				*/
+				 JQUERY: 'jquery.1.9.1.min.js',
+				 HIGHCHARTS: 'highstock.js',
+				 HIGHCHARTS_MORE: 'highcharts-more.js',
+				 HIGHCHARTS_DATA: 'data.js',
+				 HIGHCHARTS_DRILLDOWN: 'drilldown.js',
+				 HIGHCHARTS_FUNNEL: 'funnel.js',
+				 HIGHCHARTS_HEATMAP: 'heatmap.js',
+				 HIGHCHARTS_TREEMAP: 'treemap.js',
+				 HIGHCHARTS_3D: 'highcharts-3d.js',
+				 HIGHCHARTS_NODATA: 'no-data-to-display.js',
+				 // Uncomment below if you have both Highstock and Highmaps license
+				 // HIGHCHARTS_MAP: 'map.js',
+				 HIGHCHARTS_SOLID_GAUGE: 'solid-gauge.js',
+				 BROKEN_AXIS: 'broken-axis.js'
+				 */
 
 				// Highmaps serverside rendering config
 				/*
@@ -84,7 +66,7 @@
 				 HIGHCHARTS_DRILLDOWN: 'drilldown.js',
 				 HIGHCHARTS_HEATMAP: 'heatmap.js',
 				 HIGHCHARTS_TREEMAP: 'treemap.js'
-				*/
+				 */
 			},
 			TIMEOUT: 5000 /* 5 seconds timout for loading images */
 		},
@@ -156,23 +138,23 @@
 			convert,
 			exit,
 			interval,
-            counter,
-            imagesLoaded = false;
+			counter,
+			imagesLoaded = false;
 
 		messages.optionsParsed = 'Highcharts.options.parsed';
 		messages.callbackParsed = 'Highcharts.cb.parsed';
-		
+
 		window.optionsParsed = false;
 		window.callbackParsed = false;
-        
+
 		page.onConsoleMessage = function (msg) {
 			console.log(msg);
-            
+
 			/*
 			 * Ugly hack, but only way to get messages out of the 'page.evaluate()'
 			 * sandbox. If any, please contribute with improvements on this!
 			 */
-			
+
 			/* to check options or callback are properly parsed */
 			if (msg === messages.optionsParsed) {
 				window.optionsParsed = true;
@@ -190,7 +172,7 @@
 		/* scale and clip the page */
 		scaleAndClipPage = function (svg) {
 			/*	param: svg: The scg configuration object
-			*/
+			 */
 
 			var zoom = 1,
 				pageWidth = pick(params.width, svg.width),
@@ -202,7 +184,7 @@
 			}
 
 			/* set this line when scale factor has a higher precedence
-			scale has precedence : page.zoomFactor = params.scale  ? zoom * params.scale : zoom;*/
+			 scale has precedence : page.zoomFactor = params.scale  ? zoom * params.scale : zoom;*/
 
 			/* params.width has a higher precedence over scaling, to not break backover compatibility */
 			page.zoomFactor = params.scale && params.width == undefined ? zoom * params.scale : zoom;
@@ -254,38 +236,38 @@
 				exit(base64);
 			}
 		};
-        
-        function decrementImgCounter() {
-            counter -= 1;
-            if (counter < 1) {
-                imagesLoaded = true;
-            }
-        }
-        
-        function loadImages(imgUrls) {
-            var i, img;
-            counter = imgUrls.length;
-            for (i = 0; i < imgUrls.length; i += 1) {                    
-                img = new Image();                    
-                /* onload decrements the counter, also when error (perhaps 404), don't wait for this image to be loaded */
-                img.onload = img.onerror = decrementImgCounter;                    
-                /* force loading of images by setting the src attr.*/                    
-                img.src = imgUrls[i];
-            }
-        }
-        
+
+		function decrementImgCounter() {
+			counter -= 1;
+			if (counter < 1) {
+				imagesLoaded = true;
+			}
+		}
+
+		function loadImages(imgUrls) {
+			var i, img;
+			counter = imgUrls.length;
+			for (i = 0; i < imgUrls.length; i += 1) {
+				img = new Image();
+				/* onload decrements the counter, also when error (perhaps 404), don't wait for this image to be loaded */
+				img.onload = img.onerror = decrementImgCounter;
+				/* force loading of images by setting the src attr.*/
+				img.src = imgUrls[i];
+			}
+		}
+
 		renderSVG = function (svg) {
 			var svgFile;
 			// From this point we have 'loaded' or 'created' a SVG
-            
-            // Do we have to load images?
-            if (svg.imgUrls.length > 0) {
-                loadImages(svg.imgUrls);
-            } else  {
-                 // no images present, no loading, no waiting
-                imagesLoaded = true;
-            }
-            
+
+			// Do we have to load images?
+			if (svg.imgUrls.length > 0) {
+				loadImages(svg.imgUrls);
+			} else  {
+				// no images present, no loading, no waiting
+				imagesLoaded = true;
+			}
+
 			try {
 				if (outType.toLowerCase() === 'svg') {
 					// output svg
@@ -340,7 +322,7 @@
 			if (outputType === 'jpeg') {
 				document.body.style.backgroundColor = 'white';
 			}
-            
+
 			nodes = document.querySelectorAll('*[stroke-opacity]');
 
 			for (nodeIter = 0; nodeIter < nodes.length; nodeIter += 1) {
@@ -351,19 +333,19 @@
 			}
 
 			svgElem = document.getElementsByTagName('svg')[0];
-            
-            imgs = document.getElementsByTagName('image');
-            imgUrls = [];
-            
-            for (imgIndex = 0; imgIndex < imgs.length; imgIndex = imgIndex + 1) {
-                imgUrls.push(imgs[imgIndex].href.baseVal);
-            }           
-			
+
+			imgs = document.getElementsByTagName('image');
+			imgUrls = [];
+
+			for (imgIndex = 0; imgIndex < imgs.length; imgIndex = imgIndex + 1) {
+				imgUrls.push(imgs[imgIndex].href.baseVal);
+			}
+
 			return {
-			    html: document.body.innerHTML,
-			    width: svgElem.getAttribute("width"),
-			    height: svgElem.getAttribute("height"),
-                imgUrls: imgUrls
+				html: document.body.innerHTML,
+				width: svgElem.getAttribute("width"),
+				height: svgElem.getAttribute("height"),
+				imgUrls: imgUrls
 			};
 		};
 
@@ -371,7 +353,7 @@
 
 			var $container, chart, nodes, nodeIter, elem, opacity, imgIndex, imgs, imgUrls;
 
-            // dynamic script insertion
+			// dynamic script insertion
 			function loadScript(varStr, codeStr) {
 				var $script = $('<script>').attr('type', 'text/javascript');
 				$script.html('var ' + varStr + ' = ' + codeStr);
@@ -421,7 +403,7 @@
 
 			// disable animations
 			Highcharts.SVGRenderer.prototype.Element.prototype.animate = Highcharts.SVGRenderer.prototype.Element.prototype.attr;
-			Highcharts.setOptions({ 
+			Highcharts.setOptions({
 				plotOptions: {
 					series: {
 						animation: false
@@ -470,12 +452,12 @@
 
 				}, options, dataOptions);
 			} else {
-				chart = new Highcharts[constr](options, cb);				
+				chart = new Highcharts[constr](options, cb);
 			}
 
 			/* remove stroke-opacity paths, used by mouse-trackers, they turn up as
-			*  as fully opaque in the PDF
-			*/
+			 *  as fully opaque in the PDF
+			 */
 			nodes = document.querySelectorAll('*[stroke-opacity]');
 
 			for (nodeIter = 0; nodeIter < nodes.length; nodeIter += 1) {
@@ -484,19 +466,19 @@
 				elem.removeAttribute('stroke-opacity');
 				elem.setAttribute('opacity', opacity);
 			}
-            
-            imgs = document.getElementsByTagName('image');
-            imgUrls = [];
-            
-            for (imgIndex = 0; imgIndex < imgs.length; imgIndex = imgIndex + 1) {
-                imgUrls.push(imgs[imgIndex].href.baseVal);
-            }
-            
-			return {				
+
+			imgs = document.getElementsByTagName('image');
+			imgUrls = [];
+
+			for (imgIndex = 0; imgIndex < imgs.length; imgIndex = imgIndex + 1) {
+				imgUrls.push(imgs[imgIndex].href.baseVal);
+			}
+
+			return {
 				html: $('div.highcharts-container')[0].innerHTML,
 				width: chart.chartWidth,
 				height: chart.chartHeight,
-                imgUrls: imgUrls
+				imgUrls: imgUrls
 			};
 		};
 
@@ -540,10 +522,10 @@
 					// load necessary libraries
 					for (jsfile in config.files) {
 						if (config.files.hasOwnProperty(jsfile)) {
-							page.injectJs(config.files[jsfile]);	
+							page.injectJs(config.files[jsfile]);
 						}
 					}
-                    
+
 					// load chart in page and return svg height and width
 					svg = page.evaluate(createChart, constr, input, globalOptions, dataOptions, customCode, outType, callback, messages);
 
