46c45d11d9f8d137fa21e3c59081f6e3bc585aed
jon-a-nygaard <jon.arild@highsoft.com>
2015-06-02 15:27:19 +0200

    RequireJS: wrapped RadialAxis.js in self executing function.

--- js/highcharts-more.src.js
+++ js/highcharts-more.src.js
@@ -100,15 +100,21 @@ H.Pane = Pane;
 
 	return H;
 }(Highcharts));
-
-var axisProto = Highcharts.Axis.prototype,
-	tickProto = Highcharts.Tick.prototype,
-	noop = Highcharts.noop;
+(function (H) {
+var Axis = H.Axis,
+	CenteredSeriesMixin = H.CenteredSeriesMixin,
+	hiddenAxisMixin, // @todo Extract this to a new file
+	Pane = H.Pane,
+	radialAxisMixin, // @todo Extract this to a new file
+	Tick = H.Tick,
+	axisProto = Axis.prototype,
+	tickProto = Tick.prototype,
+	noop = H.noop;
 	
 /**
  * Augmented methods for the x axis in order to hide it completely, used for the X axis in gauges
  */
-var hiddenAxisMixin = {
+hiddenAxisMixin = {
 	getOffset: noop,
 	redraw: function () {
 		this.isDirty = false; // prevent setting Y axis dirty
@@ -125,7 +131,7 @@ var hiddenAxisMixin = {
  * Augmented methods for the value axis
  */
 /*jslint unparam: true*/
-var radialAxisMixin = {
+radialAxisMixin = {
 	isRadial: true,
 	
 	/**
@@ -187,7 +193,7 @@ var radialAxisMixin = {
 	 */
 	setOptions: function (userOptions) {
 		
-		var options = this.options = Highcharts.merge(
+		var options = this.options = H.merge(
 			this.defaultOptions,
 			this.defaultRadialOptions,
 			userOptions
@@ -212,7 +218,7 @@ var radialAxisMixin = {
 		this.chart.axisOffset[this.side] = 0;
 		
 		// Set the center array
-		this.center = this.pane.center = Highcharts.CenteredSeriesMixin.getCenter.call(this.pane);
+		this.center = this.pane.center = CenteredSeriesMixin.getCenter.call(this.pane);
 	},
 
 
@@ -222,7 +228,7 @@ var radialAxisMixin = {
 	 */
 	getLinePath: function (lineWidth, radius) {
 		var center = this.center;
-		radius = Highcharts.pick(radius, center[2] / 2 - this.offset);
+		radius = H.pick(radius, center[2] / 2 - this.offset);
 		
 		return this.chart.renderer.symbols.arc(
 			this.left + center[0],
@@ -291,7 +297,7 @@ var radialAxisMixin = {
 		if (this.isRadial) {
 
 			// Set the center array
-			this.center = this.pane.center = Highcharts.CenteredSeriesMixin.getCenter.call(this.pane);
+			this.center = this.pane.center = CenteredSeriesMixin.getCenter.call(this.pane);
 
 			// The sector is used in Axis.translate to compute the translation of reversed axis points (#2570)
 			if (this.isCircular) {
@@ -299,7 +305,7 @@ var radialAxisMixin = {
 			}
 			
 			// Axis len is used to lay out the ticks
-			this.len = this.width = this.height = this.center[2] * Highcharts.pick(this.sector, 1) / 2;
+			this.len = this.width = this.height = this.center[2] * H.pick(this.sector, 1) / 2;
 
 
 		}
@@ -312,7 +318,7 @@ var radialAxisMixin = {
 	getPosition: function (value, length) {
 		return this.postTranslate(
 			this.isCircular ? this.translate(value) : 0, // #2848
-			Highcharts.pick(this.isCircular ? length : this.translate(value), this.center[2] / 2) - this.offset
+			H.pick(this.isCircular ? length : this.translate(value), this.center[2] / 2) - this.offset
 		);		
 	},
 	
@@ -338,7 +344,7 @@ var radialAxisMixin = {
 	 */
 	getPlotBandPath: function (from, to, options) {
 		var center = this.center,
-			pick = Highcharts.pick,
+			pick = H.pick,
 			startAngleRad = this.startAngleRad,
 			fullRadius = center[2] / 2,
 			radii = [
@@ -371,9 +377,9 @@ var radialAxisMixin = {
 			}
 			
 			// Convert percentages to pixel values
-			radii = Highcharts.map(radii, function (radius) {
+			radii = H.map(radii, function (radius) {
 				if (percentRegex.test(radius)) {
-					radius = (Highcharts.pInt(radius, 10) * fullRadius) / 100;
+					radius = (H.pInt(radius, 10) * fullRadius) / 100;
 				}
 				return radius;
 			});
@@ -414,7 +420,7 @@ var radialAxisMixin = {
 			center = axis.center,
 			chart = axis.chart,
 			end = axis.getPosition(value),
-			each = Highcharts.each,
+			each = H.each,
 			xAxis,
 			xy,
 			tickPositions,
@@ -479,9 +485,9 @@ var radialAxisMixin = {
 /**
  * Override axisProto.init to mix in special axis instance functions and function overrides
  */
-Highcharts.wrap(axisProto, 'init', function (proceed, chart, userOptions) {
+H.wrap(axisProto, 'init', function (proceed, chart, userOptions) {
 	var axis = this,
-		extend = Highcharts.extend,
+		extend = H.extend,
 		angular = chart.angular,
 		polar = chart.polar,
 		isX = userOptions.isX,
@@ -507,7 +513,7 @@ Highcharts.wrap(axisProto, 'init', function (proceed, chart, userOptions) {
 		//extend(this, userOptions.isX ? radialAxisMixin : radialAxisMixin);
 		extend(this, radialAxisMixin);
 		isCircular = isX;
-		this.defaultRadialOptions = isX ? this.defaultRadialXOptions : Highcharts.merge(this.defaultYAxisOptions, this.defaultRadialYOptions);
+		this.defaultRadialOptions = isX ? this.defaultRadialXOptions : H.merge(this.defaultYAxisOptions, this.defaultRadialYOptions);
 		
 	}
 	
@@ -521,8 +527,8 @@ Highcharts.wrap(axisProto, 'init', function (proceed, chart, userOptions) {
 		if (!chart.panes) {
 			chart.panes = [];
 		}
-		this.pane = pane = chart.panes[paneIndex] = chart.panes[paneIndex] || new Highcharts.Pane(
-			Highcharts.splat(chartOptions.pane)[paneIndex],
+		this.pane = pane = chart.panes[paneIndex] = chart.panes[paneIndex] || new Pane(
+			H.splat(chartOptions.pane)[paneIndex],
 			chart,
 			axis
 		);
@@ -537,7 +543,7 @@ Highcharts.wrap(axisProto, 'init', function (proceed, chart, userOptions) {
 		// given in degrees relative to top, while internal computations are
 		// in radians relative to right (like SVG).
 		this.startAngleRad = startAngleRad = (paneOptions.startAngle - 90) * Math.PI / 180;
-		this.endAngleRad = endAngleRad = (Highcharts.pick(paneOptions.endAngle, paneOptions.startAngle + 360)  - 90) * Math.PI / 180;
+		this.endAngleRad = endAngleRad = (H.pick(paneOptions.endAngle, paneOptions.startAngle + 360)  - 90) * Math.PI / 180;
 		this.offset = options.offset || 0;
 		
 		this.isCircular = isCircular;
@@ -553,7 +559,7 @@ Highcharts.wrap(axisProto, 'init', function (proceed, chart, userOptions) {
 /**
  * Add special cases within the Tick class' methods for radial axes.
  */	
-Highcharts.wrap(tickProto, 'getPosition', function (proceed, horiz, pos, tickmarkOffset, old) {
+H.wrap(tickProto, 'getPosition', function (proceed, horiz, pos, tickmarkOffset, old) {
 	var axis = this.axis;
 	
 	return axis.getPosition ? 
@@ -565,7 +571,7 @@ Highcharts.wrap(tickProto, 'getPosition', function (proceed, horiz, pos, tickmar
  * Wrap the getLabelPosition function to find the center position of the label
  * based on the distance option
  */	
-Highcharts.wrap(tickProto, 'getLabelPosition', function (proceed, x, y, label, horiz, labelOptions, tickmarkOffset, index, step) {
+H.wrap(tickProto, 'getLabelPosition', function (proceed, x, y, label, horiz, labelOptions, tickmarkOffset, index, step) {
 	var axis = this.axis,
 		optionsY = labelOptions.y,
 		ret,
@@ -574,7 +580,7 @@ Highcharts.wrap(tickProto, 'getLabelPosition', function (proceed, x, y, label, h
 		angle = ((axis.translate(this.pos) + axis.startAngleRad + Math.PI / 2) / Math.PI * 180) % 360;
 
 	if (axis.isRadial) {
-		ret = axis.getPosition(this.pos, (axis.center[2] / 2) + Highcharts.pick(labelOptions.distance, -25));
+		ret = axis.getPosition(this.pos, (axis.center[2] / 2) + H.pick(labelOptions.distance, -25));
 		
 		// Automatically rotated
 		if (labelOptions.rotation === 'auto') {
@@ -620,7 +626,7 @@ Highcharts.wrap(tickProto, 'getLabelPosition', function (proceed, x, y, label, h
 /**
  * Wrap the getMarkPath function to return the path of the radial marker
  */
-Highcharts.wrap(tickProto, 'getMarkPath', function (proceed, x, y, tickLength, tickWidth, horiz, renderer) {
+H.wrap(tickProto, 'getMarkPath', function (proceed, x, y, tickLength, tickWidth, horiz, renderer) {
 	var axis = this.axis,
 		endPoint,
 		ret;
@@ -639,7 +645,11 @@ Highcharts.wrap(tickProto, 'getMarkPath', function (proceed, x, y, tickLength, t
 		ret = proceed.call(this, x, y, tickLength, tickWidth, horiz, renderer);
 	}
 	return ret;
-});/* 
+});
+
+	return H;
+}(Highcharts));
+/* 
  * The AreaRangeSeries class
  * 
  */
