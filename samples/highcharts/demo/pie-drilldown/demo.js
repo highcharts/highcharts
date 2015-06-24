f98a0c5d2810bd77b44d472df32d16c47e6a5e4c
Lars A. V. Cabrera <larsac07@gmail.com>
2015-06-24 15:42:53 +0200

    Updated pie-drilldown demo with new data. Removed trailing whitespace of MSIE name in unit test

--- samples/highcharts/demo/pie-drilldown/demo.js
+++ samples/highcharts/demo/pie-drilldown/demo.js
@@ -1,107 +1,123 @@
 $(function () {
-
-    Highcharts.data({
-        csv: document.getElementById('tsv').innerHTML,
-        itemDelimiter: '\t',
-        parsed: function (columns) {
-
-            var brands = {},
-                brandsData = [],
-                versions = {},
-                drilldownSeries = [];
-
-            // Parse percentage strings
-            columns[1] = $.map(columns[1], function (value) {
-                if (value.indexOf('%') === value.length - 1) {
-                    value = parseFloat(value);
+    // Create the chart
+    $('#container').highcharts({
+        chart: {
+            type: 'pie'
+        },
+        title: {
+            text: 'Browser market shares. January, 2015 to May, 2015'
+        },
+        subtitle: {
+            text: 'Click the slices to view versions. Source: netmarketshare.com.'
+        },
+        plotOptions: {
+            series: {
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}: {point.y:.1f}%'
                 }
-                return value;
-            });
+            }
+        },
 
-            $.each(columns[0], function (i, name) {
-                var brand,
-                    version;
-
-                if (i > 0) {
-
-                    // Remove special edition notes
-                    name = name.split(' -')[0];
-
-                    // Split into brand and version
-                    version = name.match(/([0-9]+[\.0-9x]*)/);
-                    if (version) {
-                        version = version[0];
-                    }
-                    brand = name.replace(version, '');
-
-                    // Create the main data
-                    if (!brands[brand]) {
-                        brands[brand] = columns[1][i];
-                    } else {
-                        brands[brand] += columns[1][i];
-                    }
-
-                    // Create the version data
-                    if (version !== null) {
-                        if (!versions[brand]) {
-                            versions[brand] = [];
-                        }
-                        versions[brand].push(['v' + version, columns[1][i]]);
-                    }
-                }
-
-            });
-
-            $.each(brands, function (name, y) {
-                brandsData.push({
-                    name: name,
-                    y: y,
-                    drilldown: versions[name] ? name : null
-                });
-            });
-            $.each(versions, function (key, value) {
-                drilldownSeries.push({
-                    name: key,
-                    id: key,
-                    data: value
-                });
-            });
-
-            // Create the chart
-            $('#container').highcharts({
-                chart: {
-                    type: 'pie'
-                },
-                title: {
-                    text: 'Browser market shares. November, 2013.'
-                },
-                subtitle: {
-                    text: 'Click the slices to view versions. Source: netmarketshare.com.'
-                },
-                plotOptions: {
-                    series: {
-                        dataLabels: {
-                            enabled: true,
-                            format: '{point.name}: {point.y:.1f}%'
-                        }
-                    }
-                },
-
-                tooltip: {
-                    headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-                    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
-                },
-
-                series: [{
-                    name: 'Brands',
-                    colorByPoint: true,
-                    data: brandsData
-                }],
-                drilldown: {
-                    series: drilldownSeries
-                }
-            });
+        tooltip: {
+            headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+        },
+        series: [{
+            name: "Brands",
+            colorByPoint: true,
+            data: [{
+                name: "Microsoft Internet Explorer",
+                y: 56.33,
+                drilldown: "Microsoft Internet Explorer"
+            }, {
+                name: "Chrome",
+                y: 24.030000000000005,
+                drilldown: "Chrome"
+            }, {
+                name: "Firefox",
+                y: 10.38,
+                drilldown: "Firefox"
+            }, {
+                name: "Safari",
+                y: 4.77,
+                drilldown: "Safari"
+            }, {
+                name: "Opera",
+                y: 0.9100000000000001,
+                drilldown: "Opera"
+            }, {
+                name: "Proprietary or Undetectable",
+                y: 0.2,
+                drilldown: null
+            }]
+        }],
+        drilldown: {
+            series: [{
+                name: "Microsoft Internet Explorer",
+                id: "Microsoft Internet Explorer",
+                data: [
+                    ["v11.0", 24.13],
+                    ["v8.0", 17.2],
+                    ["v9.0", 8.11],
+                    ["v10.0", 5.33],
+                    ["v6.0", 1.06],
+                    ["v7.0", 0.5]
+                ]
+            }, {
+                name: "Chrome",
+                id: "Chrome",
+                data: [
+                    ["v40.0", 5],
+                    ["v41.0", 4.32],
+                    ["v42.0", 3.68],
+                    ["v39.0", 2.96],
+                    ["v36.0", 2.53],
+                    ["v43.0", 1.45],
+                    ["v31.0", 1.24],
+                    ["v35.0", 0.85],
+                    ["v38.0", 0.6],
+                    ["v32.0", 0.55],
+                    ["v37.0", 0.38],
+                    ["v33.0", 0.19],
+                    ["v34.0", 0.14],
+                    ["v30.0", 0.14]
+                ]
+            }, {
+                name: "Firefox",
+                id: "Firefox",
+                data: [
+                    ["v35", 2.76],
+                    ["v36", 2.32],
+                    ["v37", 2.31],
+                    ["v34", 1.27],
+                    ["v38", 1.02],
+                    ["v31", 0.33],
+                    ["v33", 0.22],
+                    ["v32", 0.15]
+                ]
+            }, {
+                name: "Safari",
+                id: "Safari",
+                data: [
+                    ["v8.0", 2.56],
+                    ["v7.1", 0.77],
+                    ["v5.1", 0.42],
+                    ["v5.0", 0.3],
+                    ["v6.1", 0.29],
+                    ["v7.0", 0.26],
+                    ["v6.2", 0.17]
+                ]
+            }, {
+                name: "Opera",
+                id: "Opera",
+                data: [
+                    ["v12.x", 0.34],
+                    ["v28", 0.24],
+                    ["v27", 0.17],
+                    ["v29", 0.16]
+                ]
+            }]
         }
     });
 });
-
