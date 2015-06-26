04953360838446f23d8c90e130dbf2d1a7ed9b41
Torstein Hønsi <torstein@highsoft.com>
2015-06-26 08:38:54 +0200

    Utils: Updated PhantomJS script

--- utils/samples/phantomtest.js
+++ utils/samples/phantomtest.js
@@ -1,45 +1,90 @@
 /**
 Experimental PhantomJS runner for the sample suite.
 
-phantomjs phantomtest.js
+Usage: 
+phantomjs [arguments] phantomtest.js
+
+Arguments:
+--start  What sample number to start from. Use this to resume after error.
 
 Status
+- Requires PhantomJS 2
 - Currently only runs through highcharts/demo
-- Fails on security error on canvas getImageData method, so it is pretty much useless until this is resolved.
 */
 
+/*global console, phantom, require*/
+(function () {
+
+    'use strict';
+
+    var page = require('webpage').create(),
+        fs = require('fs'),
+        samples = [],
+        system = require('system'),
+        args = system.args,
+        i = 0;
+
+    args.forEach(function(arg, j) {
+        if (arg === '--start') {
+            i = parseInt(args[j + 1], 10);
+        }
+    });
+
+    ['highcharts', 'maps', 'stock', 'issues'].forEach(function (section) {
+        section = section + '/';
+        fs.list('../../samples/' + section).forEach(function (group) {
+            if (/^[a-z0-9\-]+$/.test(group) && fs.isDirectory('../../samples/' + section + group)) {
+                group = group + '/';
+                fs.list('../../samples/' + section + group).forEach(function (sample) {
+                    var details;
+                    if (/^[a-z0-9\-]+$/.test(sample) && fs.isDirectory('../../samples/' + section + group + sample + '/')) {
+                        details = '../../samples/' + section + group + sample + '/demo.details';
+                        details = fs.isFile(details) && fs.read(details);
+
+                        if (!details || details.indexOf('requiresManualTesting: true') === -1) {
+                            samples.push(section + group + sample);
+                        }
+                    }
+                });
+            }
+        });
+    });
+
+    page.onError = function(msg, trace) {
+
+        var msgStack = [msg];
+
+        if (trace && trace.length) {
+            msgStack.push('TRACE:');
+            trace.forEach(function(t) {
+                msgStack.push(' -> ' + t.file + ': ' + t.line + (t['function'] ? ' (in function "' + t['function'] + '")' : ''));
+            });
+        }
+
+        console.error(msgStack.join('\n'));
+        phantom.exit();
+    };
 
 
-var page = require('webpage').create(),
-	fs = require('fs'),
-	path = 'highcharts/demo/',
-	samples = fs.list('../../samples/' + path),
-	i = 2;
+    function runRecursive() {
+        var sample = samples[i];
 
+        page.open('http://utils.highcharts.local/samples/compare-view.php?path=' + sample, function (status) {
+            console.log(i, status, sample);
+        });
+    }
 
-function runRecursive() {
-	var dir;
+    page.onConsoleMessage = function (m) {
+        if (m === '@proceed') {
+            i = i + 1;
+            if (samples[i]) {
+                runRecursive();
+            } else {
+                phantom.exit();
+            }
+        }
+    };
 
-	dir = samples[i];
-
-	if (dir !== '.' && dir !== '..') {
-		page.open('http://utils.highcharts.local/samples/compare-view.php?path=' + path + dir, function (status) {
-		  	console.log(path + dir, status);
-		});
-	}
-}
-
-page.onConsoleMessage = function (m) {
-	if (m === '@proceed') {
-		i = i + 1;
-		if (samples[i]) {
-		  	runRecursive();
-		} else {
-			phantom.exit();
-		}
-	}
-};
-
-
-runRecursive();
+    runRecursive();
+}());
 
