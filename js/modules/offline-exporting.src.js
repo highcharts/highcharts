3484fe5fb6ab3dd5daeade55d9015b134d3e9411
oysteinmoseng <oystein@highsoft.com>
2015-06-29 14:17:01 +0200

    Worked on offline-download module

--- js/modules/offline-exporting.src.js
+++ js/modules/offline-exporting.src.js
@@ -32,7 +32,6 @@ Highcharts.Chart.prototype.exportChartLocal = function (exportingOptions, chartO
 		i,
 		l,
 		fallbackToExportServer = function () {
-			alert("Fallback to export server")
 			if (options.fallbackToExportServer === false) {
 				throw 'Fallback to export server disabled';
 			}
@@ -43,7 +42,6 @@ Highcharts.Chart.prototype.exportChartLocal = function (exportingOptions, chartO
 		// All callbacks receive two arguments: imageURL, and callbackArgs. callbackArgs is used only by callbacks and can contain whatever.
 		imageToDataUrl = function (imageURL, callbackArgs, successCallback, taintedCallback, noCanvasSupportCallback, failedLoadCallback, finallyCallback) {
 			var img = new Image();
-			console.log("Rendering data URL to canvas")
 			if (!webKit) {
 				img.crossOrigin = 'Anonymous'; // For some reason Safari chokes on this attribute
 			}
@@ -62,13 +60,12 @@ Highcharts.Chart.prototype.exportChartLocal = function (exportingOptions, chartO
 					// Now we try to get the contents of the canvas.
 					try {
 						dataURL = canvas.toDataURL();
-						console.log("DataURL", dataURL)
 						successCallback(dataURL, callbackArgs);
 					} catch (e) {
+						// Failed - either tainted canvas or something else went horribly wrong
 						if (e.name === 'SecurityError' || e.name === 'SECURITY_ERR' || e.message === 'SecurityError') {
 							taintedCallback(imageURL, callbackArgs);
 						} else {
-							console.log(e);
 							throw e;
 						}
 					}
@@ -88,7 +85,6 @@ Highcharts.Chart.prototype.exportChartLocal = function (exportingOptions, chartO
 		// Get blob URL from SVG code. Falls back to normal data URI.
 		svgToDataUrl = function (svg) {
 			try {
-				console.log("Creating Blob from SVG")
 				// Safari requires data URI since it doesn't allow navigation to blob URLs
 				if (!webKit) {
 					return domurl.createObjectURL(new Blob([svg], { type: 'image/svg+xml;charset-utf-16'}));
@@ -96,8 +92,7 @@ Highcharts.Chart.prototype.exportChartLocal = function (exportingOptions, chartO
 			} catch (e) {
 				// Ignore
 			}
-			console.log("Blob creation failed, using Data URI")
-			return ('data:image/svg+xml;charset=UTF-8,' + encodeURIComponent(svg));
+			return 'data:image/svg+xml;charset=UTF-8,' + encodeURIComponent(svg);
 		},
 		// Download contents by dataURL/blob
 		download = function (dataURL, extension) {
@@ -107,57 +102,64 @@ Highcharts.Chart.prototype.exportChartLocal = function (exportingOptions, chartO
 
 			// IE specific blob implementation
 			if (navigator.msSaveOrOpenBlob) {
-				console.log("Download using msSaveOrOpenBlob")
 				navigator.msSaveOrOpenBlob(dataURL, filename);
 				return;
 			}
 
+			// Try HTML5 download attr if supported
 			if (typeof a.download !== 'undefined') {
-				console.log("Downloading using <a>.download")
 				a.href = dataURL;
 				a.download = filename; // HTML5 download attribute
 				a.target = '_blank';
-				document.body.appendChild(a);						
+				document.body.appendChild(a);
 				a.click();
 				document.body.removeChild(a);
 			} else {
-				console.log("No download attribute, just opening data URL")
-				console.log("Trying window.open")
-				windowRef = window.open(dataURL, 'chart');
-				if (typeof windowRef === 'undefined' || windowRef === null) {
-					console.log("Failed, going for window.location.href")
+				// No download attr, just opening data URI
+				try {
+					windowRef = window.open(dataURL, 'chart');
+					if (typeof windowRef === 'undefined' || windowRef === null) {
+						throw 1;
+					}
+				} catch (e) {
+					// window.open failed, trying location.href
 					window.location.href = dataURL;
 				}
 			}
 		},
 		// Get data URL to an image of the chart and call download on it
 		initiateDownload = function () {
-			var svgurl, 
+			var svgurl,
 				blob,
 				svg = chart.sanitizeSVG(chartCopyContainer.innerHTML); // SVG of chart copy
 
 			// Initiate download depending on file type
 			if (options && options.type === 'image/svg+xml') {
 				// SVG download. In this case, we want to use Microsoft specific Blob if available
-				if (navigator.msSaveOrOpenBlob) {
-					console.log('Creating MSBlob from SVG')
-					blob = new MSBlobBuilder();
-					blob.append(svg);
-					svgurl = blob.getBlob('image/svg+xml');
-				} else {
-					svgurl = svgToDataUrl(svg);
+				try {
+					if (navigator.msSaveOrOpenBlob) {
+						blob = new MSBlobBuilder();
+						blob.append(svg);
+						svgurl = blob.getBlob('image/svg+xml');
+					} else {
+						svgurl = svgToDataUrl(svg);
+					}
+					download(svgurl, 'svg');
+				} catch (e) {
+					fallbackToExportServer();
 				}
-				download(svgurl, 'svg');
 			} else {
 				// PNG download - create bitmap from SVG
-
-				// Try to get PNG by rendering on canvas
+				
+				// First, try to get PNG by rendering on canvas
 				svgurl = svgToDataUrl(svg);
 				imageToDataUrl(svgurl, { /* args */ }, function (imageURL) {
 					// Success
-					console.log('PNG creation succeeded')
-					download(imageURL, 'png');
-				
+					try {
+						download(imageURL, 'png');
+					} catch (e) {
+						fallbackToExportServer();
+					}
 				}, function () {
 					// Failed due to tainted canvas
 					// Create new and untainted canvas
@@ -170,39 +172,30 @@ Highcharts.Chart.prototype.exportChartLocal = function (exportingOptions, chartO
 							try {
 								download(navigator.msSaveOrOpenBlob ? canvas.msToBlob() : canvas.toDataURL('image/png'), 'png');
 							} catch (e) {
-								console.log('An error occurred:', e)
 								fallbackToExportServer();
 							}
 						};
 
 					canvas.width = imageWidth;
 					canvas.height = imageHeight;
-
 					if (window.canvg) {
-						console.log("Using preloaded canvg")
+						// Use preloaded canvg
 						downloadWithCanVG();
 					} else {
 						// Must load canVG first
-						console.log("Loading canVG")
 						chart.showLoading();
 						HighchartsAdapter.getScript(Highcharts.getOptions().global.canvasToolsURL, function () {
 							chart.hideLoading();
 							downloadWithCanVG();
 						});
 					}
-
-				}, function () {
-					// No canvas support
-					console.log('No canvas support')
-					fallbackToExportServer();
-
-				}, function () {
-					// Failed to load image
-					console.log('Failed to load image')
-					fallbackToExportServer();
-
-				}, function () {
-					// Finally
+				},
+				// No canvas support
+				fallbackToExportServer,
+				// Failed to load image
+				fallbackToExportServer,
+				// Finally
+				function () {
 					try {
 						domurl.revokeObjectURL(svgurl);
 					} catch (e) {
@@ -211,8 +204,6 @@ Highcharts.Chart.prototype.exportChartLocal = function (exportingOptions, chartO
 				});
 			}
 		};
-
-	console.log("WebKit:", webKit);
 
 	// Hook into getSVG to get a copy of the chart copy's container
 	Highcharts.wrap(Highcharts.Chart.prototype, 'getChartHTML', function (proceed) {
@@ -228,11 +219,10 @@ Highcharts.Chart.prototype.exportChartLocal = function (exportingOptions, chartO
 		// If there are no images to embed, just go ahead and start the download process
 		if (!images.length) {
 			initiateDownload();
-		}	
+		}
 
 		// Success handler, we converted image to base64!
-		function embeddedSuccess(imageURL, callbackArgs) {		
-			console.log('Image embedded');
+		function embeddedSuccess(imageURL, callbackArgs) {
 			++imagesEmbedded;
 
 			// Change image href in chart copy
@@ -246,17 +236,18 @@ Highcharts.Chart.prototype.exportChartLocal = function (exportingOptions, chartO
 
 		// Go through the images we want to embed
 		for (i = 0, l = images.length; i < l; ++i) {
-			console.log('Embedding image as base64')
 			el = images[i];
 			imageToDataUrl(el.getAttributeNS('http://www.w3.org/1999/xlink', 'href'), { imageElement: el },
 				embeddedSuccess,
-				function () { console.log('Tainted canvas'); fallbackToExportServer(); }, 
-				function () { console.log('No canvas support'); fallbackToExportServer(); }, 
-				function () { console.log('Failed to load source'); fallbackToExportServer(); }
+				// Tainted canvas
+				fallbackToExportServer,
+				// No canvas support 
+				fallbackToExportServer,
+				// Failed to load source
+				fallbackToExportServer
 			);
 		}
 	} catch (e) {
-		console.log("An error occurred:", e)
 		fallbackToExportServer();
 	}
 };
--- samples/highcharts/exporting/offline-download/demo.js
+++ samples/highcharts/exporting/offline-download/demo.js
@@ -31,7 +31,9 @@ $(function () {
         },
 
         series: [{
-            data: [29.9, 71.5, 106.4, 129.2, 144.0, {y: 176.0/**/, marker: { symbol: 'url(http://upload.wikimedia.org/wikipedia/en/7/70/Example.png)'}/**/}, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4/*,{y: 126.0, marker: { symbol: 'url(http://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg)'}}*/]
+            data: [29.9, 71.5, 106.4, 129.2, 144.0, 
+            {y: 176.0/**, marker: { symbol: 'url(http://utils.highcharts.om/samples/sun.png)'}/**/}, 135.6, 
+            {y: 126.0/**, marker: { symbol: 'url(http://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg)'}/**/}, 148.5, 216.4, 194.1, 95.6, 54.4]
         }]
 
     });
