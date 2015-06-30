d66dcc2014c102e8c77091f2fece57d6a753b4b6
Torstein Hønsi <torstein@highsoft.com>
2015-06-30 15:37:50 +0200

    Linted samples for offline-exporting.

--- samples/highcharts/exporting/offline-download/demo.js
+++ samples/highcharts/exporting/offline-download/demo.js
@@ -3,13 +3,16 @@ $(function () {
     $('#container').highcharts({
 
         exporting: {
-            chartOptions: {
-                chart: {
-                    backgroundColor: 'red'
+            chartOptions: { // specific options for the exported image
+                plotOptions: {
+                    series: {
+                        dataLabels: {
+                            enabled: true
+                        }
+                    }
                 }
             },
             scale: 3,
-            sourceWidth: 1100,
             fallbackToExportServer: false
         },
 
@@ -31,9 +34,7 @@ $(function () {
         },
 
         series: [{
-            data: [29.9, 71.5, 106.4, 129.2, 144.0, 
-            {y: 176.0/**, marker: { symbol: 'url(http://utils.highcharts.om/samples/sun.png)'}/**/}, 135.6, 
-            {y: 126.0/**, marker: { symbol: 'url(http://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg)'}/**/}, 148.5, 216.4, 194.1, 95.6, 54.4]
+            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 126.0, 148.5, 216.4, 194.1, 95.6, 54.4]
         }]
 
     });
