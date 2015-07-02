8f9f57d6dabb383ef829cf93dae68e4ff49e7b8e
Torstein Hønsi <torstein@highsoft.com>
2015-07-02 11:42:10 +0200

    Updated demo

--- samples/stock/demo/flags-general/demo.js
+++ samples/stock/demo/flags-general/demo.js
@@ -36,29 +36,29 @@ $(function () {
             }, {
                 type : 'flags',
                 data : [{
-                    x : Date.UTC(2015, 5, 30),
-                    title : 'H',
-                    text : 'Surging US dollar curbs global IT spend'
-                }, {
-                    x : Date.UTC(2015, 5, 29),
-                    title : 'G',
-                    text : 'Euro records stunning reversal against dollar'
-                }, {
-                    x : Date.UTC(2015, 5, 26),
-                    title : 'F',
-                    text : 'Greek Negotiations Take Sharp Turn for Worse, US Dollar set to Rally '
-                }, {
-                    x : Date.UTC(2015, 5, 19),
-                    title : 'E',
-                    text : 'US Dollar Declines Over the Week on Rate Timeline'
+                    x : Date.UTC(2015, 5, 8),
+                    title : 'C',
+                    text : 'Stocks fall on Greece, rate concerns; US dollar dips'
                 }, {
                     x : Date.UTC(2015, 5, 12),
                     title : 'D',
                     text : 'Zimbabwe ditches \'worthless\' currency for the US dollar '
                 }, {
-                    x : Date.UTC(2015, 5, 8),
-                    title : 'C',
-                    text : 'Stocks fall on Greece, rate concerns; US dollar dips'
+                    x : Date.UTC(2015, 5, 19),
+                    title : 'E',
+                    text : 'US Dollar Declines Over the Week on Rate Timeline'
+                }, {
+                    x : Date.UTC(2015, 5, 26),
+                    title : 'F',
+                    text : 'Greek Negotiations Take Sharp Turn for Worse, US Dollar set to Rally '
+                }, {
+                    x : Date.UTC(2015, 5, 29),
+                    title : 'G',
+                    text : 'Euro records stunning reversal against dollar'
+                }, {
+                    x : Date.UTC(2015, 5, 30),
+                    title : 'H',
+                    text : 'Surging US dollar curbs global IT spend'
                 }],
                 onSeries : 'dataseries',
                 shape : 'circlepin',
