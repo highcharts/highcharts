35c0bc23b327f5d08ec535bf6405e5f8fdd6dee8
Torstein Hønsi <torstein@highsoft.com>
2015-08-12 10:28:14 +0200

    Prepared Map timeline demo

--- samples/highcharts/studies/timeline/demo.js
+++ samples/highcharts/studies/timeline/demo.js
@@ -4,31 +4,31 @@ $(function () {
         dataSequence = [
         {
             name: '2009',
-            data: [5, 3, 2, 1]
+            data: [1, 2, 2, 1, 1, 2, 2, 2, 1, 1]
         }, {
             name: '2010',
-            data: [5, 6, 4, 3]
+            data: [6, 12, 2, 3, 3, 2, 2, 3, 2, 2]
         }, {
             name: '2011',
-            data: [1, 3, 4, 5]
+            data: [4, 5, 6, 5, 5, 4, 9, 5, 3, 4]
         }, {
             name: '2012',
-            data: [3, 5, 5, 9]
+            data: [5, 5, 6, 6, 5, 6, 6, 5, 5, 6]
         }, {
             name: '2013',
-            data: [4, 5, 6, 1]
+            data: [6, 7, 7, 6, 6, 6, 7, 7, 6, 7]
         }, {
             name: '2014',
-            data: [5, 2, 3, 1]
+            data: [8, 9, 9, 8, 8, 8, 9, 9, 8, 9]
         }, {
             name: '2015',
-            data: [4, 5, 6, 1]
+            data: [9, 10, 4, 10, 9, 9, 9, 10, 10, 10]
         }, {
             name: '2016',
-            data: [5, 2, 3, 1]
+            data: [1, 10, 10, 10, 10, 11, 11, 11, 12, 12]
         }, {
             name: '2017',
-            data: [4, 6, 3, 2]
+            data: [11, 11, 12, 12, 12, 11, 11, 12, 12, 12]
         }
     ];
 
@@ -52,7 +52,7 @@ $(function () {
 
         colorAxis: {
             min: 0,
-            max: 10
+            max: 12
         },
 
         series: [{
@@ -74,6 +74,10 @@ $(function () {
     
     chart = $('#container').highcharts();
     
+    /**
+     * Update the chart. This happens either on updating (moving) the range input,
+     * or from a timer when the timeline is playing.
+     */
     function update(increment) {
         var input = $('#play-range')[0],
             output = $('#play-output')[0];
@@ -84,30 +88,47 @@ $(function () {
         chart.series[0].setData(dataSequence[input.value].data); // Increment dataset (updates chart)
         output.innerHTML = dataSequence[input.value].name; // Output value
         if (input.value >= input.max) { // Auto-pause
-            pause()
+            pause($('#play-pause-button')[0]);
         }
     }
+
+    /**
+     * Play the timeline.
+     */
+    function play(button) {
+        button.title = 'pause';
+        button.className = 'fa fa-pause';
+        chart.sequenceTimer = setInterval ( function () {
+            update(1);
+        }, 500);
+
+    }
     
-    function pause() {
-        var button = $('#play-pause-button')[0]
+    /** 
+     * Pause the timeline, either when the range is ended, or when clicking the pause button.
+     * Pausing stops the timer and resets the button to play mode.
+     */
+    function pause(button) {
         button.title = 'play';
         button.className = 'fa fa-play';
         clearTimeout(chart.sequenceTimer);
         chart.sequenceTimer = undefined;
     }
     
+    /**
+     * Toggle play and pause from the button
+     */
     $('#play-pause-button').bind('click', function () {
         if (chart.sequenceTimer === undefined) {
-            this.title = 'pause';
-            this.className = 'fa fa-pause';
-            chart.sequenceTimer = setInterval ( function () {
-                update(1);
-            }, 500);
+            play(this);
         } else {
-            pause();
+            pause(this);
         }
     });
     
+    /**
+     * Update the chart when the input is changed
+     */
     $('#play-range').bind('input', function () {
         update();
     });
