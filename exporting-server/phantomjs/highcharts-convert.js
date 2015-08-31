6f62ecbf377cb94ac1ec73ab1b1bc4d303ea75d9
gvaartjes <gert@highsoft.com>
2015-08-31 11:13:35 +0200

    Export: Fixed messy whitespace

--- exporting-server/phantomjs/highcharts-convert.js
+++ exporting-server/phantomjs/highcharts-convert.js
@@ -135,21 +135,21 @@
 			convert,
 			exit,
 			interval,
-            counter,
-            imagesLoaded = false;
+			counter,
+			imagesLoaded = false;
 
 		messages.optionsParsed = 'Highcharts.options.parsed';
 		messages.callbackParsed = 'Highcharts.cb.parsed';
 		
 		window.optionsParsed = false;
 		window.callbackParsed = false;
-        
+		
 		//security measures, for not allowing loading iframes
 		page.navigationLocked = true;
 		
 		page.onConsoleMessage = function (msg) {
 			console.log(msg);
-            
+			
 			/*
 			 * Ugly hack, but only way to get messages out of the 'page.evaluate()'
 			 * sandbox. If any, please contribute with improvements on this!
@@ -243,38 +243,38 @@
 				exit(base64);
 			}
 		};
-        
-        function decrementImgCounter() {
-            counter -= 1;
-            if (counter < 1) {
-                imagesLoaded = true;
-            }
-        }
-        
-        function loadImages(imgUrls) {
-            var i, img;
-            counter = imgUrls.length;
-            for (i = 0; i < imgUrls.length; i += 1) {                    
-                img = new Image();                    
-                /* onload decrements the counter, also when error (perhaps 404), don't wait for this image to be loaded */
-                img.onload = img.onerror = decrementImgCounter;                    
-                /* force loading of images by setting the src attr.*/                    
-                img.src = imgUrls[i];
-            }
-        }
-        
+		
+		function decrementImgCounter() {
+			counter -= 1;
+			if (counter < 1) {
+				imagesLoaded = true;
+			}
+		}
+		
+		function loadImages(imgUrls) {
+			var i, img;
+			counter = imgUrls.length;
+			for (i = 0; i < imgUrls.length; i += 1) {                    
+				img = new Image();                    
+				/* onload decrements the counter, also when error (perhaps 404), don't wait for this image to be loaded */
+				img.onload = img.onerror = decrementImgCounter;                    
+				/* force loading of images by setting the src attr.*/                    
+				img.src = imgUrls[i];
+			}
+		}
+		
 		renderSVG = function (svg) {
 			var svgFile;
 			// From this point we have 'loaded' or 'created' a SVG
-            
-            // Do we have to load images?
-            if (svg.imgUrls.length > 0) {
-                loadImages(svg.imgUrls);
-            } else  {
-                 // no images present, no loading, no waiting
-                imagesLoaded = true;
-            }
-            
+			
+			// Do we have to load images?
+			if (svg.imgUrls.length > 0) {
+				loadImages(svg.imgUrls);
+			} else  {
+				 // no images present, no loading, no waiting
+				imagesLoaded = true;
+			}
+			
 			try {
 				if (outType.toLowerCase() === 'svg') {
 					// output svg
@@ -332,7 +332,7 @@
 			if (outputType === 'jpeg') {
 				document.body.style.backgroundColor = 'white';
 			}
-            
+			
 			nodes = document.querySelectorAll('*[stroke-opacity]');
 
 			for (nodeIter = 0; nodeIter < nodes.length; nodeIter += 1) {
@@ -343,19 +343,19 @@
 			}
 
 			svgElem = document.getElementsByTagName('svg')[0];
-            
-            imgs = document.getElementsByTagName('image');
-            imgUrls = [];
-            
-            for (imgIndex = 0; imgIndex < imgs.length; imgIndex = imgIndex + 1) {
-                imgUrls.push(imgs[imgIndex].href.baseVal);
-            }           
+			
+			imgs = document.getElementsByTagName('image');
+			imgUrls = [];
+			
+			for (imgIndex = 0; imgIndex < imgs.length; imgIndex = imgIndex + 1) {
+				imgUrls.push(imgs[imgIndex].href.baseVal);
+			}           
 			
 			return {
-			    html: document.body.innerHTML,
-			    width: svgElem.getAttribute("width"),
-			    height: svgElem.getAttribute("height"),
-                imgUrls: imgUrls
+				html: document.body.innerHTML,
+				width: svgElem.getAttribute("width"),
+				height: svgElem.getAttribute("height"),
+				imgUrls: imgUrls
 			};
 		};
 
@@ -363,7 +363,7 @@
 
 			var $container, chart, nodes, nodeIter, elem, opacity, imgIndex, imgs, imgUrls;
 
-            // dynamic script insertion
+			// dynamic script insertion
 			function loadScript(varStr, codeStr) {
 				var $script = $('<script>').attr('type', 'text/javascript');
 				$script.html('var ' + varStr + ' = ' + codeStr);
@@ -476,19 +476,19 @@
 				elem.removeAttribute('stroke-opacity');
 				elem.setAttribute('opacity', opacity);
 			}
-            
-            imgs = document.getElementsByTagName('image');
-            imgUrls = [];
-            
-            for (imgIndex = 0; imgIndex < imgs.length; imgIndex = imgIndex + 1) {
-                imgUrls.push(imgs[imgIndex].href.baseVal);
-            }
-            
+			
+			imgs = document.getElementsByTagName('image');
+			imgUrls = [];
+			
+			for (imgIndex = 0; imgIndex < imgs.length; imgIndex = imgIndex + 1) {
+				imgUrls.push(imgs[imgIndex].href.baseVal);
+			}
+			
 			return {				
 				html: $('div.highcharts-container')[0].innerHTML,
 				width: chart.chartWidth,
 				height: chart.chartHeight,
-                imgUrls: imgUrls
+				imgUrls: imgUrls
 			};
 		};
 
@@ -536,7 +536,7 @@
 					 if (constr === 'Map') {
 						 jsFiles = config.files.highmaps;
 					 } else if (constr === 'StockChart')
-					    jsFiles = config.files.highstock;
+						jsFiles = config.files.highstock;
 					 else {
 						jsFiles = config.files.highcharts;
 					}
