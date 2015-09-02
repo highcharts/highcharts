e606021caaaacf66a31e4cbff792bb44324a2288
Torstein Hønsi <torstein@highsoft.com>
2015-09-02 15:32:41 +0200

    Better fix for #4530.

--- js/modules/map.src.js
+++ js/modules/map.src.js
@@ -1824,17 +1824,19 @@ seriesTypes.heatmap = extendClass(seriesTypes.scatter, merge(colorSeriesMixin, {
 		var series = this,
 			options = series.options,
 			xAxis = series.xAxis,
-			yAxis = series.yAxis;
+			yAxis = series.yAxis,
+			colsize = xAxis.single ? 1 : options.colsize,
+			rowsize = yAxis.single ? 1 : options.rowsize;
 
 		series.generatePoints();
 
 		each(series.points, function (point) {
-			var xPad = (options.colsize || 1) / 2,
-				yPad = (options.rowsize || 1) / 2,
-				x1 = Math.max(0, Math.round(xAxis.len - xAxis.translate(point.x - xPad, 0, 1, 0, 1))),
-				x2 = Math.min(xAxis.len, Math.round(xAxis.len - xAxis.translate(point.x + xPad, 0, 1, 0, 1))),
-				y1 = Math.max(0, Math.round(yAxis.translate(point.y - yPad, 0, 1, 0, 1))),
-				y2 = Math.min(yAxis.len, Math.round(yAxis.translate(point.y + yPad, 0, 1, 0, 1)));
+			var xPad = (colsize || 1) / 2,
+				yPad = (rowsize || 1) / 2,
+				x1 = Math.round(xAxis.len - xAxis.translate(point.x - xPad, 0, 1, 0, 1)),
+				x2 = Math.round(xAxis.len - xAxis.translate(point.x + xPad, 0, 1, 0, 1)),
+				y1 = Math.round(yAxis.translate(point.y - yPad, 0, 1, 0, 1)),
+				y2 = Math.round(yAxis.translate(point.y + yPad, 0, 1, 0, 1));
 
 			// Set plotX and plotY for use in K-D-Tree and more
 			point.plotX = point.clientX = (x1 + x2) / 2;
--- samples/issues/highcharts-4.1.8/4530-heatmap-single-colsize/demo.js
+++ samples/issues/highcharts-4.1.8/4530-heatmap-single-colsize/demo.js
@@ -19,7 +19,8 @@ $(function () {
                     0,
                     1
                 ]*/],
-                colsize: 24 * 3600 * 1000
+                colsize: 24 * 3600 * 1000,
+                rowsize: 24 * 3600 * 1000,
             }]
         });
 
@@ -27,6 +28,52 @@ $(function () {
             parseInt(chart.series[0].points[0].graphic.attr('width')) < 400,
             true,
             'Element width is acceptable'
+        );
+
+        assert.strictEqual(
+            parseInt(chart.series[0].points[0].graphic.attr('height')) < 400,
+            true,
+            'Element height is acceptable'
+        );
+
+
+
+
+        var chart = new Highcharts.Chart({
+
+            chart: {
+                type: 'heatmap',
+                renderTo: 'container',
+                width: 400,
+                height: 400,
+                inverted: true
+            },
+            
+            series: [{
+                data: [[
+                    Date.UTC(2012, 12, 2),
+                    0,
+                    93
+                ]/*, [
+                    Date.UTC(2012, 12, 3),
+                    0,
+                    1
+                ]*/],
+                colsize: 24 * 3600 * 1000,
+                rowsize: 24 * 3600 * 1000
+            }]
+        });
+
+        assert.strictEqual(
+            parseInt(chart.series[0].points[0].graphic.attr('width')) < 400,
+            true,
+            'Element width is acceptable'
+        );
+
+        assert.strictEqual(
+            parseInt(chart.series[0].points[0].graphic.attr('height')) < 400,
+            true,
+            'Element height is acceptable'
         );
         
     });
