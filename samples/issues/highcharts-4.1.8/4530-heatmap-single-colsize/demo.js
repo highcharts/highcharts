90ffe87767e16806d321e16fed52c44cbd0d9fbb
Torstein Hønsi <torstein@highsoft.com>
2015-09-02 15:48:50 +0200

    Better fix for #4530

--- samples/issues/highcharts-4.1.8/4530-heatmap-single-colsize/demo.js
+++ samples/issues/highcharts-4.1.8/4530-heatmap-single-colsize/demo.js
@@ -76,6 +76,51 @@ $(function () {
             'Element height is acceptable'
         );
         
+
+
+
+
+
+        var chart = new Highcharts.Chart({
+
+            chart: {
+                type: 'heatmap',
+                renderTo: 'container',
+                width: 400,
+                height: 400,
+                inverted: true
+            },
+
+            xAxis: {
+                minRange: 1
+            },
+            
+            series: [{
+                data: [[
+                    Date.UTC(2012, 12, 2),
+                    0,
+                    93
+                ]/*, [
+                    Date.UTC(2012, 12, 3),
+                    0,
+                    1
+                ]*/],
+                colsize: 24 * 3600 * 1000,
+                rowsize: 24 * 3600 * 1000
+            }]
+        });
+
+        assert.strictEqual(
+            parseInt(chart.series[0].points[0].graphic.attr('width')) < 400,
+            true,
+            'With minRange: Element width is acceptable'
+        );
+
+        assert.strictEqual(
+            parseInt(chart.series[0].points[0].graphic.attr('height')) < 400,
+            true,
+            'With minRange: Element height is acceptable'
+        );
     });
 
 });
