1deab6ddfca44f5a4a64a6b7d4e6280d239f38ec
Paweł Fus <pawel.fus.88@gmail.com>
2015-09-17 18:00:05 +0200

    Fixed #4579, a regression causing wrong extremes on xAxis when using minorTicks.

--- js/parts/Axis.js
+++ js/parts/Axis.js
@@ -709,8 +709,8 @@ Axis.prototype = {
 			}
 		}
 
-		if(minorTickPositions.length !== 0) { // don't change the extremes, when there is no minor ticks
-			axis.trimTicks(minorTickPositions, options.startOnTick, options.endOnTick); // #3652 #3743 #1498
+		if(minorTickPositions.length !== 0) { // #1498 - don't change the extremes, when there is no minor ticks
+			axis.trimTicks(minorTickPositions); // #3652 #3743
 		}
 		return minorTickPositions;
 	},
