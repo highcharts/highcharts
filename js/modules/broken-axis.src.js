b764346e5c33d2a3717f3ee1e2867ac960809d7f
mcb23 <marclbm23@gmail.com>
2015-10-20 13:00:21 +0200

    Update broken-axis.src.js
    Removal

--- js/modules/broken-axis.src.js
+++ js/modules/broken-axis.src.js
@@ -2,7 +2,7 @@
  * @product.name@ JS v@product.version@ (@product.date@)
  * Highcharts Broken Axis module
  * 
- * Author: Stephane Vanraes, Torstein Honsi
+ * Author: Torstein Honsi
  * License: www.highcharts.com/license
  */
 
