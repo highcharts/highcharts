7b1bfb9f757da36a220a1a0c65d54dc2d433f7ef
Paweł Fus <pawel.fus.88@gmail.com>
2015-10-21 16:00:45 +0200

    Fixed #4675, boost module did not remove tracker path after zooming out from SVG renderer.

--- js/modules/boost.src.js
+++ js/modules/boost.src.js
@@ -170,7 +170,7 @@
                 }
             }
 
-            each(['graph', 'area'], function (prop) {
+            each(['graph', 'area', 'tracker'], function (prop) {
                 if (series[prop]) {
                     series[prop] = series[prop].destroy();
                 }
