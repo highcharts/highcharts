20d5eb32840a54035288633cb5558b0790be9b68
Torstein Hønsi <torstein@highsoft.com>
2015-12-01 15:52:51 +0100

    Fixed #4789, dynamically added gauge dial was drawn on top of pivot.

--- js/highcharts-more.src.js
+++ js/highcharts-more.src.js
@@ -1164,7 +1164,8 @@ var arrayMin = Highcharts.arrayMin,
                             stroke: dialOptions.borderColor || 'none',
                             'stroke-width': dialOptions.borderWidth || 0,
                             fill: dialOptions.backgroundColor || 'black',
-                            rotation: shapeArgs.rotation // required by VML when animation is false
+                            rotation: shapeArgs.rotation, // required by VML when animation is false
+                            zIndex: 1
                         })
                         .add(series.group);
                 }
@@ -1181,7 +1182,8 @@ var arrayMin = Highcharts.arrayMin,
                     .attr({
                         'stroke-width': pivotOptions.borderWidth || 0,
                         stroke: pivotOptions.borderColor || 'silver',
-                        fill: pivotOptions.backgroundColor || 'black'
+                        fill: pivotOptions.backgroundColor || 'black',
+                        zIndex: 2
                     })
                     .translate(center[0], center[1])
                     .add(series.group);
