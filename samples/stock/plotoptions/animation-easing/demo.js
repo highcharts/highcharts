dc152df398b41f88967c0b4dbbe3d6eb00273c9c
Torstein Hønsi <torstein@highsoft.com>
2015-12-12 08:15:29 +0100

    Standalone: Updated easing in demos.

--- samples/stock/plotoptions/animation-easing/demo.js
+++ samples/stock/plotoptions/animation-easing/demo.js
@@ -5,7 +5,7 @@ $(function () {
             series: {
                 animation: {
                     duration: 2000,
-                    easing: 'swing'
+                    easing: 'easeOutBounce'
                 }
             }
         },
@@ -20,3 +20,18 @@ $(function () {
         }]
     });
 });
+/**
+ * Easing function from https://github.com/danro/easing-js/blob/master/easing.js
+ */
+Math.easeOutBounce = function (pos) {
+    if ((pos) < (1 / 2.75)) {
+        return (7.5625 * pos * pos);
+    }
+    if (pos < (2 / 2.75)) {
+        return (7.5625 * (pos -= (1.5 / 2.75)) * pos + 0.75);
+    }
+    if (pos < (2.5 / 2.75)) {
+        return (7.5625 * (pos -= (2.25 / 2.75)) * pos + 0.9375);
+    }
+    return (7.5625 * (pos -= (2.625 / 2.75)) * pos + 0.984375);
+};
