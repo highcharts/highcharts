cd8c797a0a6003bc0bd88fe29610e0e218e2a86c
Torstein Hønsi <torstein@highsoft.com>
2016-03-03 08:55:26 +0100

    krevje: Removed presentational attributes in scrollbar

--- js/highcharts.unstyled.src.js
+++ js/highcharts.unstyled.src.js
@@ -2,7 +2,7 @@
 // @compilation_level SIMPLE_OPTIMIZATIONS
 
 /**
- * @license Highcharts JS v5.0-dev (2016-02-11)
+ * @license Highcharts JS v5.0-dev (2016-03-03)
  *
  * (c) 2009-2016 Torstein Honsi
  *
--- js/modules/heatmap.unstyled.src.js
+++ js/modules/heatmap.unstyled.src.js
@@ -1,5 +1,5 @@
 /**
- * @license Highcharts JS v5.0-dev (2016-02-11)
+ * @license Highcharts JS v5.0-dev (2016-03-03)
  *
  * (c) 2011-2016 Torstein Honsi
  *
