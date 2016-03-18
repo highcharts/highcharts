8b779f534e4113a2ac02b7047162f797cfec6c15
Torstein Hønsi <torstein@highsoft.com>
2016-03-18 09:32:04 +0100

    Reverted: Fixed #5124, axis labels overlapped after redraw. (reverted from commit e251c56bde165edc478196c3fb779c29119e8e71)

--- js/highmaps.src.js
+++ js/highmaps.src.js
@@ -12118,7 +12118,7 @@
             // pre-render axes to get labels offset width
             if (chart.hasCartesianSeries) {
                 each(chart.axes, function (axis) {
-                    if (axis.visible && axis.isDirty) { // #5124
+                    if (axis.visible) {
                         axis.getOffset();
                     }
                 });
