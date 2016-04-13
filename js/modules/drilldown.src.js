highcharts/highcharts

commit ab42ff4b29a15326f627a15722d6c6010cbde06b

Added e.originalEvent to drilldown event in order to catch modifier keys and other properties. Closes #5113.

js/modules/drilldown.src.js
@@ -526,7 +526,7 @@
 		});
 	}
 	
-	H.Point.prototype.doDrilldown = function (_holdRedraw, category) {
+	H.Point.prototype.doDrilldown = function (_holdRedraw, category, originalEvent) {
 		var series = this.series,
 			chart = series.chart,
 			drilldown = chart.options.drilldown,
@@ -550,29 +550,32 @@
 			point: this,
 			seriesOptions: seriesOptions,
 			category: category,
+			originalEvent: originalEvent, // docs
 			points: category !== undefined && this.series.xAxis.ddPoints[category].slice(0)
+		}, function () {
+			if (seriesOptions) {
+				if (_holdRedraw) {
+					chart.addSingleSeriesAsDrilldown(this, seriesOptions);
+				} else {
+					chart.addSeriesAsDrilldown(this, seriesOptions);
+				}
+			}
 		});
 		
-		if (seriesOptions) {
-			if (_holdRedraw) {
-				chart.addSingleSeriesAsDrilldown(this, seriesOptions);
-			} else {
-				chart.addSeriesAsDrilldown(this, seriesOptions);
-			}
-		}
+
 	};
 
 	/**
 	 * Drill down to a given category. This is the same as clicking on an axis label.
 	 */
-	H.Axis.prototype.drilldownCategory = function (x) {
+	H.Axis.prototype.drilldownCategory = function (x, e) {
 		var key,
 			point,
 			ddPointsX = this.ddPoints[x];
 		for (key in ddPointsX) {
 			point = ddPointsX[key];
 			if (point && point.series && point.series.visible && point.doDrilldown) { // #3197
-				point.doDrilldown(true, x);
+				point.doDrilldown(true, x, e);
 			}
 		}
 		this.chart.applyDrilldown();
@@ -612,8 +615,8 @@
 			label
 				.addClass('highcharts-drilldown-axis-label')
 				.css(axis.chart.options.drilldown.activeAxisLabelStyle)
-				.on('click', function () {
-					axis.drilldownCategory(pos);
+				.on('click', function (e) {
+					axis.drilldownCategory(pos, e);
 				});
 
 		} else if (label && label.basicStyles) {
@@ -645,11 +648,11 @@
 		if (point.drilldown) {
 			
 			// Add the click event to the point 
-			H.addEvent(point, 'click', function () {
+			H.addEvent(point, 'click', function (e) {
 				if (series.xAxis && series.chart.options.drilldown.allowPointDrilldown === false) {
-					series.xAxis.drilldownCategory(x);
+					series.xAxis.drilldownCategory(x, e);
 				} else {
-					point.doDrilldown();
+					point.doDrilldown(undefined, undefined, e);
 				}
 			});
 			/*wrap(point, 'importEvents', function (proceed) { // wrapping importEvents makes point.click event work

commit c2e49755722a531d6e52cd4a7e06c463a309995d

Dev branch regression in drilldown since ab42ff4.

js/modules/drilldown.src.js
@@ -555,12 +555,14 @@
 			category: category,
 			originalEvent: originalEvent, // docs
 			points: category !== undefined && this.series.xAxis.ddPoints[category].slice(0)
-		}, function () {
+		}, function (e) {
+			var chart = e.point.series.chart,
+				seriesOptions = e.seriesOptions;
 			if (seriesOptions) {
 				if (_holdRedraw) {
-					chart.addSingleSeriesAsDrilldown(this, seriesOptions);
+					chart.addSingleSeriesAsDrilldown(e.point, seriesOptions);
 				} else {
-					chart.addSeriesAsDrilldown(this, seriesOptions);
+					chart.addSeriesAsDrilldown(e.point, seriesOptions);
 				}
 			}
 		});