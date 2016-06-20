ba1c0486bca0dd351138a6ce9a4aa7d2d735860b
oysteinmoseng <oystein@highsoft.com>
2016-06-20 15:41:12 +0200

    Multinav: Updated demos.

--- samples/stock/navigator/multiple-series/demo.js
+++ samples/stock/navigator/multiple-series/demo.js
@@ -1,96 +1,50 @@
 $(function () {
-    var chart;
-    QUnit.test('Multiple series in navigator', function (assert) {
-        chart = Highcharts.stockChart('container', {
+    var seriesOptions = [],
+        seriesCounter = 0,
+        names = ['MSFT', 'AAPL', 'GOOG'];
+
+    /**
+     * Create the chart when all data is loaded
+     */
+    function createChart() {
+
+        $('#container').highcharts('StockChart', {
+
+            rangeSelector: {
+                selected: 4
+            },
+
             plotOptions: {
                 series: {
                     showInNavigator: true
                 }
             },
-            series: [{
-                data: [1, 2, 3, 4]
-            }, {
-                data: [5, 5, 4, 5]
-            }]
-        });
-        assert.strictEqual(chart.navigator.series.length, 2, 'Navigator has two series');
-    });
 
-    QUnit.test('Setting base series on chart', function (assert) {
-        chart = Highcharts.stockChart('container', {
-            navigator: {
-                baseSeries: 1
+            tooltip: {
+                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} USD</b><br/>',
+                valueDecimals: 2
             },
-            series: [{
-                data: [1, 2, 3, 4]
-            }, {
-                data: [5, 5, 4, 5]
-            }, {
-                showInNavigator: true,
-                data: [7, 7, 7, 7]
-            }, {
-                data: [3, 2, 1, 0]
-            }]
-        });
-        assert.strictEqual(chart.navigator.series.length, 2, 'Navigator has two series');
-    });
 
-/*    QUnit.test('Overriding base series option on chart', function (assert) {
-        chart = Highcharts.stockChart('container', {
-            navigator: {
-                baseSeries: 1
-            },
-            series: [{
-                data: [1, 2, 3, 4]
-            }, {
-                showInNavigator: false,
-                data: [5, 5, 4, 5]
-            }, {
-                showInNavigator: true,
-                data: [7, 7, 7, 7]
-            }, {
-                data: [3, 2, 1, 0]
-            }]
+            series: seriesOptions
         });
-        assert.strictEqual(chart.navigator.series.length, 1, 'Navigator has one series');
-    });
-*/
-/*
-    QUnit.test('3 of 4 series shown in navigator', function (assert) {
-        chart = Highcharts.stockChart('container', {
-            plotOptions: {
-                series: {
-                    showInNavigator: true
-                }
-            },
-            series: [{
-                data: [1, 2, 3, 4]
-            }, {
-                data: [5, 5, 4, 5]
-            }, {
-                showInNavigator: false,
-                data: [7, 7, 7, 7]
-            }, {
-                data: [3, 2, 1, 0]
-            }]
+    }
+
+    $.each(names, function (i, name) {
+
+        $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
+
+            seriesOptions[i] = {
+                name: name,
+                data: data
+            };
+
+            // As we're loading the data asynchronously, we don't know what order it will arrive. So
+            // we keep a counter and create the chart when all the data is loaded.
+            seriesCounter += 1;
+
+            if (seriesCounter === names.length) {
+                createChart();
+            }
         });
-        assert.strictEqual(chart.navigator.series.length, 3, 'Navigator has three series');
     });
-*/
-    /*QUnit.test('No series in navigator', function (assert) {
-        chart = Highcharts.stockChart('container', {
-            plotOptions: {
-                series: {
-                    showInNavigator: false
-                }
-            },
-            series: [{
-                data: [1, 2, 3, 4]
-            }, {
-                data: [5, 5, 4, 5]
-            }]
-        });
-        assert.strictEqual(chart.navigator.series.length, 0, 'Navigator has no series');
-    });
-    */
 });
