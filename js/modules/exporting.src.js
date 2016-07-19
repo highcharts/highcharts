7d6f15108469d45dde1f92415b7d7cb34a342a22
jon-a-nygaard <jon.arild@highsoft.com>
2016-07-19 09:06:39 +0200

    Fixed #5504, exporting module default settings was not compatible with https.

--- js/modules/exporting.src.js
+++ js/modules/exporting.src.js
@@ -102,7 +102,7 @@ defaultOptions.exporting = {
 	//enabled: true,
 	//filename: 'chart',
 	type: 'image/png',
-	url: 'http://export.highcharts.com/',
+	url: 'https://export.highcharts.com/',
 	//width: undefined,
 	printMaxWidth: 780,
 	scale: 2,
