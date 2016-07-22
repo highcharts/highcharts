b3043512321eac1e03d460efd060648ad0c1f35e
jon-a-nygaard <jon.arild@highsoft.com>
2016-07-22 15:02:59 +0200

    Fixed #5523, marker radius was incorrect when values was of type string.

--- js/parts/Series.js
+++ js/parts/Series.js
@@ -1096,7 +1096,7 @@ Series.prototype = {
 		if (seriesOptions.marker) { // line, spline, area, areaspline, scatter
 
 			// if no hover radius is given, default to normal radius + 2
-			stateOptionsHover.radius = stateOptionsHover.radius || normalOptions.radius + stateOptionsHover.radiusPlus;
+			stateOptionsHover.radius = +stateOptionsHover.radius || +normalOptions.radius + +stateOptionsHover.radiusPlus;
 			stateOptionsHover.lineWidth = stateOptionsHover.lineWidth || normalOptions.lineWidth + stateOptionsHover.lineWidthPlus;
 
 		} else { // column, bar, pie
