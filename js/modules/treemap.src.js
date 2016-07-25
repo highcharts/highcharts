highcharts/highcharts

commit 961f6bfdc8696b82595d73ad14f82418406f7b26

Added crisp correction for crisper treemap points.

js/modules/treemap.src.js
@@ -696,7 +696,9 @@
 				});
 
 			each(points, function (point) {
-				var groupKey = 'levelGroup-' + point.node.levelDynamic;
+				var groupKey = 'levelGroup-' + point.node.levelDynamic,
+					pointAttribs,
+					crispCorr;
 				if (!series[groupKey]) {
 					series[groupKey] = series.chart.renderer.g(groupKey)
 						.attr({
@@ -706,11 +708,17 @@
 				}
 				point.group = series[groupKey];
 				// Preliminary code in prepraration for HC5 that uses pointAttribs for all series
+				pointAttribs = series.pointAttribs(point);
 				point.pointAttr = {
-					'': series.pointAttribs(point),
+					'': pointAttribs,
 					'hover': series.pointAttribs(point, 'hover'),
 					'select': {}
 				};
+
+				// Crisp correction
+				crispCorr = pointAttribs['stroke-width'] % 2 / 2;
+				point.shapeArgs.x -= crispCorr;
+				point.shapeArgs.y -= crispCorr;
 			});
 			// Call standard drawPoints
 			seriesTypes.column.prototype.drawPoints.call(this);

commit f42381f4b8254d20d66c10a1ae1a5e57e298e4f0

Regression since 961f6bf

js/modules/treemap.src.js
@@ -719,7 +719,7 @@
 				};
 
 				// Crisp correction
-				crispCorr = pointAttribs['stroke-width'] % 2 / 2;
+				crispCorr = parseInt(pointAttribs['stroke-width'], 10) % 2 / 2;
 				point.shapeArgs.x -= crispCorr;
 				point.shapeArgs.y -= crispCorr;
 			});

samples/highcharts/plotoptions/treemap-levels/demo.js
@@ -5,7 +5,7 @@ $(function () {
             layoutAlgorithm: 'squarified',
             levels: [{
                 level: 1,
-                borderWidth: '3px',
+                borderWidth: 3,
                 dataLabels: {
                     enabled: true,
                     align: 'left',

samples/highcharts/plotoptions/treemap-sortindex/demo.js
@@ -6,7 +6,7 @@ $(function () {
             alternateStartingDirection: true,
             levels: [{
                 level: 1,
-                borderWidth: '3px',
+                borderWidth: 3,
                 dataLabels: {
                     enabled: true,
                     align: 'left',