highcharts/highcharts

commit b2ec8d91778276e4ac8e65aeff09364b621d0c58

Fix issues with scopes, touch #5390.

js/highstock.src.js
@@ -2,7 +2,7 @@
 // @compilation_level SIMPLE_OPTIMIZATIONS
 
 /**
- * @license Highstock JS v4.2.5-modified (2016-07-22)
+ * @license Highstock JS v4.2.5-modified (2016-07-25)
  *
  * (c) 2009-2016 Torstein Honsi
  *
@@ -20383,7 +20383,7 @@
      * End ordinal axis logic                                                   *
      *****************************************************************************/
     /**
-     * Highstock JS v4.2.5-modified (2016-07-22)
+     * Highstock JS v4.2.5-modified (2016-07-25)
      * Highcharts Broken Axis module
      * 
      * License: www.highcharts.com/license
@@ -22966,7 +22966,11 @@
             this._events = _events;
 
             // Data events
-            this.addBaseSeriesEvents();
+            if (this.series) {
+                addEvent(this.series.xAxis, 'foundExtremes', function () {
+                    chart.scroller.modifyNavigatorAxisExtremes();
+                });
+            }
 
             addEvent(chart, 'redraw', function () {
                 // Move the scrollbar after redraw, like after data updata even if axes don't redraw
@@ -22994,7 +22998,7 @@
         removeBaseSeriesEvents: function () {
             if (this.navigatorEnabled && this.baseSeries && this.baseSeries.xAxis && this.navigatorOptions.adaptToUpdatedData !== false) {
                 removeEvent(this.baseSeries, 'updatedData', this.updatedDataHandler);
-                removeEvent(this.baseSeries.xAxis, 'foundExtremes', this.chart.scroller.modifyBaseAxisExtremes);
+                removeEvent(this.baseSeries.xAxis, 'foundExtremes', this.modifyBaseAxisExtremes);
             }
         },
 
@@ -23440,7 +23444,6 @@
 
             if (xAxis.getExtremes) {
                 unionExtremes = this.getUnionExtremes(true);
-
                 if (unionExtremes && (unionExtremes.dataMin !== xAxis.min || unionExtremes.dataMax !== xAxis.max)) {
                     xAxis.min = unionExtremes.dataMin;
                     xAxis.max = unionExtremes.dataMax;
@@ -23452,20 +23455,20 @@
          * Hook to modify the base axis extremes with information from the Navigator
          */
         modifyBaseAxisExtremes: function () {
-            if (!this.baseSeries || !this.baseSeries.xAxis) {
+            if (!this.chart.scroller.baseSeries) {
                 return;
             }
 
-            var baseSeries = this.baseSeries,
-                baseXAxis = baseSeries.xAxis,
+            var baseXAxis = this,
+                scroller = baseXAxis.chart.scroller,
                 baseExtremes = baseXAxis.getExtremes(),
                 baseMin = baseExtremes.min,
                 baseMax = baseExtremes.max,
                 baseDataMin = baseExtremes.dataMin,
                 baseDataMax = baseExtremes.dataMax,
                 range = baseMax - baseMin,
-                stickToMin = this.stickToMin,
-                stickToMax = this.stickToMax,
+                stickToMin = scroller.stickToMin,
+                stickToMax = scroller.stickToMax,
                 newMax,
                 newMin,
                 navigatorSeries = this.series,
@@ -23503,7 +23506,7 @@
             }
 
             // Reset
-            this.stickToMin = this.stickToMax = null;
+            scroller.stickToMin = scroller.stickToMax = null;
         },
 
         /**

js/parts/Scroller.js
@@ -376,7 +376,11 @@ Navigator.prototype = {
 		this._events = _events;
 
 		// Data events
-		this.addBaseSeriesEvents();
+		if (this.series) {
+			addEvent(this.series.xAxis, 'foundExtremes', function () {
+				chart.scroller.modifyNavigatorAxisExtremes();
+			});
+		}
 
 		addEvent(chart, 'redraw', function () {
 			// Move the scrollbar after redraw, like after data updata even if axes don't redraw
@@ -404,7 +408,7 @@ Navigator.prototype = {
 	removeBaseSeriesEvents: function () {
 		if (this.navigatorEnabled && this.baseSeries && this.baseSeries.xAxis && this.navigatorOptions.adaptToUpdatedData !== false) {
 			removeEvent(this.baseSeries, 'updatedData', this.updatedDataHandler);
-			removeEvent(this.baseSeries.xAxis, 'foundExtremes', this.chart.scroller.modifyBaseAxisExtremes);
+			removeEvent(this.baseSeries.xAxis, 'foundExtremes', this.modifyBaseAxisExtremes);
 		}
 	},
 
@@ -850,7 +854,6 @@ Navigator.prototype = {
 
 		if (xAxis.getExtremes) {
 			unionExtremes = this.getUnionExtremes(true);
-
 			if (unionExtremes && (unionExtremes.dataMin !== xAxis.min || unionExtremes.dataMax !== xAxis.max)) {
 				xAxis.min = unionExtremes.dataMin;
 				xAxis.max = unionExtremes.dataMax;
@@ -862,20 +865,20 @@ Navigator.prototype = {
 	 * Hook to modify the base axis extremes with information from the Navigator
 	 */
 	modifyBaseAxisExtremes: function () {
-		if (!this.baseSeries || !this.baseSeries.xAxis) {
+		if (!this.chart.scroller.baseSeries) {
 			return;
 		}
 
-		var baseSeries = this.baseSeries,
-			baseXAxis = baseSeries.xAxis,
+		var baseXAxis = this,
+			scroller = baseXAxis.chart.scroller,
 			baseExtremes = baseXAxis.getExtremes(),
 			baseMin = baseExtremes.min,
 			baseMax = baseExtremes.max,
 			baseDataMin = baseExtremes.dataMin,
 			baseDataMax = baseExtremes.dataMax,
 			range = baseMax - baseMin,
-			stickToMin = this.stickToMin,
-			stickToMax = this.stickToMax,
+			stickToMin = scroller.stickToMin,
+			stickToMax = scroller.stickToMax,
 			newMax,
 			newMin,
 			navigatorSeries = this.series,
@@ -913,7 +916,7 @@ Navigator.prototype = {
 		}
 
 		// Reset
-		this.stickToMin = this.stickToMax = null;
+		scroller.stickToMin = scroller.stickToMax = null;
 	},
 
 	/**

commit e7f7ba23fa941044205c903c45e2f0788ce40d4e

Regression since b2ec8d9, failed test for stock/issues/624.

js/highstock.src.js
@@ -23454,10 +23454,10 @@
          * Hook to modify the base axis extremes with information from the Navigator
          */
         modifyBaseAxisExtremes: function () {
-            if (!this.chart.scroller.baseSeries) {
+            if (!this.chart.scroller.baseSeries || !this.chart.scroller.baseSeries.xAxis) {
                 return;
             }
-
+        
             var baseXAxis = this,
                 scroller = baseXAxis.chart.scroller,
                 baseExtremes = baseXAxis.getExtremes(),
@@ -23470,7 +23470,7 @@
                 stickToMax = scroller.stickToMax,
                 newMax,
                 newMin,
-                navigatorSeries = this.series,
+                navigatorSeries = scroller.series,
                 hasSetExtremes = !!baseXAxis.setExtremes,
 
                 // When the extremes have been set by range selector button, don't stick to min or max.
@@ -23520,7 +23520,7 @@
 
             // Detect whether the zoomed area should stick to the minimum or maximum. If the current
             // axis minimum falls outside the new updated dataset, we must adjust.
-            scroller.stickToMin = baseSeries.xAxis.min <= baseSeries.xData[0];
+            scroller.stickToMin = isNumber(baseSeries.xAxis.min) && (baseSeries.xAxis.min <= baseSeries.xData[0]);
             // If the scrollbar is scrolled all the way to the right, keep right as new data 
             // comes in.
             scroller.stickToMax = Math.round(scroller.zoomedMax) >= Math.round(scroller.navigatorWidth);

js/parts/Scroller.js
@@ -865,10 +865,10 @@ Navigator.prototype = {
 	 * Hook to modify the base axis extremes with information from the Navigator
 	 */
 	modifyBaseAxisExtremes: function () {
-		if (!this.chart.scroller.baseSeries) {
+		if (!this.chart.scroller.baseSeries || !this.chart.scroller.baseSeries.xAxis) {
 			return;
 		}
-
+		
 		var baseXAxis = this,
 			scroller = baseXAxis.chart.scroller,
 			baseExtremes = baseXAxis.getExtremes(),
@@ -881,7 +881,7 @@ Navigator.prototype = {
 			stickToMax = scroller.stickToMax,
 			newMax,
 			newMin,
-			navigatorSeries = this.series,
+			navigatorSeries = scroller.series,
 			hasSetExtremes = !!baseXAxis.setExtremes,
 
 			// When the extremes have been set by range selector button, don't stick to min or max.
@@ -931,7 +931,7 @@ Navigator.prototype = {
 
 		// Detect whether the zoomed area should stick to the minimum or maximum. If the current
 		// axis minimum falls outside the new updated dataset, we must adjust.
-		scroller.stickToMin = baseSeries.xAxis.min <= baseSeries.xData[0];
+		scroller.stickToMin = isNumber(baseSeries.xAxis.min) && (baseSeries.xAxis.min <= baseSeries.xData[0]);
 		// If the scrollbar is scrolled all the way to the right, keep right as new data 
 		// comes in.
 		scroller.stickToMax = Math.round(scroller.zoomedMax) >= Math.round(scroller.navigatorWidth);