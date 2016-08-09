ad053d98837722b420dcea9dc9c7b3f94eea5bb0
jon-a-nygaard <jon.arild@highsoft.com>
2016-08-09 20:37:21 +0200

    Addition to #5396, fixed linting errors in activeDataLabelStyle unit tests.

--- samples/unit-tests/drilldown/activedatalabelstyle/demo.js
+++ samples/unit-tests/drilldown/activedatalabelstyle/demo.js
@@ -1,37 +1,37 @@
 QUnit.test('activeDataLabelStyle', function (assert) {
-	var getDataLabelFill = function (point) {
-		return point.dataLabel.element.childNodes[0].style.fill;
-	};
-	var chart = Highcharts.chart('container', {
-			chart: {
-				type: 'column'
-			},
-			plotOptions: {
-				series: {
-					dataLabels: {
-						enabled: true,
-						inside: true
-					}
-				}
-			},
-			series: [{
-				data: [{
-					drilldown: 'fruits',
-					y: 20,
-					color: '#000000'
-				}]
-			}],
-			drilldown: {
-				series: [{
-					id: 'fruits',
-					data: [2]
-				}]
-			}
-		})
-		series = series = chart.series[0]
-		point = series.points[0];
+    var getDataLabelFill = function (point) {
+        return point.dataLabel.element.childNodes[0].style.fill;
+    };
+    var chart = Highcharts.chart('container', {
+            chart: {
+                type: 'column'
+            },
+            plotOptions: {
+                series: {
+                    dataLabels: {
+                        enabled: true,
+                        inside: true
+                    }
+                }
+            },
+            series: [{
+                data: [{
+                    drilldown: 'fruits',
+                    y: 20,
+                    color: '#000000'
+                }]
+            }],
+            drilldown: {
+                series: [{
+                    id: 'fruits',
+                    data: [2]
+                }]
+            }
+        }),
+        series = chart.series[0],
+        point = series.points[0];
     assert.strictEqual(
-    	getDataLabelFill(point),
+        getDataLabelFill(point),
         'rgb(13, 35, 58)',
         'activeDataLabelStyle.color default to rgb(13, 35, 58)'
     );
@@ -39,47 +39,47 @@ QUnit.test('activeDataLabelStyle', function (assert) {
     // @notice Would have been nice with a chart.update.
     // @notice activeDataLabelStyle should probably be possible to override on a series or point level.
     chart.destroy();
-	chart = Highcharts.chart('container', {
-			chart: {
-				type: 'column'
-			},
-			plotOptions: {
-				series: {
-					dataLabels: {
-						enabled: true,
-						inside: true
-					}
-				}
-			},
-			series: [{
-				data: [{
-					drilldown: 'fruits',
-					y: 20,
-					color: '#000000'
-				}]
-			}],
-			drilldown: {
-				activeDataLabelStyle: {
-					color: 'contrast'
-				},
-				series: [{
-					id: 'fruits',
-					data: [2]
-				}]
-			}
-		}),
-		series = chart.series[0],
-		point = series.points[0];
+    chart = Highcharts.chart('container', {
+        chart: {
+            type: 'column'
+        },
+        plotOptions: {
+            series: {
+                dataLabels: {
+                    enabled: true,
+                    inside: true
+                }
+            }
+        },
+        series: [{
+            data: [{
+                drilldown: 'fruits',
+                y: 20,
+                color: '#000000'
+            }]
+        }],
+        drilldown: {
+            activeDataLabelStyle: {
+                color: 'contrast'
+            },
+            series: [{
+                id: 'fruits',
+                data: [2]
+            }]
+        }
+    });
+    series = chart.series[0];
+    point = series.points[0];
     assert.strictEqual(
-    	getDataLabelFill(point),
+        getDataLabelFill(point),
         'rgb(255, 255, 255)',
         'activeDataLabelStyle.color contrast to black'
     );
     point.update({
-    	color: '#FFFFFF'
+        color: '#FFFFFF'
     });
     assert.strictEqual(
-    	getDataLabelFill(point),
+        getDataLabelFill(point),
         'rgb(0, 0, 0)',
         'activeDataLabelStyle.color contrast to white'
     );
