bd5a58d88f1a161afa55b16fb60f5f0f80d1afea
Paweł Fus <pawel.fus.88@gmail.com>
2016-08-24 13:00:01 +0200

    Fixed #3961, bubble series caused javascript errors in Highstock.

--- js/highcharts.src.js
+++ js/highcharts.src.js
@@ -15304,15 +15304,17 @@
                 chartSizeMax = mathMax(chart.chartWidth, chart.chartHeight),
                 axis = this[(this.zoneAxis || 'y') + 'Axis'],
                 extremes,
-                reversed = axis.reversed,
+                reversed,
                 inverted = chart.inverted,
-                horiz = axis.horiz,
+                horiz,
                 pxRange,
                 pxPosMin,
                 pxPosMax,
                 ignoreZones = false;
 
-            if (zones.length && (graph || area) && axis.min !== UNDEFINED) {
+            if (zones.length && (graph || area) && axis && axis.min !== UNDEFINED) {
+                reversed = axis.reversed;
+                horiz = axis.horiz;
                 // The use of the Color Threshold assumes there are no gaps
                 // so it is safe to hide the original graph and area
                 if (graph) {
@@ -15362,7 +15364,7 @@
                     }
 
                     /// VML SUPPPORT
-                    if (chart.inverted && renderer.isVML) {
+                    if (inverted && renderer.isVML) {
                         if (axis.isXAxis) {
                             clipAttr = {
                                 x: 0,
