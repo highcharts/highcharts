6fba87d22d5ccb4f7b093762da990933130a5cd0
Paweł Fus <pawel.fus.88@gmail.com>
2016-09-07 14:23:38 +0200

    Fixed #3228, inputs in rangeSelector didn't allow to view the historical data in lazy loading.

--- js/highstock.src.js
+++ js/highstock.src.js
@@ -24204,7 +24204,7 @@
             function updateExtremes() {
                 var inputValue = input.value,
                     value = (options.inputDateParser || Date.parse)(inputValue),
-                    xAxis = chart.xAxis[0],
+                    xAxis = chart.scroller && chart.scroller.xAxis ? chart.scroller.xAxis : chart.xAxis[0],
                     dataMin = xAxis.dataMin,
                     dataMax = xAxis.dataMax;
                 if (value !== input.previousValue) {
