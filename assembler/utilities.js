fa31dbf01f7e6eb1a0aa777a7453b0b9f5a61b07
jon-a-nygaard <jon.arild@highsoft.com>
2016-09-17 10:30:20 +0200

    Added removeDirectory and removeFile functions to assembler/utilities.js.

--- assembler/utilities.js
+++ assembler/utilities.js
@@ -10,6 +10,22 @@ const debug = (d, text) => {
 };
 
 /**
+ * Check wether a file path exists or not.
+ * @param  {string} path Path to file or directory
+ * @returns {boolean} True if path exists, false if not.
+ */
+const exists = path => {
+    const fs = require('fs');
+    let e = true;
+    try {
+        fs.statSync(path);
+    } catch (err) {
+        e = false;
+    }
+    return e;
+};
+
+/**
  * Gets directory path from a file path
  * @param  {string} path File path
  * @returns {string} Path to directory where the file is located
@@ -41,6 +57,47 @@ const createDirectory = path => {
     }, '');
 };
 
+/**
+ * Removes a file.
+ * Creates a promise which resolves when the file is deleted.
+ * Promise is rejected if the file does not exist.
+ * @param  {string} path Path to file
+ * @returns {Promise} Returns a promise which resolves when the file is deleted.
+ */
+const removeFile = path => new Promise((resolve, reject) => {
+    const fs = require('fs');
+    if (exists(path)) {
+        fs.unlink(path, () => {
+            resolve(true);
+        });
+    } else {
+        reject('File does not exist: ' + path);
+    }
+});
+
+/**
+ * Removes a directory.
+ * Creates a promise which resolves when the directory is deleted.
+ * Promise is rejected if the file does not exist.
+ * @param  {string} path Path to file
+ * @returns {Promise} Returns a promise which resolves when the file is deleted.
+ */
+const removeDirectory = path => new Promise((resolve, reject) => {
+    const fs = require('fs');
+    if (exists(path)) {
+        const files = fs.readdirSync(path);
+        const promises = files.map(file => path + '/' + file)
+            .map(itemPath => (fs.statSync(itemPath).isDirectory()) ? removeDirectory(itemPath) : removeFile(itemPath));
+        Promise.all(promises).then(() => {
+            fs.rmdirSync(path);
+            resolve(true);
+        })
+        .catch(err => reject(err.message + '\n\r' + err.stack));
+    } else {
+        reject('Directory does not exist: ' + path);
+    }
+});
+
 const writeFile = (path, content) => {
     const fs = require('fs');
     createDirectory(folder(path));
@@ -50,6 +107,9 @@ const writeFile = (path, content) => {
 module.exports = {
     createDirectory,
     debug,
+    exists,
     folder,
+    removeDirectory,
+    removeFile,
     writeFile
 };
