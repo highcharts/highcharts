29257e58b368ecf246e9bcc25d43c3d8caf440fd
jon-a-nygaard <jon.arild@highsoft.com>
2016-09-21 11:15:43 +0200

    Added license headers to all files in parts folder.

--- js/parts/Globals.js
+++ js/parts/Globals.js
@@ -1,3 +1,11 @@
+/**
+ * @license @product.name@ JS v@product.version@ (@product.date@)
+ * Highcharts funnel module
+ *
+ * (c) 2010-2016 Torstein Honsi
+ *
+ * License: www.highcharts.com/license
+ */
 'use strict';
 /* global window */
 	var win = window,
