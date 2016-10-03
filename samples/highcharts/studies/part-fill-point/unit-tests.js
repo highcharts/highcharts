bb4cbbd363ceb8b3e5aa68a7be459f8de83525c4
Lars Alberto Vangsnes Cabrera <lars@highsoft.com>
2016-10-03 19:27:16 +0200

    PartFillPoint: Added unit tests for translate() and drawPoints(): Positions are calculated and rendered correctly.

--- samples/highcharts/studies/part-fill-point/unit-tests.js
+++ samples/highcharts/studies/part-fill-point/unit-tests.js
@@ -46,3 +46,141 @@ QUnit.test('supportsPartFill', function (assert) {
     }
 
 });
+
+/**
+ * Checks that the extra shape is calculated correctly.
+ */
+QUnit.test('translate()', function (assert) {
+    var chart,
+        point,
+        shapeArgs,
+        partFillShape,
+        partialFill;
+
+    $('#container').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Highcharts PartFillPoint'
+        },
+        series: [{
+            data: [{
+                x: 0,
+                y: 107,
+                partialFill: 0.25
+            }, {
+                x: 1,
+                y: 31,
+                partialFill: 0.5
+            }, {
+                x: 2,
+                y: 635,
+                partialFill: 0.75
+            }]
+        }]
+    });
+
+    chart = $('#container').highcharts();
+
+    point = chart.series[0].points[0];
+    shapeArgs = point.shapeArgs;
+    partFillShape = point.partFillShape;
+    partialFill = point.partialFill;
+
+    assert.equal(
+        shapeArgs.y,
+        partFillShape.y - Math.abs(shapeArgs.height - (shapeArgs.height * partialFill)),
+        'Original shapeArgs y-position was untouched'
+    );
+
+    assert.equal(
+        shapeArgs.height,
+        partFillShape.height / partialFill,
+        'Original shapeArgs height was untouched'
+    );
+
+    assert.equal(
+        partFillShape.y,
+        shapeArgs.y + Math.abs(shapeArgs.height - (shapeArgs.height * partialFill)),
+        'Extra shape has correct calculated y-position'
+    );
+
+    assert.equal(
+        partFillShape.height,
+        shapeArgs.height * partialFill,
+        'Extra shape has correct calculated height'
+    );
+});
+
+/**
+ * Checks that the extra shape is rendered correctly.
+ */
+QUnit.test('drawPoints()', function (assert) {
+    var chart,
+        $graphic,
+        $extraGraphic,
+        origY,
+        origHeight,
+        extraY,
+        extraHeight,
+        partialFill;
+
+    $('#container').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Highcharts PartFillPoint'
+        },
+        series: [{
+            data: [{
+                x: 0,
+                y: 107,
+                partialFill: 0.25
+            }, {
+                x: 1,
+                y: 31,
+                partialFill: 0.5
+            }, {
+                x: 2,
+                y: 635,
+                partialFill: 0.75
+            }]
+        }]
+    });
+
+    chart = $('#container').highcharts();
+
+    $graphic = $(chart.series[0].points[0].graphic.element);
+    $extraGraphic = $(chart.series[0].points[0].partFillGraphic.element);
+    origY = parseFloat($graphic.attr('y'));
+    origHeight = parseFloat($graphic.attr('height'));
+    extraY = parseFloat($extraGraphic.attr('y'));
+    extraHeight = parseFloat($extraGraphic.attr('height'));
+    partialFill = chart.series[0].points[0].partialFill;
+
+    assert.equal(
+        origY,
+        extraY - Math.abs(origHeight - (origHeight * partialFill)),
+        'Original shape has correct rendered y-position'
+    );
+
+    assert.equal(
+        origHeight,
+        extraHeight / partialFill,
+        'Original shape has correct rendered height'
+    );
+
+    assert.equal(
+        extraY,
+        origY + Math.abs(origHeight - (origHeight * partialFill)),
+        'Extra shape has correct rendered y-position'
+    );
+
+    assert.equal(
+        extraHeight,
+        origHeight * partialFill,
+        'Extra shape has correct rendered height'
+    );
+});
