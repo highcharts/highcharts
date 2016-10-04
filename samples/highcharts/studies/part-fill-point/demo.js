d786ba6f76c0a507010af86c15000277f9557336
Lars Alberto Vangsnes Cabrera <lars@highsoft.com>
2016-10-04 14:25:07 +0200

    xrange: Updated studies.

--- samples/highcharts/studies/part-fill-point/demo.js
+++ samples/highcharts/studies/part-fill-point/demo.js
@@ -1,27 +1,46 @@
-console.clear();
 $(function () {
 
-    // THE CHART
     $('#container').highcharts({
         chart: {
-            type: 'bar'
+            type: 'xrange'
         },
         title: {
-            text: 'Highcharts PartFillPoint'
+            text: 'Highcharts X-range study'
+        },
+        subtitle: {
+            text: 'With partially filled points'
+        },
+        xAxis: {
+            type: 'datetime',
+            min: Date.UTC(2014, 11, 1),
+            max: Date.UTC(2014, 11, 9)
+        },
+        yAxis: {
+            title: '',
+            categories: ['Prototyping', 'Development', 'Testing'],
+            reversed: true
         },
         series: [{
+            name: 'Project 1',
+            // pointPadding: 0,
+            // groupPadding: 0,
+            borderRadius: 5,
+            pointWidth: 10,
             data: [{
-                x: 0,
-                y: 107,
-                partialFill: 0.25
+                x: Date.UTC(2014, 11, 1),
+                x2: Date.UTC(2014, 11, 2),
+                partialFill: 0.25,
+                y: 0
             }, {
-                x: 1,
-                y: 31,
-                partialFill: 0.5
+                x: Date.UTC(2014, 11, 2),
+                x2: Date.UTC(2014, 11, 5),
+                partialFill: 0.5,
+                y: 1
             }, {
-                x: 2,
-                y: 635,
-                partialFill: 0.75
+                x: Date.UTC(2014, 11, 8),
+                x2: Date.UTC(2014, 11, 9),
+                partialFill: 0.75,
+                y: 2
             }]
         }]
     });
