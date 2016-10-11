55ce03578fd300ef824b1961cb347e1b800331d3
oysteinmoseng <oystein@highsoft.com>
2016-10-11 10:44:17 +0200

    Changed demos to use jQuery on() instead of bind(). Closes #5808.

--- samples/highcharts/studies/timeline/demo.js
+++ samples/highcharts/studies/timeline/demo.js
@@ -117,7 +117,7 @@ $(function () {
     /**
      * Toggle play and pause from the button
      */
-    $('#play-pause-button').bind('click', function () {
+    $('#play-pause-button').on('click', function () {
         if (chart.sequenceTimer === undefined) {
             play(this);
         } else {
@@ -128,7 +128,7 @@ $(function () {
     /**
      * Update the chart when the input is changed
      */
-    $('#play-range').bind('input', function () {
+    $('#play-range').on('input', function () {
         update();
     });
 });
