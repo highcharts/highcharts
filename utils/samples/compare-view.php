f136f31cd14c9655b2e1f77bcd5c5cadbfa1dba3
Torstein Hønsi <torstein@highsoft.com>
2016-10-24 14:04:29 +0200

    Utils

--- utils/samples/compare-view.php
+++ utils/samples/compare-view.php
@@ -737,7 +737,7 @@
 			<h2 style="margin: 0"><?php echo $path ?></h2>
 
 			<div style="text-align: right">
-				<a class="button" id="commits" style="margin-left: 1em" >Test by commit</a>
+				<a class="button" id="commits" style="margin-left: 1em" >Bisect</a>
 				<button id="comment" style="margin-left: 1em"><i class="icon-comment"></i> Comment</button>
 				<button id="reload" style="margin-left: 1em">Reload</button>
 			</div>
