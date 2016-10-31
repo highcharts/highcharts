57295e8463e67c458cd7ddd39d474aa47bcb1b9f
Torstein Hønsi <torstein@highsoft.com>
2016-10-31 14:57:10 +0100

    Added new option, `lang.numericSymbolMagnitude`, to support numeric symbol shortening in Japanese, Korean and certain Chinese locales.

---
+++ samples/highcharts/lang/numericsymbolmagnitude/demo.js
@@ -0,0 +1,33 @@
+$(function () {
+
+    Highcharts.setOptions({
+        lang: {
+            numericSymbols: ['万', '億'],
+            numericSymbolMagnitude: 10000
+        }
+    });
+
+    Highcharts.chart('container', {
+
+        title: {
+            text: 'Numeric symbols magnitude'
+        },
+
+        subtitle: {
+            text: 'Japanese uses ten thousands (万) as numeric symbol'
+        },
+
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+
+        series: [{
+            data: [2990, 7150, 10640, 12920, 14400, 17600,
+                13560, 14850, 21640, 19410, 9560, 5440],
+            type: 'column'
+        }]
+
+    });
+
+});
