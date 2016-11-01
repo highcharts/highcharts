6643d6318b2f60f063c57cf09aa8560df1a8f1d5
Torstein Hønsi <torstein@highsoft.com>
2016-11-01 15:13:05 +0100

    Samples: Updated

--- samples/stock/members/axis-setextremes/demo.js
+++ samples/stock/members/axis-setextremes/demo.js
@@ -13,8 +13,8 @@ $(function () {
 
     $('#button').click(function () {
         chart.xAxis[0].setExtremes(
-            Date.UTC(2007, 0, 1),
-            Date.UTC(2007, 11, 31)
+            Date.UTC(2014, 0, 1),
+            Date.UTC(2014, 11, 31)
         );
     });
 });
