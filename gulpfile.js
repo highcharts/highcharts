47bedfbd57181284d35cce318ff79d2a8c0613b8
jon-a-nygaard <jon.arild@highsoft.com>
2016-11-04 08:53:30 +0100

    Modified scripts task to support multiple files as input using comma separation.

--- gulpfile.js
+++ gulpfile.js
@@ -53,13 +53,13 @@ const getProductVersion = () => {
 
 /**
  * Gulp task to run the building process of distribution files. By default it builds all the distribution files. Usage: "gulp build".
- * @param {string} --file Optional command line argument. Use to build a single file. Usage: "gulp build --file highcharts.js"
+ * @param {string} --file Optional command line argument. Use to build a one or sevral files. Usage: "gulp build --file highcharts.js,modules/data.src.js"
  * @return undefined
  */
 const scripts = () => {
     let build = require('./assembler/build').build;
     // let argv = require('yargs').argv; Already declared in the upper scope
-    let files = (argv.file) ? [argv.file] : null,
+    let files = (argv.file) ? argv.file.split(',') : null,
         type = (argv.type) ? argv.type : 'both',
         debug = argv.d || false,
         version = getProductVersion(),
