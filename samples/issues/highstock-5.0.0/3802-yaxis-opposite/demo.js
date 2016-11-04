4b015d5bfbc2d19a4d574832bd2775829aeca16a
Torstein Hønsi <torstein@highsoft.com>
2016-11-04 16:00:29 +0100

    Fixed #3802, `yAxis.opposite` ignored when `setOptions` was used.

---
+++ samples/issues/highstock-5.0.0/3802-yaxis-opposite/demo.js
@@ -0,0 +1,26 @@
+$(function () {
+  QUnit.test('yAxis should be on the left side, when opposite is false', function (assert) {
+    var done = assert.async();
+
+    Highcharts.setOptions({
+        yAxis: {
+            opposite: false,
+            lineColor: 'red',
+            lineWidth: 2
+        }
+    });
+
+    var chart = $('#container').highcharts('StockChart', {
+        series: [{
+          data: [1, 20, 5, 1, 11]
+        }]
+    }).highcharts();
+
+    assert.strictEqual(
+      chart.yAxis[0].options.opposite,
+      false,
+      'yAxis is on the left side'
+    );
+
+  });
+});
