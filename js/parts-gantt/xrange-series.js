5b5e67fd829dca46157d09862c2fc60ce02518af
Lars Alberto Vangsnes Cabrera <lars@highsoft.com>
2016-11-04 18:50:21 +0100

    XRange: Refactored drawPoints() and translate(): - Moved each point calculation to separate functions.

--- js/parts-gantt/xrange-series.js
+++ js/parts-gantt/xrange-series.js
@@ -91,163 +91,173 @@ seriesTypes.xrange = extendClass(columnType, {
 		return crop;
 	},
 
-	translate: function () {
-		columnType.prototype.translate.apply(this, arguments);
+	translatePoint: function (point) {
 		var series = this,
 			xAxis = series.xAxis,
 			metrics = series.columnMetrics,
-			minPointLength = series.options.minPointLength || 0;
+			minPointLength = series.options.minPointLength || 0,
+			plotX = point.plotX,
+			posX = pick(point.x2, point.x + (point.len || 0)),
+			plotX2 = xAxis.toPixels(posX, true),
+			width = plotX2 - plotX,
+			widthDifference,
+			shapeArgs,
+			partialFill;
+
+		if (minPointLength) {
+			widthDifference = minPointLength - width;
+			if (widthDifference < 0) {
+				widthDifference = 0;
+			}
+			plotX -= widthDifference / 2;
+			plotX2 += widthDifference / 2;
+		}
+
+		plotX = Math.max(plotX, -10);
+		plotX2 = Math.min(Math.max(plotX2, -10), xAxis.len + 10);
+
+		point.shapeArgs = {
+			x: plotX,
+			y: point.plotY + metrics.offset,
+			width: plotX2 - plotX,
+			height: metrics.width
+		};
+		point.tooltipPos[0] += width / 2;
+		point.tooltipPos[1] -= metrics.width / 2;
+
+		// Add a partShapeArgs to the point, based on the shapeArgs property
+		partialFill = point.partialFill;
+		if (partialFill) {
+			// Get the partial fill amount
+			if (isObject(partialFill)) {
+				partialFill = partialFill.amount;
+			}
+			// If it was not a number, assume 0
+			if (!isNumber(partialFill)) {
+				partialFill = 0;
+			}
+			shapeArgs = point.shapeArgs;
+			point.partShapeArgs = {
+				x: shapeArgs.x + 0.5,
+				y: shapeArgs.y + 0.5,
+				width: shapeArgs.width - 1,
+				height: shapeArgs.height - 1
+			};
+			point.clipRectArgs = {
+				x: shapeArgs.x,
+				y: shapeArgs.y,
+				width: shapeArgs.width * partialFill,
+				height: shapeArgs.height
+			};
+		}
+	},
+
+	translate: function () {
+		columnType.prototype.translate.apply(this, arguments);
+		var series = this;
 
 		each(series.points, function (point) {
-			var plotX = point.plotX,
-				posX = pick(point.x2, point.x + (point.len || 0)),
-				plotX2 = xAxis.toPixels(posX, true),
-				width = plotX2 - plotX,
-				widthDifference,
-				shapeArgs,
-				partialFill;
-
-			if (minPointLength) {
-				widthDifference = minPointLength - width;
-				if (widthDifference < 0) {
-					widthDifference = 0;
-				}
-				plotX -= widthDifference / 2;
-				plotX2 += widthDifference / 2;
-			}
-
-			plotX = Math.max(plotX, -10);
-			plotX2 = Math.min(Math.max(plotX2, -10), xAxis.len + 10);
-
-			point.shapeArgs = {
-				x: plotX,
-				y: point.plotY + metrics.offset,
-				width: plotX2 - plotX,
-				height: metrics.width
-			};
-			point.tooltipPos[0] += width / 2;
-			point.tooltipPos[1] -= metrics.width / 2;
-
-			// Add a partShapeArgs to the point, based on the shapeArgs property
-			partialFill = point.partialFill;
-			if (partialFill) {
-				// Get the partial fill amount
-				if (isObject(partialFill)) {
-					partialFill = partialFill.amount;
-				}
-				// If it was not a number, assume 0
-				if (!isNumber(partialFill)) {
-					partialFill = 0;
-				}
-				shapeArgs = point.shapeArgs;
-				point.partShapeArgs = {
-					x: shapeArgs.x + 0.5,
-					y: shapeArgs.y + 0.5,
-					width: shapeArgs.width - 1,
-					height: shapeArgs.height - 1
-				};
-				point.clipRectArgs = {
-					x: shapeArgs.x,
-					y: shapeArgs.y,
-					width: shapeArgs.width * partialFill,
-					height: shapeArgs.height
-				};
-			}
+			series.translatePoint(point);
 		});
+	},
+
+	drawPoint: function (point, verb) {
+		var series = this,
+			plotY = point.plotY,
+			seriesOpts = series.options,
+			renderer = series.chart.renderer,
+			graphic = point.graphic,
+			type = point.shapeType,
+			shapeArgs = point.shapeArgs,
+			partShapeArgs = point.partShapeArgs,
+			clipRectArgs = point.clipRectArgs,
+			pfOptions = point.partialFill,
+			fill,
+			state = point.selected && 'select',
+			cutOff = seriesOpts.stacking && !seriesOpts.borderRadius;
+
+		if (isNumber(plotY) && point.y !== null) {
+			if (graphic) { // update
+				stop(graphic);
+				point.graphicOriginal[verb](
+					merge(shapeArgs)
+				);
+				if (partShapeArgs) {
+					point.graphicOverlay[verb](
+						merge(partShapeArgs)
+					);
+					point.clipRect.animate(
+						merge(clipRectArgs)
+					);
+				}
+
+			} else {
+				point.graphic = graphic = renderer.g('point')
+					.attr({
+						'class': point.getClassName()
+					})
+					.add(point.group || series.group);
+
+				point.graphicOriginal = renderer[type](shapeArgs)
+					.addClass('highcharts-partfill-original')
+					.add(graphic);
+				if (clipRectArgs && partShapeArgs) {
+
+					point.clipRect = renderer.clipRect(
+						clipRectArgs.x,
+						clipRectArgs.y,
+						clipRectArgs.width,
+						clipRectArgs.height
+					);
+
+					point.graphicOverlay = renderer[type](partShapeArgs)
+						.addClass('highcharts-partfill-overlay')
+						.add(graphic)
+						.clip(point.clipRect);
+				}
+			}
+
+			/*= if (build.classic) { =*/
+			// Presentational
+			point.graphicOriginal
+				.attr(series.pointAttribs(point, state))
+				.shadow(seriesOpts.shadow, null, cutOff);
+			if (partShapeArgs) {
+				// Ensure pfOptions is an object
+				if (!isObject(pfOptions)) {
+					pfOptions = {};
+				}
+				if (isObject(seriesOpts.partialFill)) {
+					pfOptions = merge(pfOptions, seriesOpts.partialFill);
+				}
+
+				fill = pfOptions.fill ||
+						color(series.color).brighten(-0.3).get('rgb');
+				point.graphicOverlay
+					.attr(series.pointAttribs(point, state))
+					.attr({
+						'fill': fill,
+						'stroke-width': 0.1
+					})
+					.shadow(seriesOpts.shadow, null, cutOff);
+			}
+			/*= } =*/
+
+		} else if (graphic) {
+			point.graphic = graphic.destroy(); // #1269
+		}
 	},
 
 	drawPoints: function () {
 		var series = this,
 			chart = this.chart,
 			options = series.options,
-			renderer = chart.renderer,
 			animationLimit = options.animationLimit || 250,
 			verb = chart.pointCount < animationLimit ? 'animate' : 'attr';
 
 		// draw the columns
 		each(series.points, function (point) {
-			var plotY = point.plotY,
-				graphic = point.graphic,
-				type = point.shapeType,
-				shapeArgs = point.shapeArgs,
-				partShapeArgs = point.partShapeArgs,
-				clipRectArgs = point.clipRectArgs,
-				seriesOpts = series.options,
-				pfOptions = point.partialFill,
-				fill,
-				state = point.selected && 'select',
-				cutOff = options.stacking && !options.borderRadius;
-
-			if (isNumber(plotY) && point.y !== null) {
-				if (graphic) { // update
-					stop(graphic);
-					point.graphicOriginal[verb](
-						merge(shapeArgs)
-					);
-					if (partShapeArgs) {
-						point.graphicOverlay[verb](
-							merge(partShapeArgs)
-						);
-						point.clipRect.animate(
-							merge(clipRectArgs)
-						);
-					}
-
-				} else {
-					point.graphic = graphic = renderer.g('point')
-						.attr({
-							'class': point.getClassName()
-						})
-						.add(point.group || series.group);
-
-					point.graphicOriginal = renderer[type](shapeArgs)
-						.addClass('highcharts-partfill-original')
-						.add(graphic);
-					if (clipRectArgs && partShapeArgs) {
-
-						point.clipRect = renderer.clipRect(
-							clipRectArgs.x,
-							clipRectArgs.y,
-							clipRectArgs.width,
-							clipRectArgs.height
-						);
-
-						point.graphicOverlay = renderer[type](partShapeArgs)
-							.addClass('highcharts-partfill-overlay')
-							.add(graphic)
-							.clip(point.clipRect);
-					}
-				}
-
-				/*= if (build.classic) { =*/
-				// Presentational
-				point.graphicOriginal
-					.attr(series.pointAttribs(point, state))
-					.shadow(options.shadow, null, cutOff);
-				if (partShapeArgs) {
-					// Ensure pfOptions is an object
-					if (!isObject(pfOptions)) {
-						pfOptions = {};
-					}
-					if (isObject(seriesOpts.partialFill)) {
-						pfOptions = merge(pfOptions, seriesOpts.partialFill);
-					}
-
-					fill = pfOptions.fill ||
-							color(series.color).brighten(-0.3).get('rgb');
-					point.graphicOverlay
-						.attr(series.pointAttribs(point, state))
-						.attr({
-							'fill': fill,
-							'stroke-width': 0.1
-						})
-						.shadow(options.shadow, null, cutOff);
-				}
-				/*= } =*/
-
-			} else if (graphic) {
-				point.graphic = graphic.destroy(); // #1269
-			}
+			series.drawPoint(point, verb);
 		});
 	}
 });
