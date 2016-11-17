49d5028bf0e9d59d1d3123dfcdf86cc4200a0029
jon-a-nygaard <jon.arild@highsoft.com>
2016-11-17 13:45:58 +0100

    Highstock: Addition to #5931, updated a unit test, and fixed small mistake in updateButtonStates.

--- js/parts/RangeSelector.js
+++ js/parts/RangeSelector.js
@@ -342,7 +342,7 @@ RangeSelector.prototype = {
 				isYTDButNotSelected = !isSelected;
 			} else if (type === 'all') {
 				isSameRange = baseAxis.max - baseAxis.min >= dataMax - dataMin;
-				isAllButAlreadyShowingAll = !selected && selectedExists && isSameRange;
+				isAllButAlreadyShowingAll = !isSelected && selectedExists && isSameRange;
 			}
 			// The new zoom area happens to match the range for a button - mark it selected.
 			// This happens when scrolling across an ordinal gap. It can be seen in the intraday
