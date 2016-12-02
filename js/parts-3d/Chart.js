ec6c4a81dc251612001a19e9e9726db954ee8041
Torstein Hønsi <torstein@highsoft.com>
2016-12-02 10:19:02 +0100

    Fixed #6018, 3D column not rendered in styled mode.

--- js/parts-3d/Chart.js
+++ js/parts-3d/Chart.js
@@ -150,20 +150,26 @@ merge(true, defaultOptions, {
 				}
 			}
 		}
-	},
-	/*= if (!build.classic) { =*/
-	defs: {
-		style: {
-			textContent: defaultOptions.defs.style.textContent +
-				'\n.highcharts-3d-top{' +
-					'filter: url(#highcharts-brighter)' +
-				'}' +
-				'\n.highcharts-3d-side{' +
-					'filter: url(#highcharts-darker)' +
-				'}'
-		}
 	}
-	/*= } =*/
+});
+
+/**
+ * Override the getContainer by adding the required CSS classes for column 
+ * sides (#6018)
+ */
+wrap(Chart.prototype, 'getContainer', function (proceed) {
+	proceed.apply(this, [].slice.call(arguments, 1));
+
+	this.renderer.definition({
+		tagName: 'style',
+		textContent: 
+			'.highcharts-3d-top{' +
+				'filter: url(#highcharts-brighter)' +
+			'}\n' +
+			'.highcharts-3d-side{' +
+				'filter: url(#highcharts-darker)' +
+			'}\n'
+	});
 });
 
 wrap(Chart.prototype, 'setClassName', function (proceed) {
--- samples/highcharts/css/column-3d/demo.js
+++ samples/highcharts/css/column-3d/demo.js
@@ -2,7 +2,6 @@ $(function () {
     Highcharts.chart('container', {
         chart: {
             type: 'column',
-            margin: 75,
             options3d: {
                 enabled: true,
                 alpha: 15,
@@ -10,13 +9,22 @@ $(function () {
                 depth: 50
             }
         },
+        title: {
+            text: 'Highcharts 3d column in styled mode'
+        },
         plotOptions: {
             column: {
                 depth: 25
             }
         },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
         series: [{
-            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6,
+                148.5, 216.4, 194.1, 95.6, 54.4],
+            colorByPoint: true
         }]
     });
 });
