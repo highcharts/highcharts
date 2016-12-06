dada0ff5704b36ad3dda4e65eb15bd8227d49a05
Torstein Hønsi <torstein@highsoft.com>
2016-12-06 16:32:01 +0100

    Regression causing JS error on hiding and showing series in the bar-basic demo.

--- js/parts/SvgRenderer.js
+++ js/parts/SvgRenderer.js
@@ -114,8 +114,10 @@ SVGElement.prototype = {
 	 * @returns {SVGElement} Returns the SVGElement for chaining.
 	 */
 	animate: function (params, options, complete) {
-		var animOptions = pick(options, this.renderer.globalAnimation, true);
-		if (animOptions) {
+		var animOptions = H.animObject(
+			pick(options, this.renderer.globalAnimation, true)
+		);
+		if (animOptions.duration) {
 			if (complete) { // allows using a callback with the global animation without overwriting it
 				animOptions.complete = complete;
 			}
