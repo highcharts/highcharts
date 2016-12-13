7d0ad333ab3ee7458255e7170d5b52e606fb0e3b
Torstein Hønsi <torstein@highsoft.com>
2016-12-13 13:48:52 +0100

    Fixed #6112, issue and regression with swapping series indexes.

--- samples/unit-tests/series/index/demo.js
+++ samples/unit-tests/series/index/demo.js
@@ -52,4 +52,108 @@ $(function () {
             'Order after adding'
         );
     });
+
+    QUnit.test('Updating series index (#6112)', function (assert) {
+        var chart = Highcharts.chart('container', {
+
+            series: [{
+                id: 'test1',
+                name: 'First',
+                data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+                type: 'column'
+            }, {
+                id: 'test2',
+                name: 'Second',
+                data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+                type: 'column'
+            }]
+
+        });
+
+        var swapped = false;
+        function swap() {
+
+            chart.get('test1').update({ index: swapped ? 0 : 1 }, false);
+            chart.get('test2').update({ index: swapped ? 1 : 0 }, false);
+
+            chart.redraw();
+            swapped = !swapped;
+        }
+
+        assert.strictEqual(
+            chart.series[0].name,
+            'First',
+            'Initial order'
+        );
+
+        assert.strictEqual(
+            chart.series[0].index,
+            0,
+            'Initial order'
+        );
+
+        assert.strictEqual(
+            chart.series[1].name,
+            'Second',
+            'Initial order'
+        );
+
+        assert.strictEqual(
+            chart.series[1].index,
+            1,
+            'Initial order'
+        );
+
+        swap();
+
+        assert.strictEqual(
+            chart.series[0].name,
+            'Second',
+            'Swapped once'
+        );
+
+        assert.strictEqual(
+            chart.series[0].index,
+            0,
+            'Swapped once'
+        );
+
+        assert.strictEqual(
+            chart.series[1].name,
+            'First',
+            'Swapped once'
+        );
+
+        assert.strictEqual(
+            chart.series[1].index,
+            1,
+            'Swapped once'
+        );
+
+        swap();
+
+        assert.strictEqual(
+            chart.series[0].name,
+            'First',
+            'Swapped twice'
+        );
+
+        assert.strictEqual(
+            chart.series[0].index,
+            0,
+            'Swapped twice'
+        );
+
+        assert.strictEqual(
+            chart.series[1].name,
+            'Second',
+            'Swapped twice'
+        );
+
+        assert.strictEqual(
+            chart.series[1].index,
+            1,
+            'Swapped twice'
+        );
+    });
 });
