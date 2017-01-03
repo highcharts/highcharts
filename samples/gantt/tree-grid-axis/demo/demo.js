96aad44762498ebd108f303770822dff1a7ac4df
jon-a-nygaard <jon.arild@highsoft.com>
2017-01-03 15:03:40 +0100

    Adding demo of tree-grid to samples.

---
+++ samples/gantt/tree-grid-axis/demo/demo.js
@@ -0,0 +1,49 @@
+$(function () {
+    // THE CHART
+    Highcharts.chart('container', {
+        chart: {
+            type: 'scatter',
+            marginLeft: 150,
+            marginRight: 150
+        },
+        title: {
+            text: 'Highcharts GridAxis'
+        },
+        xAxis: [{
+            type: 'datetime'
+        }],
+        yAxis: [{
+            title: '',
+            grid: true,
+            type: 'tree-grid',
+            tree: [{
+                id: '1',
+                text: 'Node 1'
+            }, {
+                id: '2',
+                parent: '1',
+                text: 'Node 2'
+            }, {
+                id: '3',
+                parent: '2',
+                text: 'Node 3'
+            }]
+        }],
+        series: [{
+            name: 'Project 1',
+            data: [{
+                x: Date.UTC(2014, 10, 18),
+                y: 0
+            }, {
+                x: Date.UTC(2014, 10, 20),
+                y: 1
+            }, {
+                x: Date.UTC(2014, 10, 26),
+                y: 0
+            }, {
+                x: Date.UTC(2014, 10, 23),
+                y: 2
+            }]
+        }]
+    });
+});
