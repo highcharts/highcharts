f36023039fc97033cbc4716bb5ca58ba6be55ba4
Torstein Hønsi <torstein@highsoft.com>
2017-01-04 15:54:36 +0100

    Fixed #6196, dead clip path references.

--- js/parts/Series.js
+++ js/parts/Series.js
@@ -1073,6 +1073,7 @@ H.Series = H.seriesType('line', null, { // base series options
 					chart[sharedClipKey] = chart[sharedClipKey].destroy();
 				}
 				if (chart[sharedClipKey + 'm']) {
+					this.markerGroup.clip();
 					chart[sharedClipKey + 'm'] = chart[sharedClipKey + 'm'].destroy();
 				}
 			}
