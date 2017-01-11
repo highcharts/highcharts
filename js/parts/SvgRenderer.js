highcharts/highcharts

commit 96a849421fbad4a401bb50e5c135a23ed1b9920e

Added support for marker.symbol setting on bubble charts.
- Refactored to inherit drawPoints from general series instead of column series.
- Required better implementation of symbols.circle.

js/parts/SvgRenderer.js
@@ -2676,7 +2676,7 @@ SVGRenderer.prototype = {
 			symbolFn = this.symbols[symbol],
 
 			// check if there's a path defined for this symbol
-			path = defined(x) && symbolFn && symbolFn(
+			path = defined(x) && symbolFn && this.symbols[symbol](
 				Math.round(x),
 				Math.round(y),
 				width,
@@ -2837,13 +2837,13 @@ SVGRenderer.prototype = {
 	 */
 	symbols: {
 		'circle': function (x, y, w, h) {
-			var cpw = 0.166 * w;
-			return [
-				'M', x + w / 2, y,
-				'C', x + w + cpw, y, x + w + cpw, y + h, x + w / 2, y + h,
-				'C', x - cpw, y + h, x - cpw, y, x + w / 2, y,
-				'Z'
-			];
+			var r = w / 2;
+			// Return a full arc
+			return this.arc(x + r, y + r, r, h / 2, {
+				start: 0,
+				end: Math.PI * 2,
+				open: false
+			});
 		},
 
 		'square': function (x, y, w, h) {
@@ -2884,7 +2884,8 @@ SVGRenderer.prototype = {
 		},
 		'arc': function (x, y, w, h, options) {
 			var start = options.start,
-				radius = options.r || w || h,
+				rx = options.r || w,
+				ry = options.r || h,
 				end = options.end - 0.001, // to prevent cos and sin of start and end from becoming equal on 360 arcs (related: #1561)
 				innerRadius = options.innerR,
 				open = options.open,
@@ -2892,34 +2893,41 @@ SVGRenderer.prototype = {
 				sinStart = Math.sin(start),
 				cosEnd = Math.cos(end),
 				sinEnd = Math.sin(end),
-				longArc = options.end - start < Math.PI ? 0 : 1;
+				longArc = options.end - start < Math.PI ? 0 : 1,
+				arc;
 
-			return [
+			arc = [
 				'M',
-				x + radius * cosStart,
-				y + radius * sinStart,
+				x + rx * cosStart,
+				y + ry * sinStart,
 				'A', // arcTo
-				radius, // x radius
-				radius, // y radius
+				rx, // x radius
+				ry, // y radius
 				0, // slanting
 				longArc, // long or short arc
 				1, // clockwise
-				x + radius * cosEnd,
-				y + radius * sinEnd,
-				open ? 'M' : 'L',
-				x + innerRadius * cosEnd,
-				y + innerRadius * sinEnd,
-				'A', // arcTo
-				innerRadius, // x radius
-				innerRadius, // y radius
-				0, // slanting
-				longArc, // long or short arc
-				0, // clockwise
-				x + innerRadius * cosStart,
-				y + innerRadius * sinStart,
-
-				open ? '' : 'Z' // close
+				x + rx * cosEnd,
+				y + ry * sinEnd
 			];
+
+			if (defined(innerRadius)) {
+				arc.push(
+					open ? 'M' : 'L',
+					x + innerRadius * cosEnd,
+					y + innerRadius * sinEnd,
+					'A', // arcTo
+					innerRadius, // x radius
+					innerRadius, // y radius
+					0, // slanting
+					longArc, // long or short arc
+					0, // clockwise
+					x + innerRadius * cosStart,
+					y + innerRadius * sinStart
+				);
+			}
+
+			arc.push(open ? '' : 'Z'); // close
+			return arc;
 		},
 
 		/**

samples/issues/highcharts-4.1.7/4396-bubble-series-min-max-size/demo.js
@@ -22,7 +22,7 @@ $(function () {
 
         $.each(chart.series, function (i, s) {
             $.each(s.points, function (j, p) {
-                radius = parseFloat(p.graphic.attr("r")) * 2;
+                radius = p.graphic.width;
                 assert.strictEqual(
                     radius <= s.options.maxSize && radius >= s.options.minSize,
                     true,

samples/issues/highcharts-4.1.8/4543-null-bubble/demo.js
@@ -23,7 +23,7 @@ $(function () {
 
 
         assert.strictEqual(
-            chart.series[0].group.element.childNodes.length,
+            chart.series[0].markerGroup.element.childNodes.length,
             3,
             'No element created for null point'
         );
@@ -32,7 +32,7 @@ $(function () {
         chart.series[0].update({ sizeByAbsoluteValue: true });
 
         assert.strictEqual(
-            chart.series[0].group.element.childNodes.length,
+            chart.series[0].markerGroup.element.childNodes.length,
             3,
             'No element created for null point'
         );

samples/issues/highcharts-4.2.1/1977-bubble-series-shadow/demo.js
@@ -1,5 +1,6 @@
 
 $(function () {
+    /* Shadows not applied on marker elements
     QUnit.test('Shadow follows bubble', function (assert) {
         var chart = Highcharts.chart('container', {
 
@@ -38,8 +39,8 @@ $(function () {
             shadow = point.graphic.shadows[0];
 
         assert.strictEqual(
-            graphic.getAttribute('cx'),
-            shadow.getAttribute('cx'),
+            graphic.getAttribute('d'),
+            shadow.getAttribute('d'),
             'Initial cx'
         );
         assert.strictEqual(
@@ -100,4 +101,5 @@ $(function () {
         assert.ok(true, 'No JS error thrown');
 
     });
+    */
 });

commit 4aaa16f68d22cfa05b1446a423b4d310ac8ea538

Regression since 96a8494. Polar columns failed.

js/parts/SvgRenderer.js
@@ -2935,7 +2935,7 @@ SVGRenderer.prototype = {
 		'arc': function (x, y, w, h, options) {
 			var start = options.start,
 				rx = options.r || w,
-				ry = options.r || h,
+				ry = options.r || h || w,
 				end = options.end - 0.001, // to prevent cos and sin of start and end from becoming equal on 360 arcs (related: #1561)
 				innerRadius = options.innerR,
 				open = options.open,