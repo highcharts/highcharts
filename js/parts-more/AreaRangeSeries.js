highcharts/highcharts

commit 5c295577bb85fbb188a07f1b60e16072039225d7

Fixed #6184, polar arearange was misshaped when connectEnds was not set.

js/parts-more/AreaRangeSeries.js
@@ -125,6 +125,7 @@ seriesType('arearange', 'area', {
 			linePath,
 			lowerPath,
 			options = this.options,
+			connectEnds = options.connectEnds !== false,
 			step = options.step,
 			higherPath,
 			higherAreaPath;
@@ -139,7 +140,11 @@ seriesType('arearange', 'area', {
 		while (i--) {
 			point = points[i];
 		
-			if (!point.isNull && !options.connectEnds && (!points[i + 1] || points[i + 1].isNull)) {
+			if (
+				!point.isNull &&
+				!connectEnds &&
+				(!points[i + 1] || points[i + 1].isNull)
+			) {
 				highAreaPoints.push({
 					plotX: point.plotX,
 					plotY: point.plotY,
@@ -157,7 +162,12 @@ seriesType('arearange', 'area', {
 			};
 			highAreaPoints.push(pointShim);
 			highPoints.push(pointShim);
-			if (!point.isNull && !options.connectEnds && (!points[i - 1] || points[i - 1].isNull)) {
+			
+			if (
+				!point.isNull &&
+				!connectEnds &&
+				(!points[i - 1] || points[i - 1].isNull)
+			) {
 				highAreaPoints.push({
 					plotX: point.plotX,
 					plotY: point.plotY,

samples/issues/highcharts-4.0.4/3419-polar-arearange/demo.js
@@ -16,7 +16,6 @@ $(function () {
         },
 
         series: [{
-            connectEnds: true,
             data: [
                 [1, 2],
                 [1, 2],

commit ef2c11cdc073b7775d7b372b204c4f1f2baaaa6d

Fixed regression since 5c29557.

js/parts-more/AreaRangeSeries.js
@@ -125,7 +125,7 @@ seriesType('arearange', 'area', {
 			linePath,
 			lowerPath,
 			options = this.options,
-			connectEnds = options.connectEnds !== false,
+			connectEnds = this.chart.polar && options.connectEnds !== false,
 			step = options.step,
 			higherPath,
 			higherAreaPath;