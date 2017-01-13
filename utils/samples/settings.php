af71ec79f861446cfc4ae2a12d9ab77eb13fd80f
Torstein Hønsi <torstein@highsoft.com>
2017-01-13 11:15:33 +0100

    Utils: Settings GUI.

--- utils/samples/settings.php
+++ utils/samples/settings.php
@@ -13,13 +13,18 @@ if (isset($_POST) && isset($_POST['leftPath'])) {
 	$_SESSION['leftPath'] = $_POST['leftPath'];
 }
 
+if (@$_SESSION['leftPath'] === '') {
+	unset($_SESSION['leftPath']);
+}
+
 ?><html>
 
 	<head>
 	<link rel="stylesheet" type="text/css" href="style.css"/>
 	<style type="text/css">
-	body {
-		margin: 2em;
+	.main {
+		max-width: 600px;
+		margin: 2em auto;
 	}
 	input[type="text"] {
 		min-width: 300px;
@@ -37,18 +42,18 @@ if (isset($_POST) && isset($_POST['leftPath'])) {
 	</head>
 
 	<body>
-		
 		<form action="" method="POST">
 
-		<div>
+		<div class="main">
+			<h1>Sample runner settings</h1>
 			<label for="leftPath">Compare against (leftPath). Defaults to 
-			<code>http://code.highcharts.com</code>. Use <code>master</code> to compare to latest
+			<code>http://code.highcharts.com</code> when left empty. Use <code>master</code> (or other branch name) to compare to latest
 			commit.</label>
-			<input type="text" name="leftPath" value="<?php echo @$_SESSION['leftPath'] ?>" />
-		</div>
+			<input type="text" name="leftPath" value="<?php echo @$_SESSION['leftPath'] ?>" />			
 
-		<div>
-			<input type="submit" value="Save">
+			<div>
+				<input type="submit" value="Save">
+			</div>
 		</div>
 		</form>
 
