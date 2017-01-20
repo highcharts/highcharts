ceafc0c879dc1ae1cdb564e19571d42410d0e22c
cvasseng <cvasseng@gmail.com>
2017-01-20 11:12:14 +0100

    Added exception handling on texture creation

--- js/modules/boost.src.js
+++ js/modules/boost.src.js
@@ -1619,23 +1619,28 @@ function GLRenderer(options) {
 		// Set up the circle texture used for bubbles
 		circleTextureHandle = gl.createTexture();
 		
-		if (circleTextureHandle && typeof circleTexture !== 'undefined') {			
-			gl.bindTexture(gl.TEXTURE_2D, circleTextureHandle);
+		if (circleTextureHandle && typeof circleTexture !== 'undefined') {
+			try {
 
-			gl.texImage2D(
-				gl.TEXTURE_2D,
-				0,
-				gl.RGBA,
-				gl.RGBA,
-				gl.UNSIGNED_BYTE,
-				circleTexture
-			);
-			
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-			gl.generateMipmap(gl.TEXTURE_2D);
+				gl.bindTexture(gl.TEXTURE_2D, circleTextureHandle);
 
-			gl.bindTexture(gl.TEXTURE_2D, null);
+				gl.texImage2D(
+					gl.TEXTURE_2D,
+					0,
+					gl.RGBA,
+					gl.RGBA,
+					gl.UNSIGNED_BYTE,
+					circleTexture
+				);
+				
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+				gl.generateMipmap(gl.TEXTURE_2D);
+
+				gl.bindTexture(gl.TEXTURE_2D, null);
+			} catch (e) {
+				console.log(e);
+			}		
 		}
 
 		isInited = true;
