320debef6bdb386f6a27933cefe632aa22ebd145
cvasseng <cvasseng@gmail.com>
2017-01-20 11:22:31 +0100

    Added mag/min filters for circle texture

--- js/modules/boost.src.js
+++ js/modules/boost.src.js
@@ -1636,6 +1636,8 @@ function GLRenderer(options) {
 				
 				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
 				gl.generateMipmap(gl.TEXTURE_2D);
 
 				gl.bindTexture(gl.TEXTURE_2D, null);
