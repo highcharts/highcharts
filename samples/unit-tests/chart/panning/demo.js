569746d59fec71b29d91da48280e44d1ed59dc25
Torstein Hønsi <torstein@highsoft.com>
2017-01-23 13:42:33 +0100

    Samples: Moved test for panning.

--- samples/unit-tests/chart/panning/demo.js
+++ samples/unit-tests/chart/panning/demo.js
@@ -1,7 +1,30 @@
 /* global TestController */
 QUnit.test('Zoom and pan key', function (assert) {
 
-    var chart = Highcharts.charts[0],
+    var chart = Highcharts.chart('container', {
+            chart: {
+                type: 'line',
+                zoomType: 'x',
+                panning: true,
+                panKey: 'shift'
+            },
+
+            title: {
+                text: 'Zooming and panning'
+            },
+
+            subtitle: {
+                text: 'Click and drag to zoom in. Hold down shift key to pan.'
+            },
+
+            xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+
+            series: [{
+                data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+            }]
+        }),
         firstZoom = {};
 
     var test = TestController(chart);
@@ -54,8 +77,5 @@ QUnit.test('Zoom and pan key', function (assert) {
         firstZoom.max - firstZoom.min,
         'Has preserved range'
     );
-
-
-
 
 });
