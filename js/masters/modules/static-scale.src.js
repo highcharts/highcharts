edd1b914d0e49a23de7a6c5d255068ba2caa1478
Lars Alberto Vangsnes Cabrera <lars@highsoft.com>
2017-02-06 16:51:21 +0100

    Added static scale as a module.

---
+++ js/masters/modules/static-scale.src.js
@@ -0,0 +1,12 @@
+/**
+ * @license @product.name@ JS v@product.version@ (@product.date@)
+ * StaticScale
+ *
+ * (c) 2016 Torstein Honsi, Lars A. V. Cabrera
+ *
+ * --- WORK IN PROGRESS ---
+ *
+ * License: www.highcharts.com/license
+ */
+'use strict';
+import '../../modules/static-scale.src.js';
