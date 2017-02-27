4af3f0c71427f9e56baed4bb39b40fae4c7dad98
Lars Alberto Vangsnes Cabrera <lars@highsoft.com>
2017-02-27 13:43:33 +0100

    Gantt: Removed JQuery.onReady() from all gantt-related demos.

--- samples/gantt/pathfinder/auto-marker-placement/demo.js
+++ samples/gantt/pathfinder/auto-marker-placement/demo.js
@@ -1,49 +1,47 @@
-$(function () {
-    Highcharts.chart('container', {
-        series: [{
-            pathfinder: {
-                marker: {
-                    enabled: true
-                }
-            },
-            data: [{
-                x: 10,
-                y: 10,
-                id: 'center'
-            }, {
-                x: 10,
-                y: 5,
-                connect: 'center'
-            }, {
-                x: 15,
-                y: 5,
-                connect: 'center'
-            }, {
-                x: 15,
-                y: 10,
-                connect: 'center'
-            }, {
-                x: 15,
-                y: 15,
-                connect: 'center'
-            }, {
-                x: 10,
-                y: 15,
-                connect: 'center'
-            }, {
-                x: 5,
-                y: 15,
-                connect: 'center'
-            }, {
-                x: 5,
-                y: 10,
-                connect: 'center'
-            }, {
-                x: 5,
-                y: 5,
-                connect: 'center'
-            }],
-            type: 'scatter'
-        }]
-    });
+Highcharts.chart('container', {
+    series: [{
+        pathfinder: {
+            marker: {
+                enabled: true
+            }
+        },
+        data: [{
+            x: 10,
+            y: 10,
+            id: 'center'
+        }, {
+            x: 10,
+            y: 5,
+            connect: 'center'
+        }, {
+            x: 15,
+            y: 5,
+            connect: 'center'
+        }, {
+            x: 15,
+            y: 10,
+            connect: 'center'
+        }, {
+            x: 15,
+            y: 15,
+            connect: 'center'
+        }, {
+            x: 10,
+            y: 15,
+            connect: 'center'
+        }, {
+            x: 5,
+            y: 15,
+            connect: 'center'
+        }, {
+            x: 5,
+            y: 10,
+            connect: 'center'
+        }, {
+            x: 5,
+            y: 5,
+            connect: 'center'
+        }],
+        type: 'scatter'
+    }]
 });
