851c1fe4b4b0cd61fad27606dec07dd3c95a46a1
Torstein Hønsi <torstein@highsoft.com>
2017-03-03 10:34:35 +0100

    Samples. Updated heatmap-canvas to use new Boost module.

--- samples/highcharts/demo/heatmap-canvas/demo.js
+++ samples/highcharts/demo/heatmap-canvas/demo.js
@@ -1,86 +1,7 @@
-
-
-/**
- * This plugin extends Highcharts in two ways:
- * - Use HTML5 canvas instead of SVG for rendering of the heatmap squares. Canvas
- *   outperforms SVG when it comes to thousands of single shapes.
- * - Add a K-D-tree to find the nearest point on mouse move. Since we no longer have SVG shapes
- *   to capture mouseovers, we need another way of detecting hover points for the tooltip.
- */
-(function (H) {
-    var Series = H.Series,
-        each = H.each;
-
-    /**
-     * Create a hidden canvas to draw the graph on. The contents is later copied over
-     * to an SVG image element.
-     */
-    Series.prototype.getContext = function () {
-        if (!this.canvas) {
-            this.canvas = document.createElement('canvas');
-            this.canvas.setAttribute('width', this.chart.chartWidth);
-            this.canvas.setAttribute('height', this.chart.chartHeight);
-            this.image = this.chart.renderer.image('', 0, 0, this.chart.chartWidth, this.chart.chartHeight).add(this.group);
-            this.ctx = this.canvas.getContext('2d');
-        }
-        return this.ctx;
-    };
-
-    /**
-     * Draw the canvas image inside an SVG image
-     */
-    Series.prototype.canvasToSVG = function () {
-        this.image.attr({ href: this.canvas.toDataURL('image/png') });
-    };
-
-    /**
-     * Wrap the drawPoints method to draw the points in canvas instead of the slower SVG,
-     * that requires one shape each point.
-     */
-    H.wrap(H.seriesTypes.heatmap.prototype, 'drawPoints', function () {
-
-        var ctx = this.getContext();
-
-        if (ctx) {
-
-            // draw the columns
-            each(this.points, function (point) {
-                var plotY = point.plotY,
-                    shapeArgs,
-                    pointAttr;
-
-                if (plotY !== undefined && !isNaN(plotY) && point.y !== null) {
-                    shapeArgs = point.shapeArgs;
-
-                    pointAttr = (point.pointAttr && point.pointAttr['']) || point.series.pointAttribs(point);
-
-                    ctx.fillStyle = pointAttr.fill;
-                    ctx.fillRect(shapeArgs.x, shapeArgs.y, shapeArgs.width, shapeArgs.height);
-                }
-            });
-
-            this.canvasToSVG();
-
-        } else {
-            this.chart.showLoading('Your browser doesn\'t support HTML5 canvas, <br>please use a modern browser');
-
-            // Uncomment this to provide low-level (slow) support in oldIE. It will cause script errors on
-            // charts with more than a few thousand points.
-            // arguments[0].call(this);
-        }
-    });
-    H.seriesTypes.heatmap.prototype.directTouch = false; // Use k-d-tree
-}(Highcharts));
-
-
-var start;
 Highcharts.chart('container', {
 
     data: {
-        csv: document.getElementById('csv').innerHTML,
-        parsed: function () {
-            start = +new Date();
-        }
+        csv: document.getElementById('csv').innerHTML
     },
 
     chart: {
@@ -88,9 +9,12 @@ Highcharts.chart('container', {
         margin: [60, 10, 80, 50]
     },
 
+    boost: {
+        useGPUTranslations: true
+    },
 
     title: {
-        text: 'Highcharts extended heat map',
+        text: 'Highcharts heat map',
         align: 'left',
         x: 40
     },
@@ -150,6 +74,7 @@ Highcharts.chart('container', {
     },
 
     series: [{
+        boostThreshold: 100,
         borderWidth: 0,
         nullColor: '#EFEFEF',
         colsize: 24 * 36e5, // one day
@@ -161,4 +86,3 @@ Highcharts.chart('container', {
     }]
 
 });
-console.log('Rendered in ' + (new Date() - start) + ' ms'); // eslint-disable-line no-console
