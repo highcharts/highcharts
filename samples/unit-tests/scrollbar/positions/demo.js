2899b8aba7e530c378bb71d74a7ef9966889d710
Paweł Fus <pawel.fus.88@gmail.com>
2017-03-13 16:31:01 +0100

    Highstock: Fixed #6453, scrollbars for multiple yAxes were positioned incorrectly.

--- samples/unit-tests/scrollbar/positions/demo.js
+++ samples/unit-tests/scrollbar/positions/demo.js
@@ -30,3 +30,39 @@ QUnit.test('#6262 - inverted chart: wrong position for chart.scrollbar without n
         'The same position for xAxis.scrollbar and chart.scrollbar'
     );
 });
+
+QUnit.test('#6453 - multiple scrollbars for yAxes on the left side.', function (assert) {
+    var chart = Highcharts.chart('container', {
+            yAxis: [{
+                scrollbar: {
+                    enabled: true
+                }
+            }, {
+                scrollbar: {
+                    enabled: true
+                }
+            }, {
+                scrollbar: {
+                    enabled: true
+                }
+            }],
+            series: [{
+                data: [4, 20, 100, 5, 2, 33, 12, 23]
+            }, {
+                data: [4, 20, 100, 5, 2, 33, 12, 23],
+                yAxis: 1
+            }, {
+                data: [4, 20, 100, 5, 2, 33, 12, 23],
+                yAxis: 2
+            }]
+        });
+
+    Highcharts.each(chart.yAxis, function (axis, index) {
+        assert.strictEqual(
+            axis.scrollbar.x > chart.plotLeft + chart.plotWidth,
+            true,
+            'Axis ' + index + ' outside the plotting area'
+        );
+    })
+
+});
