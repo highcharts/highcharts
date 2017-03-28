8cc19d9e3a6bccf6853f3ce42cab40daf8de1517
Torstein Hønsi <torstein@highsoft.com>
2017-03-28 15:25:36 +0200

    Samples: Updated pane tests after refactoring panes.

--- samples/unit-tests/pane/pane-update/demo.js
+++ samples/unit-tests/pane/pane-update/demo.js
@@ -313,3 +313,119 @@ QUnit.test('Pane responsiveness', function (assert) {
         'Green background respected'
     );
 });
+
+
+QUnit.test(
+    'Pane backgrounds, plot bands and chart updating (#3176)',
+    function (assert) {
+        var chart, options;
+
+        function getOptions() {
+
+            return {
+
+                chart: {
+                    type: 'gauge'
+                },
+
+                pane: {
+                    startAngle: -150,
+                    endAngle: 150,
+                    background: [{
+                        backgroundColor: {
+                            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                            stops: [
+                                [0, 'rgba(255, 255, 255, 0.5)'],
+                                [1, 'rgba(96, 96, 96, 0.5)']
+                            ]
+                        },
+                        borderWidth: 0,
+                        outerRadius: '109%'
+                    }, {
+                        backgroundColor: {
+                            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                            stops: [
+                                [0, 'rgba(255, 255, 255, 0.5)'],
+                                [1, 'rgba(96, 96, 96, 0.5)']
+                            ]
+                        },
+                        borderWidth: 1,
+                        outerRadius: '107%'
+                    }, {
+                        // default background
+                    }, {
+                        backgroundColor: 'rgba(192, 192, 192, 0.5)',
+                        borderWidth: 0,
+                        outerRadius: '105%',
+                        innerRadius: '103%'
+                    }]
+                },
+
+                // the value axis
+                yAxis: [{
+                    min: 0,
+                    max: 200
+
+                }],
+
+                series: [{
+                    data: [80]
+                }]
+
+            };
+        }
+
+        chart = $('#container').highcharts(getOptions()).highcharts();
+
+        assert.strictEqual(
+            chart.pane[0].background.length,
+            4,
+            "No bands, four backgrounds initially"
+        );
+
+        // Run update
+        chart.yAxis[0].update({
+            min: 0,
+            max: 400
+        });
+
+        assert.strictEqual(
+            chart.pane[0].background.length,
+            4,
+            "No bands, four backgrounds after update"
+        );
+
+
+        // Add an initial plot band
+        options = getOptions();
+        options.yAxis[0].plotBands = [{
+            from: 0,
+            to: 80,
+            color: 'green'
+        }];
+        chart = $('#container').highcharts(options).highcharts();
+
+        assert.strictEqual(
+            chart.yAxis[0].plotLinesAndBands.length,
+            1,
+            "One band, four backgrounds initially"
+        );
+        assert.strictEqual(
+            chart.pane[0].background.length,
+            4,
+            "One band, four backgrounds initially"
+        );
+
+        // Run update
+        assert.strictEqual(
+            chart.yAxis[0].plotLinesAndBands.length,
+            1,
+            "One band, four backgrounds initially"
+        );
+        assert.strictEqual(
+            chart.pane[0].background.length,
+            4,
+            "One band, four backgrounds initially"
+        );
+    }
+);
