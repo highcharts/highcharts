7667a79725d85c0323dd2254834d65763055c9c5
Kacper Madej <kacper@blacklabel.pl>
2017-04-14 14:10:19 +0200

    Highstock: Imporoved perormance for data grouping, prepare for fixing #5479.

--- js/parts/DataGrouping.js
+++ js/parts/DataGrouping.js
@@ -188,8 +188,8 @@ var seriesProto = Series.prototype,
 
 
 /**
- * Takes parallel arrays of x and y data and groups the data into intervals defined by groupPositions, a collection
- * of starting x values for each group.
+ * Takes parallel arrays of x and y data and groups the data into intervals 
+ * defined by groupPositions, a collection of starting x values for each group.
  */
 seriesProto.groupData = function (xData, yData, groupPositions, approximation) {
 	var series = this,
@@ -202,14 +202,29 @@ seriesProto.groupData = function (xData, yData, groupPositions, approximation) {
 		pointX,
 		pointY,
 		groupedY,
-		handleYData = !!yData, // when grouping the fake extended axis for panning, we don't need to consider y
-		values = [[], [], [], []],
-		approximationFn = typeof approximation === 'function' ? approximation : approximations[approximation],
+		// when grouping the fake extended axis for panning,
+		// we don't need to consider y
+		handleYData = !!yData,
+		values = [],
+		approximationFn = typeof approximation === 'function' ?
+			approximation :
+			approximations[approximation],
 		pointArrayMap = series.pointArrayMap,
 		pointArrayMapLength = pointArrayMap && pointArrayMap.length,
-		i,
 		pos = 0,
-		start = 0;
+		start = 0,
+		valuesLen,
+		i, j;
+
+	// Calculate values array size from pointArrayMap length
+	if (pointArrayMapLength) {
+		each(pointArrayMap, function () {
+			values.push([]);
+		});
+	} else {
+		values.push([]);
+	}
+	valuesLen = pointArrayMapLength || 1;
 
 	// Start with the first point within the X axis range (#2696)
 	for (i = 0; i <= dataLength; i++) {
@@ -220,9 +235,12 @@ seriesProto.groupData = function (xData, yData, groupPositions, approximation) {
 
 	for (i; i <= dataLength; i++) {
 
-		// when a new group is entered, summarize and initiate the previous group
-		while ((groupPositions[pos + 1] !== undefined && xData[i] >= groupPositions[pos + 1]) ||
-				i === dataLength) { // get the last group
+		// when a new group is entered, summarize and initiate 
+		// the previous group
+		while ((
+					groupPositions[pos + 1] !== undefined &&
+					xData[i] >= groupPositions[pos + 1]
+				) || i === dataLength) { // get the last group
 
 			// get group x and y
 			pointX = groupPositions[pos];
@@ -238,10 +256,10 @@ seriesProto.groupData = function (xData, yData, groupPositions, approximation) {
 
 			// reset the aggregate arrays
 			start = i;
-			values[0] = [];
-			values[1] = [];
-			values[2] = [];
-			values[3] = [];
+			for (j = 0; j < valuesLen; j++) {
+				values[j].length = 0; // faster than values[j] = []
+				values[j].hasNulls = false;
+			}
 
 			// Advance on the group positions
 			pos += 1;
@@ -257,12 +275,15 @@ seriesProto.groupData = function (xData, yData, groupPositions, approximation) {
 			break;
 		}
 
-		// for each raw data point, push it to an array that contains all values for this specific group
+		// for each raw data point, push it to an array that contains all values
+		// for this specific group
 		if (pointArrayMap) {
 
 			var index = series.cropStart + i,
-				point = (data && data[index]) || series.pointClass.prototype.applyOptions.apply({ series: series }, [dataOptions[index]]),
-				j,
+				point = (data && data[index]) ||
+					series.pointClass.prototype.applyOptions.apply({
+						series: series
+					}, [dataOptions[index]]),
 				val;
 
 			for (j = 0; j < pointArrayMapLength; j++) {
