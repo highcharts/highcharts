87058ee32a4f4565eff510ad6d10c9fed8e53970
Torstein Hønsi <torstein@highsoft.com>
2017-04-18 13:20:59 +0200

    Formatting

--- js/parts/Dynamics.js
+++ js/parts/Dynamics.js
@@ -291,8 +291,14 @@ extend(Chart.prototype, /** @lends Highcharts.Chart.prototype */ {
 		// update the first series in the chart. Setting two series without
 		// an id will update the first and the second respectively (#6019)
 		// chart.update and responsive.
-		each(['xAxis', 'yAxis', 'zAxis',
-			'series', 'colorAxis', 'pane'], function (coll) {
+		each([
+			'xAxis',
+			'yAxis',
+			'zAxis',
+			'series',
+			'colorAxis',
+			'pane'
+		], function (coll) {
 			if (options[coll]) {
 				each(splat(options[coll]), function (newOptions, i) {
 					var item = (
