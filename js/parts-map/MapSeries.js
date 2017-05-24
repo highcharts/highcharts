a48e6b44a097b6f87cea300680573f1fe5de730c
Torstein Hønsi <torstein@highsoft.com>
2017-05-24 10:59:53 +0200

    Highmaps: Fixed #5319, wrong label position for Netherlands in official map.
    # Conflicts:
    #	js/highmaps.src.js
    #	js/modules/map.src.js

--- js/parts-map/MapSeries.js
+++ js/parts-map/MapSeries.js
@@ -137,10 +137,8 @@ seriesType('map', 'scatter', {
 						}
 					}
 					// Cache point bounding box for use to position data labels, bubbles etc
-					point._midX = pointMinX + (pointMaxX - pointMinX) *
-						(point.middleX || (properties && properties['hc-middle-x']) || 0.5); // pick is slower and very marginally needed
-					point._midY = pointMinY + (pointMaxY - pointMinY) *
-						(point.middleY || (properties && properties['hc-middle-y']) || 0.5);
+					point._midX = pointMinX + (pointMaxX - pointMinX) * pick(point.middleX, properties['hc-middle-x'], 0.5);
+					point._midY = pointMinY + (pointMaxY - pointMinY) * pick(point.middleY, properties['hc-middle-y'], 0.5);
 					point._maxX = pointMaxX;
 					point._minX = pointMinX;
 					point._maxY = pointMaxY;
