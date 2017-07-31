4052945f5f40f567b92893bb7fd0d5edbd017721
jon-a-nygaard <jon.arild@highsoft.com>
2017-07-31 12:40:13 +0200

    Wordcloud: updated demo with a larger data set.

--- samples/highcharts/studies/wordcloud/demo.js
+++ samples/highcharts/studies/wordcloud/demo.js
@@ -1,20 +1,40 @@
 (function (H) {
+    var text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum erat ac justo sollicitudin, quis lacinia ligula fringilla. Pellentesque hendrerit, nisi vitae posuere condimentum, lectus urna accumsan libero, rutrum commodo mi lacus pretium erat. Phasellus pretium ultrices mi sed semper. Praesent ut tristique magna. Donec nisl tellus, sagittis ut tempus sit amet, consectetur eget erat. Sed ornare gravida lacinia. Curabitur iaculis metus purus, eget pretium est laoreet ut. Quisque tristique augue ac eros malesuada, vitae facilisis mauris sollicitudin. Mauris ac molestie nulla, vitae facilisis quam. Curabitur placerat ornare sem, in mattis purus posuere eget. Praesent non condimentum odio. Nunc aliquet, odio nec auctor congue, sapien justo dictum massa, nec fermentum massa sapien non tellus. Praesent luctus eros et nunc pretium hendrerit. In consequat et eros nec interdum. Ut neque dui, maximus id elit ac, consequat pretium tellus. Nullam vel accumsan lorem.';
+    var data = text
+        .split(',').join('') // remove commas
+        .split('.').join('') // remove periods
+        .split(' ')
+        .reduce(function (arr, word) {
+            var obj = arr.find(function (obj) {
+                return obj.name === word;
+            });
+            if (obj) {
+                obj.y += 1;
+            } else {
+                obj = {
+                    name: word,
+                    y: 1
+                };
+                arr.push(obj);
+            }
+            return arr;
+        }, []);
+    console.time('render');
     H.chart('container', {
+        chart: {
+            height: 1000,
+            margin: [0, 0, 0, 0],
+            plotBorderWidth: 1,
+            width: 1000
+        },
+        credits: {
+            enabled: false
+        },
         series: [{
             type: 'wordcloud',
-            data: [{
-                name: "Words",
-                y: 10
-            }, {
-                name: "dont",
-                y: 5
-            }, {
-                name: "come",
-                y: 3
-            }, {
-                name: "easy",
-                y: 1
-            }]
-        }]
+            data: data
+        }],
+        title: null
     });
+    console.timeEnd('render');
 }(Highcharts));
