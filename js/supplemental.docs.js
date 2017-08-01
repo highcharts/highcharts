b10dcea7f838e6f13f15c4c26865adb0b21bb6cb
Torstein Hønsi <torstein@highsoft.com>
2017-08-01 13:45:35 +0200

    JSDoc: Addition to previous.

--- js/supplemental.docs.js
+++ js/supplemental.docs.js
@@ -1137,185 +1137,18 @@
  * @apioption responsive.rules.condition.minWidth
  */
 
-/**
- * This method is deprecated as of version 2.0\. Instead, use options
- * preprocessing as described in [the docs](http://docs.highcharts.com/#preprocessing).
- * 
- * @type {Function}
- * @deprecated
- * @product highcharts
- * @apioption series.dataParser
- */
 
-/**
- * This method is deprecated as of version 2.0\. Instead, load the data
- * using jQuery.ajax and use options preprocessing as described in [the
- * docs](http://docs.highcharts.com/#preprocessing).
- * 
- * @type {String}
- * @deprecated
- * @product highcharts
- * @apioption series.dataURL
- */
 
-/**
- * An id for the series. This can be used after render time to get a
- * pointer to the series object through `chart.get()`.
- * 
- * @type {String}
- * @sample {highcharts} highcharts/plotoptions/series-id/ Get series by id
- * @since 1.2.0
- * @apioption series.id
- */
 
-/**
- * The index of the series in the chart, affecting the internal index
- * in the `chart.series` array, the visible Z index as well as the order
- * in the legend.
- * 
- * @type {Number}
- * @default undefined
- * @since 2.3.0
- * @apioption series.index
- */
 
-/**
- * The sequential index of the series in the legend.
- * 
- * @sample {highcharts|highstock} highcharts/series/legendindex/
- *         Legend in opposite order
- * @type {Number}
- * @see [legend.reversed](#legend.reversed), [yAxis.reversedStacks](#yAxis.
- * reversedStacks)
- * @apioption series.legendIndex
- */
 
-/**
- * The name of the series as shown in the legend, tooltip etc.
- * 
- * @type {String}
- * @sample {highcharts} highcharts/series/name/ Series name
- * @sample {highmaps} maps/demo/category-map/ Series name
- * @apioption series.name
- */
 
-/**
- * This option allows grouping series in a stacked chart. The stack
- * option can be a string or a number or anything else, as long as the
- * grouped series' stack options match each other.
- * 
- * @type {String}
- * @sample {highcharts} highcharts/series/stack/ Stacked and grouped columns
- * @default null
- * @since 2.1
- * @product highcharts highstock
- * @apioption series.stack
- */
 
-/**
- * The type of series, for example `line` or `column`.
- * 
- * @validvalue [null, "line", "spline", "column", "area", "areaspline", "pie", "arearange", "areasplinerange", "boxplot", "bubble", "columnrange", "errorbar", "funnel", "gauge", "scatter", "waterfall"]
- * @type {String}
- * @sample {highcharts} highcharts/series/type/ Line and column in the same chart
- * @sample {highmaps} maps/demo/mapline-mappoint/ Multiple types in the same map
- * @apioption series.type
- */
 
-/**
- * When using dual or multiple x axes, this number defines which xAxis
- * the particular series is connected to. It refers to either the [axis
- * id](#xAxis.id) or the index of the axis in the xAxis array, with
- * 0 being the first.
- * 
- * @type {Number|String}
- * @default 0
- * @product highcharts highstock
- * @apioption series.xAxis
- */
 
-/**
- * When using dual or multiple y axes, this number defines which yAxis
- * the particular series is connected to. It refers to either the [axis
- * id](#yAxis.id) or the index of the axis in the yAxis array, with
- * 0 being the first.
- * 
- * @type {Number|String}
- * @sample {highcharts} highcharts/series/yaxis/ Apply the column series to the secondary Y axis
- * @default 0
- * @product highcharts highstock
- * @apioption series.yAxis
- */
 
-/**
- * Define the visual z index of the series.
- * 
- * @type {Number}
- * @sample {highcharts} highcharts/plotoptions/series-zindex-default/ With no z index, the series defined last are on top
- * @sample {highcharts} highcharts/plotoptions/series-zindex/ With a z index, the series with the highest z index is on top
- * @sample {highstock} highcharts/plotoptions/series-zindex-default/ With no z index, the series defined last are on top
- * @sample {highstock} highcharts/plotoptions/series-zindex/ With a z index, the series with the highest z index is on top
- * @product highcharts highstock
- * @apioption series.zIndex
- */
 
-/**
- * An array of data points for the series. The points can be given in
- * three ways:
- * 
- * 1.  An array of numerical values. In this case, the numerical values
- * will be interpreted as y values, and x values will be automatically
- * calculated, either starting at 0 and incrementing by 1, or from `pointStart`
- * and `pointInterval` given in the plotOptions. If the axis is has
- * categories, these will be used. This option is not available for
- * range series. Example:
- * 
- * <pre>data: [0, 5, 3, 5]</pre>
- * 
- * 2.  An array of arrays with two values. In this case, the first value
- * is the x value and the second is the y value. If the first value
- * is a string, it is applied as the name of the point, and the x value
- * is incremented following the above rules.
- * 
- * For range series, the arrays will be interpreted as `[x, low,
- * high]`. In this cases, the X value can be skipped altogether to
- * make use of `pointStart` and `pointRange`.
- * 
- * Example:
- * 
- * <pre>data: [[5, 2], [6, 3], [8, 2]]</pre>
- * 
- * 3.  An array of objects with named values. In this case the objects
- * are point configuration objects as seen below.
- * 
- * Range series values are given by `low` and `high`.
- * 
- * Example:
- * 
- * <pre>data: [{
- *     name: 'Point 1',
- *     color: '#00FF00',
- *     y: 0
- * }, {
- *     name: 'Point 2',
- *     color: '#FF00FF',
- *     y: 5
- * }]</pre>
- * 
- * Note that line series and derived types like spline and area, require
- * data to be sorted by X because it interpolates mouse coordinates
- * for the tooltip. Column and scatter series, where each point has
- * its own mouse event, does not require sorting.
- * 
- * @type {Array<Object|Array|Number>}
- * @sample {highcharts} highcharts/chart/reflow-true/ 1) Numerical values
- * @sample {highcharts} highcharts/series/data-array-of-arrays/ 2a) arrays of numeric x and y
- * @sample {highcharts} highcharts/series/data-array-of-arrays-datetime/ 2b) arrays of datetime x and y
- * @sample {highcharts} highcharts/series/data-array-of-name-value/ 2c) arrays of point.name and y
- * @sample {highcharts} highcharts/series/data-array-of-objects/ 3) config objects
- * @sample {highcharts} highcharts/demo/3d-column-null-values/ 4) 3D column with null values
- * @apioption series.data
- */
+
 
 
 
