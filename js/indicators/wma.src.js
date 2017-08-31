436c110b6e9e2b35d14343e75b6dc1df660a7510
Kacper Madej <kacper@blacklabel.pl>
2017-08-31 16:10:02 +0200

    Added WMA docs and tests.

--- js/indicators/wma.src.js
+++ js/indicators/wma.src.js
@@ -1,50 +1,71 @@
-/* global Highcharts module:true */
-(function (factory) {
-	if (typeof module === 'object' && module.exports) {
-		module.exports = factory;
-	} else {
-		factory(Highcharts);
-	}
-}(function (H) {
-	'use strict';
+/**
+ * (c) 2010-2017 Kacper Madej
+ *
+ * License: www.highcharts.com/license
+ */
 
-	var isArray = H.isArray;
+'use strict';
+import H from '../parts/Globals.js';
+import '../parts/Utilities.js';
 
-	// Utils:
-	function accumulateAverage(points, xVal, yVal, i, index) {
-		var xValue = xVal[i],
-			yValue = index < 0 ? yVal[i] : yVal[i][index];
-		
-		points.push([xValue, yValue]);
-	}
+var isArray = H.isArray,
+	seriesType = H.seriesType;
+
+// Utils:
+function accumulateAverage(points, xVal, yVal, i, index) {
+	var xValue = xVal[i],
+		yValue = index < 0 ? yVal[i] : yVal[i][index];
 	
-	function weightedSumArray(array, pLen) {
-		// The denominator is the sum of the number of days as a triangular number. If there are 5 days, the triangular numbers are 5, 4, 3, 2, and 1. The sum is 5+4+3+2+1=15.
-		var denominator = (pLen + 1) / 2 * pLen; // the reduced sum of an arithmetic sequence
-		
-		// reduce VS loop => reduce
-		return array.reduce(function (prev, cur, i) {
-			return [null, prev[1] + cur[1] * (i + 1)];
-		})[1] / denominator;
-	}
+	points.push([xValue, yValue]);
+}
 
-	function populateAverage(points, xVal, yVal, i) {
-		var pLen = points.length,
-			wmaY = weightedSumArray(points, pLen),
-			wmaX = xVal[i - 1];
-		
-		points.shift(); // remove point until range < period
-		
-		return [wmaX, wmaY];
-	}
+function weightedSumArray(array, pLen) {
+	// The denominator is the sum of the number of days as a triangular number.
+	// If there are 5 days, the triangular numbers are 5, 4, 3, 2, and 1.
+	// The sum is 5 + 4 + 3 + 2 + 1 = 15.
+	var denominator = (pLen + 1) / 2 * pLen;
+	
+	// reduce VS loop => reduce
+	return array.reduce(function (prev, cur, i) {
+		return [null, prev[1] + cur[1] * (i + 1)];
+	})[1] / denominator;
+}
 
-	H.seriesType('wma', 'sma', {
+function populateAverage(points, xVal, yVal, i) {
+	var pLen = points.length,
+		wmaY = weightedSumArray(points, pLen),
+		wmaX = xVal[i - 1];
+	
+	points.shift(); // remove point until range < period
+	
+	return [wmaX, wmaY];
+}
+
+/**
+ * The SMA series type.
+ *
+ * @constructor seriesTypes.wma
+ * @augments seriesTypes.sma
+ */
+seriesType('wma', 'sma',
+	/**
+	 * Weighted moving average indicator (WMA). This series requires `linkedTo`
+	 * option to be set.
+	 * 
+	 * @extends {plotOptions.sma}
+	 * @product highstock
+	 * @sample {highstock} stock/indicators/wma
+	 *                     Weighted moving average indicator
+	 * @since 6.0.0
+	 * @optionparent plotOptions.wma
+	 */
+	{
 		name: 'WMA (9)',
 		params: {
-			period: 9,
-			index: 3
+			index: 3,
+			period: 9
 		}
-	}, {
+	}, /** @lends Highcharts.Series.prototype */ {
 		getValues: function (series, params) {
 			var period = params.period,
 				xVal = series.xData,
@@ -99,4 +120,26 @@
 			};
 		}
 	});
-}));
+
+/**
+ * A `WMA` series. If the [type](#series.wma.type) option is not
+ * specified, it is inherited from [chart.type](#chart.type).
+ * 
+ * For options that apply to multiple series, it is recommended to add
+ * them to the [plotOptions.series](#plotOptions.series) options structure.
+ * To apply to all series of this specific type, apply it to
+ * [plotOptions.wma](#plotOptions.wma).
+ * 
+ * @type {Object}
+ * @since 6.0.0
+ * @extends series,plotOptions.wma
+ * @excluding data,dataParser,dataURL
+ * @product highstock
+ * @apioption series.wma
+ */
+
+/**
+ * @extends series.sma.data
+ * @product highstock
+ * @apioption series.wma.data
+ */
--- samples/unit-tests/indicator-wma/recalculations/demo.js
+++ samples/unit-tests/indicator-wma/recalculations/demo.js
@@ -12,23 +12,23 @@ QUnit.test('Test algorithm on data updates.', function (assert) {
                 13, 14, 15, 13, 14, 15
             ]
         }, {
-            type: 'wma',
+            type: 'sma',
             linkedTo: 'main'
         }]
     });
 
     assert.strictEqual(
         chart.series[0].points.length,
-        chart.series[1].points.length + chart.series[1].options.params.period - 1,
-        'Initial number of WMA points is correct'
+        chart.series[1].points.length + chart.series[1].options.params.period,
+        'Initial number of SMA points is correct'
     );
 
     chart.series[0].addPoint(13);
 
     assert.strictEqual(
         chart.series[0].points.length,
-        chart.series[1].points.length + chart.series[1].options.params.period - 1,
-        'After addPoint number of WMA points is correct'
+        chart.series[1].points.length + chart.series[1].options.params.period,
+        'After addPoint number of SMA points is correct'
     );
 
     chart.series[0].setData([11,12,13,14,15,16,17], false);
@@ -41,8 +41,8 @@ QUnit.test('Test algorithm on data updates.', function (assert) {
 
     assert.deepEqual(
         chart.series[1].yData,
-        [12.5, 13.25, 14.125, 15.0625, 16.03125],
-        'Correct values'
+        [13, 14, 15, 16],
+        'Correctl values'
     );
 
     assert.strictEqual(
@@ -55,7 +55,7 @@ QUnit.test('Test algorithm on data updates.', function (assert) {
 
     assert.deepEqual(
         chart.series[1].yData,
-        [12.5, 13.25, 14.125, 15.0625],
+        [13, 14, 15],
         'Correct values after point.remove()'
     );
 });
