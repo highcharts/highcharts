63eb8f3d36b36ef4c448aeef70747e590b6f8573
Sebastian Bochan <sebastian@blacklabel.pl>
2017-08-31 15:49:49 +0200

    Indicators: Optimized SMA docs.

--- js/indicators/ema.src.js
+++ js/indicators/ema.src.js
@@ -114,8 +114,8 @@ seriesType('ema', 'sma',
  * 
  * For options that apply to multiple series, it is recommended to add
  * them to the [plotOptions.series](#plotOptions.series) options structure.
- * To apply to all series of this specific type, apply it to [plotOptions.
- * ema](#plotOptions.ema).
+ * To apply to all series of this specific type, apply it to 
+ * [plotOptions.ema](#plotOptions.ema).
  * 
  * @type {Object}
  * @since 6.0.0
