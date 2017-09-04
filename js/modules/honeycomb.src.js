beefe39b82babf28f643b5a64d50edf8c676cafe
oysteinmoseng <oystein@highsoft.com>
2017-09-05 00:03:27 +0200

    Improvements to honeycomb series.

--- js/modules/honeycomb.src.js
+++ js/modules/honeycomb.src.js
@@ -17,16 +17,6 @@ var seriesType = H.seriesType,
 		return Math.min(Math.max(a, x), b);
 	};
 
-
-/*
-	TODO:
-		- Borderwidth crisping
-		- Margins between points
-		- Hover halo/popout, override getHaloPath()?
-		- Add generator from maps
-*/
-
-
 // Add pixel padding for series. Uses getSeriesPixelPadding on each series and
 // adds the largest padding required. If no series has this function defined,
 // we add nothing.
@@ -70,10 +60,17 @@ H.wrap(H.Axis.prototype, 'setAxisTranslation', function (proceed) {
  * @optionparent plotOptions.honeycomb
  */
 seriesType('honeycomb', 'heatmap', {
+// Default options
 	states: {
 		hover: {
-			//	halo: true,
-			//	brightness: 0.2
+			halo: { 
+				enabled: true,
+				size: 2,
+				opacity: 0.5,
+				attributes: {
+					zIndex: 3
+				}
+			}
 		}
 	}
 
@@ -100,8 +97,7 @@ seriesType('honeycomb', 'heatmap', {
 			);
 		return {
 			// Use translate to compute how far outside the points we draw, and
-			// use this difference as padding. This is then added to
-			// minPixelPadding.
+			// use this difference as padding.
 			padding: Math.abs(coord1 - coord2) || 0,
 
 			// Offset the yAxis length to compensate for shift.
@@ -119,6 +115,9 @@ seriesType('honeycomb', 'heatmap', {
 			options = series.options,
 			xAxis = series.xAxis,
 			yAxis = series.yAxis,
+			pointPadding = options.pointPadding || 0,
+			xPointPadding = xAxis.reversed ? -pointPadding : pointPadding,
+			yPointPadding = yAxis.reversed ? -pointPadding : pointPadding,
 			xPad = (options.colsize || 1) / 3,
 			yPad = (options.rowsize || 1) / 2,
 			yShift;
@@ -152,7 +151,7 @@ seriesType('honeycomb', 'heatmap', {
 				),
 				y1 = between(
 					Math.floor(yAxis.translate(point.y - yPad, 0, 1, 0, 1)),
-					-yAxis.len, 
+					-yAxis.len,
 					2 * yAxis.len
 				),
 				y2 = between(
@@ -167,11 +166,10 @@ seriesType('honeycomb', 'heatmap', {
 				);
 
 			// Shift y-values for every second grid column
-			// We have to reverse the shift for reversed y-axes
 			if (point.x % 2) {
-				yShift = yShift || (
-					Math.round(Math.abs(y3 - y2)) * (yAxis.reversed ? -1 : 1)
-				);
+				yShift = yShift || Math.round(Math.abs(y3 - y1) / 2) *
+					// We have to reverse the shift for reversed y-axes
+					(yAxis.reversed ? -1 : 1);
 				y1 += yShift;
 				y2 += yShift;
 				y3 += yShift;
@@ -181,6 +179,20 @@ seriesType('honeycomb', 'heatmap', {
 			point.plotX = point.clientX = (x2 + x3) / 2;
 			point.plotY = y2;
 
+			// Apply point padding to translated coordinates
+			x1 += xPointPadding;
+			x2 += xPointPadding;
+			x3 -= xPointPadding;
+			x4 -= xPointPadding;
+			y1 -= yPointPadding;
+			y3 += yPointPadding;
+
+			// Store points for halo creation
+			point.tileEdges = {
+				x1: x1, x2: x2, x3: x3,	x4: x4, y1: y1, y2: y2, y3: y3
+			};
+
+			// Finally set the shape for this point
 			point.shapeType = 'path';
 			point.shapeArgs = {
 				d: [
@@ -197,7 +209,23 @@ seriesType('honeycomb', 'heatmap', {
 
 		series.translateColors();
 	},
+
 	alignDataLabel: H.seriesTypes.scatter.prototype.alignDataLabel
+
+// Point class
+}, {
+	haloPath: function (size) {
+		var hexagon = this.tileEdges;
+		return [
+			'M', hexagon.x2 - size, hexagon.y1 + size,
+			'L', hexagon.x3 + size, hexagon.y1 + size,
+			hexagon.x4 + size * 1.5, hexagon.y2,
+			hexagon.x3 + size, hexagon.y3 - size,
+			hexagon.x2 - size, hexagon.y3 - size,
+			hexagon.x1 - size * 1.5, hexagon.y2,
+			'Z'
+		];
+	}
 });
 
 
@@ -210,6 +238,14 @@ seriesType('honeycomb', 'heatmap', {
  */
 seriesType('diamondmap', 'honeycomb', { 
 	// Default options
+	states: {
+		hover: {
+			halo: {	
+				size: 4,
+				opacity: 0.5
+			}
+		}
+	}
 }, {
 	getSeriesPixelPadding: function (axis) {
 		var options = this.options;
@@ -224,6 +260,9 @@ seriesType('diamondmap', 'honeycomb', {
 			options = series.options,
 			xAxis = series.xAxis,
 			yAxis = series.yAxis,
+			pointPadding = options.pointPadding || 0,
+			xPointPadding = xAxis.reversed ? -pointPadding : pointPadding,
+			yPointPadding = yAxis.reversed ? -pointPadding : pointPadding,
 			xPad = (options.colsize || 1),
 			yPad = (options.rowsize || 1) / 2,
 			yShift;
@@ -232,35 +271,35 @@ seriesType('diamondmap', 'honeycomb', {
 
 		each(series.points, function (point) {
 			var x1 = between(
-					Math.floor(
+					Math.round(
 						xAxis.len -
-						xAxis.translate(point.x - xPad, 0, 1, 0, 1)
+						xAxis.translate(point.x - xPad, 0, 1, 0, 0)
 					), -xAxis.len, 2 * xAxis.len
 				),
 				x2 = between(
-					Math.floor(
+					Math.round(
 						xAxis.len -
-						xAxis.translate(point.x, 0, 1, 0, 1)
+						xAxis.translate(point.x, 0, 1, 0, 0)
 					), -xAxis.len, 2 * xAxis.len
 				),
 				x3 = between(
-					Math.floor(
+					Math.round(
 						xAxis.len -
-						xAxis.translate(point.x + xPad, 0, 1, 0, 1)
+						xAxis.translate(point.x + xPad, 0, 1, 0, 0)
 					), -xAxis.len, 2 * xAxis.len
 				),
 				y1 = between(
-					Math.floor(yAxis.translate(point.y - yPad, 0, 1, 0, 1)),
+					Math.round(yAxis.translate(point.y - yPad, 0, 1, 0, 0)),
 					-yAxis.len, 
 					2 * yAxis.len
 				),
 				y2 = between(
-					Math.floor(yAxis.translate(point.y, 0, 1, 0, 1)),
+					Math.round(yAxis.translate(point.y, 0, 1, 0, 0)),
 					-yAxis.len,
 					2 * yAxis.len
 				),
 				y3 = between(
-					Math.floor(yAxis.translate(point.y + yPad, 0, 1, 0, 1)),
+					Math.round(yAxis.translate(point.y + yPad, 0, 1, 0, 0)),
 					-yAxis.len,
 					2 * yAxis.len
 				);
@@ -268,9 +307,7 @@ seriesType('diamondmap', 'honeycomb', {
 			// Shift y-values for every second grid column
 			// We have to reverse the shift for reversed y-axes
 			if (point.x % 2) {
-				yShift = yShift || (
-					Math.round(Math.abs(y3 - y2)) * (yAxis.reversed ? -1 : 1)
-				);
+				yShift = Math.abs(y3 - y1) / 2 * (yAxis.reversed ? -1 : 1);
 				y1 += yShift;
 				y2 += yShift;
 				y3 += yShift;
@@ -280,6 +317,18 @@ seriesType('diamondmap', 'honeycomb', {
 			point.plotX = point.clientX = x2;
 			point.plotY = y2;
 
+			// Apply point padding to translated coordinates
+			x1 += xPointPadding;
+			x3 -= xPointPadding;
+			y1 -= yPointPadding;
+			y3 += yPointPadding;
+
+			// Store points for halo creation
+			point.tileEdges = {
+				x1: x1, x2: x2, x3: x3, y1: y1, y2: y2, y3: y3
+			};
+			
+			// Set this point's shape parameters
 			point.shapeType = 'path';
 			point.shapeArgs = {
 				d: [
@@ -293,5 +342,18 @@ seriesType('diamondmap', 'honeycomb', {
 		});
 
 		series.translateColors();
+	}
+
+// Point class
+}, {
+	haloPath: function (size) {
+		var diamond = this.tileEdges;
+		return [
+			'M', diamond.x2, diamond.y1 + size,
+			'L', diamond.x3 + size, diamond.y2,
+			diamond.x2, diamond.y3 - size,
+			diamond.x1 - size, diamond.y2,
+			'Z'
+		];
 	}
 });
