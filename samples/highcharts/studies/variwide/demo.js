5c1cdf7bea86c7cf5b91c31a83c030b3389d4a3a
Torstein Hønsi <torstein@highsoft.com>
2017-09-06 10:45:05 +0200

    Variwide series: Support pointPadding and groupPadding, category axis laid out correctly. Ref #6874.

--- samples/highcharts/studies/variwide/demo.js
+++ samples/highcharts/studies/variwide/demo.js
@@ -10,7 +10,8 @@
 
 (function (H) {
     var seriesTypes = H.seriesTypes,
-        each = H.each;
+        each = H.each,
+        pick = H.pick;
 
     H.seriesType('variwide', 'column', {
         pointPadding: 0,
@@ -28,39 +29,106 @@
                 series.relZ[i] = series.totalZ;
                 series.totalZ += z;
             });
-        },
-        translate: function () {
-            var series = this,
-                totalZ = series.totalZ,
-                xAxis = series.xAxis;
 
+            if (this.xAxis.categories) {
+                this.xAxis.variwide = true;
+            }
+        },
+
+        /**
+         * Translate an x value inside a given category index into the distorted
+         * axis translation.
+         * @param  {Number} i The category index
+         * @param  {Number} x The X pixel position in undistorted axis pixels
+         * @return {Number}   Distorted X position
+         */
+        postTranslate: function (i, x, debug) {
+
+            var len = this.xAxis.len,
+                totalZ = this.totalZ,
+                relZ = this.relZ,
+                linearSlotLeft = i / relZ.length * len,
+                linearSlotRight = (i + 1) / relZ.length * len,
+                slotLeft = (pick(relZ[i], totalZ) / totalZ) * len,
+                slotRight = (pick(relZ[i + 1], totalZ) / totalZ) * len,
+                xInsideLinearSlot = x - linearSlotLeft,
+                ret;
+
+            ret = slotLeft +
+                xInsideLinearSlot * (slotRight - slotLeft) /
+                (linearSlotRight - linearSlotLeft);
+
+            return ret;
+        },
+
+        /**
+         * Extend translation by distoring X position based on Z.
+         */
+        translate: function () {
             seriesTypes.column.prototype.translate.call(this);
 
             // Distort the points to reflect z dimension
             each(this.points, function (point, i) {
-                var shapeArgs = point.shapeArgs,
-                    relZ = series.relZ[i];
+                var left = this.postTranslate(i, point.shapeArgs.x),
+                    right = this.postTranslate(
+                        i,
+                        point.shapeArgs.x + point.shapeArgs.width
+                    );
 
-                shapeArgs.x *= (relZ / totalZ) / (shapeArgs.x / xAxis.len);
-                shapeArgs.width *= (point.z / totalZ) /
-                    (shapeArgs.width / xAxis.len);
-            });
-            this.xAxis.variwide = true;
+                point.shapeArgs.x = left;
+                point.shapeArgs.width = right - left;
+            }, this);
         }
     });
 
-    /*
-    H.wrap(H.Tick.prototype, 'getPosition', function (proceed) {
-        var pos = proceed.apply(this, Array.prototype.slice.call(arguments, 1));
+    H.wrap(H.Tick.prototype, 'getPosition', function (proceed, horiz, pos) {
+        var xy = proceed.apply(this, Array.prototype.slice.call(arguments, 1));
 
-        return pos;
+        if (horiz && this.axis.variwide) {
+            this.xOrig = xy.x;
+            xy.x =
+                this.axis.pos +
+                this.axis.series[0].postTranslate(pos, xy.x - this.axis.pos);
+        }
+        return xy;
     });
-    */
+
+    H.wrap(H.Tick.prototype, 'getLabelPosition', function (
+        proceed,
+        x,
+        y,
+        label,
+        horiz,
+        labelOptions,
+        tickmarkOffset,
+        index
+    ) {
+        var args = Array.prototype.slice.call(arguments, 1),
+            xy;
+
+        // Replace the x with the original x
+        if (this.axis.variwide && typeof this.xOrig === 'number') {
+            args[0] = this.xOrig;
+        }
+
+        xy = proceed.apply(this, args);
+
+        // Post-translate
+        if (horiz && this.axis.variwide) {
+            xy.x = this.axis.pos +
+                this.axis.series[0].postTranslate(index, xy.x - this.axis.pos);
+        }
+        return xy;
+    });
 
 }(Highcharts));
 
 
 Highcharts.chart('container', {
+
+    chart: {
+        type: 'variwide'
+    },
 
     title: {
         text: 'Highcharts variwide study'
@@ -79,7 +147,6 @@ Highcharts.chart('container', {
     },
 
     series: [{
-        type: 'variwide',
         data: [{
             x: 1,
             y: 1,
@@ -99,7 +166,6 @@ Highcharts.chart('container', {
             inside: true
         }
     }, {
-        type: 'variwide',
         data: [{
             x: 1,
             y: 1,
