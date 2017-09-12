9af502e0ca10c71fe9aec65d1c8e996a30b8edf2
Torstein Hønsi <torstein@highsoft.com>
2017-09-12 14:54:00 +0200

    Variwide: API docs. Ref #6874.

--- js/modules/variwide.src.js
+++ js/modules/variwide.src.js
@@ -21,9 +21,28 @@ var seriesType = H.seriesType,
 	each = H.each,
 	pick = H.pick;
 
-
+/**
+ * A variwide chart (sometimes called _barmekko_, _submarine chart_ or _olympic
+ * chart_) is a column chart with a variable width expressing a third dimension.
+ * 
+ * @extends {plotOptions.column}
+ * @excluding boostThreshold,crisp,depth,edgeColor,edgeWidth,groupZPadding
+ * @product highcharts
+ * @sample {highcharts} highcharts/demo/variwide/
+ *         Variwide chart
+ * @since 6.0.0
+ * @optionparent plotOptions.variwide
+ */
 seriesType('variwide', 'column', {
+	/**
+	 * In a variwide chart, the point padding is 0 in order to express the 
+	 * horizontal stacking of items.
+	 */
 	pointPadding: 0,
+	/**
+	 * In a variwide chart, the group padding is 0 in order to express the 
+	 * horizontal stacking of items.
+	 */
 	groupPadding: 0
 }, {
 	pointArrayMap: ['y', 'z'],
@@ -139,3 +158,82 @@ H.wrap(H.Tick.prototype, 'getLabelPosition', function (
 	}
 	return xy;
 });
+
+
+
+/**
+ * A `variwide` series. If the [type](#series.variwide.type) option is
+ * not specified, it is inherited from [chart.type](#chart.type).
+ * 
+ * For options that apply to multiple series, it is recommended to add
+ * them to the [plotOptions.series](#plotOptions.series) options structure.
+ * To apply to all series of this specific type, apply it to [plotOptions.
+ * variwide](#plotOptions.variwide).
+ * 
+ * @type {Object}
+ * @extends series,plotOptions.variwide
+ * @product highcharts
+ * @apioption series.variwide
+ */
+
+/**
+ * An array of data points for the series. For the `variwide` series type,
+ * points can be given in the following ways:
+ * 
+ * 1.  An array of arrays with 3 or 2 values. In this case, the values
+ * correspond to `x,y,z`. If the first value is a string, it is applied
+ * as the name of the point, and the `x` value is inferred. The `x`
+ * value can also be omitted, in which case the inner arrays should
+ * be of length 2\. Then the `x` value is automatically calculated,
+ * either starting at 0 and incremented by 1, or from `pointStart` and
+ * `pointInterval` given in the series options.
+ * 
+ *  ```js
+ *     data: [
+ *         [0, 1, 2],
+ *         [1, 5, 5],
+ *         [2, 0, 2]
+ *     ]
+ *  ```
+ * 
+ * 2.  An array of objects with named values. The objects are point
+ * configuration objects as seen below. If the total number of data
+ * points exceeds the series' [turboThreshold](#series.variwide.turboThreshold),
+ * this option is not available.
+ * 
+ *  ```js
+ *     data: [{
+ *         x: 1,
+ *         y: 1,
+ *         z: 1,
+ *         name: "Point2",
+ *         color: "#00FF00"
+ *     }, {
+ *         x: 1,
+ *         y: 5,
+ *         z: 4,
+ *         name: "Point1",
+ *         color: "#FF00FF"
+ *     }]
+ *  ```
+ * 
+ * @type {Array<Object|Array>}
+ * @extends series.line.data
+ * @excluding marker
+ * @sample {highcharts} highcharts/chart/reflow-true/ Numerical values
+ * @sample {highcharts} highcharts/series/data-array-of-arrays/ Arrays of numeric x and y
+ * @sample {highcharts} highcharts/series/data-array-of-arrays-datetime/ Arrays of datetime x and y
+ * @sample {highcharts} highcharts/series/data-array-of-name-value/ Arrays of point.name and y
+ * @sample {highcharts} highcharts/series/data-array-of-objects/ Config objects
+ * @product highcharts
+ * @apioption series.variwide.data
+ */
+
+/**
+ * The relative width for each column. The widths are distributed so they sum 
+ * up to the X axis length.
+ * 
+ * @type {Number}
+ * @product highcharts
+ * @apioption series.variwide.data.z
+ */
