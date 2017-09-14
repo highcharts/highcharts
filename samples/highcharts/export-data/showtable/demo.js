c7486eac4fae995f4c595e8bf8ab5c03fe1ac3bc
Torstein Hønsi <torstein@highsoft.com>
2017-09-14 09:46:03 +0200

    Addition to previous.

--- samples/highcharts/export-data/showtable/demo.js
+++ samples/highcharts/export-data/showtable/demo.js
@@ -50,7 +50,7 @@ Highcharts.chart('container', {
     }, {
         name: 'Other',
         data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-    }]
+    }],
 
     exporting: {
         showTable: true
