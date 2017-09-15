c30ab2bc3c2562fbc111b1ce2b45c7f61a4aea29
Torstein Hønsi <torstein@highsoft.com>
2017-09-15 18:07:38 +0200

    Reviewed bell-curve.

--- js/modules/bellcurve.src.js
+++ js/modules/bellcurve.src.js
@@ -50,7 +50,10 @@ function standardDeviation(data, average) {
 
 function normalDensity(x, mean, standardDeviation) {
 	var translation = x - mean;
-	return Math.exp(-(translation * translation) / (2 * standardDeviation * standardDeviation)) / (standardDeviation * Math.sqrt(2 * Math.PI));
+	return Math.exp(
+		-(translation * translation) /
+		(2 * standardDeviation * standardDeviation)
+	) / (standardDeviation * Math.sqrt(2 * Math.PI));
 }
 
 
@@ -63,88 +66,89 @@ function normalDensity(x, mean, standardDeviation) {
  **/
 
 /**
- * A bell curve is an areaspline series which represents the probability density function
- * of the normal distribution. It calculates mean and standard deviation of
- * the base series data and plots the curve according to the calculated parameters.
+ * A bell curve is an areaspline series which represents the probability density
+ * function of the normal distribution. It calculates mean and standard
+ * deviation of the base series data and plots the curve according to the
+ * calculated parameters.
  *
  * @product highcharts
- * @sample {highcharts} highcharts/demo/bell-curve/ Bell curve
+ * @sample {highcharts} highcharts/demo/bellcurve/ Bell curve
  * @since 6.0.0
  * @extends plotOptions.areaspline
- * @apioption plotOptions.bellcurve
+ * @optionparent plotOptions.bellcurve
  * @excluding connectNulls, stacking
  **/
 seriesType('bellcurve', 'areaspline', {
-  /**
-   * This option allows to define the length of the bell curve.
-   * A unit of the length of the bell curve is standard deviation. 
-   *
-   * @type {Number}
-   * @apioption plotOptions.bellcurve.intervals
-   **/
+   /**
+    * This option allows to define the length of the bell curve. A unit of the
+    * length of the bell curve is standard deviation. 
+    */
 	intervals: 3,
 
-  /**
-   * Defines how many points should be plotted within 1 interval.
-   *
-   * @type {Number}
-   * @apioption plotOptions.bellcurve.pointsInInterval
-   **/
+   /**
+    * Defines how many points should be plotted within 1 interval. See 
+    * `plotOptions.bellcurve.intervals`.
+    */
 	pointsInInterval: 3,
+
 	marker: {
 		enabled: false
 	}
 
-  /**
-   * A `bellcurve` series. If the [type](#series.bellcurve.type) option is not
-   * specified, it is inherited from [chart.type](#chart.type).
-   * 
-   * For options that apply to multiple series, it is recommended to add
-   * them to the [plotOptions.series](#plotOptions.series) options structure.
-   * To apply to all series of this specific type, apply it to [plotOptions.
-   * bellcurve](#plotOptions.bellcurve).
-   * 
-   * @type {Object}
-   * @since 6.0.0
-   * @extends series,plotOptions.bellcurve
-   * @excluding dataParser,dataURL,data
-   * @product highcharts
-   * @apioption series.bellcurve
-   **/
-  
-  /**
-   * An integer identifying the index to use for the base series, or a string
-   * representing the id of the series.
-   *
-   * @type {Number|String}
-   * @default undefined
-   * @apioption series.bellcurve.baseSeries
-   **/
+   /**
+    * A `bellcurve` series. If the [type](#series.bellcurve.type) option is not
+    * specified, it is inherited from [chart.type](#chart.type).
+    * 
+    * For options that apply to multiple series, it is recommended to add
+    * them to the [plotOptions.series](#plotOptions.series) options structure.
+    * To apply to all series of this specific type, apply it to [plotOptions.
+    * bellcurve](#plotOptions.bellcurve).
+    * 
+    * @type {Object}
+    * @since 6.0.0
+    * @extends series,plotOptions.bellcurve
+    * @excluding dataParser,dataURL,data
+    * @product highcharts
+    * @apioption series.bellcurve
+    **/
+   
+   /**
+    * An integer identifying the index to use for the base series, or a string
+    * representing the id of the series.
+    *
+    * @type {Number|String}
+    * @default undefined
+    * @apioption series.bellcurve.baseSeries
+    **/  
 
-  /**
-   * An array of data points for the series. For the `pareto` series type,
-   * points are calculated dynamically.
-   * 
-   * @type {Array<Object|Array>}
-   * @since 6.0.0
-   * @extends series.bellcurve.data
-   * @product highcharts
-   * @apioption series.bellcurve.data
-   **/
+   /**
+    * An array of data points for the series. For the `bellcurve` series type,
+    * points are calculated dynamically.
+    * 
+    * @type {Array<Object|Array>}
+    * @since 6.0.0
+    * @extends series.bellcurve.data
+    * @product highcharts
+    * @apioption series.bellcurve.data
+    **/
 }, merge(derivedSeriesMixin, {
 	setMean: function () {
 		this.mean = correctFloat(mean(this.baseSeries.yData));
 	},
 
 	setStandardDeviation: function () {
-		this.standardDeviation = correctFloat(standardDeviation(this.baseSeries.yData, this.mean));
+		this.standardDeviation = correctFloat(
+			standardDeviation(this.baseSeries.yData, this.mean)
+		);
 	},
 
 	setDerivedData: function () {
 		if (this.baseSeries.yData.length > 1) {
 			this.setMean();
 			this.setStandardDeviation();
-			this.setData(this.derivedData(this.mean, this.standardDeviation), false);
+			this.setData(
+				this.derivedData(this.mean, this.standardDeviation), false
+			);
 		}
 	},
 
