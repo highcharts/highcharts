b0728190bed94995a32ce8bef3e5f5ddce21b16c
Torstein Hønsi <torstein@highsoft.com>
2017-09-28 15:56:24 +0200

    Simplified wordcloud sample.

--- samples/highcharts/demo/wordcloud/demo.js
+++ samples/highcharts/demo/wordcloud/demo.js
@@ -1,31 +1,28 @@
-(function (H) {
-    var text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum erat ac justo sollicitudin, quis lacinia ligula fringilla. Pellentesque hendrerit, nisi vitae posuere condimentum, lectus urna accumsan libero, rutrum commodo mi lacus pretium erat. Phasellus pretium ultrices mi sed semper. Praesent ut tristique magna. Donec nisl tellus, sagittis ut tempus sit amet, consectetur eget erat. Sed ornare gravida lacinia. Curabitur iaculis metus purus, eget pretium est laoreet ut. Quisque tristique augue ac eros malesuada, vitae facilisis mauris sollicitudin. Mauris ac molestie nulla, vitae facilisis quam. Curabitur placerat ornare sem, in mattis purus posuere eget. Praesent non condimentum odio. Nunc aliquet, odio nec auctor congue, sapien justo dictum massa, nec fermentum massa sapien non tellus. Praesent luctus eros et nunc pretium hendrerit. In consequat et eros nec interdum. Ut neque dui, maximus id elit ac, consequat pretium tellus. Nullam vel accumsan lorem.';
-    var data = text
-        .split(',').join('') // remove commas
-        .split('.').join('') // remove periods
-        .split(' ') // split into words
-        .reduce(function (arr, word) {
-            var obj = arr.find(function (obj) {
-                return obj.name === word;
-            });
-            if (obj) {
-                obj.weight += 1;
-            } else {
-                obj = {
-                    name: word,
-                    weight: 1
-                };
-                arr.push(obj);
-            }
-            return arr;
-        }, []);
-    H.chart('container', {
-        series: [{
-            type: 'wordcloud',
-            data: data
-        }],
-        title: {
-            text: 'Wordcloud of Lorem Ipsum'
+var text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum erat ac justo sollicitudin, quis lacinia ligula fringilla. Pellentesque hendrerit, nisi vitae posuere condimentum, lectus urna accumsan libero, rutrum commodo mi lacus pretium erat. Phasellus pretium ultrices mi sed semper. Praesent ut tristique magna. Donec nisl tellus, sagittis ut tempus sit amet, consectetur eget erat. Sed ornare gravida lacinia. Curabitur iaculis metus purus, eget pretium est laoreet ut. Quisque tristique augue ac eros malesuada, vitae facilisis mauris sollicitudin. Mauris ac molestie nulla, vitae facilisis quam. Curabitur placerat ornare sem, in mattis purus posuere eget. Praesent non condimentum odio. Nunc aliquet, odio nec auctor congue, sapien justo dictum massa, nec fermentum massa sapien non tellus. Praesent luctus eros et nunc pretium hendrerit. In consequat et eros nec interdum. Ut neque dui, maximus id elit ac, consequat pretium tellus. Nullam vel accumsan lorem.';
+var data = text
+    .split(/[,\. ]+/g)
+    .reduce(function (arr, word) {
+        var obj = arr.find(function (obj) {
+            return obj.name === word;
+        });
+        if (obj) {
+            obj.weight += 1;
+        } else {
+            obj = {
+                name: word,
+                weight: 1
+            };
+            arr.push(obj);
         }
-    });
-}(Highcharts));
+        return arr;
+    }, []);
+
+Highcharts.chart('container', {
+    series: [{
+        type: 'wordcloud',
+        data: data
+    }],
+    title: {
+        text: 'Wordcloud of Lorem Ipsum'
+    }
+});
