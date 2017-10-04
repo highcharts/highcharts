6e71029b32bb3ae660843d18b52f8c8d3a62f7de
Torstein Hønsi <torstein@highsoft.com>
2017-10-04 08:37:21 +0200

    Karma: Script to scrape dependencies.

--- karma.getscripts.js
+++ karma.getscripts.js
@@ -1,10 +1,38 @@
 /* eslint-env node, es6 */
 /* global console */
+
+/*
+To do
+- replace highstock and highmaps with modules
+- translate to local files where possible
+- parse resources from demo.details?
+ */
+
+const fs = require('fs');
 const glob = require('glob-fs')({ gitignore: true });
 
 const files = glob.readdirSync('samples/unit-tests/**/**/demo.html');
 
+let dependencies = [];
+
+let i = 0;
+
 files.forEach(file => {
-    console.log(file); // eslint-disable-line no-console
+    if (i < 50) {
+        let html = fs.readFileSync(file, 'utf8');
+
+        let regex = /src="(.*?)"/g;
+        let match = regex.exec(html);
+        while (match) {
+            if (dependencies.indexOf(match[1]) === -1) {
+                dependencies.push(match[1]);
+            }
+            match = regex.exec(html);
+        }
+    }
+
+    i++;
 });
+
+console.log(dependencies); // eslint-disable-line no-console
 
