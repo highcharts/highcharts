b6358d5e03324010cdc38202b49dcc715e0791e9
Torstein Hønsi <torstein@highsoft.com>
2017-10-18 11:41:03 +0200

    New test for #4675.

--- samples/unit-tests/boost/enter-exit/demo.js
+++ samples/unit-tests/boost/enter-exit/demo.js
@@ -119,4 +119,28 @@
         assertBoosted(assert, s);
 
     });
+
+    QUnit.test("Boost module should remove tracker after zoom", function (assert) {
+        var chart = $('#container').highcharts({
+            series: [{
+                boostThreshold: 100,
+                data: (function () {
+                    var d = [],
+                        n = 5000;
+                    while (n--) {
+                        d.push(Math.random());
+                    }
+                    return d;
+                }())
+            }]
+        }).highcharts();
+
+        chart.xAxis[0].setExtremes(0, 10);
+        chart.xAxis[0].setExtremes();
+
+        assert.strictEqual(
+            chart.series[0].tracker,
+            null
+        );
+    });
 }());
