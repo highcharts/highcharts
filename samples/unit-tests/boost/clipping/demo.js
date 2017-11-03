5d27939d22548592859bb00a79d2054aa284d5ae
Torstein Hønsi <torstein@highsoft.com>
2017-11-03 12:19:42 +0100

    Fixed #6895, zoomed heat map wastn't clipped properly in boost mode.

---
+++ samples/unit-tests/boost/clipping/demo.js
@@ -0,0 +1,27 @@
+(function () {
+    QUnit.test("#6895 - clipping rectangle after set extremes.", function (assert) {
+        var chart = Highcharts.chart('container', {
+            chart: {
+                type: 'column'
+            },
+            series: [{
+                boostThreshold: 1,
+                data: [
+                    [0, 0],
+                    [0, 1],
+                    [0, 2],
+                    [0, 3],
+                    [0, 4]
+                ]
+            }]
+        });
+
+        chart.yAxis[0].setExtremes(1, 2);
+
+        assert.strictEqual(
+            chart.series[0].boostClipRect.getBBox(true).height,
+            chart.plotHeight,
+            'Correct height of the clipping box.'
+        );
+    });
+}());
