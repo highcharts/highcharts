366c3877084f7df2533eba4e5dde264a983ce8f7
Torstein Hønsi <torstein@highsoft.com>
2017-11-07 14:11:22 +0100

    Samples: Updated Gantt demo.

--- samples/gantt/grid-axis/with-navigator/demo.js
+++ samples/gantt/grid-axis/with-navigator/demo.js
@@ -2,9 +2,7 @@
 // THE CHART
 Highcharts.stockChart('container', {
     chart: {
-        type: 'scatter',
-        marginLeft: 150,
-        marginRight: 150
+        type: 'line'
     },
     title: {
         text: 'Grid axis with navigator'
@@ -14,47 +12,22 @@ Highcharts.stockChart('container', {
         id: 'bottom-datetime-axis',
         grid: true,
         opposite: true,
-        type: 'datetime',
-        tickInterval: 1000 * 60 * 60 * 24, // Day
-        labels: {
-            format: '{value:%E}'
-        }
+        type: 'datetime'
     }, {
         grid: true,
         type: 'datetime',
         opposite: true,
-        tickInterval: 1000 * 60 * 60 * 24 * 7, // Week
         labels: {
-            format: '{value:Week %W}',
             style: {
                 fontSize: '15px'
             }
         },
-        linkedTo: 0
-    }],
-    yAxis: [{
-        title: '',
-        opposite: false,
-        showLastLabel: true,
-        categories: ['Prototyping', 'Development', 'Testing'],
-        grid: true
+        linkedTo: 0,
+        tickPixelInterval: 200
     }],
     series: [{
         name: 'Project 1',
-        borderRadius: 10,
         xAxis: 0,
-        data: [{
-            x: new Date(2014, 10, 18, 12),
-            y: 0
-        }, {
-            x: new Date(2014, 10, 20, 12),
-            y: 1
-        }, {
-            x: new Date(2014, 10, 23, 12),
-            y: 2
-        }, {
-            x: new Date(2014, 10, 26, 12),
-            y: 0
-        }]
+        data: usdeur
     }]
 });
