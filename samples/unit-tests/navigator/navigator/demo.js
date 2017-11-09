0e96921d140395138b6783b8f033c3448a3be1af
Torstein Hønsi <torstein@highsoft.com>
2017-11-09 09:08:58 +0100

    Tests: Moved to name-based structure.

--- samples/unit-tests/navigator/navigator/demo.js
+++ samples/unit-tests/navigator/navigator/demo.js
@@ -90,3 +90,98 @@ QUnit.test(
         }, 0);
     }
 );
+
+QUnit.test(
+    'Scrollbar without navigator (#5709).',
+    function (assert) {
+        var done = assert.async();
+
+        $('#container').highcharts('StockChart', {
+            chart: {
+                zoomType: 'xy'
+            },
+            navigator: {
+                enabled: false
+            },
+            scrollbar: {
+                enabled: true,
+                showFull: true
+            }
+        }, function (chart) {
+            setTimeout(function () {
+                chart.addSeries({
+                    data: [1, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 1]
+                });
+                assert.strictEqual(
+                    chart.scroller.scrollbar.group.translateY >= 0,
+                    true,
+                    'Correct position for a scrollbar'
+                );
+                done();
+            }, 1);
+        });
+    }
+);
+
+QUnit.test('Missing points using navigator (#5699)', function (assert) {
+    var container = $('#container'),
+        chart = container.highcharts('StockChart', {
+            chart: {
+                width: 600,
+                height: 400
+            }
+        }).highcharts(),
+        offset = container.offset(),
+        navigator = chart.scroller,
+        done = assert.async();
+
+    chart.addSeries({
+        type: 'column',
+        name: 'USD to EUR',
+        data: usdeur
+    });
+
+    navigator.handlesMousedown({
+        pageX: offset.left + 578,
+        pageY: offset.top + 400 - 30
+    }, 0);
+
+    navigator.mouseMoveHandler({
+        pageX: offset.left + 309,
+        pageY: offset.top + 400 - 30,
+        DOMType: 'mousemove'
+    });
+
+    setTimeout(function () {
+        navigator.hasDragged = true;
+        navigator.mouseUpHandler({
+            pageX: offset.left + 308,
+            pageY: offset.top + 400 - 30,
+            DOMType: 'mouseup'
+        });
+        assert.strictEqual(
+            chart.series[0].points !== null,
+            true,
+            'Points exist.'
+        );
+        done();
+    }, 0);
+});
+
+QUnit.test('#3961 - Zone zAxis shouldn\'t cause errors in Navigator series.', function (assert) {
+    var chart = $('#container').highcharts('StockChart', {
+        series: [{
+            type: 'bubble',
+            data: [
+                [0, 10, 20],
+                [1, 10, 20]
+            ]
+        }]
+    }).highcharts();
+
+    assert.strictEqual(
+        chart.scroller.handles.length !== 0, // handles are not rendered when we get error in zones
+        true,
+        'No errors in zones for bubble series.'
+    );
+});
--- samples/unit-tests/rangeselector/input-range/demo.js
+++ samples/unit-tests/rangeselector/input-range/demo.js
@@ -101,3 +101,43 @@ QUnit.test('#6537 - 1M button should select range 28.02-31.03', function (assert
         'xAxis minimum correct'
     );
 });
+
+QUnit.test(
+    '#3228 - RangeSelector inputs setting range based on navigator xAxis.',
+    function (assert) {
+        var min = Date.UTC(2000, 0, 1),
+            middle = Date.UTC(2005, 0, 1),
+            max = Date.UTC(20010, 0, 1),
+            chart = $('#container').highcharts('StockChart', {
+                navigator: {
+                    adaptToUpdatedData: false,
+                    series: []
+                },
+                xAxis: {
+                    events: {
+                        afterSetExtremes: function () {
+                            this.series[0].setData([[middle, 20], [max, 100]]);
+                        }
+                    }
+                },
+                series: [{
+                    data: [
+              [min, 10],
+              [middle, 11],
+              [max, 10]
+                    ]
+                }]
+            }).highcharts();
+
+
+        chart.xAxis[0].setExtremes(middle, max);
+        chart.rangeSelector.minInput.value = '2000-01-01';
+        chart.rangeSelector.minInput.onkeypress({ keyCode: 13 });
+
+        assert.strictEqual(
+            chart.xAxis[0].min,
+            min,
+            'Correct extremes in xAxis'
+        );
+    }
+);
