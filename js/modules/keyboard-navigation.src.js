f8e695877407590197325744cb09111590a738d6
oysteinmoseng <oystein@highsoft.com>
2017-11-10 10:21:55 +0100

    Fixed keyboard navigation issue with moving between series with null points.

--- js/modules/keyboard-navigation.src.js
+++ js/modules/keyboard-navigation.src.js
@@ -319,7 +319,7 @@ function getClosestPoint(point, series, xWeight, yWeight) {
 	while (i--) {
 		dPoint = series.points[i];
 		if (dPoint.plotX === undefined || dPoint.plotY === undefined) {
-			return;
+			continue;
 		}
 		distance = (point.plotX - dPoint.plotX) *
 				(point.plotX - dPoint.plotX) * (xWeight || 1) +
@@ -330,7 +330,7 @@ function getClosestPoint(point, series, xWeight, yWeight) {
 			minIx = i;
 		}
 	}
-	return series.points[minIx || 0];
+	return minIx !== undefined && series.points[minIx];
 }
 
 
