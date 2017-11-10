5ab475d9f8c8c4d7861970563f03ebe6fede1091
jon-a-nygaard <jon.arild@highsoft.com>
2017-11-10 09:59:52 +0100

    Temporary fix to allow ES modules to work.

--- js/parts/Globals.js
+++ js/parts/Globals.js
@@ -4,21 +4,22 @@
  * License: www.highcharts.com/license
  */
 'use strict';
-/* global win */
-var doc = win.document,
-	SVG_NS = 'http://www.w3.org/2000/svg',
-	userAgent = (win.navigator && win.navigator.userAgent) || '',
-	svg = doc && doc.createElementNS && !!doc.createElementNS(SVG_NS, 'svg').createSVGRect,
-	isMS = /(edge|msie|trident)/i.test(userAgent) && !win.opera,
-	isFirefox = /Firefox/.test(userAgent),
-	hasBidiBug = isFirefox && parseInt(userAgent.split('Firefox/')[1], 10) < 4; // issue #38
+/* global win, window */
 
-var Highcharts = win.Highcharts ? win.Highcharts.error(16, true) : {
+// glob is a temporary fix to allow our es-modules to work.
+var glob = typeof win === 'undefined' ? window : win,
+	doc = glob.document,
+	SVG_NS = 'http://www.w3.org/2000/svg',
+	userAgent = (glob.navigator && glob.navigator.userAgent) || '',
+	svg = doc && doc.createElementNS && !!doc.createElementNS(SVG_NS, 'svg').createSVGRect,
+	isMS = /(edge|msie|trident)/i.test(userAgent) && !glob.opera,
+	isFirefox = /Firefox/.test(userAgent);
+
+var Highcharts = glob.Highcharts ? glob.Highcharts.error(16, true) : {
 	product: '@product.name@',
 	version: '@product.version@',
 	deg2rad: Math.PI * 2 / 360,
 	doc: doc,
-	hasBidiBug: hasBidiBug,
 	hasTouch: doc && doc.documentElement.ontouchstart !== undefined,
 	isMS: isMS,
 	isWebKit: /AppleWebKit/.test(userAgent),
@@ -29,7 +30,7 @@ var Highcharts = win.Highcharts ? win.Highcharts.error(16, true) : {
 	seriesTypes: {},
 	symbolSizes: {},
 	svg: svg,
-	win: win,
+	win: glob,
 	marginNames: ['plotTop', 'marginRight', 'marginBottom', 'plotLeft'],
 	noop: function () {
 		return undefined;
