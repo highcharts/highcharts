eba24d252b36481e70d6f6ae458a94d761d37df8
Torstein Hønsi <torstein@highsoft.com>
2017-12-06 09:41:31 +0100

    Samples: Linted.

--- samples/unit-tests/series-flags/pointsort/demo.js
+++ samples/unit-tests/series-flags/pointsort/demo.js
@@ -8,9 +8,10 @@ QUnit.test('Order of flags of the same x position (#3763)', function (assert) {
                 shape: 'circlepin',
                 stackDistance: 20,
                 onSeries: 's1',
-                data: (function(n) {
-                    var d = [], i = n;
-                    while(i--) {
+                data: (function (n) {
+                    var d = [],
+                        i = n;
+                    while (i--) {
                         d.push({
                             x: 1,
                             title: n - i
@@ -26,20 +27,20 @@ QUnit.test('Order of flags of the same x position (#3763)', function (assert) {
         series = chart.series[1];
 
     assert.strictEqual(
-        (function(s) {
+        (function (s) {
             var ret = true,
                 data = s.data,
                 points = s.points,
                 len = s.data.length;
-            for(var i = 0; i < len; i++) {
+            for (var i = 0; i < len; i++) {
                 if (data[i].index !== points[i].index) {
                     ret = false;
                     i = len; // quit
                 }
             }
             return ret;
-        }(series)), 
-        true, 
+        }(series)),
+        true,
         'Order of points the same as data'
     );
 });
