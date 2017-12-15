78cd5f0bf4fee44880c38f5e382d49f790db6c07
jon-a-nygaard <jon.arild@highsoft.com>
2017-12-15 14:36:40 +0100

    Added missing parameter to generate api function in jsdoc task.

--- gulpfile.js
+++ gulpfile.js
@@ -996,7 +996,10 @@ const generateAPI = (input, output, onlyBuildCurrent) => new Promise((resolve, r
             console.log(message.noSeries);
             reject(new Error(message.noSeries));
         }
-        generate(json, output, onlyBuildCurrent, () => {
+        generate(json, output, onlyBuildCurrent, {
+          platform: 'JS',
+          products: { highcharts: true, highstock: true, highmaps: true }
+        }, () => {
             console.log(message.success);
             resolve(message.success);
         });
