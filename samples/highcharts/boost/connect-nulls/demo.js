530d0c7708f8fd0d361d95936130692e7ffefc18
cvasseng <cvasseng@gmail.com>
2018-01-10 15:56:26 +0100

    Boost: fixed connectNulls=true, and renamed demo

--- samples/highcharts/boost/connect-nulls/demo.js
+++ samples/highcharts/boost/connect-nulls/demo.js
@@ -33,4 +33,37 @@ Highcharts.chart('container', {
     }]
 });
 
+Highcharts.chart('container-2', {
+    chart: {
+        zoomType: 'x',
+        panning: true,
+        panKey: 'shift'
+    },
+
+    boost: {
+        useGPUTranslations: true,
+        seriesThreshold: 1
+    },
+
+    title: {
+        text: 'Boost connectNulls: true'
+    },
+
+    tooltip: {
+        valueDecimals: 2
+    },
+
+    series: [{
+        lineWidth: 0.5,
+        connectNulls: true,
+        data: [
+            [0, 1],
+            [1, 5],
+            [2, null],
+            [3, 3],
+            [4, 0]
+        ]
+    }]
+});
+
 console.timeEnd('line');
