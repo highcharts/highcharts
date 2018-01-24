018a9913084a44c3843ef7278341f6b91d4d05f1
Torstein Hønsi <torstein@highsoft.com>
2018-01-24 09:26:02 +0100

    Typo.

--- js/supplemental.docs.js
+++ js/supplemental.docs.js
@@ -8,7 +8,7 @@
 
 /**
  * An optional scrollbar to display on the X axis in response to limiting
- * the minimum an maximum of the axis values.
+ * the minimum and maximum of the axis values.
  *
  * In styled mode, all the presentational options for the scrollbar
  * are replaced by the classes `.highcharts-scrollbar-thumb`, `.highcharts-scrollbar-arrow`, `.highcharts-scrollbar-button`, `.highcharts-scrollbar-rifles` and `.highcharts-scrollbar-track`.
