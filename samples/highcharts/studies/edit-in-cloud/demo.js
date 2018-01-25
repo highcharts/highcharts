f0d3c56bd858be23e9255e2d002a2d227f7db948
Torstein Hønsi <torstein@highsoft.com>
2018-01-25 09:24:11 +0100

    Samples: edit in cloud demo.

--- samples/highcharts/studies/edit-in-cloud/demo.js
+++ samples/highcharts/studies/edit-in-cloud/demo.js
@@ -14,6 +14,46 @@
  */
 (function (H) {
 
+    H.Chart.prototype.editInCloud = function () {
+        // Recursively remove function callbacks
+        function removeFunctions(ob) {
+            Object.keys(ob).forEach(function (key) {
+                if (typeof ob[key] === 'function') {
+                    delete ob[key];
+                }
+                if (H.isObject(ob[key])) { // object and not an array
+                    removeFunctions(ob[key]);
+                }
+            });
+        }
+
+        var options = H.merge(this.userOptions);
+        removeFunctions(options);
+        var params = {
+            name: (options.title && options.title.text) || 'Chart title',
+            options: options,
+            settings: {
+                constructor: 'Chart',
+                dataProvider: {
+                    csv: this.getCSV()
+                }
+            }
+        };
+        params = JSON.stringify(params);
+        params = encodeURIComponent(params);
+        params = unescape(params);
+        params = btoa(params);
+
+        // Open new tab
+        var a = document.createElement('a');
+        a.href = 'https://cloud.highcharts.com/create?c=' + params;
+        a.target = '_blank';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+
+    };
+
     H.getOptions().lang.editInCloud = 'Edit in Cloud';
     H.getOptions().exporting.buttons.contextButton.menuItems.push(
         'separator',
@@ -22,44 +62,7 @@
     H.getOptions().exporting.menuItemDefinitions.editInCloud = {
         textKey: 'editInCloud',
         onclick: function () {
-            // Recursively remove function callbacks
-            function removeFunctions(ob) {
-                Object.keys(ob).forEach(function (key) {
-                    if (typeof ob[key] === 'function') {
-                        delete ob[key];
-                    }
-                    if (H.isObject(ob[key])) { // object and not an array
-                        removeFunctions(ob[key]);
-                    }
-                });
-            }
-
-            var options = H.merge(this.userOptions);
-            removeFunctions(options);
-
-            var params = {
-                name: (options.title && options.title.text) || 'Chart title',
-                options: options,
-                settings: {
-                    constructor: 'Chart',
-                    dataProvider: {
-                        csv: this.getCSV()
-                    }
-                }
-            };
-            params = JSON.stringify(params);
-            params = encodeURIComponent(params);
-            params = unescape(params);
-            params = btoa(params);
-
-            // Open new tab
-            var a = document.createElement('a');
-            a.href = 'https://cloud.highcharts.com/create?c=' + params;
-            a.target = '_blank';
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
-
+            this.editInCloud();
         }
     };
 
@@ -68,7 +71,7 @@
 Highcharts.chart('container', {
 
     title: {
-        text: 'Edit in Cloud'
+        text: 'Edit in Highcharts Cloud'
     },
 
     subtitle: {
