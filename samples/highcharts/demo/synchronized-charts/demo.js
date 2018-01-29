a1da542bfa42c3c7dbfdbf26b81863a9510a0426
Torstein Hønsi <torstein@highsoft.com>
2018-01-29 12:23:32 +0100

    Fixed #7726, synced charts failed with disabled `xAxis.crosshairs.snap`.

--- samples/highcharts/demo/synchronized-charts/demo.js
+++ samples/highcharts/demo/synchronized-charts/demo.js
@@ -37,6 +37,7 @@ Highcharts.Pointer.prototype.reset = function () {
  * Highlight a point by showing tooltip, setting hover state and draw crosshair
  */
 Highcharts.Point.prototype.highlight = function (event) {
+    event = this.series.chart.pointer.normalize(event);
     this.onMouseOver(); // Show the hover marker
     this.series.chart.tooltip.refresh(this); // Show the tooltip
     this.series.chart.xAxis[0].drawCrosshair(event, this); // Show the crosshair
