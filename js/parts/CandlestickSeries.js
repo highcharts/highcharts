highcharts/highcharts

commit 791f593bfbd4ed4b4cc2f67d5cec2a13e74667ed

Docs: Moved doclets around to avoid loose doclets being visible for the new JSDoc parser.

js/parts/CandlestickSeries.js
@@ -25,6 +25,24 @@ var defaultPlotOptions = H.defaultPlotOptions,
  * @optionparent plotOptions.candlestick
  */
 var candlestickOptions = {
+	
+	/**
+	 * The specific line color for up candle sticks. The default is to inherit
+	 * the general `lineColor` setting.
+	 * 
+	 * @type {Color}
+	 * @sample {highstock} stock/plotoptions/candlestick-linecolor/ Candlestick line colors
+	 * @default null
+	 * @since 1.3.6
+	 * @product highstock
+	 * @apioption plotOptions.candlestick.upLineColor
+	 */
+	/*= } =*/
+
+	/**
+	 * @default ohlc
+	 * @apioption plotOptions.candlestick.dataGrouping.approximation
+	 */
 
 	states: {
 
@@ -96,24 +114,6 @@ var candlestickOptions = {
 	upColor: '${palette.backgroundColor}',
 
 	stickyTracking: true
-	
-	/**
-	 * The specific line color for up candle sticks. The default is to inherit
-	 * the general `lineColor` setting.
-	 * 
-	 * @type {Color}
-	 * @sample {highstock} stock/plotoptions/candlestick-linecolor/ Candlestick line colors
-	 * @default null
-	 * @since 1.3.6
-	 * @product highstock
-	 * @apioption plotOptions.candlestick.upLineColor
-	 */
-	/*= } =*/
-
-	/**
-	 * @default ohlc
-	 * @apioption plotOptions.candlestick.dataGrouping.approximation
-	 */
 
 };
 

js/modules/drilldown.src.js
@@ -78,6 +78,18 @@ defaultOptions.drilldown = {
 	 * @apioption drilldown.allowPointDrilldown
 	 */
 
+	/**
+	 * An array of series configurations for the drill down. Each series
+	 * configuration uses the same syntax as the [series](#series) option
+	 * set. These drilldown series are hidden by default. The drilldown
+	 * series is linked to the parent series' point by its `id`.
+	 * 
+	 * @type {Array<Object>}
+	 * @since 3.0.8
+	 * @product highcharts highmaps
+	 * @apioption drilldown.series
+	 */
+
 	/*= if (build.classic) { =*/
 
 	/**
@@ -171,45 +183,6 @@ defaultOptions.drilldown = {
 	 * @product highcharts highmaps
 	 */
 	drillUpButton: {
-
-		/**
-		 * Positioning options for the button within the `relativeTo` box.
-		 * Available properties are `x`, `y`, `align` and `verticalAlign`.
-		 * 
-		 * @type {Object}
-		 * @since 3.0.8
-		 * @product highcharts highmaps
-		 */
-		position: { 
-
-			/**
-			 * Horizontal alignment.
-			 * @type {String}
-			 */
-			align: 'right',
-
-			/**
-			 * The X offset of the button.
-			 * @type {Number}
-			 */
-			x: -10,
-
-			/**
-			 * The Y offset of the button.
-			 * @type {Number}
-			 */
-			y: 10
-
-			/**
-			 * Vertical alignment of the button.
-			 *
-			 * @type {String}
-			 * @default top
-			 * @validvalue ["top", "middle", "bottom"]
-			 * @product highcharts highmaps
-			 * @apioption drilldown.drillUpButton.position.verticalAlign
-			 */
-		}
 		/**
 		 * What box to align the button to. Can be either `plotBox` or
 		 * `spacingBox.
@@ -240,19 +213,46 @@ defaultOptions.drilldown = {
 		 * @product highcharts highmaps
 		 * @apioption drilldown.drillUpButton.theme
 		 */
-	}
 
-	/**
-	 * An array of series configurations for the drill down. Each series
-	 * configuration uses the same syntax as the [series](#series) option
-	 * set. These drilldown series are hidden by default. The drilldown
-	 * series is linked to the parent series' point by its `id`.
-	 * 
-	 * @type {Array<Object>}
-	 * @since 3.0.8
-	 * @product highcharts highmaps
-	 * @apioption drilldown.series
-	 */
+		/**
+		 * Positioning options for the button within the `relativeTo` box.
+		 * Available properties are `x`, `y`, `align` and `verticalAlign`.
+		 * 
+		 * @type {Object}
+		 * @since 3.0.8
+		 * @product highcharts highmaps
+		 */
+		position: {
+
+			/**
+			 * Vertical alignment of the button.
+			 *
+			 * @type {String}
+			 * @default top
+			 * @validvalue ["top", "middle", "bottom"]
+			 * @product highcharts highmaps
+			 * @apioption drilldown.drillUpButton.position.verticalAlign
+			 */
+
+			/**
+			 * Horizontal alignment.
+			 * @type {String}
+			 */
+			align: 'right',
+
+			/**
+			 * The X offset of the button.
+			 * @type {Number}
+			 */
+			x: -10,
+
+			/**
+			 * The Y offset of the button.
+			 * @type {Number}
+			 */
+			y: 10
+		}
+	}
 };	
 
 
@@ -1020,16 +1020,6 @@ wrap(H.Point.prototype, 'init', function (proceed, series, options, x) {
 				point.doDrilldown(undefined, undefined, e);
 			}
 		});
-		/*
-		wrap(point, 'importEvents', function (proceed) { // wrapping importEvents makes point.click event work
-			if (!this.hasImportedEvents) {
-				proceed.call(this);
-				H.addEvent(this, 'click', function () {
-					this.doDrilldown();
-				});
-			}
-		});
-		*/
 
 	}
 

js/modules/export-data.src.js
@@ -44,6 +44,19 @@ Highcharts.setOptions({
 	 * @optionparent exporting
 	 */
 	exporting: {
+		
+		/**
+		 * Export-data module required. Caption for the data table. Same as
+		 * chart title by default. Set to `false` to disable.
+		 *
+		 * @type {Boolean|String}
+		 * @since 6.0.4
+		 * @sample highcharts/export-data/multilevel-table
+		 *			Multiple table headers
+		 * @default undefined
+		 * @apioption exporting.tableCaption
+		 */
+		
 		/**
 		 * Options for exporting data to CSV or ExCel, or displaying the data
 		 * in a HTML table or a JavaScript structure. Requires the
@@ -143,18 +156,6 @@ Highcharts.setOptions({
 		 * @since 6.0.4
 		 */
 		useRowspanHeaders: true
-
-		/**
-		 * Export-data module required. Caption for the data table. Same as
-		 * chart title by default. Set to `false` to disable.
-		 *
-		 * @type {Boolean|String}
-		 * @since 6.0.4
-		 * @sample highcharts/export-data/multilevel-table
-		 *			Multiple table headers
-		 * @default undefined
-		 * @apioption exporting.tableCaption
-		 */
 	},
 	/**
 	 * @optionparent lang

js/modules/exporting.src.js
@@ -205,6 +205,19 @@ defaultOptions.navigation = {
 		 */
 
 		/**
+		 * The vertical offset of the button's position relative to its
+		 * `verticalAlign`.
+		 * 
+		 * @type {Number}
+		 * @sample {highcharts} highcharts/navigation/buttonoptions-verticalalign/ Buttons at lower right
+		 * @sample {highstock} highcharts/navigation/buttonoptions-verticalalign/ Buttons at lower right
+		 * @sample {highmaps} highcharts/navigation/buttonoptions-verticalalign/ Buttons at lower right
+		 * @default 0
+		 * @since 2.0
+		 * @apioption navigation.buttonOptions.y
+		 */
+
+		/**
 		 * The vertical alignment of the buttons. Can be one of "top", "middle"
 		 * or "bottom".
 		 * 
@@ -231,19 +244,6 @@ defaultOptions.navigation = {
 		 * @apioption navigation.buttonOptions.width
 		 */
 		width: 24
-
-		/**
-		 * The vertical offset of the button's position relative to its
-		 * `verticalAlign`.
-		 * 
-		 * @type {Number}
-		 * @sample {highcharts} highcharts/navigation/buttonoptions-verticalalign/ Buttons at lower right
-		 * @sample {highstock} highcharts/navigation/buttonoptions-verticalalign/ Buttons at lower right
-		 * @sample {highmaps} highcharts/navigation/buttonoptions-verticalalign/ Buttons at lower right
-		 * @default 0
-		 * @since 2.0
-		 * @apioption navigation.buttonOptions.y
-		 */
 	}
 };
 
@@ -1821,5 +1821,5 @@ Chart.prototype.callbacks.push(function (chart) {
 			chart.renderTo.parentNode.appendChild(pre);
 		};
 	}
-	// */
+	//*/
 });

js/modules/histogram.src.js
@@ -109,43 +109,6 @@ seriesType('histogram', 'column', {
 			' {series.name} <b>{point.y}</b><br/>'
 	}
 
-    /**
- 	 * A `histogram` series. If the [type](#series.histogram.type) option is not
-	 * specified, it is inherited from [chart.type](#chart.type).
-	 * 
-	 * For options that apply to multiple series, it is recommended to add
-	 * them to the [plotOptions.series](#plotOptions.series) options structure.
-	 * To apply to all series of this specific type, apply it to 
-	 * [plotOptions.histogram](#plotOptions.histogram).
-	 * 
-	 * @type {Object}
-	 * @since 6.0.0
-	 * @extends series,plotOptions.histogram
-	 * @excluding dataParser,dataURL,data
-	 * @product highcharts
-	 * @apioption series.histogram
-	 */
-   
-    /**
-	 * An integer identifying the index to use for the base series, or a string
-	 * representing the id of the series.
-	 *
-	 * @type {Number|String}
-	 * @default undefined
-	 * @apioption series.histogram.baseSeries
-	 */
-
-    /**
-	 * An array of data points for the series. For the `histogram` series type,
-	 * points are calculated dynamically. See
-	 * [histogram.baseSeries](#series.histogram.baseSeries).
-	 * 
-	 * @type {Array<Object|Array>}
-	 * @since 6.0.0
-	 * @extends series.column.data
-	 * @product highcharts
-	 * @apioption series.histogram.data
-	 */
 }, merge(derivedSeriesMixin, {
 	setDerivedData: function () {
 		var data = this.derivedData(
@@ -211,3 +174,41 @@ seriesType('histogram', 'column', {
 		);
 	}
 }));
+
+/**
+ * A `histogram` series. If the [type](#series.histogram.type) option is not
+ * specified, it is inherited from [chart.type](#chart.type).
+ * 
+ * For options that apply to multiple series, it is recommended to add
+ * them to the [plotOptions.series](#plotOptions.series) options structure.
+ * To apply to all series of this specific type, apply it to 
+ * [plotOptions.histogram](#plotOptions.histogram).
+ * 
+ * @type {Object}
+ * @since 6.0.0
+ * @extends series,plotOptions.histogram
+ * @excluding dataParser,dataURL,data
+ * @product highcharts
+ * @apioption series.histogram
+ */
+
+/**
+ * An integer identifying the index to use for the base series, or a string
+ * representing the id of the series.
+ *
+ * @type {Number|String}
+ * @default undefined
+ * @apioption series.histogram.baseSeries
+ */
+
+/**
+ * An array of data points for the series. For the `histogram` series type,
+ * points are calculated dynamically. See
+ * [histogram.baseSeries](#series.histogram.baseSeries).
+ * 
+ * @type {Array<Object|Array>}
+ * @since 6.0.0
+ * @extends series.column.data
+ * @product highcharts
+ * @apioption series.histogram.data
+ */

js/modules/no-data-to-display.src.js
@@ -51,6 +51,17 @@ defaultOptions.noData = {
 	 * @product highcharts highstock
 	 * @apioption noData.attr
 	 */
+	
+	/**
+	 * Whether to insert the label as HTML, or as pseudo-HTML rendered with
+	 * SVG.
+	 * 
+	 * @type {Boolean}
+	 * @default false
+	 * @since 4.1.10
+	 * @product highcharts highstock
+	 * @apioption noData.useHTML
+	 */
 
 	/**
 	 * The position of the no-data label, relative to the plot area.
@@ -98,17 +109,6 @@ defaultOptions.noData = {
 		 */
 		verticalAlign: 'middle'
 	}
-	
-	/**
-	 * Whether to insert the label as HTML, or as pseudo-HTML rendered with
-	 * SVG.
-	 * 
-	 * @type {Boolean}
-	 * @default false
-	 * @since 4.1.10
-	 * @product highcharts highstock
-	 * @apioption noData.useHTML
-	 */
 };
 
 /*= if (build.classic) { =*/
@@ -138,7 +138,7 @@ each([
 ], function (type) {
 	if (seriesTypes[type]) {
 		seriesTypes[type].prototype.hasData = function () {
-			return !!this.points.length; /* != 0 */
+			return !!this.points.length; // != 0
 		};
 	}
 });

js/modules/offline-exporting.src.js
@@ -386,7 +386,7 @@ Highcharts.downloadSVGLocal = function (
 		Highcharts.imageToDataUrl(
 			svgurl,
 			imageType,
-			{ /* args */ },
+			{},
 			scale,
 			function (imageURL) {
 				// Success

js/modules/oldie.src.js
@@ -666,8 +666,9 @@ if (!svg) {
 			var strokeElem = element.getElementsByTagName('stroke')[0] ||
 				createElement(this.renderer.prepVML(['<stroke/>']), null, null, element);
 			strokeElem[key] = value || 'solid';
-			this[key] = value; /* because changing stroke-width will change the dash length
-				and cause an epileptic effect */
+			// Because changing stroke-width will change the dash length and
+			// cause an epileptic effect
+			this[key] = value;
 		},
 		dSetter: function (value, key, element) {
 			var i,

commit b7945c8aed896a674f4c2d02e13eb74526946f8f

Regression since 791f593bfb, candlestick file didn't build because of wrong supercode.

.gitignore
@@ -70,3 +70,4 @@ samples/highcharts/common-js/webpack/demo.js
 
 /tmp/processed.api.json
 /package-lock.json
+/temp.js

js/parts/CandlestickSeries.js
@@ -37,7 +37,6 @@ var candlestickOptions = {
 	 * @product highstock
 	 * @apioption plotOptions.candlestick.upLineColor
 	 */
-	/*= } =*/
 
 	/**
 	 * @default ohlc
@@ -112,6 +111,7 @@ var candlestickOptions = {
 	 * @product highstock
 	 */
 	upColor: '${palette.backgroundColor}',
+	/*= } =*/
 
 	stickyTracking: true
 