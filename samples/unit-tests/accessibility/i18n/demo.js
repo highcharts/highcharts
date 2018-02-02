be7b1a352c1c16e08a0df07dc0323a6d3647b120
oysteinmoseng <oystein@highsoft.com>
2018-02-02 19:24:21 +0100

    Added langFormat and i18nFormat functions.

---
+++ samples/unit-tests/accessibility/i18n/demo.js
@@ -0,0 +1,173 @@
+QUnit.test('langFormat', function (assert) {
+    var chart = Highcharts.chart('container', {
+        lang: {
+            test: 'Test format',
+            parent: {
+                sublevel: {
+                    test2: 'Test format 2'
+                }
+            }
+        }
+    });
+    assert.strictEqual(
+        chart.langFormat('test'),
+        'Test format',
+        'Lang format takes simple key'
+    );
+    assert.strictEqual(
+        chart.langFormat('parent.sublevel.test2'),
+        'Test format 2',
+        'Lang format takes multilevel key'
+    );
+    assert.notOk(
+        chart.langFormat('parent.sublevel'),
+        'Non string result'
+    );
+    assert.notOk(
+        chart.langFormat('notexist.parent.sublevel'),
+        'Invalid keys 1'
+    );
+    assert.notOk(
+        chart.langFormat('test.notexist.notexisting'),
+        'Invalid keys 2'
+    );
+});
+
+QUnit.test('i18nFormat', function (assert) {
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'Test {substitute}',
+            { substitute: 'format' }
+        ),
+        'Test format',
+        'Simple substitution'
+    );
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'Test {substitute}',
+            { substitute: 18 }
+        ),
+        'Test 18',
+        'Simple substitution 2'
+    );
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'Test {substitute}{substitute}{substitute}{substitute}',
+            { substitute: 18 }
+        ),
+        'Test 18181818',
+        'Duplicate substitutions'
+    );
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'Test {substitute:.2f}',
+            { substitute: 3.1415 }
+        ),
+        'Test 3.14',
+        'Formatted substitution'
+    );
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'Test {arr[0]}, {arr[1]}.',
+            { arr: [0, 1, 2, 3, 4, 5] }
+        ),
+        'Test 0, 1.',
+        'Array indexing'
+    );
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'Test {arr[-1]}, {arr[-2]}, {arr[-3]}.',
+            { arr: [0, 1, 2, 3, 4, 5] }
+        ),
+        'Test 5, 4, 3.',
+        'Array reverse indexing'
+    );
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'Test {arr[10]}, {arr[-10]}.',
+            { arr: [0, 1, 2, 3, 4, 5] }
+        ),
+        'Test 5, 0.',
+        'Array overshoot indexing'
+    );
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'Test {substitute[3]}',
+            { substitute: 14 }
+        ),
+        'Test ',
+        'Indexing non-array'
+    );
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'Test {substitute[3]}',
+            { substitute: {} }
+        ),
+        'Test ',
+        'Indexing non-array 2'
+    );
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'Test {_each(arr)}',
+            { arr: [0, 1, 2, 3, 4, 5] }
+        ),
+        'Test 012345',
+        'Simple array each'
+    );
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'Test {!_each(arr)} {_each(arr)!}.',
+            { arr: [0, 1, 2, 3, 4, 5] }
+        ),
+        'Test !0!1!2!3!4!5 0!1!2!3!4!5!.',
+        'Array each pre or post'
+    );
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'Test {!!_each(arr)??}.',
+            { arr: [0, 1, 2, 3, 4, 5] }
+        ),
+        'Test !!0??!!1??!!2??!!3??!!4??!!5??.',
+        'Array each pre and post'
+    );
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'Test {!!_each(arr)??}.',
+            { arr: [0, 1, 2, 3, 4, 5] }
+        ),
+        'Test !!0??!!1??!!2??!!3??!!4??!!5??.',
+        'Array each pre and post'
+    );
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'Test {_each(arr, 3)}.',
+            { arr: [0, 1, 2, 3, 4, 5] }
+        ),
+        'Test 012.',
+        'Array each positive length'
+    );
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'Test {_each(arr, 30)}{asdf_each(arr, -30)asdf}.',
+            { arr: [0, 1, 2, 3, 4, 5] }
+        ),
+        'Test 012345.',
+        'Array each length overshoot'
+    );
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'Test {_each(arr, -2)}.',
+            { arr: [0, 1, 2, 3, 4, 5] }
+        ),
+        'Test 0123.',
+        'Array each negative length'
+    );
+    assert.strictEqual(
+        Highcharts.i18nFormat(
+            'We have {0._each(arr, -1), }and 0.{arr[-1]}.',
+            { arr: [0, 1, 2, 3, 4, 5] }
+        ),
+        'We have 0.0, 0.1, 0.2, 0.3, 0.4, and 0.5.',
+        'Array each combination'
+    );
+});
