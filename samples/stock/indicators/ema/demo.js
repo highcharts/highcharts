208249892857e5013ddffccd210836e21d5ab821
Sebastian Bochan <sebastian@blacklabel.pl>
2018-02-09 15:26:25 +0100

    Addition #7802, fixed wrong url in the demo.

--- samples/stock/indicators/ema/demo.js
+++ samples/stock/indicators/ema/demo.js
@@ -1,4 +1,4 @@
-$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-ohlc.json&callback=?', function (data) {
+$.getJSON('https://www.highcharts.com/samples/data/aapl-ohlc.json', function (data) {
 
     Highcharts.stockChart('container', {
 
