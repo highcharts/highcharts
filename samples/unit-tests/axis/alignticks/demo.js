696b48cf708a5c0254da4a49949e57f964ae309d
cosycode <32601113+cosycode@users.noreply.github.com>
2018-02-15 11:36:54 +0100

    Add test for issue #150

--- samples/unit-tests/axis/alignticks/demo.js
+++ samples/unit-tests/axis/alignticks/demo.js
@@ -27,3 +27,41 @@ QUnit.test('Align ticks on logarithmic axis (#6021)', function (assert) {
     );
 
 });
+QUnit.test('Align ticks on opposite axis (#150)', function (assert) {
+
+    var chart = Highcharts.chart('container', {
+
+        chart: {
+            height: 200,
+            width: 400
+        },
+
+        title: {
+            text: ''
+        },
+
+        series: [{
+            yAxis: 0,
+            data: [1, 2]
+        }, {
+            yAxis: 1,
+            data: [1, 2]
+        }],
+
+        yAxis: [{
+            opposite: true
+        }, {
+            min: -100,
+            max: 100
+        }]
+    });
+
+    var gridNodes1 = chart.yAxis[0].gridGroup.element.childNodes;
+    var gridNodes2 = chart.yAxis[1].gridGroup.element.childNodes;
+
+    assert.equal(
+        gridNodes1[gridNodes1.length - 1].getAttribute('d'),
+        gridNodes2[gridNodes2.length - 1].getAttribute('d'),
+        'Ticks are not aligned'
+    );
+});
