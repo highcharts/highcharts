dcfa338f2706548ecee458de3b7388af84d3a8d1
jon-a-nygaard <jon.arild@highsoft.com>
2018-02-19 08:17:33 +0100

    Fixed typo in documentation of Point.

--- js/parts/Point.js
+++ js/parts/Point.js
@@ -23,7 +23,7 @@ var Point,
 /**
  * The Point object. The point objects are generated from the `series.data` 
  * configuration objects or raw numbers. They can be accessed from the
- * `Series.points` array. Other ways to instaniate points are through {@link
+ * `Series.points` array. Other ways to instantiate points are through {@link
  * Highcharts.Series#addPoint} or {@link Highcharts.Series#setData}.
  *
  * @class
