17944393a8222d29792e1c0b313117ae7e2dcd33
Torstein Hønsi <torstein@highsoft.com>
2018-03-05 12:31:43 +0100

    Samples: Linted

--- samples/unit-tests/legend/legend-items-height/demo.js
+++ samples/unit-tests/legend/legend-items-height/demo.js
@@ -1,23 +1,24 @@
 
 
 QUnit.test(
-	'Legend item should have positive height (#6519)',
-	function (assert) {
-		var chart = Highcharts.chart('container', {
-			series: [{
-				data: [1, 3, 2, 4],
-				name: ' '
-			}, {
-				data: [2, 4, 3, 5],
-				name: ' '
-			}],
-			legend: {
-				layout: 'vertical'
-			}
-		});
-		assert.notEqual(
-			chart.legend.allItems[0]._legendItemPos[1], chart.legend.allItems[1]._legendItemPos[1],
-			'Legend item has positive height'
-		);
-	}
+    'Legend item should have positive height (#6519)',
+    function (assert) {
+        var chart = Highcharts.chart('container', {
+            series: [{
+                data: [1, 3, 2, 4],
+                name: ' '
+            }, {
+                data: [2, 4, 3, 5],
+                name: ' '
+            }],
+            legend: {
+                layout: 'vertical'
+            }
+        });
+        assert.notEqual(
+            chart.legend.allItems[0]._legendItemPos[1], // eslint-disable-line no-underscore-dangle
+            chart.legend.allItems[1]._legendItemPos[1], // eslint-disable-line no-underscore-dangle
+            'Legend item has positive height'
+        );
+    }
 );
