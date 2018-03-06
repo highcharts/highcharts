c66f678d6d0ce6dbb98419812ddf8b7bf9900bb8
cosycode <32601113+cosycode@users.noreply.github.com>
2018-03-06 15:18:29 +0100

    highcharts/chart/reflow-false -> unit-tests/chart/reflow

--- samples/unit-tests/chart/reflow/demo.js
+++ samples/unit-tests/chart/reflow/demo.js
@@ -1,19 +1,82 @@
 /* eslint func-style:0 */
 
+QUnit.test('Set reflow to false', function (assert) {
+
+    var chart = Highcharts
+        .chart('container', {
+            chart: {
+                reflow: false
+            },
+            title: {
+                text: 'Chart reflow is set to false'
+            },
+
+            xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+
+            series: [{
+                data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+            }]
+        }),
+        chartWidth = chart.chartWidth,
+        containerWidth = $('#container').width(),
+        done = assert.async();
+
+    assert.strictEqual(
+        typeof chartWidth,
+        'number',
+        'Width should be set'
+    );
+
+    var clock = null;
+
+    try {
+
+        clock = lolexInstall(chart);
+
+        // Change the container size and trigger window resize to make the chart resize
+        $('#container').width(300);
+        $(window).resize();
+
+        setTimeout(function () {
+            assert.notEqual(
+                containerWidth,
+                $('#container').width(),
+                'Container width should change'
+            );
+            assert.strictEqual(
+                chart.chartWidth,
+                chartWidth,
+                'Chart width should not change'
+            );
+            done();
+        }, 200);
+
+        lolexRunAndUninstall(clock);
+
+    } finally {
+
+        lolexUninstall(clock);
+        $('#container').width(containerWidth);
+
+    }
+});
 
 QUnit.test('Reflow height only (#6968)', function (assert) {
-    var chart = Highcharts.chart('container', {
-        chart: {
-            animation: false,
-            width: 500
-        },
-        series: [{
-            type: 'column',
-            data: [1, 3, 2, 4]
-        }]
-    });
 
-    var container = document.getElementById('container');
+    var chart = Highcharts
+        .chart('container', {
+            chart: {
+                animation: false,
+                width: 500
+            },
+            series: [{
+                type: 'column',
+                data: [1, 3, 2, 4]
+            }]
+        }),
+        container = document.getElementById('container');
     assert.strictEqual(
         chart.chartHeight,
         400,
