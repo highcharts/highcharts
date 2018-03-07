3024e50a7e7a0877ba671d03f8179ca99155e5aa
cosycode <32601113+cosycode@users.noreply.github.com>
2018-03-07 12:38:13 +0100

    Reduced amount of series data

--- samples/highcharts/demo/spline-irregular-time/demo.js
+++ samples/highcharts/demo/spline-irregular-time/demo.js
@@ -44,44 +44,6 @@ Highcharts.chart('container', {
     // of 1970/71 in order to be compared on the same x axis. Note
     // that in JavaScript, months start at 0 for January, 1 for February etc.
     series: [{
-        name: "Winter 2012-2013",
-        data: [
-            [Date.UTC(1970, 9, 20), 0],
-            [Date.UTC(1970, 10,  4), 0.28],
-            [Date.UTC(1970, 10,  9), 0.25],
-            [Date.UTC(1970, 10, 27), 0.2],
-            [Date.UTC(1970, 11,  2), 0.28],
-            [Date.UTC(1970, 11, 26), 0.28],
-            [Date.UTC(1970, 11, 29), 0.47],
-            [Date.UTC(1971, 0, 11), 0.79],
-            [Date.UTC(1971, 0, 26), 0.72],
-            [Date.UTC(1971, 1,  3), 1.02],
-            [Date.UTC(1971, 1, 11), 1.12],
-            [Date.UTC(1971, 1, 25), 1.2],
-            [Date.UTC(1971, 2, 11), 1.18],
-            [Date.UTC(1971, 3, 10), 1.19],
-            [Date.UTC(1971, 3, 30), 1.85],
-            [Date.UTC(1971, 4,  4), 2.22],
-            [Date.UTC(1971, 4, 18), 1.15],
-            [Date.UTC(1971, 5,  2), 0]
-        ]
-    }, {
-        name: "Winter 2013-2014",
-        data: [
-            [Date.UTC(1970, 9, 29), 0],
-            [Date.UTC(1970, 10,  9), 0.4],
-            [Date.UTC(1970, 11,  1), 0.25],
-            [Date.UTC(1971, 0,  1), 1.66],
-            [Date.UTC(1971, 0, 10), 1.8],
-            [Date.UTC(1971, 1, 19), 1.76],
-            [Date.UTC(1971, 2, 25), 2.62],
-            [Date.UTC(1971, 3, 18), 2.41],
-            [Date.UTC(1971, 3, 29), 2.05],
-            [Date.UTC(1971, 4, 13), 1.7],
-            [Date.UTC(1971, 4, 23), 1.1],
-            [Date.UTC(1971, 5,  9), 0]
-        ]
-    }, {
         name: "Winter 2014-2015",
         data: [
             [Date.UTC(1970, 10, 25), 0],
