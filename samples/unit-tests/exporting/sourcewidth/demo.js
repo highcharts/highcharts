6243ae5edad8189185db51cb971c83cc3a2e183d
cosycode <32601113+cosycode@users.noreply.github.com>
2018-03-09 15:34:02 +0100

    highcharts/exporting/sourcewidth -> unit-tests/exporting/sourcewidth

--- samples/unit-tests/exporting/sourcewidth/demo.js
+++ samples/unit-tests/exporting/sourcewidth/demo.js
@@ -1,5 +1,33 @@
+
 QUnit.test('Exported chart sourceWidth and sourceHeight', function (assert) {
-    var chart = Highcharts.charts[0],
+
+    var chart = Highcharts
+        .chart('container', {
+            title: {
+                text: 'Highcharts sourceWidth and sourceHeight demo'
+            },
+            subtitle: {
+                text: 'The on-screen chart is 600x400.<br/>The exported chart is 800x400<br/>(sourceWidth and sourceHeight multiplied by scale)',
+                floating: true,
+                align: 'left',
+                x: 60,
+                y: 50
+            },
+            xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+            series: [{
+                data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+            }],
+            exporting: {
+                sourceWidth: 400,
+                sourceHeight: 200,
+                // scale: 2 (default)
+                chartOptions: {
+                    subtitle: null
+                }
+            }
+        }),
         done = assert.async();
 
     Highcharts.post = function (url, data) {
@@ -50,4 +78,5 @@ QUnit.test('Exported chart sourceWidth and sourceHeight', function (assert) {
     };
 
     chart.exportChart();
+
 });
