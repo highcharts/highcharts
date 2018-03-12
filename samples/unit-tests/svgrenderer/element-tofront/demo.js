f4c57b2911da12139d575653937d74408c57f0fb
cosycode <32601113+cosycode@users.noreply.github.com>
2018-03-12 15:10:47 +0100

    .../members/element-tofront -> unit-tests/svgrenderer/element-tofront

--- samples/unit-tests/svgrenderer/element-tofront/demo.js
+++ samples/unit-tests/svgrenderer/element-tofront/demo.js
@@ -1,5 +1,40 @@
-/* global circ, rect */
+
 QUnit.test('Element to front', function (assert) {
+
+    var renderer,
+        rect,
+        circ;
+
+
+    renderer = new Highcharts.Renderer(
+        $('#container')[0],
+        400,
+        300
+    );
+
+    rect = renderer.rect(100, 100, 100, 100, 5)
+        .attr({
+            'stroke-width': 2,
+            stroke: 'red',
+            fill: 'yellow'
+        })
+        .add();
+
+    circ = renderer.circle(200, 200, 50)
+        .attr({
+            'stroke-width': 2,
+            stroke: 'red',
+            fill: 'green'
+        })
+        .add();
+
+    rect.on('click', function () {
+        rect.toFront();
+    });
+
+    circ.on('click', function () {
+        circ.toFront();
+    });
 
     function getIndex(element) {
         var i = 0,
