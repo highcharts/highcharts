d415d64d2b385cf7a18d556b651b607777d4b5b2
cosycode <32601113+cosycode@users.noreply.github.com>
2018-03-12 16:06:58 +0100

    highcharts/members/series-select -> unit-tests/series/select

--- samples/unit-tests/series/select/demo.js
+++ samples/unit-tests/series/select/demo.js
@@ -1,4 +1,18 @@
+
 QUnit.test('Select and unselect updates checkbox', function (assert) {
+
+    Highcharts.chart('container', {
+        plotOptions: {
+            series: {
+                showCheckbox: true
+            }
+        },
+        series: [{
+            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+        }, {
+            data: [129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4, 29.9, 71.5, 106.4]
+        }]
+    });
 
     var series = Highcharts.charts[0].series[0];
 
@@ -22,6 +36,5 @@ QUnit.test('Select and unselect updates checkbox', function (assert) {
         false,
         'Checked'
     );
-
 
 });
