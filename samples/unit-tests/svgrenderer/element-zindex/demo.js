d2336a87bfd92a28f83bcc64eb74cce11fd321bf
cosycode <32601113+cosycode@users.noreply.github.com>
2018-03-12 16:00:27 +0100

    .../members/element-zindex -> unit-tests/svgrenderer/element-zindex

--- samples/unit-tests/svgrenderer/element-zindex/demo.js
+++ samples/unit-tests/svgrenderer/element-zindex/demo.js
@@ -1,5 +1,68 @@
-/* global circles */
+
 QUnit.test('Element Z index', function (assert) {
+
+    var renderer,
+        circles = [],
+        group2;
+
+
+    renderer = new Highcharts.Renderer(
+        $('#container')[0],
+        450,
+        300
+    );
+
+    circles[0] = renderer.circle(200, 200, 60)
+        .attr({
+            fill: 'green',
+            zIndex: 8
+        })
+        .add();
+    circles[1] = renderer.circle(250, 200, 60)
+        .attr({
+            fill: 'yellow',
+            zIndex: 6
+        })
+        .add();
+    circles[2] = renderer.circle(300, 200, 60)
+        .attr({
+            fill: 'blue',
+            zIndex: 4
+        })
+        .add();
+    circles[3] = renderer.circle(350, 200, 60)
+        .attr({
+            fill: 'red',
+            zIndex: 2
+        })
+        .add();
+
+    group2 = renderer.g().add();
+
+    circles[4] = renderer.circle(200, 100, 60)
+        .attr({
+            fill: 'pink',
+            zIndex: undefined
+        })
+        .add(group2);
+    circles[5] = renderer.circle(250, 100, 60)
+        .attr({
+            fill: 'black',
+            zIndex: -1
+        })
+        .add(group2);
+    circles[6] = renderer.circle(300, 100, 60)
+        .attr({
+            fill: 'grey',
+            zIndex: -2
+        })
+        .add(group2);
+    circles[7] = renderer.circle(350, 100, 60)
+        .attr({
+            fill: 'orange',
+            zIndex: 10
+        })
+        .add(group2);
 
     function getIndex(element) {
         var i = 0,
@@ -11,7 +74,6 @@ QUnit.test('Element Z index', function (assert) {
             i++;
         }
     }
-
 
     assert.strictEqual(
         typeof getIndex(circles[1].element),
