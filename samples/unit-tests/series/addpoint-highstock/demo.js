b9afd78bd7290da754a1fed341c107e77df7af8e
cosycode <32601113+cosycode@users.noreply.github.com>
2018-03-14 13:04:08 +0100

    stock/members/series-addpoint -> unit-tests/series/addpoint-highstock

--- samples/unit-tests/series/addpoint-highstock/demo.js
+++ samples/unit-tests/series/addpoint-highstock/demo.js
@@ -1,4 +1,83 @@
+
 QUnit.test('Add point without shift', function (assert) {
+
+    var chart = Highcharts.stockChart('container', {
+            rangeSelector: {
+                selected: 1
+            },
+            series: [{
+                name: 'USD to EUR',
+                data: usdeur.splice(0, 500)
+            }]
+        }),
+        maxX;
+
+    function add100() {
+        var i = 0,
+            series = chart.series[0],
+            data = usdeur.splice(0, 100);
+
+        maxX = data[data.length - 1][0];
+        for (i; i < data.length; i += 1) {
+            series.addPoint(data[i], false);
+        }
+        chart.redraw();
+    }
+
+    chart.setSize(800, 300, false);
+
+    assert.strictEqual(
+        chart.series[0].data.length,
+        500,
+        'Start data length'
+    );
+
+    // Add 100 points
+    add100();
+
+    assert.strictEqual(
+        chart.xAxis[0].getExtremes().max,
+        maxX,
+        'Should stick to max'
+    );
+
+    assert.strictEqual(
+        chart.series[0].options.data.length,
+        600,
+        'Should have new data length'
+    );
+
+    // Move viewed area left
+    chart.xAxis[0].setExtremes(
+        chart.series[0].xData[400],
+        chart.series[0].xData[500]
+    );
+
+    // Once the navigator is disconnected from the max, it should stay after
+    // adding points.
+    var minBefore = chart.xAxis[0].min,
+        maxBefore = chart.xAxis[0].max;
+
+    add100();
+    assert.strictEqual(
+        chart.series[0].options.data.length,
+        700,
+        'Should have new data length'
+    );
+    assert.strictEqual(
+        chart.xAxis[0].min,
+        minBefore,
+        'Min should be unchanged'
+    );
+    assert.strictEqual(
+        chart.xAxis[0].max,
+        maxBefore,
+        'Max should be unchanged'
+    );
+
+});
+
+QUnit.test('Add point with shift', function (assert) {
     var chart = Highcharts
         .stockChart('container', {
             chart: {
