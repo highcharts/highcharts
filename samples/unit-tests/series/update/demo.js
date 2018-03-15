db5594d315c3f374b19e1c2da70beca9467c48d8
cosycode <32601113+cosycode@users.noreply.github.com>
2018-03-15 09:57:17 +0100

    unit-tests/series/update <-> unit-tests/series/update-highstock

--- samples/unit-tests/series/update/demo.js
+++ samples/unit-tests/series/update/demo.js
@@ -1,369 +1,4 @@
 
-QUnit.test(
-    'Navigator series\' should keep its position in series array, ' +
-    'even after series.update()',
-    function (assert) {
-        var chart = Highcharts
-            .stockChart('container', {
-                series: [{
-                    data: [1, 2, 3],
-                    id: '1'
-                }, {
-                    data: [1, 2, 3],
-                    id: '2'
-                }, {
-                    data: [1, 2, 3],
-                    id: '3'
-                }]
-            }),
-            initialIndexes = chart.series.map(function (s) {
-                return s.options.id;
-            }),
-            afterUpdateIndexes;
-
-
-        Highcharts.each(chart.series, function (s, i) {
-            s.update({
-                name: 'Name ' + i
-            }, false);
-        });
-        chart.redraw();
-
-        afterUpdateIndexes = chart.series.map(function (s) {
-            return s.options.id;
-        });
-
-        assert.deepEqual(
-            initialIndexes,
-            afterUpdateIndexes,
-            'Correct zIndexes after update'
-        );
-    }
-);
-
-QUnit.test('Updating zIndex (#3380)', function (assert) {
-    var chart = Highcharts.chart('container', {
-        title: {
-            text: null
-        },
-
-        plotOptions: {
-            series: {
-                lineWidth: 5
-            }
-        },
-
-        series: [{
-            type: 'column',
-            data: [1, 2],
-            color: 'blue',
-            zIndex: 3
-        }, {
-            data: [2, 1],
-            color: 'yellow',
-            zIndex: 2
-        }]
-    });
-
-    assert.ok(
-        chart.seriesGroup.element.childNodes[0] === chart.series[1].group.element,
-        'Yellow should be below initially'
-    );
-    assert.ok(
-        chart.seriesGroup.element.childNodes[2] === chart.series[0].group.element,
-        'Blue should be on top initially'
-    );
-
-    chart.series[0].update({
-        zIndex: 1
-    });
-
-    assert.ok(
-        chart.seriesGroup.element.childNodes[0] === chart.series[0].group.element,
-        'Yellow should be on top after update'
-    );
-    assert.ok(
-        chart.seriesGroup.element.childNodes[2] === chart.series[1].group.element,
-        'Blue should be below after update'
-    );
-});
-
-
-
-QUnit.test('Update without altering zIndex (#7397)', function (assert) {
-    var chart = Highcharts.chart('container', {
-        title: {
-            text: null
-        },
-
-        plotOptions: {
-            series: {
-                lineWidth: 5
-            }
-        },
-
-        series: [{
-            type: 'column',
-            data: [1, 2],
-            color: 'blue'
-        }, {
-            data: [2, 1],
-            color: 'yellow'
-        }]
-    });
-
-    assert.ok(
-        chart.seriesGroup.element.childNodes[0] === chart.series[0].group.element,
-        'Yellow should be on top initially'
-    );
-    assert.ok(
-        chart.seriesGroup.element.childNodes[2] === chart.series[1].group.element,
-        'Blue should be below initially'
-    );
-
-    chart.series[0].update({
-        type: 'spline'
-    });
-
-    assert.ok(
-        chart.seriesGroup.element.childNodes[0] === chart.series[0].group.element,
-        'Yellow should be on top after update'
-    );
-    assert.ok(
-        chart.seriesGroup.element.childNodes[2] === chart.series[1].group.element,
-        'Blue should be below after update'
-    );
-});
-
-QUnit.test(
-    'Updating types, new type lost after second update (#2322)',
-    function (assert) {
-        var data = [
-            [0, 1, 2, 3, 4]
-        ];
-
-        var chart = Highcharts.chart('container', {
-            series: [{
-                type: 'candlestick',
-                data: [
-                    [0, 4.11, 4.12, 4.50, 4.07]
-                ]
-            }]
-        });
-
-        assert.strictEqual(
-            chart.series[0].pointArrayMap.toString(),
-            'open,high,low,close',
-            'OHLC point array map'
-        );
-
-        // Update type and data at the same time
-        chart.series[0].update({
-            data: data,
-            type: 'line'
-        }, true);
-
-        assert.strictEqual(
-            chart.series[0].pointArrayMap,
-            undefined,
-            'No point array map on base Series'
-        );
-
-        // Repeat: Update type and data at the same time
-        chart.series[0].update({
-            data: data,
-            type: 'line'
-        }, true);
-
-        assert.strictEqual(
-            chart.series[0].pointArrayMap,
-            undefined,
-            'No point array map on base Series'
-        );
-
-    }
-);
-
-QUnit.test('Updating and mouse interaction', function (assert) {
-
-    assert.expect(0);
-    var chart = Highcharts.chart('container', {
-
-        chart: {
-            type: 'column'
-        },
-        series: [{
-            data: [[0, 10], [1, 19], [2, 8], [3, 24], [4, 67]],
-            events: {
-                mouseOver: function () {
-                    this.update({ dataLabels: { enabled: true } });
-                },
-                mouseOut: function () {
-                    this.update({ dataLabels: { enabled: false } });
-                }
-            }
-        }]
-    });
-
-    chart.series[0].points[0].onMouseOver();
-});
-
-QUnit.test(
-    'Udating color index, class name should change',
-    function (assert) {
-        var chart = Highcharts.chart('container', {
-
-            title: {
-                text: 'Color index'
-            },
-
-            series: [{
-                type: 'area',
-                data: [1, 3, 2, 4]
-            }]
-
-        });
-        var s = chart.series[0];
-
-        assert.notEqual(
-            s.group.element.getAttribute('class').indexOf('highcharts-color-0'),
-            -1,
-            'Correct class'
-        );
-
-        s.update({ colorIndex: 5 });
-
-        assert.strictEqual(
-            s.group.element.getAttribute('class').indexOf('highcharts-color-0'),
-            -1,
-            'Original color class gone'
-        );
-
-        assert.notEqual(
-            s.group.element.getAttribute('class').indexOf('highcharts-color-5'),
-            -1,
-            'New color class added'
-        );
-
-    }
-);
-
-QUnit.test(
-    'Series.update and setData',
-    function (assert) {
-        var chart = Highcharts.chart('container', {
-
-            chart: {
-                type: 'area'
-            },
-
-            plotOptions: {
-                area: {
-                    stacking: true
-                }
-            },
-
-            series: [{
-                data: [1, 2, 3, 4, null, null]
-            }, {
-                data: [1, 2, 3, 4, null, null]
-            }]
-
-        });
-
-        chart.series[0].points[0].kilroyWasHere = true;
-
-        chart.series[0].update({
-            data: [4, 3, 2, 1, null, null]
-        });
-
-        assert.strictEqual(
-            chart.series[0].points[0].kilroyWasHere,
-            true,
-            'Original point item is preserved'
-        );
-
-        chart.series[0].setData([1, 2, 3, 4, 5, null], false);
-        chart.series[1].setData([1, 2, 3, 4, 5, null], false);
-        chart.redraw();
-
-        assert.strictEqual(
-            chart.series[0].graph.element.getAttribute('d').lastIndexOf('M'),
-            0,
-            'Graph is continuous (#7326)'
-        );
-
-    }
-);
-
-QUnit.test(
-    'Navigator series\' do not allow linkeTo (#6734).',
-    function (assert) {
-        var chart = Highcharts.stockChart('container', {
-            series: [{
-                data: [1, 2, 1],
-                id: '1'
-            }, {
-                data: [1, 3, 1],
-                id: '2',
-                linkedTo: '1',
-                showInNavigator: true
-            }]
-        });
-
-        assert.deepEqual(
-            chart.series[2].options.linkedTo,
-            null,
-            'No linkedTo for navigator series'
-        );
-        assert.deepEqual(
-            chart.series[3].options.linkedTo,
-            null,
-            'No linkedTo in navigator series based on series with linkedTo'
-        );
-
-        chart.series[0].update({
-            type: 'spline'
-        });
-
-        assert.deepEqual(
-            chart.series[2].options.linkedTo,
-            null,
-            'No linkedTo for navigator series'
-        );
-        assert.deepEqual(
-            chart.series[3].options.linkedTo,
-            null,
-            'No linkedTo in navigator series based on series with linkedTo'
-        );
-    }
-);
-
-QUnit.test('Setting showInLegend dynamically', function (assert) {
-    var s = Highcharts.chart('container', {
-        series: [{
-            showInLegend: false
-        }]
-    }).series[0];
-
-    s.update({
-        pointStart: 100
-    });
-
-    s.update({
-        data: [1, 2, 3, 4],
-        showInLegend: false
-    });
-
-    assert.deepEqual(
-        s.points.map(function (p) {
-            return p.x;
-        }),
-        [100, 101, 102, 103],
-        'Points should start from 100 (#7933)'
-    );
-});
-
 QUnit.test('Series.update', function (assert) {
 
     var chart = Highcharts.chart('container', {
@@ -371,11 +6,9 @@ QUnit.test('Series.update', function (assert) {
             categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
             showEmpty: false
         },
-
         yAxis: {
             showEmpty: false
         },
-
         series: [{
             allowPointSelect: true,
             data: [ // use names for display in pie data labels
@@ -601,6 +234,299 @@ QUnit.test('Series.update', function (assert) {
         chart.series[0].points[8].sliced,
         true,
         'Sliced slice'
+    );
+
+});
+
+QUnit.test('Series.update and mouse interaction', function (assert) {
+
+    var chart = Highcharts.chart('container', {
+        chart: {
+            type: 'column'
+        },
+        plotOptions: {
+            series: {
+                point: {
+                    events: {
+                        mouseOver: function () {
+                            this.update({
+                                dataLabels: { enabled: true }, events: {
+                                    mouseOut: function () {
+                                        this.update({ dataLabels: { enabled: false } });
+                                    }
+                                }
+                            });
+                        },
+                        mouseOut: function () {
+                            this.update({ dataLabels: { enabled: false } });
+                        }
+                    }
+                }
+            }
+        },
+        series: [{
+            data: [[0, 10], [1, 19], [2, 8], [3, 24], [4, 67]]
+        }]
+    });
+
+    chart.series[0].points[0].onMouseOver();
+    assert.strictEqual(
+        chart.series[0].points[0].dataLabels &&
+        chart.series[0].points[0].dataLabels.enabled,
+        true,
+        'Data labels should be enabled'
+    );
+
+    chart.series[0].onMouseOut();
+    assert.notEqual(
+        chart.series[0].points[0].dataLabels &&
+        chart.series[0].points[0].dataLabels.enabled,
+        true,
+        'Data labels should not be enabled'
+    );
+
+});
+
+QUnit.test('Series.update and setData', function (assert) {
+
+    var chart = Highcharts.chart('container', {
+        chart: {
+            type: 'area'
+        },
+        plotOptions: {
+            area: {
+                stacking: true
+            }
+        },
+        series: [{
+            data: [1, 2, 3, 4, null, null]
+        }, {
+            data: [1, 2, 3, 4, null, null]
+        }]
+    });
+
+    chart.series[0].points[0].kilroyWasHere = true;
+
+    chart.series[0].update({
+        data: [4, 3, 2, 1, null, null]
+    });
+
+    assert.strictEqual(
+        chart.series[0].points[0].kilroyWasHere,
+        true,
+        'Original point item is preserved'
+    );
+
+    chart.series[0].setData([1, 2, 3, 4, 5, null], false);
+    chart.series[1].setData([1, 2, 3, 4, 5, null], false);
+    chart.redraw();
+
+    assert.strictEqual(
+        chart.series[0].graph.element.getAttribute('d').lastIndexOf('M'),
+        0,
+        'Graph is continuous (#7326)'
+    );
+
+});
+
+QUnit.test('Series.update color index, class name should change', function (assert) {
+
+    var chart = Highcharts.chart('container', {
+        title: {
+            text: 'Color index'
+        },
+        series: [{
+            type: 'area',
+            data: [1, 3, 2, 4]
+        }]
+    });
+
+    var s = chart.series[0];
+
+    assert.notEqual(
+        s.group.element.getAttribute('class').indexOf('highcharts-color-0'),
+        -1,
+        'Correct class'
+    );
+
+    s.update({ colorIndex: 5 });
+
+    assert.strictEqual(
+        s.group.element.getAttribute('class').indexOf('highcharts-color-0'),
+        -1,
+        'Original color class gone'
+    );
+
+    assert.notEqual(
+        s.group.element.getAttribute('class').indexOf('highcharts-color-5'),
+        -1,
+        'New color class added'
+    );
+
+});
+
+QUnit.test('Series.update showInLegend dynamically', function (assert) {
+
+    var s = Highcharts.chart('container', {
+        series: [{
+            showInLegend: false
+        }]
+    }).series[0];
+
+    s.update({
+        pointStart: 100
+    });
+
+    s.update({
+        data: [1, 2, 3, 4],
+        showInLegend: false
+    });
+
+    assert.deepEqual(
+        s.points.map(function (p) {
+            return p.x;
+        }),
+        [100, 101, 102, 103],
+        'Points should start from 100 (#7933)'
+    );
+
+});
+
+QUnit.test('Series.update types, new type lost after second update (#2322)', function (assert) {
+
+    var data = [
+        [0, 1, 2, 3, 4]
+    ];
+
+    var chart = Highcharts.chart('container', {
+        series: [{
+            type: 'candlestick',
+            data: [
+                [0, 4.11, 4.12, 4.50, 4.07]
+            ]
+        }]
+    });
+
+    assert.strictEqual(
+        chart.series[0].pointArrayMap.toString(),
+        'open,high,low,close',
+        'OHLC point array map'
+    );
+
+    // Update type and data at the same time
+    chart.series[0].update({
+        data: data,
+        type: 'line'
+    }, true);
+
+    assert.strictEqual(
+        chart.series[0].pointArrayMap,
+        undefined,
+        'No point array map on base Series'
+    );
+
+    // Repeat: Update type and data at the same time
+    chart.series[0].update({
+        data: data,
+        type: 'line'
+    }, true);
+
+    assert.strictEqual(
+        chart.series[0].pointArrayMap,
+        undefined,
+        'No point array map on base Series'
+    );
+
+});
+
+QUnit.test('Series.update zIndex (#3380)', function (assert) {
+
+    var chart = Highcharts.chart('container', {
+        title: {
+            text: null
+        },
+        plotOptions: {
+            series: {
+                lineWidth: 5
+            }
+        },
+        series: [{
+            type: 'column',
+            data: [1, 2],
+            color: 'blue',
+            zIndex: 3
+        }, {
+            data: [2, 1],
+            color: 'yellow',
+            zIndex: 2
+        }]
+    });
+
+    assert.ok(
+        chart.seriesGroup.element.childNodes[0] === chart.series[1].group.element,
+        'Yellow should be below initially'
+    );
+    assert.ok(
+        chart.seriesGroup.element.childNodes[2] === chart.series[0].group.element,
+        'Blue should be on top initially'
+    );
+
+    chart.series[0].update({
+        zIndex: 1
+    });
+
+    assert.ok(
+        chart.seriesGroup.element.childNodes[0] === chart.series[0].group.element,
+        'Yellow should be on top after update'
+    );
+    assert.ok(
+        chart.seriesGroup.element.childNodes[2] === chart.series[1].group.element,
+        'Blue should be below after update'
+    );
+
+});
+
+QUnit.test('Series.update without altering zIndex (#7397)', function (assert) {
+
+    var chart = Highcharts.chart('container', {
+        title: {
+            text: null
+        },
+        plotOptions: {
+            series: {
+                lineWidth: 5
+            }
+        },
+        series: [{
+            type: 'column',
+            data: [1, 2],
+            color: 'blue'
+        }, {
+            data: [2, 1],
+            color: 'yellow'
+        }]
+    });
+
+    assert.ok(
+        chart.seriesGroup.element.childNodes[0] === chart.series[0].group.element,
+        'Yellow should be on top initially'
+    );
+    assert.ok(
+        chart.seriesGroup.element.childNodes[2] === chart.series[1].group.element,
+        'Blue should be below initially'
+    );
+
+    chart.series[0].update({
+        type: 'spline'
+    });
+
+    assert.ok(
+        chart.seriesGroup.element.childNodes[0] === chart.series[0].group.element,
+        'Yellow should be on top after update'
+    );
+    assert.ok(
+        chart.seriesGroup.element.childNodes[2] === chart.series[1].group.element,
+        'Blue should be below after update'
     );
 
 });
