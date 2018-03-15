c2bf643c73f7080bb27796c92c6a3b247e1d7ddb
Torstein Hønsi <torstein@highsoft.com>
2018-03-15 09:44:40 +0100

    Revert "Fixed #7991, treemap had wrong root on export."
    This reverts commit 90ed8f0e27182850f671445950264ebe8ba65674.

--- js/modules/treemap.src.js
+++ js/modules/treemap.src.js
@@ -1011,16 +1011,13 @@ seriesType('treemap', 'scatter', {
 	translate: function () {
 		var series = this,
 			options = series.options,
-			rootId = pick(series.rootNode, options.rootId, ''),
+			rootId = series.rootNode =
+				pick(series.rootNode, series.options.rootId, ''),
 			rootNode,
 			pointValues,
 			seriesArea,
 			tree,
 			val;
-
-		// Set rootId on series.userOptions to pick it up in exporting.
-		// Set rootId on series to pick it up on next translate.
-		series.userOptions.rootId = series.rootNode = rootId;
 
 		// Call prototype function
 		Series.prototype.translate.call(series);
