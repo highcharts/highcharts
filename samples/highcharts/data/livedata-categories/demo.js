e64413284cc0dff2fd81f00d107821144f99a485
Torstein Hønsi <torstein@highsoft.com>
2018-03-15 15:47:52 +0100

    Live data: Styling category demo.

--- samples/highcharts/data/livedata-categories/demo.js
+++ samples/highcharts/data/livedata-categories/demo.js
@@ -3,6 +3,23 @@ Highcharts.chart('container', {
     chart: {
         type: 'column'
     },
+
+    plotOptions: {
+        series: {
+            colorByPoint: true,
+            dataLabels: {
+                enabled: true,
+                format: '{point.y:.1f}',
+                inside: true
+            }
+        }
+    },
+
+    yAxis: {
+        max: 10,
+        allowDecimals: false
+    },
+
     title: {
         text: 'Live Data (CSV)'
     },
