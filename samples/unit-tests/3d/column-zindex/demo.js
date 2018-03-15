ebb5182853dd72cdb405705e5ad1f16ab888aae1
cosycode <32601113+cosycode@users.noreply.github.com>
2018-03-15 15:26:00 +0100

    issues/highcharts-4.1.10/4774 -> unit-tests/3d/column-zindex

--- samples/unit-tests/3d/column-zindex/demo.js
+++ samples/unit-tests/3d/column-zindex/demo.js
@@ -43,3 +43,35 @@ QUnit.test('Column zIndex calculation #5297', function (assert) {
         'zIndex is correct for column series'
     );
 });
+
+// Highcharts 4.1.10, Issue #4774: 3D column - disabled animation
+QUnit.test(
+    '3D column chart with disabled animation should properly set zIndexes for cuboids. (#4774)',
+    function (assert) {
+
+        var chart = new Highcharts.Chart({
+            chart: {
+                animation: false,
+                renderTo: 'container',
+                options3d: {
+                    enabled: true,
+                    alpha: 45,
+                    beta: 45
+                }
+            },
+            series: [{
+                animation: false,
+                data: [10, 20, 30, 40, 50, 10, 20, 10, 20, 30, 40, 50, 10, 20, 10, 20, 30, 40, 50, 10, 20, 10, 20, 30, 40, 50, 10, 20, 10, 20, 30, 40, 50, 10, 20],
+                type: "column",
+                depth: 100
+            }]
+        });
+
+
+        assert.ok(
+        chart.series[0].points[4].graphic.attr('zIndex') < chart.series[0].points[5].graphic.attr('zIndex'),
+        'Proper zIndex'
+        );
+
+    }
+);
