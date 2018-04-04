bba5d7b821f2088ecd9a49f9a771ac1d8edf4742
Torstein Hønsi <torstein@highsoft.com>
2018-04-04 13:37:49 +0200

    Added new option, `plotOptions.bubble.animationLimit`, to avoid low-performant animation on heavy bubble series by default. Closes #8099.

--- js/parts-more/BubbleSeries.js
+++ js/parts-more/BubbleSeries.js
@@ -49,6 +49,14 @@ seriesType('bubble', 'scatter', {
     },
 
     /**
+     * If there are more points in the series than the `animationLimit`, the
+     * animation won't run. Animation affects overall performance and doesn't
+     * work well with heavy data series.
+     * @since 6.1.0
+     */
+    animationLimit: 250,
+
+    /**
      * Whether to display negative sized bubbles. The threshold is given
      * by the [zThreshold](#plotOptions.bubble.zThreshold) option, and negative
      * bubbles can be visualized by setting
@@ -337,9 +345,10 @@ seriesType('bubble', 'scatter', {
      * Perform animation on the bubbles
      */
     animate: function (init) {
-        var animation = this.options.animation;
-
-        if (!init) { // run the animation
+        if (
+            !init &&
+            this.points.length < this.options.animationLimit // #8099
+        ) {
             each(this.points, function (point) {
                 var graphic = point.graphic,
                     animationTarget;
@@ -361,7 +370,7 @@ seriesType('bubble', 'scatter', {
                     });
 
                     // Run animation
-                    graphic.animate(animationTarget, animation);
+                    graphic.animate(animationTarget, this.options.animation);
                 }
             });
 
