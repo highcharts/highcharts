e027a04c1c2beb0305cf16dd074ddaae7a1080ed
cosycode <32601113+cosycode@users.noreply.github.com>
2018-04-06 16:48:50 +0200

    Improve unit-tests/series-line

--- samples/unit-tests/series-line/type/demo.js
+++ samples/unit-tests/series-line/type/demo.js
@@ -1,14 +1,21 @@
-QUnit.test('line-simple', function (assert) {
+QUnit.test('series-line/type', function (assert) {
 
     ChartTemplate.test('line-simple', {}, function (template) {
 
         var chart = template.chart,
-            options = chart.options;
+            options = chart.options,
+            series = chart.series[0];
 
         assert.strictEqual(
             options.chart.type,
             'line',
-            'Chart type should be "line".'
+            'Chart type option should be "line".'
+        );
+
+        assert.strictEqual(
+            series.type,
+            'line',
+            'Series type should be "line".'
         );
 
     }, assert.async());
