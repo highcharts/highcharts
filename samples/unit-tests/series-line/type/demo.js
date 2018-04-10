5dd781f1a6f23146296bafc1bd99a3bd3b44f565
cosycode <32601113+cosycode@users.noreply.github.com>
2018-04-10 16:21:05 +0200

    Update unit-tests/series-line

--- samples/unit-tests/series-line/type/demo.js
+++ samples/unit-tests/series-line/type/demo.js
@@ -18,6 +18,32 @@ QUnit.test('series-line/type', function (assert) {
             'Series type should be "line".'
         );
 
-    }, assert.async());
+    });
+
+    ChartTemplate.test('line-simple', {
+
+        chart: {
+            type: undefined
+        }
+
+    }, function (template) {
+
+        var chart = template.chart,
+            options = chart.options,
+            series = chart.series[0];
+
+        assert.strictEqual(
+            options.chart.type,
+            undefined,
+            'Chart type option should be undefined.'
+        );
+
+        assert.strictEqual(
+            series.type,
+            'line',
+            'Series type should be "line" as default.'
+        );
+
+    });
 
 });
