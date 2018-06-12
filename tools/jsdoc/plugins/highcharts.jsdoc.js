0a59ac816084735dc43482a67250fdc90b4a9772
sophiebremer <40056287+sophiebremer@users.noreply.github.com>
2018-06-12 16:40:30 +0200

    Preparation

--- tools/jsdoc/plugins/highcharts.jsdoc.js
+++ tools/jsdoc/plugins/highcharts.jsdoc.js
@@ -7,7 +7,7 @@
 
 "use strict";
 
-var hcRoot = __dirname + '/../../..';
+var hcRoot = process.cwd(); // __dirname + '/../../../..';
 
 var parseTag = require('jsdoc/tag/type').parse;
 
@@ -18,6 +18,12 @@ var colors = require('colors');
 var fs = require('fs');
 var getPalette = require('highcharts-assembler/src/process.js').getPalette;
 var path = require('path');
+var namespace = {
+    _meta: {
+        commit: '',
+        branch: ''
+    }
+}
 var options = {
     _meta: {
         commit: '',
@@ -50,119 +56,18 @@ function dumpOptions() {
     );
 }
 
-function createSitemaps() {
-    let manualBoost = {
-            'plotOptions.series': 100,
-        },
-        sitemapIndex = [],
-        sitemaps = {};
-    // Add function for option sitemaps
-    function addToSitemaps(node, boost, parentProducts) {
-        if (!node.doclet ||
-            !node.meta ||
-            !node.meta.fullname
-        ) {
-            return;
+function dumpNamespace() {
+    fs.writeFile(
+        'tree_namespace.json',
+        JSON.stringify(
+            namespace,
+            undefined,
+            '  '
+        ),
+        function () {
+            //console.log('Wrote tree_namespace!');
         }
-        boost = (manualBoost[node.meta.fullname] || boost);
-        let products = (parentProducts || node.doclet.products);
-        for (var i = 0, ie = products.length; i < ie; ++i) {
-            let product = products[i];
-            sitemaps[product] = (sitemaps[product] || [])
-            sitemaps[product].push(
-                '<url><loc>https://api.highcharts.com/' + product + '/' +
-                node.meta.fullname + '.html</loc><priority>' +
-                (boost / 100) +
-                '</priority></url>'
-            );
-        }
-        if (!node.children) {
-            return;
-        }
-        boost -= 5;
-        for (var child in node.children) {
-            addToSitemaps(node.children[child], boost, products);
-        }
-    }
-    // Directory function
-    function createDirectoriesSync(dirPath, mode, callback) {
-        try {
-            fs.mkdirSync(dirPath, mode);
-            callback && callback();
-        } catch (error) {
-            if (error.errno === 34) {
-                createDirectoriesSync(path.dirname(dirPath), mode, callback);
-                createDirectoriesSync(dirPath, mode, callback);
-            }
-        }
-    };
-    // Add Highcharts options
-    for (var option in options) {
-        if (options[option].doclet &&
-            options[option].doclet.products
-        ) {
-            addToSitemaps(options[option], 100,
-                options[option].doclet.products
-            );
-        } else {
-            addToSitemaps(options[option], 100, [
-                'highcharts', 'highstock', 'highmaps'
-            ]);
-        }
-    }
-    // Add Highcharts class reference to products
-    sitemaps['class-reference'] = (sitemaps['class-reference'] || []);
-    fs.readdirSync('build/api/class-reference').forEach(function (fileName) {
-        if (fileName.lastIndexOf('.html') != fileName.length - 5) {
-            return;
-        }
-        sitemaps['class-reference'].push(
-            '<url><loc>https://api.highcharts.com/class-reference/' + fileName +
-            '</loc><priority>0.75</priority></url>'
-        );
-    });
-    // Add Highcharts products to sitemap index
-    for (var product in sitemaps) {
-        sitemapIndex.push(
-            '<sitemap><loc>https://api.highcharts.com/' + product +
-            '/sitemap.xml</loc></sitemap>'
-        );
-    }
-    // Add Highcharts wrapper to sitemap index
-    sitemapIndex.push(
-        '<sitemap><loc>'+
-        'https://api.highcharts.com/ios/highcharts/sitemap.xml' +
-        '</loc></sitemap>'
     );
-    sitemapIndex.push(
-        '<sitemap><loc>'+
-        'https://api.highcharts.com/android/highcharts/sitemap.xml' +
-        '</loc></sitemap>'
-    );
-    // Write sitemaps
-    try {
-        createDirectoriesSync('build/api');
-        fs.writeFileSync(
-            'build/api/sitemap.xml',
-            '<?xml version="1.0" encoding="UTF-8"?>' +
-            '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' +
-            sitemapIndex.sort().join('') +
-            '</sitemapindex>'
-        );
-        for (var product in sitemaps) {
-            createDirectoriesSync('build/api/' + product);
-            fs.writeFileSync(
-                'build/api/' + product + '/sitemap.xml',
-                '<?xml version="1.0" encoding="UTF-8"?>' +
-                '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' +
-                sitemaps[product].sort().join('') +
-                '</urlset>'
-            );
-        }
-        //console.log('Wrote sitemaps!');
-    } catch (error) {
-        console.error(error);
-    }
 }
 
 function resolveBinaryExpression(node) {
@@ -415,6 +320,10 @@ console.log('    found series type', node.right.arguments[0].value, '- inherited
             } else {
                 logger.error('@optionparent is missing an argument');
             }
+        } else if ((e.comment || '').indexOf('@private') < 0) {
+            console.log(node.key && node.key.name);
+            console.log(Object.keys(node).sort());
+            console.log(JSON.stringify(node.key));
         }
     }
 }
@@ -787,7 +696,7 @@ before functional code for JSDoc to see them.`.yellow
     },
 
     parseComplete: function () {
-        options._meta.version = require(__dirname + '/../../../package.json').version;
+        options._meta.version = require(hcRoot  + '/package.json').version;
         options._meta.commit = exec('git rev-parse --short HEAD', {cwd: process.cwd()}).toString().trim();
         options._meta.branch = exec('git rev-parse --abbrev-ref HEAD', {cwd: process.cwd()}).toString().trim();
         options._meta.date = (new Date()).toString();
@@ -796,7 +705,7 @@ before functional code for JSDoc to see them.`.yellow
 
         function inferTypeForTree(obj) {
             inferType(obj);
-        
+
             if (obj.meta && obj.meta.filename) {
                 // Remove user-identifiable info in filename
                 obj.meta.filename = obj.meta.filename.substr(
@@ -874,6 +783,6 @@ Highcharts.chart('container', {
         // console.log(Object.keys(files));
 
         dumpOptions();
-        // createSitemaps();
+        dumpNamespace();
     }
 };
