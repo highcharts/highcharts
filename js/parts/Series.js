4d781c4fe7e263cfd5d7735343c3e293626d7f00
sophiebremer <40056287+sophiebremer@users.noreply.github.com>
2018-07-04 10:34:13 +0200

    Fix #8566

--- js/parts/Series.js
+++ js/parts/Series.js
@@ -1726,8 +1726,8 @@ H.Series = H.seriesType('line', null, { // base series options
          * declarative filter is designed for use when callback functions are
          * not available, like when the chart options require a pure JSON
          * structure or for use with graphical editors. For programmatic
-         * control, use the `formatter` instead, and return `false` to disable
-         * a single data label.
+         * control, use the `formatter` instead, and return `undefined` to
+         * disable a single data label.
          *
          * @example
          * filter: {
