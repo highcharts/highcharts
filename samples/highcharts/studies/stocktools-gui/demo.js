299b4e52af2c9cc6691e03fd41dd64597847032a
Sebastian Bochan <sebastian@blacklabel.pl>
2018-07-23 16:16:02 +0200

    Add current price indicator to demo.

--- samples/highcharts/studies/stocktools-gui/demo.js
+++ samples/highcharts/studies/stocktools-gui/demo.js
@@ -1,5 +1,17 @@
 Highcharts.stockChart('container', {
     series: [{
+        // line
+        showPrice: {
+            enabled: true,
+            color: 'red'
+        },
+        // label
+        priceIndicator: {
+            enabled: true,
+            label: {
+                enabled: true
+            }
+        },
         data: [1, 3, 3, 4, 3, 2]
     }]
 });
