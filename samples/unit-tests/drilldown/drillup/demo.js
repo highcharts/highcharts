b81cdad85edcece86b976e3ba94bc297ae0ed829
GardEngen <gardengen@gmail.com>
2018-08-10 10:54:38 +0200

    issues/highcharts-4.0.4/3544 -> unit-tests/drilldown/drillup

--- samples/unit-tests/drilldown/drillup/demo.js
+++ samples/unit-tests/drilldown/drillup/demo.js
@@ -106,3 +106,91 @@ QUnit.test('Drilling up left one column semi-opaque (#4667)', function (assert) 
 
     }
 });
+
+// Highcharts 4.0.4, Issue #3544
+// Drillup does not work when data are set dynamically
+QUnit.test('Drill up failed on top level (#3544)', function (assert) {
+
+    var chart = Highcharts.chart('container', {
+        chart: {
+            type: 'column',
+            renderTo: 'container',
+            animation: false
+        },
+        xAxis: {
+            type: 'category'
+        },
+
+        drilldown: {
+            animation: false,
+            series: [{
+                id: 'fruits',
+                name: 'Fruits',
+                data: [
+                ['Apples', 4],
+                ['Pears', 6]]
+            }]
+        },
+
+        series: [{
+            name: 'Overview',
+            colorByPoint: true,
+            id: 'top'
+        }]
+    });
+    chart.series[0].setData([{
+        name: 'Fruits',
+        y: 10,
+        drilldown: 'fruits'
+    }]);
+
+
+    var controller = new TestController(chart);
+
+    var columnCenterX = (chart.plotSizeX + chart.plotLeft) / 2,
+        columnCenterY = (chart.plotSizeY + chart.plotTop) / 2;
+
+    controller.moveTo(columnCenterX, columnCenterY);
+
+    assert.ok(
+        controller.getPosition().relatedTarget,
+        "Drilldown column is not visible"
+    );
+
+
+    controller.click();
+
+    assert.deepEqual(
+    chart.xAxis[0].names,
+    ['Apples', 'Pears'],
+    "Drilldown failed"
+    );
+    controller.moveTo(columnCenterX, columnCenterY);
+
+    var drillUpButton = chart.drillUpButton,
+        drillUpButtonX = drillUpButton.x - (drillUpButton.getBBox().width / 2),
+        drillUpButtonY = drillUpButton.y + (drillUpButton.getBBox().height / 2);
+
+    assert.notEqual(
+        drillUpButton,
+        undefined,
+        "Drill up button is undefined"
+    );
+
+    controller.moveTo(drillUpButtonX, drillUpButtonY);
+    controller.click();
+    controller.moveTo(columnCenterX, columnCenterY);
+
+    assert.deepEqual(
+        chart.xAxis[0].names,
+        ['Fruits'],
+        "Categories is not visible after drillUp"
+        );
+
+    assert.notEqual(
+        controller.getPosition().relatedTarget,
+        undefined,
+        "Column element is undefined after drillUp"
+    );
+});
+
