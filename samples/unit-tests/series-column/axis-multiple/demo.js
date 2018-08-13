8c102a95d4bf46c50804d80ba77d85598addc367
GardEngen <gardengen@gmail.com>
2018-08-13 17:17:38 +0200

    issues/highcharts-4.0.4/3648 (1) -> unit-tests/series-column/axis-multiple

---
+++ samples/unit-tests/series-column/axis-multiple/demo.js
@@ -0,0 +1,83 @@
+
+// Highcharts 4.0.4, Issue #3648
+// Datalabel position incorrect for data points when using multiple yAxis
+QUnit.test('Wrong datalabel position (#3648)', function (assert) {
+    var chart = Highcharts.chart('container', {
+        chart: {
+            type: 'column',
+            inverted: true
+        },
+        yAxis: [{
+            width: 200,
+            lineWidth: 2
+        }, {
+            left: 250,
+            width: 200,
+            offset: 0,
+            lineWidth: 2
+        }],
+        plotOptions: {
+            series: {
+                animation: false,
+                dataLabels: {
+
+                    enabled: true,
+                    inside: true,
+                    style: {
+                        fontWeight: 'bold',
+                        color: 'red'
+                    },
+                    format: '{series.name} - {y}'
+                }
+            }
+        },
+        series: [{
+            data: [11, 12, 13, 14]
+        }, {
+            data: [21, 22, 23, 24],
+            yAxis: 1
+        }]
+
+    });
+    var pointsFirstSeries = chart.series[0].points,
+        pointsSecondSeries = chart.series[1].points,
+        heightOfFirstSeries = chart.container.childNodes[0].childNodes[4].getBBox().height,
+        xPosForDataLabels = [],
+        columnHeights = [];
+
+    for (var i = 0; i < pointsFirstSeries.length; i++) {
+        columnHeights.push(pointsFirstSeries[i].graphic.getBBox().height);
+        xPosForDataLabels.push(pointsFirstSeries[i].dataLabel.x);
+    }
+
+    //Return false if a value of the first array is lower
+    //than the corresponding value of the second array
+    function compareValuesOfTwoArrays(arr1, arr2) {
+        for (var i = 0; i < arr1.length; i++) {
+            if (arr1[i] < arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    assert.ok(
+        compareValuesOfTwoArrays(columnHeights, xPosForDataLabels),
+        "The position of the datalabels for the first series is incorrect"
+    );
+
+    xPosForDataLabels = [];
+    columnHeights = [];
+    // Adding the first series container height to the columnHeights and
+    // xPosForDataLabels for the second series
+    for (var j = 0; j < pointsSecondSeries.length; j++) {
+        columnHeights.push(heightOfFirstSeries + pointsSecondSeries[j].graphic.getBBox().height);
+        xPosForDataLabels.push(heightOfFirstSeries + pointsSecondSeries[j].dataLabel.x);
+    }
+
+    assert.ok(
+        compareValuesOfTwoArrays(columnHeights, xPosForDataLabels),
+        "The position of the datalabels for the second series is incorrect"
+    );
+
+});
