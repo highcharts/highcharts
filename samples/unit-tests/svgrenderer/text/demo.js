82b7446a52a6c0ed61064e8d8cc341d1b104d599
GardEngen <gardengen@gmail.com>
2018-08-16 13:52:20 +0200

    issues/highcharts-4.0.4/3501 -> unit-tests/svgrenderer/text

--- samples/unit-tests/svgrenderer/text/demo.js
+++ samples/unit-tests/svgrenderer/text/demo.js
@@ -368,3 +368,46 @@ QUnit.test('Text height (#3842)', function (assert) {
     }
 
 });
+
+
+// Highcharts 4.0.4, Issue #3501
+// After adding a new style the text width is not respected
+QUnit.test('Adding new text style (#3501)', function (assert) {
+    var renderer;
+    try {
+
+        renderer = new Highcharts.Renderer(
+            document.getElementById('container'),
+            500,
+            300
+        );
+
+        var rect = renderer.rect(100, 100, 100, 100)
+            .attr({
+                'stroke-width': 1,
+                stroke: 'blue',
+                fill: 'none'
+            })
+            .add();
+
+        var txt = renderer.text('Initial text adapts to box width', 100, 120)
+            .css({
+                width: '100px'
+            })
+            .add();
+
+        txt.css({ fill: 'red' });
+
+        txt.attr({ text: 'After running .css once, the new text does not respect box width' });
+
+        var rectWidth = rect.element.getBBox().width,
+            textWidth = txt.element.getBBox().width;
+
+        assert.ok(
+            rectWidth > textWidth,
+            "The text width is not respected"
+        );
+    } finally {
+        renderer.destroy();
+    }
+});
