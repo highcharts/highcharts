e7145d99d1155e4940a8ccff6253ad68e86fae62
Paweł Fus <pawel.fus.88@gmail.com>
2018-08-22 15:39:07 +0200

    StockTools: Added tooltip positioner to the demo.

--- samples/highcharts/studies/stocktools-gui/demo.js
+++ samples/highcharts/studies/stocktools-gui/demo.js
@@ -6,6 +6,38 @@ $.getJSON('https://www.highcharts.com/samples/data/aapl-ohlc.json', function (da
                 align: 'left'
             }
         },
+        tooltip: {
+            shape: 'square',
+            headerShape: 'callout',
+            borderWidth: 0,
+            shadow: false,
+            positioner: function (width, height, point) {
+                var chart = this.chart,
+                    position;
+
+                if (point.isHeader) {
+                    position = {
+                        x: Math.max(
+                            // Left side limit
+                            chart.plotLeft,
+                            Math.min(
+                                point.plotX + chart.plotLeft - width / 2,
+                                // Right side limit
+                                chart.chartWidth - width - chart.marginRight
+                            )
+                        ),
+                        y: point.plotY
+                    };
+                } else {
+                    position = {
+                        x: point.series.chart.plotLeft,
+                        y: point.series.yAxis.top - chart.plotTop
+                    };
+                }
+
+                return position;
+            }
+        },
         series: [{
             type: 'ohlc',
             id: 'aapl',
