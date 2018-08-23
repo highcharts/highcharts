90a1d2de9b4a8c267832d038882d55579b597381
Torstein Hønsi <torstein@highsoft.com>
2018-08-23 15:34:50 +0200

    Fixed #8713, setting cropThreshold on a windbarb series broke the chart.

--- js/parts/Series.js
+++ js/parts/Series.js
@@ -3416,8 +3416,10 @@ H.Series = H.seriesType('line', null, { // base series options
 
             // only crop if it's actually spilling out
             } else if (
-                processedXData[0] < min ||
-                processedXData[dataLength - 1] > max
+                series.yData && (
+                    processedXData[0] < min ||
+                    processedXData[dataLength - 1] > max
+                )
             ) {
                 croppedData = this.cropData(
                     series.xData,
