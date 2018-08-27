887afd00c024617a4d985eaf0d02ba6aced0ec35
Paweł Fus <pawel.fus.88@gmail.com>
2018-08-27 12:34:05 +0200

    StockTools: Added volume to the demo.

--- samples/highcharts/studies/stocktools-gui/demo.js
+++ samples/highcharts/studies/stocktools-gui/demo.js
@@ -1,11 +1,45 @@
-$.getJSON('https://www.highcharts.com/samples/data/aapl-ohlc.json', function (data) {
+$.getJSON('https://www.highcharts.com/samples/data/aapl-ohlcv.json', function (data) {
+
+    // split the data set into ohlc and volume
+    var ohlc = [],
+        volume = [],
+        dataLength = data.length,
+        i = 0;
+
+    for (i; i < dataLength; i += 1) {
+        ohlc.push([
+            data[i][0], // the date
+            data[i][1], // open
+            data[i][2], // high
+            data[i][3], // low
+            data[i][4] // close
+        ]);
+
+        volume.push([
+            data[i][0], // the date
+            data[i][5] // the volume
+        ]);
+    }
 
     Highcharts.stockChart('container', {
-        yAxis: {
+        yAxis: [{
             labels: {
                 align: 'left'
+            },
+            height: '80%',
+            lineWidth: 2,
+            resize: {
+                enabled: true
             }
-        },
+        }, {
+            labels: {
+                align: 'left'
+            },
+            top: '80%',
+            height: '20%',
+            offset: 0,
+            lineWidth: 2
+        }],
         tooltip: {
             shape: 'square',
             headerShape: 'callout',
@@ -40,9 +74,15 @@ $.getJSON('https://www.highcharts.com/samples/data/aapl-ohlc.json', function (da
         },
         series: [{
             type: 'ohlc',
-            id: 'aapl',
+            id: 'aapl-ohlc',
             name: 'AAPL Stock Price',
-            data: data
+            data: ohlc
+        }, {
+            type: 'column',
+            id: 'aapl-volume',
+            name: 'AAPL Volume',
+            data: volume,
+            yAxis: 1
         }]
     });
 });
