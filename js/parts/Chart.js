1214704ca39c546e1c458a7845c7f04cb51760a2
sophiebremer <40056287+sophiebremer@users.noreply.github.com>
2018-08-29 12:19:15 +0200

    JSDoc-like any type

--- js/parts/Chart.js
+++ js/parts/Chart.js
@@ -1290,7 +1290,7 @@ extend(Chart.prototype, /** @lends Highcharts.Chart.prototype */ {
      *
      * @function Highcharts.Chart#reflow
      *
-     * @param  {any} e
+     * @param  {*} e
      *         Event arguments. Used primarily when the function is called
      *         internally as a response to window resize.
      *
