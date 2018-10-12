602935654146b7245e26e8fccf409115654b1dd1
sophiebremer <40056287+sophiebremer@users.noreply.github.com>
2018-10-12 14:33:38 +0200

    Docs: Fixed external links.

--- js/parts/Series.js
+++ js/parts/Series.js
@@ -57,13 +57,13 @@ var addEvent = H.addEvent,
 /**
  * This is the base series prototype that all other series types inherit from.
  * A new series is initialized either through the
- * {@link https://api.highcharts.com/highcharts/series|series}
+ * [series](https://api.highcharts.com/highcharts/series)
  * option structure, or after the chart is initialized, through
  * {@link Highcharts.Chart#addSeries}.
  *
  * The object can be accessed in a number of ways. All series and point event
  * handlers give a reference to the `series` object. The chart object has a
- * {@link Highcharts.Chart.series|series} property that is a collection of all
+ * {@link Highcharts.Chart#series|series} property that is a collection of all
  * the chart's series. The point objects and axis objects also have the same
  * reference.
  *
@@ -73,11 +73,11 @@ var addEvent = H.addEvent,
  *
  * Configuration options for the series are given in three levels. Options for
  * all series in a chart are given in the
- * {@link https://api.highcharts.com/highcharts/plotOptions.series|
- * plotOptions.series} object. Then options for all series of a specific type
- * are given in the plotOptions of that type, for example `plotOptions.line`.
- * Next, options for one single series are given in the series array, or as
- * arguements to `chart.addSeries`.
+ * [plotOptions.series](https://api.highcharts.com/highcharts/plotOptions.series)
+ * object. Then options for all series of a specific type are given in the
+ * plotOptions of that type, for example `plotOptions.line`. Next, options for
+ * one single series are given in the series array, or as arguements to
+ * `chart.addSeries`.
  *
  * The data in the series is stored in various arrays.
  *
