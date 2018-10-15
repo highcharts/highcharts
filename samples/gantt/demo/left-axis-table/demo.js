e1c2d61e39392cc6a8dca45c43ba215c6525cc6d
Torstein Hønsi <torstein@highsoft.com>
2018-10-15 15:43:47 +0200

    Samples: left axis table as Gantt chart.

--- samples/gantt/demo/left-axis-table/demo.js
+++ samples/gantt/demo/left-axis-table/demo.js
@@ -1,41 +1,16 @@
 
-// THE CHART
-Highcharts.chart('container', {
+Highcharts.ganttChart('container', {
 
     title: {
         text: 'Left Axis as Table'
     },
 
-    xAxis: [{
-        grid: {
-            enabled: true
-        },
-        type: 'datetime',
-        opposite: true,
-        tickInterval: 1000 * 60 * 60 * 24, // Day
-        labels: {
-            format: '{value:%E}'
-        },
-        min: Date.UTC(2017, 10, 17),
-        max: Date.UTC(2017, 11, 16)
-    }, {
-        grid: {
-            enabled: true
-        },
-        type: 'datetime',
-        opposite: true,
-        tickInterval: 1000 * 60 * 60 * 24 * 7, // Week
-        labels: {
-            format: '{value:Week %W}',
-            style: {
-                fontSize: '1.2em'
-            }
-        },
-        linkedTo: 0
-    }],
+    xAxis: {
+        tickPixelInterval: 70
+    },
 
     yAxis: {
-        reversed: true,
+        type: 'category',
         grid: {
             enabled: true,
             borderColor: 'rgba(0,0,0,0.3)',
@@ -65,7 +40,7 @@ Highcharts.chart('container', {
                 }
             }, {
                 labels: {
-                    format: '{point.x:%e. %b}'
+                    format: '{point.start:%e. %b}'
                 },
                 title: {
                     text: 'Start date'
@@ -76,42 +51,45 @@ Highcharts.chart('container', {
                 },
                 offset: 30,
                 labels: {
-                    format: '{point.x2:%e. %b}'
+                    format: '{point.end:%e. %b}'
                 }
             }]
         }
     },
 
+    tooltip: {
+        dateTimeLabelFormat: '%e %b %Y, %H:%M'
+    },
+
     series: [{
         name: 'Project 1',
-        type: 'xrange',
         data: [{
-            x: Date.UTC(2017, 10, 18, 8),
-            x2: Date.UTC(2017, 10, 25, 16),
+            start: Date.UTC(2017, 10, 18, 8),
+            end: Date.UTC(2017, 10, 25, 16),
             name: 'Start prototype',
             assignee: 'Richards',
             y: 0
         }, {
-            x: Date.UTC(2017, 10, 20, 8),
-            x2: Date.UTC(2017, 10, 24, 16),
+            start: Date.UTC(2017, 10, 20, 8),
+            end: Date.UTC(2017, 10, 24, 16),
             name: 'Develop',
             assignee: 'Michaels',
             y: 1
         }, {
-            x: Date.UTC(2017, 10, 25, 16),
-            x2: Date.UTC(2017, 10, 25, 16),
+            start: Date.UTC(2017, 10, 25, 16),
+            end: Date.UTC(2017, 10, 25, 16),
             name: 'Prototype done',
             assignee: 'Richards',
             y: 2
         }, {
-            x: Date.UTC(2017, 10, 27, 8),
-            x2: Date.UTC(2017, 11, 3, 16),
+            start: Date.UTC(2017, 10, 27, 8),
+            end: Date.UTC(2017, 11, 3, 16),
             name: 'Test prototype',
             assignee: 'Richards',
             y: 3
         }, {
-            x: Date.UTC(2017, 10, 23, 8),
-            x2: Date.UTC(2017, 11, 15, 16),
+            start: Date.UTC(2017, 10, 23, 8),
+            end: Date.UTC(2017, 11, 15, 16),
             name: 'Run acceptance tests',
             assignee: 'Halliburton',
             y: 4
