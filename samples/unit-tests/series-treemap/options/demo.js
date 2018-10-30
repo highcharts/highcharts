419f830df7378c3b5d07e1c14b05a1096f063146
jon-a-nygaard <jon.arild@highsoft.com>
2018-10-30 11:36:59 +0100

    Addition to 3dc8ce7, use formatter instead of format.

--- samples/unit-tests/series-treemap/options/demo.js
+++ samples/unit-tests/series-treemap/options/demo.js
@@ -3,38 +3,105 @@ QUnit.module('defaultOptions', function () {
         treemap = options.plotOptions.treemap;
 
     QUnit.test('dataLabels', function (assert) {
+        var dataLabels = treemap.dataLabels,
+            formatter = dataLabels.formatter,
+            ctx = {};
+
+        /**
+         * dataLabels
+         */
         assert.strictEqual(
-            typeof treemap.dataLabels,
+            typeof dataLabels,
             'object',
             'should have dataLabels default to object.'
         );
+
+        /**
+         * dataLabels.align
+         */
         assert.strictEqual(
-            treemap.dataLabels.align,
+            dataLabels.align,
             'center',
             'should have dataLabels.align default to "center".'
         );
+
+        /**
+         * dataLabels.defer
+         */
         assert.strictEqual(
-            treemap.dataLabels.defer,
+            dataLabels.defer,
             false,
             'should have dataLabels.defer default to false.'
         );
+
+        /**
+         * dataLabels.enabled
+         */
         assert.strictEqual(
-            treemap.dataLabels.enabled,
+            dataLabels.enabled,
             true,
             'should have dataLabels.enabled default to true.'
         );
+
+        /**
+         * dataLabels.format
+         */
         assert.strictEqual(
-            treemap.dataLabels.format,
-            '{point.name}',
-            'should have dataLabels.format default to "{point.name}".'
+            typeof dataLabels.format,
+            'undefined',
+            'should have dataLabels.format default to undefined.'
         );
+
+        /**
+         * dataLabels.formatter
+         */
         assert.strictEqual(
-            treemap.dataLabels.inside,
+            formatter.call(undefined),
+            '',
+            'should have dataLabels.formatter return "" when context is undefined.'
+        );
+
+        assert.strictEqual(
+            formatter.call(ctx),
+            '',
+            'should have dataLabels.formatter return "" when point is undefined.'
+        );
+
+        ctx.point = {};
+        assert.strictEqual(
+            formatter.call(ctx),
+            '',
+            'should have dataLabels.formatter return "" when point.name is undefined.'
+        );
+
+        ctx.point.name = {};
+        assert.strictEqual(
+            formatter.call(ctx),
+            '',
+            'should have dataLabels.formatter return "" when point.name is not a string.'
+        );
+
+        ctx.point.name = 'My Name';
+        assert.strictEqual(
+            formatter.call(ctx),
+            'My Name',
+            'should have dataLabels.formatter return point.name when it is a string.'
+        );
+
+        /**
+         * dataLabels.inside
+         */
+        assert.strictEqual(
+            dataLabels.inside,
             true,
             'should have dataLabels.inside default to true.'
         );
+
+        /**
+         * dataLabels.verticalAlign
+         */
         assert.strictEqual(
-            treemap.dataLabels.verticalAlign,
+            dataLabels.verticalAlign,
             'middle',
             'should have dataLabels.verticalAlign default to "middle".'
         );
