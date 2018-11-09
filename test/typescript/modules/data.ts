83835472feec7cad9fddcbd345410da4c1ee242a
bre1470 <40056287+bre1470@users.noreply.github.com>
2018-11-09 15:16:07 +0100

    Tools: Extended DTS support.

---
+++ test/typescript/modules/data.ts
@@ -0,0 +1,116 @@
+import * as Highcharts from 'highcharts';
+import DataModule from 'highcharts/modules/data';
+
+DataModule(Highcharts);
+
+function test_beforeParse() {
+    const series1: Highcharts.SeriesLineOptions = {
+        type: 'line',
+        name: 'All sessions',
+        lineWidth: 4,
+        marker: {
+            radius: 4
+        }
+    };
+    const series2: Highcharts.SeriesLineOptions = {
+        type: 'line',
+        name: 'New users'
+    };
+    Highcharts.chart('container', {
+        chart: {
+            scrollablePlotArea: {
+                minWidth: 700
+            }
+        },
+
+        data: {
+            csvURL: 'https://cdn.rawgit.com/highcharts/highcharts/' +
+                '057b672172ccc6c08fe7dbb27fc17ebca3f5b770/samples/data/analytics.csv',
+            beforeParse: (csv: string) => {
+                return csv.replace(/\n\n/g, '\n');
+            }
+        },
+
+        title: {
+            text: 'Daily sessions at www.highcharts.com'
+        },
+
+        subtitle: {
+            text: 'Source: Google Analytics'
+        },
+
+        xAxis: {
+            tickInterval: 7 * 24 * 3600 * 1000, // one week
+            tickWidth: 0,
+            gridLineWidth: 1,
+            labels: {
+                align: 'left',
+                x: 3,
+                y: -3
+            }
+        },
+
+        yAxis: [{ // left y axis
+            title: {
+                text: undefined
+            },
+            labels: {
+                align: 'left',
+                x: 3,
+                y: 16,
+                format: '{value:.,0f}'
+            },
+            showFirstLabel: false
+        }, { // right y axis
+            linkedTo: 0,
+            gridLineWidth: 0,
+            opposite: true,
+            title: {
+                text: undefined
+            },
+            labels: {
+                align: 'right',
+                x: -3,
+                y: 16,
+                format: '{value:.,0f}'
+            },
+            showFirstLabel: false
+        }],
+
+        legend: {
+            align: 'left',
+            verticalAlign: 'top',
+            borderWidth: 0
+        },
+
+        tooltip: {
+            shared: true,
+            crosshairs: true
+        },
+
+        plotOptions: {
+            series: {
+                cursor: 'pointer',
+                point: {
+                    events: {
+                        click: (e) => {
+                            console.log('click', e);
+                        }
+                    }
+                },
+                marker: {
+                    lineWidth: 1
+                }
+            }
+        },
+
+        series: [{
+            type: 'line',
+            name: 'All sessions',
+            lineWidth: 4,
+            marker: {
+                radius: 4
+            }
+        }, series2]
+    });
+}
