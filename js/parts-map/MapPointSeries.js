086fa327b76b4fcee5bde1c0be08a0d1e1820270
bre1470 <40056287+bre1470@users.noreply.github.com>
2018-11-14 09:55:22 +0100

    Docs: Reformated doclets.

--- js/parts-map/MapPointSeries.js
+++ js/parts-map/MapPointSeries.js
@@ -23,51 +23,52 @@ var merge = H.merge,
  *
  * @augments Highcharts.Series
  */
-seriesType('mappoint', 'scatter'
-/**
- * A mappoint series is a special form of scatter series where the points can
- * be laid out in map coordinates on top of a map.
- *
- * @sample maps/demo/mapline-mappoint/
- *         Map-line and map-point series.
- *
- * @extends      plotOptions.scatter
- * @product      highmaps
- * @optionparent plotOptions.mappoint
- */
-, {
-    dataLabels: {
-        /**
-         * @default   {point.name}
-         * @apioption plotOptions.mappoint.dataLabels.format
-         */
-        enabled: true,
-        formatter: function () { // #2945
-            return this.point.name;
-        },
-        crop: false,
-        defer: false,
-        overflow: false,
-        style: {
-            color: '${palette.neutralColor100}'
+seriesType('mappoint', 'scatter',
+    /**
+     * A mappoint series is a special form of scatter series where the points
+     * can be laid out in map coordinates on top of a map.
+     *
+     * @sample maps/demo/mapline-mappoint/
+     *         Map-line and map-point series.
+     *
+     * @extends      plotOptions.scatter
+     * @product      highmaps
+     * @optionparent plotOptions.mappoint
+     */
+    {
+        dataLabels: {
+            /**
+             * @default   {point.name}
+             * @apioption plotOptions.mappoint.dataLabels.format
+             */
+            enabled: true,
+            formatter: function () { // #2945
+                return this.point.name;
+            },
+            crop: false,
+            defer: false,
+            overflow: false,
+            style: {
+                color: '${palette.neutralColor100}'
+            }
         }
-    }
-// Prototype members
-}, {
-    type: 'mappoint',
-    forceDL: true
-// Point class
-}, {
-    applyOptions: function (options, x) {
-        var mergedOptions = (
-            options.lat !== undefined &&
-            options.lon !== undefined ?
+    // Prototype members
+    }, {
+        type: 'mappoint',
+        forceDL: true
+    // Point class
+    }, {
+        applyOptions: function (options, x) {
+            var mergedOptions = (
+                options.lat !== undefined &&
+                options.lon !== undefined ?
                 merge(options, this.series.chart.fromLatLonToPoint(options)) :
                 options
-        );
-        return Point.prototype.applyOptions.call(this, mergedOptions, x);
+            );
+            return Point.prototype.applyOptions.call(this, mergedOptions, x);
+        }
     }
-});
+);
 
 /**
  * A `mappoint` series. If the [type](#series.mappoint.type) option
