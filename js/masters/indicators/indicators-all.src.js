4625fe08ed499b58dd54625a32a495d2b8fb102f
wchmiel <wojc.chmiel@gmail.com>
2018-11-16 17:04:39 +0100

    StockTools: Addition Supertrend indicator.

--- js/masters/indicators/indicators-all.src.js
+++ js/masters/indicators/indicators-all.src.js
@@ -37,6 +37,7 @@ import '../../indicators/psar.src.js';
 import '../../indicators/roc.src.js';
 import '../../indicators/rsi.src.js';
 import '../../indicators/stochastic.src.js';
+import '../../indicators/supertrend.src.js';
 import '../../indicators/volume-by-price.src.js';
 import '../../indicators/vwap.src.js';
 import '../../indicators/williams-r.src.js';
