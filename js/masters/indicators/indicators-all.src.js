9e612a7d1df93747791caacb0735047b3e61cbea
Rafal Sebestjanski <raf18seb@gmail.com>
2018-11-19 15:31:21 +0100

    StockTools: Added Tema Indicator.

--- js/masters/indicators/indicators-all.src.js
+++ js/masters/indicators/indicators-all.src.js
@@ -19,6 +19,8 @@ import '../../indicators/cci.src.js';
 import '../../indicators/chaikin.src.js';
 import '../../indicators/cmf.src.js';
 import '../../indicators/ema.src.js';
+import '../../indicators/dema.src.js';
+import '../../indicators/tema.src.js';
 import '../../indicators/apo.src.js';
 import '../../indicators/ichimoku-kinko-hyo.src.js';
 import '../../indicators/keltner-channels.src.js';
