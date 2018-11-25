3997ae13736cffe8ad5b3a0ef703e8aaad2529e0
bre1470 <40056287+bre1470@users.noreply.github.com>
2018-11-25 21:43:09 +0100

    Tools: Reactivated DTS test for Gantt.

--- test/typescript/highcharts-gantt.ts
+++ test/typescript/highcharts-gantt.ts
@@ -1,6 +1,5 @@
 import * as Gantt from 'highcharts/highcharts-gantt';
 
-/*
 function test_progress() {
     Gantt.ganttChart('container', {
         title: {
@@ -16,27 +15,26 @@ function test_progress() {
             name: 'Project 1',
             data: [{
                 name: 'Start prototype',
-                // start: Date.UTC(2014, 10, 18),
-                // end: Date.UTC(2014, 10, 25),
-                // completed: 0.25
+                start: Date.UTC(2014, 10, 18),
+                end: Date.UTC(2014, 10, 25),
+                completed: 0.25
             }, {
                 name: 'Test prototype',
-                // start: Date.UTC(2014, 10, 27),
-                // end: Date.UTC(2014, 10, 29)
+                start: Date.UTC(2014, 10, 27),
+                end: Date.UTC(2014, 10, 29)
             }, {
                 name: 'Develop',
-                // start: Date.UTC(2014, 10, 20),
-                // end: Date.UTC(2014, 10, 25),
-                // completed: {
-                //     amount: 0.12,
-                //     fill: '#fa0'
-                // }
+                start: Date.UTC(2014, 10, 20),
+                end: Date.UTC(2014, 10, 25),
+                completed: {
+                    amount: 0.12,
+                    fill: '#fa0'
+                }
             }, {
                 name: 'Run acceptance tests',
-                // start: Date.UTC(2014, 10, 23),
-                // end: Date.UTC(2014, 10, 26)
+                start: Date.UTC(2014, 10, 23),
+                end: Date.UTC(2014, 10, 26)
             }]
         }]
     });
 }
-*/
