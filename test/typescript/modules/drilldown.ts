b57cbc2929805c508a2ba623062c3e24d4c7d898
bre1470 <40056287+bre1470@users.noreply.github.com>
2018-11-26 21:01:26 +0100

    Tools: Added more modules to the DTS system.

---
+++ test/typescript/modules/drilldown.ts
@@ -0,0 +1,85 @@
+import * as Highcharts from 'highcharts';
+import DrilldownModule from 'highcharts/modules/drilldown';
+
+DrilldownModule(Highcharts);
+
+function test_labelStyling() {
+    Highcharts.chart('container', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Drilldown label styling'
+        },
+        xAxis: {
+            type: 'category'
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            series: {
+                borderWidth: 0,
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        series: [{
+            name: 'Things',
+            colorByPoint: true,
+            data: [{
+                name: 'Dieren',
+                y: 5,
+                drilldown: 'animals'
+            }, {
+                name: 'Fruit',
+                y: 2,
+                drilldown: 'fruits'
+            }, {
+                name: 'Auto\'s',
+                y: 4
+            }]
+        }],
+        drilldown: {
+            drillUpButton: {
+                relativeTo: 'spacingBox',
+                position: {
+                    y: 0,
+                    x: 0
+                },
+                theme: {
+                    fill: 'white',
+                    'stroke-width': 1,
+                    stroke: 'silver',
+                    r: 0,
+                    states: {
+                        hover: {
+                            fill: '#a4edba'
+                        },
+                        select: {
+                            stroke: '#039',
+                            fill: '#a4edba'
+                        }
+                    }
+                }
+            },
+            series: [{
+                id: 'animals',
+                data: [
+                    ['Katten', 4],
+                    ['Honden', 2],
+                    ['Koeien', 1],
+                    ['Schapen', 2],
+                    ['Varkens', 1]
+                ]
+            }, {
+                id: 'fruits',
+                data: [
+                    ['Appels', 4],
+                    ['Sinaasappels', 2]
+                ]
+            }]
+        }
+    });
+}
---
+++ test/typescript/modules/histogram-bellcurve.ts
@@ -0,0 +1,4 @@
+import * as Highcharts from 'highcharts';
+import HistogramBellcurveModule from 'highcharts/modules/histogram-bellcurve';
+
+HistogramBellcurveModule(Highcharts);
