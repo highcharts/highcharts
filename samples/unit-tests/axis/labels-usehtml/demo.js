92ff0e320fb8b4a45f5dc0989d07a0ac5890560a
Torstein Hønsi <torstein@highsoft.com>
2018-11-29 10:24:43 +0100

    Fixed #9537, ellipsis not applied to X axis labels when `useHTML` was true.

--- samples/unit-tests/axis/labels-usehtml/demo.js
+++ samples/unit-tests/axis/labels-usehtml/demo.js
@@ -413,3 +413,30 @@ QUnit.test('Varying label width with HTML labels (#8809)', function (assert) {
     );
 
 });
+
+QUnit.test('Ellipsis on single-word labels (#9537)', function (assert) {
+    var chart = Highcharts.chart('container', {
+
+        chart: {
+            width: 400
+        },
+
+        xAxis: {
+            categories: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            labels: {
+                rotation: 0,
+                useHTML: true
+            }
+        },
+
+        series: [{
+            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+        }]
+    });
+
+    assert.notEqual(
+        chart.xAxis[0].ticks[0].label.element.style.width,
+        '',
+        'The element should have a width'
+    );
+});
