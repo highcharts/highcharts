4ce7c5ae14770debecd81526a7c35da69fd2479c
ppotaczek <ppotaczek@MacBook-Pro-ppotaczek.local>
2018-12-07 13:40:36 +0100

    Fixed #9525, Wrong number of points after update zoomed chart.

--- samples/unit-tests/series-variwide/variwide/demo.js
+++ samples/unit-tests/series-variwide/variwide/demo.js
@@ -65,6 +65,36 @@ QUnit.test('variwide', function (assert) {
     chart.xAxis[0].update({ crosshair: false });
     chart.series[0].points[0].onMouseOver();
     assert.ok(true, "No errors with disabled crosshair (#8173)");
+
+
+    chart.update({
+        xAxis: {
+            minRange: 1
+        },
+        series: [{
+            type: 'variwide',
+            borderWidth: 0,
+            cropThreshold: 1,
+
+            data: [
+                [0, 1, 1],
+                [1, 2, 1],
+                [2, 3, 1],
+                [3, 2, 1]
+            ]
+        }]
+    }, true, true, false);
+
+    chart.xAxis[0].setExtremes(0, 1, true, false);
+    chart.reflow();
+    chart.xAxis[0].setExtremes(0, 3, true, false);
+
+    assert.strictEqual(
+        chart.series[0].points.length,
+        4,
+        'Correct number of points after zoom and redraw (#9525)'
+    );
+
 });
 
 QUnit.test('variwide null points', function (assert) {
