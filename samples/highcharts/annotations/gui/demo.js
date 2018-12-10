301af70c71659c5db248826bea317715403511bb
Torstein Hønsi <torstein@highsoft.com>
2018-12-10 15:57:19 +0100

    Samples: Annotation instructions.

--- samples/highcharts/annotations/gui/demo.js
+++ samples/highcharts/annotations/gui/demo.js
@@ -26,6 +26,13 @@ Highcharts.chart('container', {
             }
         }
     },
+
+    title: {
+        text: 'Chart with Annotations GUI'
+    },
+    subtitle: {
+        text: 'Use the buttons to add or edit annotations'
+    },
     series: [{
         data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 23.3, 18.3, 13.9, 9.6]
     }]
