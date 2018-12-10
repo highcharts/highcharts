8560b138998869c28e9f72711e08bf5c4ff942a2
Torstein Hønsi <torstein@highsoft.com>
2018-12-10 15:02:09 +0100

    Samples: Updated resource files to v7.0.0.

--- samples/highcharts/demo/line-ajax/demo.js
+++ samples/highcharts/demo/line-ajax/demo.js
@@ -8,7 +8,7 @@ Highcharts.chart('container', {
     },
 
     data: {
-        csvURL: 'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v6.1.0/samples/data/analytics.csv',
+        csvURL: 'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/analytics.csv',
         beforeParse: function (csv) {
             return csv.replace(/\n\n/g, '\n');
         }
