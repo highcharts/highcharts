2bee0ff766e74ee1d0ffc69867cc9e299c7216a4
bre1470 <40056287+bre1470@users.noreply.github.com>
2018-12-17 14:01:38 +0100

    Docs: Fixed ohlc data doclet.

--- js/parts/OHLCSeries.js
+++ js/parts/OHLCSeries.js
@@ -1,4 +1,4 @@
-/**
+/* *
  * (c) 2010-2018 Torstein Honsi
  *
  * License: www.highcharts.com/license
@@ -336,48 +336,47 @@ seriesType('ohlc', 'column'
  * An array of data points for the series. For the `ohlc` series type,
  * points can be given in the following ways:
  *
- * 1.  An array of arrays with 5 or 4 values. In this case, the values
- * correspond to `x,open,high,low,close`. If the first value is a string,
- * it is applied as the name of the point, and the `x` value is inferred.
- * The `x` value can also be omitted, in which case the inner arrays
- * should be of length 4\. Then the `x` value is automatically calculated,
- * either starting at 0 and incremented by 1, or from `pointStart`
- * and `pointInterval` given in the series options.
+ * 1. An array of arrays with 5 or 4 values. In this case, the values correspond
+ *    to `x,open,high,low,close`. If the first value is a string, it is applied
+ *    as the name of the point, and the `x` value is inferred. The `x` value can
+ *    also be omitted, in which case the inner arrays should be of length 4\.
+ *    Then the `x` value is automatically calculated, either starting at 0 and
+ *    incremented by 1, or from `pointStart` and `pointInterval` given in the
+ *    series options.
+ *    ```js
+ *    data: [
+ *        [0, 6, 5, 6, 7],
+ *        [1, 9, 4, 8, 2],
+ *        [2, 6, 3, 4, 10]
+ *    ]
+ *    ```
  *
- * ```js
- *     data: [
- *         [0, 6, 5, 6, 7],
- *         [1, 9, 4, 8, 2],
- *         [2, 6, 3, 4, 10]
- *     ]
- * ```
+ * 2. An array of objects with named values. The following snippet shows only a
+ *    few settings, see the complete options set below. If the total number of
+ *    data points exceeds the series'
+ *    [turboThreshold](#series.ohlc.turboThreshold), this option is not
+ *    available.
+ *    ```js
+ *    data: [{
+ *        x: 1,
+ *        open: 3,
+ *        high: 4,
+ *        low: 5,
+ *        close: 2,
+ *        name: "Point2",
+ *        color: "#00FF00"
+ *    }, {
+ *        x: 1,
+ *        open: 4,
+ *        high: 3,
+ *        low: 6,
+ *        close: 7,
+ *        name: "Point1",
+ *        color: "#FF00FF"
+ *    }]
+ *    ```
  *
- * 2.  An array of objects with named values. The following snippet shows only a
- * few settings, see the complete options set below. If the total number of data
- * points exceeds the series' [turboThreshold](#series.ohlc.turboThreshold),
- * this option is not available.
- *
- *  ```js
- *     data: [{
- *         x: 1,
- *         open: 3,
- *         high: 4,
- *         low: 5,
- *         close: 2,
- *         name: "Point2",
- *         color: "#00FF00"
- *     }, {
- *         x: 1,
- *         open: 4,
- *         high: 3,
- *         low: 6,
- *         close: 7,
- *         name: "Point1",
- *         color: "#FF00FF"
- *     }]
- *  ```
- *
- * @type      {Array<Array<number>|*>}
+ * @type      {Array<Array<(number|string),number,number,number>|Array<(number|string),number,number,number,number>|*>}
  * @extends   series.arearange.data
  * @excluding y, marker
  * @product   highstock
