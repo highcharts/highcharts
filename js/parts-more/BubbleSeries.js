9874530d575b2ca14f6505b1e885e1cdf7d275ea
bre1470 <40056287+bre1470@users.noreply.github.com>
2018-12-17 13:40:41 +0100

    Docs: Reduced bubble data doclet.

--- js/parts-more/BubbleSeries.js
+++ js/parts-more/BubbleSeries.js
@@ -1,4 +1,4 @@
-/**
+/* *
  * (c) 2010-2018 Torstein Honsi
  *
  * License: www.highcharts.com/license
@@ -593,45 +593,42 @@ Axis.prototype.beforePadding = function () {
  * An array of data points for the series. For the `bubble` series type,
  * points can be given in the following ways:
  *
- * 1.  An array of arrays with 3 or 2 values. In this case, the values
- * correspond to `x,y,z`. If the first value is a string, it is applied
- * as the name of the point, and the `x` value is inferred. The `x`
- * value can also be omitted, in which case the inner arrays should
- * be of length 2\. Then the `x` value is automatically calculated,
- * either starting at 0 and incremented by 1, or from `pointStart` and
- * `pointInterval` given in the series options.
+ * 1. An array of arrays with 3 or 2 values. In this case, the values correspond
+ *    to `x,y,z`. If the first value is a string, it is applied as the name of
+ *    the point, and the `x` value is inferred. The `x` value can also be
+ *    omitted, in which case the inner arrays should be of length 2\. Then the
+ *    `x` value is automatically calculated, either starting at 0 and
+ *    incremented by 1, or from `pointStart` and `pointInterval` given in the
+ *    series options.
+ *    ```js
+ *    data: [
+ *        [0, 1, 2],
+ *        [1, 5, 5],
+ *        [2, 0, 2]
+ *    ]
+ *    ```
  *
- *  ```js
- *     data: [
- *         [0, 1, 2],
- *         [1, 5, 5],
- *         [2, 0, 2]
- *     ]
- *  ```
+ * 2. An array of objects with named values. The following snippet shows only a
+ *    few settings, see the complete options set below. If the total number of
+ *    data points exceeds the series'
+ *    [turboThreshold](#series.bubble.turboThreshold), this option is not
+ *    available.
+ *    ```js
+ *    data: [{
+ *        x: 1,
+ *        y: 1,
+ *        z: 1,
+ *        name: "Point2",
+ *        color: "#00FF00"
+ *    }, {
+ *        x: 1,
+ *        y: 5,
+ *        z: 4,
+ *        name: "Point1",
+ *        color: "#FF00FF"
+ *    }]
+ *    ```
  *
- * 2.  An array of objects with named values. The following snippet shows only a
- * few settings, see the complete options set below. If the total number of data
- * points exceeds the series' [turboThreshold](#series.bubble.turboThreshold),
- * this option is not available.
- *
- *  ```js
- *     data: [{
- *         x: 1,
- *         y: 1,
- *         z: 1,
- *         name: "Point2",
- *         color: "#00FF00"
- *     }, {
- *         x: 1,
- *         y: 5,
- *         z: 4,
- *         name: "Point1",
- *         color: "#FF00FF"
- *     }]
- *  ```
- *
- * @sample {highcharts} highcharts/chart/reflow-true/
- *         Numerical values
  * @sample {highcharts} highcharts/series/data-array-of-arrays/
  *         Arrays of numeric x and y
  * @sample {highcharts} highcharts/series/data-array-of-arrays-datetime/
@@ -641,7 +638,7 @@ Axis.prototype.beforePadding = function () {
  * @sample {highcharts} highcharts/series/data-array-of-objects/
  *         Config objects
  *
- * @type      {Array<Array<number>|*>}
+ * @type      {Array<Array<(number|string),number>|Array<(number|string),number,number>|*>}
  * @extends   series.line.data
  * @excluding marker
  * @product   highcharts
