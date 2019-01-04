highcharts/highcharts

commit 27b3314691391c7042abe17c648c0d511ca67022

Optimized the series-label module by changing the `connectorAllowed` option.
It is now set to false by default, and avoids running brute-force search for open slots in the chart when it is false. Huge performance gain on large and complex charts.

js/modules/series-label.src.js
@@ -66,9 +66,13 @@ H.setOptions({
 				enabled: true,
 				/**
 				 * Allow labels to be placed distant to the graph if necessary,
-				 * and draw a connector line to the graph.
+				 * and draw a connector line to the graph. Setting this option
+				 * to true may decrease the performance significantly, since the
+				 * algorithm with systematically search for open spaces in the
+				 * while plot area. Visually, it may also result in a more
+				 * cluttered chart, though more of the series will be labeled.
 				 */
-				connectorAllowed: true,
+				connectorAllowed: false,
 				/**
 				 * If the label is closer than this to a neighbour graph, draw a
 				 * connector.
@@ -659,7 +663,7 @@ Chart.prototype.drawSeriesLabels = function () {
 			}
 
 			// Brute force, try all positions on the chart in a 16x16 grid
-			if (!results.length && !onArea) {
+			if (labelOptions.connectorAllowed && !results.length && !onArea) {
 				for (
 					x = paneLeft + paneWidth - bBox.width;
 					x >= paneLeft;

commit d509ef759691656826facac65814d7368d354f2f

Fixed an issue in series-label placement logic, labels gravitated to the right.
Regression since 27b3314691391c7042abe17c648c0d511ca67022.

js/modules/series-label.src.js
@@ -93,7 +93,7 @@ H.setOptions({
                  * and draw a connector line to the graph. Setting this option
                  * to true may decrease the performance significantly, since the
                  * algorithm with systematically search for open spaces in the
-                 * while plot area. Visually, it may also result in a more
+                 * whole plot area. Visually, it may also result in a more
                  * cluttered chart, though more of the series will be labeled.
                  */
                 connectorAllowed: false,
@@ -766,7 +766,8 @@ Chart.prototype.drawSeriesLabels = function () {
                         best = series.checkClearPoint(
                             x,
                             y,
-                            bBox
+                            bBox,
+                            true
                         );
                     }
                     if (best) {
@@ -780,7 +781,8 @@ Chart.prototype.drawSeriesLabels = function () {
                         best = series.checkClearPoint(
                             x,
                             y,
-                            bBox
+                            bBox,
+                            true
                         );
                     }
                     if (best) {
@@ -794,7 +796,8 @@ Chart.prototype.drawSeriesLabels = function () {
                         best = series.checkClearPoint(
                             x,
                             y,
-                            bBox
+                            bBox,
+                            true
                         );
                     }
                     if (best) {
@@ -808,7 +811,8 @@ Chart.prototype.drawSeriesLabels = function () {
                         best = series.checkClearPoint(
                             x,
                             y,
-                            bBox
+                            bBox,
+                            true
                         );
                     }
                     if (best) {