13f4b797bb64802faabb83a1efc5e1ec58160dbc
Torstein Hønsi <torstein@highsoft.com>
2019-01-07 22:22:26 +0100

    Addition to #9758. Tests failed.

--- js/modules/no-data-to-display.src.js
+++ js/modules/no-data-to-display.src.js
@@ -161,10 +161,12 @@ defaultOptions.noData = {
  */
 H.Series.prototype.hasData = function () {
     return (
-        (this.visible &&
-        this.dataMax !== undefined &&
-        this.dataMin !== undefined) || // #3703
-        (this.visible && this.yData.length > 0) // #9758
+        (
+            this.visible &&
+            this.dataMax !== undefined &&
+            this.dataMin !== undefined
+        ) || // #3703
+        (this.visible && this.yData && this.yData.length > 0) // #9758
     );
 };
 
