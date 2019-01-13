87afc1074ef3dfa986dc6a8842651d0ed8211444
Torstein Hønsi <torstein@highsoft.com>
2019-01-13 11:11:38 +0100

    Study: Item series. Form control for demoing.

--- samples/highcharts/studies/item-rows/demo.js
+++ samples/highcharts/studies/item-rows/demo.js
@@ -32,27 +32,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 ['Høyre', 45, '#4677BA', 'H'],
                 ['Fremskrittspartiet', 27, '#262955', 'FrP']
             ],
-            data1: [
-                ['Conservative', 318],
-                ['Labour', 262],
-                ['Scottish National Party', 35],
-                ['Liberal Democrat', 12],
-                ['Democratic Unionist Party', 10],
-                ['Sinn Fein', 7],
-                ['Plaid Cymru', 4],
-                ['Green Party', 1],
-                ['Others', 1]
-            ],
-            data2: [
-                ['The Left', 69, '#BE3075', 'DIE LINKE'],
-                ['Social Democratic Party', 153, '#EB001F', 'SPD'],
-                ['Alliance 90/The Greens', 67, '#64A12D', 'GRÜNE'],
-                ['Free Democratic Party', 80, '#FFED00', 'FDP'],
-                ['Christian Democratic Union', 200, '#000000', 'CDU'],
-                ['Christian Social Union in Bavaria', 46, '#008AC5', 'CSU'],
-                ['Alternative for Germany', 94, '#009EE0', 'AfD']
-            ],
-
             dataLabels: {
                 enabled: true,
                 format: '{point.label}'
@@ -68,6 +47,99 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+document.getElementById('gb').addEventListener('click', function () {
+    Highcharts.charts[0].series[0].update({
+        data: [
+            ['Conservative', 318],
+            ['Labour', 262],
+            ['Scottish National Party', 35],
+            ['Liberal Democrat', 12],
+            ['Democratic Unionist Party', 10],
+            ['Sinn Fein', 7],
+            ['Plaid Cymru', 4],
+            ['Green Party', 1],
+            ['Others', 1]
+        ],
+
+        dataLabels: {
+            enabled: false
+        }
+    });
+});
+
+document.getElementById('de').addEventListener('click', function () {
+    Highcharts.charts[0].series[0].update({
+        data: [
+            ['The Left', 69, '#BE3075', 'DIE LINKE'],
+            ['Social Democratic Party', 153, '#EB001F', 'SPD'],
+            ['Alliance 90/The Greens', 67, '#64A12D', 'GRÜNE'],
+            ['Free Democratic Party', 80, '#FFED00', 'FDP'],
+            ['Christian Democratic Union', 200, '#000000', 'CDU'],
+            ['Christian Social Union in Bavaria', 46, '#008AC5', 'CSU'],
+            ['Alternative for Germany', 94, '#009EE0', 'AfD']
+        ],
+
+        dataLabels: {
+            enabled: true
+        }
+    });
+});
+
+document.getElementById('no').addEventListener('click', function () {
+    Highcharts.charts[0].series[0].update({
+        data: [
+            ['Rødt', 1, '#851914', 'R'],
+            ['Sosialistisk Venstreparti', 11, '#B0185B', 'SV'],
+            ['Arbeiderpartiet', 49, '#C6191D', 'AP'],
+            ['Senterpartiet', 19, '#5CA92E', 'SP'],
+            ['Miljøpartiet De Grønne', 1, '#024B26', 'MDG'],
+            ['Kristelig Folkeparti', 8, '#F9B234', 'KrF'],
+            ['Venstre', 8, '#036766', 'V'],
+            ['Høyre', 45, '#4677BA', 'H'],
+            ['Fremskrittspartiet', 27, '#262955', 'FrP']
+        ],
+        dataLabels: {
+            enabled: true
+        }
+    });
+});
+
+document.getElementById('parliament').addEventListener('click', function () {
+    Highcharts.charts[0].series[0].update({
+        center: ['50%', '88%'],
+        size: '170%',
+        startAngle: -100,
+        endAngle: 100
+    });
+});
+
+document.getElementById('rectangle').addEventListener('click', function () {
+    Highcharts.charts[0].series[0].update({
+        startAngle: null,
+        endAngle: null
+    });
+});
+
+document.getElementById('circle').addEventListener('click', function () {
+    Highcharts.charts[0].series[0].update({
+        center: ['50%', '50%'],
+        size: '100%',
+        startAngle: 0,
+        endAngle: 360
+    });
+});
+
+document.getElementById('rows').addEventListener('click', function () {
+    Highcharts.charts[0].series[0].update({
+        rows: Number(this.value)
+    });
+});
+
+document.getElementById('innersize').addEventListener('click', function () {
+    Highcharts.charts[0].series[0].update({
+        innerSize: this.value + '%'
+    });
+});
 
 /**
  * This is a study for an item series without axes, where point values signify
@@ -175,6 +247,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
                         itemCount += colCount + 1;
                     }
+                }
+
+                if (!rows) {
+                    return;
                 }
 
                 // We now have more slots than we have total items. Loop over
@@ -335,7 +411,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         for (var val = 0; val < point.y; val++) {
 
                             // Semi-circle
-                            if (series.center) {
+                            if (series.center && series.slots) {
 
                                 // Fill up the slots from left to right
                                 var slot = series.slots.shift();
@@ -427,4 +503,3 @@ document.addEventListener('DOMContentLoaded', function () {
     );
 
 }(Highcharts));
-
