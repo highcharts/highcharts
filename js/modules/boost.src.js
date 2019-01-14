eec2b0a9f588b66f3449e274c815b20831c73804
bre1470 <40056287+bre1470@users.noreply.github.com>
2019-01-14 15:30:07 +0100

    Docs: Switched to different literal options for boostBlending.

--- js/modules/boost.src.js
+++ js/modules/boost.src.js
@@ -275,11 +275,11 @@
  */
 
 /**
- * Sets the blending mode in the boost module.
+ * Sets the color blending in the boost module.
  *
  * @type       {string}
  * @default    undefined
- * @validvalue ["add", "mult", "darken"]
+ * @validvalue ["add", "multiply", "darken"]
  * @apioption  plotOptions.series.boostBlending
  */
 
@@ -2358,7 +2358,9 @@ function GLRenderer(postRenderCallback) {
                 gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
                 gl.blendEquation(gl.FUNC_ADD);
 
-            } else if (options.boostBlending === 'mult') {
+            } else if (options.boostBlending === 'mult' ||
+                options.boostBlending === 'multiply'
+            ) {
                 gl.blendFunc(gl.DST_COLOR, gl.ZERO);
 
             } else if (options.boostBlending === 'darken') {
