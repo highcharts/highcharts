68f93398b08ccc56e98d2bc508df19830b79005f
oysteinmoseng <oystein@highsoft.com>
2019-01-17 16:03:26 +0100

    Added workaround for Narrator for better keyboard navigation.

--- js/modules/screen-reader.src.js
+++ js/modules/screen-reader.src.js
@@ -939,6 +939,9 @@ H.Chart.prototype.callbacks.push(function (chart) {
         }
     });
 
+    // Hide desc element
+    descElement.setAttribute('aria-hidden', 'true');
+
     // Add top-secret screen reader region
     chart.addScreenReaderRegion(hiddenSectionId, tableId);
 
