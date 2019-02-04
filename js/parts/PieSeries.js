c5d47a2cb61f52c5f5d2875fb10bb5b22c949386
Torstein Hønsi <torstein@highsoft.com>
2019-02-04 09:30:46 +0100

    Docs: Exclude unused data label options from pie series and derivatives. See #10036.

--- js/parts/PieSeries.js
+++ js/parts/PieSeries.js
@@ -121,7 +121,8 @@ seriesType('pie', 'line',
 
         /**
          * @extends   plotOptions.series.dataLabels
-         * @excluding align, allowOverlap, staggerLines, step
+         * @excluding align, allowOverlap, inside, staggerLines, step,
+         *            verticalAlign
          * @product   highcharts
          */
         dataLabels: {
