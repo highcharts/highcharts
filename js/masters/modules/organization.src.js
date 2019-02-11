fda025a1507b13186b79be61eac20621e3fe27f7
Torstein Hønsi <torstein@highsoft.com>
2019-02-11 12:18:36 +0100

    Feature: Org chart, added module files

---
+++ js/masters/modules/organization.src.js
@@ -0,0 +1,10 @@
+/**
+ * @license @product.name@ JS v@product.version@ (@product.date@)
+ * Organization chart series type
+ *
+ * (c) 2019-2019 Torstein Honsi
+ *
+ * License: www.highcharts.com/license
+ */
+'use strict';
+import '../../modules/organization.src.js';
--- samples/highcharts/studies/organization-chart/demo.js
+++ samples/highcharts/studies/organization-chart/demo.js
@@ -1,86 +1,27 @@
-(function (H) {
-
-    var base = H.seriesTypes.sankey.prototype;
-    H.seriesType(
-        'orgchart',
-        'sankey',
-        {
-            nodeWidth: 50,
-
-            // @todo: Use palette
-            linkColor: '#666666',
-            linkLineWidth: 1
-        },
-        {
-            inverted: true,
-            pointAttribs: function (point, state) {
-                var attribs = base.pointAttribs.call(this, point, state);
-
-                if (!point.isNode) {
-                    attribs.stroke = this.options.linkColor;
-                    attribs['stroke-width'] = this.options.linkLineWidth;
-                    delete attribs.fill;
-                }
-                return attribs;
-            },
-            createNode: function (id) {
-                var node = base.createNode
-                    .call(this, id);
-
-                // All nodes in an org chart are equal width
-                node.getSum = function () {
-                    return 1;
-                };
-
-                return node;
-
-            },
-            translateLink: function (point) {
-
-                var fromNode = point.fromNode,
-                    toNode = point.toNode,
-                    crisp = Math.round(this.options.linkLineWidth) % 2 / 2,
-                    x1 = Math.floor(
-                        fromNode.shapeArgs.x + fromNode.shapeArgs.width
-                    ) + crisp,
-                    y1 = Math.floor(
-                        fromNode.shapeArgs.y + fromNode.shapeArgs.height / 2
-                    ) + crisp,
-                    x2 = Math.floor(toNode.shapeArgs.x) + crisp,
-                    y2 = Math.floor(toNode.shapeArgs.y + toNode.shapeArgs.height / 2) + crisp;
-
-                if (this.chart.inverted) {
-                    x1 -= fromNode.shapeArgs.width;
-                    x2 += toNode.shapeArgs.width;
-                }
-
-                point.plotY = 1;
-                point.shapeType = 'path';
-                point.shapeArgs = {
-                    d: [
-                        'M', x1, y1,
-                        'L', (x1 + x2) / 2, y1,
-                        (x1 + x2) / 2, y2,
-                        x2, y2
-                    ]
-                };
-            }
-        }
-
-    );
-
-}(Highcharts));
-
 var datasets = {
     topdown: {
         data: [
+            ['Shareholders', 'Board'],
+            ['Board', 'CEO'],
             ['CEO', 'CTO'],
             ['CEO', 'CPO'],
             ['CEO', 'CSO'],
             ['CEO', 'CMO'],
+            ['HR', 'CEO'],
             ['CTO', 'Dev1'],
-            ['CTO', 'Dev2']
-        ]
+            ['CTO', 'Dev2'],
+            ['CTO', 'Dev3'],
+            ['CTO', 'Dev4'],
+            ['CSO', 'Sales1'],
+            ['CSO', 'Sales2'],
+            ['CSO', 'Sales3'],
+            ['CMO', 'Market1'],
+            ['CMO', 'Market2']
+        ],
+        nodes: [{
+            id: 'HR',
+            column: 2
+        }]
     },
     mafia: {
         data: [
@@ -121,7 +62,7 @@ var datasets = {
     }
 };
 
-var dataset = datasets.mafia;
+var dataset = datasets.topdown;
 Highcharts.chart('container', {
 
     title: {
