45cf2688b5a71469730f7a4c94644379da281014
bre1470 <40056287+bre1470@users.noreply.github.com>
2019-02-18 14:15:59 +0100

    Docs: Made some additional properties on Chart official. Fixed #10125

--- js/parts/Interaction.js
+++ js/parts/Interaction.js
@@ -1003,6 +1003,13 @@ extend(Series.prototype, /** @lends Highcharts.Series.prototype */ {
 
         // hover this
         series.setState('hover');
+
+        /**
+         * Contains the original hovered series.
+         *
+         * @name Highcharts.Chart#hoverSeries
+         * @type {Highcharts.Series|null}
+         */
         chart.hoverSeries = series;
     },
 
