2dbad1d588f115e19b962cf4f7734abfe31e3f0c
ppotaczek <ppotaczek@192-168-1-5.tpgi.com.au>
2019-02-27 07:47:41 +0100

    Fixed #9917, chart.update with initial pane options did not change the pane.

--- js/parts-more/Pane.js
+++ js/parts-more/Pane.js
@@ -382,8 +382,9 @@ extend(Pane.prototype, {
      * @param {boolean} redraw
      */
     update: function (options, redraw) {
-
         merge(true, this.options, options);
+        merge(true, this.chart.options.pane, options); // #9917
+
         this.setOptions(this.options);
         this.render();
         this.chart.axes.forEach(function (axis) {
@@ -393,7 +394,6 @@ extend(Pane.prototype, {
             }
         }, this);
     }
-
 });
 
 H.Pane = Pane;
