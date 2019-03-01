703b8bdea5e6fa4caf25de2320fd89bc51a09c03
Kacper Madej <kacper@blacklabel.pl>
2019-03-01 13:04:25 +0100

    Pyramid3d: initial commit for the pyramid3d series type module.

---
+++ js/masters/modules/pyramid3d.src.js
@@ -0,0 +1,10 @@
+/**
+ * @license @product.name@ JS v@product.version@ (@product.date@)
+ * Highcharts funnel module
+ *
+ * (c) 2010-2019 Kacper Madej
+ *
+ * License: www.highcharts.com/license
+ */
+'use strict';
+import '../../modules/pyramid3d.src.js';
