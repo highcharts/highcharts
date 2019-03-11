highcharts/highcharts

commit 7cab2b36524cfc56b0312e7708571efe5b36e229

Highstock: Fixed #9994, navigator.xAxis.min was wrong with main xAxis.min set and ordinal enabled after addPoint.

samples/unit-tests/navigator/navigator/demo.js
@@ -41,6 +41,32 @@ QUnit.test(
             0,
             'All series, including navSeries, removed without errors (#5581)'
         );
+
+        chart = Highcharts.stockChart('container', {
+            xAxis: {
+                min: 1318607700000
+            },
+            series: [{
+                data: [
+                    [1318607640000, 420.32],
+                    [1318607700000, 420.58],
+                    [1318607760000, 421.07],
+                    [1318607820000, 421.46],
+                    [1318607880000, 421.69],
+                    [1318607940000, 421.94]
+                ]
+            }]
+        });
+
+        chart.series[0].addPoint([1318608000000, 422.03], false, true);
+        chart.series[0].addPoint([1318608060000, 421.23], false, true);
+        chart.series[0].addPoint([1318608120000, 421.97], true, true);
+
+        assert.strictEqual(
+            chart.navigator.xAxis.min,
+            1318607820000,
+            'xAxis.min should be omitted in navigator when ordinal is enabled (#9994)'
+        );
     }
 );
 

js/parts/Navigator.js
@@ -1726,6 +1726,10 @@ Navigator.prototype = {
             navAxis = this.xAxis,
             navAxisOptions = navAxis.options,
             baseAxisOptions = baseAxis.options,
+            min = (navAxisOptions && navAxisOptions.ordinal) ?
+                null : baseAxisOptions.min,
+            max = (navAxisOptions && navAxisOptions.ordinal) ?
+                null : baseAxisOptions.max,
             ret;
 
         if (!returnFalseOnNoBaseSeries || baseAxis.dataMin !== null) {
@@ -1734,7 +1738,7 @@ Navigator.prototype = {
                     navAxisOptions && navAxisOptions.min,
                     numExt(
                         'min',
-                        baseAxisOptions.min,
+                        min, // #9994
                         baseAxis.dataMin,
                         navAxis.dataMin,
                         navAxis.min
@@ -1744,7 +1748,7 @@ Navigator.prototype = {
                     navAxisOptions && navAxisOptions.max,
                     numExt(
                         'max',
-                        baseAxisOptions.max,
+                        max, // #9994
                         baseAxis.dataMax,
                         navAxis.dataMax,
                         navAxis.max

commit 414346cbe6ccc9045950689b681bc40f6bd948ed

Regression since #10100, update pie with showInLegend

js/parts/Dynamics.js
@@ -1174,6 +1174,9 @@ extend(Series.prototype, /** @lends Series.prototype */ {
                     if (Object.keys(kinds).length) {
                         point.destroyElements(kinds);
                     }
+                    if (newOptions.showInLegend === false && point.legendItem) {
+                        chart.legend.destroyItem(point);
+                    }
                 }
             }, this);
         }

samples/unit-tests/series/update/demo.js
@@ -450,11 +450,12 @@ QUnit.test('Series.update color index, class name should change', function (asse
 
 QUnit.test('Series.update showInLegend dynamically', function (assert) {
 
-    var s = Highcharts.chart('container', {
-        series: [{
-            showInLegend: false
-        }]
-    }).series[0];
+    var chart = Highcharts.chart('container', {
+            series: [{
+                showInLegend: false
+            }]
+        }),
+        s = chart.series[0];
 
     s.update({
         pointStart: 100
@@ -471,6 +472,29 @@ QUnit.test('Series.update showInLegend dynamically', function (assert) {
         }),
         [100, 101, 102, 103],
         'Points should start from 100 (#7933)'
+    );
+
+    chart.addSeries({
+        type: 'pie',
+        data: [1, 2, 3],
+        showInLegend: true
+    });
+    s = chart.series[1];
+
+    assert.deepEqual(
+        s.points.map(p => typeof p.legendItem),
+        ['object', 'object', 'object'],
+        'Pie points should show in legend'
+    );
+
+    s.update({
+        showInLegend: false
+    });
+
+    assert.strictEqual(
+        chart.legend.allItems.length,
+        0,
+        'Pie points should no longer show in legend'
     );
 
 });