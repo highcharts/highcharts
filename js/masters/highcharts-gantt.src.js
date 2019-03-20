07fa279972cb0e8e9019e10910e7f13eb760f1b5
jon-a-nygaard <jon.arild@highsoft.com>
2019-03-20 16:12:15 +0100

    Add module and requires tags to masters

--- js/masters/highcharts-gantt.src.js
+++ js/masters/highcharts-gantt.src.js
@@ -1,5 +1,6 @@
 /**
  * @license @product.name@ JS v@product.version@ (@product.date@)
+ * @module highcharts/highcharts-gantt
  *
  * (c) 2017-2018 Lars Cabrera, Torstein Honsi, Jon Arild Nygard & Oystein Moseng
  *
