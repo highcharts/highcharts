ad50f4eda94b22d4240530aa0d346bab7fbacde7
Sebastian Bochan <sebastian@blacklabel.pl>
2019-05-08 14:21:57 +0200

    Fixed #10646, minPointLength draw zero points as negative.

--- samples/unit-tests/series-column/minpointlength/demo.js
+++ samples/unit-tests/series-column/minpointlength/demo.js
@@ -21,3 +21,25 @@ QUnit.test('Negative or positive minPointLength', function (assert) {
         'Not negative is there is no space in the yAxis (#7311)'
     );
 });
+
+QUnit.test('All zero values', function (assert) {
+    var chart = Highcharts.chart('container', {
+        chart: {
+            type: 'column'
+        },
+        plotOptions: {
+            series: {
+                minPointLength: 10
+            }
+        },
+        series: [{
+            data: [0, 0, 0, 0, 0]
+        }]
+    });
+
+    assert.strictEqual(
+        chart.series[0].points[0].shapeArgs.y < 145, // 145 value of negative point
+        true,
+        'Zero values are draw as positive columns (#10646)'
+    );
+});
