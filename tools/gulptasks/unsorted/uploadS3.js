41ccceea9f990a8bfb535f0d27bf844e7c479408
Jan Martin Langeland <jan.martin.langeland@highsoft.com>
2019-06-07 16:04:37 +0200

    Tools: Initial work on migration of ant.

--- tools/gulptasks/unsorted/uploadS3.js
+++ tools/gulptasks/unsorted/uploadS3.js
@@ -1,29 +1,57 @@
 const semver = require('semver');
 const fs = require('fs-extra');
 const glob = require('glob');
-const progressBar = require('../../progress-bar.js');
 const upload = require('../../upload');
 const log = require('../lib/log');
 const pkgJsonVersion = require(('../../../package.json')).version;
 
+/**
+ * Adds number of days to the given date.
+ * @param {Date} date to add to
+ * @param {integer} days to add to the date.
+ * @return {Date} the new date with the added days.
+ */
+function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+}
 
 const DIST_DIR = 'build/dist';
 const S3_BUCKET_NAME = process.env.HIGHCHARTS_S3_BUCKET; // TODO: support git-ignore-me.properties?
 
-// refactorer her
+const HTTP_MAX_AGE = {
+    oneDay: 86400,
+    month: 2592001,
+    fiveYears: 157680000
+};
+const TODAY = new Date();
+const HTTP_EXPIRES = {
+    // approximate dates
+    oneDay: addDays(TODAY, 1),
+    month: addDays(TODAY, 30),
+    fiveYears: addDays(TODAY, 365 * 5)
+};
 
 
 /**
  * Checks if source is a directory or system file.
  *
- * @param {source} the source path to check
- * @returns {boolean} true, if directory or system file.
+ * @param {string} source path to check
+ * @return {boolean} true, if directory or system file.
  */
 function isDirectoryOrSystemFile(source) {
     return fs.lstatSync(source).isDirectory() || source.indexOf('.') === 0;
 }
 
-
+/**
+ * Transforms a filepath to a similar named S3 destination path.
+ * @param {string} filePath to create a S3 destination path for.
+ * @param {string} localPath where the file resides. E.g 'highstock'. Will be substituted with cdnPath.
+ * @param {string} cdnPath where the files should be uploaded. E.g 'stock'.
+ * @param {string} version for the distribution/release
+ * @return {object} containing from and to parameters
+ */
 function toS3FilePath(filePath, localPath, cdnPath, version = false) {
     let toPath = filePath.replace(`${DIST_DIR}`, '').replace(`/${localPath}`, cdnPath).replace('/', '');
     if (version) {
@@ -34,7 +62,7 @@ function toS3FilePath(filePath, localPath, cdnPath, version = false) {
     log.message(`${filePath} --> ${S3_BUCKET_NAME}/${toPath}`);
 
     return {
-        from: filePath,
+        from: filePath.trim(),
         to: toPath
     };
 }
@@ -42,7 +70,7 @@ function toS3FilePath(filePath, localPath, cdnPath, version = false) {
 /**
  * Creates an array of the version paths that are used when uploading to S3.
  *
- * @param {version} version, typically from package.json.
+ * @param {string} version, typically from package.json.
  * @return {string[]} an array of paths where contents should be stored. E.g 7.1.1 as input would return ['7.1.1', '7.1', '7'].
  */
 function getVersionPaths(version = pkgJsonVersion) {
@@ -52,39 +80,28 @@ function getVersionPaths(version = pkgJsonVersion) {
         `${semver.major(version)}.${semver.minor(version)}${preleaseVersion}`,
         `${version}`
     ];
-
 }
 
 
 /**
  * Upload w/progress bar.
  *
- * @param {params} params containing batchSize, bucket, files, onError callback and callback per processed file.
+ * @param {object} params containing batchSize, bucket, files, onError callback and callback per processed file.
  * @return {Promise<any> | Promise | Promise} Promise to keep
  */
 function uploadFiles(params) {
     const { files, name } = params;
 
-    log.starting(`Uploading files to bucket ${S3_BUCKET_NAME}:\n`);
-
-    const bar = new progressBar({
-        error: '',
-        total: files.length,
-        message: `\n[:bar] - Uploaded :count of :total ${name ? name : ''} files. :error`
-    });
+    log.starting(`Uploading ${files.length} files for ${name} to bucket ${S3_BUCKET_NAME}:\n`);
 
     const defaultParams = {
-        batchSize: 50,
+        batchSize: 40,
         bucket: S3_BUCKET_NAME.replace('s3://', ''),
-        files,
         onError: err => {
-            bar.tick({
-                error: `File(s) errored:\n${err && err.message} ${err.from ? ' - ' + err.from : ''}`
-            });
+            log.failure(`File(s) errored:\n${err && err.message} ${err.from ? ' - ' + err.from : ''}`);
         },
         callback: (from, to) => {
             log.message(`Uploaded ${from} --> ${to}`);
-            // bar.tick();
         }
     };
 
@@ -94,7 +111,9 @@ function uploadFiles(params) {
         const { errors } = result;
         if (errors.length) {
             errors.forEach(err => log.failure(`Failed to process file ${err.from} --> ${err.to}`));
+            return Promise.reject(new Error(`${errors[0].message}: ${errors[0].from}`));
         }
+        return Promise.resolve(result);
     });
 }
 
@@ -102,70 +121,94 @@ function uploadFiles(params) {
 /**
  * Uploads files for a specific product to S3.
  *
- * @param {localPath} localPath where the files should be uploaded. E.g 'highstock'.
- * @param {cdnPath} cdnPath where the files should be uploaded. E.g 'stock'.
- * @param {name} name of the product, e.g "Highcharts Gantt"
- * @param {version} version for the distribution/release
+ * @param {string} localPath where the files should be uploaded. E.g 'highstock'.
+ * @param {string} cdnPath where the files should be uploaded. E.g 'stock'.
+ * @param {string} prettyName of the product, e.g "Highcharts Gantt"
+ * @param {string} version for the distribution/release
  * @return {Promise<any> | Promise | Promise} Promise to keep
  */
-function uploadProductFiles(localPath, cdnPath, name, version) {
-    // FIXME: set maxage && http.expires
+function uploadProductPackage(localPath, cdnPath, prettyName, version) {
+    const promises = [];
+    const fromDir = `${DIST_DIR}/${localPath}`;
+    const zipFilePaths = glob.sync(`${DIST_DIR}/${prettyName.replace(/ /g, '-')}-${version}.zip`);
+    const zipFile = {
+        from: zipFilePaths[0],
+        to: '/'
+    };
 
-    return new Promise((resolve, reject) => {
-        const fromDir = `${DIST_DIR}/${localPath}`;
-        const zipFilePaths = glob.sync(`${DIST_DIR}/${name.replace(/ /g, '-')}-${version}.zip`);
-        const zipFile = {
-            from: zipFilePaths[0],
-            to: '/'
-        };
+    const gfxFromDir = `${fromDir}/gfx`;
+    const gfxFiles = glob.sync(`${gfxFromDir}/**/*.*`);
+    const gfxFilesToRootDir = gfxFiles.map(file => toS3FilePath(file, localPath, cdnPath));
+    log.starting(`Preparing ${gfxFilesToRootDir.length} files in ', ${gfxFromDir}`);
 
-        const gfxFromDir = `${fromDir}/gfx`;
-        const gfxFiles = glob.sync(`${gfxFromDir}/**/*.*`);
-        const gfxFilesToRootPath = gfxFiles.map(file => toS3FilePath(file, localPath, cdnPath));
-        log.starting(`Preparing ${gfxFilesToRootPath.length} files in ', ${gfxFromDir}`);
+    const gzippedFileDir = `${fromDir}/js-gzip`;
+    if (!fs.existsSync(gzippedFileDir)) {
+        throw new Error(`Missing folder ${gzippedFileDir}`);
+    }
 
-        const gzippedFileDir = `${fromDir}/js-gzip`;
-        if (!fs.existsSync(gzippedFileDir)) {
-            reject(new Error(`Missing folder ${gzippedFileDir}`));
-        }
+    const gzippedFiles = glob.sync(`${gzippedFileDir}/**/*`);
+    const gzippedFilesToRootDir = gzippedFiles.map(file => toS3FilePath(file, localPath, cdnPath));
 
-        const gzippedFiles = glob.sync(`${gzippedFileDir}/**/*`);
-        const gzippedFilesToRoot = gzippedFiles.map(file => toS3FilePath(file, localPath, cdnPath));
+    log.starting(`Preparing ${gzippedFilesToRootDir.length} files in ', ${gzippedFileDir}`);
 
-        log.starting(`Preparing ${gzippedFilesToRoot.length} files in ', ${gzippedFileDir}`);
+    const versionPaths = getVersionPaths(version);
+    let gzippedFilesToVersionDir = [];
+    let gfxFilesToVersionedDir = [];
 
-        const versionPaths = getVersionPaths(version);
-        let versionedFolderFiles = [];
-        versionPaths.forEach(versionPath => {
-            // ideally only a single folder should be uploaded while the rest should be copied.
-            const gzippedFilesToVersionDir = gzippedFiles.map(file => toS3FilePath(file, localPath, cdnPath, versionPath));
-            const gfxFilesToVersionedDir = gfxFiles.map(file => toS3FilePath(file, localPath, cdnPath, versionPath));
-            versionedFolderFiles = versionedFolderFiles.concat(gzippedFilesToVersionDir).concat(gfxFilesToVersionedDir);
-        });
-
-        const files = [
-            zipFile,
-            ...gfxFilesToRootPath,
-            ...gzippedFilesToRoot,
-            ...versionedFolderFiles
-        ].filter(path => !isDirectoryOrSystemFile(path.from));
-
-        uploadFiles({ files, name })
-            .then(
-                result => {
-                    log.success(`Finished uploading ${name} files!`);
-                    resolve(result);
-                },
-                err => {
-                    log.failure(`Failed when uploading ${name} files. Reason: ${err && err.message}`);
-                    reject(err);
-                }
-            );
+    versionPaths.forEach(versionPath => {
+        gzippedFilesToVersionDir = [...gzippedFilesToVersionDir, ...gzippedFiles.map(file => toS3FilePath(file, localPath, cdnPath, versionPath))];
+        gfxFilesToVersionedDir = [...gfxFilesToVersionedDir, ...gfxFiles.map(file => toS3FilePath(file, localPath, cdnPath, versionPath))];
     });
+
+    // TODO: need to set charset to UTF-8?
+    promises.push(uploadFiles({
+        files: [zipFile],
+        name: prettyName
+    }));
+
+    promises.push(uploadFiles({
+        files: gzippedFilesToRootDir.filter(path => !isDirectoryOrSystemFile(path.from)),
+        name: prettyName,
+        s3Params: {
+            CacheControl: `public, max-age=${HTTP_MAX_AGE.oneDay}`,
+            Expires: HTTP_EXPIRES.oneDay,
+            ContentEncoding: 'gzip'
+        }
+    }));
+
+    promises.push(uploadFiles({
+        files: gfxFilesToRootDir.filter(path => !isDirectoryOrSystemFile(path.from)),
+        name: prettyName,
+        s3Params: {
+            CacheControl: `public, max-age=${HTTP_MAX_AGE.oneDay}`,
+            Expires: HTTP_EXPIRES.oneDay
+        }
+    }));
+
+    promises.push(uploadFiles({
+        files: gzippedFilesToVersionDir.filter(path => !isDirectoryOrSystemFile(path.from)),
+        name: prettyName,
+        s3Params: {
+            CacheControl: `public, max-age=${HTTP_MAX_AGE.fiveYears}`,
+            Expires: HTTP_EXPIRES.fiveYears,
+            ContentEncoding: 'gzip'
+        }
+    }));
+
+    promises.push(uploadFiles({
+        files: gfxFilesToVersionedDir.filter(path => !isDirectoryOrSystemFile(path.from)),
+        name: prettyName,
+        s3Params: {
+            CacheControl: `public, max-age=${HTTP_MAX_AGE.fiveYears}`,
+            Expires: HTTP_EXPIRES.fiveYears
+        }
+    }));
+
+    return Promise.all(promises);
 }
 
 
 module.exports = {
     uploadFiles,
-    uploadProductFiles
+    uploadProductPackage
 };
