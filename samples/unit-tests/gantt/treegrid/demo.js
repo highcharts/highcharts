highcharts/highcharts

commit 68cf7d22a3edfe4997d9fd0db28beb22d74ecfd9

Gantt: fixed hiding categories with series, improved readability.

js/parts-gantt/TreeGrid.js
@@ -485,18 +485,19 @@ var onBeforeRender = function (e) {
                 removeFoundExtremesEvent,
                 uniqueNames = options.uniqueNames,
                 numberOfSeries = 0,
-                isDirtyData,
+                isDirty,
                 data,
                 treeGrid;
             // Check whether any of series is rendering for the first time,
-            // or its data is dirty, and only then update. #10570, #10580
-            axis.series.forEach(function (series) {
-                isDirtyData = isDirtyData ||
-                    !series.hasRendered ||
-                    series.isDirtyData;
+            // visibility has changed, or its data is dirty,
+            // and only then update. #10570, #10580
+            isDirty = axis.series.some(function (series) {
+                return !series.hasRendered ||
+                    series.isDirtyData ||
+                    series.isDirty;
             });
 
-            if (isDirtyData) {
+            if (isDirty) {
                 // Concatenate data from all series assigned to this axis.
                 data = axis.series.reduce(function (arr, s) {
                     if (s.visible) {

samples/unit-tests/gantt/treegrid/demo.js
@@ -256,7 +256,7 @@ QUnit.test('Series.setVisible', assert => {
     );
     assert.strictEqual(
         series3.points[0].y,
-        2,
+        1,
         'should have "Point 3" y-value equal 1  when "Series 2" is hidden.'
     );
     assert.strictEqual(
@@ -266,7 +266,7 @@ QUnit.test('Series.setVisible', assert => {
     );
     assert.deepEqual(
         [axis.min, axis.max],
-        [0, 2],
+        [0, 1],
         'should have axis [min, max] equal [0, 1]  when "Series 2" is hidden.'
     );
 

commit 29cadd90dfc90fa488547a5ca1ec3fca00b0e1b2

Fixed #10887, treegrid update regression
Regression since #10846. isDirty did not account for axis destroying properties on update.

js/parts-gantt/TreeGrid.js
@@ -491,11 +491,15 @@ var onBeforeRender = function (e) {
             // Check whether any of series is rendering for the first time,
             // visibility has changed, or its data is dirty,
             // and only then update. #10570, #10580
-            isDirty = axis.series.some(function (series) {
-                return !series.hasRendered ||
-                    series.isDirtyData ||
-                    series.isDirty;
-            });
+            // Also check if mapOfPosToGridNode exists. #10887
+            isDirty = (
+                !axis.mapOfPosToGridNode ||
+                axis.series.some(function (series) {
+                    return !series.hasRendered ||
+                        series.isDirtyData ||
+                        series.isDirty;
+                })
+            );
 
             if (isDirty) {
                 // Concatenate data from all series assigned to this axis.

samples/unit-tests/gantt/treegrid/demo.js
@@ -156,6 +156,64 @@ QUnit.test('Tree.getNode', function (assert) {
     );
 });
 
+QUnit.test('Axis.update', assert => {
+    const { getStyle } = Highcharts;
+    const { yAxis: [axis] } = Highcharts.chart('container', {
+        series: [{
+            data: [{
+                name: 'Point 1',
+                y: 1
+            }]
+        }],
+        yAxis: {
+            type: 'treegrid',
+            labels: {
+                style: {
+                    color: '#000000'
+                }
+            }
+        }
+    });
+    let { ticks: { 0: { label: { element } } } } = axis;
+
+
+    assert.deepEqual(
+        axis.tickPositions,
+        [0],
+        'Should have tickPositions equal [0] after render.'
+    );
+    assert.strictEqual(
+        getStyle(element, 'color', false),
+        'rgb(0, 0, 0)',
+        'Should have color equal rgb(0, 0, 0) after render.'
+    );
+
+
+    // Update the axis
+    axis.update({
+        labels: {
+            style: {
+                color: '#ff0000'
+            }
+        }
+    });
+
+    // Update reference to label element
+    ({ ticks: { 0: { label: { element } } } } = axis);
+
+
+    assert.deepEqual(
+        axis.tickPositions,
+        [0],
+        'Should still have tickPositions equal [0] after update.'
+    );
+    assert.strictEqual(
+        getStyle(element, 'color', false),
+        'rgb(255, 0, 0)',
+        'Should have color equal rgb(255, 0, 0) after update.'
+    );
+});
+
 QUnit.test('Chart.addSeries', assert => {
     const chart = Highcharts.chart('container', {
         yAxis: [{