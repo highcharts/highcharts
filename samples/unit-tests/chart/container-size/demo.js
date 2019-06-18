74ba76e3e626465798defa513033ac53957a5327
Torstein Hønsi <torstein@highsoft.com>
2019-06-18 14:10:57 +0200

    Fixed #10498, wrong chart width if parent had transform style

--- samples/unit-tests/chart/container-size/demo.js
+++ samples/unit-tests/chart/container-size/demo.js
@@ -70,13 +70,18 @@ QUnit.test('10px height (#6217)', function (assert) {
     }
 });
 
-QUnit.test('Transform (#9871)', assert => {
-    const container = document.createElement('div');
-    container.style.transform = 'scale(0.7)';
-    container.style.width = '400px';
-    document.getElementById('container').appendChild(container);
+QUnit.test('Transformed container parents', assert => {
+    const container1 = document.createElement('div');
+    container1.style.transform = 'scale(0.7)';
+    container1.style.width = '400px';
+    document.getElementById('container').appendChild(container1);
 
-    const chart = Highcharts.chart(container, {
+    const container2 = document.createElement('div');
+    container2.style.transform = 'scale(0.7)';
+    container2.style.width = '400px';
+    container1.appendChild(container2);
+
+    const chart = Highcharts.chart(container2, {
         series: [{
             data: [1, 3, 2, 4]
         }]
@@ -86,10 +91,11 @@ QUnit.test('Transform (#9871)', assert => {
         assert.strictEqual(
             chart.chartWidth,
             400,
-            "Transforms shouln't confuse the chart sizing"
+            "Transforms shouln't confuse the chart sizing (#9871, #10498)"
         );
     } finally {
         chart.destroy();
-        container.parentNode.removeChild(container);
+        container2.parentNode.removeChild(container2);
+        container1.parentNode.removeChild(container1);
     }
 });
