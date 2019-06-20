35039d9ca5023208ed71455d296b3dff4b3c6833
sigrunnu <sigrun.nummedal@gmail.com>
2019-06-20 14:40:56 +0200

    Tests: Moved issue test #4291 to unit-tests.

--- samples/unit-tests/axis/labels/demo.js
+++ samples/unit-tests/axis/labels/demo.js
@@ -1,3 +1,21 @@
+QUnit.test('Label formatting(#4291)', function (assert) {
+    var chart = $('#container').highcharts({
+        series: [{
+            data: [0, 79962.57, 9e4]
+        }],
+        yAxis: {
+            tickPositions: [0, 79962.57, 9e4]
+        }
+    }).highcharts();
+
+    assert.strictEqual(
+        chart.yAxis[0].ticks['79962.57'].label.textStr,
+        '79 962.57',
+        'Preserved decimals'
+    );
+
+});
+
 QUnit.test('Label height and ellipsis on update(#4070)', function (assert) {
     // series dataLabels can start on or off -- the problem seems to be in the update redraw
     var labelsOn = true;
