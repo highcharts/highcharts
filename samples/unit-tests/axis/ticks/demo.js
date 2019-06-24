6f5b9ce9c1e839b3f145ec95c0cf5be4b34463ad
sigrunnu <sigrun.nummedal@gmail.com>
2019-06-24 15:23:38 +0200

    Tests: Moved issue test #4442 to uniut-tests.

--- samples/unit-tests/axis/ticks/demo.js
+++ samples/unit-tests/axis/ticks/demo.js
@@ -1,3 +1,25 @@
+QUnit.test('alignTicks should consider only axes with series.(#4442)', function (assert) {
+    var chart = $('#container').highcharts({
+        yAxis: [{
+            endOnTick: false,
+            maxPadding: 0.0
+        }, {
+            endOnTick: false,
+            maxPadding: 0.0
+        }],
+        series: [{
+            data: [991, 455],
+            yAxis: 1
+        }]
+    }).highcharts();
+
+    assert.strictEqual(
+        chart.series[0].data[0].plotY,
+        0,
+        'Without extra padding'
+    );
+
+});
 QUnit.test('Ticks for a single point.', function (assert) {
     var chart = Highcharts.chart('container', {
         yAxis: {
