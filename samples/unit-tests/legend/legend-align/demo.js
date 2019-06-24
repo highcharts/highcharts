ee74deef04bcd6daa779ebfebc77ea176e8d91e2
sigrunnu <sigrun.nummedal@gmail.com>
2019-06-24 14:01:40 +0200

    Tests: Moved issue test #4449 to unit-tests.

--- samples/unit-tests/legend/legend-align/demo.js
+++ samples/unit-tests/legend/legend-align/demo.js
@@ -1,3 +1,39 @@
+QUnit.test('Legend rtl and useHTML(#4449)', function (assert) {
+
+    var ren = new Highcharts.Renderer(
+        document.getElementById('container'),
+        500,
+        300
+    );
+
+    // Reference point
+    ren.circle(100, 100, 3)
+        .attr({
+            fill: 'red'
+        })
+        .add();
+
+    // Add an empty text with useHTML, align it to the right
+    var text = ren.text('', 100, 100, true)
+        .attr({
+            align: 'right'
+        })
+        .add();
+
+    // Update the text
+    text.attr({
+        text: 'Hello World'
+    });
+
+
+    assert.strictEqual(
+        text.element.offsetLeft + text.element.offsetWidth,
+        100,
+        'Text is right aligned'
+    );
+
+});
+
 QUnit.test('Legend vertical align top with no title', function (assert) {
     var chart = Highcharts.chart('container', {
         chart: {
