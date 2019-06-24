f971b39038a98e48905e784c7cd92f6d2d1d35de
sigrunnu <sigrun.nummedal@gmail.com>
2019-06-24 15:28:49 +0200

    Tests: Moved issue tests #4485 to unit-tests.

--- samples/unit-tests/axis/ticks/demo.js
+++ samples/unit-tests/axis/ticks/demo.js
@@ -1,3 +1,48 @@
+QUnit.test('Ticks were drawn in break(#4485)', function (assert) {
+
+    $('#container').highcharts({
+        title: {
+            text: 'Sample of a simple break'
+        },
+        subtitle: {
+            text: 'Line should be interrupted between 5 and 10'
+        },
+        xAxis: {
+            type: 'datetime',
+            tickInterval: 1,
+            breaks: [{
+                from: 5,
+                to: 10,
+                breakSize: 1
+            }],
+            labels: {
+                format: '{value}{value}{value}'
+            }
+
+        },
+        series: [{
+            gapSize: 1,
+            data: (function () {
+                var data = [],
+                    i;
+                for (i = 0; i < 20; i = i + 1) {
+                    data.push(i);
+                }
+                return data;
+            }())
+        }]
+    });
+
+    var chart = $('#container').highcharts();
+
+    assert.strictEqual(
+        chart.xAxis[0].tickPositions.join(','),
+        '0,1,2,3,4,5,10,11,12,13,14,15,16,17,18,19',
+        'Skip ticks in break'
+    );
+
+});
+
 QUnit.test('alignTicks should consider only axes with series.(#4442)', function (assert) {
     var chart = $('#container').highcharts({
         yAxis: [{
