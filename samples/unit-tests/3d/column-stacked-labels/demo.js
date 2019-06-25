d8d12ad9079e54a215ff9312d864f434303a0334
sigrunnu <sigrun.nummedal@gmail.com>
2019-06-25 08:24:30 +0200

    Tests: Moved issue test #4532 to unit-tests.

--- samples/unit-tests/3d/column-stacked-labels/demo.js
+++ samples/unit-tests/3d/column-stacked-labels/demo.js
@@ -1,3 +1,33 @@
+QUnit.test("JS error when stack ID's are strings in Highcharts 3D.(#4532)", function (assert) {
+    var chart = $('#container').highcharts({
+        chart: {
+            type: 'column',
+            options3d: {
+                enabled: true,
+                alpha: 6,
+                beta: 15,
+                viewDistance: 0,
+                depth: 40
+            }
+        },
+        series: [{
+            data: [5, 10],
+            stack: "m1"
+        }, {
+            data: [2, 4],
+            stack: "m1"
+        }]
+    }).highcharts();
+
+
+    assert.strictEqual(
+        chart.series[0].points && chart.series[0].points.length === 2,
+        true,
+        'No error.'
+    );
+
+});
+
 QUnit.test('3D columns stackLabels render', function (assert) {
     var chart = new Highcharts.Chart({
         chart: {
