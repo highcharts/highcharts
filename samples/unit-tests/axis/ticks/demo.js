b787a8817032e995bd89b58ab0b84fc72373464b
sigrunnu <sigrun.nummedal@gmail.com>
2019-06-25 10:03:17 +0200

    Tests: Moved issue test #4184 to unit-tests.

--- samples/unit-tests/axis/ticks/demo.js
+++ samples/unit-tests/axis/ticks/demo.js
@@ -1,3 +1,27 @@
+QUnit.test("tickInterval option should take precedence over data range(#4184)", function (assert) {
+    var chart = $("#container").highcharts({
+        xAxis: {
+            min: 0,
+            max: 12,
+            tickInterval: 1
+        },
+        series: [{
+            type: 'column',
+            //pointRange: 1,
+            data: [
+                [7, 7],
+                [10, 8]
+            ]
+        }]
+    }).highcharts();
+
+    assert.strictEqual(
+        chart.xAxis[0].tickInterval,
+        1,
+        'Actual tick interval is as option'
+    );
+});
+
 QUnit.test('Ticks for a single point.', function (assert) {
     var chart = Highcharts.chart('container', {
         yAxis: {
