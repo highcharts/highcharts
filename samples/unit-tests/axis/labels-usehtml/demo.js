6d5e617b449965370bec5d32dba12e3df7db0584
sigrunnu <sigrun.nummedal@gmail.com>
2019-06-26 10:34:24 +0200

    Tests: Moved issue test #4938 to unit-tests.

--- samples/unit-tests/axis/labels-usehtml/demo.js
+++ samples/unit-tests/axis/labels-usehtml/demo.js
@@ -1,3 +1,32 @@
+QUnit.test('Hide label with useHTML (#4938)', function (assert) {
+    var chart = Highcharts.chart('container', {}),
+        renderer = chart.renderer,
+        g = renderer.g().add(),
+        text = renderer.text('Label', 140, 140, true).add(g);
+    assert.strictEqual(
+        text.attr('visibility'),
+        0,
+        'Text element is visible'
+    );
+    assert.strictEqual(
+        g.attr('visibility'),
+        0,
+        'Group element is visible'
+    );
+    text.hide();
+    assert.strictEqual(
+        text.attr('visibility'),
+        'hidden',
+        'Text element is hidden'
+    );
+    g.hide();
+    assert.strictEqual(
+        g.attr('visibility'),
+        'hidden',
+        'Group element is hidden'
+    );
+});
+
 QUnit.test("Use HTML in color axis data labels caused misposition.(#4615)", function (assert) {
     var chart = $('#container').highcharts({
 
