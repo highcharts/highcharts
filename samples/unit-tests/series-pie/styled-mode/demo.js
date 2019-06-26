76ed44df8bcab4550d5e334de22175f6d7b30925
sigrunnu <sigrun.nummedal@gmail.com>
2019-06-26 14:02:48 +0200

    Requested change fixed: Moved issue test #1828

--- samples/unit-tests/series-pie/styled-mode/demo.js
+++ samples/unit-tests/series-pie/styled-mode/demo.js
@@ -1,24 +1,3 @@
-QUnit.test('Pie borderColor null(#1828)', function (assert) {
-    var chart = $("#container").highcharts({
-        chart: {
-            type: 'pie'
-        },
-        series: [{
-            data: [1, 2, 3, 4, 5],
-            borderColor: null,
-            borderWidth: 1
-        }]
-    }).highcharts();
-
-    Highcharts.each(chart.series[0].points, function (point, i) {
-        assert.equal(
-            point.graphic.element.getAttribute('stroke'),
-            point.graphic.element.getAttribute('fill'),
-            'Point ' + i + ' has correct stroke'
-        );
-    });
-});
-
 QUnit.test('Styled mode for pie type series', function (assert) {
     var chart = Highcharts.chart('container', {
             chart: {
