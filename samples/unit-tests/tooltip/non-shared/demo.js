10d298cab5a1667140d1815dc58424d4c777e772
sigrunnu <sigrun.nummedal@gmail.com>
2019-06-27 11:17:38 +0200

    Tests: Moved issue-test #5721 to unit-tests.

---
+++ samples/unit-tests/tooltip/non-shared/demo.js
@@ -0,0 +1,34 @@
+QUnit.test('Non-shared tooltip should highlight the clostest point (#5721)', function (assert) {
+
+    var $container = $('#container'),
+        chart = $container.highcharts({
+            series: [{
+                data: [
+                    [0, 2],
+                    [0.51, 2]
+                ]
+            }, {
+                data: [
+                    [0.5, 1],
+                    [1, 1]
+                ]
+            }]
+        }).highcharts(),
+        offset = $container.offset(),
+        left = offset.left + chart.plotLeft,
+        top = offset.top + chart.plotTop,
+        point = chart.series[0].points[1];
+
+    chart.pointer.onContainerMouseMove({
+        type: 'mousemove',
+        pageX: left + point.plotX - 5,
+        pageY: top + point.plotY - 5,
+        target: point.series.group.element
+    });
+
+    assert.strictEqual(
+        chart.hoverPoint && chart.hoverPoint.x,
+        point.x,
+        'Correct point hovered.'
+    );
+});
