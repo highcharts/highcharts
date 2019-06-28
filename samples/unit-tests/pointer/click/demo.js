f3bcb9ede0422441603f878c6de1cee01f1cdb7c
sigrunnu <sigrun.nummedal@gmail.com>
2019-06-28 10:54:44 +0200

    Requested change fixed: wrapped #5105 in an iife

--- samples/unit-tests/pointer/click/demo.js
+++ samples/unit-tests/pointer/click/demo.js
@@ -1,68 +1,71 @@
-function test(type, tooltipEnabled) {
-    QUnit.test('Click event on ' + type + ' with tooltip enabled = ' + tooltipEnabled + ' (#5101)', function (assert) {
+(function () {
+    function test(type, tooltipEnabled) {
+        QUnit.test('Click event on ' + type + ' with tooltip enabled = ' + tooltipEnabled + ' (#5101)', function (assert) {
 
-        var chart = Highcharts.chart('container', {
+            var chart = Highcharts.chart('container', {
 
-            chart: {
-                type: type,
-                animation: false
-            },
+                chart: {
+                    type: type,
+                    animation: false
+                },
 
-            tooltip: {
-                enabled: tooltipEnabled
-            },
+                tooltip: {
+                    enabled: tooltipEnabled
+                },
 
-            plotOptions: {
-                series: {
-                    point: {
-                        events: {
-                            click: function () {
-                                this.update({
-                                    y: this.y + 1
-                                });
+                plotOptions: {
+                    series: {
+                        point: {
+                            events: {
+                                click: function () {
+                                    this.update({
+                                        y: this.y + 1
+                                    });
+                                }
                             }
                         }
                     }
-                }
-            },
+                },
 
-            series: [{
-                data: [1, 1, 1]
-            }]
+                series: [{
+                    data: [1, 1, 1]
+                }]
+            });
+            assert.strictEqual(
+                chart.series[0].points[0].y,
+                1,
+                'Initial value'
+            );
+
+            chart.series[0].points[0].onMouseOver();
+            chart.pointer.onContainerClick({
+                target: chart.series[0].points[0].graphic.element
+            });
+
+            assert.strictEqual(
+                chart.series[0].points[0].y,
+                2,
+                'First click'
+            );
+
+            chart.pointer.onContainerClick({
+                target: chart.series[0].points[0].graphic.element
+            });
+
+            assert.strictEqual(
+                chart.series[0].points[0].y,
+                3,
+                'Second click without leaving'
+            );
         });
-        assert.strictEqual(
-            chart.series[0].points[0].y,
-            1,
-            'Initial value'
-        );
+    }
 
-        chart.series[0].points[0].onMouseOver();
-        chart.pointer.onContainerClick({
-            target: chart.series[0].points[0].graphic.element
-        });
 
-        assert.strictEqual(
-            chart.series[0].points[0].y,
-            2,
-            'First click'
-        );
-
-        chart.pointer.onContainerClick({
-            target: chart.series[0].points[0].graphic.element
-        });
-
-        assert.strictEqual(
-            chart.series[0].points[0].y,
-            3,
-            'Second click without leaving'
-        );
-    });
-}
-
-test('pie', true);
-test('pie', false);
-test('column', true);
-test('column', false);
+    test('pie', true);
+    test('pie', false);
+    test('column', true);
+    test('column', false);
+}());
 
 QUnit.test('Click to add', function (assert) {
 
