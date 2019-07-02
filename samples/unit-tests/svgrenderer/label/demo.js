aeff14ee38b46d8ab955d874ca96c32a3956bff2
Dmitri Pantsenko <dmitripantsenko@gmail.com>
2019-07-02 09:41:22 +0200

    Tests: Moved issue #4324 (fixed)

--- samples/unit-tests/svgrenderer/label/demo.js
+++ samples/unit-tests/svgrenderer/label/demo.js
@@ -1,3 +1,50 @@
+QUnit.test('Image labels should have no fill (#4324)', function (assert) {
+
+    var ren = new Highcharts.Renderer(
+        document.getElementById('container'),
+        500,
+        300
+    );
+
+    var image = ren.label(
+        '',
+        100,
+        100,
+        'url(https://smartview.antaris-solutions.net//images/icons/view_alerts.png)'
+    )
+        .attr({
+            'stroke-width': 1,
+            stroke: 'blue'
+        })
+        .add();
+
+    assert.strictEqual(
+        image.box.element.getAttribute('fill'),
+        null,
+        'No fill for image'
+    );
+
+
+    var circle = ren.label(
+        '',
+        150,
+        100,
+        'circle'
+    )
+        .attr({
+            'stroke-width': 2,
+            stroke: 'blue'
+        })
+        .add();
+
+
+    assert.strictEqual(
+        circle.box.element.getAttribute('fill'),
+        'none',
+        'Fill none for circle'
+    );
+});
+
 QUnit.test('New label with rect symbol (#5324)', function (assert) {
     var renderer = new Highcharts.Renderer(
         document.getElementById('container'),
