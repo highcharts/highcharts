60d88540b6a9038e489c410c8ad189f0c34a2d6b
Dmitri Pantsenko <dmitripantsenko@gmail.com>
2019-07-03 13:22:24 +0200

    Tests: Moved issue test #5050

---
+++ samples/unit-tests/maps/setdata-svg/demo.js
@@ -0,0 +1,15 @@
+QUnit.test("Using setData with allAreas=true should not cause errors (#5050)", function (assert) {
+    assert.expect(0);
+    var chart = Highcharts.mapChart("container", {
+        series: [{
+            data: [],
+            mapData: Highcharts.maps['custom/world-palestine-highres'],
+            joinBy: 'hc-key'
+        }]
+    });
+
+    chart.series[0].setData([{
+        "hc-key": "in",
+        value: 10000
+    }]);
+});
