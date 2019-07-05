b5f7d0ca73b59390d47f895195b4511237da7df7
Dmitri Pantsenko <dmitripantsenko@gmail.com>
2019-07-05 09:13:59 +0200

    Tests: Moved issue test #4247

---
+++ samples/unit-tests/series-column/zoom-in-break/demo.js
@@ -0,0 +1,44 @@
+QUnit.test('Sub-millisecond tooltip (#4247)', function (assert) {
+    var chart;
+
+    $('#container').highcharts({
+        title: {
+            text: "Y axis translation failed"
+        },
+        legend: {
+            enabled: false
+        },
+        xAxis: {
+            min: 5
+        },
+        yAxis: {
+            breaks: [{
+                from: 3,
+                to: 8,
+                breakSize: 1
+            }]
+        },
+        chart: {
+            type: "column",
+            zoomType: "x"
+        },
+        series: [{
+            data: [9, 8, 7, 6, 5, 4, 3, 2, 1]
+        }]
+    });
+
+    chart = $('#container').highcharts();
+
+
+    assert.equal(
+        chart.yAxis[0].min,
+        0,
+        'Min makes sense'
+    );
+    assert.equal(
+        chart.yAxis[0].max,
+        3,
+        'Max stops at break'
+    );
+
+});
