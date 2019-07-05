09cca6ecd20d681125c1f3535b704edec980ba1e
Dmitri Pantsenko <dmitripantsenko@gmail.com>
2019-07-05 10:47:20 +0200

    Tests: Moved issue test #2908

--- samples/unit-tests/navigator/navigator/demo.js
+++ samples/unit-tests/navigator/navigator/demo.js
@@ -1,3 +1,21 @@
+QUnit.test("Handles should not be overlapped by xAxis labels (#2908)", function (assert) {
+    var chart = $('#container').highcharts('StockChart', {
+        navigator: {
+            height: 20
+        },
+        series: [{
+            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+        }]
+    }).highcharts();
+
+
+    assert.strictEqual(
+        chart.scroller.handles[0].zIndex >= chart.scroller.xAxis.labelGroup.zIndex,
+        true,
+        "Labels no overlap handles"
+    );
+});
+
 QUnit.test('Navigator (#4053)', function (assert) {
     var chart = new Highcharts.StockChart({
         chart: {
