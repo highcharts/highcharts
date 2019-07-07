ff88e5efcd3ab5c7b521f5f9a1c03787332b8370
bre1470 <40056287+bre1470@users.noreply.github.com>
2019-07-07 17:42:15 +0200

    Tools: Migrated tree-series mixin to TypeScript.

--- ts/mixins/tree-series.ts
+++ ts/mixins/tree-series.ts
@@ -3,36 +3,88 @@
  *  !!!!!!! SOURCE GETS TRANSPILED BY TYPESCRIPT. EDIT TS FILE ONLY. !!!!!!!
  *
  * */
+
 import H from '../parts/Globals.js';
+
+/**
+ * Internal types
+ * @private
+ */
+declare global {
+    namespace Highcharts {
+        interface TreeColorObject {
+            color: ColorString;
+            colorIndex: number;
+        }
+        interface TreeSeriesMixin {
+            getColor( // @todo
+                node: any,
+                options: any
+            ): Highcharts.TreeColorObject;
+            getLevelOptions(params: any): (Dictionary<any>|null); // @todo
+            setTreeValues(tree: any, options: any): any; // @todo
+            updateRootId(series: any): string; // @todo
+        }
+    }
+}
+
 import U from '../parts/Utilities.js';
-var isArray = U.isArray, isNumber = U.isNumber;
-var extend = H.extend, isBoolean = function (x) {
-    return typeof x === 'boolean';
-}, isFn = function (x) {
-    return typeof x === 'function';
-}, isObject = H.isObject, merge = H.merge, pick = H.pick;
+var isArray = U.isArray,
+    isNumber = U.isNumber;
+
+var extend = H.extend,
+    isBoolean = function (x: unknown): x is boolean {
+        return typeof x === 'boolean';
+    },
+    isFn = function (x: unknown): x is Function {
+        return typeof x === 'function';
+    },
+    isObject = H.isObject,
+    merge = H.merge,
+    pick = H.pick;
+
 /* eslint-disable valid-jsdoc */
+
 /**
  * @todo Combine buildTree and buildNode with setTreeValues
  * @todo Remove logic from Treemap and make it utilize this mixin.
  * @private
  */
-var setTreeValues = function setTreeValues(tree, options) {
-    var before = options.before, idRoot = options.idRoot, mapIdToNode = options.mapIdToNode, nodeRoot = mapIdToNode[idRoot], levelIsConstant = (isBoolean(options.levelIsConstant) ?
-        options.levelIsConstant :
-        true), points = options.points, point = points[tree.i], optionsPoint = point && point.options || {}, childrenTotal = 0, children = [], value;
+var setTreeValues = function setTreeValues(
+    tree: any,
+    options: any
+): any {
+    var before = options.before,
+        idRoot = options.idRoot,
+        mapIdToNode = options.mapIdToNode,
+        nodeRoot = mapIdToNode[idRoot],
+        levelIsConstant = (
+            isBoolean(options.levelIsConstant) ?
+                options.levelIsConstant :
+                true
+        ),
+        points = options.points,
+        point = points[tree.i],
+        optionsPoint = point && point.options || {},
+        childrenTotal = 0,
+        children = [] as Array<any>,
+        value;
+
     extend(tree, {
         levelDynamic: tree.level - (levelIsConstant ? 0 : nodeRoot.level),
         name: pick(point && point.name, ''),
-        visible: (idRoot === tree.id ||
-            (isBoolean(options.visible) ? options.visible : false))
+        visible: (
+            idRoot === tree.id ||
+            (isBoolean(options.visible) ? options.visible : false)
+        )
     });
     if (isFn(before)) {
         tree = before(tree, options);
     }
     // First give the children some values
-    tree.children.forEach(function (child, i) {
+    tree.children.forEach(function (child: any, i: number): void {
         var newOptions = extend({}, options);
+
         extend(newOptions, {
             index: i,
             siblings: tree.children.length,
@@ -55,44 +107,86 @@ var setTreeValues = function setTreeValues(tree, options) {
     });
     return tree;
 };
+
 /**
  * @private
  */
-var getColor = function getColor(node, options) {
-    var index = options.index, mapOptionsToLevel = options.mapOptionsToLevel, parentColor = options.parentColor, parentColorIndex = options.parentColorIndex, series = options.series, colors = options.colors, siblings = options.siblings, points = series.points, getColorByPoint, chartOptionsChart = series.chart.options.chart, point, level, colorByPoint, colorIndexByPoint, color, colorIndex;
+var getColor = function getColor(
+    node: any,
+    options: any
+): Highcharts.TreeColorObject {
+    var index = options.index,
+        mapOptionsToLevel = options.mapOptionsToLevel,
+        parentColor = options.parentColor,
+        parentColorIndex = options.parentColorIndex,
+        series = options.series,
+        colors = options.colors,
+        siblings = options.siblings,
+        points = series.points,
+        getColorByPoint,
+        chartOptionsChart = series.chart.options.chart,
+        point,
+        level: Highcharts.Dictionary<any>,
+        colorByPoint,
+        colorIndexByPoint,
+        color,
+        colorIndex;
+
     /**
      * @private
      */
-    function variation(color) {
+    function variation(color: Highcharts.ColorString): Highcharts.ColorString {
         var colorVariation = level && level.colorVariation;
+
         if (colorVariation) {
             if (colorVariation.key === 'brightness') {
-                return H.color(color).brighten(colorVariation.to * (index / siblings)).get();
+                return H.color(color).brighten(
+                    colorVariation.to * (index / siblings)
+                ).get() as any;
             }
         }
+
         return color;
     }
+
     if (node) {
         point = points[node.i];
         level = mapOptionsToLevel[node.level] || {};
         getColorByPoint = point && level.colorByPoint;
+
         if (getColorByPoint) {
             colorIndexByPoint = point.index % (colors ?
                 colors.length :
-                chartOptionsChart.colorCount);
+                chartOptionsChart.colorCount
+            );
             colorByPoint = colors && colors[colorIndexByPoint];
         }
+
         // Select either point color, level color or inherited color.
         if (!series.chart.styledMode) {
-            color = pick(point && point.options.color, level && level.color, colorByPoint, parentColor && variation(parentColor), series.color);
+            color = pick(
+                point && point.options.color,
+                level && level.color,
+                colorByPoint,
+                parentColor && variation(parentColor),
+                series.color
+            );
         }
-        colorIndex = pick(point && point.options.colorIndex, level && level.colorIndex, colorIndexByPoint, parentColorIndex, options.colorIndex);
+
+        colorIndex = pick(
+            point && point.options.colorIndex,
+            level && level.colorIndex,
+            colorIndexByPoint,
+            parentColorIndex,
+            options.colorIndex
+        );
     }
     return {
         color: color,
         colorIndex: colorIndex
     };
 };
+
 /**
  * Creates a map from level number to its given options.
  *
@@ -109,22 +203,36 @@ var getColor = function getColor(node, options) {
  * @return {Highcharts.Dictionary<object>|null}
  *         Returns a map from level number to its given options.
  */
-var getLevelOptions = function getLevelOptions(params) {
-    var result = null, defaults, converted, i, from, to, levels;
+var getLevelOptions = function getLevelOptions(
+    params: any
+): (Highcharts.Dictionary<any>|null) {
+    var result = null,
+        defaults: any,
+        converted,
+        i,
+        from: any,
+        to,
+        levels;
+
     if (isObject(params)) {
-        result = {};
+        result = {} as any;
         from = isNumber(params.from) ? params.from : 1;
         levels = params.levels;
-        converted = {};
+        converted = {} as any;
         defaults = isObject(params.defaults) ? params.defaults : {};
         if (isArray(levels)) {
-            converted = levels.reduce(function (obj, item) {
-                var level, levelIsConstant, options;
+            converted = levels.reduce(function (obj: any, item: any): any {
+                var level,
+                    levelIsConstant,
+                    options: any;
+
                 if (isObject(item) && isNumber(item.level)) {
                     options = merge({}, item);
-                    levelIsConstant = (isBoolean(options.levelIsConstant) ?
-                        options.levelIsConstant :
-                        defaults.levelIsConstant);
+                    levelIsConstant = (
+                        isBoolean(options.levelIsConstant) ?
+                            options.levelIsConstant :
+                            defaults.levelIsConstant
+                    );
                     // Delete redundant properties.
                     delete options.levelIsConstant;
                     delete options.level;
@@ -132,8 +240,7 @@ var getLevelOptions = function getLevelOptions(params) {
                     level = item.level + (levelIsConstant ? 0 : from - 1);
                     if (isObject(obj[level])) {
                         extend(obj[level], options);
-                    }
-                    else {
+                    } else {
                         obj[level] = options;
                     }
                 }
@@ -142,11 +249,16 @@ var getLevelOptions = function getLevelOptions(params) {
         }
         to = isNumber(params.to) ? params.to : 1;
         for (i = 0; i <= to; i++) {
-            result[i] = merge({}, defaults, isObject(converted[i]) ? converted[i] : {});
+            result[i] = merge(
+                {},
+                defaults,
+                isObject(converted[i]) ? converted[i] : {}
+            );
         }
     }
     return result;
 };
+
 /**
  * Update the rootId property on the series. Also makes sure that it is
  * accessible to exporting.
@@ -160,13 +272,17 @@ var getLevelOptions = function getLevelOptions(params) {
  * @return {string}
  *         Returns the resulting rootId after update.
  */
-var updateRootId = function (series) {
-    var rootId, options;
+var updateRootId = function (series: any): string {
+    var rootId,
+        options;
+
     if (isObject(series)) {
         // Get the series options.
         options = isObject(series.options) ? series.options : {};
+
         // Calculate the rootId.
         rootId = pick(series.rootNode, options.rootId, '');
+
         // Set rootId on series.userOptions to pick it up in exporting.
         if (isObject(series.userOptions)) {
             series.userOptions.rootId = rootId;
@@ -176,10 +292,12 @@ var updateRootId = function (series) {
     }
     return rootId;
 };
-var result = {
+
+var result: Highcharts.TreeSeriesMixin = {
     getColor: getColor,
     getLevelOptions: getLevelOptions,
     setTreeValues: setTreeValues,
     updateRootId: updateRootId
 };
+
 export default result;
