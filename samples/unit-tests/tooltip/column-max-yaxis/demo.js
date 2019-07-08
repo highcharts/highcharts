6f1dc52f465996ac79b6d92198072c9c65c57a15
Dmitri Pantsenko <dmitripantsenko@gmail.com>
2019-07-08 08:58:08 +0200

    Tests: Moved issue test #4511

---
+++ samples/unit-tests/tooltip/column-max-yaxis/demo.js
@@ -0,0 +1,30 @@
+QUnit.test("Tooltip isn't displayed when on column, when yAxis.max is lower than column's value. (#4511)", function (assert) {
+    var chart = $('#container').highcharts({
+        chart: {
+            type: "column"
+        },
+        yAxis: {
+            max: 5
+        },
+        tooltip: {
+            shared: true
+        },
+        series: [{
+            data: [29.9, 71.5, 106.4]
+        }]
+    }).highcharts();
+
+
+    chart.pointer.onContainerMouseMove({
+        pageX: 150,
+        pageY: 310,
+        target: chart.series[0].group.element
+    });
+
+    assert.strictEqual(
+        chart.tooltip.isHidden,
+        false,
+        'Tooltip displayed properly'
+    );
+
+});
