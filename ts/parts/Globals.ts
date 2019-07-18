f449815785e8de441fca4da06991bd3d0c46bb8e
bre1470 <40056287+bre1470@users.noreply.github.com>
2019-07-18 09:58:59 +0200

    Tools: Migrated DragNodesMixin to TypeScript.

--- ts/parts/Globals.ts
+++ ts/parts/Globals.ts
@@ -60,6 +60,7 @@ declare global {
             invertable?: any; // @todo sankey
             isSeriesBoosting?: any; // @todo boost module
             labelBySeries?: any; // @todo series label module
+            layout?: any; // @todo networkgraph
             resetZones?: any; // @todo macd indicator
             useCommonDataGrouping?: any; // @todo indicators
             getPoint: Function; // @todo boost module
