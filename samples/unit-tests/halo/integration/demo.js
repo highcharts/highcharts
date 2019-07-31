49d7b19f45a8fd56ea079062a4374b73616122e8
jon-a-nygaard <jon.arild@highsoft.com>
2019-07-31 14:32:44 +0200

    Add integration tests for halo visibility

---
+++ samples/unit-tests/halo/integration/demo.js
@@ -0,0 +1,48 @@
+QUnit.test('visibility', assert => {
+    const { series: [series1, series2] } = Highcharts.chart('container', {
+        yAxis: {
+            min: 2,
+            max: 5
+        },
+        series: [{
+            data: [1, 2, 3, 4]
+        }, {
+            data: [3, 2, 1, 4]
+        }],
+        tooltip: {
+            shared: true
+        }
+    });
+
+    assert.strictEqual(
+        series1.halo,
+        undefined,
+        'Should have no halo on Series 1 before hover'
+    );
+    assert.strictEqual(
+        series2.halo,
+        undefined,
+        'Should have no halo on Series 2 before hover'
+    );
+
+
+    series1.points[2].onMouseOver();
+    assert.ok(
+        !!series1.halo,
+        'Should have created a halo object on Series 1 after hover'
+    );
+    assert.strictEqual(
+        series1.halo.visibility,
+        undefined, // equal to "inherit"
+        'Should have halo on Series 1 with visibility "inherit" after hover'
+    );
+    assert.ok(
+        !!series2.halo,
+        'Should have created a halo object on Series 2 after hover'
+    );
+    assert.strictEqual(
+        series2.halo.visibility,
+        "hidden",
+        'Should have halo on Series 2 with visibility "hidden" when point is outside plot area. #7650'
+    );
+});
