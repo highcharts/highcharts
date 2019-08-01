a1e165fdf5bfbe5b6a3eb91cc08ee36a5c180f37
jon-a-nygaard <jon.arild@highsoft.com>
2019-08-01 15:12:47 +0200

    Remove duplicate theme files

--- js/masters/themes/avocado.js
+++
@@ -1,9 +0,0 @@
-/**
- * @license Highcharts JS v@product.version@ (@product.date@)
- *
- * (c) 2009-2019 Highsoft AS
- *
- * License: www.highcharts.com/license
- */
-'use strict';
-import '../../themes/avocado.js';
