64915d45914346841d660205af29acc521ef4419
bre1470 <40056287+bre1470@users.noreply.github.com>
2019-08-06 16:59:58 +0200

    Tools: Migrated debugger module to TypeScript.

--- ts/modules/debugger.src.ts
+++ ts/modules/debugger.src.ts
@@ -7,11 +7,38 @@
  *  !!!!!!! SOURCE GETS TRANSPILED BY TYPESCRIPT. EDIT TS FILE ONLY. !!!!!!!
  *
  * */
+
 'use strict';
+
 import H from '../parts/Globals.js';
+
+/**
+ * Internal types
+ * @private
+ */
+declare global {
+    namespace Highcharts {
+        interface Chart {
+            errorElements?: Array<SVGElement>;
+        }
+        interface ChartOptions {
+            displayErrors?: boolean;
+        }
+        interface ErrorMessageObject {
+            title: string;
+            text: string;
+        }
+        const errorMessages: (Dictionary<ErrorMessageObject>|undefined);
+    }
+}
+
 import U from '../parts/Utilities.js';
 var isNumber = U.isNumber;
-var addEvent = H.addEvent, setOptions = H.setOptions, each = H.each;
+
+var addEvent = H.addEvent,
+    setOptions = H.setOptions,
+    each = H.each;
+
 setOptions({
     /**
      * @optionparent chart
@@ -31,33 +58,62 @@ setOptions({
         displayErrors: true
     }
 });
+
 /* eslint-disable no-invalid-this */
-addEvent(H.Chart, 'displayError', function (e) {
-    var chart = this, code = e.code, msg, options = chart.options.chart, renderer = chart.renderer, chartWidth, chartHeight;
+
+addEvent(H.Chart, 'displayError', function (
+    e: Highcharts.ErrorMessageEventObject
+): void {
+    var chart = this,
+        code = e.code,
+        msg,
+        options = chart.options.chart,
+        renderer = chart.renderer,
+        chartWidth,
+        chartHeight;
+
     if (chart.errorElements) {
-        each(chart.errorElements, function (el) {
+        each(chart.errorElements, function (el: Highcharts.SVGElement): void {
             if (el) {
                 el.destroy();
             }
         });
     }
+
     if (options && options.displayErrors) {
         chart.errorElements = [];
         msg = isNumber(code) ?
-            ('Highcharts error #' + code + ': ' +
-                H.errorMessages[code].title +
-                H.errorMessages[code].text) :
+            (
+                'Highcharts error #' + code + ': ' +
+                (H.errorMessages as any)[code].title +
+                (H.errorMessages as any)[code].text
+            ) :
             code;
         chartWidth = chart.chartWidth;
         chartHeight = chart.chartHeight;
+
         // Render red chart frame.
-        chart.errorElements[0] = renderer.rect(2, 2, chartWidth - 4, chartHeight - 4).attr({
+        chart.errorElements[0] = renderer.rect(
+            2,
+            2,
+            chartWidth - 4,
+            chartHeight - 4
+        ).attr({
             'stroke-width': 4,
             stroke: '#ff0000',
             zIndex: 3
         }).add();
+
         // Render error message.
-        chart.errorElements[1] = renderer.label(msg, 0, 0, 'rect', null, null, true).css({
+        chart.errorElements[1] = renderer.label(
+            msg,
+            0,
+            0,
+            'rect',
+            null as any,
+            null as any,
+            true
+        ).css({
             color: '#ffffff',
             width: chartWidth - 16,
             padding: 0
@@ -67,17 +123,22 @@ addEvent(H.Chart, 'displayError', function (e) {
             padding: 8,
             zIndex: 10
         }).add();
+
         chart.errorElements[1].attr({
-            y: chartHeight - this.errorElements[1].getBBox().height
+            y: chartHeight - (this.errorElements as any)[1].getBBox().height
         });
     }
 });
-addEvent(H.Chart, 'beforeRedraw', function () {
+
+addEvent(H.Chart, 'beforeRedraw', function (): void {
     var errorElements = this.errorElements;
+
     if (errorElements && errorElements.length) {
-        each(errorElements, function (el) {
+        each(errorElements, function (el: Highcharts.SVGElement): void {
             el.destroy();
         });
     }
-    this.errorElements = null;
+    this.errorElements = null as any;
 });
+
+/* eslint-enable no-invalid-this */
