49edb29ea14555d3b2b1fba3233769fa7940e4ff
bre1470 <40056287+bre1470@users.noreply.github.com>
2019-08-06 20:36:02 +0200

    Tools: Updated BS browser versions.

--- test/karma-conf.js
+++ test/karma-conf.js
@@ -135,28 +135,28 @@ const browserStackBrowsers = {
     'Mac.Chrome': {
         base: 'BrowserStack',
         browser: 'chrome',
-        browser_version: '72.0',
+        browser_version: '76.0',
         os: 'OS X',
         os_version: 'Mojave'
     },
     'Mac.Firefox': {
         base: 'BrowserStack',
         browser: 'firefox',
-        browser_version: '65.0',
+        browser_version: '68.0',
         os: 'OS X',
         os_version: 'Mojave'
     },
     'Mac.Safari': {
         base: 'BrowserStack',
         browser: 'safari',
-        browser_version: '12.0',
+        browser_version: '12.1',
         os: 'OS X',
         os_version: 'Mojave'
     },
     'Win.Chrome': {
         base: 'BrowserStack',
         browser: 'chrome',
-        browser_version: '72.0',
+        browser_version: '76.0',
         os: 'Windows',
         os_version: '10'
     },
@@ -170,7 +170,7 @@ const browserStackBrowsers = {
     'Win.Firefox': {
         base: 'BrowserStack',
         browser: 'firefox',
-        browser_version: '65.0',
+        browser_version: '68.0',
         os: 'Windows',
         os_version: '10'
     },
