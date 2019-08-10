c96334aefa86704934c9e9d69aba34a39c916183
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-08-11 00:03:26 +0200

    Migrate `modules/full-screen` to TypeScript. Closes #11576
    - internal type definitioin
    - minor JSDoc changes
    
    Thanks!

--- ts/modules/full-screen.src.ts
+++ ts/modules/full-screen.src.ts
@@ -5,8 +5,25 @@
  *
  * License: www.highcharts.com/license
  */
+
 import H from '../parts/Globals.js';
+
+/**
+ * Internal types
+ * @private
+ */
+declare global {
+    namespace Highcharts {
+        class FullScreen {
+            public constructor(container: HTMLDOMElement);
+
+            public init(container: HTMLDOMElement): void;
+        }
+    }
+}
+
 /* eslint-disable no-invalid-this, valid-jsdoc */
+
 /**
  * The FullScreen class.
  * The module allows user to enable full screen mode in StockTools.
@@ -18,10 +35,13 @@ import H from '../parts/Globals.js';
  *
  * @param {Highcharts.HTMLDOMElement} container chart div
  */
-var FullScreen = H.FullScreen = function (container) {
+var FullScreen = H.FullScreen = function (
+    this: Highcharts.FullScreen,
+    container: Highcharts.HTMLDOMElement): void {
     // main div of the chart
-    this.init(container.parentNode);
-};
+    this.init(container.parentNode as Highcharts.HTMLDOMElement);
+} as any;
+
 FullScreen.prototype = {
     /**
      * Init function
@@ -29,7 +49,7 @@ FullScreen.prototype = {
      * @param {Highcharts.HTMLDOMElement} - chart div
      *
      */
-    init: function (container) {
+    init: function (container: Highcharts.HTMLDOMElement): void {
         if (container.requestFullscreen) {
             container.requestFullscreen();
         } else if (container.mozRequestFullScreen) {
