adb0c3d3af0185c613fda68240f8b25edbce646e
oysteinmoseng <oystein@highsoft.com>
2019-08-14 19:24:56 +0200

    Added high contrast themes.

---
+++ js/masters/themes/high-contrast-dark.src.js
@@ -0,0 +1,11 @@
+/**
+ * @license Highcharts JS v@product.version@ (@product.date@)
+ * @module highcharts/themes/high-contrast-dark
+ * @requires highcharts
+ *
+ * (c) 2009-2019 Highsoft AS
+ *
+ * License: www.highcharts.com/license
+ */
+'use strict';
+import '../../themes/high-contrast-dark.js';
