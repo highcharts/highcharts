719130df26781fcec0cd7dc29fc7265d3ffd8e8a
Kacper Madej <kacper@blacklabel.pl>
2019-08-20 10:55:24 +0200

    Fixed #11609, prevented reflow of destroyed charts.

--- ts/parts/Chart.ts
+++ ts/parts/Chart.ts
@@ -1696,7 +1696,12 @@ extend(Chart.prototype, /** @lends Highcharts.Chart.prototype */ {
             this.unbindReflow = addEvent(win, 'resize', function (
                 e: Event
             ): void {
-                chart.reflow(e);
+                // a removed event listener still runs in Edge and IE if the
+                // listener was removed while the event runs, so check if the
+                // chart is not destroyed (#11609)
+                if (chart.options) {
+                    chart.reflow(e);
+                }
             });
             addEvent(this, 'destroy', this.unbindReflow);
 
