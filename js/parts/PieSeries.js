dc0706cfe4592eff58cd0e038bddee08b817af91
bre1470 <40056287+bre1470@users.noreply.github.com>
2019-08-26 10:01:27 +0200

    Tools: Fixed dataLabels type for pie-based series.

--- js/parts/PieSeries.js
+++ js/parts/PieSeries.js
@@ -291,7 +291,7 @@ seriesType('pie', 'line',
      * @apioption plotOptions.pie.colors
      */
     /**
-     * @type    {Highcharts.SeriesPieDataLabelsOptionsObject|Array<Highcharts.SeriesPieDataLabelsOptionsObject>}
+     * @type    {Highcharts.SeriesPieDataLabelsOptionsObject}
      * @default {"allowOverlap": true, "connectorPadding": 5, "distance": 30, "enabled": true, "formatter": function () { return this.point.name; }, "softConnector": true, "x": 0, "connectorShape": "fixedOffset", "crookDistance": "70%"}
      *
      * @private
@@ -1186,7 +1186,7 @@ seriesType('pie', 'line',
  * @apioption series.pie.data
  */
 /**
- * @type      {Highcharts.SeriesPieDataLabelsOptionsObject|Array<Highcharts.SeriesPieDataLabelsOptionsObject>}
+ * @type      {Highcharts.SeriesPieDataLabelsOptionsObject}
  * @product   highcharts
  * @apioption series.pie.data.dataLabels
  */
