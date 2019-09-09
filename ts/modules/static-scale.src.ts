aa58c759d2ace36d3167cff4059af3af45d5cbbc
bre1470 <40056287+bre1470@users.noreply.github.com>
2019-09-09 11:41:28 +0200

    Tools: Migrated static-scale module to TypeScript.

--- ts/modules/static-scale.src.ts
+++ ts/modules/static-scale.src.ts
@@ -7,12 +7,36 @@
  *  !!!!!!! SOURCE GETS TRANSPILED BY TYPESCRIPT. EDIT TS FILE ONLY. !!!!!!!
  *
  * */
+
 'use strict';
 import H from '../parts/Globals.js';
+
+/**
+ * Internal types
+ * @private
+ */
+declare global {
+    namespace Highcharts {
+        interface Chart {
+            initiatedScale?: boolean;
+            /** @requires modules/static-scale */
+            adjustHeight(): void;
+        }
+        interface XAxisOptions {
+            staticScale?: number;
+        }
+    }
+}
+
 import U from '../parts/Utilities.js';
-var defined = U.defined, isNumber = U.isNumber;
-var Chart = H.Chart, pick = H.pick;
+var defined = U.defined,
+    isNumber = U.isNumber;
+
+var Chart = H.Chart,
+    pick = H.pick;
+
 /* eslint-disable no-invalid-this */
+
 /**
  * For vertical axes only. Setting the static scale ensures that each tick unit
  * is translated into a fixed pixel height. For example, setting the static
@@ -29,36 +53,59 @@ var Chart = H.Chart, pick = H.pick;
  * @product   gantt
  * @apioption yAxis.staticScale
  */
-H.addEvent(H.Axis, 'afterSetOptions', function () {
+
+H.addEvent(H.Axis, 'afterSetOptions', function (): void {
     var chartOptions = this.chart.options && this.chart.options.chart;
-    if (!this.horiz &&
+    if (
+        !this.horiz &&
         isNumber(this.options.staticScale) &&
-        (!chartOptions.height ||
-            (chartOptions.scrollablePlotArea &&
-                chartOptions.scrollablePlotArea.minHeight))) {
+        (
+            !(chartOptions as any).height ||
+            (
+                (chartOptions as any).scrollablePlotArea &&
+                (chartOptions as any).scrollablePlotArea.minHeight
+            )
+        )
+    ) {
         this.staticScale = this.options.staticScale;
     }
 });
-Chart.prototype.adjustHeight = function () {
+
+Chart.prototype.adjustHeight = function (): void {
     if (this.redrawTrigger !== 'adjustHeight') {
-        (this.axes || []).forEach(function (axis) {
-            var chart = axis.chart, animate = !!chart.initiatedScale &&
-                chart.options.animation, staticScale = axis.options.staticScale, height, diff;
+        (this.axes || []).forEach(function (axis: Highcharts.Axis): void {
+            var chart = axis.chart,
+                animate =
+                    !!chart.initiatedScale &&
+                    (chart.options as any).animation,
+                staticScale = axis.options.staticScale,
+                height,
+                diff;
+
             if (axis.staticScale && defined(axis.min)) {
-                height = pick(axis.unitLength, axis.max + axis.tickInterval - axis.min) * staticScale;
+                height = pick(
+                    axis.unitLength,
+                    (axis.max as any) + axis.tickInterval - axis.min
+                ) * (staticScale as any);
+
+
                 // Minimum height is 1 x staticScale.
-                height = Math.max(height, staticScale);
+                height = Math.max(height, staticScale as any);
+
                 diff = height - chart.plotHeight;
+
                 if (Math.abs(diff) >= 1) {
                     chart.plotHeight = height;
                     chart.redrawTrigger = 'adjustHeight';
                     chart.setSize(undefined, chart.chartHeight + diff, animate);
                 }
+
                 // Make sure clip rects have the right height before initial
                 // animation.
                 axis.series.forEach(function (series) {
                     var clipRect = series.sharedClipKey &&
-                        chart[series.sharedClipKey];
+                        (chart as any)[series.sharedClipKey];
+
                     if (clipRect) {
                         clipRect.attr({
                             height: chart.plotHeight
@@ -66,6 +113,7 @@ Chart.prototype.adjustHeight = function () {
                     }
                 });
             }
+
         });
         this.initiatedScale = true;
     }
