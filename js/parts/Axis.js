9925f5990dff7271b227ce054b28b93e53804a88
Torstein Hønsi <torstein@highsoft.com>
2019-09-18 09:01:07 +0200

    Revert "Bugfix #11405, Corrections - reversed axis and inverted chart."
    This reverts commit 1c9e96105071b3db1e14a5d02352d37f2d89ad67.

--- js/parts/Axis.js
+++ js/parts/Axis.js
@@ -3487,9 +3487,8 @@ H.extend(Axis.prototype, /** @lends Highcharts.Axis.prototype */ {
             }
             return x;
         };
-        // Move the line inside the chart (#11405)
-        if ((!this.reversed && value === this.min) ||
-            (this.reversed && value === this.max)) {
+        // #11405
+        if (!this.horiz && value === this.min) {
             lineWidth = defined(lineWidth) ? lineWidth * -1 : -1;
         }
         evt = {
