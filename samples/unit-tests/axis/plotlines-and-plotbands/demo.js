254ee69f3cca50229fefb61ff95ffeee09d00daf
sigrunnu <sigrun.nummedal@gmail.com>
2019-09-30 21:03:39 +0200

    Moved issue test #4964 to unit-tests

--- samples/unit-tests/axis/plotlines-and-plotbands/demo.js
+++ samples/unit-tests/axis/plotlines-and-plotbands/demo.js
@@ -1,3 +1,29 @@
+QUnit.test('Missing plotband when range is small (#4964)', function (assert) {
+    var chart = Highcharts.chart('container1', {
+        xAxis: {
+            min: 1452666442250,
+            max: 1453899392750,
+            type: 'datetime',
+            plotBands: [{
+                color: "#BDBDBD",
+                from: 1453101708000,
+                to: 1453109508000
+            }, {
+                color: "red",
+                from: 1453726531000,
+                to: 1453728606000
+            }]
+        },
+        series: [{
+        }]
+    });
+
+    assert.strictEqual(
+        chart.xAxis[0].plotLinesAndBands[1].svgElem.d.split(' ')[1] !== chart.xAxis[0].plotLinesAndBands[1].svgElem.d.split(' ')[6],
+        true,
+        'Second plotband is visible'
+    );
+});
 QUnit.test('Plot bands on added axis should be exported (#5082)', function (assert) {
     var chart = Highcharts.chart('container', {
 
