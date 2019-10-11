cb14dc2e63ab0acad564756a7b6481bbd840d171
Jan Martin Langeland <jan.martin.langeland@highsoft.com>
2019-10-11 16:20:14 +0200

    Tools: Migrated TRIXIndicator to TypeScript.

--- ts/indicators/keltner-channels.src.ts
+++ ts/indicators/keltner-channels.src.ts
@@ -206,12 +206,14 @@ H.seriesType<Highcharts.KeltnerChannelsIndicator>(
                 TL: number,
                 BL: number,
                 date: number,
-                seriesEMA: (boolean|Highcharts.IndicatorValuesObject) =
-                    EMA.prototype.getValues(series,
-                        {
-                            period: period,
-                            index: index
-                        }),
+                seriesEMA: (
+                    boolean|Highcharts.IndicatorValuesObject|
+                    Highcharts.IndicatorNullableValuesObject
+                ) = EMA.prototype.getValues(series,
+                    {
+                        period: period,
+                        index: index
+                    }),
                 seriesATR: (boolean|Highcharts.IndicatorValuesObject) =
                     ATR.prototype.getValues(series,
                         {
