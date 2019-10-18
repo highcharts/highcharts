2c0a5bb1ce6c6a5f9bd230856786b8bb3b63cc24
Torstein Hønsi <torstein@highsoft.com>
2019-10-18 10:52:16 +0200

    Samples: Worked on pictorial series study

--- samples/highcharts/studies/pictorial/demo.js
+++ samples/highcharts/studies/pictorial/demo.js
@@ -2,14 +2,8 @@
  * Experimental pictorial series type
  *
  * To do
- * - Consider simply clipping a column stack.
- * - If scaling is problematic, consider setting the path in a callback where
- *   the width and the height are supplied.
- * - Make the patterns stretch to the rect or the stack box. Check out
- *   patternContentUnits: 'objectBoundingBox'. For stack box math,
- *   see ColumnPyramidSeries.ts.
  * - One path per category or perhaps simpler, per point
- * - Fix the path.fill bug, if agreed it's a bug (should also fix hover states)
+ * - Stroke width must be translated in to the objectBoundingBox content units
  */
 
 Highcharts.seriesType(
@@ -23,19 +17,23 @@ Highcharts.seriesType(
             const pointAttribs = Highcharts.seriesTypes.column.prototype
                 .pointAttribs.call(this, point, selected);
 
+            const scaleY = point.series.yAxis.len / point.shapeArgs.height;
+
             pointAttribs.fill = {
                 pattern: {
                     path: {
-                        d: point.series.options.pictorialPath,
+                        d: point.series.options.path,
                         fill: pointAttribs.fill,
-                        strokeWidth: pointAttribs.strokeWidth,
-                        stroke: pointAttribs.stroke
+                        strokeWidth: 0,
+                        stroke: pointAttribs.stroke,
+                        transform: `scale(1, ${scaleY})`
                     },
                     x: point.shapeArgs.x,
                     y: 0,
                     width: point.shapeArgs.width,
                     height: point.series.yAxis.len,
-                    color: 'none'
+                    patternContentUnits: 'objectBoundingBox',
+                    backgroundColor: 'none'
                 }
             };
 
@@ -50,6 +48,7 @@ Highcharts.seriesType(
 // Workaround for something that looks like a bug: The `pattern.path.fill`
 // option is not applied to the path, but rather the rect. And there is no way
 // to set the fill of the <path> itself.
+/*
 Highcharts.addEvent(Highcharts.Chart, 'render', e => {
     Object.keys(e.target.renderer.patternElements).forEach(id => {
         const pattern = e.target.renderer.patternElements[id];
@@ -62,6 +61,7 @@ Highcharts.addEvent(Highcharts.Chart, 'render', e => {
         }
     });
 });
+*/
 
 Highcharts.chart('container', {
     chart: {
@@ -73,23 +73,40 @@ Highcharts.chart('container', {
     title: {
         text: 'Pictorial chart'
     },
+    subtitle: {
+        text: 'Male symbols in sidewind due to a bug in our experimental path parser'
+    },
     xAxis: {
-        type: 'category',
-        labels: {
-            style: {
-                fontSize: '18px'
-            }
-        }
+        categories: ['Men', 'Women'],
+        lineWidth: 0
+    },
+    yAxis: {
+        visible: false
     },
     plotOptions: {
         pictorial: {
             stacking: 'percent',
-            pictorialPath: 'm 50 0 l 50 150 l -50 150 l -50 -150 z'
+
+            /*
+            Icon paths from font-awesome, https://fontawesome.com/license,
+            translated using http://jsfiddle.net/highcharts/a0jrvxch/
+            */
+            path: 'M0.57143,0C0.72922,0,0.85714,0.05596,0.85714,0.125L0.57143,0.25,0.28571,0.19404,0,0.06904L0.57143,0M0.78571,0.28125L0.735,0.28125C0.63361,0.30165,0.51362,0.30253,0.40786,0.28125L0.35714,0.28125C0.23879,0.28125,0.14286,0.32322,0.14286,0.375L0.14286,0.64063C0.14286,0.66651,0.19083,0.6875,0.25,0.6875L0.32143,0.6875L0.32143,0.95313C0.32143,0.97901,0.3694,1,0.42857,1L0.71429,1C0.77346,1,0.82143,0.97901,0.82143,0.95313L0.82143,0.6875L0.89286,0.6875C0.95203,0.6875,1,0.66651,1,0.64063L1,0.375C1,0.32322,0.90406,0.28125,0.78571,0.28125z'
         }
     },
+    legend: {
+        align: 'right',
+        verticalAlign: 'middle',
+        layout: 'vertical'
+    },
     series: [{
-        data: [10, 20, 70, 30]
+        data: [10, 20],
+        name: 'Quality A'
     }, {
-        data: [90, 80, 30, 70]
+        data: [20, 50],
+        name: 'Quality B'
+    }, {
+        data: [70, 30],
+        name: 'Quality C'
     }]
 });
