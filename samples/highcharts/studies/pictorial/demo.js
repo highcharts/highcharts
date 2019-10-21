932eef28124c3c641e15af90d38c0f62a40164bf
Torstein Hønsi <torstein@highsoft.com>
2019-10-21 09:59:05 +0200

    Samples: Automatic scaling for paths in pictorial study

--- samples/highcharts/studies/pictorial/demo.js
+++ samples/highcharts/studies/pictorial/demo.js
@@ -2,8 +2,12 @@
  * Experimental pictorial series type
  *
  * To do
- * - One path per category or perhaps simpler, per point
- * - Stroke width must be translated in to the objectBoundingBox content units
+ * - Stroke width must be scaled in to the objectBoundingBox content units
+ * - Paths should be relative to the stack box or column height. Currently
+ *   simplified to yAxis.len.
+ * - Option to toggle fixed aspect ratio. Currently the path scales to width and
+ *   height independently. When fixed, the width of the stack or point should
+ *   be dictated by the path's aspect ratio.
  */
 
 Highcharts.seriesType(
@@ -17,16 +21,15 @@ Highcharts.seriesType(
             const pointAttribs = Highcharts.seriesTypes.column.prototype
                 .pointAttribs.call(this, point, selected);
 
-            const scaleY = point.series.yAxis.len / point.shapeArgs.height;
-
             pointAttribs.fill = {
                 pattern: {
                     path: {
-                        d: point.series.options.path,
+                        d: point.series.options.paths[
+                            point.index % point.series.options.paths.length
+                        ],
                         fill: pointAttribs.fill,
                         strokeWidth: 0,
-                        stroke: pointAttribs.stroke,
-                        transform: `scale(1, ${scaleY})`
+                        stroke: pointAttribs.stroke
                     },
                     x: point.shapeArgs.x,
                     y: 0,
@@ -45,36 +48,38 @@ Highcharts.seriesType(
     }
 );
 
-// Workaround for something that looks like a bug: The `pattern.path.fill`
-// option is not applied to the path, but rather the rect. And there is no way
-// to set the fill of the <path> itself.
-/*
-Highcharts.addEvent(Highcharts.Chart, 'render', e => {
-    Object.keys(e.target.renderer.patternElements).forEach(id => {
-        const pattern = e.target.renderer.patternElements[id];
-        const path = pattern.element.querySelector('path');
-        const rect = pattern.element.querySelector('rect');
+Highcharts.addEvent(Highcharts.Series, 'afterRender', function () {
+    if (this instanceof Highcharts.seriesTypes.pictorial) {
+        this.points.forEach(point => {
+            const fill = point.graphic && point.graphic.attr('fill');
+            const match = fill && fill.match(/url\(([^)]+)\)/);
+            if (match) {
+                const patternPath = document.querySelector(`${match[1]} path`);
+                if (patternPath) {
+                    const bBox = patternPath.getBBox();
+                    const scaleX = 1 / bBox.width;
+                    const scaleY = this.yAxis.len /
+                        point.shapeArgs.height /
+                        bBox.height;
 
-        if (rect) {
-            path.setAttribute('fill', rect.getAttribute('fill'));
-            rect.setAttribute('fill', 'none');
-        }
-    });
+                    patternPath.setAttribute(
+                        'transform',
+                        `scale(${scaleX} ${scaleY}) ` +
+                        `translate(${-bBox.x}, ${-bBox.y})`
+                    );
+                }
+            }
+
+        });
+    }
 });
-*/
 
 Highcharts.chart('container', {
     chart: {
-        scrollablePlotArea: {
-            minWidth: 460
-        },
         type: 'pictorial'
     },
     title: {
         text: 'Pictorial chart'
-    },
-    subtitle: {
-        text: 'Male symbols in sidewind due to a bug in our experimental path parser'
     },
     xAxis: {
         categories: ['Men', 'Women'],
@@ -91,7 +96,11 @@ Highcharts.chart('container', {
             Icon paths from font-awesome, https://fontawesome.com/license,
             translated using http://jsfiddle.net/highcharts/a0jrvxch/
             */
-            path: 'M0.57143,0C0.72922,0,0.85714,0.05596,0.85714,0.125L0.57143,0.25,0.28571,0.19404,0,0.06904L0.57143,0M0.78571,0.28125L0.735,0.28125C0.63361,0.30165,0.51362,0.30253,0.40786,0.28125L0.35714,0.28125C0.23879,0.28125,0.14286,0.32322,0.14286,0.375L0.14286,0.64063C0.14286,0.66651,0.19083,0.6875,0.25,0.6875L0.32143,0.6875L0.32143,0.95313C0.32143,0.97901,0.3694,1,0.42857,1L0.71429,1C0.77346,1,0.82143,0.97901,0.82143,0.95313L0.82143,0.6875L0.89286,0.6875C0.95203,0.6875,1,0.66651,1,0.64063L1,0.375C1,0.32322,0.90406,0.28125,0.78571,0.28125z'
+            paths: [
+                'M96 0c35.346 0 64 28.654 64 64s-28.654 64-64 64-64-28.654-64-64S60.654 0 96 0m48 144h-11.36c-22.711 10.443-49.59 10.894-73.28 0H48c-26.51 0-48 21.49-48 48v136c0 13.255 10.745 24 24 24h16v136c0 13.255 10.745 24 24 24h64c13.255 0 24-10.745 24-24V352h16c13.255 0 24-10.745 24-24V192c0-26.51-21.49-48-48-48z',
+                'M128 0c35.346 0 64 28.654 64 64s-28.654 64-64 64c-35.346 0-64-28.654-64-64S92.654 0 128 0m119.283 354.179l-48-192A24 24 0 0 0 176 144h-11.36c-22.711 10.443-49.59 10.894-73.28 0H80a24 24 0 0 0-23.283 18.179l-48 192C4.935 369.305 16.383 384 32 384h56v104c0 13.255 10.745 24 24 24h32c13.255 0 24-10.745 24-24V384h56c15.591 0 27.071-14.671 23.283-29.821z'
+            ],
+            pointPadding: 0
         }
     },
     legend: {
@@ -108,5 +117,20 @@ Highcharts.chart('container', {
     }, {
         data: [70, 30],
         name: 'Quality C'
-    }]
+    }],
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'top'
+                }
+            }
+        }]
+
+    }
 });
