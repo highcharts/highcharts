5aa2855e9618ca66dd3e46df6010e1bda41a1cc7
bre1470 <40056287+bre1470@users.noreply.github.com>
2019-11-07 12:47:02 +0100

    Docs: Added list of supported date format keys.

--- js/parts/Time.js
+++ js/parts/Time.js
@@ -421,10 +421,32 @@ Highcharts.Time.prototype = {
     },
     /**
      * Formats a JavaScript date timestamp (milliseconds since Jan 1st 1970)
-     * into a human readable date string. The format is a subset of the formats
-     * for PHP's [strftime](http://www.php.net/manual/en/function.strftime.php)
+     * into a human readable date string. The format is a subset of the format
+     * keys for PHP's
+     * [strftime](http://www.php.net/manual/en/function.strftime.php)
      * function. Additional formats can be given in the
      * {@link Highcharts.dateFormats} hook.
+     *
+     * Supported format keys:
+     * - `%a`: Short weekday, like 'Mon'
+     * - `%A`: Long weekday, like 'Monday'
+     * - `%d`: Two digit day of the month, 01 to 31
+     * - `%e`: Day of the month, 1 through 31
+     * - `%w`: Day of the week, 0 through 6
+     * - `%b`: Short month, like 'Jan'
+     * - `%B`: Long month, like 'January'
+     * - `%m`: Two digit month number, 01 through 12
+     * - `%y`: Two digits year, like 09 for 2009
+     * - `%Y`: Four digits year, like 2009
+     * - `%H`: Two digits hours in 24h format, 00 through 23
+     * - `%k`: Hours in 24h format, 0 through 23
+     * - `%I`: Two digits hours in 12h format, 00 through 11
+     * - `%l`: Hours in 12h format, 1 through 12
+     * - `%M`: Two digits minutes, 00 through 59
+     * - `%p`: Upper case AM or PM
+     * - `%P`: Lower case AM or PM
+     * - `%S`: Two digits seconds, 00 through 59
+     * - `%L`: Milliseconds (naming from Ruby)
      *
      * @function Highcharts.Time#dateFormat
      *
@@ -463,6 +485,7 @@ Highcharts.Time.prototype = {
             d: pad(dayOfMonth),
             // Day of the month, 1 through 31
             e: pad(dayOfMonth, 2, ' '),
+            // Day of the week, 0 through 6
             w: day,
             // Week (none implemented)
             // 'W': weekNumber(),
