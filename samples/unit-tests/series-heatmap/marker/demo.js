6e9c4ca62b7549170ad45aa9dc57e0af5fff1a11
Daniel Studencki <daniel.studencki@gmail.com>
2019-12-15 19:50:53 +0100

    Enhancement #11306, marker tests added.

---
+++ samples/unit-tests/series-heatmap/marker/demo.js
@@ -0,0 +1,66 @@
+QUnit.test('General marker tests', function (assert) {
+
+    var chart = Highcharts.chart('container', {
+        chart: {
+            type: 'heatmap'
+        },
+
+        colorAxis: {
+            stops: [
+                [0, '#8b0000'],
+                [0.5, '#ffffff'],
+                [1, '#00008b']
+            ]
+        },
+
+        series: [{
+            data: [
+                [0, 0, 1], [0, 1, 1.1], [0, 2, 1.2],
+                [1, 0, 2], [1, 1, 2.1], [1, 2, 2.2],
+                [2, 0, 3], [2, 1, 3.1], [2, 2, 3.2]
+            ]
+        }]
+    });
+
+    var heatmap = chart.series[0];
+
+    // Check setting a series marker's lineWidth and lineColor
+    heatmap.update({
+        marker: {
+            lineWidth: 2,
+            lineColor: 'rgba(0, 255, 0, 1)'
+        }
+    });
+
+    var marker = heatmap.points[0].graphic;
+
+    assert.strictEqual(
+        marker.element.getAttribute('stroke'),
+        'rgba(0, 255, 0, 1)',
+        'Marker\'s line color is set.'
+    );
+
+    assert.strictEqual(
+        marker.element.getAttribute('stroke-width'),
+        '2',
+        'Marker\'s line width is set.'
+    );
+
+    // Set marker's fixed width and height
+    heatmap.update({
+        marker: {
+            width: 50,
+            height: 50
+        }
+    });
+
+    var bBox = heatmap.points[0].graphic.getBBox();
+
+    assert.strictEqual(
+        bBox.width - heatmap.options.marker.lineWidth === 50 &&
+        bBox.height - heatmap.options.marker.lineWidth === 50,
+        true,
+        'Marker\'s fixed width and height are set correctly through series.marker options.'
+    );
+
+});
