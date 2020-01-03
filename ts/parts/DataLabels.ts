d21d279309c8f5e75cb946c43617cc5cf3a34840
wchmiel <wojc.chmiel@gmail.com>
2020-01-03 12:36:28 +0100

    Fixed #12688, column datalabels not visible with small values.

--- ts/parts/DataLabels.ts
+++ ts/parts/DataLabels.ts
@@ -1855,6 +1855,9 @@ if (seriesTypes.column) {
             ),
             // draw it inside the box?
             inside = pick(options.inside, !!this.options.stacking),
+            tooltipPos = point.tooltipPos || [],
+            isTooltipOutside = series.chart.inverted ? !(tooltipPos[0] >= 0) :
+                !(tooltipPos[1] <= this.chart.plotHeight),
             overshoot;
 
         // Align to the column itself, or the top of it
@@ -1913,8 +1916,9 @@ if (seriesTypes.column) {
             isNew
         );
 
-        // Hide dataLabel when column is outside plotArea (#12370).
+        // Hide dataLabel when column is outside plotArea (#12370), (#12688).
         if (
+            isTooltipOutside &&
             alignTo &&
             (
                 (alignTo.height <= 0 && alignTo.y === this.chart.plotHeight) ||
