73341bb2fc91aec824d5f83e30fa271db2e2fb4b
bre1470 <40056287+bre1470@users.noreply.github.com>
2020-01-16 12:46:30 +0100

    Docs: Fixed sample links.

--- js/parts/PieSeries.js
+++ js/parts/PieSeries.js
@@ -298,9 +298,9 @@ seriesType('pie', 'line',
          */
         softConnector: true,
         /**
-         * @sample {highcharts} highcharts/plotOptions/pie-datalabels-overflow
+         * @sample {highcharts} highcharts/plotoptions/pie-datalabels-overflow
          *         Long labels truncated with an ellipsis
-         * @sample {highcharts} highcharts/plotOptions/pie-datalabels-overflow-wrap
+         * @sample {highcharts} highcharts/plotoptions/pie-datalabels-overflow-wrap
          *         Long labels are wrapped
          *
          * @type      {Highcharts.CSSObject}
