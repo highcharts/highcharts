cb78c001d9fd0d4a99d8a42502ff82c56b4ec85c
mateusz <mateusz@MacBook-Apple.local>
2020-01-22 10:02:40 +0100

    Corrected assert function message in RTL dataLabels test.

--- samples/unit-tests/datalabels/i18n/demo.js
+++ samples/unit-tests/datalabels/i18n/demo.js
@@ -25,14 +25,14 @@ QUnit.test('RTL characters on data labels', function (assert) {
             chart.series[0].data[0].dataLabel.text.element.children[0].classList,
             ['highcharts-text-outline']
         ),
-        'The data label which arabic characters is not covered with outline'
+        'The data label which includes Arabic characters is not covered with the outline'
     );
     assert.notOk(
         ~[].indexOf.apply(
             chart.series[0].data[1].dataLabel.text.element.children[0].classList,
             ['highcharts-text-outline']
         ),
-        'The data label which hebrew characters is not covered with outline'
+        'The data label which includes Hebrew characters is not covered with the outline'
     );
 
     assert.ok(
@@ -40,14 +40,14 @@ QUnit.test('RTL characters on data labels', function (assert) {
             chart.series[0].data[2].dataLabel.text.element.children[0].classList,
             ['highcharts-text-outline']
         ),
-        'The data label which japan characters is not covered with outline'
+        'The data label which includes Japan characters is not covered with the outline'
     );
     assert.ok(
         ~[].indexOf.apply(
             chart.series[0].data[3].dataLabel.text.element.children[0].classList,
             ['highcharts-text-outline']
         ),
-        'The data label which latin characters is not covered with outline'
+        'The data label which includes Latin characters is not covered with the outline'
     );
 
 });
