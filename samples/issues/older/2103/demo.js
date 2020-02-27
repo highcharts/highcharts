684e879b95861de6c76f094e552f500062ee3080
Torstein Hønsi <torstein@highsoft.com>
2020-02-27 11:21:15 +0100

    Samples

--- samples/issues/older/2103/demo.js
+++ samples/issues/older/2103/demo.js
@@ -1,25 +1,23 @@
-$(function () {
-    $('#container').highcharts({
-        chart: {
-            type: 'pie'
-        },
+Highcharts.chart('container', {
+    chart: {
+        type: 'pie'
+    },
 
-        title: {
-            text: 'All zeroes or all null pies.<br/>Caused infinite loop.'
-        },
+    title: {
+        text: 'All zeroes or all null pies.<br/>Caused infinite loop.'
+    },
 
-        series: [{
-            data: [0, 0, 0],
-            center: ['30%', '30%']
-        }, {
-            data: [null, null, null],
-            center: ['70%', '30%']
-        }, {
-            data: [],
-            center: ['30%', '70%']
-        }, {
-            center: ['70%', '70%']
-        }]
+    series: [{
+        data: [0, 0, 0],
+        center: ['30%', '30%']
+    }, {
+        data: [null, null, null],
+        center: ['70%', '30%']
+    }, {
+        data: [],
+        center: ['30%', '70%']
+    }, {
+        center: ['70%', '70%']
+    }]
 
-    });
 });
