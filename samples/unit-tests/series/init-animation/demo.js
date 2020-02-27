9ee7b8ea4581fab338c2873e19d436f228427044
Sebastian Wedzel <bastian-blacklabel@MacBook-Pro-Sebastian.local>
2020-02-27 18:09:57 +0100

    Enhancement #12901, series defer test.

--- samples/unit-tests/series/init-animation/demo.js
+++ samples/unit-tests/series/init-animation/demo.js
@@ -64,3 +64,66 @@ QUnit.test('Initial animation - series.clip set to false', function (assert) {
         TestUtilities.lolexUninstall(clock);
     }
 });
+
+QUnit.test('Initial animation - defer test #12901', function (assert) {
+
+    var clock = null;
+
+    try {
+        clock = TestUtilities.lolexInstall();
+
+        var chart = Highcharts.chart('container', {
+                series: [{
+                    data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+                    animation: {
+                        defer: 2000,
+                        duration: 1000
+                    }
+                }]
+            }),
+            done = assert.async(),
+            width;
+
+        setTimeout(function () {
+            // animation started
+            width = chart[chart.series[0].sharedClipKey].element.width.baseVal.value;
+
+            assert.strictEqual(
+                width === 0,
+                true,
+                'Animate should not start'
+            );
+
+            setTimeout(function () {
+                // animation uncovers most of the plot
+                width = chart[chart.series[0].sharedClipKey].element.width
+                    .baseVal.value;
+                assert.strictEqual(
+                    width > 300 && width < 600,
+                    true,
+                    'Animation uncovers most of the plot'
+                );
+            }, 1100);
+
+            setTimeout(function () {
+                // animation finished
+                assert.strictEqual(
+                    // Highcharts - tested in browser
+                    chart[chart.series[0].sharedClipKey] === undefined ||
+                    // Highstock - tested in headless
+                    chart[chart.series[0].sharedClipKey].element.width
+                        .baseVal.value === chart.chartWidth,
+                    true,
+                    'Animation finished - no clip box'
+                );
+
+                // all tests are done
+                done();
+            }, 1500);
+        }, 1500);
+
+        TestUtilities.lolexRunAndUninstall(clock);
+    } finally {
+        TestUtilities.lolexUninstall(clock);
+    }
+});
