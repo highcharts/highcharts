b55277e18eee16034e6c4c9e4ed942e024bf5979
Torstein Hønsi <torstein@highsoft.com>
2020-03-10 11:27:59 +0100

    Avoid animation in Chart.setSize when global off

--- js/parts/Chart.js
+++ js/parts/Chart.js
@@ -1332,6 +1332,7 @@ extend(Chart.prototype, /** @lends Highcharts.Chart.prototype */ {
         chart.isResizing += 1;
         // set the animation for the current process
         setAnimation(animation, chart);
+        globalAnimation = renderer.globalAnimation;
         chart.oldChartHeight = chart.chartHeight;
         chart.oldChartWidth = chart.chartWidth;
         if (typeof width !== 'undefined') {
@@ -1344,14 +1345,13 @@ extend(Chart.prototype, /** @lends Highcharts.Chart.prototype */ {
         // Resize the container with the global animation applied if enabled
         // (#2503)
         if (!chart.styledMode) {
-            globalAnimation = renderer.globalAnimation;
             (globalAnimation ? animate : css)(chart.container, {
                 width: chart.chartWidth + 'px',
                 height: chart.chartHeight + 'px'
             }, globalAnimation);
         }
         chart.setChartSize(true);
-        renderer.setSize(chart.chartWidth, chart.chartHeight, animation);
+        renderer.setSize(chart.chartWidth, chart.chartHeight, globalAnimation);
         // handle axes
         chart.axes.forEach(function (axis) {
             axis.isDirty = true;
@@ -1361,7 +1361,7 @@ extend(Chart.prototype, /** @lends Highcharts.Chart.prototype */ {
         chart.isDirtyBox = true; // force redraw of plot and chart border
         chart.layOutTitles(); // #2857
         chart.getMargins();
-        chart.redraw(animation);
+        chart.redraw();
         chart.oldChartHeight = null;
         fireEvent(chart, 'resize');
         // Fire endResize and set isResizing back. If animation is disabled,
