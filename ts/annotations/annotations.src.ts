71e63669957338ee6b95be1ad8a645c690bc20cb
wchmiel <wojc.chmiel@gmail.com>
2020-03-12 14:55:45 +0100

    Improved: Fixed #13070, annotation draggable.

--- ts/annotations/annotations.src.ts
+++ ts/annotations/annotations.src.ts
@@ -87,6 +87,7 @@ declare global {
             public redraw(animation?: boolean): void;
             public redrawItem(item: AnnotationControllable, animation?: boolean): void;
             public redrawItems(items: Array<AnnotationControllable>, animation?: boolean): void;
+            public renderItems(items: Array<AnnotationControllable>): void;
             public remove(): void;
             public render(): void;
             public renderItem(item: AnnotationControllable): void;
@@ -1221,6 +1222,21 @@ merge(
             }
         },
 
+        /**
+         * @private
+         * @param {Array<Highcharts.AnnotationControllable>} items
+         */
+        renderItems: function (
+            this: Highcharts.Annotation,
+            items: Array<Highcharts.AnnotationControllable>
+        ): void {
+            var i = items.length;
+
+            while (i--) {
+                this.renderItem(items[i]);
+            }
+        },
+
         render: function (this: Highcharts.Annotation): void {
             var renderer = this.chart.renderer;
 
@@ -1251,6 +1267,10 @@ merge(
             if (this.clipRect) {
                 this.graphic.clip(this.clipRect);
             }
+
+            // Render shapes and labels before adding events (#13070).
+            this.renderItems(this.shapes);
+            this.renderItems(this.labels);
 
             this.addEvents();
 
