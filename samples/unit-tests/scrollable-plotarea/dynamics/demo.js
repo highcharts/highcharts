46a2712f7053755289d275ba4bba0e7ca328e407
Kamil <kamilmkulig@gmail.com>
2020-03-16 11:24:29 +0100

    Fixed #12991: highcharts-scrolling didn't adjust its height properly.

--- samples/unit-tests/scrollable-plotarea/dynamics/demo.js
+++ samples/unit-tests/scrollable-plotarea/dynamics/demo.js
@@ -18,3 +18,30 @@ QUnit.test('Test dynamic behaviour of Scrollable PlotArea', function (assert) {
         'Title should be outside the scrollable plot area (#11966)'
     );
 });
+
+
+QUnit.test('Responsive scrollable plot area (#12991)', function (assert) {
+
+    var chart = Highcharts.chart('container', {
+        chart: {
+            type: 'column',
+            scrollablePlotArea: {
+                minHeight: 400
+            },
+            height: 300
+        },
+        series: [{
+            data: [1]
+        }]
+    });
+
+    chart.setSize(null, 500);
+
+    var scrolling = document.getElementsByClassName('highcharts-scrolling')[0];
+    console.log('Test: ' + (scrolling.clientHeight > 300));
+
+    assert.ok(
+        document.getElementsByClassName('highcharts-scrolling')[0].clientHeight > 300,
+        'The scrollbar should disasppear after increasing the height of the chart (#12991)'
+    );
+});
