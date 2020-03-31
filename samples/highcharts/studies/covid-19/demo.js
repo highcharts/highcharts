ad1bcb3fcf9f5ce2b0a7c09393418461393ba6c1
Torstein Hønsi <torstein@highsoft.com>
2020-03-31 10:36:34 +0200

    Samples: Covid-19 dashboard

--- samples/highcharts/studies/covid-19/demo.js
+++ samples/highcharts/studies/covid-19/demo.js
@@ -1,10 +1,29 @@
-document.addEventListener('DOMContentLoaded', async function () {
-    const countriesResp = await fetch("https://pomber.github.io/covid19/timeseries.json");
-    const countries = await countriesResp.json();
-    const populationResp = await fetch('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/world-population.json');
-    const population = await populationResp.json();
+let countries;
+let population;
 
-    let countryChart;
+let mapChart;
+let countryChart;
+
+const getConfig = type => ({
+    confirmed: {
+        day0Value: 200,
+        header: 'Confirmed Covid-19 Cases',
+        name: 'Confirmed cases',
+        valueSuffix: 'confirmed cases'
+    },
+    deaths: {
+        day0Value: 10,
+        header: 'Deaths caused by Covid-19',
+        name: 'Deaths',
+        valueSuffix: 'deaths'
+    }
+}[type]);
+
+const createMap = (type = 'confirmed') => {
+
+    const config = getConfig(type);
+
+    document.getElementById('map-header').innerHTML = config.header;
 
     // Add lower case codes to the data set for inclusion in the tooltip.pointFormat
     const mapData = Highcharts.geojson(Highcharts.maps['custom/world']);
@@ -22,11 +41,12 @@ document.addEventListener('DOMContentLoaded', async function () {
     // Names used in Highcharts Map Collection
     countries['United States of America'] = countries.US;
     countries['South Korea'] = countries['Korea, South'];
+    countries['Czech Republic'] = countries.Czechia;
 
     let maxValue = 0;
     const data = Object.keys(countries).map(name => {
         const country = countries[name];
-        const total = country[country.length - 1].confirmed;
+        const total = country[country.length - 1][type];
         if (populationByName[name]) {
             const value = total / populationByName[name];
             if (populationByName[name] > 1000) {
@@ -39,63 +59,73 @@ document.addEventListener('DOMContentLoaded', async function () {
 
 
     // Initiate the map chart
-    const mapChart = Highcharts.mapChart('container', {
+    if (!mapChart) {
+        mapChart = Highcharts.mapChart('container', {
 
-        chart: {
-            spacingLeft: 1,
-            spacingRight: 1
-        },
+            chart: {
+                spacingLeft: 1,
+                spacingRight: 1
+            },
 
-        title: {
-            text: null
-        },
-
-        mapNavigation: {
-            enabled: true,
-            buttonOptions: {
-                verticalAlign: 'bottom'
-            }
-        },
-
-        colorAxis: {
-            minColor: '#ffeeee',
-            maxColor: '#aa0000',
-            max: maxValue
-        },
-
-        tooltip: {
-            headerFormat: '<b>{point.point.name}</b><br>',
-            pointFormat: '<b>{point.total}</b> confirmed cases<br>' +
-                '<b>{point.value:.2f}</b> per 1000 inhabitants<br>',
-            footerFormat: '<span style="font-size: 10px">(Click for details)</span>'
-        },
-
-        legend: {
             title: {
-                text: 'Cases per 1000 inhabitants',
-                style: {
-                    fontWeight: 'normal'
-                }
-            }
-        },
+                text: null
+            },
 
-        series: [{
-            id: 'map',
-            data: data,
-            mapData: mapData,
-            joinBy: ['name', 'name'],
-            name: 'Confirmed cases',
-            cursor: 'pointer',
-            states: {
-                select: {
-                    color: undefined,
-                    borderColor: '#333'
+            mapNavigation: {
+                enabled: true,
+                buttonOptions: {
+                    verticalAlign: 'bottom'
                 }
             },
-            borderWidth: 1,
-            borderColor: 'rgba(0, 0, 0, 0.05)'
+
+            colorAxis: {
+                minColor: 'rgba(196, 0, 0, 0.1)',
+                maxColor: 'rgba(196, 0, 0, 1)'
+            },
+
+            tooltip: {
+                headerFormat: '<b>{point.point.name}</b><br>',
+                footerFormat: '<span style="font-size: 10px">(Click for details)</span>'
+            },
+
+            legend: {
+                title: {
+                    text: 'Per 1000 inhabitants',
+                    style: {
+                        fontWeight: 'normal'
+                    }
+                }
+            },
+
+            series: [{
+                id: 'map',
+                mapData,
+                joinBy: ['name', 'name'],
+                cursor: 'pointer',
+                states: {
+                    select: {
+                        color: undefined,
+                        borderColor: '#333'
+                    }
+                },
+                borderWidth: 1,
+                borderColor: 'rgba(0, 0, 0, 0.05)'
+            }]
+        });
+    }
+    mapChart.update({
+        colorAxis: {
+            max: maxValue
+        },
+        tooltip: {
+            pointFormat: '<b>{point.total}</b> ' + config.valueSuffix + '<br>' +
+                '<b>{point.value:.2f}</b> per 1000 inhabitants<br>'
+        },
+        series: [{
+            data,
+            name: config.name
         }]
-    });
+    }, true, true);
 
     // Wrap point.select to get to the total selected points
     const onCountryClick = e => {
@@ -126,7 +156,7 @@ document.addEventListener('DOMContentLoaded', async function () {
                 document.querySelector('#info .header-text').style.paddingLeft = '40px';
                 document.querySelector('#info .header-text').innerHTML = points[0].name;
                 document.querySelector('#info .subheader')
-                    .innerHTML = 'Confirmed cases, starting the day before the 200th case<br>';
+                    .innerHTML = `${config.name}, starting the day of the ${config.day0Value}th case<br>`;
 
                 if (e && e.type === 'touchstart') {
                     document.querySelector('#info .subheader')
@@ -141,7 +171,7 @@ document.addEventListener('DOMContentLoaded', async function () {
                 document.querySelector('#info .header-text').style.paddingLeft = 0;
                 document.querySelector('#info .header-text').innerHTML = 'Comparing countries';
                 document.querySelector('#info .subheader')
-                    .innerHTML = 'Confirmed cases, starting the day before the 200th case<br>';
+                    .innerHTML = `${config.name}, starting the day of the ${config.day0Value}th case<br>`;
             }
 
             if (!countryChart) {
@@ -171,7 +201,7 @@ document.addEventListener('DOMContentLoaded', async function () {
                     },
                     tooltip: {
                         headerFormat: '<small>{series.name}</small><br>',
-                        pointFormat: '<b>Day {point.x}: {point.date:%b %e, %Y}</b><br>{point.y} confirmed cases'
+                        pointFormat: '<b>Day {point.x}: {point.date:%b %e, %Y}</b><br>{point.y} ' + config.valueSuffix
                     },
                     legend: {
                         enabled: false
@@ -193,46 +223,40 @@ document.addEventListener('DOMContentLoaded', async function () {
                 });
             }
 
-            countryChart.series.slice(0).forEach(function (s) {
-                if (
-                    !points.find(p => s.options.id === p.id) ||
-                    s.type === 'area'
-                ) {
-                    s.remove(false);
-                }
-            });
             const store = [];
-            points.forEach(function (p) {
-                if (countries[p.name]) {
-                    if (!countryChart.get(p.id)) {
-                        const firstDayAbove200 = countries[p.name].findIndex(
-                            point => point.confirmed >= 200
-                        );
-                        const data = countries[p.name]
-                            .slice(Math.max(firstDayAbove200 - 1, 0))
-                            .map((point, x) => {
-                                const [year, month, date] = point.date.split('-');
-                                const d = Date.UTC(year, month - 1, date);
-                                return {
-                                    date: d,
-                                    x,
-                                    y: point.confirmed
-                                };
-                            });
-                        countryChart.addSeries({
-                            id: p.id,
-                            name: p.name,
-                            data,
-                            type: points.length > 1 ? 'line' : 'area',
-                            color: points.length > 1 ? undefined : '#aa0000',
-                            fillColor: points.length > 1 ? undefined : p.color
-                        }, false);
-                    }
+            const series = points
+                .filter(p => countries[p.name])
+                .map(p => {
+
                     store.push(p.id);
-                }
-            });
+
+                    const firstDayAbove200 = countries[p.name].findIndex(
+                        point => point[type] >= config.day0Value
+                    );
+                    const data = countries[p.name]
+                        .slice(Math.max(firstDayAbove200 - 1, 0))
+                        .map((point, x) => {
+                            const [year, month, date] = point.date.split('-');
+                            const d = Date.UTC(year, month - 1, date);
+                            return {
+                                date: d,
+                                x,
+                                y: point[type]
+                            };
+                        });
+
+                    return {
+                        id: p.id,
+                        name: p.name,
+                        data,
+                        type: points.length > 1 ? 'line' : 'area',
+                        color: points.length > 1 ? undefined : '#aa0000',
+                        fillColor: points.length > 1 ? undefined : p.color
+                    };
+                });
+
+            countryChart.update({ series }, true, true);
             location.hash = store.join(',');
-            countryChart.redraw();
 
         // No selected points
         } else {
@@ -267,15 +291,32 @@ document.addEventListener('DOMContentLoaded', async function () {
     });
     onCountryClick();
 
-    // Activate button
-    /*
-    const resetMap = () => {
-        mapChart.getSelectedPoints().forEach(p => {
-            p.select(false);
+};
+
+const activateButtons = () => {
+    const buttons = document.querySelectorAll('input[name="source"]');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            button.parentNode.classList.add('active');
+
+            buttons.forEach(otherButton => {
+                if (otherButton !== button) {
+                    otherButton.parentNode.classList.remove('active');
+                }
+            });
+
+            createMap(button.id);
         });
-        onCountryClick();
-    };
-    document.getElementById('reset').addEventListener('click', resetMap);
-    document.getElementById('reset').addEventListener('touchstart', resetMap);
-    */
+    });
+};
+
+document.addEventListener('DOMContentLoaded', async function () {
+    const countriesResp = await fetch("https://pomber.github.io/covid19/timeseries.json");
+    countries = await countriesResp.json();
+    const populationResp = await fetch('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/world-population.json');
+    population = await populationResp.json();
+
+    activateButtons();
+
+    createMap();
 });
