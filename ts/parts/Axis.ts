019f6c997a9fc8e2c2b0dc2b9e693977f8c132c0
Torstein Hønsi <torstein@highsoft.com>
2020-04-01 11:07:48 +0200

    Additions to better default for allowDecimals

--- ts/parts/Axis.ts
+++ ts/parts/Axis.ts
@@ -5473,9 +5473,15 @@ class Axis implements AxisComposition, AxisLike {
                 axis.tickInterval,
                 void 0,
                 getMagnitude(axis.tickInterval),
-                // If the tick interval is greather than 0.5, avoid decimals, as
-                // linear axes are often used to render discrete values. #3363.
-                pick(options.allowDecimals, axis.tickInterval < 0.5),
+                pick(
+                    options.allowDecimals,
+                    // If the tick interval is greather than 0.5, avoid
+                    // decimals, as linear axes are often used to render
+                    // discrete values. #3363. If a tick amount is set, allow
+                    // decimals by default, as it increases the chances for a
+                    // good fit.
+                    axis.tickInterval < 0.5 || this.tickAmount !== void 0
+                ),
                 !!this.tickAmount
             );
         }
