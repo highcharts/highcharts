3f4970cb43e7d823a53245c3bff7fb923a873c93
Karol Kołodziej <karolkolodziej@Karols-MacBook-Pro.local>
2020-04-16 10:45:04 +0200

    Fixed #13136, cannot set tick amount when small chart size.

--- ts/parts/Axis.ts
+++ ts/parts/Axis.ts
@@ -5794,11 +5794,11 @@ class Axis implements AxisComposition {
         var axis: Highcharts.Axis = this as any,
             options = this.options,
             tickAmount = options.tickAmount,
-            tickPixelInterval = options.tickPixelInterval;
+            tickPixelInterval = options.tickPixelInterval as number;
 
         if (
             !defined(options.tickInterval) &&
-            this.len < (tickPixelInterval as any) &&
+            !tickAmount && this.len < tickPixelInterval &&
             !this.isRadial &&
             !axis.logarithmic &&
             options.startOnTick &&
