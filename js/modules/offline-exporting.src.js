ea423209b49bc7aec9c4ff1a0cfb0a794b081ae0
Gøran A. Slettemark <goransle@gmail.com>
2020-04-22 17:24:22 +0200

    Fixed #13309, PDF export always being landscape

--- js/modules/offline-exporting.src.js
+++ js/modules/offline-exporting.src.js
@@ -198,8 +198,15 @@ Highcharts.downloadSVGLocal = function (svg, options, failCallback, successCallb
      * @private
      */
     function svgToPdf(svgElement, margin) {
-        var width = svgElement.width.baseVal.value + 2 * margin, height = svgElement.height.baseVal.value + 2 * margin, pdf = new win.jsPDF(// eslint-disable-line new-cap
-        'l', 'pt', [width, height]);
+        var width = svgElement.width.baseVal.value + 2 * margin, height = svgElement.height.baseVal.value + 2 * margin, pdf;
+        if (width < height) {
+            pdf = new win.jsPDF(// eslint-disable-line new-cap
+            'p', 'pt', [width, height]);
+        }
+        else {
+            pdf = new win.jsPDF(// eslint-disable-line new-cap
+            'l', 'pt', [width, height]);
+        }
         // Workaround for #7090, hidden elements were drawn anyway. It comes
         // down to https://github.com/yWorks/svg2pdf.js/issues/28. Check this
         // later.
