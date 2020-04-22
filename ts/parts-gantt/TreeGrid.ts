17f645a70c30a33ef50d3b82c5713c66928bfd43
bre1470 <40056287+bre1470@users.noreply.github.com>
2020-04-22 12:49:47 +0200

    Restored TreeGrid module file.

---
+++ ts/parts-gantt/TreeGrid.ts
@@ -0,0 +1,24 @@
+/* *
+ *
+ *  (c) 2016 Highsoft AS
+ *  Authors: Jon Arild Nygard
+ *
+ *  License: www.highcharts.com/license
+ *
+ *  !!!!!!! SOURCE GETS TRANSPILED BY TYPESCRIPT. EDIT TS FILE ONLY. !!!!!!!
+ *
+ * */
+
+'use strict';
+
+import Axis from '../parts/Axis.js';
+import Tick from '../parts/Tick.js';
+import TreeGridAxis from './TreeGridAxis.js';
+import TreeGridTick from './TreeGridTick.js';
+
+import './GridAxis.js';
+import '../modules/broken-axis.src.js';
+
+/* eslint-disable valid-jsdoc */
+
+TreeGridAxis.compose(Axis);
