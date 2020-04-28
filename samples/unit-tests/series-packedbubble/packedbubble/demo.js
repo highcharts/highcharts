ffb7aa34aa5cd26287529c4d3d11f9eaa2b1a73d
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2020-04-28 11:46:56 +0200

    Addition t0 #11782, fixed controller issue on unit tests.

--- samples/unit-tests/series-packedbubble/packedbubble/demo.js
+++ samples/unit-tests/series-packedbubble/packedbubble/demo.js
@@ -1,5 +1,4 @@
 QUnit.test('Packed Bubble layouts operations', function (assert) {
-
     const chart = Highcharts.chart('container', {
         chart: {
             type: 'packedbubble',
@@ -29,89 +28,121 @@ QUnit.test('Packed Bubble layouts operations', function (assert) {
             data: []
         }]
     });
-
     chart.series[2].remove();
-
     function compareCollections(collections, collection) {
         var equal = true;
-
         collections.forEach(function (c) {
             if (equal) {
                 equal = c.series.length === collection.length;
             }
         });
-
         return equal;
     }
-
     assert.strictEqual(
         compareCollections(chart.graphLayoutsLookup, chart.series),
         true,
         'Series is removed from layout.series collection.'
     );
-
-    //     const controller = new TestController(chart);
-    //     controller.click(
-    //         (chart.series[0].parentNode.plotX) + chart.plotLeft,
-    //         (chart.series[0].parentNode.plotY) + chart.plotTop,
-    //         { shiftKey: false },
-    //         false
-    //     );
-
-    //     assert.ok(
-    //         chart.series[0].parentNode.selected,
-    //         'It should be possible to select a parent node that has enabled allowParentSelect property'
-    //     );
-
-    //     controller.click(
-    //         (chart.series[2].parentNode.plotX) + chart.plotLeft,
-    //         (chart.series[2].parentNode.plotY) + chart.plotTop,
-    //         { shiftKey: false },
-    //         false
-    //     );
-
-    //     assert.notOk(
-    //         chart.series[2].parentNode.selected,
-    //         'It shouldn\'t be possible to select a parent that has not enabled allowParentSelect property.'
-    //     );
-
-    //     controller.click(
-    //         (chart.series[1].parentNode.plotX) + chart.plotLeft,
-    //         (chart.series[1].parentNode.plotY) + chart.plotTop,
-    //         { shiftKey: false },
-    //         false
-    //     );
-
-    //     assert.strictEqual(
-    //         chart.series[0].getSelectedParents().length,
-    //         1,
-    //         'It shouldn\'t be possible to select more than one parent node without using key modifier.'
-    //     );
-
-    //     controller.click(
-    //         (chart.series[0].parentNode.plotX) + chart.plotLeft,
-    //         (chart.series[0].parentNode.plotY) + chart.plotTop,
-    //         { shiftKey: true },
-    //         false
-    //     );
-
-    //     assert.strictEqual(
-    //         chart.series[0].getSelectedParents().length,
-    //         2,
-    //         'It should be possible to select more than one parent node using key modifier.'
-    //     );
-
-    //     controller.click(
-    //         (chart.series[0].parentNode.plotX) + chart.plotLeft,
-    //         (chart.series[0].parentNode.plotY) + chart.plotTop,
-    //         { shiftKey: false },
-    //         false
-    //     );
-
-    //     assert.strictEqual(
-    //         chart.series[0].getSelectedParents().length,
-    //         0,
-    //         'After clicking on the selected parent, all selected parents should be deselected.'
-    //     );
-
+    const controller = new TestController(chart);
+    controller.triggerEvent(
+        'mouseover',
+        (chart.series[0].parentNode.plotX) + chart.plotLeft,
+        (chart.series[0].parentNode.plotY) + chart.plotTop,
+        { shiftKey: false },
+        false
+    );
+    controller.click(
+        (chart.series[0].parentNode.plotX) + chart.plotLeft,
+        (chart.series[0].parentNode.plotY) + chart.plotTop,
+        { shiftKey: false },
+        false
+    );
+    assert.ok(
+        chart.series[0].parentNode.selected,
+        'It should be possible to select a parent node that has enabled allowParentSelect property'
+    );
+    controller.triggerEvent(
+        'mouseover',
+        (chart.series[2].parentNode.plotX) + chart.plotLeft,
+        (chart.series[2].parentNode.plotY) + chart.plotTop,
+        { shiftKey: false },
+        false
+    );
+    controller.click(
+        (chart.series[2].parentNode.plotX) + chart.plotLeft,
+        (chart.series[2].parentNode.plotY) + chart.plotTop,
+        { shiftKey: false },
+        false
+    );
+    assert.notOk(
+        chart.series[2].parentNode.selected,
+        'It shouldn\'t be possible to select a parent that has not enabled allowParentSelect property.'
+    );
+    controller.triggerEvent(
+        'mouseover',
+        (chart.series[0].parentNode.plotX) + chart.plotLeft,
+        (chart.series[0].parentNode.plotY) + chart.plotTop,
+        { shiftKey: false },
+        false
+    );
+    controller.click(
+        (chart.series[0].parentNode.plotX) + chart.plotLeft,
+        (chart.series[0].parentNode.plotY) + chart.plotTop,
+        { shiftKey: false },
+        false
+    );
+    controller.triggerEvent(
+        'mouseover',
+        (chart.series[1].parentNode.plotX) + chart.plotLeft,
+        (chart.series[1].parentNode.plotY) + chart.plotTop,
+        { shiftKey: false },
+        false
+    );
+    controller.click(
+        (chart.series[1].parentNode.plotX) + chart.plotLeft,
+        (chart.series[1].parentNode.plotY) + chart.plotTop,
+        { shiftKey: false },
+        false
+    );
+    assert.strictEqual(
+        chart.series[0].getSelectedParents().length,
+        1,
+        'It shouldn\'t be possible to select more than one parent node without using key modifier.'
+    );
+    controller.triggerEvent(
+        'mouseover',
+        (chart.series[0].parentNode.plotX) + chart.plotLeft,
+        (chart.series[0].parentNode.plotY) + chart.plotTop,
+        { shiftKey: false },
+        false
+    );
+    controller.click(
+        (chart.series[0].parentNode.plotX) + chart.plotLeft,
+        (chart.series[0].parentNode.plotY) + chart.plotTop,
+        { shiftKey: true },
+        false
+    );
+    assert.strictEqual(
+        chart.series[0].getSelectedParents().length,
+        2,
+        'It should be possible to select more than one parent node using key modifier.'
+    );
+    controller.triggerEvent(
+        'mouseover',
+        (chart.series[0].parentNode.plotX) + chart.plotLeft,
+        (chart.series[0].parentNode.plotY) + chart.plotTop,
+        { shiftKey: false },
+        false
+    );
+    controller.click(
+        (chart.series[0].parentNode.plotX) + chart.plotLeft,
+        (chart.series[0].parentNode.plotY) + chart.plotTop,
+        { shiftKey: false },
+        false
+    );
+    assert.strictEqual(
+        chart.series[0].getSelectedParents().length,
+        0,
+        'After clicking on the selected parent, all selected parents should be deselected.'
+    );
 });
