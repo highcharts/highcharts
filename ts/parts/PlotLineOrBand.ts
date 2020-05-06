6537823d0744c37cef33cf170d5f1e36fae1c0d1
Izothep <grzegorzblachlinski@gmail.com>
2020-05-06 15:50:46 +0200

    Changed method of fixing duplicated plotBands.

--- ts/parts/PlotLineOrBand.ts
+++ ts/parts/PlotLineOrBand.ts
@@ -294,7 +294,7 @@ class PlotLineOrBand {
              * @name Highcharts.PlotLineOrBand#svgElement
              * @type {Highcharts.SVGElement}
              */
-            plotLine.svgElem = svgElem = svgElem || renderer
+            plotLine.svgElem = svgElem = renderer
                 .path()
                 .attr(attribs)
                 .add(group);
@@ -1280,16 +1280,8 @@ extend(Axis.prototype, /** @lends Highcharts.Axis.prototype */ {
                 'plotLines'
         )
     ): (Highcharts.PlotLineOrBand|undefined) {
-        var obj,
+        var obj = new PlotLineOrBand(this, options).render(),
             userOptions = this.userOptions;
-        // If path of the same id already exists on related axis,
-        // use this path instead.
-        this.plotLinesAndBands.forEach(function (PlotLineOrBand): void {
-            if (options.id && PlotLineOrBand.id === options.id && PlotLineOrBand.svgElem) {
-                obj = PlotLineOrBand;
-            }
-        });
-        obj = obj || new PlotLineOrBand(this, options).render();
 
         if (obj) { // #2189
             // Add it to the user options for exporting and Axis.update
@@ -1300,6 +1292,7 @@ extend(Axis.prototype, /** @lends Highcharts.Axis.prototype */ {
                 userOptions[coll] = updatedOptions;
             }
             this.plotLinesAndBands.push(obj);
+            this._addedPlotLB = true;
         }
 
         return obj;
