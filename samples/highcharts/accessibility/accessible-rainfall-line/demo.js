8e5ff19a7a59bf5ba7b931ac92cfd1c0fee781c7
Øystein Moseng <oystein@highsoft.com>
2020-05-08 16:54:01 +0200

    Added accessible-rainfall-line demo for video use.

---
+++ samples/highcharts/accessibility/accessible-rainfall-line/demo.js
@@ -0,0 +1,64 @@
+Highcharts.chart('container', {
+    chart: {
+        type: 'spline'
+    },
+    accessibility: {
+        point: {
+            descriptionFormatter: function (p) {
+                return p.series.name + ', ' + p.category + ', ' + p.y + ' millimetres rainfall.';
+            }
+        }
+    },
+    title: {
+        text: 'Monthly Average Rainfall'
+    },
+    subtitle: {
+        text: 'Source: WorldClimate.com'
+    },
+    xAxis: {
+        categories: [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        ],
+        crosshair: true
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Rainfall (mm)'
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size: 10px">{point.key} rainfall</span><br/>',
+        valueSuffix: 'mm'
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: 'New York',
+        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+    }, {
+        name: 'Tokyo',
+        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+    }, {
+        name: 'London',
+        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+    }, {
+        name: 'Berlin',
+        data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+    }]
+});
