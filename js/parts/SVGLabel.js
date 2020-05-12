da841a5a8850d394046e6666bdaf91ff600042a5
Torstein Hønsi <torstein@highsoft.com>
2020-05-12 09:44:39 +0200

    SVGLabel refactor, regression in alignment

--- js/parts/SVGLabel.js
+++ js/parts/SVGLabel.js
@@ -91,7 +91,7 @@ var SVGLabel = /** @class */ (function (_super) {
         if (alignFactor !== this.alignFactor) {
             this.alignFactor = alignFactor;
             // Bounding box exists, means we're dynamically changing
-            if (this.bBox) {
+            if (this.bBox && isNumber(this.xSetting)) {
                 this.attr({ x: this.xSetting }); // #5134
             }
         }
