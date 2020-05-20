d8237eaa4bfa0492a4a3cc59783c10336f75fe0c
Paweł Fus <pawel.fus.88@gmail.com>
2020-05-20 09:56:36 +0200

    Fixed #9424, `SVGRenderer.button` and styles DOM leak.

--- ts/parts/SVGRenderer.ts
+++ ts/parts/SVGRenderer.ts
@@ -1607,10 +1607,16 @@ class SVGRenderer {
                 'button'
             ),
             curState = 0,
-            styledMode = this.styledMode;
+            styledMode = this.styledMode,
+            userNormalStyle = normalState && normalState.style || {};
+
+        // Remove stylable attributes
+        if (normalState && normalState.style) {
+            delete normalState.style;
+        }
 
         // Default, non-stylable attributes
-        label.attr(merge({ padding: 8, r: 2 }, normalState as any));
+        label.attr(merge({ padding: 8, r: 2 }, normalState));
 
         if (!styledMode) {
             // Presentational
@@ -1629,6 +1635,8 @@ class SVGRenderer {
                     cursor: 'pointer',
                     fontWeight: 'normal'
                 }
+            }, {
+                style: userNormalStyle
             }, normalState);
             normalStyle = normalState.style;
             delete normalState.style;
