90a624963e20f66d26881ce503503f1bb00bbd7a
Gøran A. Slettemark <goransle@gmail.com>
2020-05-26 13:23:39 +0200

    Docs: Added redirect for removed doc

--- docs/doc-config.js
+++ docs/doc-config.js
@@ -14,6 +14,7 @@ module.exports = {
         { from: 'chart-and-series-types/technical-indicator-series', to: 'stock/technical-indicator-series' },
         { from: 'advanced-chart-features/highcharts-typescript-beta', to: 'advanced-chart-features/highcharts-typescript-declarations' },
         { from: 'maps/create-custom-maps-for-highmaps', to: 'maps/create-custom-maps' },
-        { from: 'stock/understanding-highstock', to: 'stock/understanding-highcharts-stock' }
+        { from: 'stock/understanding-highstock', to: 'stock/understanding-highcharts-stock' },
+        { from: 'getting-started/compatibility', to: 'getting-started/system-requirements' }
     ]
 };
