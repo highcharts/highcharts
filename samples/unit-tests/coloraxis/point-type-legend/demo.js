d9ad0ebcf8f8fb844553cea59ba083728a1c1d0a
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2020-05-29 15:22:04 +0200

    Addition to #12710, added unit tests.

---
+++ samples/unit-tests/coloraxis/point-type-legend/demo.js
@@ -0,0 +1,24 @@
+QUnit.test('Point type legend item', function (assert) {
+    const chart = Highcharts.chart('container', {
+        colorAxis: {
+            minColor: 'rgb(0,0,255)',
+            maxColor: 'rgb(255,0,0)',
+            showInLegend: false
+        },
+        series: [{
+            type: 'pie',
+            showInLegend: true,
+            data: [{
+                y: 100
+            }, {
+                y: 1
+            }]
+        }]
+    });
+
+    assert.strictEqual(
+        chart.legend.allItems[0].legendSymbol.attr('fill'),
+        'rgb(255,0,0)',
+        'shoud have correct color.'
+    );
+});
