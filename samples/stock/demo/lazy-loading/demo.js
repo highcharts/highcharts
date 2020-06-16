8321ff2c97aef2495aafaf2904dc41e0dd7bdcb9
Gøran A. Slettemark <goransle@gmail.com>
2020-06-16 10:09:39 +0200

    Samples: Migrated stock lazy-loading demo to demo-live-data

--- samples/stock/demo/lazy-loading/demo.js
+++ samples/stock/demo/lazy-loading/demo.js
@@ -1,92 +1,93 @@
+const dataURL = 'https://demo-live-data.highcharts.com/aapl-historical.json';
+
 /**
  * Load new data depending on the selected min and max
  */
 function afterSetExtremes(e) {
-
-    var chart = Highcharts.charts[0];
-
+    const { chart } = e.target;
     chart.showLoading('Loading data from server...');
-    $.getJSON('https://www.highcharts.com/samples/data/from-sql.php?start=' + Math.round(e.min) +
-            '&end=' + Math.round(e.max) + '&callback=?', function (data) {
-
-        chart.series[0].setData(data);
-        chart.hideLoading();
-    });
+    fetch(`${dataURL}?start=${Math.round(e.min)}&end=${Math.round(e.max)}`)
+        .then(res => res.ok && res.json())
+        .then(data => {
+            chart.series[0].setData(data);
+            chart.hideLoading();
+        }).catch(error => console.error(error.message));
 }
 
-// See source code from the JSONP handler at https://github.com/highcharts/highcharts/blob/v7.0.0/samples/data/from-sql.php
-$.getJSON('https://www.highcharts.com/samples/data/from-sql.php?callback=?', function (data) {
+fetch(dataURL)
+    .then(res => res.ok && res.json())
+    .then(data => {
 
-    // Add a null value for the end date
-    data = [].concat(data, [[Date.UTC(2011, 9, 14, 19, 59), null, null, null, null]]);
+        // Add a null value for the end date
+        data = [].concat(data, [[Date.UTC(2011, 9, 14, 19, 59), null, null, null, null]]);
 
-    // create the chart
-    Highcharts.stockChart('container', {
-        chart: {
-            type: 'candlestick',
-            zoomType: 'x'
-        },
-
-        navigator: {
-            adaptToUpdatedData: false,
-            series: {
-                data: data
-            }
-        },
-
-        scrollbar: {
-            liveRedraw: false
-        },
-
-        title: {
-            text: 'AAPL history by the minute from 1998 to 2011'
-        },
-
-        subtitle: {
-            text: 'Displaying 1.7 million data points in Highcharts Stock by async server loading'
-        },
-
-        rangeSelector: {
-            buttons: [{
-                type: 'hour',
-                count: 1,
-                text: '1h'
-            }, {
-                type: 'day',
-                count: 1,
-                text: '1d'
-            }, {
-                type: 'month',
-                count: 1,
-                text: '1m'
-            }, {
-                type: 'year',
-                count: 1,
-                text: '1y'
-            }, {
-                type: 'all',
-                text: 'All'
-            }],
-            inputEnabled: false, // it supports only days
-            selected: 4 // all
-        },
-
-        xAxis: {
-            events: {
-                afterSetExtremes: afterSetExtremes
+        // create the chart
+        Highcharts.stockChart('container', {
+            chart: {
+                type: 'candlestick',
+                zoomType: 'x'
             },
-            minRange: 3600 * 1000 // one hour
-        },
 
-        yAxis: {
-            floor: 0
-        },
+            navigator: {
+                adaptToUpdatedData: false,
+                series: {
+                    data: data
+                }
+            },
 
-        series: [{
-            data: data,
-            dataGrouping: {
-                enabled: false
-            }
-        }]
-    });
-});
+            scrollbar: {
+                liveRedraw: false
+            },
+
+            title: {
+                text: 'AAPL history by the minute from 1998 to 2011'
+            },
+
+            subtitle: {
+                text: 'Displaying 1.7 million data points in Highcharts Stock by async server loading'
+            },
+
+            rangeSelector: {
+                buttons: [{
+                    type: 'hour',
+                    count: 1,
+                    text: '1h'
+                }, {
+                    type: 'day',
+                    count: 1,
+                    text: '1d'
+                }, {
+                    type: 'month',
+                    count: 1,
+                    text: '1m'
+                }, {
+                    type: 'year',
+                    count: 1,
+                    text: '1y'
+                }, {
+                    type: 'all',
+                    text: 'All'
+                }],
+                inputEnabled: false, // it supports only days
+                selected: 4 // all
+            },
+
+            xAxis: {
+                events: {
+                    afterSetExtremes: afterSetExtremes
+                },
+                minRange: 3600 * 1000 // one hour
+            },
+
+            yAxis: {
+                floor: 0
+            },
+
+            series: [{
+                data: data,
+                dataGrouping: {
+                    enabled: false
+                }
+            }]
+        });
+    }).catch(error => console.error(error.message));
