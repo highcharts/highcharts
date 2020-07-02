3d7f1499fc01f7d8b15f61be6a58cfc18fa7d062
bre1470 <40056287+bre1470@users.noreply.github.com>
2020-07-02 18:23:47 +0200

    Migrated ControllableCircle to ES6 patterns.

--- js/annotations/controllable/ControllableCircle.js
+++ js/annotations/controllable/ControllableCircle.js
@@ -4,9 +4,9 @@
  *
  * */
 'use strict';
-import controllableMixin from './controllableMixin.js';
+import ControllableMixin from './controllableMixin.js';
 import ControllablePath from './ControllablePath.js';
-import U from './../../parts/Utilities.js';
+import U from '../../parts/Utilities.js';
 var merge = U.merge;
 /* eslint-disable no-invalid-this, valid-jsdoc */
 /**
@@ -15,41 +15,65 @@ var merge = U.merge;
  * @requires modules/annotations
  *
  * @private
- * @constructor
+ * @class
  * @name Highcharts.AnnotationControllableCircle
  *
  * @param {Highcharts.Annotation} annotation an annotation instance
  * @param {Highcharts.AnnotationsShapeOptions} options a shape's options
  * @param {number} index of the circle
- **/
-var ControllableCircle = function (annotation, options, index) {
-    this.init(annotation, options, index);
-    this.collection = 'shapes';
-};
-/**
- * A map object which allows to map options attributes to element attributes.
- *
- * @name Highcharts.AnnotationControllableCircle.attrsMap
- * @type {Highcharts.Dictionary<string>}
  */
-ControllableCircle.attrsMap = merge(ControllablePath.attrsMap, {
-    r: 'r'
-});
-merge(true, ControllableCircle.prototype, controllableMixin, /** @lends Highcharts.AnnotationControllableCircle# */ {
-    /**
-     * @type 'circle'
-     */
-    type: 'circle',
-    translate: controllableMixin.translateShape,
-    render: function (parent) {
+var ControllableCircle = /** @class */ (function () {
+    /* *
+     *
+     *  Constructors
+     *
+     * */
+    function ControllableCircle(annotation, options, index) {
+        /* *
+         *
+         *  Properties
+         *
+         * */
+        this.addControlPoints = ControllableMixin.addControlPoints;
+        this.anchor = ControllableMixin.anchor;
+        this.attr = ControllableMixin.attr;
+        this.attrsFromOptions = ControllableMixin.attrsFromOptions;
+        this.destroy = ControllableMixin.destroy;
+        this.getPointsOptions = ControllableMixin.getPointsOptions;
+        this.init = ControllableMixin.init;
+        this.linkPoints = ControllableMixin.linkPoints;
+        this.point = ControllableMixin.point;
+        this.rotate = ControllableMixin.rotate;
+        this.scale = ControllableMixin.scale;
+        this.setControlPointsVisibility = ControllableMixin.setControlPointsVisibility;
+        this.shouldBeDrawn = ControllableMixin.shouldBeDrawn;
+        this.transform = ControllableMixin.transform;
+        this.transformPoint = ControllableMixin.transformPoint;
+        this.translatePoint = ControllableMixin.translatePoint;
+        this.translateShape = ControllableMixin.translateShape;
+        this.update = ControllableMixin.update;
+        /**
+         * @type 'circle'
+         */
+        this.type = 'circle';
+        this.translate = ControllableMixin.translateShape;
+        this.init(annotation, options, index);
+        this.collection = 'shapes';
+    }
+    /* *
+     *
+     *  Functions
+     *
+     * */
+    ControllableCircle.prototype.render = function (parent) {
         var attrs = this.attrsFromOptions(this.options);
         this.graphic = this.annotation.chart.renderer
             .circle(0, -9e9, 0)
             .attr(attrs)
             .add(parent);
-        controllableMixin.render.call(this);
-    },
-    redraw: function (animation) {
+        ControllableMixin.render.call(this);
+    };
+    ControllableCircle.prototype.redraw = function (animation) {
         var position = this.anchor(this.points[0]).absolutePosition;
         if (position) {
             this.graphic[animation ? 'animate' : 'attr']({
@@ -65,15 +89,29 @@ merge(true, ControllableCircle.prototype, controllableMixin, /** @lends Highchar
             });
         }
         this.graphic.placed = Boolean(position);
-        controllableMixin.redraw.call(this, animation);
-    },
+        ControllableMixin.redraw.call(this, animation);
+    };
     /**
      * Set the radius.
      *
      * @param {number} r a radius to be set
      */
-    setRadius: function (r) {
+    ControllableCircle.prototype.setRadius = function (r) {
         this.options.r = r;
-    }
-});
+    };
+    /* *
+     *
+     *  Static Properties
+     *
+     * */
+    /**
+     * A map object which allows to map options attributes to element
+     * attributes.
+     *
+     * @name Highcharts.AnnotationControllableCircle.attrsMap
+     * @type {Highcharts.Dictionary<string>}
+     */
+    ControllableCircle.attrsMap = merge(ControllablePath.attrsMap, { r: 'r' });
+    return ControllableCircle;
+}());
 export default ControllableCircle;
