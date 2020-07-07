d6e429f2abb9b7db8162296f6273fb799121964d
bre1470 <40056287+bre1470@users.noreply.github.com>
2020-07-07 11:23:38 +0200

    Tools: Fixed import of core files.

--- js/modules/boost/boost-overrides.js
+++ js/modules/boost/boost-overrides.js
@@ -17,7 +17,7 @@ import U from '../../Core/Utilities.js';
 var addEvent = U.addEvent, error = U.error, getOptions = U.getOptions, isArray = U.isArray, isNumber = U.isNumber, pick = U.pick, wrap = U.wrap;
 import '../../parts/Series.js';
 import '../../parts/Options.js';
-import '../../parts/Interaction.js';
+import '../../Core/Interaction.js';
 import butils from './boost-utils.js';
 import boostable from './boostables.js';
 import boostableMap from './boostable-map.js';
