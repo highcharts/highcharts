114ea07c5865fa1bb6e203b82d6cc548cc1822dd
Gøran A. Slettemark <goransle@gmail.com>
2020-07-29 11:04:30 +0200

    Added initial HTMLTable datastore

---
+++ js/Data/HTMLTableDataStore.js
@@ -0,0 +1,135 @@
+/* *
+ *
+ *  Data module
+ *
+ *  (c) 2012-2020 Torstein Honsi
+ *
+ *  License: www.highcharts.com/license
+ *
+ *  !!!!!!! SOURCE GETS TRANSPILED BY TYPESCRIPT. EDIT TS FILE ONLY. !!!!!!!
+ *
+ * */
+'use strict';
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+import DataStore from './DataStore.js';
+import DataTable from './DataTable.js';
+import DataRow from './DataRow.js';
+import U from '../Core/Utilities.js';
+import H from '../Core/Globals.js';
+var win = H.win;
+var uniqueKey = U.uniqueKey;
+/** eslint-disable valid-jsdoc */
+/**
+ * @private
+ */
+var HTMLTableDataStore = /** @class */ (function (_super) {
+    __extends(HTMLTableDataStore, _super);
+    /* *
+    *
+    *  Constructors
+    *
+    * */
+    function HTMLTableDataStore(dataSet, options) {
+        if (dataSet === void 0) { dataSet = new DataTable(); }
+        if (options === void 0) { options = {}; }
+        var _this = _super.call(this, dataSet) || this;
+        _this.table = options.table || '';
+        _this.startRow = options.startRow || 0;
+        _this.endRow = options.endRow || Number.MAX_VALUE;
+        _this.startColumn = options.startColumn || 0;
+        _this.endColumn = options.endColumn || Number.MAX_VALUE;
+        return _this;
+    }
+    HTMLTableDataStore.prototype.colsToDataTable = function (cols, headers) {
+        if (headers === void 0) { headers = []; }
+        var table = new DataTable();
+        var noOfCols = cols.length;
+        if (noOfCols) {
+            var noOfRows = cols[0].length;
+            for (var i = 0; i < noOfRows; ++i) {
+                var row = new DataRow();
+                for (var j = 0; j < noOfCols; ++j) {
+                    row.insertColumn((headers.length ? headers[j] : uniqueKey()), cols[j][i]);
+                }
+                table.insertRow(row);
+            }
+        }
+        return table;
+    };
+    HTMLTableDataStore.prototype.htmlToDataTable = function (table) {
+        var columns = [], headers = [], startRow = this.startRow, endRow = this.endRow, startColumn = this.startColumn, endColumn = this.endColumn;
+        [].forEach.call(table.getElementsByTagName('tr'), function (tr, rowNo) {
+            if (rowNo >= startRow && rowNo <= endRow) {
+                [].forEach.call(tr.children, function (item, colNo) {
+                    var row = columns[colNo - startColumn];
+                    var i = 1;
+                    if ((item.tagName === 'TD' ||
+                        item.tagName === 'TH') &&
+                        colNo >= startColumn &&
+                        colNo <= endColumn) {
+                        if (!columns[colNo - startColumn]) {
+                            columns[colNo - startColumn] = [];
+                        }
+                        if (item.tagName === 'TH') {
+                            headers.push(item.innerHTML);
+                        }
+                        columns[colNo - startColumn][rowNo - startRow] = item.innerHTML;
+                        // Loop over all previous indices and make sure
+                        // they are nulls, not undefined.
+                        while (rowNo - startRow >= i &&
+                            row[rowNo - startRow - i] === void 0) {
+                            row[rowNo - startRow - i] = null;
+                            i++;
+                        }
+                    }
+                });
+            }
+        });
+        return this.colsToDataTable(columns, headers);
+    };
+    /**
+     * Handle supplied table being either an ID or an actual table
+     */
+    HTMLTableDataStore.prototype.fetchTable = function () {
+        var tableToFetch;
+        if (typeof this.table === 'string') {
+            tableToFetch = win.document.getElementById(this.table);
+        }
+        else {
+            tableToFetch = this.table;
+        }
+        this.rows = tableToFetch ? this.htmlToDataTable(tableToFetch) : new DataTable();
+    };
+    /**
+     * Load
+     * TODO: add callback / fire event
+     */
+    HTMLTableDataStore.prototype.load = function () {
+        try {
+            this.fetchTable();
+        }
+        catch (error) {
+            throw new Error('Could not fetch table');
+        }
+    };
+    /**
+     * Save
+     * @todo implement
+     */
+    HTMLTableDataStore.prototype.save = function () {
+    };
+    return HTMLTableDataStore;
+}(DataStore));
+export default HTMLTableDataStore;
---
+++ ts/Data/HTMLTableDataStore.ts
@@ -0,0 +1,172 @@
+/* *
+ *
+ *  Data module
+ *
+ *  (c) 2012-2020 Torstein Honsi
+ *
+ *  License: www.highcharts.com/license
+ *
+ *  !!!!!!! SOURCE GETS TRANSPILED BY TYPESCRIPT. EDIT TS FILE ONLY. !!!!!!!
+ *
+ * */
+
+'use strict';
+
+import DataStore from './DataStore.js';
+import DataTable from './DataTable.js';
+import DataRow from './DataRow.js';
+
+import U from '../Core/Utilities.js';
+import H from '../Core/Globals.js';
+
+const { win } = H;
+const { uniqueKey } = U;
+
+/** eslint-disable valid-jsdoc */
+
+/**
+ * @private
+ */
+
+class HTMLTableDataStore extends DataStore {
+
+
+    /* *
+    *
+    *  Constructors
+    *
+    * */
+
+    public constructor(
+        dataSet: DataTable = new DataTable(),
+        options: Highcharts.DataOptions = {}
+    ) {
+        super(dataSet);
+
+        this.table = options.table || '';
+        this.startRow = options.startRow || 0;
+        this.endRow = options.endRow || Number.MAX_VALUE;
+        this.startColumn = options.startColumn || 0;
+        this.endColumn = options.endColumn || Number.MAX_VALUE;
+    }
+
+    /* *
+    *
+    *  Properties
+    *
+    * */
+    public table: HTMLElement | string;
+    public startRow: number
+    public endRow: number
+    public startColumn: number
+    public endColumn: number
+
+    private colsToDataTable(cols: string[], headers: string[] = []): DataTable {
+        const table = new DataTable();
+        const noOfCols = cols.length;
+        if (noOfCols) {
+            const noOfRows = cols[0].length;
+            for (let i = 0; i < noOfRows; ++i) {
+                const row = new DataRow();
+                for (let j = 0; j < noOfCols; ++j) {
+                    row.insertColumn((headers.length ? headers[j] : uniqueKey()), cols[j][i]);
+                }
+                table.insertRow(row);
+            }
+        }
+        return table;
+    }
+
+    private htmlToDataTable(table: HTMLElement): DataTable {
+        const columns: [] = [],
+            headers: string[] = [],
+            startRow = this.startRow,
+            endRow = this.endRow,
+            startColumn = this.startColumn,
+            endColumn = this.endColumn;
+
+        [].forEach.call(table.getElementsByTagName('tr'), function (
+            tr: HTMLTableRowElement,
+            rowNo: number
+        ): void {
+            if (rowNo >= startRow && rowNo <= endRow) {
+                [].forEach.call(tr.children, function (
+                    item: Element,
+                    colNo: number
+                ): void {
+                    const row = (columns as any)[colNo - startColumn];
+                    let i = 1;
+
+                    if (
+                        (
+                            item.tagName === 'TD' ||
+                            item.tagName === 'TH'
+                        ) &&
+                        colNo >= startColumn &&
+                        colNo <= endColumn
+                    ) {
+                        if (!(columns as any)[colNo - startColumn]) {
+                            (columns as any)[colNo - startColumn] = [];
+                        }
+
+                        if (item.tagName === 'TH') {
+                            headers.push(item.innerHTML);
+                        }
+
+                        (columns as any)[colNo - startColumn][
+                            rowNo - startRow
+                        ] = item.innerHTML;
+
+                        // Loop over all previous indices and make sure
+                        // they are nulls, not undefined.
+                        while (
+                            rowNo - startRow >= i &&
+                            row[rowNo - startRow - i] === void 0
+                        ) {
+                            row[rowNo - startRow - i] = null;
+                            i++;
+                        }
+                    }
+                });
+            }
+        });
+
+        return this.colsToDataTable(columns, headers);
+    }
+    
+    /**
+     * Handle supplied table being either an ID or an actual table
+     */
+    private fetchTable(): void {
+        let tableToFetch;
+        if (typeof this.table === 'string') {
+            tableToFetch = win.document.getElementById(this.table);
+        } else {
+            tableToFetch = this.table;
+        }
+
+        this.rows = tableToFetch ? this.htmlToDataTable(tableToFetch) : new DataTable();
+    }
+
+    /**
+     * Load
+     * TODO: add callback / fire event
+     */
+    public load(): void {
+        try {
+            this.fetchTable();
+        } catch (error) {
+            throw new Error('Could not fetch table');
+        }
+    }
+
+    /**
+     * Save
+     * @todo implement
+     */
+    public save(): void {
+
+    }
+}
+
+export default HTMLTableDataStore;
