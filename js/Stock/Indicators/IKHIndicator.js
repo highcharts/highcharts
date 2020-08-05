1e087c8e9062b47eb7331f55d23e9fec0c65f67d
madepiet <piotr.madej10@gmail.com>
2020-08-05 15:08:37 +0200

    Fixed #13992, ikh errors in console when empty data.

--- js/Stock/Indicators/IKHIndicator.js
+++ js/Stock/Indicators/IKHIndicator.js
@@ -549,11 +549,13 @@ BaseSeries.seriesType('ikh', 'sma',
             spanA = SMA.prototype.getGraphPath.call(indicator, 
             // Reverse points, so Senkou Span A will start from the end:
             indicator.nextPoints);
-            spanA[0][0] = 'L';
-            path = SMA.prototype.getGraphPath.call(indicator, points);
-            spanAarr = spanA.slice(0, path.length);
-            for (var i = spanAarr.length - 1; i >= 0; i--) {
-                path.push(spanAarr[i]);
+            if (spanA.length) {
+                spanA[0][0] = 'L';
+                path = SMA.prototype.getGraphPath.call(indicator, points);
+                spanAarr = spanA.slice(0, path.length);
+                for (var i = spanAarr.length - 1; i >= 0; i--) {
+                    path.push(spanAarr[i]);
+                }
             }
         }
         else {
