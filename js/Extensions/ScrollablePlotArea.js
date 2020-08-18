c9714585f8dd78e3534a12898ef601f8c209db60
Karol Kołodziej <karolkolodziej01@gmail.com>
2020-08-18 10:43:46 +0200

    Touch #13917, updated the docs.

--- js/Extensions/ScrollablePlotArea.js
+++ js/Extensions/ScrollablePlotArea.js
@@ -32,11 +32,11 @@ var addEvent = U.addEvent, createElement = U.createElement, pick = U.pick, stop 
  * vertical scrolling, depending on whether the `minWidth` or `minHeight`
  * option is set.
  *
- * @sample highcharts/chart/scrollable-plotarea
+ * @sample {highcharts} highcharts/chart/scrollable-plotarea
  *         Scrollable plot area
- * @sample highcharts/chart/scrollable-plotarea-vertical
+ * @sample {highcharts} highcharts/chart/scrollable-plotarea-vertical
  *         Vertically scrollable plot area
- * @sample {gantt} highcharts/chart/scrollable-plotarea-vertical
+ * @sample {gantt} gantt/chart/scrollable-plotarea-vertical
  *         Gantt chart with vertically scrollable plot area
  *
  * @since     6.1.0
