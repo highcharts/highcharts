dd3a44d4fbe38903600e4abe9ed8f5bbdd62b585
wchmiel <wojc.chmiel@gmail.com>
2020-09-10 14:24:30 +0200

    Added InvertModifier.

---
+++ test/typescript-karma/Data-Modifiers/InvertModifier/tests.js
@@ -0,0 +1,113 @@
+import DataTable from '/base/js/Data/DataTable.js';
+import InvertModifier from '/base/js/Data/Modifiers/InvertModifier.js';
+
+QUnit.test('InvertModifier.execute', function (assert) {
+
+    const tableJSON = {
+        $class: 'DataTable',
+        rows: [{
+            $class: 'DataTableRow',
+            x: 0,
+            y: 'a'
+        }, {
+            $class: 'DataTableRow',
+            x: 1,
+            y: 'b'
+        }, {
+            $class: 'DataTableRow',
+            x: 2,
+            y: 'c'
+        }, {
+            $class: 'DataTableRow',
+            x: 3,
+            y: 'd'
+        }, {
+            $class: 'DataTableRow',
+            x: 4,
+            y: 'e'
+        }]
+    },
+    table = DataTable.fromJSON(tableJSON),
+    modifier = new InvertModifier(),
+    invertedTable = modifier.execute(table),
+    invertedTable2 = modifier.execute(invertedTable),
+    tableRowIds = table.getAllRowIds(),
+    invertedTableColumns = invertedTable.toColumns(),
+    invertedTableColumnNames = Object.keys(invertedTableColumns),
+    tableColumns = table.toColumns(),
+    tableColumnNames = Object.keys(tableColumns);
+
+    let result,
+        result2,
+        rowId,
+        columnName;
+
+    assert.ok(
+        invertedTable !== table,
+        'The inverted table should be a new table instance.'
+    );
+
+    assert.ok(
+        invertedTableColumnNames.length - 1 === table.getRowCount() &&
+        tableColumnNames.length - 1 === invertedTable.getRowCount(),
+        'The inverted and original table should have an inverted amount of rows and columns.'
+    );
+
+    result = true;
+    invertedTable.getAllRowIds().forEach(function (id) {
+        if (tableColumnNames.indexOf(id) === -1) {
+            result = false;
+        }
+    });
+    assert.ok(
+        result,
+        'The inverted table row ids should be the same as original table column names.'
+    );
+
+    result = true;
+    tableRowIds.forEach(function (id) {
+        if (invertedTableColumnNames.indexOf(id) === -1) {
+            result = false;
+        }
+    });
+    assert.ok(
+        result,
+        'The original table row ids should be the same as inverted table column names.'
+    );
+
+    result = true;
+    result2 = true;
+    for (let i = 0, iEnd = tableRowIds.length; i < iEnd; i++) {
+        rowId = tableRowIds[i];
+
+        for (let j = 0, jEnd = tableColumnNames.length; j < jEnd; j++) {
+            columnName = tableColumnNames[j];
+
+            // Check inverted table.
+            if (
+                columnName !== 'id' &&
+                table.getRowCell(rowId, columnName) !== invertedTable.getRowCell(columnName, rowId)
+            ) {
+                result = false;
+            }
+
+            // Check table inverted twice.
+            if (
+                columnName !== 'id' &&
+                table.getRowCell(rowId, columnName) !== invertedTable2.getRowCell(rowId, columnName)
+            ) {
+                result2 = false;
+            }
+        }
+    }
+
+    assert.ok(
+        result,
+        'The inverted table should have all cell values inverted properly.'
+    );
+
+    assert.ok(
+        result2,
+        'The table inverted twice should have all cell values the same as the original table.'
+    );
+});
---
+++ ts/Data/Modifiers/InvertModifier.ts
@@ -0,0 +1,207 @@
+/* *
+ *
+ *  Data Layer
+ *
+ *  (c) 2012-2020 Torstein Honsi
+ *
+ *  License: www.highcharts.com/license
+ *
+ *  !!!!!!! SOURCE GETS TRANSPILED BY TYPESCRIPT. EDIT TS FILE ONLY. !!!!!!!
+ *
+ * */
+
+/* *
+ *
+ *  Imports
+ *
+ * */
+import type DataEventEmitter from '../DataEventEmitter';
+import DataModifier from './DataModifier.js';
+import DataJSON from '../DataJSON.js';
+import DataTable from '../DataTable.js';
+import U from '../../Core/Utilities.js';
+import DataTableRow from '../DataTableRow.js';
+
+const {
+    merge,
+    defined
+} = U;
+
+class InvertModifier extends DataModifier {
+
+    /* *
+     *
+     *  Static Properties
+     *
+     * */
+
+    /**
+     * Default options for the invert modifier.
+     */
+    public static readonly defaultOptions: InvertModifier.Options = {
+        modifier: 'InvertModifier'
+    };
+
+    /* *
+     *
+     *  Static Functions
+     *
+     * */
+
+    /**
+     * Converts a class JSON to a invert modifier.
+     *
+     * @param {InvertModifier.ClassJSON} json
+     * Class JSON to convert to an instance of invert modifier.
+     *
+     * @return {InvertModifier}
+     * Series points modifier of the class JSON.
+     */
+    public static fromJSON(json: InvertModifier.ClassJSON): InvertModifier {
+        return new InvertModifier(json.options);
+    }
+
+    /* *
+     *
+     *  Constructor
+     *
+     * */
+
+    /**
+     * Constructs an instance of the invert modifier.
+     *
+     * @param {InvertModifier.Options} [options]
+     * Options to configure the invert modifier.
+     */
+    public constructor(options?: DeepPartial<InvertModifier.Options>) {
+        super();
+
+        this.options = merge(InvertModifier.defaultOptions, options);
+    }
+
+    /* *
+     *
+     *  Properties
+     *
+     * */
+
+    /**
+     * Options of the invert modifier.
+     */
+    public options: InvertModifier.Options;
+
+    /* *
+     *
+     *  Functions
+     *
+     * */
+
+    /**
+     * Create new DataTable with inverted rows and columns.
+     *
+     * @param {DataTable} table
+     * Table to modify.
+     *
+     * @param {DataEventEmitter.EventDetail} [eventDetail]
+     * Custom information for pending events.
+     *
+     * @return {DataTable}
+     * New modified table.
+     */
+    public execute(
+        table: DataTable,
+        eventDetail?: DataEventEmitter.EventDetail
+    ): DataTable {
+        const modifier = this,
+            newTable = new DataTable(),
+            columns = table.toColumns(),
+            newRowIds = Object.keys(columns),
+            oldRowsLength = table.getRowCount();
+
+        let oldRow,
+            newCells: Record<string, DataTableRow.CellType>,
+            newRow,
+            rowCell;
+
+        modifier.emit({ type: 'execute', detail: eventDetail, table });
+
+        for (let i = 0, iEnd = newRowIds.length; i < iEnd; i++) {
+            if (newRowIds[i] !== 'id') {
+                newCells = {
+                    id: newRowIds[i]
+                };
+
+                for (let j = 0; j < oldRowsLength; j++) {
+                    oldRow = table.getRow(j);
+                    rowCell = oldRow?.getCell(newRowIds[i]);
+
+                    if (defined(rowCell) && oldRow?.id) {
+                        newCells[oldRow.id] = rowCell;
+                    }
+                }
+
+                newRow = new DataTableRow(newCells);
+                newTable.insertRow(newRow);
+            }
+        }
+
+        modifier.emit({ type: 'afterExecute', detail: eventDetail, table: newTable });
+
+        return newTable;
+    }
+
+    /**
+     * Converts the invert modifier to a class JSON,
+     * including all containing all modifiers.
+     *
+     * @return {DataJSON.ClassJSON}
+     * Class JSON of this invert modifier.
+     */
+    public toJSON(): InvertModifier.ClassJSON {
+        const json = {
+            $class: 'InvertModifier',
+            options: merge(this.options)
+        };
+
+        return json;
+    }
+}
+
+/* *
+ *
+ *  Namespace
+ *
+ * */
+namespace InvertModifier {
+    /**
+     * Interface of the class JSON to convert to modifier instances.
+     */
+    export interface ClassJSON extends DataModifier.ClassJSON {
+        // nothing here yet
+    }
+
+    export interface Options extends DataModifier.Options {}
+}
+
+/* *
+ *
+ *  Register
+ *
+ * */
+
+DataJSON.addClass(InvertModifier);
+DataModifier.addModifier(InvertModifier);
+
+declare module './Types' {
+    interface DataModifierTypeRegistry {
+        Invert: typeof InvertModifier;
+    }
+}
+
+/* *
+ *
+ *  Export
+ *
+ * */
+
+export default InvertModifier;
