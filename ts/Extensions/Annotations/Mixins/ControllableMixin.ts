8ffc050f6a223b7fca16af1a16366537b60d45a8
Sebastian Wedzel <bastian-blacklabel@MacBook-Pro-Sebastian.local>
2020-09-25 18:30:56 +0200

    Fixed #13956, annotations had wrong position for rezised yAxis.

--- ts/Extensions/Annotations/Mixins/ControllableMixin.ts
+++ ts/Extensions/Annotations/Mixins/ControllableMixin.ts
@@ -260,7 +260,7 @@ var controllableMixin: Highcharts.AnnotationControllableMixin = {
         point: Highcharts.AnnotationPointType
     ): Highcharts.AnnotationAnchorObject {
         var plotBox = point.series.getPlotBox(),
-
+            chart = point.series.chart,
             box = point.mock ?
                 point.toAnchor() :
                 Tooltip.prototype.getAnchor.call({
@@ -278,7 +278,7 @@ var controllableMixin: Highcharts.AnnotationControllableMixin = {
             relativePosition: anchor,
             absolutePosition: merge(anchor, {
                 x: anchor.x + plotBox.translateX,
-                y: anchor.y + plotBox.translateY
+                y: anchor.y + (point.mock ? plotBox.translateY : chart.plotTop)
             })
         };
     },
