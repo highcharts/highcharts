58fd22c4b13e85745b9fce3179c760811fad6a45
Øystein Moseng <oystein@highsoft.com>
2020-10-05 11:03:06 +0200

    Fixed #14278, auto-highlighted first point on long touch.

--- ts/Accessibility/KeyboardNavigation.ts
+++ ts/Accessibility/KeyboardNavigation.ts
@@ -165,11 +165,15 @@ KeyboardNavigation.prototype = {
         ep.addEvent(this.tabindexContainer, 'focus',
             (e: FocusEvent): void => this.onFocus(e));
 
-        ep.addEvent(doc, 'mouseup', (): void => this.onMouseUp());
+        ['mouseup', 'touchend'].forEach((eventName): Function =>
+            ep.addEvent(doc, eventName, (): void => this.onMouseUp())
+        );
 
-        ep.addEvent(chart.renderTo, 'mousedown', (): void => {
-            this.isClickingChart = true;
-        });
+        ['mousedown', 'touchstart'].forEach((eventName): Function =>
+            ep.addEvent(chart.renderTo, eventName, (): void => {
+                this.isClickingChart = true;
+            })
+        );
 
         ep.addEvent(chart.renderTo, 'mouseover', (): void => {
             this.pointerIsOverChart = true;
