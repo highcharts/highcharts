affe0841c3b2c591d8093bed87f4429ffa8b24fc
Marita <marita.vindedal@highsoft.com>
2020-10-07 11:04:27 +0200

    Set y to be null if user types in a negative value

--- js/Series/ItemSeries.js
+++ js/Series/ItemSeries.js
@@ -279,7 +279,7 @@ BaseSeries.seriesType('item',
     },
     drawPoints: function () {
         var series = this, options = this.options, renderer = series.chart.renderer, seriesMarkerOptions = options.marker, borderWidth = this.borderWidth, crisp = borderWidth % 2 ? 0.5 : 1, i = 0, rows = this.getRows(), cols = Math.ceil(this.total / rows), cellWidth = this.chart.plotWidth / cols, cellHeight = this.chart.plotHeight / rows, itemSize = this.itemSize || Math.min(cellWidth, cellHeight);
-        /*
+        /* @todo: remove if not needed
         this.slots.forEach(slot => {
             this.chart.renderer.circle(slot.x, slot.y, 6)
                 .attr({
@@ -390,7 +390,8 @@ BaseSeries.seriesType('item',
     connectorShapes: piePoint.connectorShapes,
     getConnectorPath: piePoint.getConnectorPath,
     setVisible: piePoint.setVisible,
-    getTranslate: piePoint.getTranslate
+    getTranslate: piePoint.getTranslate,
+    isValid: piePoint.isValid
 });
 /* *
  *
--- ts/Series/ItemSeries.ts
+++ ts/Series/ItemSeries.ts
@@ -436,7 +436,7 @@ BaseSeries.seriesType<typeof Highcharts.ItemSeries>(
                 cellHeight = this.chart.plotHeight / rows,
                 itemSize = this.itemSize || Math.min(cellWidth, cellHeight);
 
-            /*
+            /* @todo: remove if not needed
             this.slots.forEach(slot => {
                 this.chart.renderer.circle(slot.x, slot.y, 6)
                     .attr({
@@ -589,7 +589,8 @@ BaseSeries.seriesType<typeof Highcharts.ItemSeries>(
         connectorShapes: piePoint.connectorShapes,
         getConnectorPath: piePoint.getConnectorPath,
         setVisible: piePoint.setVisible,
-        getTranslate: piePoint.getTranslate
+        getTranslate: piePoint.getTranslate,
+        isValid: piePoint.isValid
     }
 );
 
