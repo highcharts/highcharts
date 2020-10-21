6943efa180d302a7f73fe97eff727947f15e1ae6
djtechnoo <askeltechno@hotmail.com>
2020-10-21 13:47:33 +0200

    Refactored: map-drilldown demo, minimized jquery

--- samples/maps/demo/map-drilldown/demo.js
+++ samples/maps/demo/map-drilldown/demo.js
@@ -3,15 +3,14 @@ TODO:
 - Check data labels after drilling. Label rank? New positions?
 */
 
-var data = Highcharts.geojson(Highcharts.maps['countries/us/us-all']),
-    separators = Highcharts.geojson(Highcharts.maps['countries/us/us-all'], 'mapline'),
-    // Some responsiveness
-    small = $('#container').width() < 400;
+let data = Highcharts.geojson(Highcharts.maps['countries/us/us-all']);
+const separators = Highcharts.geojson(Highcharts.maps['countries/us/us-all'], 'mapline');
+
 
 // Set drilldown pointers
-$.each(data, function (i) {
-    this.drilldown = this.properties['hc-key'];
-    this.value = i; // Non-random bogus data
+data.forEach((d, i) => {
+    d.drilldown = d.properties['hc-key'];
+    d.value = i; // Non-random bogus data
 });
 
 // Instantiate the map
@@ -20,17 +19,17 @@ Highcharts.mapChart('container', {
         events: {
             drilldown: function (e) {
                 if (!e.seriesOptions) {
-                    var chart = this,
-                        mapKey = 'countries/us/' + e.point.drilldown + '-all',
+                    const chart = this,
+                        mapKey = 'countries/us/' + e.point.drilldown + '-all';
                         // Handle error, the timeout is cleared on success
-                        fail = setTimeout(function () {
-                            if (!Highcharts.maps[mapKey]) {
-                                chart.showLoading('<i class="icon-frown"></i> Failed loading ' + e.point.name);
-                                fail = setTimeout(function () {
-                                    chart.hideLoading();
-                                }, 1000);
-                            }
-                        }, 3000);
+                    let fail = setTimeout(function () {
+                        if (!Highcharts.maps[mapKey]) {
+                            chart.showLoading('<i class="icon-frown"></i> Failed loading ' + e.point.name);
+                            fail = setTimeout(function () {
+                                chart.hideLoading();
+                            }, 1000);
+                        }
+                    }, 3000);
 
                     // Show the spinner
                     chart.showLoading('<i class="icon-spinner icon-spin icon-3x"></i>'); // Font Awesome spinner
@@ -41,8 +40,8 @@ Highcharts.mapChart('container', {
                         data = Highcharts.geojson(Highcharts.maps[mapKey]);
 
                         // Set a non-random bogus value
-                        $.each(data, function (i) {
-                            this.value = i;
+                        data.forEach((d, i) => {
+                            d.value = i;
                         });
 
                         // Hide loading and add series
@@ -79,12 +78,6 @@ Highcharts.mapChart('container', {
         style: {
             fontSize: '16px'
         }
-    },
-
-    legend: small ? {} : {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
     },
 
     colorAxis: {
