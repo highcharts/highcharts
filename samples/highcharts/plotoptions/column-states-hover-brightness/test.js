d9de4ceabe8bbe26ee4d651b7bfc254991107996
Torstein Hønsi <torstein@highsoft.com>
2020-10-22 15:45:05 +0200

    Samples: Got rid of obsolete test.js and unit-tests.js

--- samples/highcharts/plotoptions/column-states-hover-brightness/test.js
+++
@@ -1,3 +0,0 @@
-function test() { // eslint-disable-line no-unused-vars
-    $('#container').highcharts().series[0].points[2].onMouseOver();
-}
--- samples/highcharts/xaxis/crosshair-customized/test.js
+++
@@ -1,6 +0,0 @@
-function test(chart) { // eslint-disable-line no-unused-vars
-    var point = chart.series[0].points[2];
-
-    // Set hoverPoint
-    point.onMouseOver();
-}
