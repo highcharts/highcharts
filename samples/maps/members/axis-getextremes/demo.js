28e2203628d493e9748a6ba9d8878382a8555fc4
djtechnoo <askeltechno@hotmail.com>
2020-10-22 09:59:27 +0200

    Refactored: removed jQuery from maps/axis-getextremes.

--- samples/maps/members/axis-getextremes/demo.js
+++ samples/maps/members/axis-getextremes/demo.js
@@ -1,4 +1,4 @@
-var chart;
+let chart;
 Highcharts.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/world-population-density.json', function (data) {
 
     // Initiate the chart
@@ -31,23 +31,30 @@ Highcharts.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/sam
     });
 });
 
-$('#getextremes').click(function () {
-    var xExt = chart.xAxis[0].getExtremes(),
-        yExt = chart.yAxis[0].getExtremes(),
-        colorExt = chart.colorAxis[0].getExtremes();
+const getExtremes = () => {
+    // Object holding the extremes
+    const ext = {
+        x: chart.xAxis[0].getExtremes(),
+        y: chart.yAxis[0].getExtremes(),
+        color: chart.colorAxis[0].getExtremes()
+    };
 
     chart.setTitle(null, {
         text: '<b>X axis</b><br>' +
-            'min: ' + xExt.min + ', max: ' + xExt.max + '<br>' +
-            'dataMin: ' + xExt.dataMin + ', dataMax: ' + xExt.dataMax + '<br>' +
+            'min: ' + ext.x.min + ', max: ' + ext.x.max + '<br>' +
+            'dataMin: ' + ext.x.dataMin + ', dataMax: ' + ext.x.dataMax + '<br>' +
             '<b>Y axis</b><br>' +
-            'min: ' + yExt.min + ', max: ' + yExt.max + '<br>' +
-            'dataMin: ' + yExt.dataMin + ', dataMax: ' + yExt.dataMax + '<br>' +
+            'min: ' + ext.y.min + ', max: ' + ext.y.max + '<br>' +
+            'dataMin: ' + ext.y.dataMin + ', dataMax: ' + ext.y.dataMax + '<br>' +
             '<b>Color axis</b><br>' +
-            'min: ' + colorExt.min + ', max: ' + colorExt.max + '<br>' +
-            'dataMin: ' + colorExt.dataMin + ', dataMax: ' + colorExt.dataMax + '<br>',
+            'min: ' + ext.color.min + ', max: ' + ext.color.max + '<br>' +
+            'dataMin: ' + ext.color.dataMin + ', dataMax: ' + ext.color.dataMax + '<br>',
         align: 'left',
         floating: true,
         y: 250
     });
-});
+};
+
+document.getElementById('getextremes').onclick = () => {
+    getExtremes();
+};
