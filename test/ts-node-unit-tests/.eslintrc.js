c73303ecb76bb3e6ce03215c7920c8d959769aa4
Gøran A. Slettemark <goransle@gmail.com>
2020-10-23 13:28:10 +0200

    Improved logging, simpler tests

--- test/ts-node-unit-tests/.eslintrc.js
+++ test/ts-node-unit-tests/.eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true
-    },
+    extends: [],
+    parser: '@typescript-eslint/parser',
     parserOptions: {
-        ecmaVersion: 12,
+        ecmaVersion: 2020,
         sourceType: 'module'
     },
     plugins: [
+        'highcharts',
         '@typescript-eslint'
     ],
+    env: {
+        es2021: true,
+        node: true
+    },
     rules: {
-        'no-console': 0
+        'no-console': 0,
+        'require-jsdoc': 0
     }
 };
