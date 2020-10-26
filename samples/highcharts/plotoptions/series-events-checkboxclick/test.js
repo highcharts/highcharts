acb52a9b114329e186ade86a3a0dc0a2a8a8340e
Ken-Håvard Lieng <kenh.lieng@highsoft.com>
2020-10-26 08:06:38 +0100

    Removed conflicting test.js files

--- samples/highcharts/plotoptions/series-events-checkboxclick/test.js
+++
@@ -1,7 +0,0 @@
-function test(chart) { // eslint-disable-line no-unused-vars
-    document.querySelector('#container input').dispatchEvent(new MouseEvent('click', {
-        view: window,
-        bubbles: true,
-        cancelable: true
-    }));
-}
--- samples/highcharts/tooltip/snap-50/test.js
+++
@@ -1,6 +0,0 @@
-function test(chart) { // eslint-disable-line no-unused-vars
-    chart.container.querySelectorAll('.highcharts-tracker').forEach(tracker => {
-        tracker.setAttribute('stroke', 'pink');
-        tracker.setAttribute('stroke-opacity', '0.3');
-    });
-}
