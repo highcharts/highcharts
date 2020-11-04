8eaf21a96dbce5106e6e81319155d1d7add9e5bc
Ken-Håvard Lieng <kenh.lieng@highsoft.com>
2020-11-04 08:37:36 +0100

    Used TestController in panning tests

--- samples/unit-tests/interaction/panning/demo.js
+++ samples/unit-tests/interaction/panning/demo.js
@@ -26,39 +26,9 @@ QUnit.test('#14426: Vertical panning after zooming', assert => {
         }]
     });
 
-    const offset = Highcharts.offset(chart.container);
-
-    // Zoom
-    chart.pointer.onContainerMouseDown({
-        type: 'mousedown',
-        pageX: 250 + offset.left,
-        pageY: 50 + offset.top,
-        target: chart.container
-    });
-    chart.pointer.onContainerMouseMove({
-        type: 'mousemove',
-        pageX: 400 + offset.left,
-        pageY: 300 + offset.top,
-        target: chart.container
-    });
-    chart.pointer.onDocumentMouseUp({});
-
-    // Pan
-    chart.pointer.onContainerMouseDown({
-        type: 'mousedown',
-        pageX: 250 + offset.left,
-        pageY: 300 + offset.top,
-        target: chart.container,
-        shiftKey: true
-    });
-    chart.pointer.onContainerMouseMove({
-        type: 'mousemove',
-        pageX: 250 + offset.left,
-        pageY: 50 + offset.top,
-        target: chart.container,
-        shiftKey: true
-    });
-    chart.pointer.onDocumentMouseUp({});
+    const controller = new TestController(chart);
+    controller.pan([250, 50], [400, 300]);
+    controller.pan([250, 300], [250, 50], { shiftKey: true });
 
     assert.strictEqual(chart.yAxis[0].min, 0, 'It should be possible to pan to 0 vertically');
 });
