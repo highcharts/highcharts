2e13c92b97c737bf53d59991951d6757d02d7922
Karol Kołodziej <karolkolodziej01@gmail.com>
2020-11-17 15:21:20 +0100

    Added initial class pattern to cmf indicator.

--- ts/Stock/Indicators/CMFIndicator.ts
+++ ts/Stock/Indicators/CMFIndicator.ts
@@ -16,45 +16,28 @@
 
 import type IndicatorValuesObject from './IndicatorValuesObject';
 import type LineSeries from '../../Series/Line/LineSeries';
-import type SMAIndicator from './SMA/SMAIndicator';
 import type {
     SMAOptions,
     SMAParamsOptions
 } from './SMA/SMAOptions';
 import type SMAPoint from './SMA/SMAPoint';
 import BaseSeries from '../../Core/Series/Series.js';
+const {
+    seriesTypes: {
+        sma: SMAIndicator
+    }
+} = BaseSeries;
+import U from '../../Core/Utilities.js';
+const {
+    extend,
+    merge
+} = U;
 
 /**
  * @private
  */
 declare global {
     namespace Highcharts {
-        class CMFIndicator extends SMAIndicator {
-            public data: Array<CMFIndicatorPoint>;
-            public nameBase: string;
-            public options: CMFIndicatorOptions;
-            public pointClass: typeof CMFIndicatorPoint;
-            public points: Array<CMFIndicatorPoint>;
-            public volumeSeries: LineSeries;
-            public linkedParent: LineSeries;
-            public yData: Array<Array<number>>;
-            public getMoneyFlow<TLinkedSeries extends LineSeries>(
-                xData: (Array<number>|undefined),
-                seriesYData: (
-                    Array<(number|null|undefined)>|
-                    Array<Array<(number | null | undefined)>>|
-                    undefined
-                ),
-                volumeSeriesYData: Array<number>,
-                period: number
-            ): IndicatorValuesObject<TLinkedSeries>;
-            public getValues<TLinkedSeries extends LineSeries>(
-                series: TLinkedSeries,
-                params: CMFIndicatorParamsOptions
-            ): (IndicatorValuesObject<TLinkedSeries>|undefined);
-            public isValid(): boolean;
-        }
-
         interface CMFIndicatorOptions extends SMAOptions {
             params?: CMFIndicatorParamsOptions;
         }
@@ -69,11 +52,6 @@ declare global {
     }
 }
 
-declare module '../../Core/Series/SeriesType' {
-    interface SeriesTypeRegistry {
-        cmf: typeof Highcharts.CMFIndicator;
-    }
-}
 
 // im port './SMAIndicator.js';
 
@@ -86,7 +64,7 @@ declare module '../../Core/Series/SeriesType' {
  *
  * @augments Highcharts.Series
  */
-BaseSeries.seriesType<typeof Highcharts.CMFIndicator>('cmf', 'sma',
+class CMFIndicator extends SMAIndicator {
     /**
      * Chaikin Money Flow indicator (cmf).
      *
@@ -101,7 +79,7 @@ BaseSeries.seriesType<typeof Highcharts.CMFIndicator>('cmf', 'sma',
      * @requires     stock/indicators/cmf
      * @optionparent plotOptions.cmf
      */
-    {
+    public static defaultOptions: Highcharts.CMFIndicatorOptions = merge(SMAIndicator.defaultOptions, {
         params: {
             period: 14,
             /**
@@ -110,128 +88,150 @@ BaseSeries.seriesType<typeof Highcharts.CMFIndicator>('cmf', 'sma',
              */
             volumeSeriesID: 'volume'
         }
-    },
+    } as Highcharts.CMFIndicatorOptions);
+}
+interface CMFIndicator {
+    data: Array<Highcharts.CMFIndicatorPoint>;
+    nameBase: string;
+    options: Highcharts.CMFIndicatorOptions;
+    pointClass: typeof Highcharts.CMFIndicatorPoint;
+    points: Array<Highcharts.CMFIndicatorPoint>;
+    volumeSeries: LineSeries;
+    linkedParent: LineSeries;
+    yData: Array<Array<number>>;
+    getMoneyFlow<TLinkedSeries extends LineSeries>(
+        xData: (Array<number>|undefined),
+        seriesYData: (
+            Array<(number|null|undefined)>|
+            Array<Array<(number | null | undefined)>>|
+            undefined
+        ),
+        volumeSeriesYData: Array<number>,
+        period: number
+    ): IndicatorValuesObject<TLinkedSeries>;
+    getValues<TLinkedSeries extends LineSeries>(
+        series: TLinkedSeries,
+        params: Highcharts.CMFIndicatorParamsOptions
+    ): (IndicatorValuesObject<TLinkedSeries>|undefined);
+    isValid(): boolean;
+}
+
+extend(CMFIndicator.prototype, {
+    nameBase: 'Chaikin Money Flow',
     /**
-     * @lends Highcharts.Series#
+     * Checks if the series and volumeSeries are accessible, number of
+     * points.x is longer than period, is series has OHLC data
+     * @private
+     * @param {Highcharts.CMFIndicator} this indicator to use.
+     * @return {boolean} True if series is valid and can be computed,
+     * otherwise false.
      */
-    {
-        nameBase: 'Chaikin Money Flow',
-        /**
-         * Checks if the series and volumeSeries are accessible, number of
-         * points.x is longer than period, is series has OHLC data
-         * @private
-         * @param {Highcharts.CMFIndicator} this indicator to use.
-         * @return {boolean} True if series is valid and can be computed,
-         * otherwise false.
-         */
-        isValid: function (this: Highcharts.CMFIndicator): boolean {
-            var chart = this.chart,
-                options: Highcharts.CMFIndicatorOptions = this.options,
-                series = this.linkedParent,
-                volumeSeries: LineSeries = (
-                    this.volumeSeries ||
+    isValid: function (this: CMFIndicator): boolean {
+        var chart = this.chart,
+            options: Highcharts.CMFIndicatorOptions = this.options,
+            series = this.linkedParent,
+            volumeSeries: LineSeries = (
+                this.volumeSeries ||
                     (
                         this.volumeSeries =
                         chart.get((options.params as any).volumeSeriesID) as any
                     )
-                ),
-                isSeriesOHLC: (boolean|undefined) = (
-                    series &&
-                    series.yData &&
-                    (series.yData as any)[0].length === 4
-                );
-
-            /**
-             * @private
-             * @param {Highcharts.Series} serie to check length validity on.
-             * @return {boolean|undefined} true if length is valid.
-             */
-            function isLengthValid(
-                serie: LineSeries
-            ): (boolean|undefined) {
-                return serie.xData &&
-                    serie.xData.length >= (options.params as any).period;
-            }
-
-            return !!(
+            ),
+            isSeriesOHLC: (boolean|undefined) = (
                 series &&
+                series.yData &&
+                (series.yData as any)[0].length === 4
+            );
+
+        /**
+         * @private
+         * @param {Highcharts.Series} serie to check length validity on.
+         * @return {boolean|undefined} true if length is valid.
+         */
+        function isLengthValid(
+            serie: LineSeries
+        ): (boolean|undefined) {
+            return serie.xData &&
+                serie.xData.length >= (options.params as any).period;
+        }
+
+        return !!(
+            series &&
                 volumeSeries &&
                 isLengthValid(series) &&
                 isLengthValid(volumeSeries) && isSeriesOHLC
-            );
-        },
+        );
+    },
+
+    /**
+     * Returns indicator's data.
+     * @private
+     * @param {Highcharts.CMFIndicator} this indicator to use.
+     * @param {Highcharts.Series} series to calculate values from
+     * @param {Highcharts.CMFIndicatorParamsOptions} params to pass
+     * @return {boolean|Highcharts.IndicatorNullableValuesObject} Returns false if the
+     * indicator is not valid, otherwise returns Values object.
+     */
+    getValues: function<TLinkedSeries extends LineSeries> (
+        this: CMFIndicator,
+        series: TLinkedSeries,
+        params: Highcharts.CMFIndicatorParamsOptions
+    ): (IndicatorValuesObject<TLinkedSeries>|undefined) {
+        if (!this.isValid()) {
+            return;
+        }
+
+        return this.getMoneyFlow<TLinkedSeries>(
+            series.xData,
+            series.yData,
+            (this.volumeSeries.yData as any),
+            (params.period as any)
+        );
+    },
+
+    /**
+     * @private
+     * @param {Array<number>} xData - x timestamp values
+     * @param {Array<number>} seriesYData - yData of basic series
+     * @param {Array<number>} volumeSeriesYData - yData of volume series
+     * @param {number} period - indicator's param
+     * @return {Highcharts.IndicatorNullableValuesObject} object containing computed money
+     * flow data
+     */
+    getMoneyFlow: function<TLinkedSeries extends LineSeries> (
+        xData: Array<number>,
+        seriesYData: TLinkedSeries['yData'],
+        volumeSeriesYData: Array<number>,
+        period: number
+    ): IndicatorValuesObject<TLinkedSeries> {
+        var len: number = (seriesYData as any).length,
+            moneyFlowVolume: Array<(number|null)> = [],
+            sumVolume = 0,
+            sumMoneyFlowVolume = 0,
+            moneyFlowXData: Array<number> = [],
+            moneyFlowYData: Array<(number|null)> = [],
+            values: Array<Array<(number|null)>> = [],
+            i: number,
+            point: [number, (number|null)],
+            nullIndex = -1;
 
         /**
-         * Returns indicator's data.
+         * Calculates money flow volume, changes i, nullIndex vars from
+         * upper scope!
          * @private
-         * @param {Highcharts.CMFIndicator} this indicator to use.
-         * @param {Highcharts.Series} series to calculate values from
-         * @param {Highcharts.CMFIndicatorParamsOptions} params to pass
-         * @return {boolean|Highcharts.IndicatorNullableValuesObject} Returns false if the
-         * indicator is not valid, otherwise returns Values object.
-         */
-        getValues: function<
-            TLinkedSeries extends LineSeries
-        > (
-            this: Highcharts.CMFIndicator,
-            series: TLinkedSeries,
-            params: Highcharts.CMFIndicatorParamsOptions
-        ): (IndicatorValuesObject<TLinkedSeries>|undefined) {
-            if (!this.isValid()) {
-                return;
-            }
+         * @param {Array<number>} ohlc - OHLC point
+         * @param {number} volume - Volume point's y value
+         * @return {number|null} - volume * moneyFlowMultiplier
+         **/
+        function getMoneyFlowVolume(
+            ohlc: Array<number>,
+            volume: number
+        ): (number|null) {
+            var high: number = ohlc[1],
+                low: number = ohlc[2],
+                close: number = ohlc[3],
 
-            return this.getMoneyFlow<TLinkedSeries>(
-                series.xData,
-                series.yData,
-                (this.volumeSeries.yData as any),
-                (params.period as any)
-            );
-        },
-
-        /**
-         * @private
-         * @param {Array<number>} xData - x timestamp values
-         * @param {Array<number>} seriesYData - yData of basic series
-         * @param {Array<number>} volumeSeriesYData - yData of volume series
-         * @param {number} period - indicator's param
-         * @return {Highcharts.IndicatorNullableValuesObject} object containing computed money
-         * flow data
-         */
-        getMoneyFlow: function<TLinkedSeries extends LineSeries> (
-            xData: Array<number>,
-            seriesYData: TLinkedSeries['yData'],
-            volumeSeriesYData: Array<number>,
-            period: number
-        ): IndicatorValuesObject<TLinkedSeries> {
-            var len: number = (seriesYData as any).length,
-                moneyFlowVolume: Array<(number|null)> = [],
-                sumVolume = 0,
-                sumMoneyFlowVolume = 0,
-                moneyFlowXData: Array<number> = [],
-                moneyFlowYData: Array<(number|null)> = [],
-                values: Array<Array<(number|null)>> = [],
-                i: number,
-                point: [number, (number|null)],
-                nullIndex = -1;
-
-            /**
-             * Calculates money flow volume, changes i, nullIndex vars from
-             * upper scope!
-             * @private
-             * @param {Array<number>} ohlc - OHLC point
-             * @param {number} volume - Volume point's y value
-             * @return {number|null} - volume * moneyFlowMultiplier
-             **/
-            function getMoneyFlowVolume(
-                ohlc: Array<number>,
-                volume: number
-            ): (number|null) {
-                var high: number = ohlc[1],
-                    low: number = ohlc[2],
-                    close: number = ohlc[3],
-
-                    isValid: boolean =
+                isValid: boolean =
                         volume !== null &&
                         high !== null &&
                         low !== null &&
@@ -239,77 +239,92 @@ BaseSeries.seriesType<typeof Highcharts.CMFIndicator>('cmf', 'sma',
                         high !== low;
 
 
-                /**
-                 * @private
-                 * @param {number} h - High value
-                 * @param {number} l - Low value
-                 * @param {number} c - Close value
-                 * @return {number} calculated multiplier for the point
-                 **/
-                function getMoneyFlowMultiplier(
-                    h: number,
-                    l: number,
-                    c: number
-                ): number {
-                    return ((c - l) - (h - c)) / (h - l);
-                }
-
-                return isValid ?
-                    getMoneyFlowMultiplier(high, low, close) * volume :
-                    ((nullIndex = i), null);
+            /**
+             * @private
+             * @param {number} h - High value
+             * @param {number} l - Low value
+             * @param {number} c - Close value
+             * @return {number} calculated multiplier for the point
+             **/
+            function getMoneyFlowMultiplier(
+                h: number,
+                l: number,
+                c: number
+            ): number {
+                return ((c - l) - (h - c)) / (h - l);
             }
 
+            return isValid ?
+                getMoneyFlowMultiplier(high, low, close) * volume :
+                ((nullIndex = i), null);
+        }
 
-            if (period > 0 && period <= len) {
-                for (i = 0; i < period; i++) {
-                    moneyFlowVolume[i] = getMoneyFlowVolume(
-                        (seriesYData as any)[i],
-                        volumeSeriesYData[i]
-                    );
-                    sumVolume += volumeSeriesYData[i];
-                    sumMoneyFlowVolume += (moneyFlowVolume[i] as any);
-                }
+        if (period > 0 && period <= len) {
+            for (i = 0; i < period; i++) {
+                moneyFlowVolume[i] = getMoneyFlowVolume(
+                    (seriesYData as any)[i],
+                    volumeSeriesYData[i]
+                );
+                sumVolume += volumeSeriesYData[i];
+                sumMoneyFlowVolume += (moneyFlowVolume[i] as any);
+            }
 
-                moneyFlowXData.push(xData[i - 1]);
-                moneyFlowYData.push(
-                    i - nullIndex >= period && sumVolume !== 0 ?
+            moneyFlowXData.push(xData[i - 1]);
+            moneyFlowYData.push(
+                i - nullIndex >= period && sumVolume !== 0 ?
+                    sumMoneyFlowVolume / sumVolume :
+                    null
+            );
+            values.push([moneyFlowXData[0], moneyFlowYData[0]]);
+
+            for (; i < len; i++) {
+                moneyFlowVolume[i] = getMoneyFlowVolume(
+                    (seriesYData as any)[i],
+                    volumeSeriesYData[i]
+                );
+
+                sumVolume -= volumeSeriesYData[i - period];
+                sumVolume += volumeSeriesYData[i];
+
+                sumMoneyFlowVolume -= (moneyFlowVolume[i - period] as any);
+                sumMoneyFlowVolume += (moneyFlowVolume[i] as any);
+
+                point = [
+                    xData[i],
+                    i - nullIndex >= period ?
                         sumMoneyFlowVolume / sumVolume :
                         null
-                );
-                values.push([moneyFlowXData[0], moneyFlowYData[0]]);
+                ];
 
-                for (; i < len; i++) {
-                    moneyFlowVolume[i] = getMoneyFlowVolume(
-                        (seriesYData as any)[i],
-                        volumeSeriesYData[i]
-                    );
-
-                    sumVolume -= volumeSeriesYData[i - period];
-                    sumVolume += volumeSeriesYData[i];
-
-                    sumMoneyFlowVolume -= (moneyFlowVolume[i - period] as any);
-                    sumMoneyFlowVolume += (moneyFlowVolume[i] as any);
-
-                    point = [
-                        xData[i],
-                        i - nullIndex >= period ?
-                            sumMoneyFlowVolume / sumVolume :
-                            null
-                    ];
-
-                    moneyFlowXData.push(point[0]);
-                    moneyFlowYData.push(point[1]);
-                    values.push([point[0], point[1]]);
-                }
+                moneyFlowXData.push(point[0]);
+                moneyFlowYData.push(point[1]);
+                values.push([point[0], point[1]]);
             }
-
-            return {
-                values: values,
-                xData: moneyFlowXData,
-                yData: moneyFlowYData
-            } as IndicatorValuesObject<TLinkedSeries>;
         }
-    });
+
+        return {
+            values: values,
+            xData: moneyFlowXData,
+            yData: moneyFlowYData
+        } as IndicatorValuesObject<TLinkedSeries>;
+    }
+});
+
+declare module '../../Core/Series/SeriesType' {
+    interface SeriesTypeRegistry {
+        cmf: typeof CMFIndicator;
+    }
+}
+
+BaseSeries.registerSeriesType('cmf', CMFIndicator);
+
+/* *
+ *
+ *  Default Export
+ *
+ * */
+
+export default CMFIndicator;
 
 /**
  * A `CMF` series. If the [type](#series.cmf.type) option is not
