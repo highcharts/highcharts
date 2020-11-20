64d9f20e2cd516acd71085d24186e619057e3d81
Karol Kołodziej <karolkolodziej01@gmail.com>
2020-11-20 14:32:59 +0100

    Added requested changes.

--- js/masters/indicators/momentum.src.js
+++ js/masters/indicators/momentum.src.js
@@ -11,4 +11,4 @@
  * License: www.highcharts.com/license
  */
 'use strict';
-import '../../Stock/Indicators//Momentum/MomentumIndicator.js';
+import '../../Stock/Indicators/Momentum/MomentumIndicator.js';
