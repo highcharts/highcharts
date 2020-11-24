6ec818188b3f18c8cb39eb39020f22de61b401a2
Izothep <grzegorzblachlinski@gmail.com>
2020-11-24 12:48:18 +0100

    Created additional examples of area3d series.

---
+++ samples/highcharts/3d/area-stacking/demo.js
@@ -0,0 +1,63 @@
+Highcharts.chart('container', {
+    chart: {
+        type: 'area',
+        options3d: {
+            enabled: true,
+            alpha: 15,
+            beta: 15,
+            depth: 50
+        }
+    },
+    title: {
+        text: 'Historic and Estimated Worldwide Population Growth by Region'
+    },
+    subtitle: {
+        text: 'Source: Wikipedia.org'
+    },
+    xAxis: {
+        categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
+        tickmarkPlacement: 'on',
+        title: {
+            enabled: false
+        }
+    },
+    yAxis: {
+        title: {
+            text: 'Billions'
+        },
+        labels: {
+            formatter: function () {
+                return this.value / 1000;
+            }
+        }
+    },
+    tooltip: {
+        split: true,
+        valueSuffix: ' millions'
+    },
+    plotOptions: {
+        area: {
+            depth: 20,
+            stacking: 'normal',
+            marker: {
+                enabled: false
+            }
+        }
+    },
+    series: [{
+        name: 'Asia',
+        data: [502, 635, 809, 947, 1402, 3634, 5268]
+    }, {
+        name: 'Africa',
+        data: [106, 107, 111, 133, 221, 767, 1766]
+    }, {
+        name: 'Europe',
+        data: [163, 203, 276, 408, 547, 729, 628]
+    }, {
+        name: 'America',
+        data: [18, 31, 54, 156, 339, 818, 1201]
+    }, {
+        name: 'Oceania',
+        data: [2, 2, 2, 6, 13, 30, 46]
+    }]
+});
