6801388891539d9adddd35c4334ba4182ae3d96f
sebastian@blacklabel.pl <sebastian@blacklabel.pl>
2020-11-26 17:01:53 +0100

    LinearRegression - added initial class pattern.

--- js/masters/indicators/indicators-all.src.js
+++ js/masters/indicators/indicators-all.src.js
@@ -49,6 +49,6 @@ import '../../Stock/Indicators/VWAP/VWAPIndicator.js';
 import '../../Stock/Indicators/WilliamsRIndicator.js';
 import '../../Stock/Indicators/WMAIndicator.js';
 import '../../Stock/Indicators/Zigzag/ZigzagIndicator.js';
-import '../../Stock/Indicators/RegressionIndicators.js';
+import '../../Stock/Indicators/Regression/RegressionIndicators.js';
 import '../../Stock/Indicators/ABands/ABandsIndicator.js';
 import '../../Stock/Indicators/TrendLineIndicator.js';
