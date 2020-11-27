b97f5812a148f7470015e1bdcec72e3cec0b3f47
Dominik Chudy <dominikchudy@Dominiks-MacBook.local>
2020-11-27 08:28:40 +0100

    Moved some properties and functions into the class pattern.

--- js/Stock/Indicators/AO/AOIndicator.js
+++ js/Stock/Indicators/AO/AOIndicator.js
@@ -42,8 +42,81 @@ var extend = U.extend, merge = U.merge, correctFloat = U.correctFloat, isArray =
 var AOIndicator = /** @class */ (function (_super) {
     __extends(AOIndicator, _super);
     function AOIndicator() {
-        return _super !== null && _super.apply(this, arguments) || this;
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        /**
+         *
+         * Properties
+         *
+         */
+        _this.data = void 0;
+        _this.options = void 0;
+        _this.points = void 0;
+        return _this;
     }
+    /**
+     *
+     * Functions
+     *
+     */
+    AOIndicator.prototype.drawGraph = function () {
+        var indicator = this, options = indicator.options, points = indicator.points, userColor = indicator.userOptions.color, positiveColor = options.greaterBarColor, negativeColor = options.lowerBarColor, firstPoint = points[0], i;
+        if (!userColor && firstPoint) {
+            firstPoint.color = positiveColor;
+            for (i = 1; i < points.length; i++) {
+                if (points[i].y > points[i - 1].y) {
+                    points[i].color = positiveColor;
+                }
+                else if (points[i].y < points[i - 1].y) {
+                    points[i].color = negativeColor;
+                }
+                else {
+                    points[i].color = points[i - 1].color;
+                }
+            }
+        }
+    };
+    AOIndicator.prototype.getValues = function (series) {
+        var shortPeriod = 5, longPeriod = 34, xVal = series.xData || [], yVal = series.yData || [], yValLen = yVal.length, AO = [], // 0- date, 1- Awesome Oscillator
+        xData = [], yData = [], high = 1, low = 2, shortSum = 0, longSum = 0, shortSMA, // Shorter Period SMA
+        longSMA, // Longer Period SMA
+        awesome, shortLastIndex, longLastIndex, price, i, j;
+        if (xVal.length <= longPeriod ||
+            !isArray(yVal[0]) ||
+            yVal[0].length !== 4) {
+            return;
+        }
+        for (i = 0; i < longPeriod - 1; i++) {
+            price = (yVal[i][high] + yVal[i][low]) / 2;
+            if (i >= longPeriod - shortPeriod) {
+                shortSum = correctFloat(shortSum + price);
+            }
+            longSum = correctFloat(longSum + price);
+        }
+        for (j = longPeriod - 1; j < yValLen; j++) {
+            price = (yVal[j][high] + yVal[j][low]) / 2;
+            shortSum = correctFloat(shortSum + price);
+            longSum = correctFloat(longSum + price);
+            shortSMA = shortSum / shortPeriod;
+            longSMA = longSum / longPeriod;
+            awesome = correctFloat(shortSMA - longSMA);
+            AO.push([xVal[j], awesome]);
+            xData.push(xVal[j]);
+            yData.push(awesome);
+            shortLastIndex = j + 1 - shortPeriod;
+            longLastIndex = j + 1 - longPeriod;
+            shortSum = correctFloat(shortSum -
+                (yVal[shortLastIndex][high] +
+                    yVal[shortLastIndex][low]) / 2);
+            longSum = correctFloat(longSum -
+                (yVal[longLastIndex][high] +
+                    yVal[longLastIndex][low]) / 2);
+        }
+        return {
+            values: AO,
+            xData: xData,
+            yData: yData
+        };
+    };
     /**
      * Awesome Oscillator. This series requires the `linkedTo` option to
      * be set and should be loaded after the `stock/indicators/indicators.js`
@@ -108,66 +181,7 @@ extend(AOIndicator.prototype, {
     getColumnMetrics: H.seriesTypes.column.prototype.getColumnMetrics,
     crispCol: H.seriesTypes.column.prototype.crispCol,
     translate: H.seriesTypes.column.prototype.translate,
-    drawPoints: H.seriesTypes.column.prototype.drawPoints,
-    drawGraph: function () {
-        var indicator = this, options = indicator.options, points = indicator.points, userColor = indicator.userOptions.color, positiveColor = options.greaterBarColor, negativeColor = options.lowerBarColor, firstPoint = points[0], i;
-        if (!userColor && firstPoint) {
-            firstPoint.color = positiveColor;
-            for (i = 1; i < points.length; i++) {
-                if (points[i].y > points[i - 1].y) {
-                    points[i].color = positiveColor;
-                }
-                else if (points[i].y < points[i - 1].y) {
-                    points[i].color = negativeColor;
-                }
-                else {
-                    points[i].color = points[i - 1].color;
-                }
-            }
-        }
-    },
-    getValues: function (series) {
-        var shortPeriod = 5, longPeriod = 34, xVal = series.xData || [], yVal = series.yData || [], yValLen = yVal.length, AO = [], // 0- date, 1- Awesome Oscillator
-        xData = [], yData = [], high = 1, low = 2, shortSum = 0, longSum = 0, shortSMA, // Shorter Period SMA
-        longSMA, // Longer Period SMA
-        awesome, shortLastIndex, longLastIndex, price, i, j;
-        if (xVal.length <= longPeriod ||
-            !isArray(yVal[0]) ||
-            yVal[0].length !== 4) {
-            return;
-        }
-        for (i = 0; i < longPeriod - 1; i++) {
-            price = (yVal[i][high] + yVal[i][low]) / 2;
-            if (i >= longPeriod - shortPeriod) {
-                shortSum = correctFloat(shortSum + price);
-            }
-            longSum = correctFloat(longSum + price);
-        }
-        for (j = longPeriod - 1; j < yValLen; j++) {
-            price = (yVal[j][high] + yVal[j][low]) / 2;
-            shortSum = correctFloat(shortSum + price);
-            longSum = correctFloat(longSum + price);
-            shortSMA = shortSum / shortPeriod;
-            longSMA = longSum / longPeriod;
-            awesome = correctFloat(shortSMA - longSMA);
-            AO.push([xVal[j], awesome]);
-            xData.push(xVal[j]);
-            yData.push(awesome);
-            shortLastIndex = j + 1 - shortPeriod;
-            longLastIndex = j + 1 - longPeriod;
-            shortSum = correctFloat(shortSum -
-                (yVal[shortLastIndex][high] +
-                    yVal[shortLastIndex][low]) / 2);
-            longSum = correctFloat(longSum -
-                (yVal[longLastIndex][high] +
-                    yVal[longLastIndex][low]) / 2);
-        }
-        return {
-            values: AO,
-            xData: xData,
-            yData: yData
-        };
-    }
+    drawPoints: H.seriesTypes.column.prototype.drawPoints
 });
 BaseSeries.registerSeriesType('ao', AOIndicator);
 /* *
