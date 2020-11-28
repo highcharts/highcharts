65caa698245db23cd8153a99d8f07c025406e609
bre1470 <40056287+bre1470@users.noreply.github.com>
2020-11-28 17:12:48 +0100

    Added initial class pattern to cylinder series.

--- ts/Series/CylinderSeries.ts
+++ ts/Series/CylinderSeries.ts
@@ -21,7 +21,6 @@
  * */
 
 import type ColorType from '../Core/Color/ColorType';
-import type ColumnPoint from './Column/ColumnPoint';
 import type ColumnPointOptions from './Column/ColumnPointOptions';
 import type ColumnSeriesOptions from './Column/ColumnSeriesOptions';
 import type Chart from '../Core/Chart/Chart';
@@ -33,10 +32,14 @@ import type SVGElement from '../Core/Renderer/SVG/SVGElement';
 import type { SVGElement3DLikeCuboid } from '../Core/Renderer/SVG/SVGElement3DLike';
 import type SVGPath from '../Core/Renderer/SVG/SVGPath';
 import type SVGPath3D from '../Core/Renderer/SVG/SVGPath3D';
+import BaseSeries from '../Core/Series/Series.js';
+const {
+    seriesTypes: {
+        column: ColumnSeries
+    }
+} = BaseSeries;
 import Color from '../Core/Color/Color.js';
 const { parse: color } = Color;
-import ColumnSeries from './Column/ColumnSeries.js';
-const { prototype: columnProto } = ColumnSeries;
 import H from '../Core/Globals.js';
 const {
     charts,
@@ -48,10 +51,10 @@ const {
 } = H;
 import Math3D from '../Extensions/Math3D.js';
 const { perspective } = Math3D;
-import Series from '../Core/Series/Series.js';
 import _SVGRenderer from '../Core/Renderer/SVG/SVGRenderer.js';
 import U from '../Core/Utilities.js';
 const {
+    extend,
     merge,
     pick
 } = U;
@@ -68,28 +71,12 @@ declare module '../Core/Renderer/SVG/SVGElement3DLike' {
     }
 }
 
-declare module '../Core/Series/SeriesType' {
-    interface SeriesTypeRegistry {
-        cylinder: typeof Highcharts.CylinderSeries;
-    }
-}
-
 /**
  * Internal types
  * @private
  */
 declare global {
     namespace Highcharts {
-        class CylinderPoint extends ColumnPoint {
-            public options: CylinderPointOptions;
-            public series: CylinderSeries;
-        }
-        class CylinderSeries extends ColumnSeries {
-            public data: Array<CylinderPoint>;
-            public options: CylinderSeriesOptions;
-            public pointClass: typeof CylinderPoint;
-            public points: Array<CylinderPoint>;
-        }
         interface CylinderMethodsObject extends SVGElement3DLikeCuboid {
             parts: Array<string>;
             pathType: string;
@@ -136,118 +123,11 @@ const cuboidPath = RendererProto.cuboidPath;
 const isSimplified = (path: SVGPath): boolean =>
     !path.some((seg): boolean => seg[0] === 'C');
 
-/**
-  * The cylinder series type.
-  *
-  * @requires module:highcharts-3d
-  * @requires module:modules/cylinder
-  *
-  * @private
-  * @class
-  * @name Highcharts.seriesTypes.cylinder
-  *
-  * @augments Highcharts.Series
-  */
-Series.seriesType<typeof Highcharts.CylinderSeries>(
-    'cylinder',
-    'column',
-    /**
-     * A cylinder graph is a variation of a 3d column graph. The cylinder graph
-     * features cylindrical points.
-     *
-     * @sample {highcharts} highcharts/demo/cylinder/
-     *         Cylinder graph
-     *
-     * @extends      plotOptions.column
-     * @since        7.0.0
-     * @product      highcharts
-     * @excluding    allAreas, boostThreshold, colorAxis, compare, compareBase,
-     *               dragDrop, boostBlending
-     * @requires     modules/cylinder
-     * @optionparent plotOptions.cylinder
-     */
-    {},
-    {},
-    /** @lends Highcharts.seriesTypes.cylinder#pointClass# */
-    {
-        shapeType: 'cylinder',
-        hasNewShapeType: columnProto.pointClass.prototype.hasNewShapeType
-    }
-);
-
-/**
- * A `cylinder` series. If the [type](#series.cylinder.type) option is not
- * specified, it is inherited from [chart.type](#chart.type).
+/* *
  *
- * @extends   series,plotOptions.cylinder
- * @since     7.0.0
- * @product   highcharts
- * @excluding allAreas, boostThreshold, colorAxis, compare, compareBase,
- *            boostBlending
- * @requires  modules/cylinder
- * @apioption series.cylinder
- */
-
-/**
- * An array of data points for the series. For the `cylinder` series type,
- * points can be given in the following ways:
+ *  Composition
  *
- * 1. An array of numerical values. In this case, the numerical values will be
- *    interpreted as `y` options. The `x` values will be automatically
- *    calculated, either starting at 0 and incremented by 1, or from
- *    `pointStart` and `pointInterval` given in the series options. If the axis
- *    has categories, these will be used. Example:
- *    ```js
- *    data: [0, 5, 3, 5]
- *    ```
- *
- * 2. An array of arrays with 2 values. In this case, the values correspond to
- *    `x,y`. If the first value is a string, it is applied as the name of the
- *    point, and the `x` value is inferred.
- *    ```js
- *    data: [
- *        [0, 0],
- *        [1, 8],
- *        [2, 9]
- *    ]
- *    ```
- *
- * 3. An array of objects with named values. The following snippet shows only a
- *    few settings, see the complete options set below. If the total number of
- *    data points exceeds the series'
- *    [turboThreshold](#series.cylinder.turboThreshold), this option is not
- *    available.
- *
- *    ```js
- *    data: [{
- *        x: 1,
- *        y: 2,
- *        name: "Point2",
- *        color: "#00FF00"
- *    }, {
- *        x: 1,
- *        y: 4,
- *        name: "Point1",
- *        color: "#FF00FF"
- *    }]
- *    ```
- *
- * @sample {highcharts} highcharts/chart/reflow-true/
- *         Numerical values
- * @sample {highcharts} highcharts/series/data-array-of-arrays/
- *         Arrays of numeric x and y
- * @sample {highcharts} highcharts/series/data-array-of-arrays-datetime/
- *         Arrays of datetime x and y
- * @sample {highcharts} highcharts/series/data-array-of-name-value/
- *         Arrays of point.name and y
- * @sample {highcharts} highcharts/series/data-array-of-objects/
- *         Config objects
- *
- * @type      {Array<number|Array<(number|string),(number|null)>|null|*>}
- * @extends   series.column.data
- * @product   highcharts highstock
- * @apioption series.cylinder.data
- */
+ * */
 
 // cylinder extends cuboid
 const cylinderMethods = merge(RendererProto.elements3d.cuboid, {
@@ -532,3 +412,207 @@ RendererProto.getCurvedPath = function (points: Array<PositionObject>): SVGPath 
     }
     return path;
 };
+
+/* *
+ *
+ *  Class
+ *
+ * */
+
+/**
+ * The cylinder series type.
+ *
+ * @requires module:highcharts-3d
+ * @requires module:modules/cylinder
+ *
+ * @private
+ * @class
+ * @name Highcharts.seriesTypes.cylinder
+ *
+ * @augments Highcharts.Series
+ */
+class CylinderSeries extends ColumnSeries {
+
+    /* *
+     *
+     *  Static Properties
+     *
+     * */
+
+    /**
+     * A cylinder graph is a variation of a 3d column graph. The cylinder graph
+     * features cylindrical points.
+     *
+     * @sample {highcharts} highcharts/demo/cylinder/
+     *         Cylinder graph
+     *
+     * @extends      plotOptions.column
+     * @since        7.0.0
+     * @product      highcharts
+     * @excluding    allAreas, boostThreshold, colorAxis, compare, compareBase,
+     *               dragDrop, boostBlending
+     * @requires     modules/cylinder
+     * @optionparent plotOptions.cylinder
+     */
+    public static defaultOptions: Highcharts.CylinderSeriesOptions = merge(ColumnSeries.defaultOptions);
+
+    /* *
+     *
+     *  Properties
+     *
+     * */
+
+    public data: Array<CylinderPoint> = void 0 as any;
+
+    public options: Highcharts.CylinderSeriesOptions = void 0 as any;
+
+    public points: Array<CylinderPoint> = void 0 as any;
+
+}
+
+/* *
+ *
+ *  Prototype Properties
+ *
+ * */
+
+interface CylinderSeries {
+    pointClass: typeof CylinderPoint;
+}
+
+/* *
+ *
+ *  Class
+ *
+ * */
+
+class CylinderPoint extends ColumnSeries.prototype.pointClass {
+
+    /* *
+     *
+     *  Properties
+     *
+     * */
+
+    public options: Highcharts.CylinderPointOptions = void 0 as any;
+
+    public series: CylinderSeries = void 0 as any;
+
+}
+
+/* *
+ *
+ *  Prototype Properties
+ *
+ * */
+
+interface CylinderPoint {
+    shapeType: string;
+}
+extend(CylinderPoint.prototype, {
+    shapeType: 'cylinder'
+});
+
+/* *
+ *
+ *  Registry
+ *
+ * */
+
+declare module '../Core/Series/SeriesType' {
+    interface SeriesTypeRegistry {
+        cylinder: typeof CylinderSeries;
+    }
+}
+CylinderSeries.prototype.pointClass = CylinderPoint;
+BaseSeries.registerSeriesType('cylinder', CylinderSeries);
+
+/* *
+ *
+ *  Default Export
+ *
+ * */
+
+export default CylinderSeries;
+
+/* *
+ *
+ *  API Options
+ *
+ * */
+
+/**
+ * A `cylinder` series. If the [type](#series.cylinder.type) option is not
+ * specified, it is inherited from [chart.type](#chart.type).
+ *
+ * @extends   series,plotOptions.cylinder
+ * @since     7.0.0
+ * @product   highcharts
+ * @excluding allAreas, boostThreshold, colorAxis, compare, compareBase,
+ *            boostBlending
+ * @requires  modules/cylinder
+ * @apioption series.cylinder
+ */
+
+/**
+ * An array of data points for the series. For the `cylinder` series type,
+ * points can be given in the following ways:
+ *
+ * 1. An array of numerical values. In this case, the numerical values will be
+ *    interpreted as `y` options. The `x` values will be automatically
+ *    calculated, either starting at 0 and incremented by 1, or from
+ *    `pointStart` and `pointInterval` given in the series options. If the axis
+ *    has categories, these will be used. Example:
+ *    ```js
+ *    data: [0, 5, 3, 5]
+ *    ```
+ *
+ * 2. An array of arrays with 2 values. In this case, the values correspond to
+ *    `x,y`. If the first value is a string, it is applied as the name of the
+ *    point, and the `x` value is inferred.
+ *    ```js
+ *    data: [
+ *        [0, 0],
+ *        [1, 8],
+ *        [2, 9]
+ *    ]
+ *    ```
+ *
+ * 3. An array of objects with named values. The following snippet shows only a
+ *    few settings, see the complete options set below. If the total number of
+ *    data points exceeds the series'
+ *    [turboThreshold](#series.cylinder.turboThreshold), this option is not
+ *    available.
+ *
+ *    ```js
+ *    data: [{
+ *        x: 1,
+ *        y: 2,
+ *        name: "Point2",
+ *        color: "#00FF00"
+ *    }, {
+ *        x: 1,
+ *        y: 4,
+ *        name: "Point1",
+ *        color: "#FF00FF"
+ *    }]
+ *    ```
+ *
+ * @sample {highcharts} highcharts/chart/reflow-true/
+ *         Numerical values
+ * @sample {highcharts} highcharts/series/data-array-of-arrays/
+ *         Arrays of numeric x and y
+ * @sample {highcharts} highcharts/series/data-array-of-arrays-datetime/
+ *         Arrays of datetime x and y
+ * @sample {highcharts} highcharts/series/data-array-of-name-value/
+ *         Arrays of point.name and y
+ * @sample {highcharts} highcharts/series/data-array-of-objects/
+ *         Config objects
+ *
+ * @type      {Array<number|Array<(number|string),(number|null)>|null|*>}
+ * @extends   series.column.data
+ * @product   highcharts highstock
+ * @apioption series.cylinder.data
+ */
+
+''; // keeps doclets above in the transpiled file
