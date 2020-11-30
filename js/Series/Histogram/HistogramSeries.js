eaa36df1d0692934c130a0049fba26bcb81d6292
bre1470 <40056287+bre1470@users.noreply.github.com>
2020-11-30 15:11:59 +0100

    Moved histogram series into a subfolder.

--- js/Series/Histogram/HistogramSeries.js
+++ js/Series/Histogram/HistogramSeries.js
@@ -22,10 +22,10 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-import BaseSeries from '../Core/Series/Series.js';
+import BaseSeries from '../../Core/Series/Series.js';
 var ColumnSeries = BaseSeries.seriesTypes.column;
-import DerivedSeriesMixin from '../Mixins/DerivedSeries.js';
-import U from '../Core/Utilities.js';
+import DerivedSeriesMixin from '../../Mixins/DerivedSeries.js';
+import U from '../../Core/Utilities.js';
 var arrayMax = U.arrayMax, arrayMin = U.arrayMin, correctFloat = U.correctFloat, extend = U.extend, isNumber = U.isNumber, merge = U.merge, objectEach = U.objectEach;
 /* ************************************************************************** *
  *  HISTOGRAM
