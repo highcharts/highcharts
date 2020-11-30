2ed1c0f40c19afdc5cdd54fca89a5b954903bdf9
sebastian@blacklabel.pl <sebastian@blacklabel.pl>
2020-11-30 14:21:42 +0100

    Splitting Linear Regression into separe regression files.

--- js/masters/indicators/regressions.src.js
+++ js/masters/indicators/regressions.src.js
@@ -11,4 +11,4 @@
  * License: www.highcharts.com/license
  */
 'use strict';
-import '../../Stock/Indicators/Regression/RegressionIndicators.js';
+import '../../Stock/Indicators/LinearRegression/LinearRegression.js';
