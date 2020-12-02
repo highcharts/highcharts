0c8bc35da39484b5aa662fcaee63c8e76bffc9c1
Marita <marita.vindedal@highsoft.com>
2020-12-02 09:32:05 +0100

    Moved candlestick series to subfolder.

--- js/masters/modules/stock.src.js
+++ js/masters/modules/stock.src.js
@@ -14,7 +14,7 @@ import '../../Core/Axis/OrdinalAxis.js';
 import './broken-axis.src.js';
 import '../../Extensions/DataGrouping.js';
 import '../../Series/OHLC/OHLCSeries.js';
-import '../../Series/CandlestickSeries.js';
+import '../../Series/Candlestick/CandlestickSeries.js';
 import '../../Series/Flags/FlagsSeries.js';
 import '../../Core/Scrollbar.js';
 import '../../Core/Navigator.js';
