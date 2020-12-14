84b62dbdec58123f9ae9ea3bff762b368f080540
sebastian@blacklabel.pl <sebastian@blacklabel.pl>
2020-12-14 11:32:09 +0100

    Fixed missing properties in interfaces.

--- ts/Stock/Indicators/ABands/ABandsIndicator.ts
+++ ts/Stock/Indicators/ABands/ABandsIndicator.ts
@@ -253,7 +253,14 @@ class ABandsIndicator extends SMAIndicator {
 interface ABandsIndicator {
     getTranslatedLinesNames: typeof MultipleLinesMixin['getTranslatedLinesNames'];
     linesApiNames: typeof MultipleLinesMixin['linesApiNames'];
+    nameBase: string;
+    nameComponents: Array<string>;
+    pointArrayMap: Array<string>;
+    pointValKey: string;
     pointClass: typeof ABandsPoint;
+    toYData: typeof MultipleLinesMixin.toYData;
+    translate: typeof MultipleLinesMixin.translate;
+    drawGraph: typeof MultipleLinesMixin.drawGraph;
 }
 
 extend(ABandsIndicator.prototype, {
--- ts/Stock/Indicators/VBP/VBPIndicator.ts
+++ ts/Stock/Indicators/VBP/VBPIndicator.ts
@@ -775,6 +775,7 @@ namespace VBPIndicator {
 
 interface VBPIndicator {
     nameBase: string;
+    calculateOn: string;
     pointClass: typeof VBPPoint;
 
     crispCol: ColumnSeries['crispCol'];
