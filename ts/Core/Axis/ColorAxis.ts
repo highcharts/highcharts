b2003758ed3fc33d8a1a00bbcd3189a01dacabd6
Dominik Chudy <spidahas@gmail.com>
2020-12-21 14:49:41 +0100

    Replaced destroy call with super method.

--- ts/Core/Axis/ColorAxis.ts
+++ ts/Core/Axis/ColorAxis.ts
@@ -41,6 +41,7 @@ import LineSeries from '../../Series/Line/LineSeries.js';
 import palette from '../../Core/Color/Palette.js';
 import Point from '../Series/Point.js';
 import U from '../Utilities.js';
+import { param } from 'jquery';
 const {
     addEvent,
     erase,
@@ -1293,12 +1294,12 @@ class ColorAxis extends Axis implements AxisLike {
         chart.isDirtyLegend = true;
     }
 
-    // Removing the whole axis (#14283)
+    //   Removing the whole axis (#14283)
     public destroy(): void {
         this.chart.isDirtyLegend = true;
 
         this.destroyItems();
-        H.Axis.prototype.destroy.call(this);
+        super.destroy(...[].slice.call(arguments));
     }
 
     /**
