102460a15fa99764af8580f6879e01ef9cd5d05e
Ken-Håvard Lieng <kenh.lieng@highsoft.com>
2020-12-22 10:28:56 +0100

    Fixed #8635, zooming variwide chart did not work

--- ts/Series/Variwide/VariwideComposition.ts
+++ ts/Series/Variwide/VariwideComposition.ts
@@ -160,7 +160,7 @@ wrap(Tick.prototype, 'getLabelPosition', function (
 
     // Post-translate
     if (this.axis.variwide && this.axis.categories) {
-        this.postTranslate(xy, xOrY, index);
+        this.postTranslate(xy, xOrY, this.pos);
     }
     return xy;
 });
