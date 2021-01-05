eaf2cc827bb9442f4e2abd5829d1f10743b229b0
Dominik Chudy <spidahas@gmail.com>
2021-01-05 09:55:13 +0100

    Deleted unnecessary import.

--- ts/Core/Axis/ColorAxis.ts
+++ ts/Core/Axis/ColorAxis.ts
@@ -40,7 +40,6 @@ import LineSeries from '../../Series/Line/LineSeries.js';
 import palette from '../../Core/Color/Palette.js';
 import Point from '../Series/Point.js';
 import U from '../Utilities.js';
-import { param } from 'jquery';
 const {
     addEvent,
     erase,
