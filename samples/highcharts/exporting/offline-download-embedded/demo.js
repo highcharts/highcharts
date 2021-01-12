58a35a3b5ddbdcd62210c87892c347d1c031bd65
Øystein Moseng <oystein@highsoft.com>
2021-01-12 18:14:53 +0100

    Removed unused code from offline-exporting samples.

--- samples/highcharts/exporting/offline-download-embedded/demo.js
+++ samples/highcharts/exporting/offline-download-embedded/demo.js
@@ -1,12 +1,8 @@
-/* Automate testing of module somewhat */
-
 var nav = Highcharts.win.navigator,
     isMSBrowser = /Edge\/|Trident\/|MSIE /.test(nav.userAgent),
     isEdgeBrowser = /Edge\/\d+/.test(nav.userAgent),
     containerEl = document.getElementById('container'),
-    parentEl = containerEl.parentNode,
-    oldDownloadURL = Highcharts.downloadURL,
-    nextPlease = true;
+    parentEl = containerEl.parentNode;
 
 function addText(text) {
     var heading = document.createElement('h2');
@@ -14,24 +10,7 @@ function addText(text) {
     parentEl.appendChild(heading);
 }
 
-function addURLView(title, url) {
-    var iframe = document.createElement('iframe');
-    if (isMSBrowser && Highcharts.isObject(url)) {
-        addText(title +
-        ': Microsoft browsers do not support Blob iframe.src, test manually'
-        );
-        return;
-    }
-    iframe.src = url;
-    iframe.width = 400;
-    iframe.height = 300;
-    iframe.title = title;
-    iframe.style.display = 'inline-block';
-    parentEl.appendChild(iframe);
-}
-
 function fallbackHandler(options) {
-    nextPlease = true;
     if (isMSBrowser &&
         !(options.type === 'image/svg+xml' && isEdgeBrowser) ||
         options.type === 'application/pdf') {
@@ -42,76 +21,7 @@ function fallbackHandler(options) {
     }
 }
 
-Highcharts.downloadURL = function (dataURL, filename) {
-    // Emulate toBlob behavior for long URLs
-    if (dataURL.length > 2000000) {
-        dataURL = Highcharts.dataURLtoBlob(dataURL);
-        if (!dataURL) {
-            throw 'Data URL length limit reached';
-        }
-    }
-    // Show result in an iframe instead of downloading
-    addURLView(filename, dataURL);
-    nextPlease = true;
-};
-
-Highcharts.Chart.prototype.exportTest = function (type) {
-    this.exportChartLocal({
-        type: type
-    }, {
-        title: {
-            text: type
-        },
-        subtitle: {
-            text: false
-        }
-    });
-};
-
-/* Does not work after downloadURL was removed from namespace
-   https://github.com/highcharts/highcharts/commit/3912f1ecbb79d098defa93e729d968bebd4222f8
-Highcharts.Chart.prototype.callbacks.push(function (chart) {
-    if (!chart.options.chart.forExport) {
-        var menu = chart.exportSVGElements && chart.exportSVGElements[0],
-            oldHandler,
-            types = ['image/png', 'image/jpeg', 'image/svg+xml',
-                'application/pdf'],
-            intervalRef;
-
-        // Try to avoid race conditions by queuing requests.
-        // In getSVGForLocalExport we wrap getChartHTML, and we risk getting
-        // the svg of the wrong chart copy. No quick fix for this. Would
-        // have to be able to link each call to export to a copy container.
-        intervalRef = setInterval(function () {
-            if (nextPlease) {
-                var type = types.pop();
-                if (type) {
-                    nextPlease = false;
-                    chart.exportTest(type);
-                } else {
-                    clearInterval(intervalRef);
-                }
-            }
-        }, 100);
-
-        // Allow manual testing by resetting downloadURL handler when trying
-        // to export manually
-        if (menu) {
-            oldHandler = menu.element.onclick;
-            menu.element.onclick = function () {
-                Highcharts.downloadURL = oldDownloadURL;
-                oldHandler.call(this);
-            };
-        }
-    }
-});
-*/
-
-/* End of automation code */
-
-
 Highcharts.chart('container', {
-
     exporting: {
         chartOptions: { // specific options for the exported image
             plotOptions: {
@@ -159,5 +69,4 @@ Highcharts.chart('container', {
             }
         }, 148.5, 216.4, 194.1, 95.6, 54.4]
     }]
-
 });
