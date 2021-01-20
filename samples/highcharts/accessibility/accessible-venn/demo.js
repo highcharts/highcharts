7ce675e8ba13aa7d856b350cd37b6d9d1ff9c3b2
Øystein Moseng <oystein@highsoft.com>
2021-01-20 11:17:49 +0100

    Accessible-venn demo tweaks.

--- samples/highcharts/accessibility/accessible-venn/demo.js
+++ samples/highcharts/accessibility/accessible-venn/demo.js
@@ -1,24 +1,53 @@
+var getByClass = function (className) {
+    return Array.prototype.slice.call(
+        document.getElementsByClassName(className)
+    );
+};
+var getEl = function (id) {
+    return document.getElementById(id);
+};
+var nextMouseOver = null;
+
+function removeHover() {
+    getByClass('dataLabel').forEach(function (e) {
+        e.classList.remove('over');
+    });
+    getEl('aicon').classList.remove('over');
+}
+
+getEl('container').addEventListener('mouseleave', function () {
+    removeHover();
+});
+
+document.addEventListener('mouseup', function (e) {
+    if (!getEl('container').contains(e.target)) {
+        removeHover();
+    }
+});
+
 Highcharts.chart('container', {
+    lang: {
+        accessibility: {
+            chartContainerLabel: 'Venn diagram of accessibility selling points'
+        }
+    },
+
     accessibility: {
-        enabled: true,
         point: {
             descriptionFormatter: function (point) {
                 return point.accessibility.description;
             }
         },
-        description: 'A venn chart showing the benefits of designing for accessibility'
+        screenReaderSection: {
+            beforeChartFormat: '<p>A venn diagram showing 4 major benefits of designing for accessibility, ' +
+                'and how they combine together to form the selling point of accessibility.</p>'
+        },
+        landmarkVerbosity: 'one'
     },
 
     chart: {
         margin: 0,
         spacing: 0
-    },
-
-    plotOptions: {
-        venn: {
-            borderWidth: 0,
-            borderColor: '#000'
-        }
     },
 
     series: [{
@@ -37,12 +66,31 @@ Highcharts.chart('container', {
                 borderWidth: 0
             }
         },
-
         dataLabels: {
+            enabled: true,
+            useHTML: true,
             style: {
-                enabled: false,
                 textOutline: '0px contrast',
                 fontSize: '14px'
+            }
+        },
+        point: {
+            events: {
+                mouseOver: function () {
+                    var point = this;
+                    var pointName = point.name;
+                    var category = pointName.split(',')[0];
+
+                    clearTimeout(nextMouseOver);
+                    nextMouseOver = setTimeout(function () {
+                        getByClass('dataLabel').forEach(function (e) {
+                            e.classList.remove('over');
+                        });
+
+                        getEl(category).classList.add('over');
+                        getEl('aicon').classList.add('over');
+                    }, 100);
+                }
             }
         },
         name: 'Business Case for Accessibility',
@@ -50,23 +98,18 @@ Highcharts.chart('container', {
             sets: ['2'],
             value: 5,
             accessibility: {
-                enabled: true,
                 description: 'Drive Innovation - Accessibility features in products and services often solve unanticipated problems for all users.'
             },
             name: 'innovation',
             color: Highcharts.getOptions().colors[0],
             dataLabels: {
-                enabled: true,
-                useHTML: true,
-                formatter: function () {
-                    return '<div id="innovation"  class="dataLabel"><h4 class="open">Drive Innovation</h4><p class="info">Accessibility features in products and services often solve unanticipated problems for all users. <a href="#">Learn more</a></p><i id="innovation-icon" class="fas fa-cogs"></i><h4 class="closed">Drive Innovation</h4></div>';
-                }
+                format: '<div id="innovation"  class="dataLabel"><h4 class="open">Drive Innovation</h4><p class="info">Accessibility features in products and services often solve unanticipated problems for all users.</p><i id="innovation-icon" class="fas fa-cogs"></i><h4 class="closed">Drive Innovation</h4></div>'
             }
         }, {
             sets: ['3'],
             value: 5,
             accessibility: {
-                description: 'Enhance your brand - Accessible content will not only will enhance customer loyalty and brand awareness, but also improve organic search results.'
+                description: 'Enhance your brand - Accessible content will not only enhance customer loyalty and brand awareness, but also improve organic search results.'
             },
             name: 'brand',
             color: Highcharts.getOptions().colors[2],
@@ -74,10 +117,7 @@ Highcharts.chart('container', {
             dataLabels: {
                 y: 110,
                 x: -30,
-                useHTML: true,
-                formatter: function () {
-                    return '<div id="brand" class="dataLabel"><h4>Enhance your brand</h4><i id="brand-icon" class="far fa-smile"></i><p  class="info">Accessible content will not only will enhance customer loyalty and brand awareness, but also improve organic search results. <a href="#">Learn more</a></p></div>';
-                }
+                format: '<div id="brand" class="dataLabel"><h4>Enhance your brand</h4><i id="brand-icon" class="far fa-smile"></i><p  class="info">Accessible content will not only enhance customer loyalty and brand awareness, but also improve organic search results.</p></div>'
             }
         }, {
             sets: ['4'],
@@ -91,10 +131,7 @@ Highcharts.chart('container', {
                 y: 0,
                 x: 0,
                 zIndex: 10,
-                useHTML: true,
-                formatter: function () {
-                    return '<div id="reach" class="dataLabel"><i id="reach-icon" class="fas fa-funnel-dollar"></i><h4 >Extend Market Reach</h4><p  class="info">Reach the <b>1.3 billion people</b> world-wide who are affected by a visual impairment with accessible content. <a href="#">Learn more</a> </p></div>';
-                }
+                format: '<div id="reach" class="dataLabel"><i id="reach-icon" class="fas fa-funnel-dollar"></i><h4 >Extend Market Reach</h4><p  class="info">Reach the <b>1.3 billion people</b> world-wide who are affected by a visual impairment with accessible content.</p></div>'
             }
         }, {
             sets: ['5'],
@@ -107,17 +144,14 @@ Highcharts.chart('container', {
             dataLabels: {
                 y: 5,
                 x: -10,
-                useHTML: true,
-                formatter: function () {
-                    return '<div id="legal" class="dataLabel"><i id="legal-icon" class="fas fa-gavel"></i><h4 >Minimize Legal Risk</h4><p class="info">You or your customer might be facing a lawsuit if your software products are not accessible. <a href="#">Learn more</a></p> </div>';
-                }
+                format: '<div id="legal" class="dataLabel"><i id="legal-icon" class="fas fa-gavel"></i><h4 >Minimize Legal Risk</h4><p class="info">You or your customer might be facing a lawsuit if your software products are not accessible.</p> </div>'
             }
         }, {
             sets: ['4', '5'],
             value: 3,
-            name: 'reach,legal',
+            name: 'legal,reach',
             accessibility: {
-                description: 'The Business Case for Accessibility'
+                enabled: false
             },
             dataLabels: {
                 enabled: false
@@ -125,12 +159,12 @@ Highcharts.chart('container', {
             color: Highcharts.getOptions().colors[5],
             borderColor: '#333'
         }, {
-            sets: ['2', '3'],
+            sets: ['3', '2'],
             value: 3,
             accessibility: {
-                description: 'The Business Case for Accessibility'
+                enabled: false
             },
-            name: 'innovation,brand',
+            name: 'brand,innovation',
             color: Highcharts.getOptions().colors[2],
             borderColor: '#333',
             dataLabels: {
@@ -140,7 +174,7 @@ Highcharts.chart('container', {
             sets: ['3', '4'],
             value: 3,
             accessibility: {
-                description: 'The Business Case for Accessibility'
+                enabled: false
             },
             name: 'brand,reach',
             color: Highcharts.getOptions().colors[2],
@@ -151,9 +185,9 @@ Highcharts.chart('container', {
             sets: ['2', '4', '5'],
             value: 2,
             accessibility: {
-                description: 'The Business Case for Accessibility'
+                enabled: false
             },
-            name: 'innovation,reach,legal',
+            name: 'legal,innovation,reach',
             color: Highcharts.getOptions().colors[5],
             dataLabels: {
                 enabled: false
@@ -162,7 +196,7 @@ Highcharts.chart('container', {
             sets: ['5', '2'],
             value: 3,
             accessibility: {
-                description: 'The Business Case for Accessibility'
+                enabled: false
             },
             name: 'legal,innovation',
             color: Highcharts.getOptions().colors[5],
@@ -173,7 +207,7 @@ Highcharts.chart('container', {
             sets: ['2', '4'],
             value: 1,
             accessibility: {
-                description: 'The Business Case for Accessibility'
+                enabled: false
             },
             name: 'innovation,reach',
             color: '#fff',
@@ -184,7 +218,7 @@ Highcharts.chart('container', {
             sets: ['3', '5'],
             value: 3,
             accessibility: {
-                description: 'The Business Case for Accessibility'
+                enabled: false
             },
             name: 'brand,legal',
             color: '#fff',
@@ -195,7 +229,7 @@ Highcharts.chart('container', {
             sets: ['3', '4', '5'],
             value: 2,
             accessibility: {
-                description: 'The Business Case for Accessibility'
+                enabled: false
             },
             name: 'brand,reach,legal',
             color: '#fff',
@@ -207,9 +241,9 @@ Highcharts.chart('container', {
             sets: ['2', '3', '5'],
             value: 1,
             accessibility: {
-                description: 'The Business Case for Accessibility'
+                enabled: false
             },
-            name: 'innovation,brand,legal',
+            name: 'brand,innovation,legal',
             color: '#fff',
             opacity: 1,
             dataLabels: {
@@ -219,9 +253,9 @@ Highcharts.chart('container', {
             sets: ['2', '3', '4'],
             value: 2,
             accessibility: {
-                description: 'The Business Case for Accessibility'
+                enabled: false
             },
-            name: 'innovation,brand,reach',
+            name: 'brand,innovation,reach',
             color: Highcharts.getOptions().colors[2],
             dataLabels: {
                 enabled: false
@@ -230,30 +264,21 @@ Highcharts.chart('container', {
             sets: ['2', '3', '4', '5'],
             value: 3,
             accessibility: {
-                description: 'The Business Case for Accessibility'
+                enabled: false
             },
             color: '#fff',
             opacity: 1,
-            name: 'innovation,brandreachlegal',
+            name: 'innovation,brand,reach,legal',
             dataLabels: {
-                enabled: true,
-                useHTML: true,
                 y: -30,
                 x: -30,
-                formatter: function () {
-                    return '<i id="aicon" class="fas fa-universal-access"></i>';
-                }
+                format: '<i id="aicon" class="fas fa-universal-access"></i>'
             }
         }]
     }],
 
     title: {
-        text: '',
-        style: {
-            fontFamily: 'Roboto',
-            fontSize: '20px',
-            fontWeight: 'bold'
-        }
+        text: ''
     },
 
     tooltip: {
@@ -263,21 +288,4 @@ Highcharts.chart('container', {
     exporting: {
         enabled: false
     }
-
-});
-
-$('i').mouseover(function (e) {
-    e.preventDefault();
-    $('.dataLabel').removeClass('over');
-    const idArray = this.id.split('-');
-    const id = idArray[0];
-
-    $('#' + id).addClass('over');
-    $('#' + id).css({ zIndex: 1000 });
-    $('#aicon').addClass('over');
-
-    setTimeout(function () {
-        $('.dataLabel').removeClass('over');
-        $('#aicon').removeClass('over');
-    }, 5000);
 });
