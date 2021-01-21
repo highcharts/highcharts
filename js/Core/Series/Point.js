cb513fc36466becbb03f22edf764b9ac1e1f82ee
kemusag <77784146+kemusag@users.noreply.github.com>
2021-01-21 13:19:56 +0100

    Also update Point.js
    Missed the JS file

--- js/Core/Series/Point.js
+++ js/Core/Series/Point.js
@@ -1136,7 +1136,7 @@ var Point = /** @class */ (function () {
             markerAttribs = series.markerAttribs(point, state);
         }
         // Apply hover styles to the existing point
-        if (point.graphic) {
+        if (point.graphic && !point.hasDummyGraphic) {
             if (previousState) {
                 point.graphic.removeClass('highcharts-point-' + previousState);
             }
