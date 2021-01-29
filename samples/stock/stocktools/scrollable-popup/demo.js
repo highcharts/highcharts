3af967c7abdd504182af86f31028193e5550efe0
Karol Kołodziej <karolkolodziej01@gmail.com>
2021-01-29 14:44:19 +0100

    Added requested changes inside the test.

--- samples/stock/stocktools/scrollable-popup/demo.js
+++ samples/stock/stocktools/scrollable-popup/demo.js
@@ -1,31 +1,15 @@
-Highcharts.getJSON('https://www.highcharts.com/samples/data/aapl-ohlcv.json', function (data) {
-    var ohlc = [],
-        dataLength = data.length,
-        i = 0;
-    for (i; i < dataLength; i += 1) {
-        ohlc.push([
-            data[i][0], // the date
-            data[i][1], // open
-            data[i][2], // high
-            data[i][3], // low
-            data[i][4] // close
-        ]);
-    }
-    Highcharts.stockChart('container', {
-        mapNavigation: {
-            enabled: true
-        },
-        stockTools: {
-            gui: {
-                enabled: true,
-                buttons: ['indicators']
-            }
-        },
-        series: [{
-            type: 'ohlc',
-            id: 'aapl-ohlc',
-            name: 'AAPL Stock Price',
-            data: ohlc
-        }]
-    });
+Highcharts.stockChart('container', {
+    mapNavigation: {
+        enabled: true
+    },
+    stockTools: {
+        gui: {
+            enabled: true,
+            buttons: ['indicators']
+        }
+    },
+    series: [{
+        type: 'line',
+        data: Array.from(Array(50)).map(() => Math.random() * 10)
+    }]
 });
