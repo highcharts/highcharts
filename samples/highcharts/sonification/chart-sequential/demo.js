d32d780291b7b64185a004186ebca54f03497481
Marita <marita.vindedal@highsoft.com>
2021-02-26 11:10:46 +0100

    Added button features to demos.

--- samples/highcharts/sonification/chart-sequential/demo.js
+++ samples/highcharts/sonification/chart-sequential/demo.js
@@ -1,3 +1,11 @@
+Highcharts.setOptions({
+    lang: {
+        accessibility: {
+            chartContainerLabel: 'Click play button to sonify chart.'
+        }
+    }
+});
+
 // Set up a simple chart
 var chart = Highcharts.chart('container', {
     title: {
@@ -5,6 +13,21 @@ var chart = Highcharts.chart('container', {
     },
     legend: {
         enabled: false
+    },
+    sonification: {
+        events: {
+            onPointStart: function (e, point) {
+                point.onMouseOver();
+            },
+            onEnd: function () {
+                document.getElementById('sonify').style.visibility = 'visible';
+                document.getElementById('overlay').style.visibility = 'visible';
+                document.getElementById('stop').style.visibility = 'hidden';
+            }
+        }
+    },
+    accessibility: {
+        landmarkVerbosity: 'one'
     },
     series: [{
         data: [1, 2, 4, 5, 7, 9, 11, 13]
@@ -15,6 +38,9 @@ var chart = Highcharts.chart('container', {
 
 // Click button to call chart.sonify()
 document.getElementById('sonify').onclick = function () {
+    document.getElementById('sonify').style.visibility = 'hidden';
+    document.getElementById('overlay').style.visibility = 'hidden';
+    document.getElementById('stop').style.visibility = 'visible';
     chart.sonify({
         duration: 3000,
         order: 'sequential',
@@ -34,4 +60,12 @@ document.getElementById('sonify').onclick = function () {
             }
         }]
     });
+    document.getElementById('stop').focus();
+};
+
+document.getElementById('stop').onclick = function () {
+    chart.cancelSonify();
+    document.getElementById('sonify').style.visibility = 'visible';
+    document.getElementById('overlay').style.visibility = 'visible';
+    document.getElementById('stop').style.visibility = 'hidden';
 };
