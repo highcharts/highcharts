619953ab4dd325c8bf2f42af9fce2113798597a7
Torstein Hønsi <torstein@highsoft.com>
2021-03-01 14:32:09 +0100

    Typesafing

--- ts/Extensions/DraggablePoints.ts
+++ ts/Extensions/DraggablePoints.ts
@@ -405,14 +405,21 @@ const columnDragDropProps = seriesTypes.column.prototype.dragDropProps = {
         },
         // Position handle at bottom if column is below threshold
         handlePositioner: function (point: ColumnPoint): PositionObject {
-            var bBox: BBoxObject = point.shapeArgs || (point.graphic as any).getBBox(),
+            var bBox = (
+                    point.shapeArgs ||
+                    (point.graphic && point.graphic.getBBox()) ||
+                    {}
+                ),
+                reversed = point.series.yAxis.reversed,
+                threshold = point.series.options.threshold || 0,
+                y = point.y || 0,
                 bottom =
-                    (!point.series.yAxis.reversed && (point.y as any) >= (point.series.options.threshold || 0)) ||
-                    (point.series.yAxis.reversed && (point.y as any) < (point.series.options.threshold || 0));
+                    (!reversed && y >= threshold) ||
+                    (reversed && y < threshold);
 
             return {
-                x: bBox.x,
-                y: bottom ? bBox.y : bBox.y + bBox.height
+                x: bBox.x || 0,
+                y: bottom ? (bBox.y || 0) : (bBox.y || 0) + (bBox.height || 0)
             };
         },
         // Horizontal handle
