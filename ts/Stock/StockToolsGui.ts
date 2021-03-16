7473dc060dd07ee8dfa2d6e0d981a18049791029
Ken-Håvard Lieng <kenh.lieng@highsoft.com>
2021-03-16 09:57:38 +0100

    Fixed #15324, bad alignment with stocktools

--- ts/Stock/StockToolsGui.ts
+++ ts/Stock/StockToolsGui.ts
@@ -953,6 +953,7 @@ addEvent(Chart, 'getMargins', function (): void {
 ['beforeRender', 'beforeRedraw'].forEach((event: string): void => {
     addEvent(Chart, event, function (): void {
         if (this.stockTools) {
+            const optionsChart = this.options.chart as Highcharts.ChartOptions;
             const listWrapper = this.stockTools.listWrapper,
                 offsetWidth = listWrapper && (
                     (
@@ -965,7 +966,14 @@ addEvent(Chart, 'getMargins', function (): void {
             let dirty = false;
 
             if (offsetWidth && offsetWidth < this.plotWidth) {
-                this.spacingBox.x += offsetWidth;
+                const nextX = pick(
+                    optionsChart.spacingLeft,
+                    optionsChart.spacing && optionsChart.spacing[3],
+                    0
+                ) + offsetWidth;
+                const diff = nextX - this.spacingBox.x;
+                this.spacingBox.x = nextX;
+                this.spacingBox.width -= diff;
                 dirty = true;
             } else if (offsetWidth === 0) {
                 dirty = true;
