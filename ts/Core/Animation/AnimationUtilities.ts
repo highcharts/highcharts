44324796949ac1cd6048ab8a506187d0b164f579
Marita <marita.vindedal@highsoft.com>
2021-04-14 11:14:33 +0200

    Migration: Changed variable and function declarations.

--- ts/Core/Animation/AnimationUtilities.ts
+++ ts/Core/Animation/AnimationUtilities.ts
@@ -46,7 +46,7 @@ const {
  * This function always relates to a chart, and sets a property on the renderer,
  * so it should be moved to the SVGRenderer.
  */
-const setAnimation = function setAnimation(
+function setAnimation(
     animation: (boolean|Partial<AnimationOptions>|undefined),
     chart: Chart
 ): void {
@@ -55,7 +55,7 @@ const setAnimation = function setAnimation(
         chart.options.chart.animation,
         true
     );
-};
+}
 
 /**
  * Get the animation in object form, where a disabled animation is always
@@ -70,7 +70,7 @@ const setAnimation = function setAnimation(
  * @return {Highcharts.AnimationOptionsObject}
  *         An object with at least a duration property.
  */
-const animObject = function animObject(
+function animObject(
     animation?: (boolean|DeepPartial<AnimationOptions>)
 ): AnimationOptions {
     return isObject(animation) ?
@@ -79,7 +79,7 @@ const animObject = function animObject(
             animation as AnimationOptions
         ) as any :
         { duration: animation as boolean ? 500 : 0, defer: 0 };
-};
+}
 
 /**
  * Get the defer as a number value from series animation options.
@@ -99,7 +99,7 @@ const animObject = function animObject(
  * @return {number}
  *        The numeric value.
  */
-const getDeferredAnimation = function (
+function getDeferredAnimation(
     chart: Chart,
     animation: (false|Partial<AnimationOptions>),
     series?: Series
@@ -132,7 +132,7 @@ const getDeferredAnimation = function (
     };
 
     return anim;
-};
+}
 
 /**
  * The global animate method, which uses Fx to create individual animators.
@@ -152,12 +152,12 @@ const getDeferredAnimation = function (
  *
  * @return {void}
  */
-const animate = function (
+function animate(
     el: (HTMLDOMElement|SVGElement),
     params: (CSSObject|SVGAttributes),
     opt?: boolean|Partial<AnimationOptions>
 ): void {
-    var start,
+    let start,
         unit = '',
         end,
         fx,
@@ -212,7 +212,7 @@ const animate = function (
         }
         fx.run(start as any, end as any, unit);
     });
-};
+}
 
 /**
  * Stop running animation.
@@ -235,9 +235,9 @@ const animate = function (
  * improvement in all cases where we stop the animation from .attr. Instead of
  * stopping everything, we can just stop the actual attributes we're setting.
  */
-const stop = function (el: SVGElement, prop?: string): void {
+function stop(el: SVGElement, prop?: string): void {
 
-    var i = Fx.timers.length;
+    let i = Fx.timers.length;
 
     // Remove timers related to this element (#4519)
     while (i--) {
@@ -245,7 +245,7 @@ const stop = function (el: SVGElement, prop?: string): void {
             Fx.timers[i].stopped = true; // #4667
         }
     }
-};
+}
 
 const animationExports = {
     animate,
