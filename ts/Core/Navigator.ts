2dfaeef5c7367be55129e62305a2e541350a0e97
Pawel Lysy <pawel.lysy@blacklabel.pl>
2021-04-26 12:06:44 +0200

    Fix typo in comment.

--- ts/Core/Navigator.ts
+++ ts/Core/Navigator.ts
@@ -2246,7 +2246,7 @@ class Navigator {
                 baseNavigatorOptions = baseOptions.navigatorOptions || {};
 
                 // The dataLabels options are not merged correctly
-                // if the settings are and array, #13847.
+                // if the settings are an array, #13847.
                 userNavOptions.dataLabels = splat(userNavOptions.dataLabels);
                 mergedNavSeriesOptions = merge(
                     baseOptions,
