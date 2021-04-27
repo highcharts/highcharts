dbc839bd89a88dc3ceb10e084390a6018b304755
Ken-Håvard Lieng <kenh.lieng@highsoft.com>
2021-04-27 10:57:40 +0200

    Fixed #15523, negativeColor did not work for lollipop series

--- samples/unit-tests/series-lollipop/lollipop/demo.js
+++ samples/unit-tests/series-lollipop/lollipop/demo.js
@@ -5,10 +5,16 @@ QUnit.test('Lollipop offset affection.', function (assert) {
         },
         series: [
             {
+                color: '#0000ff',
+                negativeColor: '#ff0000',
                 data: [
                     {
                         low: 2
-                    }
+                    },
+                    3,
+                    -4,
+                    2,
+                    -5
                 ]
             },
             {
@@ -23,5 +29,17 @@ QUnit.test('Lollipop offset affection.', function (assert) {
         chart.series[1].data[0].shapeArgs.x,
         2,
         'Lollipop and Errorbar connectors should be in the same place.'
+    );
+
+    assert.deepEqual(
+        chart.series[0].points.map(p => p.graphic.attr('fill')),
+        [
+            '#0000ff',
+            '#0000ff',
+            '#ff0000',
+            '#0000ff',
+            '#ff0000'
+        ],
+        '#15523: Only negative points should use negativeColor'
     );
 });
