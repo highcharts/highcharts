09ed02026908e499dbd2df62179cf057708ced3e
Karol Kołodziej <karolkolodziej01@gmail.com>
2021-05-07 14:08:35 +0200

    Added test.

--- samples/unit-tests/navigator/navigator/demo.js
+++ samples/unit-tests/navigator/navigator/demo.js
@@ -1197,15 +1197,28 @@ QUnit.test('Navigator dafault dataLabels enabled, #13847.', function (assert) {
 });
 
 QUnit.test('Scrolling when the range is set, #14742.', function (assert) {
-    const chart = Highcharts.stockChart('container', {
-        xAxis: {
-            range: 15
-        },
-        series: [{
-            // 16 points -> range is 15
-            data: [7, 6, 9, 14, 8, 8, 5, 6, 4, 1, 3, 9, 4, 6, 7, 4]
-        }]
-    });
+    let cursor = 8;
+    const chunk = 3,
+        originalData = [7, 6, 9, 14, 8, 8, 5, 6, 4, 1, 3, 9, 4, 6, 7, 4],
+        chart = Highcharts.stockChart('container', {
+            xAxis: {
+                range: 15
+            },
+            series: [{
+                // 16 points -> range is 15
+                data: originalData
+            }]
+        });
+
+    function addPoints() {
+        const data = originalData.slice(cursor, cursor + chunk);
+        cursor += chunk;
+        for (let i = 0; i < data.length; i++) {
+            chart.series[0].addPoint(data[i], false, true);
+        }
+
+        chart.redraw();
+    }
 
     assert.strictEqual(
         chart.xAxis[0].min,
@@ -1241,5 +1254,20 @@ QUnit.test('Scrolling when the range is set, #14742.', function (assert) {
         chart.xAxis[0].min,
         0,
         `When all button enabled, adding point should not change the extremes.`
+    );
+
+    chart.xAxis[0].setExtremes(2, 5);
+    addPoints();
+
+    assert.strictEqual(
+        chart.xAxis[0].min,
+        chart.series[0].data[0].x,
+        `After changing the extremes and adding shifted points,
+        min should stay at the begging of the data.`
+    );
+    assert.ok(
+        chart.xAxis[0].max > chart.xAxis[0].min,
+        `After changing the extremes and adding shifted points,
+        the range should not equal zero.`
     );
 });
