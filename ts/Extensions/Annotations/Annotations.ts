cd09c8403234b20dd55446a839b91546ea8c421f
Pawel Lysy <pawel.lysy@blacklabel.pl>
2021-05-07 17:10:04 +0200

    Create test case for annotation's label visibility, #15508.

--- ts/Extensions/Annotations/Annotations.ts
+++ ts/Extensions/Annotations/Annotations.ts
@@ -165,6 +165,8 @@ declare global {
             point?: (string|MockPointOptions);
             itemType?: string;
             vertical?: VerticalAlignValue;
+            xAxis?: number;
+            yAxis?: number;
         }
         interface AnnotationsOptions extends AnnotationControllableOptionsObject { // @todo AnnotationOptions.d.ts
             animation: Partial<AnimationOptions>;
