3394163b7e03a3ab843e59e1ad7258986eebc86f
Ken-Håvard Lieng <kenh.lieng@highsoft.com>
2021-06-10 12:40:23 +0200

    Fixed #15849, candlestick fill was wrong when updating from up to down

--- ts/Core/Series/Point.ts
+++ ts/Core/Series/Point.ts
@@ -365,6 +365,10 @@ class Point {
             }
         }
 
+        if (point.resolveColor) {
+            point.resolveColor();
+        }
+
         return point;
     }
 
@@ -692,8 +696,6 @@ class Point {
 
         // Add a unique ID to the point if none is assigned
         this.id = defined(this.id) ? this.id : uniqueKey();
-
-        this.resolveColor();
 
         series.chart.pointCount++;
 
