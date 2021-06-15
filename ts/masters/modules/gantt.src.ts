c23bae9b03ac15f5657658574a970f51d856fe24
Ken-Håvard Lieng <kenh.lieng@highsoft.com>
2021-06-15 13:39:26 +0200

    Added double-compose protection

--- ts/masters/modules/gantt.src.ts
+++ ts/masters/modules/gantt.src.ts
@@ -18,11 +18,9 @@ import Scrollbar from '../../Core/Scrollbar.js';
 import '../../Extensions/RangeSelector.js';
 import '../../Core/Navigator.js';
 const G: AnyRecord = Highcharts;
-// Compositions
-if (!G.Scrollbar) {
-    Scrollbar.compose(G.Axis);
-}
 // Classes
 G.Scrollbar = Scrollbar;
 G.GanttChart = GanttChart;
 G.ganttChart = GanttChart.ganttChart;
+// Compositions
+Scrollbar.compose(G.Axis);
