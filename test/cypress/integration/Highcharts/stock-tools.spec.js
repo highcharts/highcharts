9f2a712a54eb64176459b658aa36af096f72d004
Ken-Håvard Lieng <kenh.lieng@highsoft.com>
2021-06-15 09:47:28 +0200

    Added chart command

--- test/cypress/integration/Highcharts/stock-tools.spec.js
+++ test/cypress/integration/Highcharts/stock-tools.spec.js
@@ -1,23 +1,15 @@
 describe('Stock Tools', () => {
-    let chart;
-
     beforeEach(() => {
         cy.viewport(1000, 500);
     });
 
     before(() => {
-        cy.intercept('https://demo-live-data.highcharts.com/*').as('data');
         cy.visit('/stock/demo/stock-tools-gui');
-        return cy.wait('@data')
-            .then(cy.window)
-            .then(win => {
-                chart = win.Highcharts.charts[0];
-            });
     });
 
     it('#15730: Should close popup after hiding annotation', () => {
         cy.get('.highcharts-label-annotation').first().click();
-        cy.get('.highcharts-container').click().should(() =>
+        cy.get('.highcharts-container').click().chart().should(chart =>
             assert.strictEqual(chart.annotations.length, 1)
         );
         cy.get('.highcharts-annotation').click();
