highcharts/highcharts

commit 4a118e15842b7f924b93d6d6a162d97da6807602

Fixed regressions with chart.update and setSize
- Fixed #13995
- Fixed #14130

ts/Core/Axis/Axis.ts
@@ -465,7 +465,7 @@ declare global {
             public renderTick(pos: number, i: number): void;
             public renderUnsquish(): void;
             public setAxisSize(): void;
-            public setAxisTranslation(saveOld?: boolean): void;
+            public setAxisTranslation(): void;
             public setExtremes(
                 newMin?: number,
                 newMax?: number,
@@ -5172,12 +5172,9 @@ class Axis implements AxisComposition, AxisLike {
      * @private
      * @function Highcharts.Axis#setAxisTranslation
      *
-     * @param {boolean} [saveOld]
-     * TO-DO: parameter description
-     *
      * @fires Highcharts.Axis#event:afterSetAxisTranslation
      */
-    public setAxisTranslation(saveOld?: boolean): void {
+    public setAxisTranslation(): void {
         var axis = this,
             range = (axis.max as any) - (axis.min as any),
             pointRange = axis.axisPointRange || 0,
@@ -5272,9 +5269,6 @@ class Axis implements AxisComposition, AxisLike {
         }
 
         // Secondary values
-        if (saveOld) {
-            axis.oldTransA = transA;
-        }
         axis.translationSlope = axis.transA = transA =
             axis.staticScale ||
             axis.len / ((range + pointRangePadding) || 1);
@@ -5537,7 +5531,7 @@ class Axis implements AxisComposition, AxisLike {
         }
 
         // set the translation factor used in translate function
-        axis.setAxisTranslation(true);
+        axis.setAxisTranslation();
 
         // hook for ordinal axes and radial axes
         fireEvent(this, 'initialAxisTranslation');
@@ -7237,7 +7231,7 @@ class Axis implements AxisComposition, AxisLike {
      */
     public renderMinorTick(pos: number): void {
         const axis: Highcharts.Axis = this as any;
-        const slideInTicks = axis.chart.hasRendered && isNumber(axis.oldMin);
+        const slideInTicks = axis.chart.hasRendered && isNumber(axis.oldTransA);
         const minorTicks = axis.minorTicks;
 
         if (!minorTicks[pos]) {
@@ -7268,7 +7262,7 @@ class Axis implements AxisComposition, AxisLike {
         const axis: Highcharts.Axis = this as any;
         const isLinked = axis.isLinked;
         const ticks = axis.ticks;
-        const slideInTicks = axis.chart.hasRendered && isNumber(axis.oldMin);
+        const slideInTicks = axis.chart.hasRendered && isNumber(axis.oldTransA);
 
         // Linked axes need an extra check to find out if
         if (!isLinked ||
@@ -7492,6 +7486,7 @@ class Axis implements AxisComposition, AxisLike {
         }
         // End stacked totals
 
+        axis.oldTransA = axis.transA;
         axis.isDirty = false;
 
         fireEvent(this, 'afterRender');

js/Core/Axis/Axis.js
@@ -1187,12 +1187,9 @@ var Axis = /** @class */ (function () {
      * @private
      * @function Highcharts.Axis#setAxisTranslation
      *
-     * @param {boolean} [saveOld]
-     * TO-DO: parameter description
-     *
      * @fires Highcharts.Axis#event:afterSetAxisTranslation
      */
-    Axis.prototype.setAxisTranslation = function (saveOld) {
+    Axis.prototype.setAxisTranslation = function () {
         var axis = this, range = axis.max - axis.min, pointRange = axis.axisPointRange || 0, closestPointRange, minPointOffset = 0, pointRangePadding = 0, linkedParent = axis.linkedParent, ordinalCorrection, hasCategories = !!axis.categories, transA = axis.transA, isXAxis = axis.isXAxis;
         // Adjust translation for padding. Y axis with categories need to go
         // through the same (#1784).
@@ -1252,9 +1249,6 @@ var Axis = /** @class */ (function () {
             }
         }
         // Secondary values
-        if (saveOld) {
-            axis.oldTransA = transA;
-        }
         axis.translationSlope = axis.transA = transA =
             axis.staticScale ||
                 axis.len / ((range + pointRangePadding) || 1);
@@ -1445,7 +1439,7 @@ var Axis = /** @class */ (function () {
             });
         }
         // set the translation factor used in translate function
-        axis.setAxisTranslation(true);
+        axis.setAxisTranslation();
         // hook for ordinal axes and radial axes
         fireEvent(this, 'initialAxisTranslation');
         // In column-like charts, don't cramp in more ticks than there are
@@ -2645,7 +2639,7 @@ var Axis = /** @class */ (function () {
      */
     Axis.prototype.renderMinorTick = function (pos) {
         var axis = this;
-        var slideInTicks = axis.chart.hasRendered && isNumber(axis.oldMin);
+        var slideInTicks = axis.chart.hasRendered && isNumber(axis.oldTransA);
         var minorTicks = axis.minorTicks;
         if (!minorTicks[pos]) {
             minorTicks[pos] = new Tick(axis, pos, 'minor');
@@ -2673,7 +2667,7 @@ var Axis = /** @class */ (function () {
         var axis = this;
         var isLinked = axis.isLinked;
         var ticks = axis.ticks;
-        var slideInTicks = axis.chart.hasRendered && isNumber(axis.oldMin);
+        var slideInTicks = axis.chart.hasRendered && isNumber(axis.oldTransA);
         // Linked axes need an extra check to find out if
         if (!isLinked ||
             (pos >= axis.min && pos <= axis.max) || ((_a = axis.grid) === null || _a === void 0 ? void 0 : _a.isColumn)) {
@@ -2827,6 +2821,7 @@ var Axis = /** @class */ (function () {
             axis.stacking.renderStackTotals();
         }
         // End stacked totals
+        axis.oldTransA = axis.transA;
         axis.isDirty = false;
         fireEvent(this, 'afterRender');
     };

js/Core/Axis/BrokenAxis.js
@@ -203,8 +203,8 @@ var BrokenAxisAdditions = /** @class */ (function () {
                 }
                 Axis.prototype.setExtremes.call(this, newMin, newMax, redraw, animation, eventArguments);
             };
-            axis.setAxisTranslation = function (saveOld) {
-                Axis.prototype.setAxisTranslation.call(this, saveOld);
+            axis.setAxisTranslation = function () {
+                Axis.prototype.setAxisTranslation.call(this);
                 brokenAxis.unitLength = null;
                 if (brokenAxis.hasBreaks) {
                     var breaks = axis.options.breaks || [], 

js/Core/Axis/TreeGridAxis.js
@@ -529,7 +529,7 @@ var TreeGridAxis;
             fireEvent(axis, 'foundExtremes');
             // setAxisTranslation modifies the min and max according to
             // axis breaks.
-            axis.setAxisTranslation(true);
+            axis.setAxisTranslation();
             axis.tickmarkOffset = 0.5;
             axis.tickInterval = 1;
             axis.tickPositions = axis.treeGrid.mapOfPosToGridNode ?

js/Core/Chart/Chart.js
@@ -563,13 +563,8 @@ var Chart = /** @class */ (function () {
         if (hasCartesianSeries) {
             // set axes scales
             axes.forEach(function (axis) {
-                // Don't do setScale again if we're only resizing. Regression
-                // #13507. But we need it after chart.update (responsive), as
-                // axis is initialized again (#12137).
-                if (!chart.isResizing || !isNumber(axis.min)) {
-                    axis.updateNames();
-                    axis.setScale();
-                }
+                axis.updateNames();
+                axis.setScale();
             });
         }
         chart.getMargins(); // #3098

samples/unit-tests/chart/chart-update-axis/demo.js
@@ -171,3 +171,63 @@ QUnit.test('Updating unidentified axes by index (#6019)', function (assert) {
         'Updated titles'
     );
 });
+
+QUnit.test('Stacking consistent after update', assert => {
+    const options = {
+        chart: {
+            width: 250,
+            type: "column"
+        },
+        yAxis: {
+            title: {
+                text: 'test 1'
+            }
+        },
+        plotOptions: {
+            series: {
+                stacking: "normal",
+                animation: false
+            }
+        },
+        series: [{
+            data: [500]
+        }, {
+            data: [300]
+        }],
+        legend: {
+            enabled: false
+        }
+    };
+
+    const chart = Highcharts.chart('container', options);
+
+    const getPositions = () => [
+        chart.series[0].points[0].graphic.getBBox().y,
+        chart.series[0].points[0].graphic.getBBox().height,
+        chart.series[1].points[0].graphic.getBBox().y,
+        chart.series[1].points[0].graphic.getBBox().height
+    ];
+    const originalPositions = getPositions();
+    chart.update({
+        chart: {
+            width: 200
+        },
+        yAxis: {
+            title: {
+                text: 'test 2'
+            }
+        },
+        series: [{
+            data: [500]
+        }, {
+            data: [300]
+        }]
+    }, undefined, undefined, false);
+
+    assert.deepEqual(
+        getPositions(),
+        originalPositions,
+        'Horizontal bar positions should not change after update (#14130)'
+    );
+
+});

samples/unit-tests/chart/setsize/demo.js
@@ -544,3 +544,49 @@ QUnit.test('Succession of setSize and other dynamics', assert => {
         done();
     }, 2);
 });
+
+QUnit.test('Succession of setSize and adders', assert => {
+    var chart = Highcharts.chart('container', {
+
+    });
+    chart.setSize(undefined, undefined);
+
+    chart.addAxis({
+        id: 'xaxis1'
+    });
+
+
+    chart.addAxis({
+        id: 'yaxis1'
+    });
+
+    chart.addSeries({
+        type: 'column',
+        yAxis: 'yaxis1',
+        data: [1, 2, 3, 4]
+    }, false);
+
+
+    chart.addAxis({
+        id: 'yaxis2'
+    });
+
+    chart.addSeries({
+        type: 'column',
+        yAxis: 'yaxis2',
+        data: [1, 2, 3, 4]
+    }, false);
+
+    chart.redraw();
+
+    const colHeight = chart.series[0].points[0].graphic.getBBox().height;
+    assert.ok(
+        typeof colHeight === 'number' && colHeight > 0,
+        'The column height should be a positive number'
+    );
+    assert.strictEqual(
+        colHeight,
+        chart.series[1].points[0].graphic.getBBox().height,
+        'The two first columns should be equal height (#13995)'
+    );
+});

ts/Core/Axis/BrokenAxis.ts
@@ -334,8 +334,8 @@ class BrokenAxisAdditions {
                 );
             };
 
-            axis.setAxisTranslation = function (saveOld?: boolean): void {
-                Axis.prototype.setAxisTranslation.call(this, saveOld);
+            axis.setAxisTranslation = function (): void {
+                Axis.prototype.setAxisTranslation.call(this);
 
                 brokenAxis.unitLength = null as any;
                 if (brokenAxis.hasBreaks) {

commit 4772fd2e032e07b10d8bad3bb1dc5a57f78b0bab

Fixed regression with sliding ticks Visible in the line-ajax demo, regression since 4a118e15842b7f924b93d6d6a162d97da6807602

ts/Core/Axis/Axis.ts
@@ -17,7 +17,28 @@
  * */
 
 import type AnimationOptions from '../Animation/AnimationOptions';
+import A from '../Animation/AnimationUtilities.js';
+import type { EventCallback } from '../Callback';
+import type Chart from '../Chart/Chart';
+import Color from '../Color/Color.js';
+import Palette from '../Color/Palette.js';
+import D from '../DefaultOptions.js';
+import F from '../Foundation.js';
+import H from '../Globals.js';
+import type PointerEvent from '../PointerEvent';
+import type { AlignValue } from '../Renderer/AlignObject';
+import type CSSObject from '../Renderer/CSSObject';
+import type FontMetricsObject from '../Renderer/FontMetricsObject';
+import type PositionObject from '../Renderer/PositionObject';
+import type SizeObject from '../Renderer/SizeObject';
+import type SVGAttributes from '../Renderer/SVG/SVGAttributes';
+import type SVGElement from '../Renderer/SVG/SVGElement';
+import type SVGPath from '../Renderer/SVG/SVGPath';
+import type Point from '../Series/Point';
+import type Series from '../Series/Series';
+import U from '../Utilities.js';
 import type AxisComposition from './AxisComposition';
+import AxisDefaults from './AxisDefaults.js';
 import type AxisLike from './AxisLike';
 import type {
     AxisCrosshairOptions,
@@ -29,36 +50,15 @@ import type {
     YAxisOptions
 } from './AxisOptions';
 import type { AxisTypeOptions } from './AxisType';
-import type TickPositionsArray from './TickPositionsArray';
-import type { AlignValue } from '../Renderer/AlignObject';
-import type Chart from '../Chart/Chart';
-import type CSSObject from '../Renderer/CSSObject';
-import type { EventCallback } from '../Callback';
-import type FontMetricsObject from '../Renderer/FontMetricsObject';
 import type PlotLineOrBand from './PlotLineOrBand';
-import type Point from '../Series/Point';
-import type PointerEvent from '../PointerEvent';
-import type PositionObject from '../Renderer/PositionObject';
-import type Series from '../Series/Series';
-import type SizeObject from '../Renderer/SizeObject';
-import type SVGAttributes from '../Renderer/SVG/SVGAttributes';
-import type SVGElement from '../Renderer/SVG/SVGElement';
-import type SVGPath from '../Renderer/SVG/SVGPath';
-import A from '../Animation/AnimationUtilities.js';
+import Tick from './Tick.js';
+import type TickPositionsArray from './TickPositionsArray';
 const { animObject } = A;
-import AxisDefaults from './AxisDefaults.js';
-import Color from '../Color/Color.js';
-import F from '../Foundation.js';
 const {
     registerEventOptions
 } = F;
-import H from '../Globals.js';
 const { deg2rad } = H;
-import Palette from '../Color/Palette.js';
-import D from '../DefaultOptions.js';
 const { defaultOptions } = D;
-import Tick from './Tick.js';
-import U from '../Utilities.js';
 const {
     arrayMax,
     arrayMin,
@@ -3537,10 +3537,12 @@ class Axis {
      *
      * @param {number} pos
      * The position in axis values.
+     *
+     * @param {boolean} slideIn
+     * Whether the tick should animate in from last computed position
      */
-    public renderMinorTick(pos: number): void {
+    public renderMinorTick(pos: number, slideIn?: boolean): void {
         const axis = this;
-        const slideInTicks = axis.chart.hasRendered && axis.old;
         const minorTicks = axis.minorTicks;
 
         if (!minorTicks[pos]) {
@@ -3548,7 +3550,7 @@ class Axis {
         }
 
         // Render new ticks in old position
-        if (slideInTicks && minorTicks[pos].isNew) {
+        if (slideIn && minorTicks[pos].isNew) {
             minorTicks[pos].render(null as any, true);
         }
 
@@ -3566,12 +3568,14 @@ class Axis {
      *
      * @param {number} i
      * The tick index.
+     *
+     * @param {boolean} slideIn
+     * Whether the tick should animate in from last computed position
      */
-    public renderTick(pos: number, i: number): void {
+    public renderTick(pos: number, i: number, slideIn?: boolean): void {
         const axis = this,
             isLinked = axis.isLinked,
-            ticks = axis.ticks,
-            slideInTicks = axis.chart.hasRendered && axis.old;
+            ticks = axis.ticks;
 
         // Linked axes need an extra check to find out if
         if (
@@ -3586,7 +3590,7 @@ class Axis {
             // NOTE this seems like overkill. Could be handled in tick.render by
             // setting old position in attr, then set new position in animate.
             // render new ticks in old position
-            if (slideInTicks && ticks[pos].isNew) {
+            if (slideIn && ticks[pos].isNew) {
                 // Start with negative opacity so that it is visible from
                 // halfway into the animation
                 ticks[pos].render(i, true, -1);
@@ -3642,12 +3646,15 @@ class Axis {
         // If the series has data draw the ticks. Else only the line and title
         if (axis.hasData() || isLinked) {
 
+            const slideInTicks = axis.chart.hasRendered &&
+                axis.old && isNumber(axis.old.min);
+
             // minor ticks
             if (axis.minorTickInterval && !axis.categories) {
                 axis.getMinorTickPositions().forEach(function (
                     pos: number
                 ): void {
-                    axis.renderMinorTick(pos);
+                    axis.renderMinorTick(pos, slideInTicks);
                 });
             }
 
@@ -3655,7 +3662,7 @@ class Axis {
             // we can get the position of the neighbour label. #808.
             if (tickPositions.length) { // #1300
                 tickPositions.forEach(function (pos: number, i: number): void {
-                    axis.renderTick(pos, i);
+                    axis.renderTick(pos, i, slideInTicks);
                 });
                 // In a categorized axis, the tick marks are displayed
                 // between labels. So we need to add a tick mark and