aab8ca06ebefd907b3609b347f1d7f89520093bc
Ken-Håvard Lieng <kenh.lieng@highsoft.com>
2021-06-16 12:51:38 +0200

    Added test

--- samples/unit-tests/annotations/annotations-dynamic/demo.js
+++ samples/unit-tests/annotations/annotations-dynamic/demo.js
@@ -204,6 +204,23 @@ QUnit.test("Annotation's dynamic methods", function (assert) {
         'blue',
         '#15424: Fourth line should be blue (lineColor)'
     );
+
+    const rect = chart.addAnnotation({
+        shapes: [{
+            type: 'path',
+            points: [
+                { xAxis: 0, yAxis: 0, x: 1, y: 150000 },
+                { xAxis: 0, yAxis: 0, x: 3, y: 150000 },
+                { xAxis: 0, yAxis: 0, x: 3, y: 100000 },
+                { xAxis: 0, yAxis: 0, x: 1, y: 100000 }
+            ]
+        }]
+    });
+
+    assert.ok(
+        rect.clipRect,
+        '#15726: Rectangle annotations should be clipped'
+    );
 });
 
 QUnit.test(
