f0c1977ea3dce25a4cb6ae9c458f3671c902f1a7
bre1470 <40056287+bre1470@users.noreply.github.com>
2021-06-23 17:27:06 +0200

    Made avocado theme compatible to ES6 tree shaking.

--- ts/masters/themes/avocado.src.ts
+++ ts/masters/themes/avocado.src.ts
@@ -8,4 +8,5 @@
  * License: www.highcharts.com/license
  */
 'use strict';
-import '../../Extensions/Themes/Avocado.js';
+import AvocadoTheme from '../../Extensions/Themes/Avocado.js';
+AvocadoTheme.apply();
