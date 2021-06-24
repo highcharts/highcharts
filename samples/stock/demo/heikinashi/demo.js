9563f4cca301a9ee8bbf7d4ff10f4f30f1546664
Karol Kołodziej <karolkolodziej01@gmail.com>
2021-06-24 14:40:09 +0200

    Added requested changes in general documentation and demo.

--- samples/stock/demo/heikinashi/demo.js
+++ samples/stock/demo/heikinashi/demo.js
@@ -1,21 +1,10 @@
 Highcharts.getJSON('https://demo-live-data.highcharts.com/aapl-ohlcv.json', function (data) {
-    var ohlc = [],
-        dataLength = data.length,
-        i = 0;
-
-    for (i; i < dataLength; i += 1) {
-        ohlc.push([
-            data[i][0], // the date
-            data[i][1], // open
-            data[i][2], // high
-            data[i][3], // low
-            data[i][4] // close
-        ]);
-    }
-
     Highcharts.stockChart('container', {
         title: {
             text: 'Candlestick and Heiken Ashi series comparison.'
+        },
+        rangeSelector: {
+            selected: 1
         },
         yAxis: [{
             title: {
@@ -33,11 +22,11 @@ Highcharts.getJSON('https://demo-live-data.highcharts.com/aapl-ohlcv.json', func
         series: [{
             type: 'candlestick',
             name: 'Candlestick',
-            data: ohlc
+            data: data
         }, {
             type: 'heikinashi',
             name: 'Heikin Ashi',
-            data: ohlc,
+            data: data,
             yAxis: 1
         }]
     });
