d019c11989a53113feccb8df85b006a6ae5d4d3f
Ken-Håvard Lieng <kenh.lieng@highsoft.com>
2021-07-05 12:22:57 +0200

    Added test

--- test/cypress/integration/Highcharts/stock-tools.spec.js
+++ test/cypress/integration/Highcharts/stock-tools.spec.js
@@ -17,5 +17,20 @@ describe('Stock Tools', () => {
         cy.get('.highcharts-popup').should('be.visible');
         cy.get('.highcharts-toggle-annotations').click();
         cy.get('.highcharts-popup').should('be.hidden');
+        cy.get('.highcharts-toggle-annotations').click();
+    });
+
+    it('#15725: Should use the same axis for all points in multi-step annotation', () => {
+        cy.get('.highcharts-elliott3').first().click();
+        cy.get('.highcharts-container')
+            .click(100, 210)
+            .click(120, 260)
+            .click(140, 210)
+            .click(160, 260);
+        cy.chart().should(chart =>
+            chart.annotations[1].points.forEach(point =>
+                assert.ok(point.y > -50 && point.y < 50)
+            )
+        );
     });
 });
