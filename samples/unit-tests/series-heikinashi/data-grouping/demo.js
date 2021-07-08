4bebc7d015f0da338801365face84b8450409233
Karol Kołodziej <karolkolodziej01@gmail.com>
2021-07-08 15:37:05 +0200

    Added testes.

---
+++ samples/unit-tests/series-heikinashi/data-grouping/demo.js
@@ -0,0 +1,39 @@
+// QUnit.test('directTouch', function (assert) {
+//     Highcharts.stockChart('container', {
+//         series: [
+//             {
+//                 type: 'heikinashi',
+//                 dataGrouping: {
+//                     enabled: true,
+//                     forced: true,
+//                     units: [['millisecond', [3]]]
+//                 },
+//                 data: [
+//                     [1, 2, 4, 2, 4],
+//                     [2, 1, 2, 1, 2],
+//                     [3, 1, 2, 1, 2],
+//                     [4, 1, 2, 1, 2],
+//                     [5, 1, 2, 1, 2],
+//                     [6, 1, 2, 1, 2],
+//                     [7, 1, 2, 1, 2],
+//                     [8, 1, 2, 1, 2],
+//                     [9, 1, 2, 1, 2],
+//                     [10, 1, 2, 1, 2],
+//                     [11, 1, 2, 1, 2],
+//                     [12, 1, 2, 1, 2],
+//                     [13, 1, 2, 1, 2],
+//                     [14, 1, 2, 1, 2],
+//                     [15, 1, 2, 1, 2]
+//                 ]
+//             }
+//         ]
+//     });
+
+//     //chart.xAxis[0].setExtremes(5, undefined);
+//     // assert.strictEqual(
+//     //     thirdPointOpen,
+//     //     chart.series[0].points[1].open,
+//     //     `When data grouping enabled, changing the extremes should not
+//     //     influence the heikinashi candles.`
+//     // );
+// });
