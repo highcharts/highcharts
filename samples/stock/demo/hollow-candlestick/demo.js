d055a4c2b0f2248ed3621db3975ba32339c56de8
Karol Kołodziej <karolkolodziej01@gmail.com>
2021-07-12 12:32:14 +0200

    Changed navigator color in demos and in docs image.

--- samples/stock/demo/hollow-candlestick/demo.js
+++ samples/stock/demo/hollow-candlestick/demo.js
@@ -3,6 +3,11 @@ Highcharts.getJSON('https://demo-live-data.highcharts.com/aapl-ohlcv.json', func
         rangeSelector: {
             selected: 1
         },
+        navigator: {
+            series: {
+                color: Highcharts.getOptions().colors[0]
+            }
+        },
         series: [{
             type: 'hollowcandlestick',
             name: 'Hollow Candlestick',
