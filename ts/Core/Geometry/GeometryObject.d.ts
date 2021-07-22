a52e73b3b34ed467c36ce72ecd7761ec3fdc581c
bre1470 <40056287+bre1470@users.noreply.github.com>
2021-07-22 14:21:24 +0200

    Cleaned up in geometry utilities.

--- ts/Core/Geometry/GeometryObject.d.ts
+++ ts/Core/Geometry/GeometryObject.d.ts
@@ -1,5 +1,15 @@
 /* *
  *
+ *  (c) 2010-2021 Highsoft AS
+ *
+ *  License: www.highcharts.com/license
+ *
+ *  !!!!!!! SOURCE GETS TRANSPILED BY TYPESCRIPT. EDIT TS FILE ONLY. !!!!!!!
+ *
+ * */
+
+/* *
+ *
  *  Imports
  *
  * */
