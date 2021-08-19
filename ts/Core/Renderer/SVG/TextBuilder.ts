b06e9ba5c595274583e2b22729571fde88ee576e
Torstein Hønsi <torstein@highsoft.com>
2021-08-19 11:20:26 +0200

    Fixed additional case for #15235, nested space removed in HTML

--- ts/Core/Renderer/SVG/TextBuilder.ts
+++ ts/Core/Renderer/SVG/TextBuilder.ts
@@ -190,6 +190,22 @@ class TextBuilder {
         const x = attr(wrapper.element, 'x');
         wrapper.firstLineMetrics = void 0;
 
+        // Remove empty tspans (including breaks) from the beginning because
+        // SVG's getBBox doesn't count empty lines. The use case is tooltip
+        // where the header is empty. By doing this in the DOM rather than in
+        // the AST, we can inspect the textContent directly and don't have to
+        // recurse down to look for valid content.
+        let firstChild: ChildNode|null;
+        while ((firstChild = wrapper.element.firstChild)) {
+            if (
+                /^[\s\u200B]*$/.test(firstChild.textContent || ' ')
+            ) {
+                wrapper.element.removeChild(firstChild);
+            } else {
+                break;
+            }
+        }
+
         // Modify hard line breaks by applying the rendered line height
         [].forEach.call(
             wrapper.element.querySelectorAll('tspan.highcharts-br'),
@@ -446,16 +462,6 @@ class TextBuilder {
         };
 
         nodes.forEach(modifyChild);
-
-        // Remove empty spans from the beginning because SVG's getBBox doesn't
-        // count empty lines. The use case is tooltip where the header is empty.
-        while (nodes[0]) {
-            if (nodes[0].tagName === 'tspan' && !nodes[0].children) {
-                nodes.splice(0, 1);
-            } else {
-                break;
-            }
-        }
     }
 
     /*
