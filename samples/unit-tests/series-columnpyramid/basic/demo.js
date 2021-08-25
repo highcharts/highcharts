fb32ca9fb36a9cf14a60ea736a380af274b688ab
Mateusz Bernacik <mat.bernacik@gmail.com>
2021-08-25 17:24:29 +0200

    Fixed #16150, corrected pyramids' shapes calculations.

--- samples/unit-tests/series-columnpyramid/basic/demo.js
+++ samples/unit-tests/series-columnpyramid/basic/demo.js
@@ -54,3 +54,51 @@ QUnit.test('Column pyramid series - 0 dataLabel #12514', function (assert) {
         'Label should be translated and had different position translate attributes than starting values #12514'
     );
 });
+
+QUnit.test(
+    "Column pyramid series inverted - yAxis width #16150",
+    function (assert) {
+        const chart = Highcharts.chart("container", {
+            chart: {
+                type: "columnpyramid",
+                inverted: true,
+                width: 600,
+                animation: false,
+                height: 600
+            },
+            yAxis: {
+                width: "40%"
+            },
+
+            series: [
+                {
+                    data: [5, 4, 2]
+                }
+            ]
+        });
+
+        var roundPath = function (dArray) {
+            return dArray.map(item =>
+                Highcharts.map(item, function (value) {
+                    var number = Math.round(value);
+                    return Highcharts.isNumber(number) ? number : value;
+                })
+            );
+        };
+
+        const validPath = [
+            ["M", 370, 37],
+            ["L", 370, 37],
+            ["L", 335, 223],
+            ["L", 406, 223],
+            ["Z"]
+        ];
+
+        const testedPath = roundPath(chart.series[0].points[0].shapeArgs.d);
+        assert.deepEqual(
+            testedPath,
+            validPath,
+            "testedPath should be equal to validPath, #16150"
+        );
+    }
+);
