b58656fe36d7373d7dac092182875ebfb627b4a6
info <mustapha@highsoft.com>
2021-09-03 14:34:07 +0200

    Added linter review

--- samples/highcharts/chartchooser/treemap-monochrome/demo.js
+++ samples/highcharts/chartchooser/treemap-monochrome/demo.js
@@ -1,67 +1,65 @@
-let categories = 11;
-
 Highcharts.getJSON(
-  "https://raw.githubusercontent.com/mekhatria/demo_highcharts/master/denmarkExportIn2018.json?callback=?",
+    "https://cdn.jsdelivr.net/gh/highcharts/highcharts@dac9b997d8/samples/data/denmark-export-2018.json",
 
-  function (dataJson) {
-    Highcharts.chart("container", {
-      accessibility: {
-        screenReaderSection: {
-          beforeChartFormat:
-            "<{headingTagName}>{chartTitle}</{headingTagName}><div>{typeDescription}</div><div>{chartSubtitle}</div><div>{chartLongdesc}</div>",
-        },
-        description:
-          "Tree map detailing exported goods from Denmark in 2018. The services category is the largest, and makes up 39% of the total export. Transport services are the most exported. Chemicals are the second most exported category with 13%, then agriculture with 12%, and machinery with 10%. The other categories are textiles, electronics, metals, minerals, vehicles, stone, and other.",
-      },
-      series: [
-        {
-          name: "Export",
-          type: "treemap",
-          layoutAlgorithm: "squarified",
-          allowDrillToNode: true,
-          turboThreshold: dataJson.length,
-          dataLabels: {
-            enabled: false,
-          },
-          color: "#ffffff",
-          borderColor: "black",
-          levels: [
-            {
-              level: 1,
-              dataLabels: {
-                enabled: true,
-                format: "{point.name} {point.value:.0f}%",
-                style: {
-                  textOutline: false,
-                  fontSize: 18,
+    function (dataJson) {
+        Highcharts.chart("container", {
+            accessibility: {
+                screenReaderSection: {
+                    beforeChartFormat:
+            "<{headingTagName}>{chartTitle}</{headingTagName}><div>{typeDescription}</div><div>{chartSubtitle}</div><div>{chartLongdesc}</div>"
                 },
-                zIndex: 9,
-              },
-              borderWidth: 3,
+                description:
+          "Tree map detailing exported goods from Denmark in 2018. The services category is the largest, and makes up 39% of the total export. Transport services are the most exported. Chemicals are the second most exported category with 13%, then agriculture with 12%, and machinery with 10%. The other categories are textiles, electronics, metals, minerals, vehicles, stone, and other."
             },
-            {
-              level: 2,
-              dataLabels: {
-                enabled: false,
-              },
-              borderWidth: 1,
+            series: [
+                {
+                    name: "Export",
+                    type: "treemap",
+                    layoutAlgorithm: "squarified",
+                    allowDrillToNode: true,
+                    turboThreshold: dataJson.length,
+                    dataLabels: {
+                        enabled: false
+                    },
+                    color: "#ffffff",
+                    borderColor: "black",
+                    levels: [
+                        {
+                            level: 1,
+                            dataLabels: {
+                                enabled: true,
+                                format: "{point.name} {point.value:.0f}%",
+                                style: {
+                                    textOutline: false,
+                                    fontSize: 18
+                                },
+                                zIndex: 9
+                            },
+                            borderWidth: 3
+                        },
+                        {
+                            level: 2,
+                            dataLabels: {
+                                enabled: false
+                            },
+                            borderWidth: 1
+                        }
+                    ],
+                    data: dataJson
+                }
+            ],
+            title: {
+                text: "Denmark Export 2018"
             },
-          ],
-          data: dataJson,
-        },
-      ],
-      title: {
-        text: "Denmark Export 2018",
-      },
-      subtitle: {
-        useHTMl: true,
-        text: "Source:Harvard.edu",
-      },
-      tooltip: {
-        useHTML: true,
-        valueDecimals: 2,
-        pointFormat: "<b>{point.name}</b>: <b>{point.value}%",
-      },
-    });
-  }
+            subtitle: {
+                useHTMl: true,
+                text: "Source:Harvard.edu"
+            },
+            tooltip: {
+                useHTML: true,
+                valueDecimals: 2,
+                pointFormat: "<b>{point.name}</b>: <b>{point.value}%"
+            }
+        });
+    }
 );
--- samples/highcharts/chartchooser/treemap/demo.js
+++ samples/highcharts/chartchooser/treemap/demo.js
@@ -1,81 +1,79 @@
-let colors = [
-  "#E59866",
-  "#F8C471",
-  "#F7DC6F",
-  "#82E0AA",
-  "#73C6B6",
-  "#85C1E9",
-  "#BB8FCE",
-  "#F1948A",
-  "#B2BABB",
+const colors = [
+    "#E59866",
+    "#F8C471",
+    "#F7DC6F",
+    "#82E0AA",
+    "#73C6B6",
+    "#85C1E9",
+    "#BB8FCE",
+    "#F1948A",
+    "#B2BABB"
 ];
 
 Highcharts.getJSON(
-  "https://raw.githubusercontent.com/mekhatria/demo_highcharts/master/denmarkExportIn2018.json?callback=?",
-  //"https://cdn.jsdelivr.net/gh/highcharts/highcharts@dac9b997d8/samples/data/denmark-export-2018.json",
+    "https://cdn.jsdelivr.net/gh/highcharts/highcharts@dac9b997d8/samples/data/denmark-export-2018.json",
 
-  function (dataJson) {
-    return false;
-    Highcharts.chart("container", {
-      accessibility: {
-        screenReaderSection: {
-          beforeChartFormat:
-            "<{headingTagName}>{chartTitle}</{headingTagName}><div>{typeDescription}</div><div>{chartSubtitle}</div><div>{chartLongdesc}</div>",
-        },
-        description:
-          "Tree map detailing exported goods from Denmark in 2018. The services category is the largest, and makes up 39% of the total export. Transport services are the most exported. Chemicals are the second most exported category with 13%, then agriculture with 12%, and machinery with 10%. The other categories are textiles, electronics, metals, minerals, vehicles, stone, and other.",
-      },
-      colors: colors,
-      series: [
-        {
-          name: "Export",
-          type: "treemap",
-          layoutAlgorithm: "squarified",
-          allowDrillToNode: true,
-          turboThreshold: Infinity,
-          dataLabels: {
-            enabled: false,
-            crop: true,
-          },
-          borderColor: "black",
-          levels: [
-            {
-              level: 1,
-              colorByPoint: true,
-              dataLabels: {
-                enabled: true,
-                format: "{point.name} {point.value:.0f}%",
-                style: {
-                  textOutline: false,
-                  fontSize: 18,
+    function (dataJson) {
+        Highcharts.chart("container", {
+            accessibility: {
+                screenReaderSection: {
+                    beforeChartFormat:
+            "<{headingTagName}>{chartTitle}</{headingTagName}><div>{typeDescription}</div><div>{chartSubtitle}</div><div>{chartLongdesc}</div>"
                 },
-                zIndex: 9,
-              },
-              borderWidth: 3,
+                description:
+          "Tree map detailing exported goods from Denmark in 2018. The services category is the largest, and makes up 39% of the total export. Transport services are the most exported. Chemicals are the second most exported category with 13%, then agriculture with 12%, and machinery with 10%. The other categories are textiles, electronics, metals, minerals, vehicles, stone, and other."
             },
-            {
-              level: 2,
-              dataLabels: {
-                enabled: false,
-              },
-              borderWidth: 1,
+            colors: colors,
+            series: [
+                {
+                    name: "Export",
+                    type: "treemap",
+                    layoutAlgorithm: "squarified",
+                    allowDrillToNode: true,
+                    turboThreshold: Infinity,
+                    dataLabels: {
+                        enabled: false,
+                        crop: true
+                    },
+                    borderColor: "black",
+                    levels: [
+                        {
+                            level: 1,
+                            colorByPoint: true,
+                            dataLabels: {
+                                enabled: true,
+                                format: "{point.name} {point.value:.0f}%",
+                                style: {
+                                    textOutline: false,
+                                    fontSize: 18
+                                },
+                                zIndex: 9
+                            },
+                            borderWidth: 3
+                        },
+                        {
+                            level: 2,
+                            dataLabels: {
+                                enabled: false
+                            },
+                            borderWidth: 1
+                        }
+                    ],
+                    data: dataJson
+                }
+            ],
+            title: {
+                text: "Denmark Export 2018"
             },
-          ],
-          data: dataJson,
-        },
-      ],
-      title: {
-        text: "Denmark Export 2018",
-      },
-      subtitle: {
-        useHTMl: true,
-        text: "Source:Harvard.edu",
-      },
-      tooltip: {
-        useHTML: true,
-        valueDecimals: 2,
-        pointFormat: "<b>{point.name}</b>: <b>{point.value}%",
-      },
-    });
-  }
+            subtitle: {
+                useHTMl: true,
+                text: "Source:Harvard.edu"
+            },
+            tooltip: {
+                useHTML: true,
+                valueDecimals: 2,
+                pointFormat: "<b>{point.name}</b>: <b>{point.value}%"
+            }
+        });
+    }
 );
