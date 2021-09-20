da5a3da73586b4e519aee024d9872e2ec079676e
Pawel Lysy <pawel.lysy@blacklabel.pl>
2021-09-20 13:56:57 +0200

    Improved text formatting, #15008.

--- samples/highcharts/annotations-advanced/controllable/demo.js
+++ samples/highcharts/annotations-advanced/controllable/demo.js
@@ -201,130 +201,116 @@ var chart = Highcharts.chart('container', {
                     }
                 }
             }]
-        },
-        {
+        }, {
             type: 'ellipse',
             xAxis: 0,
             yAxis: 0,
-            points: [
-                {
-                    x: 8,
-                    y: 4
-                },
-                {
-                    x: 12,
-                    y: 6
-                }
-            ],
+            points: [{
+                x: 8,
+                y: 4
+            }, {
+                x: 12,
+                y: 6
+            }],
             ry: 1,
-            controlPoints: [
-                {
-                    positioner: function (target) {
-                        return {
-                            x:
-                                target.chart.plotLeft +
-                                target.points[0].plotX -
-                                this.graphic.width / 2,
-                            y:
-                                target.chart.plotTop +
-                                target.points[0].plotY -
-                                this.graphic.width / 2
-                        };
-                    },
-                    events: {
-                        drag: function (e, target) {
-                            target.translatePoint(
-                                e.chartX -
-                                    (target.points[0].plotX +
-                                        target.chart.plotLeft),
-                                e.chartY -
-                                    (target.points[0].plotY +
-                                        target.chart.plotTop),
-                                0
-                            );
+            controlPoints: [{
+                positioner: function (target) {
+                    return {
+                        x:
+                            target.chart.plotLeft +
+                            target.points[0].plotX -
+                            this.graphic.width / 2,
+                        y:
+                            target.chart.plotTop +
+                            target.points[0].plotY -
+                            this.graphic.width / 2
+                    };
+                },
+                events: {
+                    drag: function (e, target) {
+                        target.translatePoint(
+                            e.chartX -
+                                (target.points[0].plotX +
+                                    target.chart.plotLeft),
+                            e.chartY -
+                                (target.points[0].plotY +
+                                    target.chart.plotTop),
+                            0
+                        );
 
-                            target.redraw(false);
-                        }
+                        target.redraw(false);
                     }
+                }
+            }, {
+                positioner: function (target) {
+                    return {
+                        x:
+                            target.chart.plotLeft +
+                            target.points[1].plotX -
+                            this.graphic.width / 2,
+                        y:
+                            target.chart.plotTop +
+                            target.points[1].plotY -
+                            this.graphic.width / 2
+                    };
                 },
-                {
-                    positioner: function (target) {
-                        return {
-                            x:
-                                target.chart.plotLeft +
-                                target.points[1].plotX -
-                                this.graphic.width / 2,
-                            y:
-                                target.chart.plotTop +
-                                target.points[1].plotY -
-                                this.graphic.width / 2
-                        };
-                    },
-                    events: {
-                        drag: function (e, target) {
-                            target.translatePoint(
-                                e.chartX -
-                                    (target.points[1].plotX +
-                                        target.chart.plotLeft),
-                                e.chartY -
-                                    (target.points[1].plotY +
-                                        target.chart.plotTop),
-                                1
-                            );
-                            target.redraw(false);
-                        }
+                events: {
+                    drag: function (e, target) {
+                        target.translatePoint(
+                            e.chartX -
+                                (target.points[1].plotX +
+                                    target.chart.plotLeft),
+                            e.chartY -
+                                (target.points[1].plotY +
+                                    target.chart.plotTop),
+                            1
+                        );
+                        target.redraw(false);
                     }
+                }
+            }, {
+                positioner: function (target) {
+                    const position = target.getAbsolutePosition(
+                            target.points[0]
+                        ),
+                        position2 = target.getAbsolutePosition(
+                            target.points[1]
+                        ),
+                        attrs = target.getAttrs(position, position2);
+
+                    return {
+                        x:
+                            attrs.cx - this.graphic.width / 2 +
+                            attrs.ry * Math.sin((attrs.angle * Math.PI) / 180),
+                        y:
+                            attrs.cy - this.graphic.height / 2 -
+                            attrs.ry * Math.cos((attrs.angle * Math.PI) / 180)
+                    };
                 },
-                {
-                    positioner: function (target) {
+                events: {
+                    drag: function (e, target) {
                         const position = target.getAbsolutePosition(
                                 target.points[0]
                             ),
                             position2 = target.getAbsolutePosition(
                                 target.points[1]
                             ),
-                            attrs = target.getAttrs(position, position2);
+                            attrs = target.getAttrs(position, position2),
+                            dx = e.chartX - attrs.cx,
+                            dy = e.chartY - attrs.cy,
+                            newR = Math.max(
+                                Math.sqrt(dx * dx + dy * dy), 5
+                            ),
+                            yAxis = target.getYAxis(),
+                            newRY = Math.abs(
+                                yAxis.toValue(0) - yAxis.toValue(newR)
+                            );
+                        target.options.ry = newRY;
 
-                        return {
-                            x:
-                                attrs.cx -
-                                this.graphic.width / 2 +
-                                attrs.ry * Math.sin(
-                                    (attrs.angle * Math.PI) / 180
-                                ),
-                            y:
-                                attrs.cy -
-                                this.graphic.height / 2 -
-                                attrs.ry * Math.cos(
-                                    (attrs.angle * Math.PI) / 180
-                                )
-                        };
-                    },
-                    events: {
-                        drag: function (e, target) {
-                            const position = target.getAbsolutePosition(
-                                    target.points[0]
-                                ),
-                                position2 = target.getAbsolutePosition(
-                                    target.points[1]
-                                ),
-                                attrs = target.getAttrs(position, position2),
-                                dx = e.chartX - attrs.cx,
-                                dy = e.chartY - attrs.cy,
-                                newR = Math.max(
-                                    Math.sqrt(dx * dx + dy * dy), 5
-                                ),
-                                yAxis = target.getYAxis(),
-                                newRY = Math.abs(
-                                    yAxis.toValue(0) - yAxis.toValue(newR)
-                                );
-                            target.options.ry = newRY;
-
-                            target.redraw(false);
-                        }
+                        target.redraw(false);
                     }
                 }
-            ]
+            }]
         }
 
         ],
