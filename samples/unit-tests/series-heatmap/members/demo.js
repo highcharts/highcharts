f5101d122685522aa82f398ecd9f5e14ff69a08a
K009 <spidahas@gmail.com>
2021-09-24 13:12:27 +0200

    Fixed the test and changed comment in heatmapseries file.

--- samples/unit-tests/series-heatmap/members/demo.js
+++ samples/unit-tests/series-heatmap/members/demo.js
@@ -197,7 +197,8 @@ QUnit.test('seriesTypes.heatmap.pointClass.setState', function (assert) {
             },
 
             series: [{
-                data: [[0, 0, 1]]
+                data: [[0, 0, 1]],
+                borderRadius: 39
             }]
         }),
         point = chart.series[0].points[0],
@@ -210,54 +211,33 @@ QUnit.test('seriesTypes.heatmap.pointClass.setState', function (assert) {
         0,
         'When state:normal zIndex is 0'
     );
+
     setState.call(point, 'hover');
     assert.strictEqual(point.graphic.zIndex, 1, 'When state:hover zIndex is 1');
+
     setState.call(point, 'select');
     assert.strictEqual(
         point.graphic.zIndex,
         0,
         'When state:select zIndex is 0'
     );
-});
-
-QUnit.test('Check if borderRadius is correct according to user settings', function (assert) {
-    const chart = new Highcharts.Chart('container', {
-            chart: {
-                type: 'heatmap'
-            },
-
-            series: [{
-                borderWidth: 1,
-                data: [
-                    [0, 0, 10],
-                    [0, 1, 19],
-                    [0, 2, 8],
-                    [0, 3, 24]
-                ],
-                borderRadius: 39
-            }]
-        }),
-        point = chart.series[0].points[0],
-        setState = Highcharts.seriesTypes.heatmap.prototype
-            .pointClass.prototype.setState;
 
     setState.call(point, '');
     assert.strictEqual(
-        point.graphic.r,
+        point.graphic.d.split(' ')[1] - point.graphic.getBBox().x,
         point.series.options.borderRadius,
         'Border radius should have the value set in options on state:normal'
     );
 
     setState.call(point, 'hover');
     assert.strictEqual(
-        point.graphic.r,
+        point.graphic.d.split(' ')[1] - point.graphic.getBBox().x,
         point.series.options.borderRadius,
         'Border radius should have the value set in options on state:hover'
     );
 
-    setState.call(point, '');
     assert.strictEqual(
-        point.graphic.r,
+        point.graphic.d.split(' ')[1] - point.graphic.getBBox().x,
         point.series.options.borderRadius,
         'Border radius should have the value set in options on state:normal'
     );
