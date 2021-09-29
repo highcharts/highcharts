061acb7d69a000f011266e241138de91a8b37f9a
Pawel Lysy <pawel.lysy@blacklabel.pl>
2021-09-29 16:24:51 +0200

    Created test for #16234.

--- test/cypress/integration/Highcharts/stock-tools.spec.js
+++ test/cypress/integration/Highcharts/stock-tools.spec.js
@@ -181,3 +181,26 @@ describe('An indicator on indicator, #15696.', () => {
             .should('not.contain', 'SMA (14)')
     });
 });
+
+describe('Popup tests, #16234', () => {
+    beforeEach(() => {
+        cy.viewport(1000, 500);
+    });
+
+    before(() => {
+        cy.visit('highcharts/annotations/gui/');
+    });
+    it('Should create Circle Annotation, and check if there are label options', () => {
+
+        cy.get('.highcharts-submenu-wrapper').invoke('show');
+        cy.get('.highcharts-circle-annotation').click();
+        cy.get('.highcharts-container')
+            .click(250, 200, { force: true })
+            .click(250, 250, { force: true });
+
+        cy.get('.highcharts-annotation').children().first().click();
+        cy.get('.highcharts-annotation-edit-button').click();
+
+        cy.contains('Label options').should('not.exist');
+    });
+});
