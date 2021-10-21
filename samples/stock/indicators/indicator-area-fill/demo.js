5991de21e3c01ae8ebfa0993d5c6140a898f3e93
Pawel Lysy <pawel.lysy@blacklabel.pl>
2021-10-21 14:36:33 +0200

    Created demo for areaFill.

---
+++ samples/stock/indicators/indicator-area-fill/demo.js
@@ -0,0 +1,34 @@
+Highcharts.getJSON('https://demo-live-data.highcharts.com/aapl-ohlc.json', function (data) {
+
+    Highcharts.stockChart('container', {
+
+        rangeSelector: {
+            selected: 2
+        },
+
+        title: {
+            text: 'AAPL Stock Price'
+        },
+
+        legend: {
+            enabled: true
+        },
+
+        plotOptions: {
+            series: {
+                showInLegend: true
+            }
+        },
+
+        series: [{
+            type: 'ohlc',
+            id: 'aapl',
+            name: 'AAPL Stock Price',
+            data: data
+        }, {
+            type: 'bb',
+            linkedTo: 'aapl',
+            fillColor: 'rgba(200, 150, 0, 0.3)'
+        }]
+    });
+});
