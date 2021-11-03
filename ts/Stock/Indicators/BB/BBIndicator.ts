11fc44620d208834579353c78e2429995ff6feb4
Pawel Lysy <pawel.lysy@blacklabel.pl>
2021-11-03 15:34:54 +0100

    Fixed typo.

--- ts/Stock/Indicators/BB/BBIndicator.ts
+++ ts/Stock/Indicators/BB/BBIndicator.ts
@@ -110,7 +110,7 @@ class BBIndicator extends SMAIndicator {
     public static defaultOptions: BBOptions = merge(SMAIndicator.defaultOptions, {
 
         /**
-         * Option for fill color between lines in Boiliger Bands Indicator.
+         * Option for fill color between lines in Bollinger Bands Indicator.
          *
          * @sample {highstock} stock/indicators/indicator-area-fill
          *      Background fill between lines.
