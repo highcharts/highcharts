6a2a187de432c02e28e1f828f1ce3fd374b6f884
bre1470 <40056287+bre1470@users.noreply.github.com>
2021-11-05 10:36:33 +0100

    Fixed #16549; Restored color map mixin to fix point event listener.

--- ts/Extensions/Boost/WGLRenderer.ts
+++ ts/Extensions/Boost/WGLRenderer.ts
@@ -14,7 +14,7 @@
 
 import type Axis from '../../Core/Axis/Axis';
 import type Chart from '../../Core/Chart/Chart';
-import type ColorMapComposition from '../../Series/ColorMapComposition';
+import type ColorMapMixin from '../../Series/ColorMapMixin';
 import type ColorString from '../../Core/Color/ColorString';
 import type Point from '../../Core/Series/Point';
 import type PositionObject from '../../Core/Renderer/PositionObject';
@@ -597,8 +597,8 @@ function GLRenderer(
                         point.shapeArgs;
 
                     pointAttr = chart.styledMode ?
-                        (point.series as ColorMapComposition.SeriesComposition)
-                            .colorAttribs(point as ColorMapComposition.PointComposition) :
+                        (point.series as ColorMapMixin.ColorMapSeries)
+                            .colorAttribs(point as ColorMapMixin.ColorMapPoint) :
                         pointAttr = point.series.pointAttribs(point);
 
                     swidth = pointAttr['stroke-width'] || 0;
