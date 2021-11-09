16ad25a55b6214d7336c8aad5c0356cdaad5204b
bre1470 <40056287+bre1470@users.noreply.github.com>
2021-11-09 13:07:28 +0100

    Tools: Removed API docs from product-distribution folders.

--- tools/gulptasks/jsdoc-zips.js
+++ tools/gulptasks/jsdoc-zips.js
@@ -1,18 +1,13 @@
 // Libraries sorted by require path
 const fs = require('fs');
 const gulp = require('gulp');
+const path = require('path');
 const zip = require('gulp-zip');
-const { promisify } = require('util');
 const log = require('./lib/log');
 
-/* Promisify utility functions. NOTE: When configured Node.js version is
->=11.14.0, switch to require().promises. */
-const copyFile = promisify(fs.copyFile);
-const { existsSync } = fs;
-
 // Constants
-const DIST_BUILD = 'build';
-const DIST_DIR = 'build/dist';
+const DIR_BUILD = 'build';
+const DIR_ZIPS = 'build/api/zips';
 const DIR_API = 'build/api';
 const DIR_LIB = 'tools/gulptasks/lib/api';
 
@@ -24,7 +19,10 @@ const DIR_LIB = 'tools/gulptasks/lib/api';
  */
 function copyLibFiles() {
     const promises = ['server.js', 'README.md'].map(
-        filename => copyFile(`${DIR_LIB}/${filename}`, `${DIR_API}/${filename}`)
+        filename => fs.promises.copyFile(
+            `${DIR_LIB}/${filename}`,
+            `${DIR_API}/${filename}`
+        )
     );
     return Promise.all(promises);
 }
@@ -32,20 +30,25 @@ function copyLibFiles() {
 /**
  * Creates a zip archive for a single product API.
  *
- * @param {string} product The product folder name.
+ * @param {Record<string,string>} distPath
+ * Product folder path.
+ *
+ * @param {Record<string,string>} zipName
+ * Product zip file.
+ *
  * @return {Promise<void>} Returns a Promise that resolves when the zip archive
  * is built.
  */
-function zipProductAPI(product) {
+function zipProductAPI(distPath, zipName) {
     const DIR_CLASS_REFERENCE = `${DIR_API}/class-reference`;
 
-    const DIR_PRODUCT = `${DIR_API}/${product}`;
-    if (!existsSync(DIR_PRODUCT)) {
+    const DIR_PRODUCT = path.join(DIR_API, distPath);
+
+    if (!fs.existsSync(DIR_PRODUCT)) {
         return Promise.reject(new Error(`Missing folder: ${DIR_PRODUCT}. Has the other dist tasks been run in advance?`));
     }
 
-    const zipFileName = `${product}/api.zip`;
-    log.message(`Zipping file ${zipFileName}`);
+    log.message(`Zipping file ${zipName}`);
 
     return new Promise((resolve, reject) => {
         gulp.src([
@@ -53,9 +56,11 @@ function zipProductAPI(product) {
             `${DIR_CLASS_REFERENCE}/**`,
             `${DIR_API}/server.js`,
             `${DIR_API}/README.md`
-        ], { base: DIST_BUILD })
-            .pipe(zip(zipFileName))
-            .pipe(gulp.dest(DIST_DIR))
+        ], {
+            base: DIR_BUILD
+        })
+            .pipe(zip(zipName))
+            .pipe(gulp.dest(DIR_ZIPS))
             .on('error', reject)
             .on('end', resolve);
     });
@@ -67,27 +72,48 @@ function zipProductAPI(product) {
  * @return {Promise<void>} Returns a Promise that resolves when all the zip
  * archives is built.
  */
-function createProductsAPIArchives() {
+function jsdocZips() {
+
+    const properties = require('../../build-properties.json');
+    const { products } = properties;
+    let { version } = properties;
+    if (version.endsWith('-modified')) {
+        version = version.substr(0, version.length - 9);
+    }
 
     // Check if the class references exists
     const DIR_CLASS_REFERENCE = `${DIR_API}/class-reference`;
-    if (!existsSync(DIR_CLASS_REFERENCE)) {
-        return Promise.reject(new Error(`Missing folder: ${DIR_CLASS_REFERENCE}. Has the other dist tasks been run in advance?`));
+    if (!fs.existsSync(DIR_CLASS_REFERENCE)) {
+        return Promise.reject(new Error(
+            `Missing folder: ${DIR_CLASS_REFERENCE}. Has the other dist tasks been run in advance?`
+        ));
     }
 
     // Copy lib files, then zip all product api archives
-    return copyLibFiles().then(() => {
-        const properties = require('../../build-properties.json');
-        const fileNames = Object.keys(properties.products).map(p => properties.products[p].distpath);
-        const zipTasks = fileNames.map(zipProductAPI);
-        return Promise.all(zipTasks);
-    });
+    let promiseChain = copyLibFiles();
+
+    Object
+        .keys(products)
+        .forEach(name => {
+            promiseChain = promiseChain.then(() => zipProductAPI(
+                products[name].distpath,
+                `${name.replace(/ /gu, '-')}-${version}-API.zip`
+            ));
+        });
+
+    return promiseChain;
 }
 
+/* *
+ *
+ *  Tasks
+ *
+ * */
 
 require('./jsdoc');
-gulp.task('dist-api', gulp.series(
+
+gulp.task('jsdoc-zips', gulp.series(
     'jsdoc',
     'test-tree',
-    createProductsAPIArchives
+    jsdocZips
 ));
