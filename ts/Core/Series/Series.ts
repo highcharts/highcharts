214cbcbd6eee220df8121b0796351383d4c309be
raf18seb <raf18seb@gmail.com>
2021-11-16 15:15:06 +0100

    Addition to #11636, improved fix, moved code to MapSeries.

--- ts/Core/Series/Series.ts
+++ ts/Core/Series/Series.ts
@@ -4129,8 +4129,6 @@ class Series {
         options: DeepPartial<SeriesTypeOptions>,
         redraw?: boolean
     ): void {
-        // Save full mapData before options cleaning below
-        const mapData = options && options.mapData;
 
         options = cleanRecursively(options, this.userOptions);
 
@@ -4221,19 +4219,7 @@ class Series {
                 preserve.push(key + 'Data');
             });
 
-            let hasMapDataChanged;
-
-            // When updating the series.mapData, current value is required
-            // to be set before series.setData(), #11636
-            if (mapData) {
-                hasMapDataChanged = series.options.mapData !== mapData;
-
-                if (hasMapDataChanged) {
-                    series.options.mapData = mapData;
-                }
-            }
-
-            if (options.data || hasMapDataChanged) {
+            if (options.data) {
                 // setData uses dataSorting options so we need to update them
                 // earlier
                 if (options.dataSorting) {
