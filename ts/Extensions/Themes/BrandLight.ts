e3e3379e8360cb59898c6eff13bef8c61a38752e
Nancy Dillon <nanc.dillon@gmail.com>
2021-11-16 07:32:45 -0500

    Update BrandLight.ts
    fixed line with error

--- ts/Extensions/Themes/BrandLight.ts
+++ ts/Extensions/Themes/BrandLight.ts
@@ -43,7 +43,8 @@ namespace BrandLightTheme {
      * */
 
     export const options: DeepPartial<Options> = {
-        colors: ['#8087E8', '#A3EDBA', '#F19E53', '#6699A1', '#E1D369', '#87B4E7', '#DA6D85', '#BBBAC5'],
+        colors: ['#8087E8', '#A3EDBA', '#F19E53', '#6699A1',
+                 '#E1D369', '#87B4E7', '#DA6D85', '#BBBAC5'],
         chart: {
             backgroundColor: '#f0f0f0',
             style: {
