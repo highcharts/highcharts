552f3c2adfc2648d586073dca780f2a58b757cd4
Nancy Dillon <nanc.dillon@gmail.com>
2021-11-16 07:38:28 -0500

    Update BrandDark.ts
    fixed indentation

--- ts/Extensions/Themes/BrandDark.ts
+++ ts/Extensions/Themes/BrandDark.ts
@@ -44,7 +44,7 @@ namespace BrandDarkTheme {
 
     export const options: DeepPartial<Options> = {
         colors: ['#8087E8', '#A3EDBA', '#F19E53', '#6699A1',
-                 '#E1D369', '#87B4E7', '#DA6D85', '#BBBAC5'],
+            '#E1D369', '#87B4E7', '#DA6D85', '#BBBAC5'],
 
         chart: {
             backgroundColor: {
