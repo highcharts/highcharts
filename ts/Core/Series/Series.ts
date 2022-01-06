dcbefc32ce5332814dbeb2efdc5db09517400637
bre1470 <40056287+bre1470@users.noreply.github.com>
2022-01-06 12:52:25 +0100

    Fixed typing of Point.x and MockPoint.x.

--- ts/Core/Series/Series.ts
+++ ts/Core/Series/Series.ts
@@ -2325,7 +2325,6 @@ class Series {
             // some API data
             point.category = (
                 categories &&
-                point.x !== null &&
                 typeof categories[point.x] !== 'undefined' ?
                     categories[point.x] :
                     point.x as any
