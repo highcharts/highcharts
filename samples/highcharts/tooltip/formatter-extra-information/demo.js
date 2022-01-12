750133e40714e22b22495cca7ef1af59c436c6f7
Karol Kołodziej <karolkolodziej01@gmail.com>
2022-01-12 15:04:43 +0100

    Improved demo- added second series and custom information.

--- samples/highcharts/tooltip/formatter-extra-information/demo.js
+++ samples/highcharts/tooltip/formatter-extra-information/demo.js
@@ -3,8 +3,13 @@ Highcharts.chart('container', {
     tooltip: {
         formatter: function () {
             const basicInformation = 'The value for <b>' + this.x +
-            '</b> is <b>' + this.y + '</b>',
-                extraInformation = this.point.extraInfrmation || '';
+                '</b> is <b>' + this.y + '</b>',
+                firstSeriesExtra = this.point.extraInfrmation || '',
+                custom = this.series.options.custom,
+                secondSeriesExtra = custom ? custom.extraTooltipInformation[this.point.x] : '',
+                extraInformation = this.series.name === 'Series 1' ?
+                    firstSeriesExtra :
+                    `Some information: ${secondSeriesExtra} %`;
 
             return basicInformation + '<br>' + extraInformation;
         }
@@ -38,5 +43,22 @@ Highcharts.chart('container', {
             95.6,
             54.4
         ]
+    }, {
+        data: [
+            194.1,
+            95.6,
+            54.4,
+            29.9,
+            71.5,
+            106.4,
+            129.2,
+            144.0,
+            176.0,
+            135.6,
+            148.5,
+            216.4],
+        custom: {
+            extraTooltipInformation: [13, 1, 4, 5, -6, 8, 9, -1, 3, 1, 4, -2]
+        }
     }]
 });
