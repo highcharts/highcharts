015d50c6622b52f92f99e95a42d0fff3ebd1bacf
Karol Kołodziej <karolkolodziej01@gmail.com>
2022-01-12 15:30:13 +0100

    Simplified demo, moved extra information to custom object.

--- samples/highcharts/tooltip/formatter-extra-information/demo.js
+++ samples/highcharts/tooltip/formatter-extra-information/demo.js
@@ -4,12 +4,7 @@ Highcharts.chart('container', {
         formatter: function () {
             const basicInformation = 'The value for <b>' + this.x +
                 '</b> is <b>' + this.y + '</b>',
-                firstSeriesExtra = this.point.extraInfrmation || '',
-                custom = this.series.options.custom,
-                secondSeriesExtra = custom ? custom.extraTooltipInformation[this.point.x] : '',
-                extraInformation = this.series.name === 'Series 1' ?
-                    firstSeriesExtra :
-                    `Some information: ${secondSeriesExtra} %`;
+                extraInformation = this.point.custom && this.point.custom.extraInformation || '';
 
             return basicInformation + '<br>' + extraInformation;
         }
@@ -24,7 +19,9 @@ Highcharts.chart('container', {
             29.9,
             {
                 y: 71.5,
-                extraInfrmation: 'First month when XYZ introduced.'
+                custom: {
+                    extraInformation: 'First month when XYZ introduced.'
+                }
             },
             106.4,
             129.2,
@@ -32,33 +29,20 @@ Highcharts.chart('container', {
             176.0,
             {
                 y: 135.6,
-                extraInfrmation: 'The XYZ was missing.'
+                custom: {
+                    extraInformation: 'The XYZ was missing.'
+                }
             },
             148.5,
             216.4,
             {
                 y: 194.1,
-                extraInfrmation: 'Changes to the XYZ introduced.'
+                custom: {
+                    extraInformation: 'Changes to the XYZ introduced.'
+                }
             },
             95.6,
             54.4
         ]
-    }, {
-        data: [
-            194.1,
-            95.6,
-            54.4,
-            29.9,
-            71.5,
-            106.4,
-            129.2,
-            144.0,
-            176.0,
-            135.6,
-            148.5,
-            216.4],
-        custom: {
-            extraTooltipInformation: [13, 1, 4, 5, -6, 8, 9, -1, 3, 1, 4, -2]
-        }
     }]
 });
