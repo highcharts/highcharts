440391d25a2658e52c7c55bb9b5db718c4753baa
raf18seb <raf18seb@gmail.com>
2022-01-17 12:46:07 +0100

    Fixed #16045, tooltip with opacity did't hide properly.

--- ts/Core/Renderer/SVG/SVGElement.ts
+++ ts/Core/Renderer/SVG/SVGElement.ts
@@ -547,7 +547,7 @@ class SVGElement implements SVGElementLike {
                 }
             }, deferTime);
         } else {
-            this.attr(params, void 0, complete);
+            this.attr(params, void 0, complete || animOptions.complete);
             // Call the end step synchronously
             objectEach(params, function (val: any, prop: string): void {
                 if (animOptions.step) {
