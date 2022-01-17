2ec55b2aa98622c6916bbf3a4968c0cd4be72e5c
Øystein Moseng <oystein@highsoft.com>
2022-01-17 13:04:44 +0100

    Fixed #16339, kbd focus management issues with fullscreen change.

--- ts/Accessibility/Components/MenuComponent.ts
+++ ts/Accessibility/Components/MenuComponent.ts
@@ -189,8 +189,25 @@ class MenuComponent extends AccessibilityComponent {
      * proxy overlay.
      */
     public onChartRender(): void {
+        const chart = this.chart,
+            focusEl = chart.focusElement,
+            a11y = chart.accessibility;
         this.proxyProvider.clearGroup('chartMenu');
         this.proxyMenuButton();
+
+        if (
+            this.exportButtonProxy &&
+            focusEl &&
+            focusEl === chart.exportingGroup
+        ) {
+            if (focusEl.focusBorder) {
+                chart.setFocusToElement(
+                    focusEl, this.exportButtonProxy.buttonElement
+                );
+            } else if (a11y) {
+                a11y.keyboardNavigation.tabindexContainer.focus();
+            }
+        }
     }
 
 
