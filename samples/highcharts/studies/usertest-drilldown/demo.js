98ea3e7b527f8a4a892aa014217d21cafbf791b6
Øystein Moseng <oystein@highsoft.com>
2022-03-01 10:51:29 +0100

    Updated drilldown user test study.

--- samples/highcharts/studies/usertest-drilldown/demo.js
+++ samples/highcharts/studies/usertest-drilldown/demo.js
@@ -36,20 +36,11 @@ function makeLinkWraps(chart, role, desc) {
 }
 
 Highcharts.addEvent(Highcharts.Chart.prototype, 'render', function () {
+    const desc = this.container.getElementsByTagNameNS('http://www.w3.org/2000/svg', 'desc')[0];
+    desc.textContent = 'Created with Highcharts';
+
     setTimeout(() => {
-        switch (this.container.parentNode.id) {
-        case 'containerA':
-            makeLinkWraps(this, null, 'Expandable datapoint');
-            break;
-        case 'containerB':
-            makeLinkWraps(this, null, 'Clickable datapoint');
-            break;
-        case 'containerC':
-            makeLinkWraps(this, 'button');
-            break;
-        default:
-            break;
-        }
+        makeLinkWraps(this, 'button');
     }, 1);
 });
 
@@ -77,18 +68,15 @@ function announce(text) {
 
 Highcharts.addEvent(Highcharts.Chart, 'afterApplyDrilldown', function () {
     const series = this.series[0];
-    series.group.element.focus();
-    announce('Navigated to details for ' + series.name + '. New bar series with ' + series.points.length + ' bars.');
+    announce('Navigated to details for ' + series.name + '. New bar series with ' + series.points.length + ' bars. Back to Country exports button.');
 
     // Fix drillup btn
     setTimeout(() => {
         const chart = this;
         this.accessibility.components.zoom.drillUpProxyButton.remove();
-        const btnGroup = this.drillUpButton.element;
-        const parentGroup = btnGroup.parentNode;
-        parentGroup.setAttribute('aria-hidden', false);
-        btnGroup.setAttribute('aria-hidden', true);
+        this.drillUpButton.element.setAttribute('aria-hidden', true);
 
+        const parentGroup = series.group.element.parentNode;
         const link = chart.drillUpLink = document.createElementNS('http://www.w3.org/2000/svg', 'a');
 
         link.setAttribute('tabindex', '-1');
@@ -100,8 +88,8 @@ Highcharts.addEvent(Highcharts.Chart, 'afterApplyDrilldown', function () {
             chart.drillUp();
         });
 
-        parentGroup.appendChild(link);
-        link.appendChild(btnGroup);
+        parentGroup.insertBefore(link, series.group.element);
+        link.focus();
     }, 10);
 });
 
@@ -122,149 +110,150 @@ document.addEventListener('keydown', function (e) {
     }
 });
 
-function makeChart(container, title, expandableLabel) {
-    return Highcharts.chart(container, {
-        chart: {
-            type: 'bar'
-        },
+Highcharts.chart('container', {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+        text: 'Exports by country'
+    },
+    subtitle: {
+        text: 'Click each country for details'
+    },
+    xAxis: {
+        type: 'category'
+    },
+    yAxis: {
         title: {
-            text: 'Exports by country - ' + title
-        },
-        subtitle: {
-            text: 'Click each country for details'
-        },
-        xAxis: {
-            type: 'category',
-            accessibility: {
-                description: 'Countries'
-            }
-        },
-        yAxis: {
-            title: {
-                text: 'Billion USD'
-            }
-        },
-        accessibility: {
-            announceNewData: {
-                enabled: false
-            },
-            landmarkVerbosity: 'disabled',
-            screenReaderSection: {
-                beforeChartFormat: '<div>{chartTitle}, {typeDescription}</div><div>{xAxisDescription}</div><div>{yAxisDescription}</div>'
-            },
-            point: {
-                descriptionFormatter: function (point) {
-                    let label = point.name + ', ' + point.y + ' billion USD.';
-                    if (point.series.name === 'Country exports' && expandableLabel) {
-                        label += ' Click to load detailed data.';
-                    }
-                    return label;
-                }
-            },
-            series: {
-                describeSingleSeries: true
-            }
-        },
-        legend: {
+            text: 'Billion USD'
+        }
+    },
+    accessibility: {
+        announceNewData: {
             enabled: false
         },
-        plotOptions: {
-            series: {
-                borderWidth: 0,
-                dataLabels: {
-                    enabled: true
-                }
+        landmarkVerbosity: 'disabled',
+        screenReaderSection: {
+            beforeChartFormat: '<div>{chartTitle}, {typeDescription}</div><div>{xAxisDescription}</div><div>{yAxisDescription}</div>',
+            beforeChartFormatter: function (chart) {
+                const chartTitle = chart.options.title.text;
+                const typeDesc = 'Bar chart with ' + chart.series[0].points.length + ' bars.';
+                const xAxisDescription = 'The chart has 1 X axis displaying ' +
+                    (chart.series[0].name === 'Country exports' ? 'countries.' : 'export categories.');
+                const yAxisDescription = 'The chart has 1 Y axis displaying billion USD. Data ranges from ' +
+                    chart.yAxis[0].dataMin + ' billion USD to ' + chart.yAxis[0].dataMax + ' billion USD.';
+                return `<div>${chartTitle}, ${typeDesc}</div><div>${xAxisDescription}</div><div>${yAxisDescription}</div>`; // eslint-disable-line
             }
         },
-        tooltip: {
-            backgroundColor: 'rgba(255, 255, 255, 0.9)',
-            valueSuffix: ' billion USD'
+        point: {
+            descriptionFormatter: function (point) {
+                let label = point.name + ', ' + point.y + ' billion USD.';
+                if (point.series.name === 'Country exports') {
+                    label += ' Click to load detailed data.';
+                }
+                return label;
+            }
+        },
+        series: {
+            describeSingleSeries: true
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    plotOptions: {
+        series: {
+            accessibility: {
+                description: 'Currently at bottom level.'
+            },
+            borderWidth: 0,
+            dataLabels: {
+                enabled: true
+            }
+        }
+    },
+    tooltip: {
+        backgroundColor: 'rgba(255, 255, 255, 0.9)',
+        valueSuffix: ' billion USD'
+    },
+    series: [{
+        name: 'Country exports',
+        colorByPoint: true,
+        accessibility: {
+            description: 'Currently at top level.'
+        },
+        data: [{
+            name: 'China',
+            y: 2591,
+            drilldown: 'china'
+        }, {
+            name: 'USA',
+            y: 1431,
+            drilldown: 'usa'
+        }, {
+            name: 'Germany',
+            y: 1378,
+            drilldown: 'germany'
+        }, {
+            name: 'Turkey',
+            y: 169.5,
+            drilldown: 'turkey'
+        }]
+    }],
+    drilldown: {
+        breadcrumbs: {
+            showFullPath: false,
+            floating: true,
+            buttonTheme: {
+                fill: '#f7f7f7',
+                padding: 8,
+                stroke: '#cccccc',
+                'stroke-width': 1
+            },
+            position: {
+                align: 'right'
+            }
         },
         series: [{
-            name: 'Country exports',
-            colorByPoint: true,
-            accessibility: {
-                point: {
-                    valueDescriptionFormat: '{xDescription}{separator}{value}. Expandable.'
-                }
-            },
-            data: [{
-                name: 'China',
-                y: 2591,
-                drilldown: 'china'
-            }, {
-                name: 'USA',
-                y: 1431,
-                drilldown: 'usa'
-            }, {
-                name: 'Germany',
-                y: 1378,
-                drilldown: 'germany'
-            }, {
-                name: 'Turkey',
-                y: 169.5,
-                drilldown: 'turkey'
-            }]
-        }],
-        drilldown: {
-            breadcrumbs: {
-                showFullPath: false,
-                floating: true,
-                buttonTheme: {
-                    fill: '#f7f7f7',
-                    padding: 8,
-                    stroke: '#cccccc',
-                    'stroke-width': 1
-                },
-                position: {
-                    align: 'right'
-                }
-            },
-            series: [{
-                id: 'china',
-                name: 'China',
-                data: [
-                    ['Phone system devices', 223.2],
-                    ['Computers and optical readers', 170.2],
-                    ['Integrated circuits', 117.1],
-                    ['Miscellaneous articles', 55.2],
-                    ['Lamps and lighting', 37.6]
-                ]
-            }, {
-                id: 'usa',
-                name: 'USA',
-                data: [
-                    ['Oil and gas', 144.3],
-                    ['Cars and car parts', 78.9],
-                    ['Integrated circuits', 44.2],
-                    ['Phone system devices', 28.1],
-                    ['Electro-medical devices', 28.0]
-                ]
-            }, {
-                id: 'germany',
-                name: 'Germany',
-                data: [
-                    ['Cars and car parts', 176.4],
-                    ['Medication', 60.1],
-                    ['Blood fractions', 31.8],
-                    ['Aircraft and spacecraft', 20.7],
-                    ['Electro-medical devices', 16.7]
-                ]
-            }, {
-                id: 'turkey',
-                name: 'Turkey',
-                data: [
-                    ['Cars and car parts', 16.3],
-                    ['Trucks', 4.4],
-                    ['Oil and gas', 6.1],
-                    ['Jewelry', 5.1],
-                    ['Gold', 2.0]
-                ]
-            }]
-        }
-    });
-}
-
-makeChart('containerA', 'version A');
-makeChart('containerB', 'version B');
-makeChart('containerC', 'version C', true);
+            id: 'china',
+            name: 'China',
+            data: [
+                ['Phone system devices', 223.2],
+                ['Computers and optical readers', 170.2],
+                ['Integrated circuits', 117.1],
+                ['Miscellaneous articles', 55.2],
+                ['Lamps and lighting', 37.6]
+            ]
+        }, {
+            id: 'usa',
+            name: 'USA',
+            data: [
+                ['Oil and gas', 144.3],
+                ['Cars and car parts', 78.9],
+                ['Integrated circuits', 44.2],
+                ['Phone system devices', 28.1],
+                ['Electro-medical devices', 28.0]
+            ]
+        }, {
+            id: 'germany',
+            name: 'Germany',
+            data: [
+                ['Cars and car parts', 176.4],
+                ['Medication', 60.1],
+                ['Blood fractions', 31.8],
+                ['Aircraft and spacecraft', 20.7],
+                ['Electro-medical devices', 16.7]
+            ]
+        }, {
+            id: 'turkey',
+            name: 'Turkey',
+            data: [
+                ['Cars and car parts', 16.3],
+                ['Trucks', 4.4],
+                ['Oil and gas', 6.1],
+                ['Jewelry', 5.1],
+                ['Gold', 2.0]
+            ]
+        }]
+    }
+});
