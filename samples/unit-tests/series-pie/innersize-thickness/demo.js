028c553b32bf5c4b04e7c2474dc30b90e4cbae42
raf18seb <raf18seb@gmail.com>
2022-03-15 11:28:46 +0100

    Cleaning up and merging 3 tests into 1.

--- samples/unit-tests/series-pie/innersize-thickness/demo.js
+++ samples/unit-tests/series-pie/innersize-thickness/demo.js
@@ -1,29 +1,44 @@
-QUnit.test('Thickness cannot be greater than size.(#6647)', assert => {
-    let clicks = 0;
-    const chart = new Highcharts.chart('container', {
-        chart: {
-            type: 'pie'
-        },
-        plotOptions: {
-            pie: {
-                events: {
-                    click: () => {
-                        clicks++;
-                    }
-                }
-            }
-        },
+QUnit.test('Pie: innerSize and thickness', assert => {
+    const chart = Highcharts.chart('container', {
         series: [
             {
-                data: [
-                    ['Firefox', 44.2],
-                    ['IE7', 26.6],
-                    ['IE6', 20],
-                    ['Chrome', 3.1],
-                    ['Other', 5.4]
-                ]
+                type: 'pie',
+                innerSize: 200,
+                data: [1, 2, 3, 4, 5, 6]
             }
         ]
+    });
+
+    const series = chart.series[0],
+        center = series.center;
+
+    assert.strictEqual(
+        center[3] > center[2],
+        false,
+        'The innerSize should not be greater than size, #3623.'
+    );
+
+    series.update({
+        innerSize: '50%'
+    });
+
+    const innerSize = series.options.innerSize;
+
+    assert.strictEqual(
+        series.center[3],
+        (series.center[2] * parseInt(innerSize, 10)) / 100,
+        'Inner size should be 50% of outer size'
+    );
+
+    let clicks = 0;
+
+    series.update({
+        innerSize: undefined,
+        events: {
+            click: () => {
+                clicks++;
+            }
+        }
     });
 
     const controller = new TestController(chart);
@@ -39,30 +54,32 @@ QUnit.test('Thickness cannot be greater than size.(#6647)', assert => {
     );
 
     assert.strictEqual(
-        clicks, 1, 'Clicking on a side of a pie slice should fire click event.'
+        clicks,
+        1,
+        'Clicking on a side of a pie slice should fire click event.'
     );
 
     chart.series[0].update({
-        thickness: 20,
+        thickness: 20
     });
 
     assert.strictEqual(
-        clicks, 1, 'Clicking on a empty field should not incrase the clicks.'
+        clicks,
+        1,
+        'Clicking on a empty field should not incrase the clicks variable.'
     );
 
-    const series = chart.series[0],
-        thickness = series.options.thickness,
-        size = series.center[2];
+    const thickness = series.options.thickness;
 
     assert.strictEqual(
         thickness,
         series.points[0].shapeArgs.r - series.points[0].shapeArgs.innerR,
-        'Thickness should be same as points distance.'
+        'Thickness should be same as points length.'
     );
 
     assert.strictEqual(
         thickness + series.points[0].shapeArgs.innerR,
-        size / 2,
+        series.center[2] / 2,
         'Thickness should not be greater than size.'
     );
 
@@ -84,15 +101,43 @@ QUnit.test('Thickness cannot be greater than size.(#6647)', assert => {
         series.points[0].shapeArgs.innerR,
         15,
         'It should be equal to half innerSize.'
-    )
+    );
 
     series.update({
-        thickness: 20,
+        thickness: 20
     });
-    
+
     assert.notEqual(
         series.points[0].shapeArgs.innerR,
         15,
-        'It should not be equal to half innerSize.'
-    )
+        'Thickness should overwrite innerSize.'
+    );
 });
+
+QUnit.test(
+    'The inner size of a pie with an additional gauge series (#13629).',
+    assert => {
+        const chart = Highcharts.chart('container', {
+            series: [
+                {
+                    type: 'gauge',
+                    data: [10]
+                },
+                {
+                    type: 'pie',
+                    innerSize: '50%',
+                    startAngle: -90,
+                    endAngle: 90,
+                    center: ['50%', '60%'],
+                    data: [1, 2, 3]
+                }
+            ]
+        });
+
+        assert.notEqual(
+            chart.series[1].center[3],
+            0,
+            'The innerSize is not equal 0.'
+        );
+    }
+);
