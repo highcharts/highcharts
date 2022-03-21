7655a9a65499d9b2591b7d00f11951c12f6ed6fe
Pawel Lysy <pawel.lysy@blacklabel.pl>
2022-03-21 11:41:52 +0100

    Improved recalculations of the axes, #15735.

--- test/cypress/integration/Highcharts/stock-tools/indicators/add-remove-indicator.spec.js
+++ test/cypress/integration/Highcharts/stock-tools/indicators/add-remove-indicator.spec.js
@@ -1,48 +1,3 @@
-
-// describe('Adding custom indicator on a separate axis through indicator popup, #15804.', () => {
-//     beforeEach(() => {
-//         cy.viewport(1000, 500);
-//     });
-
-//     before(() => {
-//         cy.visit('/stock/demo/stock-tools-gui');
-//     });
-
-//     it('#15730: Should close popup after hiding annotation', () => {
-//         // Add custom indicator which should use another axis.
-//         cy.window().then((win) => {
-//             const H = win.Highcharts,
-//                 bindingsUtils = H._modules['Extensions/Annotations/NavigationBindings.js'].prototype.utils;
-
-//             H.seriesType(
-//                 'customIndicatorBasedOnRSI',
-//                 'rsi', {
-//                 name: 'Custom Indicator',
-//                 color: 'red'
-//                 }, {
-
-//                 }
-//             );
-//             bindingsUtils.indicatorsWithAxes.push('customIndicatorBasedOnRSI');
-//         });
-
-//         cy.openIndicators()
-//         cy.get('.highcharts-indicator-list')
-//             .contains('CUSTOMINDICATORBASEDONRSI')
-//             .click();
-//         cy.addIndicator();
-
-//         cy.chart().should(chart =>
-//             assert.strictEqual(
-//                 chart.yAxis.length,
-//                 4,
-//                 `After adding a custom indicator that is based on other oscillators,
-//                 another axis should be added.`
-//             )
-//         );
-//     });
-// });
-
 describe('Axis height recalculation.', () => {
     beforeEach(() => {
         cy.viewport(1000, 500);
@@ -96,7 +51,39 @@ describe('Axis height recalculation.', () => {
             .eq(2)
             .click(); // Remove that indicator.
 
-        cy.chart().should(chart =>{
+        cy.chart().should(chart => {
+            // Exclude 2 first axes and navigator axis.
+            const yAxis = chart.yAxis;
+
+            for(let i = 3; i < chart.yAxis.length - 1; i++) {
+                assert.closeTo(
+                    yAxis[i].top + yAxis[i].height,
+                    yAxis[i + 1].top,
+                    1,
+                    `The end of the ${i + 1} axis should be close to beginning of the ${i + 2} axis.`
+                )
+            }
+        });
+
+        cy.chart().should(chart => {
+            const yAxis = chart.yAxis[0];
+            yAxis.update({
+                top: yAxis.top + 30,
+                height: yAxis.height - 30
+            });
+        });
+        cy.openIndicators()
+        cy.get('.highcharts-tab-item')
+            .eq(1)
+            .click(); // Open EDIT bookmark.
+        cy.get('.highcharts-indicator-list')
+            .contains('ATR (30)')
+            .click();
+        cy.get('.highcharts-popup-rhs-col')
+            .children('.highcharts-popup button')
+            .eq(2)
+            .click(); // Remove that indicator.
+        cy.chart().should(chart => {
             // Exclude 2 first axes and navigator axis.
             const yAxis = chart.yAxis;
 
