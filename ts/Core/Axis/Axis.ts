c2687e42c02d06f16ffe5e44685644f2a8d5f3d8
Torstein Hønsi <torstein@highsoft.com>
2022-03-22 11:08:08 +0100

    Attempt at adjusting to tickAmount at an early stage (#17100)

--- ts/Core/Axis/Axis.ts
+++ ts/Core/Axis/Axis.ts
@@ -2030,6 +2030,37 @@ class Axis {
                     this.min as any,
                     this.max as any
                 );
+                const startingTickInterval = this.tickInterval;
+                let adjustedTickInterval = this.tickInterval;
+                while (adjustedTickInterval < startingTickInterval * 2) {
+                    tickPositions = this.getLinearTickPositions(
+                        this.tickInterval,
+                        this.min as any,
+                        this.max as any
+                    );
+
+                    // If there are more tick positions than the set tickAmount,
+                    // increase the tickInterval and continue until it fits.
+                    // (#17100)
+                    if (
+                        this.tickAmount &&
+                        tickPositions.length > this.tickAmount
+                    ) {
+                        adjustedTickInterval *= 1.1;
+                        this.tickInterval = normalizeTickInterval(
+                            adjustedTickInterval,
+                            void 0,
+                            getMagnitude(adjustedTickInterval),
+                            pick(
+                                options.allowDecimals,
+                                true
+                            ),
+                            true
+                        );
+                    } else {
+                        break;
+                    }
+                }
             }
 
             // Too dense ticks, keep only the first and last (#4477)
@@ -2450,40 +2481,6 @@ class Axis {
 
                 adjustExtremes();
 
-            // We have too many ticks, run second pass to try to reduce ticks
-            } else if (currentTickAmount > tickAmount) {
-                /**
-                 * @todo #17100
-                 * - Refactor to avoid duplication with the code block in
-                 *   `setTickInterval`
-                 * - Guard for dateTime, log axis and tick interval option (like
-                 *   the original)
-                 */
-                if (
-                    !axis.dateTime &&
-                    !axis.logarithmic &&
-                    !axis.options.tickInterval
-                ) {
-                    axis.tickInterval = normalizeTickInterval(
-                        axis.tickInterval * 1.1,
-                        void 0,
-                        getMagnitude(axis.tickInterval * 1.1),
-                        pick(
-                            options.allowDecimals,
-                            // If the tick interval is greather than 0.5, avoid
-                            // decimals, as linear axes are often used to render
-                            // discrete values. #3363. If a tick amount is set,
-                            // allow decimals by default, as it increases the
-                            // chances for a good fit.
-                            axis.tickInterval < 0.5 ||
-                                this.tickAmount !== void 0
-                        ),
-                        true
-                    );
-                } else {
-                    axis.tickInterval *= 2;
-                }
-                axis.setTickPositions();
             }
 
             // The finalTickAmt property is set in getTickAmount
