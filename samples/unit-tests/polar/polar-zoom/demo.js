2c3206d7d3f6f627422069d77d5d3cf9757f49f3
Mateusz Bernacik <bm64@Mateuszs-MBP-2.localdomain>
2022-03-28 14:56:12 +0200

    Tests: Inverted Polar and semi circle, touch #3424.

--- samples/unit-tests/polar/polar-zoom/demo.js
+++ samples/unit-tests/polar/polar-zoom/demo.js
@@ -45,6 +45,8 @@ QUnit.test('Axes zoom', function (assert) {
             size: 200
         },
         yAxis: {
+            minRange: 1,
+            startOnTick: false,
             endOnTick: false,
             max: 8
         },
@@ -58,10 +60,11 @@ QUnit.test('Axes zoom', function (assert) {
         }]
     });
 
+    let minY = 4,
+        maxY = 8;
+
     const controller = new TestController(chart),
         [centerX, centerY, diameter] = chart.pane[0].center,
-        minY = 4,
-        maxY = 8,
         xPos = centerX + chart.plotLeft,
         minPosY = chart.yAxis[0].toPixels(minY) + centerY - diameter / 2,
         maxPosY = chart.yAxis[0].toPixels(maxY) + centerY - diameter / 2;
@@ -69,17 +72,19 @@ QUnit.test('Axes zoom', function (assert) {
     controller.mouseMove(xPos, minPosY);
     controller.mouseDown(xPos, minPosY);
     controller.mouseMove(xPos, maxPosY);
-    controller.mouseUp();
+    controller.mouseUp(xPos, maxPosY);
 
-    assert.strictEqual(
+    assert.close(
         chart.yAxis[0].getExtremes().min,
         minY,
+        0.1,
         `Zooming y axis from ${minY} to ${maxY} should set correct min`
     );
 
-    assert.strictEqual(
+    assert.close(
         chart.yAxis[0].getExtremes().max,
         maxY,
+        0.1,
         `Zooming y axis from ${minY} to ${maxY} should set correct max`
     );
 
@@ -89,15 +94,17 @@ QUnit.test('Axes zoom', function (assert) {
     controller.mouseMove(xPos, minPosY);
     controller.mouseUp();
 
-    assert.strictEqual(
+    assert.close(
         chart.yAxis[0].getExtremes().min,
         minY,
+        0.1,
         `Zooming y axis from ${maxY} to ${minY} should set correct min`
     );
 
-    assert.strictEqual(
+    assert.close(
         chart.yAxis[0].getExtremes().max,
         maxY,
+        0.1,
         `Zooming y axis from ${maxY} to ${minY} should set correct max`
     );
 
@@ -107,23 +114,26 @@ QUnit.test('Axes zoom', function (assert) {
         }
     });
 
-    const minX = 4,
-        maxX = 6,
-        yPos = centerY + chart.plotTop;
+    let minX = 4,
+        maxX = 6;
+
+    const yPos = centerY + chart.plotTop;
 
     controller.mouseDown(xPos, yPos + diameter / 4);
     controller.mouseMove(xPos - diameter / 4, yPos);
     controller.mouseUp();
 
-    assert.strictEqual(
+    assert.close(
         chart.xAxis[0].getExtremes().min,
         minX,
+        0.1,
         `Zooming x axis from ${minX} to ${maxX} should set correct min`
     );
 
-    assert.strictEqual(
+    assert.close(
         chart.xAxis[0].getExtremes().max,
         maxX,
+        0.1,
         `Zooming x axis from ${minX} to ${maxX} should set correct max`
     );
 
@@ -133,15 +143,17 @@ QUnit.test('Axes zoom', function (assert) {
     controller.mouseMove(xPos, yPos + diameter / 4);
     controller.mouseUp();
 
-    assert.strictEqual(
+    assert.close(
         chart.xAxis[0].getExtremes().min,
         minX,
+        0.1,
         `Zooming x axis from ${maxX} to ${minX} should set correct min`
     );
 
-    assert.strictEqual(
+    assert.close(
         chart.xAxis[0].getExtremes().max,
         maxX,
+        0.1,
         `Zooming x axis from ${maxX} to ${minX} should set correct max`
     );
 
@@ -158,17 +170,97 @@ QUnit.test('Axes zoom', function (assert) {
     controller.mouseMove(xPos, yPos + diameter / 2);
     controller.mouseUp();
 
-    assert.deepEqual(
-        [chart.xAxis[0].getExtremes().min, chart.yAxis[0].getExtremes().min],
-        [minX, minY],
-        `Zooming xy from (x1: ${maxX}, y1: ${minY})
-        to (x2: ${minX}, y2: ${maxY}) should set correct min`
+    [
+        ['xAxis', 'min', minX],
+        ['yAxis', 'min', minY],
+        ['xAxis', 'max', maxX],
+        ['yAxis', 'max', maxY]
+    ].forEach(([axisName, extremeName, expected]) => {
+        assert.close(
+            chart[axisName][0].getExtremes()[extremeName],
+            expected,
+            0.1,
+            `Zooming xy from (x1: ${maxX}, y1: ${minY})
+            to (x2: ${minX}, y2: ${maxY}) should set correct ${axisName}
+            ${extremeName}`
+        );
+    });
+
+    chart.xAxis[0].setExtremes();
+    chart.yAxis[0].setExtremes();
+
+    chart.update({
+        chart: {
+            inverted: true
+        },
+        xAxis: {
+            max: 7
+        }
+    });
+
+    minX = 0;
+    maxX = 5;
+    minY = 4;
+    maxY = 6;
+
+    controller.mouseDown(xPos - diameter / 4, yPos);
+    controller.mouseMove(xPos, yPos + diameter / 2);
+    controller.mouseUp();
+
+    [
+        ['xAxis', 'min', minX],
+        ['yAxis', 'min', minY],
+        ['xAxis', 'max', maxX],
+        ['yAxis', 'max', maxY]
+    ].forEach(([axisName, extremeName, expected]) => {
+        assert.close(
+            chart[axisName][0].getExtremes()[extremeName],
+            expected,
+            0.1,
+            `Zooming xy in inverted polar from (x1: ${maxX}, y1: ${minY})
+            to (x2: ${minX}, y2: ${maxY}) should set correct ${axisName}
+            ${extremeName}`
+        );
+    });
+
+    let isSelection;
+
+    chart.update({
+        chart: {
+            events: {
+                selection: function () {
+                    isSelection = Boolean(this.pointer.selectionMarker);
+                }
+            }
+        },
+        pane: {
+            startAngle: -90,
+            endAngle: 90
+        }
+    });
+
+    controller.mouseDown(xPos - diameter / 4, yPos + 1);
+    controller.mouseMove(xPos + diameter / 2, yPos + 1);
+    controller.mouseUp();
+
+    assert.ok(
+        !isSelection,
+        'Zoom shouldn\'t work outside top semi circle'
     );
 
-    assert.deepEqual(
-        [chart.xAxis[0].getExtremes().max, chart.yAxis[0].getExtremes().max],
-        [maxX, maxY],
-        `Zooming xy from (x1: ${maxX}, y1: ${minY})
-        to (x2: ${minX}, y2: ${maxY}) should set correct max`
+    chart.update({
+        pane: {
+            startAngle: 90,
+            endAngle: 270
+        }
+    });
+
+    controller.mouseDown(xPos - diameter / 4, yPos - 1);
+    controller.mouseMove(xPos + diameter / 2, yPos - 1);
+    controller.mouseUp();
+
+    assert.ok(
+        !isSelection,
+        'Zoom shouldn\'t work outside bottom semi circle'
     );
 });
