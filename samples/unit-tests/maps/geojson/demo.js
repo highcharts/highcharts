28c48631a75b857c5f5a4fb2e91dbb07d2ca31c6
raf18seb <raf18seb@gmail.com>
2022-04-06 19:03:59 +0200

    Improved proj4 tests to cover all cases.

--- samples/unit-tests/maps/geojson/demo.js
+++ samples/unit-tests/maps/geojson/demo.js
@@ -1,43 +1,54 @@
 QUnit.test('GeoJSON and map transforms', assert => {
 
-    // #17139
-    const proj4 = window.proj4;
-    window.proj4 = undefined;
-
     const chart = Highcharts.mapChart('container', {
         chart: {
-            map: 'custom/world',
-            proj4: proj4 // #17139
+            map: 'custom/world'
         },
         series: [{
         }]
     });
 
+    const proj4 = window.proj4;
 
     [
-        { lon: 0, lat: 0 },
-        { lon: 0, lat: 60 },
-        { lon: 90, lat: 0 },
-        { lon: 0, lat: -60 },
-        { lon: -90, lat: 0 }
-    ].forEach(lonLat => {
-        const projected = chart.fromLatLonToPoint(lonLat),
-            result = chart.fromPointToLatLon(projected);
-        assert.close(
-            result.lon,
-            lonLat.lon,
-            0.001,
-            `Roundtrip conversion of [${lonLat.lon}, ${lonLat.lat}] ` +
-                'should result in the same lon value'
-        );
-        assert.close(
-            result.lat,
-            lonLat.lat,
-            0.001,
-            `Roundtrip conversion of [${lonLat.lon}, ${lonLat.lat}] ` +
-                'should result in the same lat value'
-        );
-    });
+        'with projection from window.proj4',
+        'with proj from chart.proj4' // #17139
+    ].forEach((testDescription, iteration) => {
+        [
+            { lon: 0, lat: 0 },
+            { lon: 0, lat: 60 },
+            { lon: 90, lat: 0 },
+            { lon: 0, lat: -60 },
+            { lon: -90, lat: 0 }
+        ].forEach(lonLat => {
+            const projected = chart.fromLatLonToPoint(lonLat),
+                result = chart.fromPointToLatLon(projected);
+            assert.close(
+                result.lon,
+                lonLat.lon,
+                0.001,
+                `Roundtrip conversion of [${lonLat.lon}, ${lonLat.lat}] ` +
+                    'should result in the same lon value ' + testDescription
+            );
+            assert.close(
+                result.lat,
+                lonLat.lat,
+                0.001,
+                `Roundtrip conversion of [${lonLat.lon}, ${lonLat.lat}] ` +
+                    'should result in the same lat value ' + testDescription
+            );
+        });
 
-    window.proj4 = proj4;
+        if (iteration === 0) {
+            window.proj4 = undefined;
+
+            chart.update({
+                chart: {
+                    proj4: proj4
+                }
+            }, false);
+        } else {
+            window.proj4 = proj4;
+        }
+    });
 });
