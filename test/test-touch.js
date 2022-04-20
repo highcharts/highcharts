40fbca63fe3749ce808d52bd8ae468c2195b076f
bre1470 <40056287+bre1470@users.noreply.github.com>
2022-04-20 14:18:31 +0200

    Fixed test-touch for IE.

--- test/test-touch.js
+++ test/test-touch.js
@@ -1,8 +1,9 @@
-window.addEventListener('HighchartsModuleLoaded', function (e) {
-    var module = e.detail.module;
-    var path = e.detail.path;
-    switch (path) {
-        case 'Core/Globals.js':
+if (typeof CustomEvent === 'function') {
+    window.addEventListener('HighchartsModuleLoaded', function (e) {
+        var module = e.detail.module;
+        var path = e.detail.path;
+        if (path === 'Core/Globals.js') {
             module.hasTouch = true; // needs to be faked with TestController
-    }
-});
+        }
+    });
+}
