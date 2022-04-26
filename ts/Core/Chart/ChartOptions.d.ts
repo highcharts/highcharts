c00b33cba26d74e92aac76ebabcc803d04624bc9
“hihajdus” <sebastian.hajdus@gmail.com>
2022-04-26 13:44:22 +0200

    Zoom options refactoring

--- ts/Core/Chart/ChartOptions.d.ts
+++ ts/Core/Chart/ChartOptions.d.ts
@@ -133,6 +133,7 @@ export interface ChartOptions {
     type?: string;
     width?: (null|number);
     zoomBySingleTouch?: boolean;
+    zooming?: Partial<ChartZoomingOptions>;
     zoomType?: ('x'|'xy'|'y');
 }
 
@@ -171,6 +172,12 @@ export interface ChartSelectionCallbackFunction {
 export interface ChartSelectionContextObject {
     xAxis: Array<ChartSelectionAxisContextObject>;
     yAxis: Array<ChartSelectionAxisContextObject>;
+}
+
+export interface ChartZoomingOptions {
+    singleTouch: boolean;
+    type?: ('x'|'xy'|'y');
+    pinchType?: string;
 }
 
 /* *
