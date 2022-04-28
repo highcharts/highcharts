c7b039a038742edd134c5e6e8cc8caa2d8e54c28
madepiet <piotr.madej10@gmail.com>
2022-04-28 11:21:01 +0200

    Tests improved.

--- samples/unit-tests/maps/mapline-linewidth/demo.js
+++ samples/unit-tests/maps/mapline-linewidth/demo.js
@@ -25,119 +25,65 @@ QUnit.test(
     }
 );
 
-(async () => {
-    const mapData = await fetch(
-        'https://code.highcharts.com/mapdata/countries/gb/gb-all.topo.json'
-    ).then(response => response.json());
+QUnit.test(
+    'Setting lineWidth hover state for the mapLine series (#17105)',
+    function (assert) {
 
-    QUnit.test(
-        'Setting lineWidth hover state for the mapLine series (#17105)',
-        function (assert) {
-
-            // Initialize the chart
-            const chart = Highcharts.mapChart('container', {
-
-                title: {
-                    text: 'Highmaps simple flight routes demo'
-                },
-
-                legend: {
-                    align: 'left',
-                    layout: 'vertical',
-                    floating: true
-                },
-
-                mapNavigation: {
-                    enabled: true
-                },
-
-                tooltip: {
-                    formatter: function () {
-                        return this.point.id + (
-                            this.point.lat ?
-                                '<br>Lat: ' + this.point.lat + ' Lon: ' + this.point.lon : ''
-                        );
-                    }
-                },
-
-                series: [{
-                    mapData,
-                    name: 'Basemap',
-                    showInLegend: false
-                }, {
-                    type: 'mappoint',
-                    name: 'Cities',
-                    dataLabels: {
-                        format: '{point.id}'
-                    },
-                    data: [{
-                        id: 'London',
-                        lat: 51.507222,
-                        lon: -0.1275
-                    }, {
-                        id: 'Glasgow',
-                        lat: 55.858,
-                        lon: -4.259
-                    }, {
-                        id: 'Belfast',
-                        lat: 54.597,
-                        lon: -5.93
-                    }, {
-                        id: 'Lerwick',
-                        lat: 60.155,
-                        lon: -1.145,
-                        dataLabels: {
-                            align: 'left',
-                            x: 5,
-                            verticalAlign: 'middle'
-                        }
-                    }]
-                }]
-            });
-
-            // Function to return an SVG path between two points, with an arc
-            function pointsToPath(fromPoint, toPoint, invertArc) {
-                const
-                    from = chart.mapView.lonLatToProjectedUnits(fromPoint),
-                    to = chart.mapView.lonLatToProjectedUnits(toPoint),
-                    curve = 0.05,
-                    arcPointX = (from.x + to.x) /
-                        (invertArc ? 2 + curve : 2 - curve),
-                    arcPointY = (from.y + to.y) /
-                        (invertArc ? 2 + curve : 2 - curve);
-                return [
-                    ['M', from.x, from.y],
-                    ['Q', arcPointX, arcPointY, to.x, to.y]
-                ];
-            }
-
-            const londonPoint = chart.get('London');
-
-            // Add a series of lines for London
-            chart.addSeries({
-                name: 'London flight routes',
-                type: 'mapline',
-                lineWidth: 2,
-                color: Highcharts.getOptions().colors[3],
-                // Needed functionality on hover state
-                states: {
-                    hover: {
-                        color: 'red',
-                        lineWidth: 4
-                    }
-                },
+        // Initialize the chart
+        const chart = Highcharts.mapChart('container', {
+            series: [{
+                type: 'mappoint',
                 data: [{
-                    id: 'London - Belfast',
-                    path: pointsToPath(londonPoint, chart.get('Belfast'), true)
+                    id: 'one',
+                    lat: 1,
+                    lon: 5
+                }, {
+                    id: 'two',
+                    lat: 15,
+                    lon: 1
                 }]
-            }, true, false);
-
-            chart.series[2].points[0].setState('hover');
-
-            assert.strictEqual(
-                chart.series[2].options.states.hover.lineWidth,
-                4,
-                'Line width should have a value of 4 on hover state (#17105).'
-            );
+            }]
         });
-})();
+
+        // Function to return an SVG path between two points, with an arc
+        function pointsToPath(fromPoint, toPoint, invertArc) {
+            const
+                from = chart.mapView.lonLatToProjectedUnits(fromPoint),
+                to = chart.mapView.lonLatToProjectedUnits(toPoint),
+                curve = 0.05,
+                arcPointX =
+                    (from.x + to.x) /
+                    (invertArc ? 2 + curve : 2 - curve),
+                arcPointY =
+                    (from.y + to.y) /
+                    (invertArc ? 2 + curve : 2 - curve);
+            return [
+                ['M', from.x, from.y],
+                ['Q', arcPointX, arcPointY, to.x, to.y]
+            ];
+        }
+
+        const londonPoint = chart.get('one'),
+            belfastPoint = chart.get('two');
+
+        chart.addSeries({
+            type: 'mapline',
+            states: {
+                hover: {
+                    lineWidth: 4
+                }
+            },
+            data: [{
+                id: 'one - two',
+                path: pointsToPath(londonPoint, belfastPoint, true)
+            }]
+        }, true, false);
+
+        chart.series[1].points[0].setState('hover');
+
+        assert.strictEqual(
+            chart.series[1].options.states.hover.lineWidth,
+            4,
+            'Line width should have a value of 4 on hover state (#17105).'
+        );
+    });
