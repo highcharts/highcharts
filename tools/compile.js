5d02e0b905e2973213db18a23febf2a29bba449e
bre1470 <40056287+bre1470@users.noreply.github.com>
2022-05-05 16:34:18 +0200

    Tools: Updated dependencies.

--- tools/compile.js
+++ tools/compile.js
@@ -1,7 +1,11 @@
 /* eslint-env node, es6 */
 /* eslint no-console:0, no-path-concat:0, no-nested-ternary:0, valid-jsdoc:0 */
 /* eslint-disable func-style */
+
+// NOPE, YOU CAN NOT FIX IT! The JS compiler is gone and only the Java version
+// is left. (-_-) Keep the old version.
 const ClosureCompiler = require('google-closure-compiler').jsCompiler;
+
 const statSync = require('fs').statSync;
 const {
     getFile,
