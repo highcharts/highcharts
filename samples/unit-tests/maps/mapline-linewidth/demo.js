e1d35d30f770d85a7003c9e6d6a7c64b7901465d
madepiet <piotr.madej10@gmail.com>
2022-05-18 08:38:34 +0200

    Improved tests, borderWidth.

--- samples/unit-tests/maps/mapline-linewidth/demo.js
+++ samples/unit-tests/maps/mapline-linewidth/demo.js
@@ -1,68 +1,60 @@
 QUnit.test(
-    'Setting lineWidth of mapline series should work in all browsers (#5201)',
+    'Setting lineWidth and borderWidth should work in all browsers (#5201, #17105)',
     function (assert) {
-        var chart = Highcharts.mapChart('container', {
-            series: [
-                {
-                    type: 'mapline',
-                    data: Highcharts.geojson(
-                        Highcharts.maps['countries/us/custom/us-small'],
-                        'mapline'
-                    ),
-                    lineWidth: 30
-                }
-            ]
-        });
-
-        assert.close(
-            chart.series[0].transformGroups[0].element.getAttribute(
-                'stroke-width'
-            ) * chart.mapView.getSVGTransform().scaleX,
-            30,
-            0.0000000001,
-            'Line width should be set'
-        );
-    }
-);
-
-QUnit.test(
-    'Setting lineWidth hover state for the mapLine series (#17105)',
-    function (assert) {
-
-        // Initialize the chart
         const chart = Highcharts.mapChart('container', {
             series: [{
-                type: 'mappoint',
                 data: [{
-                    id: 'one',
-                    lat: 1,
-                    lon: 5
-                }, {
-                    id: 'two',
-                    lat: 15,
-                    lon: 1
+                    path: [
+                        ["M", 10, 10],
+                        ["H", 90],
+                        ["V", 90],
+                        ["H", 10],
+                        ["L", 10, 10]
+                    ]
+                }],
+                nullInteraction: true,
+                states: {
+                    hover: {
+                        borderWidth: 5
+                    }
+                }
+            }, {
+                type: 'mapline',
+                states: {
+                    hover: {
+                        lineWidth: 10
+                    }
+                },
+                data: [{
+                    path: [
+                        ["M", 400, 50],
+                        ["L", -200, 50]
+                    ]
                 }]
             }]
         });
 
-        chart.addSeries({
-            type: 'mapline',
-            states: {
-                hover: {
-                    lineWidth: 4
-                }
-            },
-            data: [{
-                id: 'one - two',
-                path: [["M", 5, 1], ["Q", 2.9268292682926833, 7.8048780487804885, 1, 15]]
-            }]
-        }, true, false);
+        chart.series[0].points[0].setState('hover', false);
+        chart.series[1].points[0].setState('hover', false);
 
-        chart.series[1].points[0].setState('hover');
-
-        assert.strictEqual(
-            chart.series[1].options.states.hover.lineWidth,
-            4,
-            'Line width should have a value of 4 on hover state (#17105).'
+        assert.close(
+            chart.series[0]
+                .transformGroups[0].element.childNodes[0]
+                .getAttribute('stroke-width') *
+                chart.mapView.getSVGTransform().scaleX,
+            5,
+            0.0000000001,
+            'Border width in the map should have a value of 5 on hover state (#17105).'
         );
-    });
+
+        assert.close(
+            chart.series[1]
+                .transformGroups[0].element.childNodes[0]
+                .getAttribute('stroke-width') *
+                chart.mapView.getSVGTransform().scaleX,
+            10,
+            0.0000000001,
+            'Line width in mapline should have a value of 10 on hover state (#5201, #17105).'
+        );
+    }
+);
