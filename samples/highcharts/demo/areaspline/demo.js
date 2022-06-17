a653753582b44a841cef9f090a368e486e93dc12
fanuel nteziryayo <fanuel.nteziryayo@highsoft.com>
2022-06-17 14:55:42 +0200

    Updated areaspline demo data

--- samples/highcharts/demo/areaspline/demo.js
+++ samples/highcharts/demo/areaspline/demo.js
@@ -1,16 +1,21 @@
+// Retrieved from https://www.ssb.no/jord-skog-jakt-og-fiskeri/jakt
 Highcharts.chart('container', {
     chart: {
         type: 'areaspline'
     },
     title: {
-        text: 'Average fruit consumption during one week'
+        text: 'Moose and deer hunting in Norway, 2000 - 2021'
+    },
+    subtitle: {
+        align: 'center',
+        text: 'Source: <a href="https://www.ssb.no/jord-skog-jakt-og-fiskeri/jakt" target="_blank">SSB</a>'
     },
     legend: {
         layout: 'vertical',
         align: 'left',
         verticalAlign: 'top',
-        x: 150,
-        y: 100,
+        x: 120,
+        y: 70,
         floating: true,
         borderWidth: 1,
         backgroundColor:
@@ -18,28 +23,42 @@ Highcharts.chart('container', {
     },
     xAxis: {
         categories: [
-            'Monday',
-            'Tuesday',
-            'Wednesday',
-            'Thursday',
-            'Friday',
-            'Saturday',
-            'Sunday'
+            '2000-2001',
+            '2001-2002',
+            '2002-2003',
+            '2003-2004',
+            '2004-2005',
+            '2005-2006',
+            '2006-2007',
+            '2007-2008',
+            '2008-2009',
+            '2009-2010',
+            '2010-2011',
+            '2011-2012',
+            '2012-2013',
+            '2013-2014',
+            '2014-2015',
+            '2015-2016',
+            '2016-2017',
+            '2017-2018',
+            '2018-2019',
+            '2019-2020',
+            '2020-2021'
         ],
-        plotBands: [{ // visualize the weekend
-            from: 4.5,
-            to: 6.5,
+        plotBands: [{ // Highlight the two last years
+            from: 19,
+            to: 20,
             color: 'rgba(68, 170, 213, .2)'
         }]
     },
     yAxis: {
         title: {
-            text: 'Fruit units'
+            text: 'Quantity'
         }
     },
     tooltip: {
         shared: true,
-        valueSuffix: ' units'
+        valueSuffix: ''
     },
     credits: {
         enabled: false
@@ -50,10 +69,56 @@ Highcharts.chart('container', {
         }
     },
     series: [{
-        name: 'John',
-        data: [3, 4, 3, 5, 4, 10, 12]
+        name: 'Moose',
+        data:
+            [
+                38000,
+                37300,
+                37892,
+                38564,
+                36770,
+                36026,
+                34978,
+                35657,
+                35620,
+                35971,
+                36409,
+                36435,
+                34643,
+                34956,
+                33199,
+                31136,
+                30835,
+                31611,
+                30666,
+                30319,
+                31766
+            ]
     }, {
-        name: 'Jane',
-        data: [1, 3, 4, 3, 3, 5, 4]
+        name: 'Deer',
+        data:
+            [
+                22534,
+                23599,
+                24533,
+                25195,
+                25896,
+                27635,
+                29173,
+                32646,
+                35686,
+                37709,
+                39143,
+                36829,
+                35031,
+                36202,
+                35140,
+                33718,
+                37773,
+                42556,
+                43820,
+                46445,
+                50048
+            ]
     }]
 });
