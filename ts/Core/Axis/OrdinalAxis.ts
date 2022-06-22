93655d0f9bde67af3017e7f254a486ab7d3fe61d
karolkolodziej <karolkolodziej01@gmail.com>
2022-06-22 12:23:28 +0200

    Fixed #17405, zooming on boosted chart freezed the chart sometime.

--- ts/Core/Axis/OrdinalAxis.ts
+++ ts/Core/Axis/OrdinalAxis.ts
@@ -946,13 +946,25 @@ namespace OrdinalAxis {
                 i,
                 ordinalPositions = [] as Array<number>,
                 overscrollPointsRange = Number.MAX_VALUE,
-                useOrdinal = false;
+                useOrdinal = false,
+                adjustOrdinalExtremesPoints = false;
 
             // Apply the ordinal logic
             if (isOrdinal || hasBreaks) { // #4167 YAxis is never ordinal ?
+                let distanceBetweenPoint = 0;
 
                 axis.series.forEach(function (series, i): void {
                     uniqueOrdinalPositions = [];
+
+                    // For an axis with multiple series, check if the distance
+                    // between points is identical throughout all series.
+                    if (i > 0) {
+                        adjustOrdinalExtremesPoints =
+                            distanceBetweenPoint !== series.processedXData[1] -
+                                series.processedXData[0];
+                    }
+                    distanceBetweenPoint =
+                        series.processedXData[1] - series.processedXData[0];
 
                     if (
                         (!ignoreHiddenSeries || series.visible !== false) &&
@@ -1018,6 +1030,14 @@ namespace OrdinalAxis {
                         }
                     }
                 });
+
+                // If the distance between points is not identical throughout
+                // all series, remove the first and last ordinal position to
+                // avoid enabling ordinal logic when it is not needed, #17405.
+                if (adjustOrdinalExtremesPoints) {
+                    ordinalPositions.pop();
+                    ordinalPositions.shift();
+                }
 
                 // cache the length
                 len = ordinalPositions.length;
