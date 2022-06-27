182122b22d90307e3e9a897c14f39b61eec64c5e
fanuel nteziryayo <fanuel.nteziryayo@highsoft.com>
2022-06-27 13:30:22 +0200

    Fixed #17404, sorting on pie-legend

--- samples/highcharts/demo/pie-legend/demo.js
+++ samples/highcharts/demo/pie-legend/demo.js
@@ -36,18 +36,18 @@ Highcharts.chart('container', {
             y: 74.77,
             sliced: true,
             selected: true
-        }, {
-            name: 'Internet Explorer',
-            y: 2.02
-        }, {
+        },  {
+            name: 'Edge',
+            y: 12.82
+        },  {
             name: 'Firefox',
             y: 4.63
         }, {
-            name: 'Edge',
-            y: 12.82
-        }, {
             name: 'Safari',
             y: 2.44
+        }, {
+            name: 'Internet Explorer',
+            y: 2.02
         }, {
             name: 'Other',
             y: 3.28
