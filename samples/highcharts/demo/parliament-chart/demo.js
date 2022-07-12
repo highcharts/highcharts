a72941ab315e78a5273487c28cbdccd168e5c8a9
fanuel nteziryayo <fanuel.nteziryayo@highsoft.com>
2022-07-12 14:15:51 +0200

    Updated parliament-chart demo data

--- samples/highcharts/demo/parliament-chart/demo.js
+++ samples/highcharts/demo/parliament-chart/demo.js
@@ -5,11 +5,13 @@ Highcharts.chart('container', {
     },
 
     title: {
-        text: 'Highcharts item chart'
+        text: 'Distribution of seats'
     },
 
     subtitle: {
-        text: 'Parliament visualization'
+        text: 'Bundestag election 20221. Source: ' +
+            '<a href="https://www.bundeswahlleiter.de/en/bundeswahlleiter.html"' +
+            'target="_blank">Bundeswahlleiter</a> '
     },
 
     legend: {
@@ -20,13 +22,14 @@ Highcharts.chart('container', {
         name: 'Representatives',
         keys: ['name', 'y', 'color', 'label'],
         data: [
-            ['The Left', 69, '#BE3075', 'DIE LINKE'],
-            ['Social Democratic Party', 153, '#EB001F', 'SPD'],
-            ['Alliance 90/The Greens', 67, '#64A12D', 'GRÜNE'],
-            ['Free Democratic Party', 80, '#FFED00', 'FDP'],
-            ['Christian Democratic Union', 200, '#000000', 'CDU'],
-            ['Christian Social Union in Bavaria', 46, '#008AC5', 'CSU'],
-            ['Alternative for Germany', 94, '#009EE0', 'AfD']
+            ['The Left', 39, '#BE3075', 'DIE LINKE'],
+            ['Social Democratic Party', 206, '#EB001F', 'SPD'],
+            ['Alliance 90/The Greens', 118, '#64A12D', 'GRÜNE'],
+            ['Free Democratic Party', 92, '#FFED00', 'FDP'],
+            ['Christian Democratic Union', 152, '#000000', 'CDU'],
+            ['Christian Social Union in Bavaria', 45, '#008AC5', 'CSU'],
+            ['Alternative for Germany', 83, '#009EE0', 'AfD'],
+            ['South Schleswig Voters\' Association', 1, '#284B63', 'SSW']
         ],
         dataLabels: {
             enabled: true,
