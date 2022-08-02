74a2a9bc5534f82d7645c22592b1361cb901dc0c
bre1470 <40056287+bre1470@users.noreply.github.com>
2022-08-02 20:11:10 +0200

    Fixed boost refresh after empty series array.

--- ts/Extensions/Boost/BoostSeries.ts
+++ ts/Extensions/Boost/BoostSeries.ts
@@ -415,7 +415,7 @@ function createAndAttachRenderer(
         // Fall back to image tag if foreignObject isn't supported,
         // or if we're exporting.
         if (chart.renderer.forExport || !foSupported) {
-            boost.target = chart.renderer.image(
+            target.renderTarget = boost.target = chart.renderer.image(
                 '',
                 0,
                 0,
@@ -445,7 +445,8 @@ function createAndAttachRenderer(
                 .createElement('foreignObject')
                 .add(targetGroup);
 
-            boost.target = doc.createElement('canvas') as any;
+            target.renderTarget = boost.target =
+                doc.createElement('canvas') as any;
             boost.targetCtx = boost.target.getContext('2d');
 
             boost.targetFo.element.appendChild(boost.target as any);
@@ -507,7 +508,7 @@ function createAndAttachRenderer(
     }
 
     boost.resize();
-    boost.clear(target);
+    boost.clear();
 
     if (!boost.wgl) {
         boost.wgl = new WGLRenderer((wgl): void => {
@@ -714,7 +715,7 @@ function exitBoost(
 
         // Clear previous run
         if (boost.clear) {
-            boost.clear(series);
+            boost.clear();
         }
     }
 }
@@ -792,7 +793,7 @@ function onSeriesHide(
             boost.wgl.clear();
         }
         if (boost.clear) {
-            boost.clear(this);
+            boost.clear();
         }
     }
 }
@@ -959,7 +960,7 @@ function seriesRenderCanvas(this: Series): void {
         // When switching from chart boosting mode, destroy redundant
         // series boosting targets
         if (this.boost && this.boost.target) {
-            this.boost.target = this.boost.target.destroy();
+            this.renderTarget = this.boost.target = this.boost.target.destroy();
         }
     }
 
@@ -1165,8 +1166,7 @@ function wrapPointHaloPath(
         point.plotY = series.xAxis.len - plotX;
     }
 
-    const halo: SVGPath =
-        proceed.apply(this, Array.prototype.slice.call(arguments, 1));
+    const halo: SVGPath = proceed.apply(this, [].slice.call(arguments, 1));
 
     if (series.boosted && inverted) {
         point.plotX = plotX;
@@ -1261,7 +1261,6 @@ function wrapSeriesFunctions(
     if (method === 'translate') {
         [
             'column',
-            'bar',
             'arearange',
             'columnrange',
             'heatmap',
@@ -1289,7 +1288,7 @@ function wrapSeriesGetExtremes(
     ) {
         return {};
     }
-    return proceed.apply(this, Array.prototype.slice.call(arguments, 1));
+    return proceed.apply(this, [].slice.call(arguments, 1));
 }
 
 /**
@@ -1312,7 +1311,7 @@ function wrapSeriesMarkerAttribs(
     }
 
     const attribs: SVGAttributes =
-        proceed.apply(this, Array.prototype.slice.call(arguments, 1));
+        proceed.apply(this, [].slice.call(arguments, 1));
 
     if (series.boosted && inverted) {
         point.plotX = plotX;
@@ -1373,7 +1372,7 @@ function wrapSeriesProcessData(
             series.options.stacking ||
             !hasExtremes(series, true)
         ) {
-            proceed.apply(series, Array.prototype.slice.call(arguments, 1));
+            proceed.apply(series, [].slice.call(arguments, 1));
             dataToMeasure = series.processedXData;
         }
 
@@ -1402,7 +1401,7 @@ function wrapSeriesProcessData(
         }
     // The series type is not boostable
     } else {
-        proceed.apply(this, Array.prototype.slice.call(arguments, 1));
+        proceed.apply(this, [].slice.call(arguments, 1));
     }
 }
 
