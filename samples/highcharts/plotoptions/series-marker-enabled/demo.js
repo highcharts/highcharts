4ebf1a6faa61cc0d3ea17f9372670b1ddf6fdfa7
Kamil Musiałowski <kamil.musialowski@blacklabel.pl>
2022-08-15 15:04:45 +0200

    Fixed formatting, improved HTML, fixed deprecated property.

--- samples/highcharts/plotoptions/series-marker-enabled/demo.js
+++ samples/highcharts/plotoptions/series-marker-enabled/demo.js
@@ -1,19 +1,19 @@
-Highcharts.chart("container", {
+Highcharts.chart('container', {
     xAxis: {
         categories: [
-            "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+            'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
+            'Oct', 'Nov', 'Dec'
         ]
     },
 
     plotOptions: {
         series: {
             marker: {
-                enabled: false
-            },
-            states: {
-                hover: {
-                    enabled: false
+                enabled: false,
+                states: {
+                    hover: {
+                        enabled: false
+                    }
                 }
             }
         }
@@ -21,8 +21,8 @@ Highcharts.chart("container", {
 
     series: [{
         data: [
-            29.9, 71.5, 106.4, 129.2, 144.0, 176.0,
-            135.6, 148.5, 216.4, 194.1, 95.6, 54.4
+            29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1,
+            95.6, 54.4
         ]
     }]
 });
