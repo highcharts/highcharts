7c7c2afd1c0d3c4484183f3eec5680810f02d02b
bre1470 <40056287+bre1470@users.noreply.github.com>
2022-08-16 10:01:34 +0200

    Docs: Improved typing of colors option.

--- ts/Core/DefaultOptions.ts
+++ ts/Core/DefaultOptions.ts
@@ -116,7 +116,7 @@ const defaultOptions: Options = {
      * @sample {highcharts} highcharts/chart/colors/
      *         Assign a global color theme
      *
-     * @type    {Array<Highcharts.ColorString>}
+     * @type    {Array<(Highcharts.ColorString|Highcharts.GradientColorObject|Highcharts.PatternObject)>}
      * @default ["#7cb5ec", "#434348", "#90ed7d", "#f7a35c", "#8085e9",
      *          "#f15c80", "#e4d354", "#2b908f", "#f45b5b", "#91e8e1"]
      */
