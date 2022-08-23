09f462985a61eda6da476d29875c3e0c1b6f676b
Øystein Moseng <oystein@highsoft.com>
2022-08-23 16:24:10 +0200

    Chartchooser: continuous-flow-streamgraph a11y.

--- samples/highcharts/chartchooser/continuous-flow-streamgraph-accessible/demo.js
+++ samples/highcharts/chartchooser/continuous-flow-streamgraph-accessible/demo.js
@@ -1,15 +1,16 @@
 Highcharts.chart('container', {
-
     chart: {
         type: 'streamgraph',
         marginBottom: 30,
         zoomType: 'x'
     },
+
     title: {
         floating: true,
         align: 'left',
         text: 'JS Frameworks Trends'
     },
+
     subtitle: {
         floating: true,
         align: 'left',
@@ -22,8 +23,7 @@ Highcharts.chart('container', {
         crosshair: true,
         labels: {
             align: 'left',
-            reserveSpace: false,
-            rotation: 270
+            reserveSpace: false
         },
         lineWidth: 0,
         margin: 20,
@@ -49,13 +49,22 @@ Highcharts.chart('container', {
         csvURL: "https://cdn.jsdelivr.net/gh/highcharts/highcharts@24912efc85/samples/data/js-frameworks-trends.csv"
     },
 
+    plotOptions: {
+        series: {
+            lineWidth: 2,
+            lineColor: '#467AB4'
+        }
+    },
+
     series: [{
         color: '#a0d9ff'
     }, {
         color: '#5891c8'
     }, {
-        color: '#346da4'
+        color: '#346da4',
+        lineColor: '#AEC6E0'
     }, {
-        color: '#0F487F'
+        color: '#0F487F',
+        lineColor: '#6C97C6'
     }]
 });
