0caa3e848284b1a94edd680496a305ce7589a94c
Torstein Hønsi <torstein@highsoft.com>
2022-08-31 15:00:56 +0200

    Maps, do not interact on invisible points

--- ts/Series/Map/MapPoint.ts
+++ ts/Series/Map/MapPoint.ts
@@ -185,7 +185,12 @@ class MapPoint extends ScatterSeries.prototype.pointClass {
      */
     public onMouseOver(e?: PointerEvent): void {
         U.clearTimeout(this.colorInterval as any);
-        if (!this.isNull || this.series.options.nullInteraction) {
+        if (
+            // Valid...
+            (!this.isNull && this.visible) ||
+            // ... or interact anyway
+            this.series.options.nullInteraction
+        ) {
             super.onMouseOver.call(this, e);
         } else {
             // #3401 Tooltip doesn't hide when hovering over null points
