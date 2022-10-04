ed2c8a907f4136155e320c5189194ed4524deba1
Kacper Madej <kacper@blacklabel.pl>
2022-10-04 14:18:56 +0200

    Used declare for type types.

--- ts/Core/Chart/Chart.ts
+++ ts/Core/Chart/Chart.ts
@@ -4200,10 +4200,4 @@ export default Chart;
  * @type {boolean|undefined}
  */
 
-/**
- * The dimensions the user can pan the chart.
- *
- * @typedef {"x"|"y"|"xy"} Highcharts.ChartPanningTypeValue
- */
-
 ''; // keeps doclets above in JS file
