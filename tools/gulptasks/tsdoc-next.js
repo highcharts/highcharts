a45c1dbca8e873f3e27dfd9a1ae3f0b9500be249
bre1470 <40056287+bre1470@users.noreply.github.com>
2022-10-14 10:03:08 +0200

    Tools: Updated tsdoc-next for future setup.

--- tools/gulptasks/tsdoc-next.js
+++ tools/gulptasks/tsdoc-next.js
@@ -12,6 +12,11 @@ const gulp = require('gulp');
 
 const NEXT_TARGET = 'tree-next.json';
 
+const NEXT_TEMPLATE = (
+    'node_modules/@highcharts/highcharts-documentation-generators/' +
+    'templates/classes.hbs'
+);
+
 /* *
  *
  *  Tasks
@@ -25,22 +30,57 @@ const NEXT_TARGET = 'tree-next.json';
  */
 function task() {
 
+    const args = require('yargs').argv;
     const fs = require('fs');
-    const generator = require(
+    const generators = require(
         '@highcharts/highcharts-documentation-generators'
-    ).TypeScript4;
+    );
     const logLib = require('./lib/log');
 
-    return Promise
-        .resolve()
-        .then(() => fs.writeFileSync(
-            NEXT_TARGET,
-            generator.JSON.stringify(
-                generator.ProjectDoc.load('ts').toJSON()
-            )
-        ))
+    if (args.old) {
+        const generator = generators.TypeScript4;
+
+        return Promise
+            .resolve()
+            .then(() => generator.ProjectDoc.load('ts'))
+            .then(project => fs.promises.writeFile(
+                NEXT_TARGET,
+                generator.JSON.stringify(project.toJSON())
+            ))
+            .then(logLib.success)
+            .catch(logLib.failure);
+    }
+
+    const generator = generators.Generator;
+    const parser = generators.Parser;
+    const source = (
+        fs.existsSync('code/es-modules/') ?
+            'code/es-modules/' :
+            'ts/'
+    );
+
+    if (source === 'ts/') {
+        logLib.warn('ES modules not found, using ./ts/ folder.');
+    }
+
+    return parser.Project
+        .load('ts', args.debug)
+        .then(project => {
+            fs.promises.writeFile(
+                NEXT_TARGET,
+                parser.JSON.stringify(project.toJSON())
+            );
+        })
+        .then(() => Promise.all([
+            generator.Project.load(NEXT_TARGET),
+            generator.Template.load(NEXT_TEMPLATE)
+        ]))
+        .then(([project, template]) => {
+            template.write('tree-next.html', project.ast);
+        })
         .then(logLib.success)
-        .catch(logLib.failure);
+        .catch(error => logLib.failure(error, error.stack));
+
 }
 
 gulp.task('tsdoc-next', task);
