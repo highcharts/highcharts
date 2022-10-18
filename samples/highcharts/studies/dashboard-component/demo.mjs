62033551075c09b4eb76621934f8201d64c919c9
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2022-10-18 11:07:00 +0200

    Fixed dashboard-component study

--- samples/highcharts/studies/dashboard-component/demo.mjs
+++ samples/highcharts/studies/dashboard-component/demo.mjs
@@ -1,13 +1,13 @@
-import HighchartsComponent from  '../../../../code/es-modules/Extensions/DashboardPlugin/HighchartsComponent.js';
-import DOMComponent from  '../../../../code/es-modules/Dashboard/Component/HTMLComponent.js';
-import CSVStore from '../../../../code/es-modules/Data/Stores/CSVStore.js';
+import Highcharts from 'https://code.highcharts.com/es-modules/masters/highmaps.src.js';
 
-// Bring in other forms of Highcharts
-import Highcharts from 'https://code.highcharts.com/es-modules/masters/highcharts.src.js';
-import Maps from 'https://code.highcharts.com/stock/es-modules/masters/highmaps.src.js';
+import HighchartsComponent from 'https://code.highcharts.com/es-modules/Extensions/DashboardPlugin/HighchartsComponent.js';
+import DOMComponent from 'https://code.highcharts.com/es-modules/Dashboard/Component/HTMLComponent.js';
+import CSVStore from 'https://code.highcharts.com/es-modules/Data/Stores/CSVStore.js';
+
+Highcharts.AST.allowedAttributes.push('rows');
 
 const style = {
-    height: 300
+  height: 300
 };
 
 const target = document.querySelector('#container');
@@ -15,155 +15,156 @@ const target = document.querySelector('#container');
 
 // The store
 const store = new CSVStore(undefined, {
-    csv: '',
-    firstRowAsNames: true
+  csv: '',
+  firstRowAsNames: true
 });
 //store.load();
 
 store.table.setColumns(
-    {
-        Country: ['Norway', 'Sweden', 'Denmark'],
-        Population: [25, 50, 100],
-        Greatness: [100, 20, 1],
-        'hc-key': ['no', 'se', 'dk']
-    }
+  {
+    Country: ['Norway', 'Sweden', 'Denmark'],
+    Population: [25, 50, 100],
+    Greatness: [100, 20, 1],
+    'hc-key': ['no', 'se', 'dk']
+  }
 );
 
-const components = [];
+store.on('load', (e) => {
+  console.log(e)
+})
 
-components.push(new HighchartsComponent({
-    id: 'stockChartComponent',
-    parentElement: target,
-    store,
-    tableAxisMap: {
-        'hc-key': null,
-        Country: 'x'
+const components = [];
+components.push(new DOMComponent({
+  store,
+  parentElement: target,
+  elements: [
+    {
+      tagName: 'p',
+      textContent: 'Datatable as CSV:'
     },
-    syncEvents: [
-        'visibility'
-    ],
-    style,
-    chartOptions: {
-        chart: {
-            type: 'column',
-            animation: false
-        },
-        xAxis: {
-            type: 'category'
+    {
+      tagName: 'textarea',
+      attributes: {
+        id: 'csvedit',
+        rows: 10,
+      },
+      textContent: store.save({})
+    }],
+  events: {
+    redraw: e => {
+      // Insert the updated store data to the textArea 
+      e.component.elements = e.component.elements.map(el => {
+        if (el.tagName === 'textarea') {
+          el.textContent = e.component.store.save({});
         }
+        return el;
+      });
+    },
+    afterRender: e => {
+      const component = e.target;
+      component.contentElement.querySelector('#csvedit')
+        .addEventListener('change', e => {
+          component.store.table.columns = [];
+          component.store.options.csv = e.target.value;
+          component.store.load();
+        });
     }
+  }
 }));
 
-
-/* components.push(new DOMComponent({ */
-/*     store, */
-/*     parentElement: target, */
-/*     elements: [ */
-/*         { */
-/*             tagName: 'p', */
-/*             textContent: 'Datatable as CSV:' */
-/*         }, */
-/*         { */
-/*             tagName: 'textArea', */
-/*             attributes: { */
-/*                 id: 'csvedit', */
-/*                 rows: 10, */
-/*                 onchange: () => { */
-/*                     store.options.csv = document.querySelector('textArea').value; */
-/*                     store.load(); */
-/*                 } */
-/*             }, */
-/*             textContent: store.save({}) */
-/*         }], */
-/*     events: { */
-/*         redraw: e => { */
-/*             // Insert the updated store data to the textArea */
-/*             e.component.elements = e.component.elements.map(el => { */
-/*                 if (el.tagName === 'textArea') { */
-/*                     el.textContent = e.component.store.save({}); */
-/*                 } */
-/*                 return el; */
-/*             }); */
-/*         } */
-/*     } */
-/* })); */
-
+components.push(new HighchartsComponent({
+  id: 'stockChartComponent',
+  parentElement: target,
+  store,
+  tableAxisMap: {
+    'hc-key': null,
+    Country: 'x'
+  },
+  syncEvents: [
+    'visibility'
+  ],
+  style,
+  chartOptions: {
+    title: null,
+    chart: {
+      type: 'column',
+      animation: false
+    },
+    xAxis: {
+      type: 'category'
+    }
+  }
+}));
 
 components.map(comp => {
-    comp.parentElement = document.createElement('div');
-    target.appendChild(comp.parentElement);
-    comp.render();
-    return comp;
+  comp.parentElement = document.createElement('div');
+  target.appendChild(comp.parentElement);
+  comp.render();
+  return comp;
 });
-
 
 // async map chart
 (async function createMapComponent() {
-    const response = await fetch('https://code.highcharts.com/mapdata/custom/europe.geo.json');
-    const mapData = await response.json();
+  const response = await fetch('https://code.highcharts.com/mapdata/custom/europe.geo.json');
+  const mapData = await response.json();
 
-    console.log(mapData);
-    Highcharts.maps['custom/europe'] = Highcharts.geojson(mapData);
-
-    const component = new HighchartsComponent({
-        parentElement: target,
-        chartConstructor: 'map',
-        store,
-        title: {
-            text: 'Click a country to hide it'
-        },
-        tableAxisMap: {
-            'hc-key': 'x',
-            Country: null
-        },
-        syncEvents: [
-            'visibility'
-        ],
-        chartOptions: {
-            tooltip: {
-                enabled: false
-            },
-            plotOptions: {
-                series: {
-                    events: {
-                        click: e => {
-                            e.point.series.hide();
-                        }
-                    }
-                }
-            },
-            chart: {
-                map: 'custom/europe',
-                borderWidth: 1
-            },
-            series: [{
-                name: 'basemap',
-                id: 'basemap',
-                data: [1, 2, 3]
-            }],
-            colorAxis: {
-                min: 0
+  const component = new HighchartsComponent({
+    parentElement: target,
+    chartConstructor: 'mapChart',
+    store,
+    tableAxisMap: {
+      'hc-key': 'x',
+      Country: null
+    },
+    syncEvents: [
+      'visibility'
+    ],
+    chartOptions: {
+      title: null,
+      tooltip: {
+        enabled: false
+      },
+      plotOptions: {
+        series: {
+          events: {
+            click: e => {
+              e.point.series.hide();
             }
+          }
         }
-    });
+      },
+      chart: {
+        map: mapData,
+        borderWidth: 1,
+      },
+      series: [{
+        name: 'basemap',
+        id: 'basemap',
+        data: [['no', 1]]
+      }],
+      colorAxis: {
+        min: 0
+      },
 
+    }
+  });
 
-    console.log(component);
-    component.parentElement = document.createElement('div');
-    target.appendChild(component.parentElement);
+  component.parentElement = document.createElement('div');
+  target.appendChild(component.parentElement);
 
-    component.render();
+  component.render();
 }()).then(() => {
-    const component = components[0];
-    console.log(component.activeGroup.getSharedState());
-    document.querySelector("#stopsync").addEventListener('click', () => {
-        console.log(component.sync);
-        component.sync.stop();
-    });
-    document.querySelector("#startsync").addEventListener('click', () => {
-        component.sync.start();
-    });
-    component.activeGroup.getSharedState().on('afterColumnVisibilityChange', e => {
-        console.log(e);
-    });
-});
+  document.querySelector("#stopsync").addEventListener('click', () => {
+    components.forEach(component => {
+      component.sync.stop();
+    })
+  });
+
+  document.querySelector("#startsync").addEventListener('click', () => {
+    components.forEach(component => {
+      component.sync.start();
+    })
+  });
+
+
+})
