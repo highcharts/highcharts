d43681aef9a45415f479ae3699dd27ac3fa311ee
MarkusBarstad <110829288+MarkusBarstad@users.noreply.github.com>
2022-10-27 14:19:31 +0200

    Added demo for tooltip colored by custom CSS

---
+++ samples/highcharts/css/tooltip-styledmode/demo.js
@@ -0,0 +1,37 @@
+Highcharts.chart('container', {
+    chart: {
+        styledMode: true
+    },
+    title: {
+        text: 'Tooltip styled by custom CSS'
+    },
+    yAxis: {
+        title: {
+            text: null
+        }
+    },
+    series: [{
+        type: 'line',
+        name: 'Apples',
+        className: 'apples',
+        data: [
+            0,
+            -1,
+            3,
+            1,
+            0]
+    }, {
+        type: 'line',
+        name: 'Oranges',
+        className: 'oranges',
+        data: [
+            1,
+            3,
+            {
+                className: 'bloodOrange',
+                y: 4
+            },
+            6,
+            7]
+    }]
+});
