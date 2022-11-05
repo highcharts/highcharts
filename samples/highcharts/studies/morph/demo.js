31b17d28da6be0550151f0d868fd892174dcf296
Torstein Hønsi <torstein@highsoft.com>
2022-11-05 08:06:35 +0100

    Study: Morphing series types. Scatter support.

--- samples/highcharts/studies/morph/demo.js
+++ samples/highcharts/studies/morph/demo.js
@@ -1,5 +1,9 @@
 // Morph an SVG element into a different type of element
-Highcharts.SVGElement.prototype.morph = function (shapeType, shapeArgs) {
+Highcharts.SVGElement.prototype.morph = function (
+    shapeType,
+    symbolName,
+    shapeArgs
+) {
     // Return the point that is closest to an imaginary point directly above the
     // shape. To avoid rotation when morphing.
     const alignToTopPoint = (points, bBox) => {
@@ -44,17 +48,32 @@ Highcharts.SVGElement.prototype.morph = function (shapeType, shapeArgs) {
         return path;
     };
 
-    const newShape = this.renderer[shapeType](shapeArgs)
-        .attr({
-            fill: 'none'
-        })
-        .add(this.parentGroup);
+    if (!this.newShape) {
+        const newShape = symbolName ?
+            this.renderer.symbol(symbolName)
+                .attr(shapeArgs) :
+            this.renderer[shapeType](shapeArgs);
+
+        newShape
+            .attr({
+                fill: 'none'
+            })
+            .add(this.parentGroup);
+        this.newShape = newShape;
+    } else {
+        this.newShape.attr(shapeArgs);
+    }
+
+    if (this.newShape.element.getTotalLength() === 0) {
+        return false;
+    }
 
     const attribs = {
         fill: this.element.getAttribute('fill'),
         stroke: this.element.getAttribute('stroke'),
         'stroke-width': this.element.getAttribute('stroke-width')
     };
+
     const interrim = this.renderer.path(getPath(this))
         .attr(attribs)
         .add(this.parentGroup);
@@ -62,20 +81,42 @@ Highcharts.SVGElement.prototype.morph = function (shapeType, shapeArgs) {
     this.element.remove();
 
     interrim.animate({
-        d: getPath(newShape)
+        d: getPath(this.newShape)
     }, {
         duration: 1000,
         complete: () => {
-            newShape.attr(attribs);
-            this.element = newShape.element;
+            this.newShape.attr(attribs);
+            this.element = this.newShape.element;
             interrim.destroy();
-            delete newShape.element;
-            newShape.destroy();
+            delete this.newShape.element;
+            this.newShape = this.newShape.destroy();
         }
     });
 
     // Chainable
     return this;
+};
+
+Highcharts.SVGElement.prototype.sleep = function (key) {
+    this.renderer.sleeping[key] = this;
+};
+
+Highcharts.SVGRenderer.prototype.awake = function (
+    sleepKey,
+    shapeType,
+    symbolName
+) {
+    const zombie = this.sleeping[sleepKey];
+    if (zombie) {
+        zombie.attr = attribs => {
+            if (zombie.morph(shapeType, symbolName, attribs) !== false) {
+                delete zombie.attr;
+            }
+            return zombie;
+        };
+    }
+    delete this.sleeping[sleepKey];
+    return zombie;
 };
 
 
@@ -92,35 +133,30 @@ Highcharts.SVGElement.prototype.morph = function (shapeType, shapeArgs) {
         e.target.renderer.sleeping = {};
     });
 
-    wrap(Point.prototype, 'destroy', function () {
-        this.series.chart.renderer.sleeping[
-            getSleepKey(this)
-        ] = this.graphic;
+    wrap(Point.prototype, 'destroy', function (proceed) {
+        this.graphic.sleep(getSleepKey(this));
         delete this.graphic;
+        proceed.call(this);
     });
 
     const beforeDrawPoints = function (proceed) {
-        const sleeping = this.chart.renderer.sleeping;
-
         this.points.forEach(point => {
             const key = getSleepKey(point),
-                zombie = sleeping[key];
-
+                zombie = this.chart.renderer.awake(
+                    key,
+                    point.shapeType,
+                    this.symbol
+                );
             if (zombie) {
                 point.graphic = zombie.add(this.group);
-
-                zombie.attr = attribs => {
-                    zombie.morph(point.shapeType, attribs);
-                    delete zombie.attr;
-                };
             }
-            delete sleeping[key];
 
             point.hasNewShapeType = () => false;
         });
 
         proceed.call(this);
     };
+    wrap(seriesTypes.column.prototype, 'drawPoints', beforeDrawPoints);
     wrap(seriesTypes.pie.prototype, 'drawPoints', beforeDrawPoints);
     wrap(Series.prototype, 'drawPoints', beforeDrawPoints);
 
@@ -134,6 +170,39 @@ Highcharts.SVGElement.prototype.morph = function (shapeType, shapeArgs) {
     });
 }(Highcharts));
 
+// /////////////////////////////////////////////////////////////////////////////
+
+const chart = Highcharts.chart('chart-container', {
+    title: {
+        text: 'Morphed chart'
+    },
+    accessibility: {
+        enabled: false
+    },
+    colors: ['#cad2c5', '#84a98c', '#52796f', '#354f52'],
+    xAxis: {
+        showEmpty: false
+    },
+    yAxis: {
+        showEmpty: false
+    },
+    series: [{
+        type: 'column',
+        data: [1, 3, 2, 4],
+        colorByPoint: true
+    }]
+});
+
+document
+    .querySelector('#button-group-types')
+    .querySelectorAll('button')
+    .forEach(button => {
+        button.addEventListener('click', () => {
+            chart.series[0].update({ type: button.dataset.type });
+        });
+    });
+
+// /////////////////////////////////////////////////////////////////////////////
 
 const ren = new Highcharts.Renderer(
     document.getElementById('container'),
@@ -151,7 +220,7 @@ const shape = ren.circle(100, 100, 50)
 
 document.getElementById('circle').addEventListener('click', () => {
     shape
-        .morph('circle', {
+        .morph('circle', undefined, {
             x: 100,
             y: 100,
             r: 50
@@ -164,7 +233,7 @@ document.getElementById('circle').addEventListener('click', () => {
 
 document.getElementById('column').addEventListener('click', () => {
     shape
-        .morph('rect', {
+        .morph('rect', undefined, {
             x: 100,
             y: 100,
             width: 10,
@@ -178,7 +247,7 @@ document.getElementById('column').addEventListener('click', () => {
 
 document.getElementById('bar').addEventListener('click', () => {
     shape
-        .morph('rect', {
+        .morph('rect', undefined, {
             x: 150,
             y: 50,
             width: 100,
@@ -192,7 +261,7 @@ document.getElementById('bar').addEventListener('click', () => {
 
 document.getElementById('marker').addEventListener('click', () => {
     shape
-        .morph('circle', {
+        .morph('circle', undefined, {
             x: 200,
             y: 300,
             r: 3
@@ -205,36 +274,18 @@ document.getElementById('marker').addEventListener('click', () => {
 
 document.getElementById('arc').addEventListener('click', () => {
     shape
-        .morph('path', Highcharts.SVGRenderer.prototype.symbols.arc(
-            200,
-            50,
-            100,
-            100,
-            {
-                innerR: 0,
-                r: 100,
-                start: 0.2,
-                end: 0.9
-            }
-        ))
+        .morph(undefined, 'arc', {
+            x: 200,
+            y: 50,
+            width: 100,
+            height: 100,
+            innerR: 0,
+            r: 100,
+            start: 0.2,
+            end: 0.9
+        })
         .animate({
             stroke: 'rgb(0,128,128)',
             fill: 'rgba(0,128,128,0.3)'
         });
 });
-
-const chart = Highcharts.chart('chart-container', {
-    title: {
-        text: 'Morphed chart'
-    },
-    colors: ['#cad2c5', '#84a98c', '#52796f', '#354f52'],
-    series: [{
-        type: 'column',
-        data: [1, 3, 2, 4],
-        colorByPoint: true
-    }]
-});
-
-setTimeout(() => {
-    chart.series[0].update({ type: 'pie' });
-}, 1234);
