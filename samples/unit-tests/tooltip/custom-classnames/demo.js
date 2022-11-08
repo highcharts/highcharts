87141b313bd277528b090afe6424358e44729ba9
MarkusBarstad <110829288+MarkusBarstad@users.noreply.github.com>
2022-11-08 15:44:43 +0100

    Added tests for split and shared

--- samples/unit-tests/tooltip/custom-classnames/demo.js
+++ samples/unit-tests/tooltip/custom-classnames/demo.js
@@ -1,5 +1,5 @@
 QUnit.test(
-    'Normal tooltip has correct className string when custom CSS is applied',
+    'Normal tooltip has correct classes when className is supplied',
     function (assert) {
         const chart = Highcharts.chart('container', {
             chart: {
@@ -38,6 +38,100 @@ QUnit.test(
             ).length,
             1,
             'Tooltip with correct classes should exist'
+        );
+    }
+);
+
+QUnit.test(
+    'Shared tooltip has correct classes when className is supplied',
+    function (assert) {
+        const chart = Highcharts.chart('container', {
+            chart: {
+                styledMode: true
+            },
+            tooltip: {
+                shared: true
+            },
+            series: [{
+                className: 'oranges',
+                data: [
+                    1,
+                    {
+                        className: 'blood-orange',
+                        y: 4
+                    },
+                    7]
+            }, {
+                name: 'Apples',
+                className: 'apples',
+                data: [
+                    0,
+                    3,
+                    0]
+            }]
+        });
+        const point = chart.series[0].points[0];
+        const controller = new TestController(chart);
+
+        controller.moveTo(
+            chart.plotLeft + point.plotX,
+            chart.plotTop + point.plotY
+        );
+
+        assert.strictEqual(
+            document.getElementsByClassName(
+                'highcharts-label highcharts-tooltip ' +
+                'highcharts-tooltip-0 oranges'
+            ).length,
+            1,
+            'Tooltip with correct classes should exist'
+        );
+    }
+);
+
+QUnit.test(
+    'Split tooltip has correct classes when className is supplied',
+    function (assert) {
+        const chart = Highcharts.chart('container', {
+            chart: {
+                styledMode: true
+            },
+            tooltip: {
+                split: true
+            },
+            series: [{
+                className: 'oranges',
+                data: [
+                    1,
+                    {
+                        className: 'blood-orange',
+                        y: 4
+                    },
+                    7]
+            }, {
+                name: 'Apples',
+                className: 'apples',
+                data: [
+                    0,
+                    3,
+                    0]
+            }]
+        });
+        const point = chart.series[0].points[0];
+        const controller = new TestController(chart);
+
+        controller.moveTo(
+            chart.plotLeft + point.plotX,
+            chart.plotTop + point.plotY
+        );
+
+        assert.strictEqual(
+            document.getElementsByClassName(
+                'highcharts-label highcharts-tooltip-box ' +
+                'highcharts-tooltip-0 oranges'
+            ).length,
+            1,
+            'Split tooltip with correct classes should exist for single series'
         );
     }
 );
