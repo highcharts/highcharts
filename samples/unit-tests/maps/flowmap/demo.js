d42ae4423fc67f49cb6b5d6786daf0eafe9a2bae
raf18seb <raf18seb@gmail.com>
2022-11-09 13:11:10 +0100

    Added more tests for a flowmap.

--- samples/unit-tests/maps/flowmap/demo.js
+++ samples/unit-tests/maps/flowmap/demo.js
@@ -11,81 +11,152 @@
             }, {
                 type: 'mappoint',
                 data: [{
-                    id: 'Vik i Sogn',
-                    lat: 61.087220,
-                    lon: 6.579700
+                    id: 'A',
+                    lat: 50,
+                    lon: 0
                 }, {
-                    id: 'Krakow',
-                    lon: 19.944981,
-                    lat: 50.064651
+                    id: 'B',
+                    lon: 20,
+                    lat: 50
+                }, {
+                    id: 'C',
+                    lon: 30,
+                    lat: 40
                 }]
             }, {
                 type: 'flowmap',
-                keys: [
-                    'from',
-                    'to',
-                    'weight',
-                    'curveFactor',
-                    'growTowards',
-                    'markerEnd'
-                ],
-                showInLegend: true,
-                data: [
-                    ['Vik i Sogn', 'Krakow', 0.5, 3]
-                ]
+                linkedTo: ':previous',
+                // keys: [
+                //     'from',
+                //     'to',
+                //     'weight',
+                //     'curveFactor',
+                //     'growTowards',
+                //     'markerEnd'
+                // ],
+                data: [{
+                    from: 'A',
+                    to: 'B',
+                    weight: 1
+                }]
             }]
         });
 
+        const series = chart.series[2];
+
+        // Test curveFactor
+
+        assert.strictEqual(
+            series.points[0].shapeArgs.d[0][2],
+            series.points[0].shapeArgs.d[1][2],
+            `Based on 2 points with the same lat, the arrow should be
+            horizontal (no curve).`
+        );
+
+        series.points[0].update({
+            curveFactor: 1
+        });
+
         assert.notStrictEqual(
-            chart.series[2].points[0].graphic.attr('visibility'),
-            'hidden',
-            'Flowmap point graphic should be visible.'
+            series.points[0].shapeArgs.d[0][2],
+            series.points[0].shapeArgs.d[1][2],
+            `Based on 2 points with the same lat, the arrow should no longer be
+            horizontal (curved).`
         );
 
-        chart.series[2].addPoint(
-            ['Krakow', 'Vik i Sogn', 0.5, 3, true, {
-                enabled: true,
-                height: 10,
-                width: 10
-            }]);
+        // Test weight
 
-        const addedPoint = chart.series[2].points[1];
-
-        addedPoint.setVisible(false);
+        series.addPoint({
+            from: 'B',
+            to: 'C'
+        });
 
         assert.strictEqual(
-            addedPoint.graphic.attr('visibility'),
-            'hidden',
-            'Flowmap point graphic should be hidden.'
+            series.points[1].shapeArgs.d.length,
+            0,
+            `The point without a weight defined on a point or series level
+            should not be drawn.`
         );
 
-        // curveFactor
+        series.points[1].update({
+            weight: 1
+        });
+
         assert.strictEqual(
-            addedPoint.options.curveFactor,
-            3,
-            'Flowmap point curveFactor value should be correct.'
+            series.points[1].shapeArgs.d.length,
+            8,
+            `The point with a weight defined on a point level should be drawn.`
         );
 
-        // growTowards
+        series.points[1].update({
+            weight: null
+        });
+
         assert.strictEqual(
-            addedPoint.growTowards,
-            true,
-            'Flowmap point should have growTowards property set.'
+            series.points[1].shapeArgs.d.length,
+            0,
+            `The point without a weight defined on a point or series level
+            should not be drawn.`
         );
 
-        // markerEnd
+        series.update({
+            weight: 1
+        });
+
         assert.strictEqual(
-            addedPoint.markerEnd.enabled,
-            true,
-            'Flowmap point should have markerEnd.'
+            series.points[1].shapeArgs.d.length,
+            8,
+            `The point with a weight defined on a series level should be drawn.`
         );
 
-        addedPoint.remove();
+        // Test style attribs
+
+        assert.strictEqual(
+            series.points[1].graphic.attr('fill'),
+            chart.options.colors[series.index],
+            `The point with a weight defined on a series level should be drawn.`
+        );
+
+        // assert.strictEqual(
+        //     series.points[1].graphic.attr('fillOpacity'),
+        //     series.options.fillOpacity,
+        //     `The point with a weight defined on a series level should be drawn.`
+        // );
+
+        // assert.notStrictEqual(
+        //     series.points[1].color,
+        //     series.options.nullColor,
+        //     `The not-null point should have a correct color in options.`
+        // );
+
+        assert.strictEqual(
+            series.points[1].graphic.attr('stroke'),
+            series.points[1].color,
+            `The point's stroke color should be correct.`
+        );
+
+        // series.points[1].setVisible(false);
+
+        // // assert.strictEqual(
+        // //     addedPoint.graphic.attr('visibility'),
+        // //     'hidden',
+        // //     'Flowmap point graphic should be hidden.'
+        // // );
+
+        const pointToRemove = series.points[1];
+
+        pointToRemove.remove();
 
         assert.notOk(
-            addedPoint.graphics,
+            pointToRemove.graphics,
             true,
-            'After point.remove(), the flowmap point graphic should be destroyed.'
+            'After point.remove(), the point graphic should be destroyed.'
+        );
+
+        assert.strictEqual(
+            series.points.length,
+            1,
+            'After point.remove(), there should be only one point left.'
         );
 
         chart.series[2].remove();
