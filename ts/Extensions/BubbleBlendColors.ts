89d038c3df58c05899c74c9af74cbb3db26a128a
madepiet <piotr.madej10@gmail.com>
2022-11-15 16:45:22 +0100

    Initial commit.

---
+++ ts/Extensions/BubbleBlendColors.ts
@@ -0,0 +1,53 @@
+/* *
+ *
+ *  (c) 2010-2022
+ *
+ *  License: www.highcharts.com/license
+ *
+ *  !!!!!!! SOURCE GETS TRANSPILED BY TYPESCRIPT. EDIT TS FILE ONLY. !!!!!!!
+ *
+ * */
+
+'use strict';
+
+/* *
+ *  Imports
+ *
+ * */
+
+import BubbleSeries from '../Series/Bubble/BubbleSeries';
+import U from '../Core/Utilities.js';
+const {
+    merge,
+    extend
+} = U;
+
+class BubbleBlendColors extends BubbleSeries {
+
+}
+
+/* *
+ *
+ *  Prototype properties
+ *
+ * */
+
+interface BubbleBlendColors {
+
+}
+
+/* *
+ *
+ *  Default export
+ *
+ * */
+
+export default BubbleBlendColors;
+
+/* *
+ *
+ *  API options
+ *
+ * */
+
+''; // adds doclets above to transpiled file
---
+++ ts/masters/modules/bubble-blend-colors.src.ts
@@ -0,0 +1,13 @@
+/**
+ * @license Highcharts JS v@product.version@ (@product.date@)
+ * @module highcharts/modules/bubble-blend-colors
+ * @requires highcharts
+ *
+ * Blend colors module for bubble series
+ *
+ * (c) 2010-2022
+ *
+ * License: www.highcharts.com/license
+ */
+'use strict';
+import '../../Extensions/BubbleBlendColors';
