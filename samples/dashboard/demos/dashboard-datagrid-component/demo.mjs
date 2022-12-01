2a4e0dd3f2cf841fae3b9b427986a81432c18feb
bre1470 <40056287+bre1470@users.noreply.github.com>
2022-12-01 12:36:29 +0100

    Changed demo to ESM only.

--- samples/dashboard/demos/dashboard-datagrid-component/demo.mjs
+++ samples/dashboard/demos/dashboard-datagrid-component/demo.mjs
@@ -1,6 +1,13 @@
 // Bring in other forms of Highcharts
+import Dashboard from '../../../../code/es-modules/masters/dashboard.src.js';
+import DataGrid from '../../../../code/es-modules/masters/datagrid.src.js';
+import Highcharts from '../../../../code/es-modules/masters/highcharts.src.js';
+import DraggablePoints from 'masters/modules/draggable-points.src.js';
 import HighchartsPlugin from '../../../../code/es-modules/Extensions/DashboardPlugins/HighchartsPlugin.js';
 import DataGridPlugin from '../../../../code/es-modules/Extensions/DashboardPlugins/DataGridPlugin.js';
+
+Highcharts.win.Highcharts = Highcharts;
+DraggablePoints(Highcharts);
 
 const { CSVStore, PluginHandler } = Dashboard;
 
