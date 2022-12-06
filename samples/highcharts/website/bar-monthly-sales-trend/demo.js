9ee3ddcb6307aca8b185b8ee82b9172322772a64
sigrunnu <sigrun.nummedal@gmail.com>
2022-12-06 13:55:31 +0100

    Moved bar monthly sales trend.

--- samples/highcharts/website/bar-monthly-sales-trend/demo.js
+++ samples/highcharts/website/bar-monthly-sales-trend/demo.js
@@ -1,15 +1,23 @@
 Highcharts.chart('container', {
+    chart: {
+        type: 'column'
+    },
     title: {
         text: 'Monthly Sales Trend (May-Aug)'
     },
     xAxis: {
         categories: ['May', 'June', 'July', 'Aug']
     },
+
     yAxis: {
         title: {
             text: ''
+        },
+        labels: {
+            format: '{value}k $'
         }
     },
+
     series: [{
         name: 'Sales',
         data: [206, 216, 247, 330]
