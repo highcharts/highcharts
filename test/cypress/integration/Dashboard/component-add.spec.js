ea6dcc3efe94322d3042497db43e17faf9ad8ad0
Pawel Lysy <pawel.lysy@blacklabel.pl>
2022-12-06 15:25:32 +0100

    Added cypress tests for adding component.

--- test/cypress/integration/Dashboard/component-add.spec.js
+++ test/cypress/integration/Dashboard/component-add.spec.js
@@ -15,7 +15,7 @@ function dropComponent(elementName) {
 
 describe('Add component through UI', () => {
     before(() => {
-        cy.visit('/dashboard/demos/dashboard-add-layout');
+        cy.visit('/dashboard/cypress/add-layout');
         cy.viewport(1200, 1000);
         cy.get('.hd-edit-context-menu-btn').click();
         cy.get('.hd-edit-toggle-slider').click();
@@ -23,29 +23,77 @@ describe('Add component through UI', () => {
 
     it('should be able to add a layout', function() {
         grabComponent('layout');
-        dropComponent("#dashboard-col-0")
+        dropComponent('#dashboard-col-0');
+        cy.dashboard().then((dashboard) => {
+            assert.equal(
+                dashboard.layouts.length,
+                2,
+                'New layout should be added.'
+            );
+        });
     });
 
     it('should be able to add a HTML component', function() {
         grabComponent('HTML');
         dropComponent('#dashboard-col-0');
+        cy.dashboard().then((dashboard) => {
+            assert.equal(
+                dashboard.layouts[0].rows[0].cells.length,
+                3,
+                'New cell should be added.'
+            );
+
+            let m = dashboard.mountedComponents;
+            assert.equal(
+                m[m.length - 1].component.type,
+                'HTML',
+                `New component's type should be HTML`
+            );
+        });
     });
 
     it('should be able to add a chart component', function() {
         grabComponent('chart');
         dropComponent('#dashboard-col-1')
+        cy.dashboard().then((dashboard) => {
+            assert.equal(
+                dashboard.layouts[0].rows[0].cells.length,
+                4,
+                'New cell should be added.'
+            );
+
+            let m = dashboard.mountedComponents;
+            assert.equal(
+                m[m.length - 1].component.type,
+                'Highcharts',
+                `New component's type should be HTML`
+            );
+        });
     });
 
     it('should be possible to resize the added component', function() {
         cy.get('.hd-edit-resize-snap-x').trigger('mousedown');
         cy.get('.hd-cell').eq(1).trigger('mousemove');
         cy.get('.hd-cell').eq(1).trigger('mouseup');
+        cy.dashboard().then((dashboard) => {
+
+            let m = dashboard.mountedComponents,
+                component =  m[m.length - 1];
+
+            assert.closeTo(
+                component.component.dimensions.width,
+                223,
+                1,
+                'Width of the element should be equal to given value.'
+                // Any better way of getting width?
+            );
+        });
     });
 
-    // TODO: add after the datagrid component is added
-    it('should be able to add a Data grid component', function() {
-        grabComponent('datagrid');
-        dropComponent('#dashboard-col-0')
-    });
+    // // TODO: add after the datagrid component is added
+    // it('should be able to add a Data grid component', function() {
+    //     grabComponent('datagrid');
+    //     dropComponent('#dashboard-col-0')
+    // });
     // TODO: how to get dashboard values?
 });
