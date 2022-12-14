236555f54a76b94cdfdec43a0aa683a03454b93e
Torstein Hønsi <torstein@highsoft.com>
2022-12-14 10:33:09 +0100

    Samples: Static data for mapmarker demos

--- samples/maps/series-mappoint/datalabels-mapmarker/demo.js
+++ samples/maps/series-mappoint/datalabels-mapmarker/demo.js
@@ -4,100 +4,354 @@
         'https://code.highcharts.com/mapdata/custom/europe.topo.json'
     ).then(response => response.json());
 
-    // Data structure: [country_code, latitude, longitude, capital_city]
-    const newData = [
-        ['dk', 55.66, 12.58, 'Copenhagen'],
-        ['fo', 62, -6.79, 'Torshavn'],
-        ['hr', 45.8, 16, 'Zagreb'],
-        ['nl', 52.35, 4.91, 'Amsterdam'],
-        ['ee', 59.43, 24.71, 'Tallinn'],
-        ['bg', 42.68, 23.31, 'Sofia'],
-        ['es', 40.4, -3.68, 'Madrid'],
-        ['it', 41.9, 12.48, 'Rome'],
-        ['sm', 43.93, 12.41, 'San Marino'],
-        ['va', 41.9, 12.45, 'Vatican'],
-        ['tr', 39.93, 32.86, 'Ankara'],
-        ['mt', 35.88, 14.5, 'Valetta'],
-        ['fr', 48.86, 2.33, 'Paris'],
-        ['no', 59.91, 10.75, 'Oslo'],
-        ['de', 52.51, 13.4, 'Berlin'],
-        ['ie', 53.31, -6.23, 'Dublin'],
-        ['ua', 50.43, 30.51, 'Kyiv'],
-        ['fi', 60.16, 24.93, 'Helsinki'],
-        ['se', 59.33, 18.05, 'Stockholm'],
-        ['ru', 55.75, 37.6, 'Moscow'],
-        ['gb', 51.5, -0.08, 'London'],
-        ['cy', 35.16, 33.36, 'Nicosia'],
-        ['pt', 38.71, -9.13, 'Lisbon'],
-        ['gr', 37.98, 23.73, 'Athens'],
-        ['lt', 54.68, 25.31, 'Vilnius'],
-        ['si', 46.05, 14.51, 'Ljubljana'],
-        ['ba', 43.86, 18.41, 'Sarajevo'],
-        ['mc', 43.73, 7.41, 'Monaco'],
-        ['al', 41.31, 19.81, 'Tirana'],
-        ['nc', 35.18, 33.36, 'North Nicosia'],
-        ['rs', 44.83, 20.5, 'Belgrade'],
-        ['ro', 44.43, 26.1, 'Bucharest'],
-        ['me', 42.43, 19.26, 'Podgorica'],
-        ['li', 47.13, 9.51, 'Vaduz'],
-        ['at', 48.2, 16.36, 'Vienna'],
-        ['sk', 48.15, 17.11, 'Bratislava'],
-        ['hu', 47.5, 19.08, 'Budapest'],
-        ['ad', 42.2, 1.24, 'Andorra la Vella'],
-        ['lu', 49.6, 6.11, 'Luxembourg'],
-        ['ch', 46.91, 7.46, 'Bern'],
-        ['be', 50.83, 4.33, 'Brussels'],
-        ['pl', 52.25, 21, 'Warsaw'],
-        ['mk', 42, 21.43, 'Skopje'],
-        ['lv', 56.95, 24.1, 'Riga'],
-        ['by', 53.9, 27.56, 'Minsk'],
-        ['is', 64.15, -21.95, 'Reykjavik'],
-        ['md', 47, 28.85, 'Chisinau'],
-        ['cz', 50.08, 14.46, 'Prague']
+    const data = [
+        [
+            'dk',
+            55.66,
+            12.58,
+            'Copenhagen',
+            0
+        ],
+        [
+            'fo',
+            62,
+            -6.79,
+            'Torshavn',
+            1
+        ],
+        [
+            'hr',
+            45.8,
+            16,
+            'Zagreb',
+            0
+        ],
+        [
+            'nl',
+            52.35,
+            4.91,
+            'Amsterdam',
+            -4
+        ],
+        [
+            'ee',
+            59.43,
+            24.71,
+            'Tallinn',
+            -4
+        ],
+        [
+            'bg',
+            42.68,
+            23.31,
+            'Sofia',
+            1
+        ],
+        [
+            'es',
+            40.4,
+            -3.68,
+            'Madrid',
+            11
+        ],
+        [
+            'it',
+            41.9,
+            12.48,
+            'Rome',
+            12
+        ],
+        [
+            'sm',
+            43.93,
+            12.41,
+            'San Marino',
+            3
+        ],
+        [
+            'va',
+            41.9,
+            12.45,
+            'Vatican',
+            12
+        ],
+        [
+            'tr',
+            39.93,
+            32.86,
+            'Ankara',
+            9
+        ],
+        [
+            'mt',
+            35.88,
+            14.5,
+            'Valetta',
+            19
+        ],
+        [
+            'fr',
+            48.86,
+            2.33,
+            'Paris',
+            -1
+        ],
+        [
+            'no',
+            59.91,
+            10.75,
+            'Oslo',
+            -8
+        ],
+        [
+            'de',
+            52.51,
+            13.4,
+            'Berlin',
+            -5
+        ],
+        [
+            'ie',
+            53.31,
+            -6.23,
+            'Dublin',
+            3
+        ],
+        [
+            'ua',
+            50.43,
+            30.51,
+            'Kyiv',
+            -3
+        ],
+        [
+            'fi',
+            60.16,
+            24.93,
+            'Helsinki',
+            -6
+        ],
+        [
+            'se',
+            59.33,
+            18.05,
+            'Stockholm',
+            -4
+        ],
+        [
+            'ru',
+            55.75,
+            37.6,
+            'Moscow',
+            -5
+        ],
+        [
+            'gb',
+            51.5,
+            -0.08,
+            'London',
+            -1
+        ],
+        [
+            'cy',
+            35.16,
+            33.36,
+            'Nicosia',
+            16
+        ],
+        [
+            'pt',
+            38.71,
+            -9.13,
+            'Lisbon',
+            15
+        ],
+        [
+            'gr',
+            37.98,
+            23.73,
+            'Athens',
+            13
+        ],
+        [
+            'lt',
+            54.68,
+            25.31,
+            'Vilnius',
+            -3
+        ],
+        [
+            'si',
+            46.05,
+            14.51,
+            'Ljubljana',
+            0
+        ],
+        [
+            'ba',
+            43.86,
+            18.41,
+            'Sarajevo',
+            7
+        ],
+        [
+            'mc',
+            43.73,
+            7.41,
+            'Monaco',
+            8
+        ],
+        [
+            'al',
+            41.31,
+            19.81,
+            'Tirana',
+            11
+        ],
+        [
+            'nc',
+            35.18,
+            33.36,
+            'North Nicosia',
+            16
+        ],
+        [
+            'rs',
+            44.83,
+            20.5,
+            'Belgrade',
+            2
+        ],
+        [
+            'ro',
+            44.43,
+            26.1,
+            'Bucharest',
+            1
+        ],
+        [
+            'me',
+            42.43,
+            19.26,
+            'Podgorica',
+            9
+        ],
+        [
+            'li',
+            47.13,
+            9.51,
+            'Vaduz',
+            4
+        ],
+        [
+            'at',
+            48.2,
+            16.36,
+            'Vienna',
+            0
+        ],
+        [
+            'sk',
+            48.15,
+            17.11,
+            'Bratislava',
+            -1
+        ],
+        [
+            'hu',
+            47.5,
+            19.08,
+            'Budapest',
+            -1
+        ],
+        [
+            'ad',
+            42.2,
+            1.24,
+            'Andorra la Vella',
+            7
+        ],
+        [
+            'lu',
+            49.6,
+            6.11,
+            'Luxembourg',
+            -2
+        ],
+        [
+            'ch',
+            46.91,
+            7.46,
+            'Bern',
+            2
+        ],
+        [
+            'be',
+            50.83,
+            4.33,
+            'Brussels',
+            -2
+        ],
+        [
+            'pl',
+            52.25,
+            21,
+            'Warsaw',
+            -10
+        ],
+        [
+            'mk',
+            42,
+            21.43,
+            'Skopje',
+            4
+        ],
+        [
+            'lv',
+            56.95,
+            24.1,
+            'Riga',
+            -2
+        ],
+        [
+            'by',
+            53.9,
+            27.56,
+            'Minsk',
+            -3
+        ],
+        [
+            'is',
+            64.15,
+            -21.95,
+            'Reykjavik',
+            -6
+        ],
+        [
+            'md',
+            47,
+            28.85,
+            'Chisinau',
+            0
+        ],
+        [
+            'cz',
+            50.08,
+            14.46,
+            'Prague',
+            -3
+        ]
     ];
-    // Get temperature for specific localization, and add it to the chart. It
-    // takes point as first argument, countries series as second and capitals
-    // series as third. Capitals series have to be the 'mappoint' series type,
-    // and it should be defined before in the series array.
-    let labelrank = 0;
-    async function getTemp(point, capitals) {
-
-        const json = await fetch(
-            'https://api.met.no/weatherapi/locationforecast/2.0/?' +
-                `lat=${point[1]}&lon=${point[2]}`
-        ).then(response => response.json());
-
-        const temp = json.properties.timeseries[0].data.instant.details
-                .air_temperature,
-            value = parseInt(temp, 10);
-
-        if (Number.isInteger(value)) {
-            capitals.addPoint({
-                name: point[3],
-                lat: point[1],
-                lon: point[2],
-                colorKey: 'y',
-                y: value,
-                labelrank: labelrank++
-            });
-        }
-    }
 
     // Create the chart
     Highcharts.mapChart('container', {
         chart: {
             map: topology,
-            events: {
-                load: function () {
-                    const capitals = this.series[1];
-                    newData.forEach(elem => getTemp(elem, capitals));
-                }
-            },
             margin: 1
         },
 
         title: {
-            text: 'Current temperatures in capitals of Europe',
+            text: 'Winter day temperatures in  Europe',
             align: 'right',
             floating: true,
             style: {
@@ -172,6 +426,8 @@
             enableMouseTracking: false
         }, {
             name: 'Capitals of Europe',
+            keys: ['id', 'lat', 'lon', 'name', 'y'],
+            data,
             animation: false,
             type: 'mappoint',
             showInLegend: false,
