bf7b7384da6000939457258181663adda631bacd
Pawel Lysy <pawel.lysy@blacklabel.pl>
2022-12-21 16:34:17 +0100

    Fixed test.

--- test/typescript-karma/Dashboard/SerializeHelpers/CSVConverterHelper.test.js
+++ test/typescript-karma/Dashboard/SerializeHelpers/CSVConverterHelper.test.js
@@ -1,18 +1,17 @@
-import DataTable from '/base/code/es-modules/Data/DataTable.js';
-import DataTableHelper from '/base/code/es-modules/Dashboard/SerializeHelper/DataTableHelper.js';
+import CSVConverterHelper from '/base/code/es-modules/Dashboard/SerializeHelper/CSVConverterHelper.js';
 import CSVConverter from '/base/code/es-modules/Data/Converters/CSVConverter.js';
 
-QUnit.skip('JSON serializer for CSVConverter', function (assert) {
+QUnit.test('JSON serializer for CSVConverter', function (assert) {
 
         let converter = new CSVConverter({
                 csv: 'Date:Grade;\n24 May;5,8\n25 May;7,9\n15 July;8,1',
                 decimalPoint: '.',
                 itemDelimiter: ',',
                 lineDelimiter: '\n'
-            }),
-            json = DataTableHelper.toJSON(converter),
-            converter2 = DataTableHelper.fromJSON(json),
-            json2 = DataTableHelper.toJSON(converter2);
+            })
+        let json = CSVConverterHelper.toJSON(converter),
+            converter2 = CSVConverterHelper.fromJSON(json),
+            json2 = CSVConverterHelper.toJSON(converter2);
 
 
     assert.deepEqual(
