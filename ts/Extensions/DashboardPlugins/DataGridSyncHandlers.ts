6b7fe9947f4268a68b56ae7bb16e1e1c10b51e6d
Sebastian Bochan <sebastian@blacklabel.pl>
2023-01-18 14:19:35 +0100

    Changed indexing of elements in datagrid, refactored sync.

--- ts/Extensions/DashboardPlugins/DataGridSyncHandlers.ts
+++ ts/Extensions/DashboardPlugins/DataGridSyncHandlers.ts
@@ -28,7 +28,10 @@ import ComponentGroup from '../../Dashboard/Component/ComponentGroup.js';
 import ComponentTypes from '../../Dashboard/Component/ComponentType';
 import DataGridComponent from './DataGridComponent.js';
 import U from '../../Core/Utilities.js';
-const { addEvent } = U;
+const {
+    addEvent,
+    pick
+} = U;
 
 /* *
  *
@@ -89,14 +92,21 @@ const configs: {
             'afterHoverPointChange',
             function (this: DataGridComponent, e: SharedState.PointHoverEvent): void {
                 const { dataGrid } = this;
-
                 if (dataGrid) {
                     if (e.hoverPoint) {
-                        const point = e.hoverPoint,
-                            pointIndex = point.index || point.x || 0;
+                        const point = e.hoverPoint;
+                        let highlightedDataRow;
 
-                        dataGrid.toggleRowHighlight(dataGrid.rowElements[pointIndex]);
-                        dataGrid.hoveredRow = dataGrid.rowElements[pointIndex];
+                        for (let i = 0, iEnd = dataGrid.rowElements.length; i < iEnd; ++i) {
+                            if (dataGrid.rowElements[i].dataset.rowXIndex === String(point.x || 0)) {
+                                highlightedDataRow = dataGrid.rowElements[i];
+                            }
+                        }
+
+                        if (highlightedDataRow) {
+                            dataGrid.toggleRowHighlight(highlightedDataRow);
+                            dataGrid.hoveredRow = highlightedDataRow;
+                        }
                     }
                 }
             }
