a05568245cff2c19813a90c87a2b2eb1b9475c2a
karolkolodziej <karolkolodziej01@gmail.com>
2023-01-27 15:19:49 +0100

    Added return when clicking on selected city.

--- samples/dashboards/demos/climate/demo.js
+++ samples/dashboards/demos/climate/demo.js
@@ -239,6 +239,10 @@ async function setupDashboard() {
                             const point = e.point;
                             const city = point.name;
 
+                            if (cityScope === city) {
+                                return; // already selected
+                            }
+
                             cityScope = city;
                             const store = await dataPool.getStore(city);
 
