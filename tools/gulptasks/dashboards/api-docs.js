b15d04a33fe99019668ed8e76407eb59e2900a1a
bre1470 <40056287+bre1470@users.noreply.github.com>
2023-02-03 13:05:52 +0100

    Added additional api variant with clean theme, search, dark-mode.

--- tools/gulptasks/dashboards/api-docs.js
+++ tools/gulptasks/dashboards/api-docs.js
@@ -19,6 +19,9 @@ async function apiDocs() {
 
     await fsLib.deleteDirectory('build/api/dashboards-2', true);
     await processLib.exec('npx typedoc --options tools/dashboards/typedoc.json --tsconfig ts/tsconfig.json');
+
+    await fsLib.deleteDirectory('build/api/dashboards-3', true);
+    await processLib.exec('npx jsdoc -c tools/dashboards/jsdoc-clean.json code/es-modules/Dashboards/ code/es-modules/Data/');
 }
 
 gulp.task('dashboards/api-docs', gulp.series('scripts', apiDocs));
