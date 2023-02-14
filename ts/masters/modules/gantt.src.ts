113191fe98259b84da7de272de5236c244efb848
Marita <marita.vindedal@highsoft.com>
2023-02-14 11:14:29 +0100

    Added compose to CurrentDateIndication.

--- ts/masters/modules/gantt.src.ts
+++ ts/masters/modules/gantt.src.ts
@@ -17,6 +17,7 @@ import RangeSelector from '../../Stock/RangeSelector/RangeSelector.js';
 import XRangeSeries from '../../Series/XRange/XRangeSeries.js';
 import '../../Series/Gantt/GanttSeries.js';
 import GanttChart from '../../Core/Chart/GanttChart.js';
+import CurrentDateIndication from '../../Extensions/CurrentDateIndication.js';
 const G: AnyRecord = Highcharts;
 // Classes
 G.GanttChart = GanttChart;
@@ -25,6 +26,7 @@ G.Navigator = Navigator;
 G.RangeSelector = RangeSelector;
 G.Scrollbar = Scrollbar;
 // Compositions
+CurrentDateIndication.compose(G.Axis, G.PlotLineOrBand);
 Navigator.compose(G.Axis, G.Chart, G.Series);
 RangeSelector.compose(G.Axis, G.Chart);
 Scrollbar.compose(G.Axis);
