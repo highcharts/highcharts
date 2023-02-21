58019c9dba3fd4d2bfea73d6ac12a57ee91dfb66
info <mustapha@highsoft.com>
2023-02-21 12:59:27 +0100

    Blog demo about accessibility tags

---
+++ samples/highcharts/blog/accessibility-tags/demo.js
@@ -0,0 +1,42 @@
+Highcharts.chart('container', {
+    chart: {
+        type: 'column'
+    },
+    legend: {
+        symbolWidth: 40
+    },
+    title: {
+        text: 'Palmer Penguin Species'
+    },
+    subtitle: {
+        text:
+      'Total counts of Palmer Penguin species, from the Palmer Penguins dataset'
+    },
+    xAxis: {
+        title: {
+            text: 'Penguin Species'
+        },
+        categories: ['Chinstrap', 'Gentoo', 'Adelie'],
+        accessibility: {
+            description: 'Penguin species: Chinstrap, Gentoo, and Adelie.'
+        }
+    },
+    yAxis: {
+        title: {
+            text: 'Count of Penguins'
+        },
+        accessibility: {
+            description: 'Total number of penguins for each species.'
+        }
+    },
+    series: [
+        {
+            name: 'Penguins',
+            data: [68, 124, 152],
+            accessibility: {
+                description:
+          'Adelie penguins are the most abundant species in this dataset'
+            }
+        }
+    ]
+});
