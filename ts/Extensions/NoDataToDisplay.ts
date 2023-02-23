48b93eb300a5ce99b99a0e6e13d0a8ed38bcdb14
Torstein Hønsi <torstein@highsoft.com>
2023-02-23 13:42:46 +0000

    Relative font size: No-data

--- ts/Extensions/NoDataToDisplay.ts
+++ ts/Extensions/NoDataToDisplay.ts
@@ -176,7 +176,7 @@ defaultOptions.noData = {
         /** @ignore */
         fontWeight: 'bold',
         /** @ignore */
-        fontSize: '0.75',
+        fontSize: '0.75em',
         /** @ignore */
         color: Palette.neutralColor60
     }
