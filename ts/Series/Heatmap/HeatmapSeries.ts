5a030350f4d89b254d3fcd841ba638deb9297968
MarkusBarstad <110829288+MarkusBarstad@users.noreply.github.com>
2023-02-23 03:36:15 +0100

    Avoiding creating vars, might be a bit obtuse

--- ts/Series/Heatmap/HeatmapSeries.ts
+++ ts/Series/Heatmap/HeatmapSeries.ts
@@ -466,12 +466,18 @@ class HeatmapSeries extends ScatterSeries {
                         scaleValue = function (
                             value: number, from: number[], to: number[]
                         ): number {
-                            const scale = (to[1] - to[0]) / (from[1] - from[0]);
-                            const capped = Math.min(
-                                from[1], Math.max(from[0], value)
-                            ) - from[0];
-
-                            return ~~(capped * scale + to[0]);
+                            return ~~(
+                                (
+                                    Math.min(
+                                        from[1],
+                                        Math.max(from[0], value)
+                                    ) - from[0]
+                                ) *
+                                (
+                                    (to[1] - to[0]) /
+                                    (from[1] - from[0])
+                                ) + to[0]
+                            );
                         };
 
                     if (!heatmap.boost && !chart.boost) {
