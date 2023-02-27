6524b7ec2b2fc4d061cb7e632ada0d998080a728
MarkusBarstad <110829288+MarkusBarstad@users.noreply.github.com>
2023-02-27 13:15:35 +0100

    Rudamentary unit-test created

---
+++ samples/unit-tests/series-heatmap/interpolation/demo.js
@@ -0,0 +1,33 @@
+QUnit.test('Interpolated image test', function (assert) {
+    var chart = Highcharts.chart('container', {
+        chart: {
+            type: 'heatmap'
+        },
+
+        colorAxis: {
+            stops: [
+                [0, '#3060cf'],
+                [0.5, '#fffbbc'],
+                [0.9, '#c4463a']
+            ]
+        },
+
+        series: [
+            {
+                data: [
+                    [1, 0, 2],
+                    [1, 1, 2.1],
+                    [1, 2, 2.2],
+                    [2, 0, 3],
+                    [2, 1, 3.1],
+                    [2, 2, 3.2]
+                ],
+                interpolation: true
+            }
+        ]
+    });
+
+    var heatmap = chart.series[0];
+
+    assert.ok(heatmap.image, 'Should have an image');
+});
