3a24806d29680123162f2b9eb82746fcfdc18a2f
Dawid Draguła <dawid.dragula@gmail.com>
2023-03-10 19:16:37 +0100

    Fixed #18636, waterfall lines missing when there was a null point.

--- samples/unit-tests/series-waterfall/null-value/demo.js
+++ samples/unit-tests/series-waterfall/null-value/demo.js
@@ -2,29 +2,17 @@ QUnit.test(
     'The connector line in waterfall in case of a null value (#8024)',
     function (assert) {
         var chart = Highcharts.chart('container', {
-                series: [
-                    {
-                        type: 'waterfall',
-                        data: [120000, null, 231000, -342000, -233000]
-                    }
-                ]
-            }),
-            splittedPath = chart.series[0].graph.d.split(' '),
-            startPoint = [splittedPath[4], splittedPath[5]],
-            endPoint = [splittedPath[7], splittedPath[8]];
+            series: [
+                {
+                    type: 'waterfall',
+                    data: [120000, null, 231000, -342000, -233000]
+                }
+            ]
+        });
 
-        // Test: the eqality of path's x coordinate of points next to each other
-        assert.strictEqual(
-            startPoint[0],
-            endPoint[0],
-            'The x coordinates are equal.'
-        );
-
-        // Test: the eqality of path's y coordinate of points next to each other
-        assert.strictEqual(
-            startPoint[1],
-            endPoint[1],
-            'The y coordinates are equal.'
+        assert.notOk(
+            isNaN(chart.series[0].graph.pathArray[2][2]),
+            'The connector line skips a point with a null Y (#18636)'
         );
     }
 );
