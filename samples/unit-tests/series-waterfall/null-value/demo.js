919fe7b8e533fd6d15a8493ee9115d6020ef06ba
Dawid Draguła <dawid.dragula@gmail.com>
2023-03-13 21:39:04 +0100

    Added handling the connectNulls option.

--- samples/unit-tests/series-waterfall/null-value/demo.js
+++ samples/unit-tests/series-waterfall/null-value/demo.js
@@ -1,18 +1,26 @@
 QUnit.test(
-    'The connector line in waterfall in case of a null value (#8024)',
+    'The connector line in waterfall in case of a null value (#18636)',
     function (assert) {
-        var chart = Highcharts.chart('container', {
-            series: [
-                {
-                    type: 'waterfall',
-                    data: [120000, null, 231000, -342000, -233000]
-                }
-            ]
-        });
+        const chart = Highcharts.chart('container', {
+                series: [
+                    {
+                        type: 'waterfall',
+                        data: [120000, null, 231000, -342000, -233000]
+                    }
+                ]
+            }),
+            series = chart.series[0];
 
-        assert.notOk(
-            isNaN(chart.series[0].graph.pathArray[2][2]),
-            'The connector line skips a point with a null Y (#18636)'
+        assert.strictEqual(
+            series.graph.pathArray.length, 4,
+            'Connector lines should display correctly with false connectNulls'
+        );
+
+        series.update({ connectNulls: true });
+
+        assert.strictEqual(
+            series.graph.pathArray.length, 6,
+            'Connector lines should display correctly with true connectNulls'
         );
     }
 );
