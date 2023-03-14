fa287eb3471a4670234802c842e8bbf62f3f0c5e
Pawel Lysy <pawel.lysy@blacklabel.pl>
2023-03-14 10:12:59 +0100

    Improved cypress test.

--- test/cypress/integration/Dashboards/edit-component.cy.js
+++ test/cypress/integration/Dashboards/edit-component.cy.js
@@ -11,6 +11,7 @@ describe('Editable component options', () => {
 
         cy.get('.highcharts-dashboards-component').first().click();
         cy.get('.highcharts-dashboards-edit-toolbar-cell > .highcharts-dashboards-edit-toolbar-item:nth-child(2)').click();
+        cy.get('button.highcharts-dashboards-outer-accordeon-header-btn').contains('Chart id').click();
         cy.get('#chartID').as('chartIDfield')
 
         cy.get('@chartIDfield').invoke('val').then(exisitingVal =>{
