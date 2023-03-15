3dac0576f3f04a81a18ba345474fb0e2395ca3d5
MarkusBarstad <110829288+MarkusBarstad@users.noreply.github.com>
2023-03-15 15:36:35 +0100

    Generating some heatmap data for demo

--- samples/highcharts/series-heatmap/interpolation/demo.js
+++ samples/highcharts/series-heatmap/interpolation/demo.js
@@ -1,22 +1,57 @@
-const data = [];
+const
+    mapData = [],
+    h = 20,
+    w = 25,
+    maxClusterSize = 4;
+let randomClusters = 300;
 
-for (let i = 0; i < 20; i++) {
-    let randomFactor = 0;
-
-    for (let j = 0; j < 10; j++) {
-        if (randomFactor <= 0) {
-            randomFactor = Math.random() * 8;
-        }
-
-        data.push({
-            x: j,
-            y: i,
-            value: randomFactor
-        });
-
-        randomFactor--;
-    }
+for (let i = 0; i < h; i++) {
+    mapData.push(Array(w).fill(0));
 }
+const randomFloored = n => ~~(Math.random() * n);
+
+const withinBound = (val, min, max) => val > min && val < max;
+
+const addIfWithin = (y, x, heat) => {
+    if (withinBound(x, -1, w) && withinBound(y, -1, h)) {
+        mapData[y][x] += heat;
+    }
+};
+
+const clusterRegion = (y, x, i, heat) => {
+    addIfWithin(y + i, x, heat);
+    addIfWithin(y + i, x + i, heat);
+    addIfWithin(y, x + i, heat);
+};
+
+const placeCluster = (x, y, size) => {
+    addIfWithin(y, x, size);
+
+    for (let i = 1; i <= size; i++) {
+        const heat = size - i;
+        clusterRegion(y, x, i, heat);
+        clusterRegion(y, x, -i, heat);
+        addIfWithin(y - i, x + i, heat);
+        addIfWithin(y + i, x - i, heat);
+    }
+};
+
+while (randomClusters--) {
+    const
+        clusterXPos = randomFloored(w),
+        clusterYPos = randomFloored(h),
+        clusterSize = randomFloored(maxClusterSize);
+
+    placeCluster(clusterXPos, clusterYPos, clusterSize);
+}
+
+const points = mapData.map(
+    (row, yIndex) => row.map(
+        (heat, xIndex) => (
+            { x: xIndex, y: yIndex, value: heat }
+        )
+    )
+);
 
 Highcharts.chart('container', {
     chart: {
@@ -34,15 +69,16 @@ Highcharts.chart('container', {
     },
     colorAxis: {
         stops: [
-            [0, '#3060cf'],
-            [0.5, '#FFFF00'],
-            [0.9, '#D22B2B']
+            [0, '#3D00FF'],
+            [0.32, '#00FFBC'],
+            [0.64, '#C2FF00'],
+            [0.96, '#FF0043']
         ]
     },
     series: [{
-        colsize: 3,
-        rowsize: 3,
-        data: data,
+        colsize: 1,
+        rowsize: 1,
+        data: points.flat(1),
         interpolation: true,
         opacity: 0.8
     }]
