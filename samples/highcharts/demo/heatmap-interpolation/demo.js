7e4849f112e3d0464269fa058db4f1ffa1b9c7fb
MarkusBarstad <110829288+MarkusBarstad@users.noreply.github.com>
2023-03-23 14:59:23 +0100

    Responsive, gradual decline in opacity

--- samples/highcharts/demo/heatmap-interpolation/demo.js
+++ samples/highcharts/demo/heatmap-interpolation/demo.js
@@ -1,5 +1,6 @@
 Highcharts.chart('container', {
     chart: {
+        height: '80%',
         type: 'heatmap',
         plotBackgroundImage: 'https://cdn.jsdelivr.net/gh/highcharts/highcharts@3c36bd6dc1/samples/graphics/example-screenshot.png'
     },
@@ -24,15 +25,28 @@ Highcharts.chart('container', {
     },
     colorAxis: {
         stops: [
-            [0, 'rgba(61, 0, 255, 0.2)'],
-            [0.16, 'rgba(0, 255, 188, 1.0)'],
-            [0.24, 'rgba(194, 255, 0, 1.0)'],
-            [0.64, 'rgba(255, 0, 67, 1.0)']
+            [0, 'rgba(61, 0, 255, 0.3)'],
+            [0.16, 'rgba(0, 255, 188, 0.5)'],
+            [0.24, 'rgba(194, 255, 0, 0.7)'],
+            [0.64, 'rgba(255, 0, 67, 0.9)']
         ]
     },
     series: [{
         name: 'User activity',
         data: JSON.parse(document.getElementById('data').innerText),
         interpolation: true
-    }]
+    }],
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 800
+            },
+            // Make the labels less space demanding on mobile
+            chartOptions: {
+                chart: {
+                    height: '60%'
+                }
+            }
+        }]
+    }
 });
