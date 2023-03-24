c750c1a0ae5c48ee66b1287cde7fd8271269805d
MarkusBarstad <110829288+MarkusBarstad@users.noreply.github.com>
2023-03-24 04:00:36 +0100

    WIP: Better datagen

--- samples/highcharts/demo/heatmap-interpolation/demo.js
+++ samples/highcharts/demo/heatmap-interpolation/demo.js
@@ -25,13 +25,14 @@ Highcharts.chart('container', {
     },
     colorAxis: {
         stops: [
-            [0, 'rgba(61, 0, 255, 0.3)'],
-            [0.16, 'rgba(0, 255, 188, 0.5)'],
-            [0.24, 'rgba(194, 255, 0, 0.7)'],
-            [0.64, 'rgba(255, 0, 67, 0.9)']
+            [0, 'rgba(61, 0, 255, 0.1)'],
+            [0.3, 'rgba(0, 255, 188, 0.3)'],
+            [0.6, 'rgba(194, 255, 0, 0.6)'],
+            [0.9, 'rgba(255, 0, 67, 0.9)']
         ]
     },
     series: [{
+        turboThreshold: 10000,
         name: 'User activity',
         data: JSON.parse(document.getElementById('data').innerText),
         interpolation: true
