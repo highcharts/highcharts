7a3f30f35534cfffe4bfce1f96b46bad98868fb3
Torstein Hønsi <torstein@highsoft.com>
2023-03-28 07:28:27 +0200

    Samples, failing test

--- samples/unit-tests/annotations/annotations-measure/demo.js
+++ samples/unit-tests/annotations/annotations-measure/demo.js
@@ -97,8 +97,8 @@ QUnit.test('#13664 - annotation measure on yAxis', function (assert) {
         controlPointYPos = y + height / 2;
 
     assert.equal(
-        controlPointYPos,
-        axisMiddlePos,
+        Math.round(controlPointYPos),
+        Math.round(axisMiddlePos),
         `Annotation's control points should be positioned in the middle of yAxis
         #17995`
     );
