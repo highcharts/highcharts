6326a69aff6f3038042f30006fe83082ed910829
karolkolodziej <karolkolodziej01@gmail.com>
2023-05-04 13:19:58 +0200

    Added dataPool to minimal demo.

--- samples/dashboards/demos/minimal/demo.js
+++ samples/dashboards/demos/minimal/demo.js
@@ -1,115 +1,147 @@
-const { CSVConnector } = Dashboards.DataConnector.registry;
-
 const csvData = document.getElementById('csv').innerText,
-    connector = new CSVConnector(void 0, {
-        csv: csvData,
-        firstRowAsNames: true
+    dataPool = new Dashboards.DataPool();
+
+async function setupDashboard() {
+    await dataPool.loadConnector({
+        name: 'Vitamin',
+        type: 'CSVConnector',
+        options: {
+            csv: csvData,
+            firstRowAsNames: true
+        }
+    });
+    dataPool.setConnectorOptions({
+        name: 'Vitamin',
+        type: 'CSVConnector',
+        options: {
+            csv: csvData,
+            firstRowAsNames: true
+        }
     });
 
-connector.load();
+    console.log(dataPool.getConnectorsNames());
 
-Dashboards.board('container', {
-    connector,
-    editMode: {
-        enabled: true,
-        contextMenu: {
+    const dataConnector = await dataPool.getConnector('Vitamin');
+
+    Dashboards.board('container', {
+        editMode: {
             enabled: true,
-            items: ['editMode']
-        }
-    },
-    gui: {
-        layouts: [{
-            id: 'layout-1',
-            rowClassName: 'custom-row',
-            cellClassName: 'custom-cell',
-            rows: [{
-                cells: [{
-                    id: 'dashboard-col-0',
-                    width: '50%'
-                }, {
-                    id: 'dashboard-col-1'
-                }, {
-                    id: 'dashboard-col-12'
-                }]
-            }, {
-                id: 'dashboard-row-1',
-                cells: [{
-                    id: 'dashboard-col-2',
-                    width: '1'
-                }]
-            }]
-        }]
-    },
-    components: [
-        {
-            connector,
-            sync: {
-                visibility: true,
-                highlight: true,
-                selection: true
-            },
-            cell: 'dashboard-col-0',
-            type: 'Highcharts',
-            columnKeyMap: {
-                Food: 'x',
-                'Vitamin A': 'value'
-            },
-            chartOptions: {
-                chart: {
-                    type: 'pie'
-                }
+            contextMenu: {
+                enabled: true,
+                items: ['editMode']
             }
-        }, {
-            cell: 'dashboard-col-1',
-            connector,
-            sync: {
-                visibility: true,
-                highlight: true,
-                selection: true
-            },
-            type: 'Highcharts',
-            columnKeyMap: {
-                Food: 'x',
-                'Vitamin A': 'y'
-            },
-            chartOptions: {
-                xAxis: {
-                    type: 'category'
+        },
+        gui: {
+            layouts: [
+                {
+                    id: 'layout-1',
+                    rowClassName: 'custom-row',
+                    cellClassName: 'custom-cell',
+                    rows: [
+                        {
+                            cells: [
+                                {
+                                    id: 'dashboard-col-0',
+                                    width: '50%'
+                                },
+                                {
+                                    id: 'dashboard-col-1'
+                                },
+                                {
+                                    id: 'dashboard-col-12'
+                                }
+                            ]
+                        },
+                        {
+                            id: 'dashboard-row-1',
+                            cells: [
+                                {
+                                    id: 'dashboard-col-2',
+                                    width: '1'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        components: [
+            {
+                connector: dataConnector,
+                sync: {
+                    visibility: true,
+                    highlight: true,
+                    selection: true
                 },
-                chart: {
-                    animation: false,
-                    type: 'column'
-                }
-            }
-        }, {
-            cell: 'dashboard-col-12',
-            connector,
-            sync: {
-                visibility: true,
-                highlight: true,
-                selection: true
-            },
-            type: 'Highcharts',
-            columnKeyMap: {
-                Food: 'x',
-                'Vitamin A': 'y'
-            },
-            chartOptions: {
-                xAxis: {
-                    type: 'category'
+                cell: 'dashboard-col-0',
+                type: 'Highcharts',
+                columnKeyMap: {
+                    Food: 'x',
+                    'Vitamin A': 'value'
                 },
-                chart: {
-                    animation: false,
-                    type: 'scatter'
+                chartOptions: {
+                    chart: {
+                        type: 'pie'
+                    }
+                }
+            },
+            {
+                cell: 'dashboard-col-1',
+                connector: dataConnector,
+                sync: {
+                    visibility: true,
+                    highlight: true,
+                    selection: true
+                },
+                type: 'Highcharts',
+                columnKeyMap: {
+                    Food: 'x',
+                    'Vitamin A': 'y'
+                },
+                chartOptions: {
+                    xAxis: {
+                        type: 'category'
+                    },
+                    chart: {
+                        animation: false,
+                        type: 'column'
+                    }
+                }
+            },
+            {
+                cell: 'dashboard-col-12',
+                connector: dataConnector,
+                sync: {
+                    visibility: true,
+                    highlight: true,
+                    selection: true
+                },
+                type: 'Highcharts',
+                columnKeyMap: {
+                    Food: 'x',
+                    'Vitamin A': 'y'
+                },
+                chartOptions: {
+                    xAxis: {
+                        type: 'category'
+                    },
+                    chart: {
+                        animation: false,
+                        type: 'scatter'
+                    }
+                }
+            },
+            {
+                cell: 'dashboard-col-2',
+                type: 'DataGrid',
+                connector: dataConnector,
+                editable: true,
+                sync: {
+                    highlight: true
                 }
             }
-        }, {
-            cell: 'dashboard-col-2',
-            type: 'DataGrid',
-            connector,
-            editable: true,
-            sync: {
-                highlight: true
-            }
-        }]
-});
+        ]
+    });
+}
+
+setupDashboard();
