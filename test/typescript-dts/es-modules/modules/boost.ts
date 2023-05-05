5457bd8d70b35a4dd17e68bef66895eb27aded63
bre1470 <40056287+bre1470@users.noreply.github.com>
2023-05-05 14:48:19 +0200

    Fixed doclets and test to reflect boost master.

--- test/typescript-dts/es-modules/modules/boost.ts
+++ test/typescript-dts/es-modules/modules/boost.ts
@@ -1,7 +1,5 @@
 import Highcharts from 'highcharts/es-modules/masters/highcharts.src.js';
 import 'highcharts/es-modules/masters/modules/boost.src.js';
 
-const chart = Highcharts.chart('container', {});
-
-// Highcharts.Chart should be composed with boost functions:
-chart.isChartSeriesBoosting(chart);
+// Highcharts should be composed with boost functions:
+Highcharts.hasWebGLSupport();
