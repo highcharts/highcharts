627ef1c3ef3fea9503e81bbc1704a5f3c6c716e9
Jakub Szumiński <jakubszuminskixrp@gmail.com>
2023-05-07 19:38:23 +0200

    Added fill-opacity attribute.

--- samples/unit-tests/series-area/fillopacity/demo.js
+++ samples/unit-tests/series-area/fillopacity/demo.js
@@ -10,7 +10,8 @@ QUnit.test('Fill opacity zero (#4888)', function (assert) {
         },
         series: [
             {
-                data: [1, 3, 2, 4]
+                data: [1, 3, 2, 4],
+                color: '#A1A1A1'
             },
             {
                 data: [3, 2, 5, 2],
@@ -20,23 +21,29 @@ QUnit.test('Fill opacity zero (#4888)', function (assert) {
         ]
     });
 
+    // Testing HEX colors
     assert.strictEqual(
         chart.series[0].area.element.getAttribute('fill').replace(/ /g, ''),
-        'rgba(44,175,254,0)',
+        '#A1A1A1',
+        'Fill color should be set'
+    );
+
+    assert.strictEqual(
+        chart.series[0].area.element.getAttribute('fill-opacity'),
+        '0',
         'Fill opacity should be set'
     );
 
-    const areaFill = chart.series[1].area.element.getAttribute('fill').replace(/ /g, '');
-    assert.ok(
-        areaFill === 'blue' || areaFill === 'rgba(0,0,255,0.25)',
-        'There should be support for HTML color names'
+    // Testing HTML colors
+    assert.strictEqual(
+        chart.series[1].area.element.getAttribute('fill').replace(/ /g, ''),
+        'blue',
+        'HTML color names should be supported'
     );
 
-    if (areaFill === 'blue') {
-        assert.strictEqual(
-            chart.series[1].area.element.getAttribute('fill-opacity'),
-            '0.25',
-            'There should be opacity set for the HTML color names'
-        );
-    }
+    assert.strictEqual(
+        chart.series[1].area.element.getAttribute('fill-opacity'),
+        '0.25',
+        'Fill opacity should be set when no fillColor defined'
+    );
 });
