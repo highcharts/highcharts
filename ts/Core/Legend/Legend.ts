f35fb39dc8f0175c2d757cd40677f460f9a8b514
Torstein Hønsi <torstein@highsoft.com>
2023-05-11 08:28:37 +0200

    Legend item styling should take immediate effect

--- ts/Core/Legend/Legend.ts
+++ ts/Core/Legend/Legend.ts
@@ -368,12 +368,8 @@ class Legend {
         }
 
         if (!this.chart.styledMode) {
-            const legend = this,
-                options = legend.options,
-                hiddenColor = (legend.itemHiddenStyle as any).color,
-                textColor = visible ?
-                    options.itemStyle.color :
-                    hiddenColor,
+            const { itemHiddenStyle } = this,
+                hiddenColor = (itemHiddenStyle as any).color,
                 symbolColor = visible ?
                     (item.color || hiddenColor) :
                     hiddenColor,
@@ -381,9 +377,9 @@ class Legend {
             let symbolAttr: SVGAttributes = { fill: symbolColor };
 
             if (label) {
-                label.css({
-                    fill: textColor
-                });
+                label.css(
+                    merge(visible ? this.itemStyle : itemHiddenStyle)
+                );
             }
 
             if (line) {
