272125cd04f40c659baf2c9011238ae770dda9f1
Andreas Tistel <andreas.tistel@highsoft.com>
2023-06-22 11:11:38 +0200

    Maps/chart/border - getjson to fetch

--- samples/maps/chart/border/demo.js
+++ samples/maps/chart/border/demo.js
@@ -4,59 +4,60 @@
         'https://code.highcharts.com/mapdata/custom/world.topo.json'
     ).then(response => response.json());
 
-    Highcharts.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/world-population-density.json', function (data) {
+    const data = await fetch(
+        'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/world-population-density.json'
+    ).then(response => response.json());
 
-        // Initialize the chart
-        Highcharts.mapChart('container', {
+    // Initialize the chart
+    Highcharts.mapChart('container', {
 
-            chart: {
-                borderWidth: 1,
-                borderColor: 'silver',
-                borderRadius: 3,
-                shadow: true
-            },
+        chart: {
+            borderWidth: 1,
+            borderColor: 'silver',
+            borderRadius: 3,
+            shadow: true
+        },
 
-            credits: {
-                position: {
-                    x: -10,
-                    y: -10
-                }
-            },
+        credits: {
+            position: {
+                x: -10,
+                y: -10
+            }
+        },
 
+        title: {
+            text: 'Chart with a 1px silver border,  radius 3 and shadow'
+        },
+
+        mapNavigation: {
+            enabled: true,
+            buttonOptions: {
+                alignTo: 'spacingBox',
+                x: 10,
+                verticalAlign: 'bottom'
+            }
+        },
+
+        colorAxis: {
+            min: 1,
+            max: 1000,
+            type: 'logarithmic'
+        },
+
+        legend: {
             title: {
-                text: 'Chart with a 1px silver border,  radius 3 and shadow'
-            },
+                text: 'Population per km²'
+            }
+        },
 
-            mapNavigation: {
-                enabled: true,
-                buttonOptions: {
-                    alignTo: 'spacingBox',
-                    x: 10,
-                    verticalAlign: 'bottom'
-                }
-            },
-
-            colorAxis: {
-                min: 1,
-                max: 1000,
-                type: 'logarithmic'
-            },
-
-            legend: {
-                title: {
-                    text: 'Population per km²'
-                }
-            },
-
-            series: [{
-                data: data,
-                mapData: topology,
-                joinBy: ['iso-a2', 'code'],
-                name: 'Population density',
-                tooltip: {
-                    valueSuffix: '/km²'
-                }
-            }]
-        });
+        series: [{
+            data: data,
+            mapData: topology,
+            joinBy: ['iso-a2', 'code'],
+            name: 'Population density',
+            tooltip: {
+                valueSuffix: '/km²'
+            }
+        }]
     });
 })();
