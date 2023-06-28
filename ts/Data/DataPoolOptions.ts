88dc1565aeb1d1fd2393d10fe4782c171c16057c
karolkolodziej <karolkolodziej01@gmail.com>
2023-06-28 08:40:37 +0200

    Added documentation for DataPoolConnectorOptions.

--- ts/Data/DataPoolOptions.ts
+++ ts/Data/DataPoolOptions.ts
@@ -19,7 +19,7 @@
  *
  * */
 
-import type { DataConnectorTypes } from './Connectors/DataConnectorType.js';
+import type { DataConnectorTypes } from './Connectors/DataConnectorType';
 
 /* *
  *
@@ -33,8 +33,22 @@ export interface DataPoolOptions {
 
 export interface DataPoolConnectorOptions
 <T extends keyof DataConnectorTypes = keyof DataConnectorTypes> {
+    /**
+     * The unique identifier of the connector. Used later when referencing
+     * the connector in the component where it is used.
+     **/
     id: string;
+    /**
+     * The options of the given connector type.
+     **/
     options: DataConnectorTypes[T]['prototype']['options'];
+    /**
+     * The type of the connector, depends on your data source.
+     * Possible values are:
+     * - `CSV`
+     * - `GoogleSheets`
+     * - `HTMLTable`
+     **/
     type: T;
 }
 
