700e5640e01e3acbdf18a28fe866e1d38deeeec0
DJTechnoo <askeltechno@hotmail.com>
2023-06-29 13:55:26 +0200

    Demos: Datagrid, KPI

---
+++ samples/dashboards/datagrid-component/datagrid-options/demo.js
@@ -0,0 +1,40 @@
+const csvData = document.getElementById('csv').innerText;
+
+const dataGridOptions = {
+    columns: {
+        'Vitamin A': {
+            headerFormat: '{text} mg'
+        }
+    }
+};
+
+Dashboards.board('container', {
+    dataPool: {
+        connectors: [{
+            type: 'CSV',
+            id: 'data',
+            options: {
+                csv: csvData,
+                firstRowAsNames: true
+            }
+        }]
+    },
+    gui: {
+        layouts: [{
+            id: 'layout-1',
+            rows: [{
+                cells: [{
+                    id: 'dashboard-col-1'
+                }]
+            }]
+        }]
+    },
+    components: [{
+        cell: 'dashboard-col-1',
+        connector: {
+            id: 'data'
+        },
+        type: 'DataGrid',
+        dataGridOptions
+    }]
+}, true);
