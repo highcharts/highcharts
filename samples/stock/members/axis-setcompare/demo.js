8c5f7580d7e0db23c9a9416c4a297c2f741b64a5
andreastistel <137054126+andreastistel@users.noreply.github.com>
2023-06-29 12:47:23 +0200

    Stock/members/axis-setcompare  - getjson to fetch

--- samples/stock/members/axis-setcompare/demo.js
+++ samples/stock/members/axis-setcompare/demo.js
@@ -1,79 +1,82 @@
-var seriesOptions = [],
-    seriesCounter = 0,
-    names = ['MSFT', 'AAPL', 'GOOG'],
-    chart;
+(async () => {
 
-/**
- * Create the chart
- */
-function createChart() {
+    var seriesOptions = [],
+        seriesCounter = 0,
+        names = ['MSFT', 'AAPL', 'GOOG'],
+        chart;
 
-    chart = Highcharts.stockChart('container', {
+    /**
+     * Create the chart
+     */
+    function createChart() {
 
-        rangeSelector: {
-            selected: 4
-        },
+        chart = Highcharts.stockChart('container', {
 
-        yAxis: {
-            labels: {
-                formatter: function () {
-                    var compare = this.axis.series[0].userOptions.compare || 'none';
-                    return (compare !== 'none' && this.value > 0 ? ' + ' : '') + this.value +
+            rangeSelector: {
+                selected: 4
+            },
+
+            yAxis: {
+                labels: {
+                    formatter: function () {
+                        var compare = this.axis.series[0].userOptions.compare || 'none';
+                        return (compare !== 'none' && this.value > 0 ? ' + ' : '') + this.value +
                         { none: ' USD', value: ' USD', percent: ' %' }[compare];
+                    }
                 }
-            }
-        },
+            },
 
-        plotOptions: {
-            series: {
-                compare: 'value'
-            }
-        },
+            plotOptions: {
+                series: {
+                    compare: 'value'
+                }
+            },
 
-        tooltip: {
-            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} USD</b> ({point.change})<br/>',
-            changeDecimals: 2,
-            valueDecimals: 2
-        },
+            tooltip: {
+                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} USD</b> ({point.change})<br/>',
+                changeDecimals: 2,
+                valueDecimals: 2
+            },
 
-        series: seriesOptions
-    });
-}
-
-function success(data) {
-    var name = this.url.match(/(msft|aapl|goog)/)[0].toUpperCase();
-    var i = names.indexOf(name);
-    seriesOptions[i] = {
-        name: name,
-        data: data
-    };
-
-    // As we're loading the data asynchronously, we don't know what order it
-    // will arrive. So we keep a counter and create the chart when all the data is loaded.
-    seriesCounter += 1;
-
-    if (seriesCounter === names.length) {
-        createChart();
+            series: seriesOptions
+        });
     }
-}
 
-Highcharts.getJSON(
-    'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/msft-c.json',
-    success
-);
-Highcharts.getJSON(
-    'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/aapl-c.json',
-    success
-);
-Highcharts.getJSON(
-    'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/goog-c.json',
-    success
-);
+    function success(url, data) {
+        var name = url.match(/(msft|aapl|goog)/)[0].toUpperCase();
+        var i = names.indexOf(name);
+        seriesOptions[i] = {
+            name: name,
+            data: data
+        };
 
-// buttons behaviour
-document.querySelectorAll('button.compare').forEach(function (button) {
-    button.addEventListener('click', function () {
-        var compare = this.getAttribute('data-compare');
-        chart.yAxis[0].setCompare(compare);
+        // As we're loading the data asynchronously, we don't know what order it
+        // will arrive. So we keep a counter and create the chart when all the data is loaded.
+        seriesCounter += 1;
+
+        if (seriesCounter === names.length) {
+            createChart();
+        }
+    }
+
+    await fetch('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/msft-c.json')
+        .then(response => response.json())
+        .then(data => success('msft', data));
+
+    await fetch('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/aapl-c.json')
+        .then(response => response.json())
+        .then(data => success('aapl', data));
+
+    await fetch('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/goog-c.json')
+        .then(response => response.json())
+        .then(data => success('goog', data));
+
+    // buttons behaviour
+    document.querySelectorAll('button.compare').forEach(function (button) {
+        button.addEventListener('click', function () {
+            var compare = this.getAttribute('data-compare');
+            chart.yAxis[0].setCompare(compare);
+        });
     });
-});
+
+})();
