d4fcdaddaace37ed913047d04e6fb1651516d3f8
DJTechnoo <askeltechno@hotmail.com>
2023-06-30 14:56:53 +0200

    Demos: Added all remaining samples.

---
+++ samples/dashboards/highcharts-components/highcharts/demo.js
@@ -0,0 +1,28 @@
+Dashboards.board('container', {
+    gui: {
+        layouts: [{
+            id: 'layout-1',
+            rows: [{
+                cells: [{
+                    id: 'dashboard-col-0'
+                }]
+            }]
+        }]
+    },
+    components: [{
+        cell: 'dashboard-col-0',
+        type: 'Highcharts',
+        chartOptions: {
+            legend: {
+                align: 'right',
+                verticalAlign: 'top',
+                layout: 'vertical',
+                x: 0,
+                y: 100
+            },
+            series: [{
+                data: [1, 2, 3, 4]
+            }]
+        }
+    }]
+});
