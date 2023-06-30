3798e9ad0e4ca66540acfce2be29ddf653b31818
Pawel Lysy <pawel.lysy@blacklabel.pl>
2023-06-30 14:48:40 +0200

    Improved changelog script.

--- changelog/generate.js
+++ changelog/generate.js
@@ -301,6 +301,9 @@ const getFile = url => new Promise((resolve, reject) => {
 
         if (params.dashboards && params.release) {
             const version = params.release;
+            if (!/^\d+\.\d+\.\d(?:-\w+)$/su.test(version)) {
+                throw new Error('No valid `--release x.x.x` provided.');
+            }
             const dashboardsName = 'Highcharts Dashboards';
             const dashboardsProduct = {
                 'Highcharts Dashboards': {
