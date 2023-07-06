7c13b6c9ca4ee3f3331e5a47154440ab2eba9100
andreastistel <137054126+andreastistel@users.noreply.github.com>
2023-07-06 09:20:04 +0200

    Highcharts/members - updated keywords

--- samples/highcharts/members/highcharts-numberformat/demo.js
+++ samples/highcharts/members/highcharts-numberformat/demo.js
@@ -1,4 +1,4 @@
-var converters = {
+const converters = {
     // Latin to Farsi
     fa: function (number) {
         return number.toString().replace(/\d/g, function (d) {
@@ -24,7 +24,7 @@ Highcharts.chart('container', {
 
     chart: {
         numberFormatter: function () {
-            var ret = Highcharts.numberFormat.apply(0, arguments);
+            const ret = Highcharts.numberFormat.apply(0, arguments);
             return converters.ar(ret);
         },
         type: 'column'
@@ -35,7 +35,8 @@ Highcharts.chart('container', {
     },
 
     series: [{
-        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0,
+            135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
         dataLabels: {
             enabled: true,
             format: '{y:.1f}',
