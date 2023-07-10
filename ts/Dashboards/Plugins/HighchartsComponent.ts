d7054e7085593adfa66bca321c3ae6e5febf1a27
Pawel Lysy <pawel.lysy@blacklabel.pl>
2023-07-10 17:17:54 +0200

    Implemented dashboards/lint and karma-test gulptasks.

--- ts/Dashboards/Plugins/HighchartsComponent.ts
+++ ts/Dashboards/Plugins/HighchartsComponent.ts
@@ -27,7 +27,7 @@ import type {
     AxisOptions,
     Chart,
     Options as ChartOptions,
-    Highcharts,
+    Highcharts as HighchartsConstructor,
     Point,
     Series,
     SeriesOptions
@@ -58,10 +58,10 @@ const {
 
 declare module '../../Core/GlobalsLike' {
     interface GlobalsLike {
-        chart: typeof Highcharts.chart;
-        ganttChart: typeof Highcharts.chart;
-        mapChart: typeof Highcharts.chart;
-        stockChart: typeof Highcharts.chart;
+        chart: typeof HighchartsConstructor.chart;
+        ganttChart: typeof HighchartsConstructor.chart;
+        mapChart: typeof HighchartsConstructor.chart;
+        stockChart: typeof HighchartsConstructor.chart;
     }
 }
 
@@ -85,7 +85,7 @@ class HighchartsComponent extends Component {
      * */
 
     /** @private */
-    public static charter?: typeof Highcharts;
+    public static charter?: typeof HighchartsConstructor;
 
     /** @private */
     public static syncHandlers = HighchartsSyncHandlers;
@@ -713,7 +713,7 @@ class HighchartsComponent extends Component {
     private createChart(): Chart {
         const charter = (
             HighchartsComponent.charter ||
-            Globals.win.Highcharts as unknown as typeof Highcharts
+            Globals.win.Highcharts as unknown as typeof HighchartsConstructor
         );
         if (this.chartConstructor !== 'chart') {
             const factory = charter[this.chartConstructor];
