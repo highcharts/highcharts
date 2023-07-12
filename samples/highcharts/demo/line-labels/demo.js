111b08ea5d7b7bf579e3221b84b1057d65ead647
andreastistel <137054126+andreastistel@users.noreply.github.com>
2023-07-12 07:50:09 +0200

    Updated dataset and colors.

--- samples/highcharts/demo/line-labels/demo.js
+++ samples/highcharts/demo/line-labels/demo.js
@@ -1,22 +1,25 @@
-// Data retrieved https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature
+// Data retrieved https://www.yr.no/nb/historikk/tabell/1-92416/Norway/Vestland/Bergen/Bergen?q=siste-13-m%C3%A5neder
+// https://www.yr.no/nb/historikk/tabell/5-53070/Norge/Vestland/Vik/Vik%20i%20Sogn?q=siste-13-m%C3%A5neder
+// https://www.yr.no/nb/historikk/tabell/5-15660/Norge/Innlandet/Skj%C3%A5k/Skj%C3%A5k%20(Austin)?q=siste-13-m%C3%A5neder
+
 Highcharts.chart('container', {
     chart: {
         type: 'line'
     },
     title: {
-        text: 'Monthly Average Temperature'
+        text: 'Monthly Average Rainfall'
     },
     subtitle: {
         text: 'Source: ' +
-            '<a href="https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature" ' +
-            'target="_blank">Wikipedia.com</a>'
+            '<a href="https://www.yr.no" ' +
+            'target="_blank">www.yr.no</a>'
     },
     xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        categories: ['Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
     },
     yAxis: {
         title: {
-            text: 'Temperature (°C)'
+            text: 'Rainfall (mm)'
         }
     },
     plotOptions: {
@@ -28,12 +31,19 @@ Highcharts.chart('container', {
         }
     },
     series: [{
-        name: 'Reggane',
-        data: [16.0, 18.2, 23.1, 27.9, 32.2, 36.4, 39.8, 38.4, 35.5, 29.2,
-            22.0, 17.8]
+        name: 'Vik i sogn',
+        color: '#f50aed',
+        data: [45.3, 80.8, 86.8, 47.9, 155.4, 76.2, 86.1, 163.3, 161.5, 81.1,
+            12.6, 34.0, 23.2]
     }, {
-        name: 'Tallinn',
-        data: [-2.9, -3.6, -0.6, 4.8, 10.2, 14.5, 17.6, 16.5, 12.0, 6.5,
-            2.0, -0.9]
+        name: 'Bergen',
+        color: '#051efc',
+        data: [164.1, 233.4, 169.2, 163.7, 304.6, 190.7, 226.5, 297.8, 260.1,
+            181.0, 42.8, 81.9, 67.8]
+    }, {
+        name: 'Skjåk',
+        color: '#0df201',
+        data: [47.2, 31.3, 46.0, 51.2, 46.8, 25.5, 22.8, 21.2, 47.1,
+            12.1, 15.1, 16.5, 22.9]
     }]
 });
