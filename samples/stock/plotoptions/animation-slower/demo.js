1d4224f73100148f579d7cc60345e8f54120612f
andreastistel <137054126+andreastistel@users.noreply.github.com>
2023-07-17 13:34:40 +0200

    Stock/plotoptions/animation-slower - usdeur fetch

--- samples/stock/plotoptions/animation-slower/demo.js
+++ samples/stock/plotoptions/animation-slower/demo.js
@@ -1,19 +1,26 @@
-Highcharts.stockChart('container', {
+(async () => {
 
-    plotOptions: {
-        series: {
-            animation: {
-                duration: 2000
+    const usdeur = await fetch(
+        'https://www.highcharts.com/samples/data/usdeur.json'
+    ).then(response => response.json());
+
+    Highcharts.stockChart('container', {
+
+        plotOptions: {
+            series: {
+                animation: {
+                    duration: 2000
+                }
             }
-        }
-    },
+        },
 
-    rangeSelector: {
-        selected: 1
-    },
+        rangeSelector: {
+            selected: 1
+        },
 
-    series: [{
-        name: 'USD to EUR',
-        data: usdeur
-    }]
-});
+        series: [{
+            name: 'USD to EUR',
+            data: usdeur
+        }]
+    });
+})();
