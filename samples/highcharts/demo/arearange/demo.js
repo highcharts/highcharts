a97f1f8b6848679140a52248dfec86fb46629396
andreastistel <137054126+andreastistel@users.noreply.github.com>
2023-07-20 14:30:16 +0200

    Added color gradient based on temp

--- samples/highcharts/demo/arearange/demo.js
+++ samples/highcharts/demo/arearange/demo.js
@@ -1,11 +1,24 @@
 (async () => {
-
     const data = await fetch(
         'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/range.json'
     ).then(response => response.json());
 
-    Highcharts.chart('container', {
+    // Function to calculate the color based on temperature value
+    const getTemperatureColor = temperature => {
+        const minTemp = -20;
+        const maxTemp = 30;
+        const normalizedTemp = (temperature - minTemp) / (maxTemp - minTemp);
+        const r = Math.round(255 - normalizedTemp * 255);
+        const b = Math.round(255 * normalizedTemp);
+        return `rgb(${r}, 0, ${b})`;
+    };
 
+    // Set color for each data point in the 'data' array
+    data.forEach(point => {
+        point.color = getTemperatureColor(point[1]);
+    });
+
+    Highcharts.chart('container', {
         chart: {
             type: 'arearange',
             zoomType: 'x',
@@ -14,39 +27,44 @@
                 scrollPositionX: 1
             }
         },
-
         title: {
             text: 'Temperature variation by day'
         },
-
         xAxis: {
             type: 'datetime',
             accessibility: {
                 rangeDescription: 'Range: Jan 1st 2017 to Dec 31 2017.'
             }
         },
-
         yAxis: {
             title: {
                 text: null
             }
         },
-
         tooltip: {
             crosshairs: true,
             shared: true,
             valueSuffix: '°C',
             xDateFormat: '%A, %b %e'
         },
-
         legend: {
             enabled: false
         },
-
         series: [{
             name: 'Temperatures',
-            data: data
+            data: data,
+            color: {
+                linearGradient: {
+                    x1: 0,
+                    x2: 0,
+                    y1: 0,
+                    y2: 1
+                },
+                stops: [
+                    [0, getTemperatureColor(-20)],
+                    [1, getTemperatureColor(30)]
+                ]
+            }
         }]
-
     });
 })();
