859b4f683a31127047c77f98013ec9eb81e903d8
Jakub Szumiński <jakubszuminskixrp@gmail.com>
2023-07-21 09:56:33 +0200

    Added a test.

--- samples/unit-tests/series-pie/resize/demo.js
+++ samples/unit-tests/series-pie/resize/demo.js
@@ -15,7 +15,14 @@ QUnit.test('Redraw with dirty box', function (assert) {
                 dataLabels: {
                     enabled: false
                 },
-                slicedOffset: 0
+                slicedOffset: 0,
+                allowPointSelect: true,
+                states: {
+                    select: {
+                        enabled: false,
+                        color: 'red'
+                    }
+                }
             }
         ]
     });
@@ -28,11 +35,21 @@ QUnit.test('Redraw with dirty box', function (assert) {
         'Pie fills plot'
     );
 
+    // Select one point before resizing
+    chart.series[0].points[0].select(true);
+
     chart.setTitle({ text: null });
     assert.ok(chart.plotHeight > oldPlotHeight, 'Plot height is increased');
     assert.strictEqual(
         chart.series[0].center[2], // The best I could find to test. Change the test if the logic is changed.
         chart.plotHeight,
         'Pie fills plot'
+    );
+
+    // Check if the styles from series.states.select were applied
+    assert.notStrictEqual(
+        chart.series[0].points[0].graphic.attr('fill'),
+        'red',
+        'Styles from a disabled series state should not be applied, #19402.'
     );
 });
