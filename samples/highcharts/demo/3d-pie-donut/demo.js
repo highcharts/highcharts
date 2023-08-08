a04e9706bd943ed9091882b565c1ba14b6f67385
Tord <tord@Tords-MacBook-Pro.local>
2023-08-08 11:01:13 +0200

    Changed posittions of lables

--- samples/highcharts/demo/3d-pie-donut/demo.js
+++ samples/highcharts/demo/3d-pie-donut/demo.js
@@ -18,22 +18,101 @@ Highcharts.chart('container', {
     plotOptions: {
         pie: {
             innerSize: 100,
-            depth: 45
+            depth: 45,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}',
+                distance: -50,
+                style: {
+                    color: '#000000'
+                }
+            }
         }
     },
     series: [{
         name: 'Medals',
         data: [
-            ['Norway', 16],
-            ['Germany', 12],
-            ['USA', 8],
-            ['Sweden', 8],
-            ['Netherlands', 8],
-            ['ROC', 6],
-            ['Austria', 7],
-            ['Canada', 4],
-            ['Japan', 3]
-
+            {
+                name: 'Norway',
+                y: 16,
+                dataLabels: {
+                    x: 20,
+                    y: -20,
+                    rotation: 30
+                }
+            },
+            {
+                name: 'Germany',
+                y: 12,
+                dataLabels: {
+                    x: 55,
+                    y: 75,
+                    rotation: 300
+                }
+            },
+            {
+                name: 'USA',
+                y: 8,
+                dataLabels: {
+                    x: -30,
+                    y: 95,
+                    rotation: 350
+                }
+            },
+            {
+                name: 'Sweden',
+                y: 8,
+                dataLabels: {
+                    x: 30,
+                    y: 95,
+                    rotation: 15
+                }
+            },
+            {
+                name: 'Netherlands',
+                y: 8,
+                dataLabels: {
+                    x: -45,
+                    y: 85,
+                    rotation: 50
+                }
+            },
+            {
+                name: 'ROC',
+                y: 6,
+                dataLabels: {
+                    x: -55,
+                    y: 20,
+                    rotation: 273
+                }
+            },
+            {
+                name: 'Austria',
+                y: 7,
+                dataLabels: {
+                    x: -35,
+                    y: -10,
+                    rotation: 313
+                }
+            },
+            {
+                name: 'Canada',
+                y: 4,
+                dataLabels: {
+                    x: 15,
+                    y: -35,
+                    rotation: 340
+                }
+            },
+            {
+                name: 'Japan',
+                y: 3,
+                dataLabels: {
+                    x: 63,
+                    y: -47,
+                    rotation: 353
+                }
+            }
         ]
     }]
 });
