870e6c41c19bfb69bb0a9e95d33f6ec0b3566ed7
Pawel Lysy <pawel.lysy@blacklabel.pl>
2023-08-10 14:41:51 +0200

    Improved cypress test.

--- test/cypress/integration/Dashboards/Components/KPI/KPIComponent.cy.js
+++ test/cypress/integration/Dashboards/Components/KPI/KPIComponent.cy.js
@@ -3,18 +3,16 @@ describe('KPI Component test', () => {
         cy.visit('/dashboards/cypress/kpi-with-connector');
     });
 
-    it('KPI Component should be rendered', () => {
-        cy.get('.highcharts-dashboards-component-kpi-value').as('kpiValue');
-        cy.get('@kpiValue').should('be.visible');
-        cy.get('@kpiValue').should('have.text', 1200);
+    it('KPI Component should have a last value of the point when connector attached.', () => {
+        cy.get('.highcharts-dashboards-component-kpi-value')
+            .should('be.visible')
+            .should('have.text', 1200);
     });
 
-    it('KPI Component\'s value should change after selection', () => {
+    it('KPI Component\'s value should change after selection to last visible point.', () => {
         cy.chart().then(chart => {
             chart.xAxis[0].setExtremes(null, Date.UTC(2019, 0, 7));
-            cy.get('.highcharts-dashboards-component-kpi-value').as('kpiValue');
-            cy.get('@kpiValue').should('have.text', 700);
-
-        })
+            cy.get('.highcharts-dashboards-component-kpi-value').should('have.text', 700);
+        });
     });
 });
