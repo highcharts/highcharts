2fb9db3d77e6a5ecf2f076e6cd7c4ad7babdbafd
jedrzejruta <jedrek.ruta@gmail.com>
2023-08-23 15:21:32 +0200

    Fixed #19314, wrong tooltip size with outside prop enabled.

--- samples/unit-tests/tooltip/split/demo.js
+++ samples/unit-tests/tooltip/split/demo.js
@@ -232,6 +232,38 @@ QUnit.test('Split tooltip with useHTML and outside', function (assert) {
         `Tooltip with outside and split properties set to true should be
         rendered properly - y position (#17720).`
     );
+    chart.update({
+        tooltip: {
+            shadow: false
+        }
+    });
+
+    chart.tooltip.refresh(chart.series[0].points[0]);
+    const tooltipClientRect = chart.tooltip.container.getBoundingClientRect();
+
+    chart.update({
+        tooltip: {
+            shadow: true
+        }
+    });
+
+    chart.tooltip.refresh(chart.series[0].points[0]);
+
+    assert.close(
+        tooltipClientRect.width,
+        chart.tooltip.container.getBoundingClientRect().width,
+        1,
+        `Tooltip's shadow offsetX should be evaluated in container width
+        to avoid cutting it off (#19314).`
+    );
+
+    assert.close(
+        tooltipClientRect.height,
+        chart.tooltip.container.getBoundingClientRect().height,
+        1,
+        `Tooltip's shadow offsetY should be evaluated in container height
+        to avoid cutting it off (#19314).`
+    );
 });
 
 QUnit.test('Split tooltip in floated container (#13943),', function (assert) {
