7a04690e6bee2123e3c971c260ba2ea09c805f4b
Pawel Lysy <pawel.lysy@blacklabel.pl>
2023-08-31 13:27:24 +0200

    Improved formatting.

--- samples/dashboards/demo/datagrid-sync/demo.js
+++ samples/dashboards/demo/datagrid-sync/demo.js
@@ -56,87 +56,75 @@ Dashboards.board('container', {
         ]
     },
     gui: {
-        layouts: [
-            {
-                id: 'layout-1',
-                rows: [
-                    {
-                        cells: [
-                            {
-                                responsive: {
-                                    small: {
-                                        width: '100%'
-                                    }
-                                },
-                                id: 'dashboard-col-0'
-                            },
-                            {
-                                responsive: {
-                                    small: {
-                                        width: '100%'
-                                    }
-                                },
-                                id: 'dashboard-col-1'
-                            },
-                            {
-                                id: 'dashboard-col-2'
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
+        layouts: [{
+            id: 'layout-1',
+            rows: [{
+                cells: [{
+                    responsive: {
+                        small: {
+                            width: '100%'
+                        }
+                    },
+                    id: 'dashboard-col-0'
+                }, {
+                    responsive: {
+                        small: {
+                            width: '100%'
+                        }
+                    },
+                    id: 'dashboard-col-1'
+                }, {
+                    id: 'dashboard-col-2'
+                }]
+            }]
+        }]
     },
-    components: [
-        {
-            cell: 'dashboard-col-0',
-            type: 'Highcharts',
-            connector: {
-                id: 'synchro-data'
-            },
-            sync: {
-                highlight: true
-            },
-            columnAssignment: {
-                Food: 'x',
-                'Vitamin A': 'y'
-            },
-            chartOptions: chartOptions
+    components: [{
+        cell: 'dashboard-col-0',
+        type: 'Highcharts',
+        connector: {
+            id: 'synchro-data'
         },
-        {
-            cell: 'dashboard-col-1',
-            connector: {
-                id: 'synchro-data'
-            },
-            type: 'Highcharts',
-            sync: {
-                highlight: true
-            },
-            columnAssignment: {
-                Food: 'x',
-                'Vitamin A': 'y'
-            },
-            allowConnectorUpdate: false,
-            chartOptions: Highcharts.merge(chartOptions, {
-                title: {
-                    text: 'Dragging points does not affect other components'
-                },
-                subtitle: {
-                    useHTML: true,
-                    text: 'Dragging points <em>will not update</em> the grid'
-                }
-            })
+        sync: {
+            highlight: true
         },
-        {
-            cell: 'dashboard-col-2',
-            connector: {
-                id: 'synchro-data'
+        columnAssignment: {
+            Food: 'x',
+            'Vitamin A': 'y'
+        },
+        chartOptions: chartOptions
+    }, {
+        cell: 'dashboard-col-1',
+        connector: {
+            id: 'synchro-data'
+        },
+        type: 'Highcharts',
+        sync: {
+            highlight: true
+        },
+        columnAssignment: {
+            Food: 'x',
+            'Vitamin A': 'y'
+        },
+        allowConnectorUpdate: false,
+        chartOptions: Highcharts.merge(chartOptions, {
+            title: {
+                text: 'Dragging points does not affect other components'
             },
-            type: 'DataGrid',
-            editable: true,
-            sync: {
-                highlight: true
+            subtitle: {
+                useHTML: true,
+                text: 'Dragging points <em>will not update</em> the grid'
             }
+        })
+    }, {
+        cell: 'dashboard-col-2',
+        connector: {
+            id: 'synchro-data'
+        },
+        type: 'DataGrid',
+        editable: true,
+        sync: {
+            highlight: true
         }
-    ]
+    }]
 });
