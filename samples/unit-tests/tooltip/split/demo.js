3d418295f361630b58f4a7ba7275ee05a9402c13
Torstein Hønsi <torstein@highsoft.com>
2023-08-31 09:24:03 +0200

    Simplified fix for tooltip.outside clip. Typesafing.

--- samples/unit-tests/tooltip/split/demo.js
+++ samples/unit-tests/tooltip/split/demo.js
@@ -226,7 +226,7 @@ QUnit.test('Split tooltip with useHTML and outside', function (assert) {
 
     assert.close(
         chart.yAxis[0].toPixels(point.y),
-        tooltipClient.y + tooltipClient.height -
+        tooltipClient.y + tooltipClient.height - 2 -
             pointBox.height - (pointBox.height / 2),
         4,
         `Tooltip with outside and split properties set to true should be
