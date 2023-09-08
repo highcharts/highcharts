3fd037cbb3eea4f90d500401b13eb57f02bc3811
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2023-09-08 16:07:38 +0200

    Added separate dashboards conffig, more specific pattern in base config

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -4,7 +4,7 @@ export default defineConfig({
     fixturesFolder: 'test/cypress/fixtures',
     e2e: {
         baseUrl: 'http://localhost:3030/samples/view?mobile=true&path=/',
-        specPattern: 'test/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
+        specPattern: 'test/cypress/integration/Highcharts/**/*.cy.{js,jsx,ts,tsx}',
         supportFile: 'test/cypress/support/index.js',
         testIsolation: false
     }
