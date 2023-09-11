3276e4691eb99e8c48c51464ba7ec084efd1cf8b
Sebastian Bochan <sebastian@blacklabel.pl>
2023-09-11 12:12:52 +0200

    Fixed typo.

--- test/cypress/integration/DataGrid/cell-format.cy.js
+++ test/cypress/integration/DataGrid/cell-format.cy.js
@@ -3,7 +3,7 @@ describe('Remove the dashboard.', () => {
         cy.visit('/dashboards/cypress/datagrid-cell-formatter');
     });
 
-    it('Dates un cells should be formatted.', () => {
+    it('Dates in cells should be formatted.', () => {
         cy.get('.highcharts-datagrid-cell').eq(0).should('have.text', '2022-01-01');
     });
 });
