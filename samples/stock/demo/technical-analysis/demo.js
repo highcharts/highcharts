1bc0ff989b532773ef4cf40254158a0a57dffed1
Kamil Musiałowski <kamil.musialowski@blacklabel.pl>
2023-09-21 13:18:10 +0200

    Removed static data, using JSON fetch instead.

--- samples/stock/demo/technical-analysis/demo.js
+++ samples/stock/demo/technical-analysis/demo.js
@@ -1,59 +1,66 @@
-Highcharts.stockChart('container', {
+(async () => {
 
-    yAxis: {
-        plotBands: [{
-            color: '#FDFD96',
-            from: 182.94,
-            to: 177.57,
-            zIndex: 3,
-            label: {
-                text: 'Resistance Zone'
-            }
-        }]
-    },
+    const data = await fetch(
+        'https://cdn.jsdelivr.net/gh/highcharts/highcharts@c44c51a/samples/data/mini-ohlc.json'
+    ).then(response => response.json());
 
-    annotations: [{
-        type: 'fibonacci',
-        langKey: 'Fibonacci',
-        typeOptions: {
-            points: [{
-                x: 1631021400000,
-                y: 157.26
-            }, {
-                x: 1633354200000,
-                y: 157.26
-            }],
-            height: 138.27 - 157.26,
-            xAxis: 0,
-            yAxis: 0
-        }
-    }, {
-        type: 'crookedLine',
-        langKey: 'Trendline',
-        typeOptions: {
-            points: [{
-                x: 1636727400000,
-                y: 147.48
-            }, {
-                x: 1642516200000,
-                y: 182.5
+    Highcharts.stockChart('container', {
+
+        yAxis: {
+            plotBands: [{
+                color: '#FDFD96',
+                from: 182.94,
+                to: 177.57,
+                zIndex: 3,
+                label: {
+                    text: 'Resistance Zone'
+                }
             }]
         },
-        shapeOptions: {
-            stroke: 'orange',
-            strokeWidth: 2
-        }
-    }],
 
-    series: [{
-        id: 'main',
-        type: 'candlestick',
-        color: '#FF6F6F',
-        upColor: '#6FB76F',
-        data: data, //eslint-disable-line
-        dataGrouping: {
-            enabled: false
-        }
-    }]
+        annotations: [{
+            type: 'fibonacci',
+            langKey: 'Fibonacci',
+            typeOptions: {
+                points: [{
+                    x: 1631021400000,
+                    y: 157.26
+                }, {
+                    x: 1633354200000,
+                    y: 157.26
+                }],
+                height: 138.27 - 157.26,
+                xAxis: 0,
+                yAxis: 0
+            }
+        }, {
+            type: 'crookedLine',
+            langKey: 'Trendline',
+            typeOptions: {
+                points: [{
+                    x: 1636727400000,
+                    y: 147.48
+                }, {
+                    x: 1642516200000,
+                    y: 182.5
+                }]
+            },
+            shapeOptions: {
+                stroke: 'orange',
+                strokeWidth: 2
+            }
+        }],
 
-});
+        series: [{
+            id: 'main',
+            type: 'candlestick',
+            color: '#FF6F6F',
+            upColor: '#6FB76F',
+            data: data, //eslint-disable-line
+            dataGrouping: {
+                enabled: false
+            }
+        }]
+
+    });
+})();
