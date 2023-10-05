fba5fd450d0072bb951b1bcc911337ef628a8245
bre1470 <40056287+bre1470@users.noreply.github.com>
2023-10-05 10:42:20 +0200

    Splitted ParallelAxis composition from ParallelCoordinates.

--- ts/masters/modules/parallel-coordinates.src.ts
+++ ts/masters/modules/parallel-coordinates.src.ts
@@ -10,4 +10,12 @@
  * License: www.highcharts.com/license
  */
 'use strict';
-import '../../Extensions/ParallelCoordinates.js';
+import Highcharts from '../../Core/Globals.js';
+import ParallelCoordinates from '../../Extensions/ParallelCoordinates/ParallelCoordinates.js';
+const G: AnyRecord = Highcharts;
+ParallelCoordinates.compose(
+    G.Axis,
+    G.Chart,
+    G.defaultOptions,
+    G.Series
+);
