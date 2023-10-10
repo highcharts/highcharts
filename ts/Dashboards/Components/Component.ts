a44a38178b2da293864f2a2d95bf985c0fb27660
Dawid Draguła <dawid.dragula@gmail.com>
2023-10-10 18:18:54 +0200

    Updated docs and API for crossfilter.

--- ts/Dashboards/Components/Component.ts
+++ ts/Dashboards/Components/Component.ts
@@ -1154,7 +1154,8 @@ namespace Component {
 
     /**
      * The sync can be an object configuration containing: `highlight`,
-     * `visibility` or `extremes`.
+     * `visibility` or `extremes`. For the Navigator Component `crossfilter`
+     * sync can be used.
      * ```
      * Example:
      * {
@@ -1219,6 +1220,8 @@ namespace Component {
          * {@link https://jsfiddle.net/gh/get/library/pure/highcharts/highcharts/tree/master/samples/dashboards/component-options/sync-highlight/ | Highlight Sync }
          *
          * {@link https://jsfiddle.net/gh/get/library/pure/highcharts/highcharts/tree/master/samples/dashboards/component-options/sync-visibility/ | Visibility Sync }
+         *
+         * {@link https://jsfiddle.net/gh/get/library/pure/highcharts/highcharts/tree/master/samples/dashboards/demo/crossfilter | Crossfilter Sync } (Navigator Component only)
          */
         sync: SyncOptions;
         /**
