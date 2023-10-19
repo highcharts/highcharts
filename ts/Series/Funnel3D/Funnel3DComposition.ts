65443b4d4769cd34a97bf002257db88186a8407d
bre1470 <40056287+bre1470@users.noreply.github.com>
2023-10-19 19:09:57 +0200

    Migrated 3D elements to class pattern, merged cuboid into base.

--- ts/Series/Funnel3D/Funnel3DComposition.ts
+++ ts/Series/Funnel3D/Funnel3DComposition.ts
@@ -20,20 +20,16 @@
  *
  * */
 
-import type BBoxObject from '../../Core/Renderer/BBoxObject';
 import type Chart from '../../Core/Chart/Chart';
 import type ColorType from '../../Core/Color/ColorType';
-import type GradientColor from '../../Core/Color/GradientColor';
 import type SVGAttributes from '../../Core/Renderer/SVG/SVGAttributes';
 import type SVGElement from '../../Core/Renderer/SVG/SVGElement';
 import type SVGElement3D from '../../Core/Renderer/SVG/SVGElement3D';
-import type { SVGElement3DLikeCuboid } from '../../Core/Renderer/SVG/SVGElement3DLike';
 import type SVGPath from '../../Core/Renderer/SVG/SVGPath';
 import type SVGPath3D from '../../Core/Renderer/SVG/SVGPath3D';
 import type SVGRenderer from '../../Core/Renderer/SVG/SVGRenderer';
 
-import Color from '../../Core/Color/Color.js';
-const { parse: color } = Color;
+import SVGElement3DFunnel from './SVGElement3DFunnel.js';
 import H from '../../Core/Globals.js';
 const { charts } = H;
 import SVGRenderer3D from '../../Core/Renderer/SVG/SVGRenderer3D.js';
@@ -62,46 +58,33 @@ declare module '../../Core/Renderer/SVG/SVGElementLike' {
     }
 }
 
-declare module '../../Core/Renderer/SVG/SVGElement3DLike' {
-    interface SVGElement3DLike {
-        funnel3d?: Highcharts.Funnel3dMethodsObject;
-    }
-}
-
 declare module '../../Core/Renderer/SVG/SVGRendererLike' {
     interface SVGRendererLike {
         funnel3d(shapeArgs: SVGAttributes): SVGElement;
-        funnel3dPath(shapeArgs: SVGAttributes): Highcharts.Funnel3dPathsObject;
+        funnel3dPath(shapeArgs: SVGAttributes): Funnel3DPathsObject;
     }
 }
 
-/**
- * Internal types
- * @private
- */
-declare global {
-    namespace Highcharts {
-        interface Funnel3dMethodsObject extends SVGElement3DLikeCuboid {
-            parts: Array<string>;
-            mainParts: Array<string>;
-            sideGroups: Array<string>;
-            sideParts: Record<string, Array<string>>;
-            pathType: string;
-            opacitySetter(opacity: number): SVGElement;
-            fillSetter(this: SVGElement, fill: ColorType): SVGElement;
-            adjustForGradient(this: SVGElement): void;
-            zIndexSetter(this: SVGElement): boolean;
-            onAdd(this: SVGElement): void;
-        }
-        interface Funnel3dPathsObject extends SVGPath3D {
-            backLower: SVGPath;
-            backUpper: SVGPath;
-            frontLower: SVGPath;
-            frontUpper: SVGPath;
-            rightLower: SVGPath;
-            rightUpper: SVGPath;
-        }
-    }
+interface Funnel3DMethodsObject {
+    parts: Array<string>;
+    mainParts: Array<string>;
+    sideGroups: Array<string>;
+    sideParts: Record<string, Array<string>>;
+    pathType: string;
+    opacitySetter(opacity: number): SVGElement;
+    fillSetter(this: SVGElement, fill: ColorType): SVGElement;
+    adjustForGradient(this: SVGElement): void;
+    zIndexSetter(this: SVGElement): boolean;
+    onAdd(this: SVGElement): void;
+}
+
+interface Funnel3DPathsObject extends SVGPath3D {
+    backLower: SVGPath;
+    backUpper: SVGPath;
+    frontLower: SVGPath;
+    frontUpper: SVGPath;
+    rightLower: SVGPath;
+    rightUpper: SVGPath;
 }
 
 /* *
@@ -122,288 +105,9 @@ namespace Funnel3DComposition {
 
     export function compose(SVGRendererClass: typeof SVGRenderer): void {
         SVGRenderer3D.compose(SVGRendererClass);
-        wrapElement3D(SVGRendererClass.prototype.elements3d);
+        SVGRendererClass.prototype.Element3D.types.funnel3d =
+            SVGElement3DFunnel;
         wrapRenderer3D(SVGRendererClass);
-    }
-
-    function wrapElement3D(elements3d: typeof SVGElement3D): void {
-        elements3d.funnel3d = merge(elements3d.cuboid, {
-            parts: [
-                'top', 'bottom',
-                'frontUpper', 'backUpper',
-                'frontLower', 'backLower',
-                'rightUpper', 'rightLower'
-            ],
-            mainParts: ['top', 'bottom'],
-            sideGroups: [
-                'upperGroup', 'lowerGroup'
-            ],
-            sideParts: {
-                upperGroup: ['frontUpper', 'backUpper', 'rightUpper'],
-                lowerGroup: ['frontLower', 'backLower', 'rightLower']
-            },
-            pathType: 'funnel3d',
-
-            // override opacity and color setters to control opacity
-            opacitySetter: function (
-                this: SVGElement,
-                opacity: number
-            ): SVGElement {
-                const funnel3d = this,
-                    parts = funnel3d.parts,
-                    chart: Chart =
-                        H.charts[funnel3d.renderer.chartIndex] as any,
-                    filterId = 'group-opacity-' + opacity + '-' + chart.index;
-
-                // use default for top and bottom
-                funnel3d.parts = funnel3d.mainParts;
-                funnel3d.singleSetterForParts('opacity', opacity);
-
-                // restore
-                funnel3d.parts = parts;
-
-                if (!(chart.renderer as any).filterId) {
-
-                    chart.renderer.definition({
-                        tagName: 'filter',
-                        attributes: {
-                            id: filterId
-                        },
-                        children: [{
-                            tagName: 'feComponentTransfer',
-                            children: [{
-                                tagName: 'feFuncA',
-                                attributes: {
-                                    type: 'table',
-                                    tableValues: '0 ' + opacity
-                                }
-                            }]
-                        }]
-                    });
-                    funnel3d.sideGroups.forEach(function (
-                        groupName: string
-                    ): void {
-                        funnel3d[groupName].attr({
-                            filter: 'url(#' + filterId + ')'
-                        });
-                    });
-
-                    // styled mode
-                    if (funnel3d.renderer.styledMode) {
-                        chart.renderer.definition({
-                            tagName: 'style',
-                            textContent: '.highcharts-' + filterId +
-                                ' {filter:url(#' + filterId + ')}'
-                        });
-
-                        (funnel3d.sideGroups as any).forEach(function (
-                            group: SVGElement
-                        ): void {
-                            group.addClass('highcharts-' + filterId);
-                        });
-                    }
-                }
-
-                return funnel3d;
-            },
-
-            fillSetter: function (
-                this: SVGElement,
-                fill: ColorType
-            ): SVGElement {
-                // extract alpha channel to use the opacitySetter
-                let funnel3d = this,
-                    fillColor: (Color|ColorType) = color(fill),
-                    alpha: number = (fillColor as any).rgba[3],
-                    partsWithColor: Record<string, ColorType> = {
-                        // standard color for top and bottom
-                        top: color(fill).brighten(0.1).get() as any,
-                        bottom: color(fill).brighten(-0.2).get() as any
-                    };
-
-                if (alpha < 1) {
-                    (fillColor as any).rgba[3] = 1;
-                    fillColor = (fillColor as any).get('rgb');
-
-                    // set opacity through the opacitySetter
-                    funnel3d.attr({
-                        opacity: alpha
-                    });
-                } else {
-                    // use default for full opacity
-                    fillColor = fill;
-                }
-
-                // add gradient for sides
-                if (
-                    !(fillColor as any).linearGradient &&
-                    !(fillColor as any).radialGradient &&
-                    funnel3d.gradientForSides
-                ) {
-                    fillColor = {
-                        linearGradient: { x1: 0, x2: 1, y1: 1, y2: 1 },
-                        stops: [
-                            [0, color(fill).brighten(-0.2).get()],
-                            [0.5, fill],
-                            [1, color(fill).brighten(-0.2).get()]
-                        ]
-                    };
-                }
-
-                // gradient support
-                if ((fillColor as any).linearGradient) {
-                    // color in steps, as each gradient will generate a key
-                    funnel3d.sideGroups.forEach(function (
-                        sideGroupName: string
-                    ): void {
-                        const box = funnel3d[sideGroupName].gradientBox,
-                            gradient: NonNullable<GradientColor['linearGradient']> =
-                                (fillColor as any).linearGradient,
-                            alteredGradient = merge<GradientColor>(
-                                (fillColor as any),
-                                {
-                                    linearGradient: {
-                                        x1: box.x + gradient.x1 * box.width,
-                                        y1: box.y + gradient.y1 * box.height,
-                                        x2: box.x + gradient.x2 * box.width,
-                                        y2: box.y + gradient.y2 * box.height
-                                    }
-                                }
-                            );
-
-                        funnel3d.sideParts[sideGroupName].forEach(function (
-                            partName: string
-                        ): void {
-                            partsWithColor[partName] = alteredGradient;
-                        });
-                    });
-                } else {
-                    merge(true, partsWithColor, {
-                        frontUpper: fillColor as any,
-                        backUpper: fillColor as any,
-                        rightUpper: fillColor as any,
-
-                        frontLower: fillColor as any,
-                        backLower: fillColor as any,
-                        rightLower: fillColor as any
-                    });
-
-                    if ((fillColor as any).radialGradient) {
-                        funnel3d.sideGroups.forEach(function (
-                            sideGroupName: string
-                        ): void {
-                            const gradBox = funnel3d[sideGroupName].gradientBox,
-                                centerX = gradBox.x + gradBox.width / 2,
-                                centerY = gradBox.y + gradBox.height / 2,
-                                diameter = Math.min(
-                                    gradBox.width,
-                                    gradBox.height
-                                );
-
-                            funnel3d.sideParts[sideGroupName].forEach(
-                                function (partName: string): void {
-                                    funnel3d[partName].setRadialReference([
-                                        centerX, centerY, diameter
-                                    ]);
-                                }
-                            );
-                        });
-                    }
-                }
-
-                funnel3d.singleSetterForParts('fill', null, partsWithColor);
-
-                // fill for animation getter (#6776)
-                funnel3d.color = funnel3d.fill = fill;
-
-                // change gradientUnits to userSpaceOnUse for linearGradient
-                if ((fillColor as any).linearGradient) {
-                    [funnel3d.frontLower, funnel3d.frontUpper]
-                        .forEach(function (
-                            part: Record<string, SVGElement>
-                        ): void {
-                            const elem: SVGElement = part.element,
-                                grad = (
-                                    elem &&
-                                    funnel3d.renderer.gradients[elem.gradient]
-                                );
-
-                            if (
-                                grad &&
-                                grad.attr('gradientUnits') !== 'userSpaceOnUse'
-                            ) {
-                                grad.attr({
-                                    gradientUnits: 'userSpaceOnUse'
-                                });
-                            }
-                        });
-                }
-
-                return funnel3d;
-            },
-
-            adjustForGradient: function (this: SVGElement): void {
-                let funnel3d = this,
-                    bbox: BBoxObject;
-
-                funnel3d.sideGroups.forEach(function (
-                    sideGroupName: string
-                ): void {
-                    // use common extremes for groups for matching gradients
-                    let topLeftEdge = {
-                            x: Number.MAX_VALUE,
-                            y: Number.MAX_VALUE
-                        },
-                        bottomRightEdge = {
-                            x: -Number.MAX_VALUE,
-                            y: -Number.MAX_VALUE
-                        };
-
-                    // get extremes
-                    funnel3d.sideParts[sideGroupName].forEach(function (
-                        partName: string
-                    ): void {
-                        const part = funnel3d[partName];
-
-                        bbox = part.getBBox(true);
-                        topLeftEdge = {
-                            x: Math.min(topLeftEdge.x, bbox.x),
-                            y: Math.min(topLeftEdge.y, bbox.y)
-                        };
-                        bottomRightEdge = {
-                            x: Math.max(bottomRightEdge.x, bbox.x + bbox.width),
-                            y: Math.max(bottomRightEdge.y, bbox.y + bbox.height)
-                        };
-                    });
-
-                    // store for color fillSetter
-                    funnel3d[sideGroupName].gradientBox = {
-                        x: topLeftEdge.x,
-                        width: bottomRightEdge.x - topLeftEdge.x,
-                        y: topLeftEdge.y,
-                        height: bottomRightEdge.y - topLeftEdge.y
-                    };
-                });
-            },
-
-            zIndexSetter: function (this: SVGElement): boolean {
-                // this.added won't work, because zIndex is set after the prop
-                // is set, but before the graphic is really added
-                if (this.finishedOnAdd) {
-                    this.adjustForGradient();
-                }
-
-                // run default
-                return this.renderer.Element.prototype.zIndexSetter.apply(
-                    this, arguments
-                );
-            },
-
-            onAdd: function (this: SVGElement): void {
-                this.adjustForGradient();
-                this.finishedOnAdd = true;
-            }
-        });
     }
 
     function wrapRenderer3D(SVGRendererClass: typeof SVGRenderer): void {
@@ -467,7 +171,7 @@ namespace Funnel3DComposition {
             funnel3dPath: function (
                 this: SVGRenderer,
                 shapeArgs: any // @todo: Type it. It's an extended SVGAttributes.
-            ): Highcharts.Funnel3dPathsObject {
+            ): Funnel3DPathsObject {
                 // Check getCylinderEnd for better error message if
                 // the cylinder module is missing
                 if (!this.getCylinderEnd) {
@@ -522,7 +226,7 @@ namespace Funnel3DComposition {
                     middleTopArgs = bottomArgs,
                     middleTop = bottom,
                     middleBottom = bottom,
-                    ret: Highcharts.Funnel3dPathsObject,
+                    ret: Funnel3DPathsObject,
 
                     // masking for cylinders or a missing part of a side shape
                     useAlphaCorrection;
