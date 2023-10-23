dd50413feec885a19982350ae9c5472565f04e03
Sebastian Bochan <sebastian@blacklabel.pl>
2023-10-23 14:16:07 +0200

    Adapted changelog to dashboards requiremenets.

--- changelog/generate.js
+++ changelog/generate.js
@@ -82,7 +82,8 @@ const getFile = url => new Promise((resolve, reject) => {
         var log = await prLog(
             params.since,
             params.fromCache,
-            params.branches
+            params.branches,
+            params.highchartsDashboards
         ).catch(e => console.error(e));
 
         callback(log);
@@ -299,7 +300,7 @@ const getFile = url => new Promise((resolve, reject) => {
         const d = new Date();
         const review = [];
 
-        if (params.dashboards && params.release) {
+        if (params.highchartsDashboards && params.release) {
             const version = params.release;
             if (!/^\d+\.\d+\.\d+(?:-\w+)?$/su.test(version)) {
                 throw new Error('No valid `--release x.x.x` provided.');
