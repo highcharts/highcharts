ea3e1d6ad1f0afe394851acb26d0047b27387ce7
bre1470 <40056287+bre1470@users.noreply.github.com>
2023-10-23 18:23:31 +0200

    Tools: Improved and prepared cypress for product-specific tests.

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -1,10 +1,33 @@
 import { defineConfig } from 'cypress';
+import { getProducts } from './tools/gulptasks/lib/test.js';
+
+const products = [];
+
+for (const product of getProducts()) {
+    switch (product) {
+        case 'Dashboards':
+            if (!products.includes('dashboards')) {
+                products.push('dashboards');
+            }
+            if (!products.includes('data-grid')) {
+                products.push('data-grid');
+            }
+            continue;
+        default:
+            if (!products.includes('highcharts')) {
+                products.push('highcharts');
+            }
+            continue;
+    }
+}
+
+const productsPattern = products.length ? `{${products.join(',')}}` : '**';
 
 export default defineConfig({
     fixturesFolder: 'test/cypress/fixtures',
     e2e: {
         baseUrl: 'http://localhost:3030/samples/view?mobile=true&path=/',
-        specPattern: 'test/cypress/integration/Highcharts/**/*.cy.{js,jsx,ts,tsx}',
+        specPattern: `test/cypress/${productsPattern}/integration/*.cy.{js,jsx,ts,tsx}`,
         supportFile: 'test/cypress/support/index.js',
         testIsolation: false
     }
