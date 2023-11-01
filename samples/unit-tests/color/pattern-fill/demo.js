3792f758d38d72db38e9ce3594da02ab70e65789
Xillez <kent.wincent46@gmail.com>
2023-11-01 14:32:27 +0100

    #19980 Added tests for patterfill + linting fixes

--- samples/unit-tests/color/pattern-fill/demo.js
+++ samples/unit-tests/color/pattern-fill/demo.js
@@ -709,3 +709,142 @@ QUnit.test('Destroy and recreate chart', function (assert) {
 QUnit.test('#14765: Global patterns', assert => {
     assert.ok(Highcharts.patterns, 'Global patterns should be defined');
 });
+
+function testPatternfillForChart(assert, chart) {
+    const points = chart.series[0].points,
+        customPattern = doc.getElementById(
+            getPointFillId(points[1])
+        ),
+        defaultPattern = doc.getElementById(
+            getPointFillId(points[0])
+        );
+
+    assert.ok(
+        customPattern.hasAttribute('patternTransform'),
+        'custom pattern element doesn\'t have patternTransform attribute'
+    );
+    assert.ok(
+        customPattern.getAttribute('patternTransform').includes('scale'),
+        'custom pattern; patternTransform attribute should contain scale'
+    );
+    assert.ok(
+        defaultPattern.hasAttribute('patternTransform'),
+        'custom pattern element doesn\'t have patternTransform attribute'
+    );
+    assert.ok(
+        defaultPattern.getAttribute('patternTransform').includes('scale'),
+        'default pattern; patternTransform attribute should contain scale'
+    );
+}
+
+QUnit.test('#19980/pattern-fill/geojson', function (assert) {
+    var done = assert.async();
+    fetch('https://code.highcharts.com/mapdata/custom/world-continents.geo.json')
+        .then(response => response.json())
+        .then(geoJSON => {
+            var chart = Highcharts.mapChart('container', {
+                chart: {
+                    map: geoJSON
+                },
+                series: [
+                    {
+                        data: [
+                            {
+                                'hc-key': 'eu',
+                                color: {
+                                    patternIndex: 8
+                                }
+                            },
+                            {
+                                'hc-key': 'af',
+                                color: {
+                                    pattern: {
+                                        path: {
+                                            d: 'M 3 3 L 8 3 L 8 8 Z'
+                                        },
+                                        width: 12,
+                                        height: 12,
+                                        color: '#ff0000',
+                                        opacity: 0.5,
+                                        backgroundColor: '#000000'
+                                    }
+                                }
+                            },
+                            {
+                                'hc-key': 'as',
+                                color: {
+                                    pattern: {
+                                        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Flag_of_Queensland.svg/1920px-Flag_of_Queensland.svg.png',
+                                        aspectRatio: 2
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                ]
+            });
+            testPatternfillForChart(assert, chart);
+
+            done();
+        })
+        .catch(error => {
+            assert.ok(false, 'Error downloading or processing data: ' + error);
+            done();
+        });
+});
+
+QUnit.test('#19980/pattern-fill/topojson', function (assert) {
+    var done = assert.async();
+    fetch('https://code.highcharts.com/mapdata/custom/world-continents.topo.json')
+        .then(response => response.json())
+        .then(geoJSON => {
+            var chart = Highcharts.mapChart('container', {
+                chart: {
+                    map: geoJSON
+                },
+                series: [
+                    {
+                        data: [
+                            {
+                                'hc-key': 'eu',
+                                color: {
+                                    patternIndex: 8
+                                }
+                            },
+                            {
+                                'hc-key': 'af',
+                                color: {
+                                    pattern: {
+                                        path: {
+                                            d: 'M 3 3 L 8 3 L 8 8 Z'
+                                        },
+                                        width: 12,
+                                        height: 12,
+                                        color: '#ff0000',
+                                        opacity: 0.5,
+                                        backgroundColor: '#000000'
+                                    }
+                                }
+                            },
+                            {
+                                'hc-key': 'as',
+                                color: {
+                                    pattern: {
+                                        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Flag_of_Queensland.svg/1920px-Flag_of_Queensland.svg.png',
+                                        aspectRatio: 2
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                ]
+            });
+            testPatternfillForChart(assert, chart);
+
+            done();
+        })
+        .catch(error => {
+            assert.ok(false, 'Error downloading or processing data: ' + error);
+            done();
+        });
+});
