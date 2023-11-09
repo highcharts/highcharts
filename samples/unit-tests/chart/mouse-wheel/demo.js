a832d09fc3dc6376da5fafd138353684f22a15ec
DJTechnoo <askeltechno@hotmail.com>
2023-11-09 14:19:07 +0100

    Added mouseWheel event, changed unit test.

--- samples/unit-tests/chart/mouse-wheel/demo.js
+++ samples/unit-tests/chart/mouse-wheel/demo.js
@@ -1,8 +1,7 @@
-QUnit.test('Pinching to zoom on chart(#19976)', function (assert) {
+QUnit.test('Mouse wheel zoom on chart(#19976)', function (assert) {
 
     const chart = Highcharts.chart('container', {
         chart: {
-            inverted: true, // Hack to zoom on x with pinch
             zooming: {
                 type: 'x'
             }
@@ -14,12 +13,13 @@ QUnit.test('Pinching to zoom on chart(#19976)', function (assert) {
     });
 
     const controller = new TestController(chart);
-    controller.pinch(100, 100, 50);
+
+    controller.mouseWheel(200, 100, -1000, true);
     const actualMin = chart.series[0].xAxis.min;
 
     assert.close(
         actualMin,
-        3.3,
+        3.7,
         1,
         'Should zoom to retract xAxis to this extreme.'
     );
