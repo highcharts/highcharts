ba07d16f24489c13887035254c2ae0cf81ca59c9
Torstein Hønsi <torstein@highsoft.com>
2023-11-15 14:25:43 +0100

    Axis layout runs: optimized and commmented

--- ts/Core/Chart/ChartOptions.d.ts
+++ ts/Core/Chart/ChartOptions.d.ts
@@ -109,7 +109,7 @@ export interface ChartOptions {
     marginRight?: number;
     marginTop?: number;
     numberFormatter?: NumberFormatterCallbackFunction;
-    axesLayoutRuns?: number;
+    axisLayoutRuns?: number;
     panKey?: string;
     panning?: ChartPanningOptions;
     pinchType?: string;
