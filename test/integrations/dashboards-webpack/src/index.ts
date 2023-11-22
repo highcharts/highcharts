4339e6b1d27a5309792c866139744a86fa995708
karolkolodziej <karolkolodziej01@gmail.com>
2023-11-22 11:10:15 +0100

    Added scripts.

--- test/integrations/dashboards-webpack/src/index.ts
+++ test/integrations/dashboards-webpack/src/index.ts
@@ -1,18 +1,17 @@
 // Import Highcharts module
-import * as Highcharts from 'highcharts';
-import * as Dashboards from '@highcharts/dashboards/dashboards';
-import DataGrid from '@highcharts/dashboards/es-modules/DataGrid/DataGrid';
-import HighchartsPlugin from '@highcharts/dashboards/es-modules/Dashboards/Plugins/HighchartsPlugin';
+import Highcharts from 'highcharts/es-modules/masters/highcharts.src';
+import Dashboards from '@highcharts/dashboards/es-modules/masters/dashboards.src'
+import DataGrid from '@highcharts/dashboards/es-modules/masters/datagrid.src'
+import HighchartsPlugin from '@highcharts/dashboards/es-modules/Dashboards/Plugins/HighchartsPlugin'
 import DataGridPlugin from '@highcharts/dashboards/es-modules/Dashboards/Plugins/DataGridPlugin';
 
 HighchartsPlugin.custom.connectHighcharts(Highcharts as any);
 (Dashboards.PluginHandler as any).addPlugin(HighchartsPlugin);
 
-DataGridPlugin.custom.connectDataGrid(DataGrid);
+DataGridPlugin.custom.connectDataGrid(DataGrid as any);
 (Dashboards.PluginHandler as any).addPlugin(DataGridPlugin);
 
-
-// Create a chart
+// Create a new dashboard
 Dashboards.board('container', {
     editMode: {
         enabled: true,
@@ -26,12 +25,18 @@ Dashboards.board('container', {
             rows: [{
                 cells: [{
                     id: 'dashboard-cell-1',
+                }, {
+                    id: 'dashboard-cell-2',
                 }]
             }]
         }]
     },
     components: [{
         cell: 'dashboard-cell-1',
+        type: 'KPI',
+        value: 10
+    }, {
+        cell: 'dashboard-cell-2',
         type: 'Highcharts',
         chartOptions: {
             series: [{
