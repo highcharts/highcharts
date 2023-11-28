d232e2d811ddd9fe2051e6b35fec00fe0c8d6fa3
karolkolodziej <karolkolodziej01@gmail.com>
2023-11-28 14:25:32 +0100

    Fixed d.ts files for HighchartsTypes.

---
+++ tools/gulptasks/dashboards/scripts-dts/es-modules/Dashboards/Plugins/HighchartsTypes.d.ts
@@ -0,0 +1,20 @@
+/*!*
+ *
+ *  Copyright (c) Highsoft AS. All rights reserved.
+ *
+ *!*/
+
+import type { default as H } from 'highcharts';
+export type { Axis } from 'highcharts';
+export type { AxisOptions } from 'highcharts';
+export type { Chart } from 'highcharts';
+export type { Globals } from 'highcharts';
+export type Highcharts = typeof H;
+export type { Options } from 'highcharts';
+export type OrdinalAxis = any;
+export type Pane = any;
+export type { Point } from 'highcharts';
+export type PolarComposition = any;
+export type { Series } from 'highcharts';
+export type { SeriesOptions } from 'highcharts';
+export default H;
