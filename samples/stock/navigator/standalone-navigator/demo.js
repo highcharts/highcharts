00763ebcf3dc501ddba183c1cc20d2473a8f549b
Mateusz Bernacik <mat.bernacik@gmail.com>
2023-12-07 09:45:27 +0100

    Added update method for standalone nav and cleanup.

--- samples/stock/navigator/standalone-navigator/demo.js
+++ samples/stock/navigator/standalone-navigator/demo.js
@@ -1,23 +1,29 @@
 const nav = Highcharts.navigator('container', {
     navigator: {
         series: [{
-            data: Array.from({length: 45}, (_, x) => Math.floor(Math.sin(x / 10) * 20))
+            data: Array.from(
+                { length: 45 }, (_, x) => Math.floor(Math.sin(x / 10) * 20)
+            )
         }]
     }
-})
+});
 
 
 const chart1 = Highcharts.chart('chart1', {
-    series:[{
-        data: Array.from({length: 50}, (_, x) => Math.floor(Math.sin(x / 10) * 20))
+    series: [{
+        data: Array.from(
+            { length: 50 }, (_, x) => Math.floor(Math.sin(x / 10) * 20)
+        )
     }]
-})
+});
 
 const chart2 = Highcharts.chart('chart2', {
-    series:[{
-        data: Array.from({length: 50}, (_, x) => Math.floor(Math.cos(x / 10) * 20))
+    series: [{
+        data: Array.from(
+            { length: 50 }, (_, x) => Math.floor(Math.cos(x / 10) * 20)
+        )
     }]
-})
+});
 
 
 nav.bind(chart1);
