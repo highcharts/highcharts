15263d4069a8cb203d804715aee129546754f278
hubertkozik <hubertkozik13@gmail.com>
2023-12-07 15:39:53 +0100

    Added documentation.

---
+++ samples/highcharts/coloraxis/width-and-height/demo.js
@@ -0,0 +1,11 @@
+Highcharts.chart('container', {
+    colorAxis: {
+        width: '50%',
+        height: 80
+    },
+
+    series: [{
+        type: 'column',
+        data: [35, 22, 14, 9, 60, 7, 15, 30, 45, 12, 25]
+    }]
+});
