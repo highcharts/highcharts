09de62abe5361c12bdb7009bbc65ce41ef28638d
hubertkozik <hubertkozik13@gmail.com>
2023-12-12 14:24:23 +0100

    Fixed lint issue.

--- ts/Core/Defaults.ts
+++ ts/Core/Defaults.ts
@@ -1916,8 +1916,7 @@ const defaultOptions: DefaultOptions = {
          * @sample {highmaps} maps/tooltip/background-border/ Background and
          *         border demo
          *
-         * @type
-         * {Highcharts.ColorString|Highcharts.GradientColorObject|Highcharts.PatternObject}
+         * @type {Highcharts.ColorString|Highcharts.GradientColorObject|Highcharts.PatternObject}
          * @apioption tooltip.borderColor
          */
 
