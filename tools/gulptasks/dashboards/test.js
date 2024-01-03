8504e127790ae6feae5f92e271f6873cc3dce9c6
karolkolodziej <karolkolodziej01@gmail.com>
2024-01-03 11:54:47 +0100

    Added test-karma.

--- tools/gulptasks/dashboards/test.js
+++ tools/gulptasks/dashboards/test.js
@@ -15,10 +15,11 @@ const gulp = require('gulp');
 
 const { lintDTS } = require('../lint-dts');
 const { lintTS } = require('../lint-ts');
+const { testKarma } = require('../test-karma');
 
 gulp.task('dashboards/test', gulp.series(
     'dashboards/scripts',
     () => lintTS({ dashboards: true }),
-    'dashboards/test-karma',
+    () => testKarma({ dashboards: true }),
     () => lintDTS({ dashboards: true })
 ));
