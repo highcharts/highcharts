552eb279b6dfc97013437fd8f1a4bbf6a8581e33
Torstein Hønsi <torstein@highsoft.com>
2024-01-08 14:10:27 +0100

    Regression with boosted bubble series

--- ts/Extensions/Boost/BoostSeries.ts
+++ ts/Extensions/Boost/BoostSeries.ts
@@ -1398,7 +1398,8 @@ function wrapSeriesProcessData(
     let dataToMeasure = this.options.data;
 
     if (boostEnabled(this.chart) && BoostableMap[this.type]) {
-        const series = this as BoostSeriesComposition;
+        const series = this as BoostSeriesComposition,
+            isScatter = series.is('scatter') && !series.is('bubble');
 
         // If there are no extremes given in the options, we also need to
         // process the data to read the data extremes. If this is a heatmap,
@@ -1406,13 +1407,13 @@ function wrapSeriesProcessData(
         if (
             // First pass with options.data:
             !getSeriesBoosting(series, dataToMeasure) ||
-            series.is('scatter') ||
-            // processedYData for the stack (#7481):
+            isScatter ||
+            // Use processedYData for the stack (#7481):
             series.options.stacking ||
             !hasExtremes(series, true)
         ) {
-            // extra check for zoomed scatter data
-            if (series.is('scatter') && !series.yAxis.treeGrid) {
+            // Extra check for zoomed scatter data
+            if (isScatter && !series.yAxis.treeGrid) {
                 scatterProcessData.call(series, arguments[1]);
             } else {
                 proceed.apply(series, [].slice.call(arguments, 1));
