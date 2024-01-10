b02521f40c5a7af9c444bd8ab1ca082f465a4086
magdalena-gut <magdalena@blacklabel.com>
2024-01-10 13:25:38 +0100

    Allow point.name as an empty string.

--- ts/Series/Map/MapPoint.ts
+++ ts/Series/Map/MapPoint.ts
@@ -139,7 +139,9 @@ class MapPoint extends ScatterPoint {
                 // Copy over properties; #20231 prioritize point.name
                 extend(point, {
                     ...mapPoint,
-                    name: point.name || mapPoint.name
+                    name: typeof point.name !== 'undefined' ?
+                        point.name :
+                        mapPoint.name
                 });
             } else if (series.pointArrayMap.indexOf('value') !== -1) {
                 point.value = point.value || null;
