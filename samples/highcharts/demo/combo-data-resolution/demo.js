aaa7ac3dc3db1e6387f67bd7193902574b1908de
Markus Knutson Barstad <mkb@0xF0F0F.local>
2024-01-18 13:27:18 +0100

    Added draft generated dataset

--- samples/highcharts/demo/combo-data-resolution/demo.js
+++ samples/highcharts/demo/combo-data-resolution/demo.js
@@ -1,16 +1,31 @@
+const lineData = [
+    0, 0, 1, 2, 2, 3, 3, 3, 3, 3, 3, 3,
+    3, 3, 2, 2, 2, 1, 1, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 1, 2, 3, 4, 4, 5, 6, 7, 7,
+    8, 8, 8, 9, 9, 9, 8, 8, 7, 7, 6, 5,
+    4, 2, 1, 0, 1, 2, 3, 3, 3, 3, 3, 2,
+    2, 1, 0, 0, 1, 2, 3, 4, 4, 5, 6, 7,
+    7, 7, 7, 7, 7, 6, 6, 5, 3, 2, 0, 1
+];
+const columnData = [];
+let date = 0;
+
+lineData.forEach((d, i) => {
+    if (i % 23 === 0) {
+        columnData.push([Date.UTC(2024, 0, date++), d]);
+    }
+});
+
 Highcharts.chart('container', {
     series: [{
         type: 'column',
-        data: [
-            [Date.UTC(2024, 0, 1), 0.1],
-            [Date.UTC(2024, 0, 2), 0.7],
-            [Date.UTC(2024, 0, 3), 0.3],
-            [Date.UTC(2024, 0, 4), 0.9]
-        ],
+        data: columnData,
         pointRange: 24 * 36e5,
-        pointPlacement: 'between'
+        pointPlacement: 'between',
+        min: 0
     }, {
-        data: new Array(4 * 24).fill(1).map(Math.random),
+        data: lineData,
         pointStart: Date.UTC(2024, 0, 1),
         pointInterval: 36e5
     }],
