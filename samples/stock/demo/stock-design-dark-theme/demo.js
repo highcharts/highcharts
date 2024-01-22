9bf8e3308e8028b6d4f044c87b6561ae30ad7f6d
Mateusz Bernacik <mat.bernacik@gmail.com>
2024-01-22 11:31:10 +0100

    Added dark themed stock chart demo.

---
+++ samples/stock/demo/stock-design-dark-theme/demo.js
@@ -0,0 +1,166 @@
+(async () => {
+    const data = await fetch(
+        'https://www.highcharts.com/samples/data/new-intraday.json'
+    ).then(response => response.json());
+
+    Highcharts.stockChart('container', {
+        chart: {
+            backgroundColor: '#202d3b'
+        },
+        rangeSelector: {
+            buttons: [{
+                text: '1m',
+                title: 'Set timeframe to 1 minute',
+                type: 'hour',
+                count: 1,
+                dataGrouping: {
+                    units: [
+                        ['minute', [1]]
+                    ]
+                }
+            }, {
+                text: '5m',
+                title: 'Set timeframe to 5 minutes',
+                type: 'hour',
+                count: 6,
+                dataGrouping: {
+                    units: [
+                        ['minute', [5]]
+                    ]
+                }
+            }, {
+                text: '15m',
+                title: 'Set timeframe to 15 minutes',
+                type: 'day',
+                count: 1,
+                dataGrouping: {
+                    units: [
+                        ['minute', [15]]
+                    ]
+                }
+            }, {
+                text: '30m',
+                title: 'Set timeframe to 30 minutes',
+                type: 'day',
+                count: 3,
+                dataGrouping: {
+                    units: [
+                        ['minute', [30]]
+                    ]
+                }
+            }, {
+                text: '1h',
+                title: 'Set timeframe to 1 hour',
+                dataGrouping: {
+                    units: [
+                        ['hour', [1]]
+                    ]
+                }
+            }, {
+                text: '4h',
+                title: 'Set timeframe to 4 hours',
+                dataGrouping: {
+                    units: [
+                        ['hour', [4]]
+                    ]
+                }
+            }, {
+                text: 'D',
+                title: 'Set timeframe to 1 day',
+                dataGrouping: {
+                    units: [
+                        ['day', [1]]
+                    ]
+                }
+            }],
+            buttonTheme: {
+                r: 0,
+                padding: 1
+            },
+            inputStyle: {
+                color: '#c5c7c9'
+            },
+            labelStyle: {
+                color: '#c5c7c9'
+            },
+            selected: 4
+        },
+        navigator: {
+            xAxis: {
+                labels: {
+                    style: {
+                        color: '#c5c7c9',
+                        opacity: 1,
+                        textOutline: 0
+                    }
+                }
+            },
+            series: {
+                lineColor: '#FFF'
+            }
+        },
+        tooltip: {
+            backgroundColor: '#fbfbfb',
+            borderRadius: 0,
+            borderWidth: 0,
+            padding: 3,
+            pointFormat: ''
+        },
+        xAxis: {
+            gridLineColor: '#21323f',
+            gridLineWidth: 1,
+            lineColor: '#999999',
+            tickColor: '#999999',
+            tickLength: 5,
+            labels: {
+                style: {
+                    color: '#c5c7c9'
+                }
+            }
+        },
+        yAxis: {
+            crosshair: {
+                label: {
+                    backgroundColor: '#fbfbfb',
+                    borderRadius: 0,
+                    enabled: true,
+                    padding: 3,
+                    style: {
+                        color: '#000'
+                    }
+                }
+            },
+            gridLineColor: '#21323f',
+            lineColor: '#999999',
+            lineWidth: 1,
+            labels: {
+                align: 'left',
+                style: {
+                    color: '#c5c7c9'
+                }
+            }
+
+        },
+        series: [{
+            name: 'AAPL',
+            color: '#b82d0d',
+            upColor: '#8cc076',
+            data: data,
+            lineColor: '#ccc',
+            type: 'candlestick',
+            lastPrice: {
+                color: '#c0c0c0',
+                enabled: true,
+                label: {
+                    backgroundColor: '#fbfbfb',
+                    borderRadius: 0,
+                    enabled: true,
+                    padding: 3,
+                    style: {
+                        color: '#000'
+                    }
+                }
+            }
+        }]
+    });
+})();
