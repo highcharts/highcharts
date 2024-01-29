18c863c7e11ad684dd7842995105237e452d27bd
karolkolodziej <karolkolodziej01@gmail.com>
2024-01-29 11:05:14 +0100

    Fixed demos.

--- samples/dashboards/cypress/component-with-no-connector/demo.mjs
+++ samples/dashboards/cypress/component-with-no-connector/demo.mjs
@@ -1,18 +1,16 @@
 import Dashboards from '../../../../code/dashboards/es-modules/masters/dashboards.src.js';
-import DataGrid from '../../../../code/dashboards/es-modules/masters/datagrid.src.js';
+import DataGrid from '../../../../code/datagrid/es-modules/masters/datagrid.src.js';
 import Highcharts from '../../../../code/es-modules/masters/highcharts.src.js';
-import HighchartsPlugin from '../../../../code/dashboards/es-modules/Dashboards/Plugins/HighchartsPlugin.js';
-import DataGridPlugin from '../../../../code/dashboards/es-modules/Dashboards/Plugins/DataGridPlugin.js';
 
 Highcharts.win.Highcharts = Highcharts;
 
 const { PluginHandler } = Dashboards;
 
-HighchartsPlugin.custom.connectHighcharts(Highcharts);
-PluginHandler.addPlugin(HighchartsPlugin);
+Dashboards.HighchartsPlugin.custom.connectHighcharts(Highcharts);
+Dashboards.DataGridPlugin.custom.connectDataGrid(DataGrid.DataGrid);
 
-DataGridPlugin.custom.connectDataGrid(DataGrid.DataGrid);
-PluginHandler.addPlugin(DataGridPlugin);
+Dashboards.PluginHandler.addPlugin(Dashboards.HighchartsPlugin);
+Dashboards.PluginHandler.addPlugin(Dashboards.DataGridPlugin);
 
 Dashboards.board(
     'container',
