e43d8950b328be40b6ebc31ce6a7f9a862cd4238
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2024-01-29 15:50:17 +0100

    Set rangeselector values in language module template, fixes in language file

--- i18n/lang-module.ts
+++ i18n/lang-module.ts
@@ -6,3 +6,34 @@ H.setOptions({
     lang: languageOptions
 });
 
+if ('RangeSelector' in H) {
+    H.RangeSelector.prototype.defaultButtons = [{
+        type: 'month',
+        count: 1,
+        text: H.getOptions().lang.rangeSelector.monthText,
+        title: H.getOptions().lang.rangeSelector.monthTitle
+    }, {
+        type: 'month',
+        count: 3,
+        text: H.getOptions().lang.rangeSelector.monthText,
+        title: H.getOptions().lang.rangeSelector.monthTitle
+    }, {
+        type: 'month',
+        count: 6,
+        text: H.getOptions().lang.rangeSelector.monthText,
+        title: H.getOptions().lang.rangeSelector.monthTitle
+    }, {
+        type: 'ytd',
+        text: H.getOptions().lang.rangeSelector.YTDText,
+        title: H.getOptions().lang.rangeSelector.YTDTitle
+    }, {
+        type: 'year',
+        count: 1,
+        text: H.getOptions().lang.rangeSelector.yearText,
+        title: H.getOptions().lang.rangeSelector.yearTitle
+    }, {
+        type: 'all',
+        text: 'All',
+        title: H.getOptions().lang.rangeSelector.allTitle
+    }]
+}
