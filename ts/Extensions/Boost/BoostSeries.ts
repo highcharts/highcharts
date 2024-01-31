ed32adb20b6e9495bff70f2323bc7532ed1597eb
Torstein Hønsi <torstein@highsoft.com>
2024-01-31 15:46:07 +0100

    Pinch-zoom: Regression in clipping of panes in Boost

--- ts/Extensions/Boost/BoostSeries.ts
+++ ts/Extensions/Boost/BoostSeries.ts
@@ -462,8 +462,6 @@ function createAndAttachRenderer(
                 zIndex: series.options.zIndex
             });
 
-        targetGroup?.clip(boost.clipRect);
-
         if (target instanceof ChartClass) {
             (target.boost as any).markerGroup = target.renderer
                 .g()
@@ -476,7 +474,19 @@ function createAndAttachRenderer(
     boost.canvas.height = height;
 
     if (boost.clipRect) {
-        boost.clipRect.attr(getBoostClipRect(chart, target));
+        const box = getBoostClipRect(chart, target),
+
+            // When using panes, the image itself must be clipped. When not
+            // using panes, it is better to clip the target group, because then
+            // we preserve clipping on touch- and mousewheel zoom preview.
+            clippedElement = (
+                box.width === chart.clipBox.width &&
+                box.height === chart.clipBox.height
+            ) ? targetGroup :
+                (boost.targetFo || boost.target);
+
+        boost.clipRect.attr(box);
+        clippedElement?.clip(boost.clipRect);
     }
 
     boost.resize();
@@ -998,13 +1008,6 @@ function scatterProcessData(
 
     return true;
 }
-
-const zoomStart = {
-    xMin: 0,
-    xMax: 0,
-    yMin: 0,
-    yMax: 0
-};
 
 /**
  * @private
