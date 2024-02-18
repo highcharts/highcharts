626ffbf5f2745477987c4cb3b7243ea016e535b0
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2024-02-18 08:39:33 +0100

    Untracked vendor folder, updated update-vendor and dependencies

--- tools/gulptasks/unsorted/update-vendor.js
+++ tools/gulptasks/unsorted/update-vendor.js
@@ -12,6 +12,11 @@ function updateVendor() {
         copyFile
     } = require('../../filesystem.js');
 
+    const { mkdirSync } = require('fs');
+
+    // Create the vendor folder if it doesn't exist
+    mkdirSync('./vendor', { recursive: true });
+
     console.log((
         'Note: This task only copies the files into the vendor folder.\n' +
         'To upgrade, run npm update jspdf && npm update svg2pdf.js`'
@@ -33,6 +38,31 @@ function updateVendor() {
         [
             './node_modules/svg2pdf.js/dist/svg2pdf.umd.min.js',
             './vendor/svg2pdf.js'
+        ],
+        [
+            './node_modules/moment/min/moment.min.js',
+            './vendor/moment.js'
+        ],
+        [
+
+            './node_modules/moment-timezone/builds/moment-timezone-with-data-2012-2022.js',
+            './vendor/moment-timezone-with-data-2012-2022.js'
+        ],
+        [
+            './node_modules/requirejs/require.js',
+            './vendor/require.js'
+        ],
+        [
+            './node_modules/proj4/dist/proj4.js',
+            './vendor/proj4.js'
+        ],
+        [
+            './node_modules/jquery/dist/jquery.min.js',
+            './vendor/jquery.js'
+        ],
+        [
+            './node_modules/topojson-client/dist/topojson-client.min.js',
+            './vendor/topojson-client.min.js'
         ]
     ].map(([source, target]) => copyFile(source, target));
 
