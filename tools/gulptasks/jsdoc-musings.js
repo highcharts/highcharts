3e439dca3e399aa41e019f249e31e0928374002b
Markus Knutson Barstad <mkb@0xF0F0F.local>
2024-03-04 16:33:23 +0100

    Started building doclets from meta.json

--- tools/gulptasks/jsdoc-musings.js
+++ tools/gulptasks/jsdoc-musings.js
@@ -1,25 +1,24 @@
 /**
-*      Script for propagating interface declerations with jsdocs
-*      made from properties in corresponding "*Defaults"
-*
-*      Example:
-*          Props from "ScatterSeriesDefaults" objects -> JSDocs in "ScatterSeriesOptions" interfaces
-*
-*      TODO:
-*          - Succesfully change the name of this file
-*          - Get this file into a pipeline
-*          - Snap up interfaces from the tree
-*          - Put our new docs into those interfaces
-*
-*      Questions:
-*          - Do we have access to a tree with "*Defaults.js"?
-*/
+ *      Script building JSDocs for interfaces.
+ *      The JSDoclets are buildt from two sources:
+ *          - an object-tree representing interfaces generated with the 'hc-gen'-repo
+ *          - properties in '*Defaults' files which correspond to a given interface
+ *              For instance:
+ *                  object-props from "ScatterSeriesDefaults" -> JSDocs in "ScatterSeriesOptions" interfaces
+ *
+ *      TODO:
+ *          - Succesfully change the name of this file
+ *          - Get this file into a pipeline
+ *          - Read the '*Defaults'-files here
+ *          - Put our new docs into those interfaces
+ *
+ */
 const gulp = require('gulp');
+const { exec } = require('child_process');
+const log = require('./lib/log');
+const fs = require('node:fs');
 
 function declarationPropegation() {
-    const { exec } = require('child_process');
-    const log = require('./lib/log');
-    const fs = require('node:fs');
     const treeName = 'meta.json';
     const treeFolder = '../hc-integration-gen/dist/meta/';
     const treePath = treeFolder + treeName;
@@ -38,13 +37,37 @@ function declarationPropegation() {
 
                 log.success('Succesfully generated ' + treeName);
 
-                fs.readFile(treePath, 'utf8', (readingError, data) => {
+                fs.readFile(treePath, 'utf8', (readingError, tree) => {
                     if (readingError) {
                         log.failure('Failed to read ' + treeName);
                         return;
                     }
 
-                    // Do stuff
+                    // Temporary object to check results while building this script
+                    const res = {};
+                    const parsedTree = JSON.parse(tree);
+                    for (const key of Object.keys(parsedTree)) {
+                        if (!key.includes('HighchartsProductSeries')) {
+                            const interfaceFileName = key.replace('plotOptions.', '') + 'Options.d.ts';
+                            const options = parsedTree[key];
+                            const samples = options.samples;
+
+                            if (samples) {
+                                for (const sample of samples) {
+                                    const products = sample.products;
+                                    const value = sample.value;
+
+                                    // Temporary measure to store the to-be doclets in a results object, for debugging
+                                    res[interfaceFileName] = `
+                                        @sample ${products && `{${products.join('|')}}` || ''} ${value}\n
+                                        @product ${products && products.join(' ') || ''}\n
+                                    `;
+                                }
+
+                            }
+
+                        }
+                    }
 
                     log.success('Interface declarations propegated with jsDocs');
                 });
