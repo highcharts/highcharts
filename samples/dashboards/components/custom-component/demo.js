2e3000d3f9295b1ce2dc16cc35ae6f193a20e95f
Sebastian Bochan <sebastian@blacklabel.pl>
2024-03-04 11:39:34 +0100

    Applied requested changes.

--- samples/dashboards/components/custom-component/demo.js
+++ samples/dashboards/components/custom-component/demo.js
@@ -9,9 +9,7 @@ class YouTubeComponent extends Component {
     }
 
     resize(width, height) {
-        super.resize.call(this, width, height);
-        this.youTubeElement.setAttribute('width', width - 10); // padding
-        this.youTubeElement.setAttribute('height', height - 10); // padding
+        super.resize(width, height);
     }
 
     async load() {
