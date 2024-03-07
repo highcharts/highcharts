096303ffc17b95450833803dd35a5e1d4eecb023
Markus Knutson Barstad <mkb@0xF0F0F.local>
2024-03-07 12:25:17 +0100

    Stripping default-files down to props

--- tools/gulptasks/jsdoc-hack.js
+++ tools/gulptasks/jsdoc-hack.js
@@ -22,6 +22,216 @@ function declarationPropegation() {
     const treeName = 'meta.json';
     const treeFolder = '../hc-integration-gen/dist/meta/';
     const treePath = treeFolder + treeName;
+    const files = [
+        {
+            defaults: 'BellcurveSeriesDefaults.js',
+            options: 'BellcurveSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'BoxPlotSeriesDefaults.js',
+            options: 'BoxPlotSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'BubbleLegendDefaults.js',
+            options: 'BubbleSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'BulletSeriesDefaults.js',
+            options: 'BulletTargetOptions.d.ts'
+        },
+        {
+            defaults: 'CandlestickSeriesDefaults.js',
+            options: 'CandlestickSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'ColumnSeriesDefaults.js',
+            options: 'ColumnSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'ColumnPyramidSeriesDefaults.js',
+            options: 'ColumnPyramidSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'CylinderSeriesDefaults.js',
+            options: 'CylinderSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'DependencyWheelSeriesDefaults.js',
+            options: 'DependencyWheelSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'DotPlotSeriesDefaults.js',
+            options: 'DotPlotSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'DumbbellSeriesDefaults.js',
+            options: 'DumbbellSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'ErrorBarSeriesDefaults.js',
+            options: 'ErrorBarSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'FlagsSeriesDefaults.js',
+            options: 'FlagsSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'FunnelSeriesDefaults.js',
+            options: 'FunnelSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'Funnel3DSeriesDefaults.js',
+            options: 'Funnel3DSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'GanttSeriesDefaults.js',
+            options: 'GanttSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'HLCSeriesDefaults.js',
+            options: 'HLCSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'HeatmapSeriesDefaults.js',
+            options: 'HeatmapSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'HeikinAshiSeriesDefaults.js',
+            options: 'HeikinAshiSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'HistogramSeriesDefaults.js',
+            options: 'HistogramSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'ItemSeriesDefaults.js',
+            options: 'ItemSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'MapSeriesDefaults.js',
+            options: 'MapSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'MapPointSeriesDefaults.js',
+            options: 'MapPointSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'OHLCSeriesDefaults.js',
+            options: 'OHLCSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'MapLineSeriesDefaults.js',
+            options: 'MapLineSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'OrganizationSeriesDefaults.js',
+            options: 'OrganizationSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'NetworkgraphSeriesDefaults.js',
+            options: 'NetworkgraphSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'ParetoSeriesDefaults.js',
+            options: 'ParetoSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'PackedBubbleSeriesDefaults.js',
+            options: 'PackedBubbleSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'PieSeriesDefaults.js',
+            options: 'PieSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'PolygonSeriesDefaults.js',
+            options: 'PolygonSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'PyramidSeriesDefaults.js',
+            options: 'PyramidSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'Pyramid3DSeriesDefaults.js',
+            options: 'Pyramid3DSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'SankeySeriesDefaults.js',
+            options: 'SankeySeriesOptions.d.ts'
+        },
+        {
+            defaults: 'SolidGaugeSeriesDefaults.js',
+            options: 'SolidGaugeSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'Scatter3DSeriesDefaults.js',
+            options: 'Scatter3DSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'ScatterSeriesDefaults.js',
+            options: 'ScatterSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'StreamgraphSeriesDefaults.js',
+            options: 'StreamgraphSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'TiledWebMapSeriesDefaults.js',
+            options: 'TiledWebMapSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'TilemapSeriesDefaults.js',
+            options: 'TilemapSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'SunburstSeriesDefaults.js',
+            options: 'SunburstSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'TimelineSeriesDefaults.js',
+            options: 'TimelineSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'VariablePieSeriesDefaults.js',
+            options: 'VariablePieSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'TreemapSeriesDefaults.js',
+            options: 'TreemapSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'TreegraphSeriesDefaults.js',
+            options: 'TreegraphSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'VariwideSeriesDefaults.js',
+            options: 'VariwideSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'VectorSeriesDefaults.js',
+            options: 'VectorSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'VennSeriesDefaults.js',
+            options: 'VennSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'WaterfallSeriesDefaults.js',
+            options: 'WaterfallSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'WindbarbSeriesDefaults.js',
+            options: 'WindbarbSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'XRangeSeriesDefaults.js',
+            options: 'XRangeSeriesOptions.d.ts'
+        },
+        {
+            defaults: 'WordcloudSeriesDefaults.js',
+            options: 'WordcloudSeriesOptions.d.ts'
+        }
+    ];
 
     log.starting('Interface decleration propegation');
 
@@ -45,51 +255,73 @@ function declarationPropegation() {
 
                     const parsedTree = JSON.parse(tree);
 
-                    for (const key of Object.keys(parsedTree)) {
+                    for (const { defaults, options } of files) {
+                        const interfaceName = defaults.split('.js')[0];
+                        const seriesName = defaults.split('Series')[0];
 
-                        if (!key.includes('HighchartsProductSeries')) {
-                            const [interfaceFileName, defaultsFileName] = [
-                                key.replace('plotOptions.', '') + 'Options.d.ts',
-                                key.replace('plotOptions.', '') + 'Defaults.js'
-                            ];
-                            const optionsObjDocs = parsedTree[key];
-                            const samples = optionsObjDocs.samples;
+                        fs.readFile(
+                            './code/es-modules/Series/' +
+                            seriesName + '/' +
+                            defaults,
+                            (err, content) => {
+                                if (err) {
+                                    log.failure('Failed to read ' + defaults);
+                                    return;
+                                }
+
+                                // Start from the defaults-object, chop comments
+                                const top = content
+                                    .toString()
+                                    .split(interfaceName)[1]
+                                    .replace(/\/\*.*?\*\//smg, '');
+
+                                log.message(top);
+                            }
+                        );
+
+                        const treeLookup = 'plotOptions.' + seriesName.toLowerCase();
+                        const optionsObjDocs = parsedTree[treeLookup];
+                        const samples = (
+                            optionsObjDocs &&
+                            optionsObjDocs.samples
+                        );
+
+                        if (samples) {
                             let jsdocStr = (
                                 '/**\n' +
-                                optionsObjDocs.description
-                                    .split('\n')
-                                    .map(line => ('* ' + line + '\n'))
-                                    .join('')
+                                    optionsObjDocs.description
+                                        .split('\n')
+                                        .map(line => ('* ' + line + '\n'))
+                                        .join('')
                             );
 
-                            if (samples) {
+                            for (const sample of samples) {
+                                const products = sample.products;
+                                jsdocStr += (
+                                    `* @sample ${
+                                        (
+                                            products &&
+                                                `{${products.join('|')}} ` ||
+                                                ''
+                                        ) + sample.value
+                                    }\n*         ${sample.name}\n*` +
 
-                                for (const sample of samples) {
-                                    const products = sample.products;
-                                    jsdocStr += (
-                                        `* @sample ${
-                                            (
-                                                products && `{${products.join('|')}} ` || ''
-                                            ) + sample.value
-                                        }\n*         ${sample.name}\n*` +
-
-                                        // Unsure about this, @product is probably
-                                        // future field of optionsObjDocs
-                                        `${
-                                            products && (
-                                                '* @product' + products.join(' ')
-                                            ) ||
-                                            ''
-                                        }\n`
-                                    );
-                                }
+                                    // Unsure about this, @product is probably
+                                    // future field of optionsObjDocs
+                                    `${
+                                        products && (
+                                            '* @product' + products.join(' ')
+                                        ) ||
+                                        ''
+                                    }\n`
+                                );
                             }
 
                             jsdocStr += '**/';
-                            log.message('\n' + jsdocStr);
-
+                            // log.message('\n' + jsdocStr);
                         }
                     }
+
 
                     log.success('Interface declarations propegated with jsDocs');
                 });
