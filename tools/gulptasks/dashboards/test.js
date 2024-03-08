5f0ead9a37583e5553e18e8e14ed527d1116e665
Sebastian Bochan <sebastian@blacklabel.pl>
2024-03-08 09:53:20 +0100

    Optymized dashboards gulp task: dashboards/test.

--- tools/gulptasks/dashboards/test.js
+++ tools/gulptasks/dashboards/test.js
@@ -12,15 +12,10 @@ const gulp = require('gulp');
  *
  * */
 
-
-const { lintDTS } = require('../lint-dts');
-const { lintTS } = require('../lint-ts');
 const { testKarma } = require('../test-karma');
 
 gulp.task('dashboards/test', gulp.series(
     'dashboards/scripts',
-    () => lintTS({ datagrid: true }),
-    () => lintTS({ dashboards: true }),
-    () => testKarma({ dashboards: true }),
-    () => lintDTS({ dashboards: true })
+    'dlint',
+    () => testKarma({ dashboards: true })
 ));
