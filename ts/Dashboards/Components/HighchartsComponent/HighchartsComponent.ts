62540a0133698522569de6c9e2bc055864b33337
Dawid Draguła <dawid.dragula@gmail.com>
2024-03-11 16:45:52 +0100

    Fixed point dragging listening in Highcharts Component.

--- ts/Dashboards/Components/HighchartsComponent/HighchartsComponent.ts
+++ ts/Dashboards/Components/HighchartsComponent/HighchartsComponent.ts
@@ -334,13 +334,20 @@ class HighchartsComponent extends Component {
         const { connector: store, chart } = this;
 
         if (store && chart && this.options.allowConnectorUpdate) {
-            chart.series.forEach((series): void => {
-                series.points.forEach((point): void => {
-                    addEvent(point, 'drag', (): void => {
-                        this.onChartUpdate(point, store);
-                    });
-                });
-            });
+            for (let i = 0, iEnd = chart.series.length; i < iEnd; ++i) {
+                const series = chart.series[i];
+                series.update({
+                    point: {
+                        events: {
+                            drag: (e: { target: Point }): void => {
+                                this.onChartUpdate(e.target, store);
+                            }
+                        }
+                    }
+                }, false);
+            }
+
+            chart.redraw();
         }
     }
 
