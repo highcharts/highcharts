e92f8d7e26f17628b8f7179a37278e5efa8c9051
marvin19 <marita.vindedal@highsoft.com>
2024-03-12 12:54:46 +0100

    Some refactoring.

--- samples/highcharts/studies/accessible-stevie/demo.js
+++ samples/highcharts/studies/accessible-stevie/demo.js
@@ -1,7 +1,8 @@
-const descriptionDivs = [];
-const tooltipDivs = [];
+const descriptionDivs = [],
+    tooltipDivs = [],
+    tooltipCheckbox = document.getElementById('checkbox-info'),
+    nullpointCheckbox = document.getElementById('checkbox-nullpoint');
 let parsedData = [],
-    checkboxChecked = true,
     nullpointCheckboxChecked = true;
 
 
@@ -26,11 +27,12 @@ Highcharts.chart('container', {
                     const id = `tooltip-${point.series.name}-${point.index}`;
                     const descriptionDivId = id + '-description';
                     const tooltipDivId = id + '-tooltip';
-                    if (checkboxChecked) {
+
+                    if (tooltipCheckbox.checked) {
                         point.graphic.element.setAttribute('aria-describedby', descriptionDivId);
                     }
 
-                    if (nullpointCheckboxChecked && point.y === 0) {
+                    if (nullpointCheckbox.checked && point.y === 0) {
                         point.graphic.element.setAttribute('aria-hidden', 'true');
                     }
 
@@ -54,23 +56,14 @@ Highcharts.chart('container', {
                         }
 
                         // This text gets stripped of HTML tags and is used as the description for the points
-                        const descriptionDiv = document.createElement('div');
-                        descriptionDiv.setAttribute('id', descriptionDivId);
-                        descriptionDiv.classList.add('tooltip-description');
                         const addPunctuation = tooltipText.replace(/<\/li>/g, '</li>. '); // Hack for adding punctuation after the song
                         let strippedDescription = addPunctuation.replace(/<[^>]*>?/gm, '');
                         strippedDescription = strippedDescription.replace(/Wins: \d+|Nominations: \d+/g, '');
-                        descriptionDiv.innerHTML = strippedDescription;
-                        document.body.appendChild(descriptionDiv);
+                        const descriptionDiv = createAndAppendDiv(descriptionDivId, strippedDescription);
                         descriptionDivs.push(descriptionDiv);
 
                         // The same text as above, but contains html for tooltip formatting
-                        const tooltipDiv = document.createElement('div');
-                        tooltipDiv.setAttribute('id', tooltipDivId);
-                        tooltipDiv.setAttribute('aria-hidden', 'true');
-                        tooltipDiv.classList.add('visually-hidden');
-                        tooltipDiv.innerHTML = tooltipText;
-                        document.body.appendChild(tooltipDiv);
+                        const tooltipDiv = createAndAppendDiv(tooltipDivId, tooltipText);
                         tooltipDivs.push(tooltipDiv);
 
                     }
@@ -78,8 +71,6 @@ Highcharts.chart('container', {
             }
         }
     },
-
-    // Checking
     title: {
         text: 'Stevie Wonder Grammy Awards Wins and Nominations'
     },
@@ -170,23 +161,28 @@ Highcharts.chart('container', {
     }]
 });
 
-// Button to toggle more information
-const screenReaderSection = document.getElementById('highcharts-screen-reader-region-before-0');
+// Chart variables
+const chart = Highcharts.charts[0],
+    points = chart.series.map(s => s.points).flat();
 
-const moreInfoButton = document.createElement('button'),
+// New DOM elements
+const screenReaderSection = document.getElementById('highcharts-screen-reader-region-before-0'),
+    moreInfoButton = document.createElement('button'),
     infoP = document.createElement('p'),
     infoDiv = document.createElement('div');
 
+// Adding more info button to the DOM and setting attributes
 moreInfoButton.textContent = 'Toggle more information';
 moreInfoButton.setAttribute('aria-expanded', 'true');
 infoDiv.setAttribute('id', '#info-div');
 infoDiv.style.display = 'block';
 infoP.textContent = document.getElementById('rich-description').textContent;
-
 screenReaderSection.firstChild.appendChild(moreInfoButton);
 moreInfoButton.parentNode.appendChild(infoDiv);
 infoDiv.appendChild(infoP);
 
+// Function to create tooltip text with <strong> and <br> for
+// attempt of styling withous useHTML
 function createTooltipText(
     grammy, year, seriesName, y, songValues, awardValues
 ) {
@@ -198,20 +194,30 @@ function createTooltipText(
 
     let tooltipText =
         `<strong>${grammy} Grammy Awards (${year})</strong>. </br>` +
-        `<strong>${seriesName.charAt(0).toUpperCase() + seriesName.slice(1)}</strong>: ${y}</br>`;
+        `<strong>${seriesName.charAt(0).toUpperCase() +
+        seriesName.slice(1)}</strong>: ${y}</br>`;
 
     if (songValues.includes('None') || awardValues.includes('None')) {
         return tooltipText;
     }
     tooltipText += mappedValues.map(value => `<strong>${value.award.trim()}: </strong>${value.song}. </br>`).join('');
 
-
     return tooltipText;
 }
 
+function createAndAppendDiv(id, innerHTML) {
+    const div = document.createElement('div');
+    div.setAttribute('id', id);
+    div.classList.add('tooltip-description');
+    div.innerHTML = innerHTML;
+    document.body.appendChild(div);
+    return div;
+}
+
+// More information button that is appended to the screen reader
+// section in the chart container.
 moreInfoButton.addEventListener('click', () => {
     const expanded = moreInfoButton.getAttribute('aria-expanded') === 'true';
-    const infoDiv = document.getElementById('#info-div');
 
     if (!expanded) {
         infoDiv.style.display = 'block';
@@ -220,46 +226,29 @@ moreInfoButton.addEventListener('click', () => {
         infoDiv.style.display = 'none';
         moreInfoButton.setAttribute('aria-expanded', 'false');
     }
-
 });
-// Verbosity settings
-const tooltipCheckbox = document.getElementById('checkbox-info');
-const nullpointCheckbox = document.getElementById('checkbox-nullpoint');
+
+// Verbosity checkbox for hiding points with zero value
 nullpointCheckbox.addEventListener('change', () => {
-    const chart = Highcharts.charts[0];
-    const points = chart.series.map(s => s.points).flat();
-
     nullpointCheckboxChecked = !nullpointCheckboxChecked;
-
     points.forEach(point => {
-        // If nullpointCheckbox is checked and the point's y value is 0, set aria-hidden to true
         if (nullpointCheckbox.checked && point.y === 0) {
             point.graphic.element.setAttribute('aria-hidden', 'true');
         } else {
-            // If nullpointCheckbox is not checked or the point's y value is not 0, set aria-hidden to false
             point.graphic.element.setAttribute('aria-hidden', 'false');
         }
     });
 });
 
+// Verbosity checkbox for announcing tooltip details to screen reader
 tooltipCheckbox.addEventListener('change', () => {
-    checkboxChecked = !checkboxChecked;
-
-    const chart = Highcharts.charts[0];
-    const points = chart.series.map(s => s.points).flat();
-
     points.forEach(point => {
-        if (checkboxChecked) {
-            const id = `tooltip-${point.series.name}-${point.index}-description`;
+        if (tooltipCheckbox.checked) {
+            const id =
+                `tooltip-${point.series.name}-${point.index}-description`;
             point.graphic.element.setAttribute('aria-describedby', id);
         } else {
             point.graphic.element.removeAttribute('aria-describedby');
         }
-    });
-
-    descriptionDivs.forEach(descriptionDiv => {
-        console.log(descriptionDiv);
-        descriptionDiv.setAttribute('style', 'display: none');
-        //descriptionDiv.setAttribute('aria-hidden', !checkboxChecked);
     });
 });
