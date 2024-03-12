15301089cdebfdf605b24e6a623ac1d2e93f6fbe
karolkolodziej <karolkolodziej01@gmail.com>
2024-03-12 11:36:06 +0100

    Improved linting.

--- samples/dashboards/components/climate-all/demo.js
+++ samples/dashboards/components/climate-all/demo.js
@@ -876,7 +876,8 @@ async function updateBoard(board, city, column, scale, newData) {
     );
     const selectionTable = await dataPool.getConnectorTable('Range Selection');
     const cityTable = await dataPool.getConnectorTable(city);
-    const citiesTable = await dataPool.getConnectorTable('Cities'); // Geographical data
+    // Geographical data
+    const citiesTable = await dataPool.getConnectorTable('Cities');
 
     const [
         timeRangeSelector,
@@ -933,7 +934,9 @@ async function updateBoard(board, city, column, scale, newData) {
     for (let i = 0, iEnd = mapPoints.length; i < iEnd; ++i) {
         // Get elevation of city
         const cityName = mapPoints[i].name;
-        const cityInfo = citiesTable.getRowObject(citiesTable.getRowIndexBy('city', cityName));
+        const cityInfo = citiesTable.getRowObject(
+            citiesTable.getRowIndexBy('city', cityName)
+        );
 
         const pointTable = await dataPool.getConnectorTable(cityName);
 
@@ -1049,7 +1052,8 @@ function changeTheme() {
     toggleContainer.className = className;
 }
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-    toggle.checked = isDarkModeEnabled();
-    changeTheme();
-});
+window.matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', () => {
+        toggle.checked = isDarkModeEnabled();
+        changeTheme();
+    });
