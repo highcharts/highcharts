30ada2cf7b188f074d7718e189d77b9dd7029ec8
Xillez <kent.wincent46@gmail.com>
2024-03-12 14:07:05 +0100

    PR feedback

--- ts/Stock/RangeSelector/RangeSelector.ts
+++ ts/Stock/RangeSelector/RangeSelector.ts
@@ -241,9 +241,7 @@ class RangeSelector {
             ), // #1568
             baseXAxisOptions: AxisOptions,
             range = rangeOptions._range,
-            rangeMin,
-            minSetting: (number|null|undefined),
-            rangeSetting: (number|undefined),
+            rangeMin: (number|undefined),
             ctx: Axis,
             ytdExtremes,
             addOffsetMin = true;
@@ -364,24 +362,25 @@ class RangeSelector {
 
         // Update the chart
         if (!baseAxis) {
-            // X-axis options not defined, avoids undefined error and
-            // sets deferred range to undefined (#20529).
-            if (!chart.userOptions.xAxis) {
-                chart.userOptions.xAxis = {};
-            }
-
             // Axis not yet instantiated. Temporarily set min and range
-            // options and remove them on chart load (#4317).
-            // User options have superceeded options (#20529).
-            baseXAxisOptions = splat(chart.userOptions.xAxis)[0];
-            rangeSetting = baseXAxisOptions.range;
-            baseXAxisOptions.range = range;
-            minSetting = baseXAxisOptions.min;
-            baseXAxisOptions.min = rangeMin;
+            // options and axes once defined and remove them on
+            // chart load (#4317 & #20529).
+            baseXAxisOptions = splat(chart.options.xAxis)[0];
+            const axisRangeUpdateEvent = addEvent(
+                chart,
+                'afterGetAxes',
+                function (): void {
+                    const xAxis = chart.xAxis[0];
+                    xAxis.range = xAxis.options.range = range;
+                    xAxis.min = xAxis.options.min = rangeMin;
+                }
+            );
             addEvent(chart, 'load', function resetMinAndRange(): void {
+                const xAxis = chart.xAxis[0];
                 chart.setFixedRange(rangeOptions._range);
-                baseXAxisOptions.range = rangeSetting;
-                baseXAxisOptions.min = minSetting;
+                xAxis.options.range = baseXAxisOptions.range;
+                xAxis.options.min = baseXAxisOptions.min;
+                axisRangeUpdateEvent(); // Remove event
             });
         } else {
             // Existing axis object. Set extremes after render time.
