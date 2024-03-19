5a12bb1f53c24dc306239060315a2cf53a3decde
Kamil Musiałowski <kamil.musialowski@blacklabel.pl>
2024-03-19 13:13:05 +0100

    Added whole orderbook logic.

--- samples/stock/demo/orderbook-chart/demo.js
+++ samples/stock/demo/orderbook-chart/demo.js
@@ -1,59 +1,185 @@
-// Custom template helper
-Highcharts.Templating.helpers.abs = value => Math.abs(value);
+function getRandomNumber(min, max) {
+    return Math.round(Math.random() * (max - min)) + min;
+}
 
-// Age categories
-const categories = [];
+function generateBidAndAskData(n) {
+    const data = [[], []];
+    let bidPrice = getRandomNumber(29000, 30000),
+        askPrice = bidPrice + 0.5;
+    for (let i = 0; i < n; i++) {
+        bidPrice -= ((i * getRandomNumber(8, 10)));
+        askPrice += ((i * getRandomNumber(8, 10)));
+        data[0].push({
+            x: i,
+            y: (-i - 1) * getRandomNumber(70000, 110000),
+            price: bidPrice
+        });
+
+        data[1].push({
+            x: i,
+            y: (i + 1) * getRandomNumber(70000, 110000),
+            price: askPrice
+        });
+    }
+    return data;
+}
+
+const [bidsData, asksData] = generateBidAndAskData(10);
+
+function updateData(series) {
+    const data = generateBidAndAskData(10);
+    series.forEach((s, i) => {
+        s.setData(data[i]);
+    });
+}
 
 Highcharts.chart('container', {
     chart: {
-        type: 'bar'
-    },
-    title: {
-        text: 'Orderbook chart'
-    },
-    xAxis: [{
-        categories: categories,
-        reversed: false,
-        labels: {
-            step: 1
+        type: 'bar',
+        backgroundColor: '#23232f',
+        marginTop: 70,
+        events: {
+            load() {
+                this.titles = [];
+                setInterval(() => {
+                    updateData(this.series);
+                }, 200);
+            },
+            render() {
+                // Create and align titles
+                if (this.titles.length) {
+                    this.titles.forEach(title => {
+                        title.destroy();
+                    });
+                    this.titles.length = 0;
+                }
+
+                const { plotLeft, plotTop, plotWidth, renderer } = this;
+
+                this.titles.push(
+                    renderer.text(
+                        'Bids', plotLeft, plotTop - 5
+                    )
+                        .attr({ fill: '#ffffff' })
+                        .css({
+                            fontWeight: 700,
+                            fontSize: 18
+                        })
+                        .add()
+                );
+
+                this.titles.push(
+                    renderer.text(
+                        'Asks', plotWidth - 35, plotTop - 5
+                    )
+                        .attr({ fill: '#ffffff' })
+                        .css({
+                            fontWeight: 700,
+                            fontSize: 18
+                        })
+                        .add()
+                );
+
+                this.titles.push(
+                    renderer.text(
+                        'Price', plotWidth / 2 - plotLeft, plotTop - 5
+                    )
+                        .attr({ fill: '#ffffff' })
+                        .css({
+                            fontWeight: 700,
+                            fontSize: 18
+                        })
+                        .add()
+                );
+            }
         }
+    },
+
+    title: {
+        text: 'Orderbook live chart',
+        style: {
+            color: '#ffffff'
+        }
+    },
+
+    xAxis: [{
+        reversed: true,
+        visible: false
     }, {
         opposite: true,
-        reversed: false,
-        categories: categories,
-        linkedTo: 0,
-        labels: {
-            step: 1
-        }
-    }],
-    yAxis: {
         visible: false
+    }],
+
+    yAxis: {
+        visible: false,
+        min: -1200000,
+        max: 1200000
     },
+
     legend: {
         enabled: false
     },
 
+    navigation: {
+        buttonOptions: {
+            theme: {
+                fill: 'none'
+            }
+        }
+    },
+
     plotOptions: {
         series: {
+            animation: false,
             stacking: 'normal',
             pointPadding: 0,
-            groupPadding: 0
+            groupPadding: 0,
+            dataLabels: {
+                enabled: true,
+                color: '#ffffff'
+            },
+            borderWidth: 0
         }
     },
 
     series: [{
+        dataLabels: [{
+            x: -999,
+            style: {
+                fontSize: 14,
+                textOutline: 0
+            },
+            formatter() {
+                return Math.abs(this.point.y);
+            }
+        }, {
+            align: 'right',
+            style: {
+                fontSize: 13,
+                textOutline: 0
+            },
+            format: '{point.price}'
+        }],
         name: 'Bids',
-        color: 'blue',
-        data: [
-            -8.98, -7.52, -6.65, -5.72, -4.85,
-            -3.71, -2.76, -2.07, -1.70, -1.47
-        ]
+        color: '#42b3f0',
+        data: bidsData
     }, {
+        dataLabels: [{
+            x: 999,
+            style: {
+                fontSize: 14,
+                textOutline: 0
+            }
+        }, {
+            align: 'left',
+            style: {
+                fontSize: 13,
+                textOutline: 0
+            },
+            format: '{point.price}'
+        }],
         name: 'Asks',
-        color: 'red',
-        data: [
-            8.84, 7.42, 6.57, 5.68, 4.83,
-            3.74, 2.80, 2.14, 1.79, 1.59
-        ]
+        color: '#d76769',
+        data: asksData
     }]
 });
