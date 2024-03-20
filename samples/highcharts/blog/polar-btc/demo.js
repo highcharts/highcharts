226c276aaffec2ceeef69db01f920f454616c5b6
Mateusz Bernacik <mat.bernacik@gmail.com>
2024-03-20 13:41:14 +0100

    Requested changes.

--- samples/highcharts/blog/polar-btc/demo.js
+++ samples/highcharts/blog/polar-btc/demo.js
@@ -17,8 +17,10 @@ Highcharts.chart('container', {
         gridLineColor: '#8991ad',
         lineWidth: 0,
         tickmarkPlacement: 'on',
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        categories: [
+            'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+        ],
         accessibility: {
             description: 'Months of the Year'
         }
@@ -57,14 +59,18 @@ Highcharts.chart('container', {
         color: '#ffbf00',
         name: '2020',
         zIndex: 1,
-        data: [9349.1, 8543.7, 6412.5, 8629.0, 9454.8, 9135.4,
-            11333.4, 11644.2, 10776.1, 13797.3, 19698.1, 28949.4]
+        data: [
+            9349.1, 8543.7, 6412.5, 8629.0, 9454.8, 9135.4,
+            11333.4, 11644.2, 10776.1, 13797.3, 19698.1, 28949.4
+        ]
     }, {
         color: '#fb922c',
         fillOpacity: 0.2,
         name: '2021',
         zIndex: 0,
-        data: [33108.1, 45164.0, 58763.7, 57720.3, 37298.6, 35026.9,
-            41553.7, 47130.4, 43823.3, 61309.6, 56882.9, 46219.5]
+        data: [
+            33108.1, 45164.0, 58763.7, 57720.3, 37298.6, 35026.9,
+            41553.7, 47130.4, 43823.3, 61309.6, 56882.9, 46219.5
+        ]
     }]
 });
