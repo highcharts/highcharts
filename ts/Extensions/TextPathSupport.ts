7caff927bdfc80da815fda6864e528c5da3cae3a
Markus Knutson Barstad <mkb@0xfcfcfc.local>
2024-04-05 14:06:21 +0200

    Check if labels have allowOverlap

--- ts/Extensions/TextPathSupport.ts
+++ ts/Extensions/TextPathSupport.ts
@@ -41,112 +41,112 @@ function hideOverlappingPolygons(this: Chart): void {
                 linkOrPointIndex < length;
                 linkOrPointIndex++
             ) {
-                const label = textPathCandidates[linkOrPointIndex]
-                        .dataLabel
-                        ?.text
-                        ?.element,
-                    tp = label && label.querySelector('textPath');
+                const label = textPathCandidates[linkOrPointIndex].dataLabel;
 
-                if (tp) {
-                    const polygon: [number, number][] = [],
-                        { b, h } = serie
-                            .chart
-                            .renderer
-                            .fontMetrics(label),
-                        descender = h - b,
-                        lineCleanerRegex = new RegExp(
-                            '(<tspan>|' +
-                            '<tspan(?!\\sclass="highcharts-br")[^>]*>|' +
-                            '<\\/tspan>)',
-                            'g'
-                        ),
-                        lines = tp
-                            .innerHTML
-                            .replace(lineCleanerRegex, '')
-                            .split(
-                                /<tspan class="highcharts-br"[^>]*>/
+                if (label && !label.options?.allowOverlap) {
+                    const tp = label.text?.element.querySelector('textPath');
+
+                    if (tp) {
+                        const polygon: [number, number][] = [],
+                            { b, h } = serie
+                                .chart
+                                .renderer
+                                .fontMetrics(label.element),
+                            descender = h - b,
+                            lineCleanerRegex = new RegExp(
+                                '(<tspan>|' +
+                                '<tspan(?!\\sclass="highcharts-br")[^>]*>|' +
+                                '<\\/tspan>)',
+                                'g'
                             ),
-                        numOfLines = lines.length;
+                            lines = tp
+                                .innerHTML
+                                .replace(lineCleanerRegex, '')
+                                .split(
+                                    /<tspan class="highcharts-br"[^>]*>/
+                                ),
+                            numOfLines = lines.length;
 
 
-                    // Calculate top and bottom coordinates for
-                    // either the start or the end of a single
-                    // character, and append it to the polygon.
-                    const appendTopAndBottom = (
-                        charIndex: number,
-                        positionOfChar: PositionObject
-                    ): [[number, number], [number, number]] => {
-                        const { x, y } = positionOfChar,
-                            rotation = (
-                                tp.getRotationOfChar(charIndex) - 90
-                            ) * deg2rad,
-                            cosRot = Math.cos(rotation),
-                            sinRot = Math.sin(rotation);
-                        return [
-                            [
-                                x - descender * cosRot,
-                                y - descender * sinRot
-                            ],
-                            [
-                                x + b * cosRot,
-                                y + b * sinRot
-                            ]
-                        ];
-                    };
-
-                    for (
-                        let i = 0, lineIndex = 0;
-                        lineIndex < numOfLines;
-                        lineIndex++
-                    ) {
-                        const line = lines[lineIndex],
-                            lineLen = line.length;
+                        // Calculate top and bottom coordinates for
+                        // either the start or the end of a single
+                        // character, and append it to the polygon.
+                        const appendTopAndBottom = (
+                            charIndex: number,
+                            positionOfChar: PositionObject
+                        ): [[number, number], [number, number]] => {
+                            const { x, y } = positionOfChar,
+                                rotation = (
+                                    tp.getRotationOfChar(charIndex) - 90
+                                ) * deg2rad,
+                                cosRot = Math.cos(rotation),
+                                sinRot = Math.sin(rotation);
+                            return [
+                                [
+                                    x - descender * cosRot,
+                                    y - descender * sinRot
+                                ],
+                                [
+                                    x + b * cosRot,
+                                    y + b * sinRot
+                                ]
+                            ];
+                        };
 
                         for (
-                            let lineCharIndex = 0;
-                            lineCharIndex < lineLen;
-                            lineCharIndex += 5
+                            let i = 0, lineIndex = 0;
+                            lineIndex < numOfLines;
+                            lineIndex++
                         ) {
-                            const srcCharIndex = (
-                                    i +
-                                    lineCharIndex +
-                                    lineIndex
-                                ),
-                                [lower, upper] = appendTopAndBottom(
-                                    srcCharIndex,
-                                    tp.getStartPositionOfChar(srcCharIndex)
-                                );
+                            const line = lines[lineIndex],
+                                lineLen = line.length;
 
-                            if (lineCharIndex === 0) {
-                                polygon.push(upper);
-                                polygon.push(lower);
-                            } else {
-                                if (lineIndex === 0) {
-                                    polygon.unshift(upper);
-                                }
-                                if (lineIndex === numOfLines) {
+                            for (
+                                let lineCharIndex = 0;
+                                lineCharIndex < lineLen;
+                                lineCharIndex += 5
+                            ) {
+                                const srcCharIndex = (
+                                        i +
+                                        lineCharIndex +
+                                        lineIndex
+                                    ),
+                                    [lower, upper] = appendTopAndBottom(
+                                        srcCharIndex,
+                                        tp.getStartPositionOfChar(srcCharIndex)
+                                    );
+
+                                if (lineCharIndex === 0) {
+                                    polygon.push(upper);
                                     polygon.push(lower);
+                                } else {
+                                    if (lineIndex === 0) {
+                                        polygon.unshift(upper);
+                                    }
+                                    if (lineIndex === numOfLines) {
+                                        polygon.push(lower);
+                                    }
                                 }
                             }
+
+                            i += lineLen - 1;
+
+                            const srcCharIndex = i + lineIndex;
+
+                            const
+                                charPos = tp.getEndPositionOfChar(srcCharIndex),
+                                [lower, upper] = appendTopAndBottom(
+                                    srcCharIndex,
+                                    charPos
+                                );
+                            polygon.unshift(upper);
+                            polygon.unshift(lower);
                         }
 
-                        i += lineLen - 1;
-
-                        const srcCharIndex = i + lineIndex;
-
-                        const
-                            charPos = tp.getEndPositionOfChar(srcCharIndex),
-                            [lower, upper] = appendTopAndBottom(
-                                srcCharIndex,
-                                charPos
-                            );
-                        polygon.unshift(upper);
-                        polygon.unshift(lower);
+                        // Close it
+                        polygon.push(polygon[0].slice() as [number, number]);
+                        polygonMap.push([polygon, linkOrPointIndex]);
                     }
-
-                    // Close it
-                    polygon.push(polygon[0].slice() as [number, number]);
-                    polygonMap.push([polygon, linkOrPointIndex]);
                 }
             }
 
