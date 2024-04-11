c5db31e667b50d8b86ebd33ef4f68d76fc79bd61
hubertkozik <hubertkozik13@gmail.com>
2024-04-11 14:48:44 +0200

    Fixed #20560, destroyed points in arearange were wrongly used on hover.

--- ts/Core/Series/Point.ts
+++ ts/Core/Series/Point.ts
@@ -1582,7 +1582,12 @@ class Point {
                 ));
             }
 
-        } else if (halo && halo.point && halo.point.haloPath) {
+        } else if (
+            halo &&
+            halo.point &&
+            halo.point.haloPath &&
+            !halo.point.destroyed
+        ) {
             // Animate back to 0 on the current halo point (#6055)
             halo.animate(
                 { d: halo.point.haloPath(0) },
