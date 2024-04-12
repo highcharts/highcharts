eeb67bd891936c227e8f6ff9b1df681735bfb2c3
bre1470 <40056287+bre1470@users.noreply.github.com>
2024-04-12 15:37:36 +0200

    Docs: Moved doclets for arearange.

---
+++ ts/Series/AreaRange/AreaRangeSeriesDefaults.ts
@@ -0,0 +1,38 @@
+/* *
+ *
+ *  (c) 2010-2024 Torstein Honsi
+ *
+ *  License: www.highcharts.com/license
+ *
+ *  !!!!!!! SOURCE GETS TRANSPILED BY TYPESCRIPT. EDIT TS FILE ONLY. !!!!!!!
+ *
+ * */
+
+'use strict';
+
+/* *
+ *
+ *  Imports
+ *
+ * */
+
+import SeriesRegistry from '../../Core/Series/SeriesRegistry.js';
+const {
+    area: { defaultOptions: AreaSeriesDefaults }
+} = SeriesRegistry.seriesTypes;
+
+/* *
+ *
+ *  API Options
+ *
+ * */
+
+const AreaRangeSeriesDefaults = AreaSeriesDefaults;
+
+/* *
+ *
+ *  Default Export
+ *
+ * */
+
+export default AreaRangeSeriesDefaults;
