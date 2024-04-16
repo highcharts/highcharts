f56a420d56755d39b0e908de195a55892f86192b
Mateusz Bernacik <mat.bernacik@gmail.com>
2024-04-16 11:09:36 +0200

    Added demo and data improvements.

--- samples/stock/demo/flags-general/demo.js
+++ samples/stock/demo/flags-general/demo.js
@@ -2,64 +2,65 @@
 
     // Load the dataset
     const data = await fetch(
-        'https://cdn.jsdelivr.net/gh/highcharts/highcharts@1485682/samples/data/btc-ohlc-historical.json'
+        'https://cdn.jsdelivr.net/gh/highcharts/highcharts@1485682/samples/data/btc-historical.json'
     ).then(response => response.json());
 
     // Create the chart
     Highcharts.stockChart('container', {
+        accessibility: {
+            typeDescription: `Stock chart with a line series and a flags series
+            indicating key events.`
+        },
 
         title: {
             text: 'Bitcoin Historical Price and Halvings'
         },
 
         xAxis: {
-            overscroll: 2678400000 // 31 days
-        },
-
-        yAxis: {
-            labels: {
-                align: 'left'
-            }
+            overscroll: 31536000000 // 1 year
         },
 
         series: [{
             name: 'Bitcoin Price',
+            color: '#ffbf00',
             data: data,
             id: 'dataseries',
-            type: 'candlestick'
+            tooltip: {
+                pointFormat: `<span style='color:{point.color}'>●</span>
+                    {series.name}: <b>{point.y:.2f}$</b><br/>`
+            }
 
             // the event marker flags
         }, {
             type: 'flags',
+            color: '#fb922c',
+            onSeries: 'dataseries',
             accessibility: {
                 exposeAsGroupOnly: true,
                 description: 'Bitcoin Halving Events'
             },
-            data: [
-                {
-                    x: Date.UTC(2012, 10, 28),
-                    title: '1st',
-                    text: '1st Halving'
-                },
-                {
-                    x: Date.UTC(2016, 6, 9),
-                    title: '2nd',
-                    text: '2nd Halving'
-                },
-                {
-                    x: Date.UTC(2020, 4, 11),
-                    title: '3rd',
-                    text: '3rd Halving'
-                },
-                {
-                    x: Date.UTC(2024, 3, 19),
-                    title: '4th',
-                    text: '4th Halving'
-                }
-            ],
-            onSeries: 'dataseries',
-            shape: 'circlepin',
-            width: 16
+            data: [{
+                x: Date.UTC(2016, 6, 9),
+                title: '2nd Halving',
+                text: 'Reward down: 25 BTC to 12.5 BTC per block'
+            },
+            {
+                x: Date.UTC(2020, 4, 11),
+                title: '3rd Halving',
+                text: 'Reward down: 12.5 BTC to 6.25 BTC per block'
+            }]
+        }, {
+            type: 'flags',
+            color: '#fb922c',
+            accessibility: {
+                exposeAsGroupOnly: true,
+                description: 'Bitcoin Halving Events'
+            },
+            data: [{
+                x: Date.UTC(2024, 3, 19),
+                title: '4th Halving',
+                text: 'Reward down: 6.25 BTC to 3.125 BTC per block'
+            }]
         }]
     });
 })();
