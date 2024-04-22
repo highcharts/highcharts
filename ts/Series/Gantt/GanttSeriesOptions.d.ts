7c5457fd3710c243935c5fa18c07a58178810ea3
bre1470 <40056287+bre1470@users.noreply.github.com>
2024-04-22 14:55:38 +0200

    Docs: Copied doclets for gantt.

--- ts/Series/Gantt/GanttSeriesOptions.d.ts
+++ ts/Series/Gantt/GanttSeriesOptions.d.ts
@@ -18,13 +18,14 @@
 
 import type AnimationOptions from '../../Core/Animation/AnimationOptions';
 import type ColorType from '../../Core/Color/ColorType';
-import type {
-    ConnectorsStartMarkerOptions,
-    ConnectorsOptions
+import type ConnectorsOptions, {
+    ConnectorsStartMarkerOptions
 } from '../../Gantt/ConnectorsOptions';
+import type DataLabelOptions from '../../Core/Series/DataLabelOptions';
 import type GanttSeries from './GanttSeries';
 import type { SeriesStatesOptions } from '../../Core/Series/SeriesOptions';
-import type XRangeSeriesOptions from '../../Series/XRange/XRangeSeriesOptions';
+import type TooltipOptions from '../../Core/TooltipOptions';
+import type XRangeSeriesOptions from '../XRange/XRangeSeriesOptions';
 
 /* *
  *
@@ -32,6 +33,11 @@ import type XRangeSeriesOptions from '../../Series/XRange/XRangeSeriesOptions';
  *
  * */
 
+/**
+ * @declare Highcharts.ConnectorsAnimationOptionsObject
+ *
+ * @optionparent series.gantt.connectors.animation
+ */
 export interface GanttAnimationOptions extends Partial<AnimationOptions> {
     reversed?: boolean;
 }
@@ -46,15 +52,67 @@ export interface GanttConnectorStartMarkerOptions extends ConnectorsStartMarkerO
 }
 
 export type GanttDependencyOptions = (
-    string|
-    GanttConnectorOptions|
-    Array<GanttConnectorOptions>|
-    Array<string>
+    | string
+    | GanttConnectorOptions
+    | Array<GanttConnectorOptions>
+    | Array<string>
 );
 
+
+/**
+ * A `gantt` series. If the [type](#series.gantt.type) option is not specified,
+ * it is inherited from [chart.type](#chart.type).
+ *
+ * A `gantt` series.
+ *
+ * @extends plotOptions.xrange
+ *
+ * @extends series,plotOptions.gantt
+ *
+ * @product gantt
+ *
+ * @requires highcharts-gantt
+ *
+ * @excluding boostThreshold, dashStyle, findNearestPointBy,
+ *            getExtremesFromAll, marker, negativeColor, pointInterval,
+ *            pointIntervalUnit, pointPlacement, pointStart
+ */
 export interface GanttSeriesOptions extends XRangeSeriesOptions {
+
     connectors?: GanttConnectorOptions;
+
+    /**
+     *
+     * @excluding afterAnimate
+     *
+     * @apioption series.gantt.events
+     */
+    events?: GanttSeriesEventsOptions;
+
+    /**
+     * Data for a Gantt series.
+     *
+     * @declare Highcharts.GanttPointOptionsObject
+     *
+     * @type {Array<*>}
+     *
+     * @extends series.xrange.data
+     *
+     * @excluding className, connect, dataLabels, events,
+     *            partialFill, selected, x, x2
+     *
+     * @product gantt
+     */
+    data?: Array<GanttPointOptions>;
+
+    dataLabels?: Partial<DataLabelOptions>;
+
+    grouping?: boolean;
+
     states?: SeriesStatesOptions<GanttSeries>;
+
+    tooltip?: Partial<TooltipOptions>;
+
 }
 
 /* *
