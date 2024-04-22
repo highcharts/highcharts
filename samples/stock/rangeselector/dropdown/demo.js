2e0279bd295551071ec7d25dc4fb8d2322043b32
Pawel Lysy <pawel.lysy@blacklabel.pl>
2024-04-22 11:11:05 +0200

    Revert "Test for chromium."
    This reverts commit 50177eaff836022aa425cd05656c6e4485344450.

--- samples/stock/rangeselector/dropdown/demo.js
+++ samples/stock/rangeselector/dropdown/demo.js
@@ -1,19 +1,47 @@
-const chart = Highcharts.stockChart('container', {
-    chart: {
-        type: 'line',
-        width: 400
-    },
-    rangeSelector: {
-        selected: 2
-    },
-    title: {
-        text: 'RangeSelector dropdown demo'
-    },
-    tooltip: {
-        valueSuffix: '°C'
-    },
-    series: [{
-        name: 'Temperatures',
-        data: [1, 2, 3, 4, 3, 4, 32, 4, 324, 32, 4, 32, 4, 1]
-    }]
-});
+(async () => {
+
+    // Load the dataset
+    const data = await fetch(
+        'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/range.json'
+    ).then(response => response.json());
+
+    const chart = Highcharts.stockChart('container', {
+        chart: {
+            type: 'arearange',
+            width: 400
+        },
+        rangeSelector: {
+            selected: 2
+        },
+        title: {
+            text: 'RangeSelector dropdown demo'
+        },
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        series: [{
+            name: 'Temperatures',
+            data: data
+        }]
+    });
+
+
+    document.getElementById('dropdown').addEventListener('click', e => {
+        const dropdown = e.target.dataset.dropdown;
+        if (dropdown) {
+            chart.update({
+                rangeSelector: {
+                    dropdown
+                }
+            });
+        }
+    });
+
+    document.getElementById('width').addEventListener('click', e => {
+        const width = e.target.dataset.width;
+        if (width) {
+            chart.setSize(width);
+        }
+    });
+
+})();
