79ca4fb5a7328f368f1e7bc5d445354983559035
DJTechnoo <askeltechno@hotmail.com>
2024-04-22 10:52:31 +0200

    Test clean up.

--- samples/unit-tests/navigator/navigator/demo.js
+++ samples/unit-tests/navigator/navigator/demo.js
@@ -1,34 +1,32 @@
 QUnit.test(
     'Handles should not be overlapped by xAxis labels (#2908)',
     function (assert) {
-        var chart = $('#container')
-            .highcharts('StockChart', {
-                navigator: {
-                    height: 20,
-                    xAxis: {
-                        id: 'test'
-                    }
-                },
-                series: [
-                    {
-                        data: [
-                            29.9,
-                            71.5,
-                            106.4,
-                            129.2,
-                            144.0,
-                            176.0,
-                            135.6,
-                            148.5,
-                            216.4,
-                            194.1,
-                            95.6,
-                            54.4
-                        ]
-                    }
-                ]
-            })
-            .highcharts();
+        const chart = Highcharts.StockChart('container', {
+            navigator: {
+                height: 20,
+                xAxis: {
+                    id: 'test'
+                }
+            },
+            series: [
+                {
+                    data: [
+                        29.9,
+                        71.5,
+                        106.4,
+                        129.2,
+                        144.0,
+                        176.0,
+                        135.6,
+                        148.5,
+                        216.4,
+                        194.1,
+                        95.6,
+                        54.4
+                    ]
+                }
+            ]
+        });
 
         assert.ok(
             chart.scroller.handles[0].zIndex >=
