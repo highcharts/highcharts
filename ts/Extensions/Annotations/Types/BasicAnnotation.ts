f4bf5e8c5ac0fb916213f5c3ca93967811b390c1
DJTechnoo <askeltechno@hotmail.com>
2024-04-30 13:10:48 +0200

    Renamed css vars, refactoring.

--- ts/Extensions/Annotations/Types/BasicAnnotation.ts
+++ ts/Extensions/Annotations/Types/BasicAnnotation.ts
@@ -30,7 +30,7 @@ import type PositionObject from '../../../Core/Renderer/PositionObject';
 import Annotation from '../Annotation.js';
 import MockPoint from '../MockPoint.js';
 import U from '../../../Core/Utilities.js';
-const { merge, pick } = U;
+const { merge } = U;
 
 /* *
  *
@@ -349,10 +349,8 @@ class BasicAnnotation extends Annotation {
             delete options.labelOptions;
             const type = options.shapes[0].type;
 
-            options.shapes[0].className = pick(
-                options.shapes[0].className,
-                ''
-            ) + ' highcharts-basic-shape';
+            options.shapes[0].className =
+                (options.shapes[0].className || '') + ' highcharts-basic-shape';
             // The rectangle is rendered as a path, whereas other basic shapes
             // are rendered as their respective SVG shapes.
             if (type && type !== 'path') {
