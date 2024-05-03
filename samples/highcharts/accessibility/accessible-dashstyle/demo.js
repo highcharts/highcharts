1ae8c34c774dd412cf57c8a74953a7ec6a4a41f4
marvin19 <marita.vindedal@highsoft.com>
2024-05-03 10:30:50 +0200

    Added accessible-dashstyle demo.

---
+++ samples/highcharts/accessibility/accessible-dashstyle/demo.js
@@ -0,0 +1,23 @@
+Highcharts.chart('container', {
+    chart: {
+        type: 'spline'
+    },
+
+    legend: {
+        symbolWidth: 80
+    },
+
+    plotOptions: {
+        series: {
+            color: '#000000'
+        }
+    },
+
+    series: [{
+        data: [1, 3, 2, 4, 5, 4, 6, 2, 3, 5, 6],
+        dashStyle: 'longdash'
+    }, {
+        data: [2, 4, 1, 3, 4, 2, 9, 1, 2, 3, 4, 5],
+        dashStyle: 'shortdot'
+    }]
+});
