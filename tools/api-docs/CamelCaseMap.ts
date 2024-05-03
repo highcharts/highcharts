561563d883040b593524f794d68c79167e291a3e
bre1470 <40056287+bre1470@users.noreply.github.com>
2024-05-03 10:36:38 +0200

    Tools: Added camel case map and fixed libs typing for api docs.

---
+++ tools/api-docs/CamelCaseMap.ts
@@ -0,0 +1,96 @@
+/* *
+ *
+ *  (c) Highsoft AS
+ *
+ * */
+
+
+/* *
+ *
+ *  Constants
+ *
+ * */
+
+
+export const CamelCaseMap: Record<string, string> = {
+    'abands': 'ABands',
+    'ad': 'AD',
+    'ao': 'AO',
+    'apo': 'APO',
+    'arcdiagram': 'ArcDiagram',
+    'area3d': 'Area3D',
+    'arearange': 'AreaRange',
+    'areaspline': 'AreaSpline',
+    'areasplinerange': 'AreaSplineRange',
+    'aroonoscillator': 'AroonOscillator',
+    'atr': 'ATR',
+    'bb': 'BB',
+    'boxplot': 'BoxPlot',
+    'cci': 'CCI',
+    'cmf': 'CMF',
+    'cmo': 'CMO',
+    'column3d': 'Column3D',
+    'columnpyramid': 'ColumnPyramid',
+    'columnrange': 'ColumnRange',
+    'dema': 'DEMA',
+    'dependencywheel': 'DependencyWheel',
+    'disparityindex': 'DisparityIndex',
+    'dmi': 'DMI',
+    'dotplot': 'DotPlot',
+    'dpo': 'DPO',
+    'ema': 'EMA',
+    'errorbar': 'ErrorBar',
+    'flowmap': 'FlowMap',
+    'funnel3d': 'Funnel3D',
+    'geoheatmap': 'GeoHeatmap',
+    'heikinashi': 'HeikinAshi',
+    'hlc': 'HLC',
+    'hollowcandlestick': 'HollowCandlestick',
+    'ikh': 'IKH',
+    'keltnerchannels': 'KeltnerChannels',
+    'linearregression': 'LinearRegression',
+    'linearregressionangle': 'LinearRegressionAngle',
+    'linearregressionintercept': 'LinearRegressionIntercept',
+    'linearregressionslopes': 'LinearRegressionSlopes',
+    'macd': 'MACD',
+    'mapbubble': 'MapBubble',
+    'mapline': 'MapLine',
+    'mappoint': 'MapPoint',
+    'mfi': 'MFI',
+    'natr': 'NATR',
+    'obv': 'OBV',
+    'ohlc': 'OHLC',
+    'packedbubble': 'PackedBubble',
+    'pc': 'PC',
+    'pivotpoints': 'PivotPoints',
+    'ppo': 'PPO',
+    'priceenvelopes': 'PriceEnvelopes',
+    'psar': 'PSAR',
+    'pyramid3d': 'Pyramid3D',
+    'roc': 'ROC',
+    'rsi': 'RSI',
+    'scatter3d': 'Scatter3D',
+    'slowstochastic': 'SlowStochastic',
+    'sma': 'SMA',
+    'solidgauge': 'SolidGauge',
+    'tema': 'TEMA',
+    'tiledwebmap': 'TiledWebMap',
+    'trendline': 'TrendLine',
+    'trix': 'TRIX',
+    'variablepie': 'VariablePie',
+    'vbp': 'VBP',
+    'vwap': 'VWAP',
+    'williamsr': 'WilliamsR',
+    'wma': 'WMA',
+    'xrange': 'XRange'
+}
+
+
+/* *
+ *
+ *  Default Export
+ *
+ * */
+
+
+export default CamelCaseMap;
