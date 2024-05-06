4b811740eb22e56d113392a3cd712055b3f516d1
marvin19 <marita.vindedal@highsoft.com>
2024-05-06 13:34:51 +0200

    Tweaked colors in demo.

--- samples/highcharts/accessibility/point-description-enabled-threshold/demo.js
+++ samples/highcharts/accessibility/point-description-enabled-threshold/demo.js
@@ -27,7 +27,7 @@ Highcharts.chart('container', {
     series: [{
         name: 'Emma',
         data: [9438, 10439, 11023, 13204, 10392, 9201, 12039],
-        color: '#3A3691',
+        color: '#00A855',
         accessibility: {
             description: 'Emma walked the most in total during the week with ' +
             '75739 steps.'
@@ -35,11 +35,11 @@ Highcharts.chart('container', {
     }, {
         name: 'John',
         data: [10200, 6243, 9472, 6311, 7901, 11320, 8032],
-        color: '#009AFA'
+        color: '#3A3691'
     }, {
         name: 'Alex',
         data: [9029, 5532, 7632, 10320, 6210, 13209, 3052],
-        color: '#00A855',
+        color: '#009AFA',
         accessibility: {
             description: 'Alex had the highest number of steps in a day ' +
                 'during the week on Saturday. He walked 13209 steps that day.'
