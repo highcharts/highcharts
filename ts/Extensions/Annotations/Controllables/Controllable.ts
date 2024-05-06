b66fb32d2786c5677bc0aec524bca4e7485f308c
DJTechnoo <askeltechno@hotmail.com>
2024-05-06 12:49:49 +0200

    Enhanced annotations update, removed unnessecary destroy and rerender.

--- ts/Extensions/Annotations/Controllables/Controllable.ts
+++ ts/Extensions/Annotations/Controllables/Controllable.ts
@@ -304,19 +304,16 @@ abstract class Controllable implements ControlTarget {
     ): void {
         const annotation = this.annotation,
             options = merge(true, this.options, newOptions),
-            parentGroup = this.graphic.parentGroup,
             Constructor: any = this.constructor;
 
-        this.destroy();
         const newControllable = new Constructor(
             annotation,
             options,
             this.index,
             this.itemType
         );
-        merge(true, this as any, newControllable);
+        merge(true, this as Controllable, newControllable);
 
-        this.render(parentGroup);
         this.redraw();
     }
 
