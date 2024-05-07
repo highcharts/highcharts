de31237d1d8a272866dae1ff392809ce18a91eb6
marvin19 <marita.vindedal@highsoft.com>
2024-05-07 09:14:36 +0200

    Added chart title and series name to dashstyle demo.

--- samples/highcharts/accessibility/accessible-dashstyle/demo.js
+++ samples/highcharts/accessibility/accessible-dashstyle/demo.js
@@ -2,7 +2,9 @@ Highcharts.chart('container', {
     chart: {
         type: 'spline'
     },
-
+    title: {
+        text: 'Dash styles'
+    },
     legend: {
         symbolWidth: 80
     },
@@ -15,9 +17,11 @@ Highcharts.chart('container', {
 
     series: [{
         data: [1, 3, 2, 4, 5, 4, 6, 2, 3, 5, 6],
-        dashStyle: 'longdash'
+        dashStyle: 'longdash',
+        name: 'Long dash'
     }, {
         data: [2, 4, 1, 3, 4, 2, 9, 1, 2, 3, 4, 5],
-        dashStyle: 'shortdot'
+        dashStyle: 'shortdot',
+        name: 'Short dot'
     }]
 });
