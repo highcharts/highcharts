45370492a3963cd690a0308d5d8f3ece4dc0ca31
Markus Knutson Barstad <markusknutsonbarstad@Markus-sin-MacBook-Pro.local>
2024-05-14 13:37:50 +0200

    Using a11y.descr, added back column settings

--- samples/highcharts/demo/combo-data-resolution/demo.js
+++ samples/highcharts/demo/combo-data-resolution/demo.js
@@ -2,12 +2,12 @@ const milliSecPerYear = 365.25 * 24 * 60 * 60 * 1000;
 
 Highcharts.chart('container', {
     title: {
-        text: 'Annual surface temperature change from 1961 to 2022',
+        text: 'Annual surface temperature change from 1961 to 2021',
         align: 'left'
     },
     accessibility: {
         description: `
-            A chart of annual surface temperature change from 1961 to 2022.
+            A chart of annual surface temperature change from 1961 to 2021.
             It employs two series, one column and one line,
             to convey the same data at different resolutions.
             There is only one column per thirty years,
@@ -19,7 +19,9 @@ Highcharts.chart('container', {
         name: 'Mean per thirty years',
         data: JSON.parse(document.getElementById('columnData').textContent),
         pointRange: 30 * 365 * 24 * 36e5,
-
+        groupPadding: 0,
+        pointPadding: 0,
+        pointPlacement: -0.5,
         tooltip: {
             // Subtract 30 years from the year to get the start of the period
             headerFormat: (
@@ -29,17 +31,21 @@ Highcharts.chart('container', {
     }, {
         name: 'Yearly mean',
         data: JSON.parse(document.getElementById('lineData').textContent),
-        description: `
-            A data series illustrating the same data as the first,
-            but at different time intervals
-        `
+        accessibility: {
+            description: `
+                A data series illustrating the same data as the first,
+                but at different time intervals
+            `
+        }
     }],
     tooltip: {
         valueSuffix: '°C',
         valueDecimals: 2
     },
     yAxis: {
-        description: 'temperature',
+        accessibility: {
+            description: 'temperature'
+        },
         minPadding: 0,
         maxPadding: 0,
         labels: {
