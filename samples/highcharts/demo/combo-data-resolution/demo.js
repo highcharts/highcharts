4708b60039e84e645a44d0eab18b2cc96f51c27b
Markus Knutson Barstad <markusknutsonbarstad@Markus-sin-MacBook-Pro.local>
2024-05-14 13:30:26 +0200

    Changed dataset

--- samples/highcharts/demo/combo-data-resolution/demo.js
+++ samples/highcharts/demo/combo-data-resolution/demo.js
@@ -1,11 +1,13 @@
+const milliSecPerYear = 365.25 * 24 * 60 * 60 * 1000;
+
 Highcharts.chart('container', {
     title: {
-        text: 'Global temperature mean anomalies from 1880 to 2016',
+        text: 'Annual surface temperature change from 1961 to 2022',
         align: 'left'
     },
     accessibility: {
         description: `
-            A chart of monthly mean temperature anomalies from 1880 to 2016.
+            A chart of annual surface temperature change from 1961 to 2022.
             It employs two series, one column and one line,
             to convey the same data at different resolutions.
             There is only one column per thirty years,
@@ -14,20 +16,18 @@ Highcharts.chart('container', {
     },
     series: [{
         type: 'column',
-        name: 'Average per thirty years',
+        name: 'Mean per thirty years',
         data: JSON.parse(document.getElementById('columnData').textContent),
         pointRange: 30 * 365 * 24 * 36e5,
-        groupPadding: 0,
-        pointPadding: 0,
-        pointPlacement: -0.5,
+
         tooltip: {
-            headerFormat: '<span>{point.x:%Y} ' +
-                '{(subtract point.x (multiply 30 ' +
-                '(multiply 60 (multiply 1000' +
-                '(multiply 60 (multiply 365.25 24)))))):%Y}</span>'
+            // Subtract 30 years from the year to get the start of the period
+            headerFormat: (
+                '<span>{(subtract point.x 946684800000):%Y} - {point.x:%Y}<br>'
+            )
         }
     }, {
-        name: 'Monthly average',
+        name: 'Yearly mean',
         data: JSON.parse(document.getElementById('lineData').textContent),
         description: `
             A data series illustrating the same data as the first,
@@ -45,7 +45,7 @@ Highcharts.chart('container', {
         labels: {
             format: '{value:.1f}°C'
         },
-        name: 'Temperature anomalies',
+        name: 'Temperature change',
         title: {
             enabled: false
         }
