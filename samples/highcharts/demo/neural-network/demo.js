1b72097bd6ac69f7674c6da6040698047281cb9a
jszuminski <jakubszuminskixrp@gmail.com>
2024-06-03 09:05:00 +0200

    Neural network demo: minor corrections, docs, comments.

--- samples/highcharts/demo/neural-network/demo.js
+++ samples/highcharts/demo/neural-network/demo.js
@@ -22,14 +22,20 @@ const layers = [{
     label: 'Output Layer (sigmoid)'
 }];
 
+// Generates series for a neural network based on the defined layers.
 function generateData() {
+    // If there are no layers defined, we have no neural network to visualize
     if (layers.length === 0) {
         return [];
     }
 
     const data = [];
 
+    // Recursive function to generate all possible connections of nodes
+    // for each layer in then network
     function generate(currentIndices) {
+        // Base case: If the current indices length matches the number of
+        // layers, store the combination in the data array.
         if (currentIndices.length === layers.length) {
             data.push({
                 data: [...currentIndices]
@@ -37,8 +43,14 @@ function generateData() {
             return;
         }
 
+        // Get the current dimension index based on the length of
+        // current indices.
         const dimensionIndex = currentIndices.length;
+
+        // Iterate through all nodes in the current layer (dimensionIndex).
         for (let i = 0; i < layers[dimensionIndex].nodes; i++) {
+            // Recursively call generate with the new node index added to
+            // the current indices.
             generate([...currentIndices, i]);
         }
     }
