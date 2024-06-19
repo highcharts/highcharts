00f10744d4cb5f82576fbe21ee6c507ee0d3de6c
Tord <tord@Tords-MacBook-Pro.local>
2024-06-19 10:20:22 +0200

    First draft incomplete.

---
+++ samples/highcharts/demo/line-chart-race/demo.js
@@ -0,0 +1,120 @@
+Highcharts.chart('container', {
+    data: {
+        csvURL: 'https://cdn.jsdelivr.net/gh/highcharts/highcharts@0dbb5d2/samples/data/race_data.csv',
+        beforeParse: function (csv) {
+            return csv.replace(/\n\n/g, '\n');
+        }
+    },
+    chart: {
+        marginRight: 150
+        // events: {
+        //    load: loadfunc()
+        // }
+    },
+    title: {
+        text: 'Line chart race'
+    },
+    Axis: {
+        allowDecimals: false
+        // min: 100,
+        // max: 101
+    },
+    yAxis: {
+        title: {
+            text: 'Goals'
+        }
+        // max: 200
+    },
+    plotOptions: {
+        series: {
+            animation: false,
+
+            // With the series label pointIndex feature
+            label: {
+                enabled: false,
+                pointIndex: -1
+            },
+
+            // With the data labels keyPoints feature
+            dataLabels: {
+                align: 'left',
+                verticalAlign: 'middle',
+                allowOverlap: true,
+                enabled: true,
+                format: '{series.name}: {point.y}',
+                keyPoints: {
+                    last: true
+                },
+                overflow: 'allow',
+                crop: false
+            },
+            marker: {
+                enabled: false
+            }
+            // step: true
+        }
+    },
+    series: [{
+        name: 'Sebastian Vettel'
+    }, {
+        name: 'Fernando Alonso'
+    }, {
+        name: 'Kimi Räikkönen'
+    }, {
+        name: 'Lewis Hamilton'
+    }, {
+        name: 'Jenson Button'
+    }]
+});
+function loadfunc() {
+    for (let j = 0; j < 5; j++) {
+        Highcharts.charts[0].series[j].data.forEach(point => {
+            point.update({
+                visible: false
+            });
+        });
+    }
+
+}
+
+let i = 0;
+let n = 0;
+
+const increment = series => {
+    const last = series.points.at(n - 1);
+
+    series.points.at(n).update({
+        visible: true
+    });
+    if (last) {
+        last.update({
+            dataLabels: {
+                enabled: false
+            },
+            marker: {
+                enabled: false
+            }
+        }, false);
+    }
+    console.log(n);
+};
+
+
+const timer = setInterval(() => {
+
+    if (i === 0) {
+        loadfunc();
+    }
+
+    const chart = Highcharts.charts[0];
+
+    for (let j = 0; j < 5; j++) {
+        increment(chart.series[j]);
+    }
+    chart.redraw(false);
+    n++;
+
+    if (i++ > chart.series[0].data.length - 2) {
+        clearInterval(timer);
+    }
+}, 1000);
