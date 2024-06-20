d072532d951c1edaf24f1b3f64f8ed3b59de6a45
Markus Knutson Barstad <markusknutsonbarstad@Markus-sin-MacBook-Pro.local>
2024-06-20 15:01:46 +0200

    Added fix for inverted bubble halos

--- ts/Series/Bubble/BubblePoint.ts
+++ ts/Series/Bubble/BubblePoint.ts
@@ -63,10 +63,27 @@ class BubblePoint extends ScatterPoint {
      * @private
      */
     public haloPath(size: number): SVGPath {
+        const computedSize = size === 0 ?
+            0 :
+            (this.marker ? this.marker.radius || 0 : 0) +
+            size;
+
+        if (this.series.chart.inverted) {
+            const pos = this.pos() || [0, 0],
+                { xAxis, yAxis, chart } = this.series;
+
+            return chart.renderer.symbols.circle(
+                xAxis.len - pos[1] - computedSize,
+                yAxis.len - pos[0] - computedSize,
+                computedSize * 2,
+                computedSize * 2
+            );
+        }
+
         return Point.prototype.haloPath.call(
             this,
             // #6067
-            size === 0 ? 0 : (this.marker ? this.marker.radius || 0 : 0) + size
+            computedSize
         );
     }
 
