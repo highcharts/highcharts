9cb2eb40780bf1a4e461e17d7fb87a04ec9d362b
Dawid Draguła <dawid.dragula@gmail.com>
2024-06-20 15:48:43 +0200

    Added strict row heights datagrid to the benchmark sample.

--- samples/data-grid/v2/benchmark/demo.js
+++ samples/data-grid/v2/benchmark/demo.js
@@ -1,39 +1,59 @@
 const oldDatagridTimer = document.querySelector('#render-old'),
-    newDatagridTimer = document.querySelector('#render-new');
+    newDatagridTimerAH = document.querySelector('#render-new-ah'),
+    newDatagridTimerSH = document.querySelector('#render-new-sh');
 
 const dataTable = new DataGrid.DataTable({
     columns: {
         a: Array.from({ length: 10e4 }, (_, i) => `A${i}`),
         b: Array.from({ length: 10e4 }, (_, i) => `B${i}`),
-        c: Array.from({ length: 10e4 }, (_, i) => `C${i}`),
+        c: Array.from({ length: 10e4 }, (_, i) => `C${i} - longer text`),
         d: Array.from({ length: 10e4 }, (_, i) => `D${i}`),
         e: Array.from({ length: 10e4 }, (_, i) => `E${i}`),
         f: Array.from({ length: 10e4 }, (_, i) => `F${i}`)
     }
 });
 
-const startOldTime = new Date().getTime();
-var t0 = performance.now();
+let startTime = new Date().getTime();
+let startPerfo = performance.now();
 // eslint-disable-next-line
 const grid = new DataGrid.DataGrid('datagrid-old', {
     dataTable: dataTable
 });
-var t1 = performance.now();
-const endOldTime = new Date().getTime();
+let endPerfo = performance.now();
+let endTime = new Date().getTime();
 oldDatagridTimer.innerHTML =
-    'Time: ' + (endOldTime - startOldTime) + ' Perf: ' + (t1 - t0).toFixed(2);
+    'Time: ' + (endTime - startTime) +
+    ' Perf: ' + (endPerfo - startPerfo).toFixed(2);
 
-// NEW
-const startNewTime = new Date().getTime();
-var t2 = performance.now();
+// NEW (autoheight)
+startTime = new Date().getTime();
+startPerfo = performance.now();
 // eslint-disable-next-line no-unused-vars
-const newgrid = new DataGrid.DataGrid2('container', {
+const newGridAH = new DataGrid.DataGrid2('container-ah', {
     table: dataTable,
     settings: {
         rowBufferSize: 5
     }
 });
-var t3 = performance.now();
-const endNewTime = new Date().getTime();
-newDatagridTimer.innerHTML =
-    'Time: ' + (endNewTime - startNewTime) + ' Perf: ' + (t3 - t2).toFixed(2);
+endPerfo = performance.now();
+endTime = new Date().getTime();
+newDatagridTimerAH.innerHTML =
+    'Time: ' + (endTime - startTime) +
+    ' Perf: ' + (endPerfo - startPerfo).toFixed(2);
+
+// NEW (strictheight)
+startTime = new Date().getTime();
+startPerfo = performance.now();
+// eslint-disable-next-line no-unused-vars
+const newGridSH = new DataGrid.DataGrid2('container-sh', {
+    table: dataTable,
+    settings: {
+        rowBufferSize: 5,
+        strictRowHeights: true
+    }
+});
+endPerfo = performance.now();
+endTime = new Date().getTime();
+newDatagridTimerSH.innerHTML =
+    'Time: ' + (endTime - startTime) +
+    ' Perf: ' + (endPerfo - startPerfo).toFixed(2);
