5865d2578a3d95b828edff7c6fb3669d68281a97
Tord <tord@Tords-MacBook-Pro.local>
2024-06-25 09:03:06 +0200

    Updated dataset.

--- samples/highcharts/demo/arearange/demo.js
+++ samples/highcharts/demo/arearange/demo.js
@@ -1,57 +1,65 @@
-(async () => {
-    const data = await fetch(
-        'https://www.highcharts.com/samples/data/range.json'
-    ).then(response => response.json());
+// https://veret.gfi.uib.no/
 
-    Highcharts.chart('container', {
-        chart: {
-            type: 'arearange',
-            zooming: {
-                type: 'x'
-            },
-            scrollablePlotArea: {
-                minWidth: 600,
-                scrollPositionX: 1
-            }
+Highcharts.chart('container', {
+    data: {
+        csvURL: 'https://cdn.jsdelivr.net/gh/highcharts/highcharts@31c9ab8/samples/data/TempFloridaBergen2023.csv',
+        beforeParse: function (csv) {
+            return csv.replace(/\n\n/g, '\n');
+        }
+    },
+    chart: {
+        type: 'arearange',
+        zooming: {
+            type: 'x'
         },
+        scrollablePlotArea: {
+            minWidth: 600,
+            scrollPositionX: 1
+        }
+    },
+    title: {
+        text: 'Temperature variation by day',
+        align: 'left'
+    },
+    subtitle: {
+        text: 'Source: ' +
+            '<a href="https://veret.gfi.uib.no/"' +
+            'target="_blank">Universitetet i Bergen</a>',
+        align: 'left'
+    },
+    xAxis: {
+        type: 'datetime',
+        accessibility: {
+            rangeDescription: 'Range: Jan 1st 2023 to Jan 1st 2024.'
+        }
+    },
+    yAxis: {
         title: {
-            text: 'Temperature variation by day'
-        },
-        xAxis: {
-            type: 'datetime',
-            accessibility: {
-                rangeDescription: 'Range: Jan 1st 2017 to Dec 31 2017.'
-            }
-        },
-        yAxis: {
-            title: {
-                text: null
-            }
-        },
-        tooltip: {
-            crosshairs: true,
-            shared: true,
-            valueSuffix: '°C',
-            xDateFormat: '%A, %b %e'
-        },
-        legend: {
-            enabled: false
-        },
-        series: [{
-            name: 'Temperatures',
-            data: data,
-            color: {
-                linearGradient: {
-                    x1: 0,
-                    x2: 0,
-                    y1: 0,
-                    y2: 1
-                },
-                stops: [
-                    [0, '#ff0000'],
-                    [1, '#0000ff']
-                ]
-            }
-        }]
-    });
-})();
+            text: null
+        }
+    },
+    tooltip: {
+        crosshairs: true,
+        shared: true,
+        valueSuffix: '°C',
+        xDateFormat: '%A, %b %e'
+    },
+    legend: {
+        enabled: false
+    },
+    series: [{
+        name: 'Temperatures',
+        color: {
+            linearGradient: {
+                x1: 0,
+                x2: 0,
+                y1: 0,
+                y2: 1
+            },
+            stops: [
+                [0, '#ff0000'],
+                [1, '#0000ff']
+            ]
+        }
+    }]
+});
