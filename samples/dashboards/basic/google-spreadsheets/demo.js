613bc5c7b51b1e1dc52ca20eba0cd6cb3be02e6a
jomar-honsi <jomar.honsi@highsoft.com>
2024-06-25 17:13:12 +0200

    Prepared demo.

--- samples/dashboards/basic/google-spreadsheets/demo.js
+++ samples/dashboards/basic/google-spreadsheets/demo.js
@@ -1,14 +1,34 @@
-// Create the chart
-Highcharts.chart('container', {
-
-    title: {
-        text: 'Highcharts data from Google Spreadsheets, beforeParse'
+Dashboards.board('container', {
+    dataPool: {
+        connectors: [{
+            id: 'gs-conn',
+            type: 'GoogleSheets',
+            options: {
+                googleAPIKey: 'AIzaSyCQ0Jh8OFRShXam8adBbBcctlbeeA-qJOk',
+                // eslint-disable-next-line max-len
+                googleSpreadsheetKey: '1U17c4GljMWpgk1bcTvUzIuWT8vdOnlCBHTm5S8Jh8tw',
+                firstRowAsNames: true
+            },
+            beforeParse: data => {
+                data.forEach(row => {
+                    // row.age = row.age + 10;
+                    console.log(row);
+                });
+                return data;
+            }
+        }]
     },
-
-    data: {
-        googleAPIKey: 'AIzaSyCQ0Jh8OFRShXam8adBbBcctlbeeA-qJOk',
-        googleSpreadsheetKey: '1U17c4GljMWpgk1bcTvUzIuWT8vdOnlCBHTm5S8Jh8tw',
-        error: console.error
-    }
-
+    components: [
+        {
+            renderTo: 'dashboard-col-0',
+            connector: {
+                id: 'gs-conn'
+            },
+            type: 'DataGrid',
+            title: 'Google Sheets Data',
+            dataGridOptions: {
+                editable: false
+            }
+        }
+    ]
 });
