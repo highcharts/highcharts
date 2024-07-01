01dda2245e744a1b91c202c79362b68eb0f08144
Tord <tord@Tords-MacBook-Pro.local>
2024-07-01 09:45:37 +0200

    Changed dataset.

--- samples/highcharts/demo/bar-stacked/demo.js
+++ samples/highcharts/demo/bar-stacked/demo.js
@@ -1,20 +1,20 @@
-// Data retrieved from: https://www.uefa.com/uefachampionsleague/history/
+// Data retrieved from: https://maxf1.net/en/results/teams/
 Highcharts.chart('container', {
     chart: {
         type: 'bar'
     },
     title: {
-        text: 'UEFA CL top scorers by season'
+        text: 'F1 top constructors by season'
     },
     xAxis: {
         categories: [
-            '2023/24', '2022/23', '2021/22', '2020/21', '2019/20', '2018/19'
+            '2023', '2022', '2021', '2020', '2019', '2018'
         ]
     },
     yAxis: {
         min: 0,
         title: {
-            text: 'Goals'
+            text: 'Points'
         }
     },
     legend: {
@@ -29,13 +29,13 @@ Highcharts.chart('container', {
         }
     },
     series: [{
-        name: 'Cristiano Ronaldo',
-        data: [null, null, 6, 4, 4, 6]
+        name: 'Red Bull',
+        data: [860, 759, 585.5, 319, 417, 419]
     }, {
-        name: 'Lionel Messi',
-        data: [null, 4, 5, 5, 3, 12]
+        name: 'Mercedes',
+        data: [409, 554, 613.5, 573, 739, 655]
     }, {
-        name: 'Robert Lewandowski',
-        data: [3, 5, 13, 5, 15, 8]
+        name: 'Ferrari',
+        data: [406, 515, 323.5, 131, 504, 571]
     }]
 });
