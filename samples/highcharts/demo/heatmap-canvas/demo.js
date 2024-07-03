f21d4b2ef9bf5d0ce442549798ac9edd055ea3f6
Tord <tord@Tords-MacBook-Pro.local>
2024-07-03 08:38:58 +0200

    Dataset update.

--- samples/highcharts/demo/heatmap-canvas/demo.js
+++ samples/highcharts/demo/heatmap-canvas/demo.js
@@ -19,15 +19,15 @@ Highcharts.chart('container', {
     },
 
     subtitle: {
-        text: 'Temperature variation by day and hour through 2017',
+        text: 'Temperature variation by day and hour through 2023',
         align: 'left',
         x: 40
     },
 
     xAxis: {
         type: 'datetime',
-        min: Date.UTC(2017, 0, 1),
-        max: Date.UTC(2017, 11, 31, 23, 59, 59),
+        min: Date.UTC(2023, 0, 1),
+        max: Date.UTC(2023, 11, 31, 23, 59, 59),
         labels: {
             align: 'left',
             x: 5,
