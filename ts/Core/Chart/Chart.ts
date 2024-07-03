557968eb67f57b42a97902bbfd6febec411e0554
hubertkozik <hubertkozik13@gmail.com>
2024-07-03 17:40:39 +0200

    Commit to run CI.

--- ts/Core/Chart/Chart.ts
+++ ts/Core/Chart/Chart.ts
@@ -3646,6 +3646,7 @@ class Chart {
         // Remove active points for shared tooltip
         this.hoverPoints?.forEach((point): void => point.setState());
 
+
         for (const axis of axes) {
             const {
                     horiz,
