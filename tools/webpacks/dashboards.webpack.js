0a6f5346a8f984b1fdac74dfe55cfdb6a6fcd6bd
karolkolodziej <karolkolodziej01@gmail.com>
2024-07-05 09:45:39 +0200

    Initial dashboards webpack setup.

--- tools/webpacks/dashboards.webpack.js
+++ tools/webpacks/dashboards.webpack.js
@@ -4,28 +4,44 @@ const path = require('path');
 const projectPath = path.resolve(__dirname, '..', '..');
 
 module.exports = {
+    devtool: 'inline-source-map',
     entry: path.resolve(projectPath, 'ts', 'masters-dashboards', 'dashboards.src.ts'),
     mode: 'development',
     resolve: {
-        extensions: ['.ts', '.js']
+        extensions: ['.ts', '.js'],
+        extensionAlias: {
+            '.js': ['.ts', '.js'],
+        },
     },
     module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
+        rules: [{
+            test: /\.ts$/,
+            use: [{
+                loader: 'ts-loader',
+                options: {
+                    transpileOnly: true
+                }
+            }]
+        }]
+    },
+    optimization: {
+        usedExports: true,
+        minimize: false
     },
     output: {
         chunkFormat: 'module',
         filename: 'dashboards.src.js',
-        path: path.resolve(projectPath, 'code'),
+        path: path.resolve(projectPath, 'code', 'dashboards'),
         uniqueName: 'dashboards',
         library: {
-            name: 'Dashboards',
-            type: 'umd'
+            export: 'default',
+            name: {
+                amd: `highcharts/dashboards`,
+                commonjs: `highcharts/dashboards`,
+                root: 'Highcharts.Dashboards'
+            },
+            type: 'umd',
+            umdNamedDefine: true
         }
     }
 };
