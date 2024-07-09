0537991e15aa78865ed8488f23f6d49fae5238fc
jomar-honsi <jomar.honsi@highsoft.com>
2024-07-09 17:30:07 +0200

    Fixed lint issue.

--- ts/Dashboards/SerializeHelper/DataTableHelper.ts
+++ ts/Dashboards/SerializeHelper/DataTableHelper.ts
@@ -21,6 +21,7 @@
 
 import type DataTableOptions from '../../Data/DataTableOptions';
 import type Globals from '../Globals';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import type JSON from '../JSON';
 
 import DataTable from '../../Data/DataTable.js';
