db1183e8cbfa48b174c49f6299b8d24f9708bc52
bre1470 <40056287+bre1470@users.noreply.github.com>
2024-07-12 15:38:53 +0200

    Tools: Added API database mockup.

---
+++ tools/api-docs/database.ts
@@ -0,0 +1,320 @@
+/* *
+ *
+ *  API Database API Mockup
+ *
+ *  (c) Highsoft AS
+ *
+ *  Authors:
+ *  - Sophie Bremer
+ *
+ * */
+
+
+/* *
+ *
+ *  Imports
+ *
+ * */
+
+
+import FS from 'node:fs/promises';
+import FSLib from '../libs/fs';
+
+
+/* *
+ *
+ *  Declarations
+ *
+ * */
+
+
+interface Data {
+    description: Array<string>;
+    doclet: Array<string>;
+    meta: Array<string>;
+    name: Array<string>;
+}
+
+
+/* *
+ *
+ *  Constant
+ *
+ * */
+
+
+const DATABASE_NAME = 'tree-database';
+
+
+const KEY_ANTI_PATTERN = /[^\w\- ]+/gsu;
+
+
+const SPACE_PATTERN = /\s+/gsu;
+
+
+const VALUE_ANTI_PATTERN = /[^\w\-\n\r\\\/\[\] .:,;?!=+*(){}<>'"`^#$%&@]+/gsu;
+
+
+/* *
+ *
+ *  Functions
+ *
+ * */
+
+
+function sanitizeKey(
+    key: string
+): string {
+
+    while (KEY_ANTI_PATTERN.test(key)) {
+        key = key.replace(KEY_ANTI_PATTERN, ' ');
+    }
+
+    return key.replace(SPACE_PATTERN, ' ');
+}
+
+
+function sanitizeValue(
+    value: string
+): string {
+
+    while (VALUE_ANTI_PATTERN.test(value)) {
+        value = value.replace(VALUE_ANTI_PATTERN, '');
+    }
+
+    return value;
+}
+
+
+/* *
+ *
+ *  Class
+ *
+ * */
+
+
+export class APIDatabase {
+
+
+    /* *
+     *
+     *  Constructor
+     *
+     * */
+
+
+    public constructor(
+        product: string,
+        storageFolder: string = process.cwd()
+    ) {
+        this.cache = {};
+        this.namePrefix = `${product}.`;
+        this.product = product;
+        this.storageFolder = storageFolder;
+    }
+
+
+    /* *
+     *
+     *  Properties
+     *
+     * */
+
+
+    private readonly cache: Record<string, Data>;
+
+
+    private readonly namePrefix: string;
+
+
+    public readonly product: string;
+
+
+    private readonly storageFolder: string;
+
+
+    /* *
+     *
+     *  Functions
+     *
+     * */
+
+
+    private getFilePath(
+        name: string
+    ) {
+        return FSLib.path([this.storageFolder, sanitizeKey(name) + '.json']);
+    }
+
+
+    public async getNode(
+        nodeName: string
+    ): Promise<(APIDatabase.Node|undefined)> {
+        const data = await this.getTreeData();
+        const nodePath = this.namePrefix + nodeName;
+
+        let index = data.name.indexOf(nodePath);
+
+        if (index === -1) {
+            return void 0;
+        }
+
+        return {
+            name: nodeName,
+            description: data.description[index],
+            doclet: JSON.parse(data.doclet[index]) as APIDatabase.Doclet,
+            meta: JSON.parse(data.meta[index]) as APIDatabase.Meta
+        };
+    }
+
+
+    public async getNodeChildren(
+        nodeName: string
+    ): Promise<Array<APIDatabase.Node>> {
+        const children: Array<APIDatabase.Node> = [];
+        const data = await this.getTreeData();
+        const nodePath = this.namePrefix + nodeName;
+
+        let indexOffset = nodePath.length;
+
+        let index = -1;
+
+        for (const name of data.name) {
+
+            ++index;
+
+            if (
+                !name.startsWith(nodePath) ||
+                name.indexOf('.', indexOffset) !== -1 // No direct child
+            ) {
+                continue;
+            }
+
+            children.push({
+                name: nodeName,
+                description: data.description[index],
+                doclet: JSON.parse(data.doclet[index]) as APIDatabase.Doclet,
+                meta: JSON.parse(data.meta[index]) as APIDatabase.Meta
+            });
+
+        }
+
+        return children;
+    }
+
+
+    private async getTreeData(): Promise<Data> {
+        const name = DATABASE_NAME;
+
+        let data = this.cache[name];
+
+        if (data) {
+            return data;
+        }
+
+        const filePath = this.getFilePath(name);
+
+        if (!FSLib.isFile(filePath)) {
+            await this.saveTreeData({
+                description: [],
+                doclet: [],
+                meta: [],
+                name: []
+            });
+        }
+
+        data = JSON.parse(await FS.readFile(filePath, 'utf8'));
+
+        this.cache[name] = data;
+
+        return data;
+    }
+
+
+    private async saveTreeData(
+        data: Data
+    ): Promise<Data> {
+        const name = DATABASE_NAME;
+
+        await FS.writeFile(
+            this.getFilePath(name),
+            JSON.stringify(data),
+            'utf8'
+        );
+
+        return data;
+    }
+
+
+    public async setNode(
+        node: APIDatabase.Node
+    ): Promise<APIDatabase.Node> {
+        const data = await this.getTreeData();
+        const nodePath = this.namePrefix + node.name;
+
+        let index = data.name.indexOf(nodePath);
+
+        if (index === -1) {
+            index = data.name.length;
+        }
+
+        data.doclet[index] = JSON.stringify(node.doclet);
+        data.description[index] = node.description;
+        data.meta[index] = JSON.stringify(node.meta);
+        data.name[index] = node.name;
+
+        await this.saveTreeData(data);
+
+        return node;
+    }
+
+
+}
+
+
+/* *
+ *
+ *  Class Namespace
+ *
+ * */
+
+
+export namespace APIDatabase {
+
+
+    /* *
+     *
+     *  Declarations
+     *
+     * */
+
+
+    export interface Doclet {
+        type: Array<string>;
+    }
+
+
+    export interface Meta {
+        file: string;
+    }
+
+
+    export interface Node {
+        name: string;
+        description: string;
+        doclet: Doclet;
+        meta: Meta;
+    }
+
+
+}
+
+
+/* *
+ *
+ *  Default Export
+ *
+ * */
+
+
+export default APIDatabase;
