65fdc014e4af414f578eff9d4987f168338e01dc
jomar-honsi <jomar.honsi@highsoft.com>
2024-07-19 16:43:06 +0200

    Added selection of sheet + modification method

--- samples/dashboards/googlesheets/tutorial/demo.js
+++ samples/dashboards/googlesheets/tutorial/demo.js
@@ -1,14 +1,25 @@
 const googleApiKey = 'AIzaSyCQ0Jh8OFRShXam8adBbBcctlbeeA-qJOk';
 const googleSpreadsheetKey = '1Z6vzR7EUZiqLBDZ66jf82rw5kvPBQHzmMLyY4miUVKA';
 
-Dashboards.board('container', {
+/*
+googleAPIKey: '',
+googleSpreadsheetKey: '',
+worksheet: 1,
+enablePolling: false,
+dataRefreshRate: 2,
+firstRowAsNames: true
+*/
+
+const board = Dashboards.board('container', {
     dataPool: {
         connectors: [{
             id: 'conn-orig',
             type: 'GoogleSheets',
             options: {
                 googleAPIKey: googleApiKey,
-                googleSpreadsheetKey: googleSpreadsheetKey
+                googleSpreadsheetKey: googleSpreadsheetKey,
+                enablePolling: true,
+                dataRefreshRate: 5
             }
         }, {
             id: 'conn-mod',
@@ -42,7 +53,7 @@ Dashboards.board('container', {
             id: 'conn-orig'
         },
         type: 'DataGrid',
-        title: 'Original sheet',
+        title: 'Original sheet 1',
         dataGridOptions: {
             editable: false
         }
@@ -52,9 +63,31 @@ Dashboards.board('container', {
             id: 'conn-mod'
         },
         type: 'DataGrid',
-        title: 'Modified sheet',
+        title: 'Modified sheet 1',
         dataGridOptions: {
             editable: false
         }
     }]
+});
+
+const modification = document.getElementById('modification');
+const sheet = document.getElementById('sheet');
+
+
+sheet.addEventListener('input', async e => {
+    const worksheet = Number(e.target.value) + 1;
+
+    const comp = board.getComponentByCellId('orig-sheet-cell');
+
+    await comp.update({
+        title: {
+            text: 'Original sheet ' + worksheet
+        },
+        connector: {
+            id: 'conn-orig',
+            options: {
+                worksheet: worksheet
+            }
+        }
+    });
 });
