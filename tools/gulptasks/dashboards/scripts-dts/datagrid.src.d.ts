86f447838767c40972c69a5bfc2d5511985bdf2a
karolkolodziej <karolkolodziej01@gmail.com>
2024-07-19 13:59:00 +0200

    Fixed DataGridOptions .d.ts files.

--- tools/gulptasks/dashboards/scripts-dts/datagrid.src.d.ts
+++ tools/gulptasks/dashboards/scripts-dts/datagrid.src.d.ts
@@ -10,6 +10,7 @@ export { default as DataGrid } from "./es-modules/DataGrid/DataGrid";
 export { default as DataGridColumn } from './es-modules/DataGrid/DataGridColumn';
 export { default as DataGridRow } from './es-modules/DataGrid/DataGridRow';
 export { default as DataGridCell } from './es-modules/DataGrid/DataGridCell';
+export { default as DataGridOptions } from './es-modules/DataGrid/DataGridOptions';
 
 export const win: typeof Globals.win;
 
