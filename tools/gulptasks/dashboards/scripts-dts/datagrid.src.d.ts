9874887a21f8b183e71129b568d700df0f5ae2c1
karolkolodziej <karolkolodziej01@gmail.com>
2024-07-19 13:44:37 +0200

    Fixed some .d.ts.

--- tools/gulptasks/dashboards/scripts-dts/datagrid.src.d.ts
+++ tools/gulptasks/dashboards/scripts-dts/datagrid.src.d.ts
@@ -7,6 +7,9 @@
 import Globals from "./es-modules/DataGrid/Globals";
 
 export { default as DataGrid } from "./es-modules/DataGrid/DataGrid";
+export { default as DataGridColumn } from './es-modules/DataGrid/DataGridColumn';
+export { default as DataGridRow } from './es-modules/DataGrid/DataGridRow';
+export { default as DataGridCell } from './es-modules/DataGrid/DataGridCell';
 
 export const win: typeof Globals.win;
 
