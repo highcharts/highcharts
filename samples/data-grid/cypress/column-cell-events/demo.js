043b43bd331cef87a60adadb3e03593429e5ecd8
Sebastian Bochan <sebastian@blacklabel.pl>
2024-07-22 13:26:05 +0200

    Created cypress test for events.

---
+++ samples/data-grid/cypress/column-cell-events/demo.js
@@ -0,0 +1,46 @@
+const doc = document;
+
+DataGrid.dataGrid('container', {
+    table: {
+        columns: {
+            product: ['Apples', 'Pears', 'Plums', 'Bananas'],
+            weight: [100, 40, 0.5, 200],
+            price: [1.5, 2.53, 5, 4.5],
+            metaData: ['a', 'b', 'c', 'd']
+        }
+    },
+    defaults: {
+        columns: {
+            editable: true,
+            sorting: true
+        }
+    },
+    columns: {
+        product: {
+            editable: true
+        },
+        weight: {
+            events: {
+                afterSorting: function () {
+                    doc.getElementById('columnSorting').value = 'afterSorting';
+                }
+            }
+        }
+    },
+    events: {
+        cell: {
+            click: function () {
+                doc.getElementById('cellClick').value = 'cellClick';
+            },
+            mouseOver: function () {
+                doc.getElementById('cellMouseOver').value = 'cellMouseOver';
+            },
+            mouseOut: function () {
+                doc.getElementById('cellMouseOut').value = 'cellMouseOut';
+            },
+            afterEdit: function () {
+                doc.getElementById('cellAfterEdit').value = 'cellAfterEdit';
+            }
+        }
+    }
+});
---
+++ test/cypress/data-grid/integration/options/events.cy.js
@@ -0,0 +1,34 @@
+describe('Exporting to JSON.', () => {
+    before(() => {
+        cy.visit('data-grid/cypress/column-cell-events');
+    });
+
+    it.skip('Cell mouseOver / mouseOut event.', () => {
+        cy.get('.highcharts-datagrid-row[data-row-index="1"] > td[data-column-id="product"]')
+            .trigger('mouseover');
+        cy.get('#cellMouseOver').should('have.value', 'cellMouseOver');
+
+        cy.get('.highcharts-datagrid-row[data-row-index="1"] > td[data-column-id="product"]')
+            .click()
+            .trigger('mouseout');
+        cy.get('#cellMouseOut').should('have.value', 'cellMouseOut');
+    });
+
+    it.skip('Cell click event', () => {
+        cy.get('.highcharts-datagrid-row[data-row-index="1"] > td[data-column-id="product"]').click({force: true});
+        cy.get('#cellClick').should('have.value', 'cellClick');
+    });
+
+    it('Cell afterEdit event', () => {
+        cy.get('.highcharts-datagrid-row[data-row-index="1"] > td[data-column-id="product"]')
+            .click({force: true})
+            .clear()
+            .type('Strawberries');
+        cy.get('#cellAfterEdit').should('have.value', 'cellAfterEdit');
+    });
+
+    it('After sorting event.', () => {
+        cy.get('th[data-column-id="product"]').click();
+        cy.get('#columnSorting').should('have.value', 'afterSorting');
+    });
+});
