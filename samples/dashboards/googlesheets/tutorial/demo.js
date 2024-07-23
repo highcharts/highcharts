245b464eb1d6584c3d8fb6df0fd68c8690a847ed
jomar-honsi <jomar.honsi@highsoft.com>
2024-07-23 16:32:36 +0200

    Split selection of data modifiers and beforeParse.

--- samples/dashboards/googlesheets/tutorial/demo.js
+++ samples/dashboards/googlesheets/tutorial/demo.js
@@ -1,7 +1,6 @@
 const googleApiKey = 'AIzaSyCQ0Jh8OFRShXam8adBbBcctlbeeA-qJOk';
 const googleSpreadsheetKey = '1Z6vzR7EUZiqLBDZ66jf82rw5kvPBQHzmMLyY4miUVKA';
 
-
 /* Helper functions */
 function sheetTitle(n = 1) {
     return {
@@ -11,26 +10,32 @@ function sheetTitle(n = 1) {
 }
 
 /* Modification implementations */
+function bfpVoid(data) {
+    return data;
+}
+
 function bfpModifyHeader(data) {
-    data.forEach(row => {
-        row[0] += '*';
+    data.forEach(col => {
+        col[0] = col[0].toUpperCase();
     });
-    // Special label from first header item
-    data[0][0] = '-';
+    // Special header for the first column
+    data[0][0] = '*';
 
     return data;
 }
 
 function bfpModifyData(data) {
-    // Derive everyone of their oranges
-    data[1][2] = 0;
-    data[2][2] = 0;
-    data[3][2] = 0;
+    // Deprive everyone of their oranges
+    for (let i = 1; i < data.length; i++) {
+        data[i][2] = 0;
+    }
 
     return data;
 }
 
-let modSelection = 0;
+// Function to perform in 'beforeParse' of the modified sheet connector
+let beforeParseSelector = 0;
+const beforeParseFunction = [bfpVoid, bfpModifyHeader, bfpModifyData];
 
 
 /* Create the Dashboard */
@@ -49,16 +54,8 @@ const board = Dashboards.board('container', {
             options: {
                 googleAPIKey: googleApiKey,
                 googleSpreadsheetKey: googleSpreadsheetKey,
-                beforeParse: data => {
-                    switch (modSelection) {
-                    case 1:
-                        return bfpModifyHeader(data);
-                    case 2:
-                        return bfpModifyData(data);
-                    default:
-                        return data;
-                    }
-                }
+                beforeParse: data =>
+                    beforeParseFunction[beforeParseSelector](data)
             }
         }]
     },
@@ -85,19 +82,22 @@ const board = Dashboards.board('container', {
     }]
 });
 
-const modification = document.getElementById('modification');
-const sheet = document.getElementById('sheet');
 
-sheet.addEventListener('input', async event => {
-    console.log('Sheet event:', event);
+//
+// Worksheet selection processing
+//
+const worksheetSelect = document.getElementById('worksheet-select');
+const beforeParseSelect = document.getElementById('before-parse-select');
+const datamodifierSelect = document.getElementById('datamodifier-select');
 
+worksheetSelect.addEventListener('input', async e => {
     // The sheet selection has changed
-    const worksheet = Number(event.target.value) + 1;
+    const worksheet = Number(e.target.value) + 1;
 
     // Update the original sheet component
-    let conn = board.dataPool.connectors['conn-orig'];
-    conn.options.worksheet = worksheet;
-    await conn.load();
+    let connector = board.dataPool.connectors['conn-orig'];
+    connector.options.worksheet = worksheet;
+    await connector.load();
 
     let comp = board.getComponentByCellId('orig-sheet-cell');
     const title = sheetTitle(worksheet);
@@ -108,9 +108,9 @@ sheet.addEventListener('input', async event => {
     });
 
     // Update the modified sheet component
-    conn = board.dataPool.connectors['conn-mod'];
-    conn.options.worksheet = worksheet;
-    await conn.load();
+    connector = board.dataPool.connectors['conn-mod'];
+    connector.options.worksheet = worksheet;
+    await connector.load();
 
     comp = board.getComponentByCellId('mod-sheet-cell');
     await comp.update({
@@ -120,17 +120,69 @@ sheet.addEventListener('input', async event => {
     });
 });
 
-const bfpExecuter = [null, bfpModifyHeader, bfpModifyData];
 
-modification.addEventListener('input', async event => {
-    console.log('Mod event:', event);
-
-    const i = Number(event.target.value);
-    const conn = board.dataPool.connectors['conn-mod'];
-
-    if (i < bfpExecuter.length) {
-        modSelection = i;
-        // Update the modified sheet component
-        await conn.load();
+//
+// 'beforeParse' processing
+//
+beforeParseSelect.addEventListener('input', async e => {
+    const value = e.target.value;
+    if (value >= beforeParseFunction.length) {
+        return;
     }
+
+    // Used in the 'beforeParse' callback of the modified sheet connector
+    beforeParseSelector = value;
+
+    // Update the modified sheet component
+    await board.dataPool.connectors['conn-mod'].load();
+
+    // Re-apply data modifier
+    await applyDataModifier(datamodifierSelect.value);
 });
+
+
+//
+// Data modifier processing
+//
+const MathModifier = Dashboards.DataModifier.types.Math;
+const SortModifier = Dashboards.DataModifier.types.Sort;
+const RangeModifier = Dashboards.DataModifier.types.Range;
+
+const mathModifier = new MathModifier({
+    type: 'Math',
+    columnFormulas: [{
+        column: 'Sum',
+        formula: 'B1+C1+D1'
+    }]
+});
+
+const sortModifier = new SortModifier({
+    type: 'Sort',
+    orderByColumn: 'Jane',
+    order: 'asc'
+});
+
+const rangeModifier = new RangeModifier({
+    type: 'Range',
+    ranges: [{
+        column: 'John',
+        minValue: 4,
+        maxValue: 7
+    }]
+});
+
+const dataModifiers = [null, mathModifier, sortModifier, rangeModifier];
+
+datamodifierSelect.addEventListener('input', async e => {
+    const value = e.target.value;
+    if (value >= dataModifiers.length) {
+        return;
+    }
+    await applyDataModifier(value);
+});
+
+
+async function applyDataModifier(value) {
+    const connector = board.dataPool.connectors['conn-mod'];
+    await connector.table.setModifier(dataModifiers[value]);
+}
