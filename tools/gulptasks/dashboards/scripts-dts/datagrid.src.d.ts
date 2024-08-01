2ce7137d9a4dc894eb2dc40a190fd01973de234c
karolkolodziej <karolkolodziej01@gmail.com>
2024-08-01 09:08:17 +0200

    Added DataConverter to the Dashboards and DataGrid namespace.

--- tools/gulptasks/dashboards/scripts-dts/datagrid.src.d.ts
+++ tools/gulptasks/dashboards/scripts-dts/datagrid.src.d.ts
@@ -8,6 +8,13 @@ import Globals from "./es-modules/DataGrid/Globals";
 
 export { default as DataGrid } from "./es-modules/DataGrid/DataGrid";
 
+export { default as DataConnector } from "./es-modules/Data/Connectors/DataConnector";
+export { default as DataConverter } from "./es-modules/Data/Converters/DataConverter";
+export { default as DataCursor } from "./es-modules/Data/DataCursor";
+export { default as DataModifier } from "./es-modules/Data/Modifiers/DataModifier";
+export { default as DataPool } from "./es-modules/Data/DataPool";
+export { default as DataTable } from "./es-modules/Data/DataTable";
+
 export const win: typeof Globals.win;
 
 export as namespace DataGrid;
