15fea61504da824ec0aa420ab288e857e9690e51
karolkolodziej <karolkolodziej01@gmail.com>
2024-08-01 10:46:12 +0200

    Made DataEvent singular in the import.

--- tools/gulptasks/dashboards/scripts-dts/dashboards.src.d.ts
+++ tools/gulptasks/dashboards/scripts-dts/dashboards.src.d.ts
@@ -29,7 +29,7 @@ export { default as DataGridPlugin } from "./es-modules/Dashboards/Plugins/DataG
 export { default as DataConnector } from "./es-modules/Data/Connectors/DataConnector";
 export { default as DataConverter } from "./es-modules/Data/Converters/DataConverter";
 export { default as DataCursor } from "./es-modules/Data/DataCursor";
-export { default as DataEvents } from "./es-modules/Data/DataEvent";
+export { default as DataEvent } from "./es-modules/Data/DataEvent";
 export { default as DataModifier } from "./es-modules/Data/Modifiers/DataModifier";
 export { default as DataPool } from "./es-modules/Data/DataPool";
 export { default as DataTable } from "./es-modules/Data/DataTable";
