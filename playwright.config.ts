126baf5f9c995b99935d37e255fcae77a3bfe270
Pawel Lysy <pawel.lysy@blacklabel.pl>
2024-08-20 14:09:03 +0200

    Test.

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,16 +39,6 @@ export default defineConfig({
             use: { ...devices["Desktop Chrome"] },
         },
 
-
-        /* Test against branded browsers. */
-        // {
-        //   name: 'Microsoft Edge',
-        //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-        // },
-        // {
-        //   name: 'Google Chrome',
-        //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-        // },
     ],
 
     /* Run your local dev server before starting the tests */
