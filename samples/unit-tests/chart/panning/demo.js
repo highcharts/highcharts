a0dd0346df86350a02d1ad0eb84100b1d06ca6e4
Mateusz Bernacik <mat.bernacik@gmail.com>
2024-08-22 17:54:08 +0200

    Added test.

--- samples/unit-tests/chart/panning/demo.js
+++ samples/unit-tests/chart/panning/demo.js
@@ -726,3 +726,43 @@ QUnit.test(
         assert.strictEqual(actualMin, -75, 'Min must be -75; not panning');
     }
 );
+
+QUnit.test('Column chart panning.', function (assert) {
+    const data = [];
+
+    for (let i = 0; i < 100000; i++) {
+        data.push([400 + i, 1]);
+    }
+
+    const chart = Highcharts.stockChart('container', {
+        chart: {
+            events: {
+                load: function () {
+                    this.xAxis[0].setExtremes(2000, 45000);
+                }
+            }
+        },
+
+        series: [{
+            type: 'column',
+            data: data,
+            dataGrouping: {
+                units: [[
+                    'second',
+                    [1]
+                ]]
+            }
+        }]
+    });
+
+    const controller = new TestController(chart);
+
+    controller.pan([100, 200], [200, 200]);
+
+    assert.equal(
+        chart.xAxis[0].min,
+        0,
+        `It should be possible to pan to the axis minimum in a data grouped
+        ordinal column chart, #21524.`
+    );
+});
