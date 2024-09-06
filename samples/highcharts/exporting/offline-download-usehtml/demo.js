93ca5f8d4ee644a3c182c1917a17f272869c4d56
Markus Knutson Barstad <markusknutsonbarstad@Markus-sin-MacBook-Pro.local>
2024-09-06 10:56:04 +0200

    Readded demo i removed by mistake

---
+++ samples/highcharts/exporting/offline-download-usehtml/demo.js
@@ -0,0 +1,42 @@
+Highcharts.chart('container', {
+    exporting: {
+        chartOptions: { // specific options for the exported image
+            plotOptions: {
+                series: {
+                    dataLabels: {
+                        useHTML: true,
+                        enabled: true
+                    }
+                }
+            }
+        },
+        scale: 3,
+        fallbackToExportServer: false,
+        error: function (opt, err) {
+            console.log('Failed with error: "' + err + '". Options:', opt);
+        },
+        allowHTML: true
+    },
+
+    title: {
+        text: 'Offline export with HTML data labels'
+    },
+
+    chart: {
+        type: 'area'
+    },
+
+    xAxis: {
+        categories: [
+            'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+        ]
+    },
+
+    series: [{
+        data: [
+            29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 126.0,
+            148.5, 216.4, 194.1, 95.6, 54.4
+        ]
+    }]
+});
