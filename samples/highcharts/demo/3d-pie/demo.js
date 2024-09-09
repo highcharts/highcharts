e6127a04b15587a563bf726954e0196a5b8261ea
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2024-09-09 15:08:09 +0200

    Revert "Added larger visual diff"
    This reverts commit 4e19013e258409f3a2b3fc896400254bc0f2a71b.

--- samples/highcharts/demo/3d-pie/demo.js
+++ samples/highcharts/demo/3d-pie/demo.js
@@ -5,11 +5,10 @@ Highcharts.chart('container', {
             enabled: true,
             alpha: 45,
             beta: 0
-        },
-        height: 150
+        }
     },
     title: {
-        text: 'Global smartphone               shipments market share, Q1 2022',
+        text: 'Global smartphone shipments market share, Q1 2022',
         align: 'left'
     },
     subtitle: {
