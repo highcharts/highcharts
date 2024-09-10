e52f263bad676de128c0f774c05ab1dad310d3f0
Kacper Madej <kacper.madej@blacklabel.net>
2024-09-10 10:39:45 +0200

    WiP. Workaround test.

--- samples/highcharts/tooltip/scroll/demo.js
+++ samples/highcharts/tooltip/scroll/demo.js
@@ -1,8 +1,9 @@
 const chartOptions = {
-        series: [{
-            type: 'column',
-            data: [42]
-        }]
-    },
-    chart1 = Highcharts.chart('container', chartOptions),
-    chart2 = Highcharts.chart('container2', chartOptions);
+    series: [{
+        type: 'column',
+        data: [42]
+    }]
+};
+
+Highcharts.chart('container', chartOptions);
+Highcharts.chart('container2', chartOptions);
