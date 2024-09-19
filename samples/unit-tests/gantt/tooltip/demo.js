29343c69cf14a9b789de321009486082f5a3dd55
Torstein Hønsi <torstein@highsoft.com>
2024-09-19 09:48:18 +0200

    Time refactor: Failing test on CI

--- samples/unit-tests/gantt/tooltip/demo.js
+++ samples/unit-tests/gantt/tooltip/demo.js
@@ -1,7 +1,7 @@
 QUnit.test('Gantt tooltip', assert => {
     const chart = Highcharts.ganttChart('container', {
-        time: {
-            locale: 'en-GB'
+        lang: {
+            locale: 'es' // Consistent between browsers
         },
         series: [
             {
@@ -25,7 +25,8 @@ QUnit.test('Gantt tooltip', assert => {
     assert.strictEqual(
         chart.tooltip.label.text.element.textContent
             .replace(/\u200B/g, ';'),
-        'Series 1;Task;Start: Tuesday, 1 Jan 2019;End: Monday, 7 Jan 2019;',
+        'Series 1;Task;Start: martes, 1 de ene de 2019;End: lunes, 7 de ene ' +
+            'de 2019;',
         'All times on midnight - tooltip should show the date without time'
     );
 
@@ -34,7 +35,7 @@ QUnit.test('Gantt tooltip', assert => {
     assert.strictEqual(
         chart.tooltip.label.text.element.textContent
             .replace(/\u200B/g, ';'),
-        'Series 1;Milestone;Saturday, 5 Jan 2019;',
+        'Series 1;Milestone;sábado, 5 de ene de 2019;',
         'All times on midnight - tooltip should show the date without time'
     );
 
@@ -61,8 +62,8 @@ QUnit.test('Gantt tooltip', assert => {
         [
             'Series 1',
             'Task',
-            'Start: Tuesday, 1 Jan, 08:00',
-            'End: Monday, 7 Jan, 16:00',
+            'Start: martes, 1 ene, 08:00',
+            'End: lunes, 7 ene, 16:00',
             ''
         ],
         'Intraday times - tooltip should show the date and time of day'
@@ -73,7 +74,7 @@ QUnit.test('Gantt tooltip', assert => {
     assert.strictEqual(
         chart.tooltip.label.text.element.textContent
             .replace(/\u200B/g, ';'),
-        'Series 1;Milestone;Saturday, 5 Jan, 12:00;',
+        'Series 1;Milestone;sábado, 5 ene, 12:00;',
         'Intraday times - tooltip should show the date and time of day'
     );
 });
@@ -83,9 +84,11 @@ QUnit.test(
     'and globally (#10365)',
     function (assert) {
         var chart = Highcharts.ganttChart('container', {
+                lang: {
+                    locale: 'es' // Consistent between browsers
+                },
                 time: {
-                    timezoneOffset: 6 * 60,
-                    locale: 'en-GB'
+                    timezoneOffset: 6 * 60
                 },
                 series: [
                     {
@@ -106,8 +109,8 @@ QUnit.test(
         assert.strictEqual(
             chart.container.querySelector('.highcharts-tooltip').textContent
                 .replace(/\u200B/g, ';'),
-            'Series 1;Task 1;Start: Monday, 1 Jun, 18:00;End: Tuesday, 2 ' +
-            'Jun, 18:00;',
+            'Series 1;Task 1;Start: lunes, 1 jun, 18:00;End: martes, 2 jun, ' +
+                '18:00;',
             'The tooltip should show the start and end shifted 6 hours ' +
                 'relative to UTC.'
         );
