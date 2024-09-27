707c7b814d3decde3bffe78ee478d00c3117ed38
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2024-09-27 12:56:00 +0200

    Added milestone workflow using github-script

---
+++ .github/scripts/assignMilestone.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+/**
+ * @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments
+ * @param {number} prNumber
+ * @param {string} milestoneName
+ */
+export async function assignNextMilestone (
+    { core, context, github },
+    prNumber,
+    milestoneName
+) {
+    core.debug("Running something at the moment");
+
+    const milestones = await github.rest.issues.listMilestones();
+
+    const nextMileStone = milestones.data.find(milestone => milestone.title === milestoneName);
+
+    // Assign the milestone
+    if (nextMileStone?.number) {
+        await github.rest.issues.update({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: prNumber,
+            milestone: nextMileStone.number
+        });
+    }
+    return context.actor;
+};
+
+export default assignNextMilestone
