a5088321d8d5111c08c6d14456b0405a6033148e
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2024-09-28 11:02:44 +0200

    Added `borderRadius` option to dependencywheel api docs

--- ts/Series/DependencyWheel/DependencyWheelSeriesDefaults.ts
+++ ts/Series/DependencyWheel/DependencyWheelSeriesDefaults.ts
@@ -105,6 +105,18 @@ const DependencyWheelSeriesDefaults: DependencyWheelSeriesOptions = {
     curveFactor: 0.6,
 
     /**
+     * The corner radius of the border surrounding each node. A number
+     * signifies pixels. A percentage string, like for example `50%`, signifies
+     * a relative size. For nodes this is relative to the node width.
+     *
+     * @type    {number|string|Highcharts.BorderRadiusOptionsObject}
+     * @default 3
+     * @product highcharts
+     * @since   11.0.0
+    */
+    borderRadius: 3,
+
+    /**
      * The start angle of the dependency wheel, in degrees where 0 is up.
      */
     startAngle: 0,
