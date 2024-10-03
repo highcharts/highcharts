3d4e820855a842712adab350cd13b69de83aa980
Kacper Madej <kacper.madej@blacklabel.net>
2024-10-03 11:20:16 +0200

    Paths readability and code format changed.

--- tools/gulptasks/unsorted/update-devDependencies.js
+++ tools/gulptasks/unsorted/update-devDependencies.js
@@ -1,5 +1,6 @@
 /* eslint no-console: 0 */
-const gulp = require('gulp');
+const gulp = require('gulp'),
+    path = require('path');
 /**
  * Update the updateDevDependencies files to resolve vulnerabilities
  *
@@ -8,22 +9,22 @@ const gulp = require('gulp');
  */
 function updateDevDependencies() {
     const { readFile, writeFile } = require('fs/promises');
-    const promises = [];
+
+    const promises = [],
+        pathPartToNodeModules = [__dirname, '..', '..', '..', 'node_modules'];
 
     // Update taffydb to @jsdoc/salty in outdated devDependencies
-    promises.push(
-        readFile(
-            './node_modules/ink-docstrap/template/publish.js'
-        ).then(content => writeFile(
-            './node_modules/ink-docstrap/template/publish.js',
-            content.toString().replace(
-                'require(\'taffydb\')',
-                'require(\'@jsdoc/salty\')'
-            )
-        ))
+    const publishPath = path.join(
+        ...pathPartToNodeModules, 'ink-docstrap', 'template', 'publish.js'
     );
+    promises.push(readFile(publishPath).then(content => writeFile(
+        publishPath,
+        content.toString().replace(
+            'require(\'taffydb\')',
+            'require(\'@jsdoc/salty\')'
+        )
+    )));
 
     return Promise.all(promises);
-
 }
 gulp.task('update-devDependencies', updateDevDependencies);
