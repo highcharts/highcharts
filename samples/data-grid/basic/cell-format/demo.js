dd9d493408c57dea1b3395dd223a62a7cd65b1a7
karolkolodziej <karolkolodziej01@gmail.com>
2024-10-28 09:06:08 +0100

    Removed hire date column

--- samples/data-grid/basic/cell-format/demo.js
+++ samples/data-grid/basic/cell-format/demo.js
@@ -3,13 +3,6 @@ DataGrid.dataGrid('container', {
         columns: {
             ID: [1, 2, 3, 4, 5],
             Name: ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
-            HireDate: [
-                1579046400000,
-                1532304000000,
-                1552176000000,
-                1622332800000,
-                1510963200000
-            ],
             Age: [28, 35, 40, 23, 30],
             Department: ['HR', 'Engineering', 'Sales', 'Marketing', 'Finance'],
             Salary: [50000, 75000, 55000, 45000, 65000],
@@ -17,16 +10,6 @@ DataGrid.dataGrid('container', {
         }
     },
     columns: [{
-        id: 'HireDate',
-        cells: {
-            formatter: function () {
-                return new Date(this.value).toLocaleDateString();
-            }
-        },
-        header: {
-            format: 'Hire date'
-        }
-    }, {
         id: 'Salary',
         cells: {
             className: '{#if (gt value 50000)}highlight-cell{/if}',
