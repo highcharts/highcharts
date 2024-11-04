57cd6b33de906b8e1ae8a60dac04785e3020f02f
Markus Knutson Barstad <markus.knutson.barstad@highsoft.com>
2024-11-04 13:55:32 +0100

    Demo name change, avoid confusion with dashboards

--- ts/Series/PackedBubble/PackedBubbleSeriesDefaults.ts
+++ ts/Series/PackedBubble/PackedBubbleSeriesDefaults.ts
@@ -174,7 +174,7 @@ const PackedBubbleSeriesDefaults: PackedBubbleSeriesOptions = {
         },
 
         /**
-         * @sample {highcharts} highcharts/demo/packed-bubble-dashboard
+         * @sample {highcharts} highcharts/demo/packed-bubble-project-status/
          *         Dashboard with dataLabels on parentNodes
          *
          * @declare Highcharts.SeriesPackedBubbleDataLabelsTextPathOptionsObject
@@ -269,7 +269,7 @@ const PackedBubbleSeriesDefaults: PackedBubbleSeriesOptions = {
          * In case of split series, this option allows user to drag and
          * drop points between series, for changing point related series.
          *
-         * @sample highcharts/demo/packed-bubble-dashboard/
+         * @sample highcharts/demo/packed-bubble-project-status/
          *         Example of drag'n drop bubbles for bubble kanban
          */
         dragBetweenSeries: false,
