c7e914dff4b4301b3aa30ebdfe6fc1deb50e17ac
Markus Knutson Barstad <markus.knutson.barstad@highsoft.com>
2024-11-12 10:53:24 +0100

    Fixed typing

--- ts/Series/PackedBubble/PackedBubblePoint.ts
+++ ts/Series/PackedBubble/PackedBubblePoint.ts
@@ -157,7 +157,7 @@ class PackedBubblePoint extends BubblePoint implements DragNodesPoint {
  *
  * */
 
-interface PackedBubblePoint extends Omit<NetworkgraphPoint, 'onMouseOver' | 'optionsToObject'> {
+interface PackedBubblePoint extends NetworkgraphPoint {
     className: BubblePointType['className'];
     fromNode: NetworkgraphPoint;
     linksFrom: Array<NetworkgraphPoint>;
