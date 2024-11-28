644debca29becf75e3eb1c1d75ed90ac2be6bac7
Dawid Draguła <dawid.dragula@gmail.com>
2024-11-28 14:55:36 +0100

    Added more rows to sample.

--- samples/data-grid/basic/container-height/demo.js
+++ samples/data-grid/basic/container-height/demo.js
@@ -6,11 +6,19 @@ const heightSliderLabel = document.getElementById('height-slider-label');
 DataGrid.dataGrid(container, {
     dataTable: {
         columns: {
-            product: ['Apples', 'Pears', 'Plums', 'Bananas'],
-            weight: [100, 40, 0.5, 200],
-            price: [1.5, 2.53, 5, 4.5],
-            metaData: ['a', 'b', 'c', 'd'],
-            icon: ['Apples URL', 'Pears URL', 'Plums URL', 'Bananas URL']
+            no: Array.from({ length: 400 }, (_, i) => i + 1),
+            product: [...Array.from({ length: 100 }, () =>
+                ['Apples', 'Pears', 'Plums', 'Bananas']
+            )].flat(),
+            weight: [...Array.from({ length: 100 }, () =>
+                [100, 40, 0.5, 200]
+            )].flat(),
+            price: [...Array.from({ length: 100 }, () =>
+                [1.5, 2.53, 5, 4.5]
+            )].flat(),
+            metaData: [...Array.from({ length: 100 }, () =>
+                ['a', 'b', 'c', 'd']
+            )].flat()
         }
     }
 });
