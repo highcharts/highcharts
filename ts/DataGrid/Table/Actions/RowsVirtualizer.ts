a84bff395a66434e0e65277992b062380727ca79
Dawid Draguła <dawid.dragula@gmail.com>
2024-11-29 14:14:07 +0100

    Fixed mock row height.

--- ts/DataGrid/Table/Actions/RowsVirtualizer.ts
+++ ts/DataGrid/Table/Actions/RowsVirtualizer.ts
@@ -439,7 +439,10 @@ class RowsVirtualizer {
      */
     private getDefaultRowHeight(): number {
         const mockRow = makeHTMLElement('tr', {
-            className: Globals.classNames.rowElement
+            className: Globals.classNames.rowElement,
+            style: {
+                position: 'absolute'
+            }
         }, this.viewport.tbodyElement);
 
         const defaultRowHeight = mockRow.offsetHeight;
