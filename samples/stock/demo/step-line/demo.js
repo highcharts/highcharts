a9dcb12aad6ba3a5b85d94bf544618c061638863
Kamil Musiałowski <kamil.musialowski@blacklabel.pl>
2024-12-11 18:00:22 +0100

    Improved dataset for step line stock demo.

--- samples/stock/demo/step-line/demo.js
+++ samples/stock/demo/step-line/demo.js
@@ -2,42 +2,77 @@
 
     // Load the dataset
     const data = await fetch(
-        'https://demo-live-data.highcharts.com/aapl-c.json'
+        'https://cdn.jsdelivr.net/gh/highcharts/highcharts@sha/samples/data/investment-simulator.json'
     ).then(response => response.json());
 
     // Create the chart
     Highcharts.stockChart('container', {
+
+        chart: {
+            spacingRight: 25
+        },
 
         rangeSelector: {
             selected: 1
         },
 
         title: {
-            text: 'AAPL Stock Price'
+            text: 'Recurring stock investment simulator'
         },
 
         xAxis: {
             overscroll: '10px'
         },
 
-        series: [{
-            name: 'AAPL Stock Price',
-            data: data,
-            step: true,
-            tooltip: {
-                valueDecimals: 2
-            },
-            lastPrice: {
-                enabled: true,
-                color: 'transparent',
-                label: {
+        yAxis: {
+            labels: {
+                align: 'left'
+            }
+        },
+
+        legend: {
+            enabled: true
+        },
+
+        plotOptions: {
+            series: {
+                tooltip: {
+                    valueDecimals: 2
+                },
+                pointStart: '2024-01-01',
+                pointInterval: 86400000, // One day
+                lastPrice: {
                     enabled: true,
-                    backgroundColor: '#ffffff',
-                    borderColor: '#2caffe',
-                    borderWidth: 1,
-                    style: {
-                        color: '#000000'
+                    color: 'transparent',
+                    label: {
+                        enabled: true,
+                        format: '{value:.2f}',
+                        backgroundColor: '#ffffff',
+                        borderWidth: 1,
+                        style: {
+                            color: '#000000'
+                        }
                     }
+                }
+            }
+        },
+
+        series: [{
+            name: 'Invested amount',
+            data: data[0],
+            step: true,
+            dashStyle: 'ShortDot',
+            lastPrice: {
+                label: {
+                    borderColor: '#2caffe'
+                }
+            }
+        }, {
+            name: 'Portfolio value',
+            data: data[1],
+            lastPrice: {
+                label: {
+                    borderColor: '#544fc5'
                 }
             }
         }]
