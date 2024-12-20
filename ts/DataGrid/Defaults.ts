77efde9ec672d1c8d641ca48a44ce1c7d05c2338
Dawid Draguła <dawid.dragula@gmail.com>
2024-12-20 14:31:03 +0100

    Changed sortable column headers to spans, added hidden description.

--- ts/DataGrid/Defaults.ts
+++ ts/DataGrid/Defaults.ts
@@ -57,6 +57,7 @@ namespace Defaults {
                     }
                 },
                 sorting: {
+                    sortable: 'Sortable.',
                     announcements: {
                         ascending: 'Sorted ascending.',
                         descending: 'Sorted descending.',
