e0e3efe5c0f4b74d7fbcf4036a3ca9754e7c8233
Sebastian Bochan <sebastian@blacklabel.pl>
2025-01-06 09:46:16 +0100

    Fixed issue with triggering sort when cell is edited.

--- ts/DataGrid/Table/Actions/ColumnSorting.ts
+++ ts/DataGrid/Table/Actions/ColumnSorting.ts
@@ -174,6 +174,11 @@ class ColumnSorting {
         const querying = viewport.dataGrid.querying;
         const sortingController = querying.sorting;
 
+        // Do not call sorting when cell is currently edited and validated.
+        if (viewport.validator.errorCell) {
+            return;
+        }
+
         const currentOrder = (
             sortingController.currentSorting?.columnId === this.column.id ?
                 sortingController.currentSorting.order : null
