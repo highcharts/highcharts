f7be28bdd3c90fe57855d952b191f4a754a2551e
Kamil Musiałowski <kamil.musialowski@blacklabel.pl>
2025-01-20 17:48:40 +0100

    Added requested changes.

--- samples/stock/demo/drag-on-axis/demo.js
+++ samples/stock/demo/drag-on-axis/demo.js
@@ -1,4 +1,4 @@
-function zoomOnYAxis() {
+(function (Highcharts) {
     function centerZoom(axis, strength) {
         let zoomOffset = (axis.max - axis.min) / 2;
         const center = axis.min + zoomOffset;
@@ -9,97 +9,78 @@ function zoomOnYAxis() {
             zoomOffset = 0;
         }
 
-        axis.setExtremes(center - zoomOffset, center + zoomOffset, false);
-        axis.chart.redraw(false);
+        axis.setExtremes(center - zoomOffset, center + zoomOffset, true, false);
     }
 
-    Highcharts.addEvent(Highcharts.Chart, 'load', function () {
-        const chart = this,
-            renderer = chart.renderer,
-            yAxes = chart.yAxis.filter(axis => !axis.options.isInternal);
+    Highcharts.addEvent(Highcharts.Axis, 'afterInit', function () {
+        const axis = this,
+            renderer = axis.chart.renderer;
 
-        yAxes.forEach(yAxis => {
+        if (!axis.isXAxis && !axis.options.isInternal) {
             // Create a transparent rectangle on yAxis to create a zoom area
-            yAxis.axisZoomRect = renderer.rect()
+            axis.axisZoomRect = renderer.rect()
                 .attr({
                     width: 50,
-                    height: yAxis.height,
-                    fill: 'transparent'
+                    height: 300,
+                    fill: 'transparent',
+                    zIndex: 8
                 })
+                .addClass('highcharts-no-mousewheel')
                 .css({ cursor: 'ns-resize' })
                 .add();
 
             // Enabled zooming via mousewheel scroll
-            yAxis.axisZoomRect.on('mousewheel', event => {
-                centerZoom(yAxis, event.deltaY * 0.0001);
+            axis.axisZoomRect.on('mousewheel', event => {
+                centerZoom(axis, event.deltaY * 0.0001);
             });
 
             // Enable zooming by drag on mouse click
-            yAxis.axisZoomRect.on('mousedown', () => {
-                yAxis.drag = true;
+            axis.axisZoomRect.on('mousedown', () => {
+                axis.drag = true;
             });
 
             // Zoom on yAxis while dragging the mouse
             document.addEventListener('mousemove', event => {
-                if (yAxis.drag) {
-                    centerZoom(yAxis, event.movementY * 0.001);
+                if (axis.drag) {
+                    centerZoom(axis, event.movementY * 0.001);
                 }
             });
 
             // Disable zooming when mouse click is released
             document.addEventListener('mouseup', () => {
-                yAxis.drag = false;
+                axis.drag = false;
             });
-        });
+        }
     });
 
     Highcharts.addEvent(Highcharts.Chart, 'render', function () {
         const chart = this,
-            yAxes = chart.yAxis.filter(axis => !axis.options.isInternal);
+            yAxes = chart.yAxis;
 
         yAxes.forEach(yAxis => {
-            yAxis.axisZoomRect.attr({ x: yAxis.width - 20, y: yAxis.top });
+            if (yAxis.axisZoomRect) {
+                yAxis.axisZoomRect.attr({ x: yAxis.width - 20, y: yAxis.top });
+            }
         });
     });
-}
-
-zoomOnYAxis();
+}(Highcharts));
 
 Highcharts.getJSON('https://demo-live-data.highcharts.com/aapl-c.json', data => {
     Highcharts.stockChart('container', {
-        chart: {
-            zooming: {
-                mouseWheel: {
-                    enabled: false
-                }
-            }
-        },
         yAxis: [{
             startOnTick: false,
             endOnTick: false,
-            height: '50%',
-            labels: {
-                style: {
-                    // Pass the drag event through labels
-                    'pointer-events': 'none'
-                }
-            }
+            height: '50%'
         }, {
             startOnTick: false,
             endOnTick: false,
             height: '50%',
-            top: '50%',
-            labels: {
-                style: {
-                    // Pass the drag event through labels
-                    'pointer-events': 'none'
-                }
-            }
+            top: '50%'
         }],
         series: [{
             data
         }, {
-            data: data.slice(data.length / 5, data.length),
+            data,
             yAxis: 1
         }]
     });
