e98f8e32d08675159f97394c25ca55c2cbf7da32
Sebastian Bochan <sebastian@blacklabel.pl>
2025-01-23 13:24:20 +0100

    Added product validation in tooling, added Grid to lint-ts.

---
+++ tools/gulptasks/utils.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) Highsoft AS
+ */
+
+/* *
+ *
+ *  Constants
+ *
+ * */
+const PRODUCTS = ['Highcharts', 'Grid'];
+
+/**
+ * The validation of product name.
+ * @param {*} name of product
+ * @return {Promise<void>}
+ *    Promise to keep
+ */
+function validateProduct(name) {
+    const logLib = require('../libs/log');
+
+    if (PRODUCTS.indexOf(name || 'Highcharts') < 0) {
+        logLib.warn('Cannot find a product' + name + '.');
+        return false;
+    }
+
+    return true;
+}
+
+/* *
+ *
+ *  Exports
+ *
+ * */
+
+
+module.exports = {
+    validateProduct
+};
