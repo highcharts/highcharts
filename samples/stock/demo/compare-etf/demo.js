5df8357ee7577edaf6cbe1672ac3e4c1a7e5b7be
Kamil Musiałowski <kamil.musialowski@blacklabel.pl>
2025-01-29 14:19:40 +0100

    Demo adjustments.

--- samples/stock/demo/compare-etf/demo.js
+++ samples/stock/demo/compare-etf/demo.js
@@ -1,102 +1,126 @@
 const PERIOD_CAP = 12.03,
-    PERIOD_BUFFER = 15;
+    PERIOD_BUFFER = 15,
+    CSPXid = '0P0000OO1Z',
+    ESPOid = '0P0001HVAN';
+
+const timeSeriesConnector =
+// eslint-disable-next-line no-undef
+    new HighchartsConnectors.Morningstar.TimeSeriesConnector({
+        api: {
+            url: 'https://demo-live-data.highcharts.com',
+            access: {
+                url: 'https://demo-live-data.highcharts.com/token/oauth',
+                token: 'token'
+            }
+        },
+        currencyId: 'USD',
+        startDate: '2024-01-01',
+        endDate: '2025-01-01',
+        series: {
+            type: 'Price'
+        },
+        securities: [{
+            id: CSPXid, // CSSPX
+            idType: 'MSID'
+        }, {
+            id: ESPOid, // ESPO
+            idType: 'MSID'
+        }]
+    });
 (async () => {
+    await timeSeriesConnector.load();
 
-    const names = ['MSFT', 'AAPL', 'GOOG'];
+    const {
+        [CSPXid]: CSPXValues,
+        [ESPOid]: ESPOValues,
+        Date: date
+    } = timeSeriesConnector.table.getColumns();
 
-    /**
-     * Create the chart when all data is loaded
-     * @return {undefined}
-     */
-    function createChart(series) {
+    const CSPXdata = [],
+        ESPOdata = [];
 
-        Highcharts.stockChart('container', {
+    for (let i = 0; i < date.length; i++) {
+        CSPXdata.push([date[i], CSPXValues[i]]);
+        ESPOdata.push([date[i], ESPOValues[i]]);
+    }
 
-            rangeSelector: {
-                selected: 4
+    Highcharts.stockChart('container', {
+        rangeSelector: {
+            selected: 4
+        },
+
+        yAxis: {
+            labels: {
+                format: '{#if (gt value 0)}+{/if}{value}%'
             },
-
-            yAxis: {
-                labels: {
-                    format: '{#if (gt value 0)}+{/if}{value}%'
-                },
-                opposite: false,
-                plotLines: [{
-                    value: PERIOD_CAP,
-                    width: 2,
-                    color: 'rgba(6, 79, 233, 1)',
-                    dashStyle: 'Dash',
-                    label: {
-                        align: 'right',
-                        text: `OUTCOME PERIOD CAP: ${PERIOD_CAP}%`,
-                        x: -3,
-                        style: {
-                            color: 'rgba(6, 79, 233, 1)',
-                            fontWeight: 'bold'
-                        }
+            opposite: false,
+            plotLines: [{
+                value: PERIOD_CAP,
+                width: 2,
+                color: 'rgba(6, 79, 233, 1)',
+                dashStyle: 'Dash',
+                label: {
+                    align: 'right',
+                    text: `OUTCOME PERIOD CAP: ${PERIOD_CAP}%`,
+                    x: -3,
+                    style: {
+                        color: 'rgba(6, 79, 233, 1)',
+                        fontWeight: 'bold'
                     }
-                }, {
-                    value: -PERIOD_BUFFER,
-                    width: 2,
-                    color: 'rgba(233, 6, 41, 1)',
-                    dashStyle: 'Dash',
-                    label: {
-                        align: 'right',
-                        verticalAlign: 'bottom',
-                        text: `OUTCOME PERIOD BUFFER: ${PERIOD_BUFFER}%`,
-                        x: -3,
-                        y: 13,
-                        style: {
-                            color: 'rgba(233, 6, 41, 1)',
-                            fontWeight: 'bold'
-                        }
-                    }
-                }, {
-                    value: 0,
-                    width: 2,
-                    dashStyle: 'ShortDot'
-                }],
-                plotBands: [{
-                    from: 0,
-                    to: PERIOD_CAP,
-                    color: 'rgba(6, 79, 233, 0.2)'
-                }, {
-                    from: 0,
-                    to: -PERIOD_BUFFER,
-                    color: 'rgba(233, 6, 41, 0.2)'
-                }]
-            },
-
-            plotOptions: {
-                series: {
-                    compare: 'percent',
-                    showInNavigator: true
                 }
-            },
+            }, {
+                value: -PERIOD_BUFFER,
+                width: 2,
+                color: 'rgba(233, 6, 41, 1)',
+                dashStyle: 'Dash',
+                label: {
+                    align: 'right',
+                    verticalAlign: 'bottom',
+                    text: `OUTCOME PERIOD BUFFER: ${PERIOD_BUFFER}%`,
+                    x: -3,
+                    y: 13,
+                    style: {
+                        color: 'rgba(233, 6, 41, 1)',
+                        fontWeight: 'bold'
+                    }
+                }
+            }, {
+                value: 0,
+                width: 2,
+                dashStyle: 'ShortDot'
+            }],
+            plotBands: [{
+                from: 0,
+                to: PERIOD_CAP,
+                color: 'rgba(6, 79, 233, 0.2)'
+            }, {
+                from: 0,
+                to: -PERIOD_BUFFER,
+                color: 'rgba(233, 6, 41, 0.2)'
+            }]
+        },
 
-            tooltip: {
-                pointFormat: '<span style="color:{series.color}">' +
-                    '{series.name}</span>: <b>{point.y}</b> ' +
-                    '({point.change}%)<br/>',
-                valueDecimals: 2,
-                split: true
-            },
+        plotOptions: {
+            series: {
+                compare: 'percent',
+                showInNavigator: true
+            }
+        },
 
-            series
-        });
+        tooltip: {
+            pointFormat: '<span style="color:{series.color}">' +
+                        '{series.name}</span>: <b>{point.y} USD</b> ' +
+                        '({point.change}%)<br/>',
+            valueDecimals: 2,
+            split: true
+        },
 
-    }
-
-    const series = [];
-    for (const name of names) {
-        const response = await fetch(
-            'https://cdn.jsdelivr.net/gh/highcharts/highcharts@f0e61a1/' +
-            'samples/data/' + name.toLowerCase() + '-c.json'
-        );
-        const data = await response.json();
-        series.push({ name, data });
-    }
-
-    createChart(series);
-
+        series: [{
+            name: 'iShares Core S&P 500 UCITS ETF',
+            data: CSPXdata
+        }, {
+            name: 'VanEck Video Gaming and eSports ETF',
+            data: ESPOdata
+        }]
+    });
 })();
