d99efe411a20a62ddbeb395a447f06d63c9439b2
Dawid Draguła <dawid.dragula@gmail.com>
2025-02-14 17:55:04 +0100

    Added missing imports to Grid Pro dts.

--- tools/gulptasks/dashboards/scripts-dts/datagrid.src.d.ts
+++ tools/gulptasks/dashboards/scripts-dts/datagrid.src.d.ts
@@ -4,9 +4,13 @@
  *
  *!*/
 
-import Grid from "./es-modules/Grid/Core/Grid";
-import Globals from "./es-modules/Grid/Core/Globals";
-import Defaults from "./es-modules/Grid/Core/Defaults";
+import Grid from './es-modules/Grid/Core/Grid';
+import Globals from './es-modules/Grid/Core/Globals';
+import Defaults from './es-modules/Grid/Core/Defaults';
+
+import './es-modules/Grid/Pro/GridEvents';
+import './es-modules/Grid/Pro/CellEditing/CellEditingComposition';
+import './es-modules/Grid/Pro/Dash3Compatibility';
 
 export { /** @deprecated Use `Grid` instead. */ default as DataGrid } from './es-modules/Grid/Core/Grid.js';
 export { default as Grid } from './es-modules/Grid/Core/Grid.js';
@@ -15,13 +19,13 @@ export { default as TableRow } from './es-modules/Grid/Core/Table/Content/TableR
 export { default as TableCell } from './es-modules/Grid/Core/Table/Content/TableCell.js';
 export { default as Options } from './es-modules/Grid/Core/Options.js';
 
-export { default as DataConnector } from "./es-modules/Data/Connectors/DataConnector";
-export { default as DataConverter } from "./es-modules/Data/Converters/DataConverter";
-export { default as DataCursor } from "./es-modules/Data/DataCursor";
-export { default as DataEvent } from "./es-modules/Data/DataEvent";
-export { default as DataModifier } from "./es-modules/Data/Modifiers/DataModifier";
-export { default as DataPool } from "./es-modules/Data/DataPool";
-export { default as DataTable } from "./es-modules/Data/DataTable";
+export { default as DataConnector } from './es-modules/Data/Connectors/DataConnector';
+export { default as DataConverter } from './es-modules/Data/Converters/DataConverter';
+export { default as DataCursor } from './es-modules/Data/DataCursor';
+export { default as DataEvent } from './es-modules/Data/DataEvent';
+export { default as DataModifier } from './es-modules/Data/Modifiers/DataModifier';
+export { default as DataPool } from './es-modules/Data/DataPool';
+export { default as DataTable } from './es-modules/Data/DataTable';
 
 /** @deprecated Use `grid` instead. */
 export const dataGrid: typeof Grid.grid;
