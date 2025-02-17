49b4267c4dea94b3b844667279872344497ecf30
KamilKubik <kamilkubik2904@gmail.com>
2025-02-17 11:51:33 +0100

    Handled nested accordion toggle rendering.

--- ts/Dashboards/EditMode/AccordionMenu.ts
+++ ts/Dashboards/EditMode/AccordionMenu.ts
@@ -311,6 +311,7 @@ class AccordionMenu {
         parentNode: HTMLElement,
         component: Component
     ): void {
+        debugger;
 
         if (options.type === 'nested') {
             return this.renderNested(parentNode, options, component);
@@ -326,6 +327,7 @@ class AccordionMenu {
             ...options,
             iconsURLPrefix: this.iconsURLPrefix,
             value: component.getEditableOptionValue(options.propertyPath),
+            enabledOnOffLabels: options.type === 'toggle',
             onchange: (
                 value: boolean | string | number
             ): void => this.updateOptions(options.propertyPath || [], value)
@@ -361,6 +363,7 @@ class AccordionMenu {
             const accordionOptions = nestedOptions[i].options;
             const showToggle = !!nestedOptions[i].showToggle;
             const propertyPath = nestedOptions[i].propertyPath || [];
+            const lang = (component.board?.editMode || EditGlobals).lang;
 
             const collapsedHeader = EditRenderer.renderCollapseHeader(
                 parentElement, {
@@ -372,13 +375,16 @@ class AccordionMenu {
                         this.updateOptions(propertyPath, value),
                     isNested: !!accordionOptions,
                     isStandalone: !accordionOptions,
-                    lang: (component.board?.editMode || EditGlobals).lang
+                    lang
                 }
             );
 
             for (let j = 0, jEnd = accordionOptions?.length; j < jEnd; ++j) {
                 this.renderAccordion(
-                    accordionOptions[j] as EditableOptions.Options,
+                    merge(
+                        accordionOptions[j] as EditableOptions.Options,
+                        { lang, isNested: true }
+                    ),
                     collapsedHeader.content,
                     component
                 );
