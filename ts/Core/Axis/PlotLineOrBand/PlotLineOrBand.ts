c73ba45111d1208b5941f68f0324471474d590bc
Markus Knutson Barstad <markus.knutson.barstad@highsoft.com>
2025-02-18 12:33:20 +0100

    Label formatter plotlineorband

--- ts/Core/Axis/PlotLineOrBand/PlotLineOrBand.ts
+++ ts/Core/Axis/PlotLineOrBand/PlotLineOrBand.ts
@@ -16,7 +16,7 @@
  *
  * */
 import type Chart from '../../Chart/Chart';
-import type Templating from '../../Templating';
+// Import type Templating from '../../Templating';
 import type {
     PlotBandLabelOptions,
     PlotBandOptions
@@ -422,8 +422,9 @@ class PlotLineOrBand {
     ): string | undefined {
         return defined(optionsLabel.formatter) ?
             (optionsLabel.formatter as
-              Templating.FormatterCallback<PlotLineOrBand>)
-                .call(this) :
+             any)
+            // Templating.FormatterCallback<PlotLineOrBand>)
+                .call(this, this) :
             optionsLabel.text;
     }
 
