5fb04b04add6abf50a40ae3ab861c94732bef29b
Sebastian Bochan <sebastian@blacklabel.pl>
2025-02-23 13:36:36 +0100

    Renamed CreditsMore to CreditsComposition.

--- ts/masters-datagrid/datagrid.src.ts
+++ ts/masters-datagrid/datagrid.src.ts
@@ -38,7 +38,7 @@ import TableCell from '../Grid/Core/Table/Content/TableCell.js';
 import GridEvents from '../Grid/Pro/GridEvents.js';
 import CellEditingComposition from '../Grid/Pro/CellEditing/CellEditingComposition.js';
 import Dash3Compatibility from '../Grid/Pro/Dash3Compatibility.js';
-import CreditsMore from '../Grid/Pro/CreditsMore.js';
+import CreditsComposition from '../Grid/Pro/CreditsComposition.js';
 
 // Fill registries
 import '../Data/Connectors/CSVConnector.js';
@@ -137,7 +137,7 @@ G.TableCell = G.TableCell || TableCell;
 
 GridEvents.compose(G.Column, G.HeaderCell, G.TableCell);
 CellEditingComposition.compose(G.Table, G.TableCell);
-CreditsMore.compose(G.Grid);
+CreditsComposition.compose(G.Grid);
 Dash3Compatibility.compose(G.Table);
 
 
