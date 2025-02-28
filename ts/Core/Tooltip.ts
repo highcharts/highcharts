7a54f614fc4a9d41ad443e3a36bb1e759b47859f
SamVanini <vaninisamantha@yahoo.it>
2025-02-28 21:13:35 +0100

    Fixed #22599, added boolean array to FormatterCallbackFunctionInterface

--- ts/Core/Tooltip.ts
+++ ts/Core/Tooltip.ts
@@ -1864,7 +1864,7 @@ namespace Tooltip {
         (
             this: Point,
             tooltip: Tooltip
-        ): (false|string|Array<string>);
+        ): (false|string|Array<(boolean | string)>);
     }
 
     export interface HeaderFormatterEventObject {
