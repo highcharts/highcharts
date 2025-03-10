ef36060126c4b5c0f2965902e5158f3ca38ffdcc
Torstein Hønsi <torstein@highsoft.com>
2025-03-10 10:20:35 +0100

    Foreign object: annotation drag had duplicated events

--- ts/Extensions/Annotations/EventEmitter.ts
+++ ts/Extensions/Annotations/EventEmitter.ts
@@ -94,7 +94,11 @@ abstract class EventEmitter {
         addMouseDownEvent(this.graphic.element);
 
         (emitter.labels || []).forEach((label): void => {
-            if (label.options.useHTML && label.graphic.text) {
+            if (
+                label.options.useHTML &&
+                label.graphic.text &&
+                !label.graphic.text.foreignObject
+            ) {
                 // Mousedown event bound to HTML element (#13070).
                 addMouseDownEvent(label.graphic.text.element);
             }
@@ -153,7 +157,11 @@ abstract class EventEmitter {
                 emitter.graphic.css(cssPointer);
 
                 (emitter.labels || []).forEach((label): void => {
-                    if (label.options.useHTML && label.graphic.text) {
+                    if (
+                        label.options.useHTML &&
+                        label.graphic.text &&
+                        !label.graphic.text.foreignObject
+                    ) {
                         label.graphic.text.css(cssPointer);
                     }
                 });
