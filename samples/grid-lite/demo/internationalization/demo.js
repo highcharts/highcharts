03d110cacc8429fd057e1886bbcf444a9bcb63fc
Dawid Draguła <dawid.dragula@gmail.com>
2025-03-13 13:24:31 +0100

    Divided demo to separate two: a11y and i18n.

--- samples/grid-lite/demo/internationalization/demo.js
+++ samples/grid-lite/demo/internationalization/demo.js
@@ -15,21 +15,10 @@ const grid = Grid.grid('container', {
             minVisibleRows: 4
         }
     },
-    columnDefaults: {
-        cells: {
-            editable: true
-        }
-    },
-    credits: {
-        text: 'Highcharts Grid'
-    },
     columns: [{
         id: 'id',
         header: {
             format: 'ID'
-        },
-        cells: {
-            editable: false
         }
     }, {
         id: 'weight',
@@ -55,14 +44,6 @@ const languages = {
                         descending: 'Sorted descending.',
                         none: 'Not sorted.'
                     }
-                },
-                cellEditing: {
-                    editable: 'Editable.',
-                    announcements: {
-                        started: 'Entered cell editing mode.',
-                        edited: 'Edited cell value.',
-                        cancelled: 'Editing cancelled.'
-                    }
                 }
             },
             decimalPoint: '.'
@@ -85,7 +66,7 @@ const languages = {
             }
         }],
         caption: {
-            text: 'Fruits Table'
+            text: 'Fruits Table - Internationalization Demo'
         }
     },
     no: {
@@ -97,14 +78,6 @@ const languages = {
                         ascending: 'Sortert stigende.',
                         descending: 'Sortert synkende.',
                         none: 'Ikke sortert.'
-                    }
-                },
-                cellEditing: {
-                    editable: 'Redigerbar.',
-                    announcements: {
-                        started: 'Gått inn i redigeringsmodus for celle.',
-                        edited: 'Celleverdi redigert.',
-                        cancelled: 'Redigering avbrutt.'
                     }
                 }
             },
@@ -128,7 +101,7 @@ const languages = {
             }
         }],
         caption: {
-            text: 'Frukttabell'
+            text: 'Frukttabell - Internasjonaliseringsdemo'
         }
     },
     pl: {
@@ -140,14 +113,6 @@ const languages = {
                         ascending: 'Posortowano rosnąco.',
                         descending: 'Posortowano malejąco.',
                         none: 'Nie posortowano.'
-                    }
-                },
-                cellEditing: {
-                    editable: 'Edytowalne.',
-                    announcements: {
-                        started: 'Rozpoczeto edycję komórki.',
-                        edited: 'Zmieniono wartość komórki.',
-                        cancelled: 'Anulowano edycję.'
                     }
                 }
             },
@@ -171,7 +136,7 @@ const languages = {
             }
         }],
         caption: {
-            text: 'Tabela Owoców'
+            text: 'Tabela Owoców - Demo internacjonalizacji'
         }
     },
     zh: {
@@ -183,14 +148,6 @@ const languages = {
                         ascending: '已按升序排序。',
                         descending: '已按降序排序。',
                         none: '未排序。'
-                    }
-                },
-                cellEditing: {
-                    editable: '可编辑。',
-                    announcements: {
-                        started: '进入单元格编辑模式。',
-                        edited: '已编辑单元格值。',
-                        cancelled: '编辑已取消。'
                     }
                 }
             },
@@ -214,67 +171,21 @@ const languages = {
             }
         }],
         caption: {
-            text: '水果表'
+            text: '水果表 - 国际化演示'
         }
     }
 };
 
-const editorInputs = document.querySelectorAll('.editor input');
 const languageSelect = document.getElementById('lang-select');
-
-function getOptionValue(path) {
-    let cursor = grid.options;
-    for (const key of path.split('.')) {
-        cursor = cursor[key];
-    }
-    return cursor;
-}
-
-function setOption(input) {
-    const result = {};
-    const path = input.name.split('.');
-    let cursor = result;
-
-    for (let i = 0, iEnd = path.length - 1; i < iEnd; i++) {
-        cursor[path[i]] = cursor = {};
-    }
-
-    cursor[path[path.length - 1]] =
-        input.type === 'checkbox' ? input.checked : input.value;
-
-    grid.update(result);
-}
-
-function setInputValue(input) {
-    const value = getOptionValue(input.name);
-    if (input.type === 'checkbox') {
-        input.checked = value;
-    } else {
-        input.value = value;
-    }
-}
 
 function setLanguage(lang) {
     grid.update(languages[lang]);
     grid.container.setAttribute('lang', lang);
     grid.container.setAttribute('aria-lang', lang);
-    for (const input of editorInputs) {
-        setInputValue(input);
-    }
 }
 
 languageSelect.addEventListener('change', () => {
     setLanguage(languageSelect.value);
 });
-
-for (const input of editorInputs) {
-    input.addEventListener('change', () => {
-        setOption(input);
-
-        if (input.name.split[0] === 'lang') {
-            languageSelect.value = '-';
-        }
-    });
-}
 
 setLanguage('en');
