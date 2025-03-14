a4c65207e6214191a2d0966695905f3f344ccc2a
Dawid Draguła <dawid.dragula@gmail.com>
2025-03-14 15:14:54 +0100

    Updated grid-lite/basic samples.

--- samples/grid-lite/basic/header-visibility/demo.js
+++ samples/grid-lite/basic/header-visibility/demo.js
@@ -1,11 +1,17 @@
 const dg = Grid.grid('container', {
     dataTable: {
         columns: {
-            product: ['Apples', 'Pears', 'Plums', 'Bananas'],
-            weight: [100, 40, 0.5, 200],
-            price: [1.5, 2.53, 5, 4.5],
-            metaData: ['a', 'b', 'c', 'd'],
-            icon: ['Apples URL', 'Pears URL', 'Plums URL', 'Bananas URL']
+            product: [
+                'Apples', 'Pears', 'Plums', 'Bananas',
+                'Oranges', 'Peaches', 'Pineapples', 'Grapes'
+            ],
+            weight: [100, 40, 0.5, 200, 150, 300, 1000, 500],
+            price: [1.5, 2.53, 5, 4.5, 3.5, 2.5, 1, 2],
+            metaData: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+            icon: [
+                'Apples URL', 'Pears URL', 'Plums URL', 'Bananas URL',
+                'Oranges URL', 'Peaches URL', 'Pineapples URL', 'Grapes URL'
+            ]
         }
     }
 });
