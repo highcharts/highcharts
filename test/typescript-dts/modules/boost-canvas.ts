367117069edc268f87c0a9a2489caeaeaaffb007
hubertkozik <hubertkozik13@gmail.com>
2025-03-18 12:11:57 +0100

    Fixed TS tests, removed factory calls.

--- test/typescript-dts/modules/boost-canvas.ts
+++ test/typescript-dts/modules/boost-canvas.ts
@@ -1,4 +1,2 @@
 import * as Highcharts from 'highcharts';
-import BoostCanvasModule from 'highcharts/modules/boost-canvas';
-
-BoostCanvasModule(Highcharts);
+import 'highcharts/modules/boost-canvas';
