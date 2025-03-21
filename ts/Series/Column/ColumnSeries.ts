8b6d6d757d44671ecc029269f49feba19332c162
Markus Knutson Barstad <markus.knutson.barstad@highsoft.com>
2025-03-21 11:08:35 +0100

    Reverted conditional

--- ts/Series/Column/ColumnSeries.ts
+++ ts/Series/Column/ColumnSeries.ts
@@ -850,21 +850,23 @@ class ColumnSeries extends Series {
             onMouseOver = function (e: PointerEvent): void {
                 pointer?.normalize(e);
 
-                const point = pointer?.getPointFromEvent(e);
+                const point = pointer?.getPointFromEvent(e),
+                    // Run point events only for points inside plot area, #21136
+                    isInsidePlot = chart.scrollablePlotArea ?
+                        chart.isInsidePlot(
+                            e.chartX - chart.plotLeft,
+                            e.chartY - chart.plotTop,
+                            {
+                                visiblePlotOnly: true
+                            }
+                        ) : true;
 
                 // Undefined on graph in scatterchart
                 if (
                     pointer &&
                     point &&
                     series.options.enableMouseTracking &&
-                    // Run point events only for points inside plot area, #21136
-                    chart.isInsidePlot(
-                        e.chartX - chart.plotLeft,
-                        e.chartY - chart.plotTop,
-                        {
-                            visiblePlotOnly: true
-                        }
-                    )
+                    isInsidePlot
                 ) {
                     pointer.isDirectTouch = true;
                     point.onMouseOver(e);
