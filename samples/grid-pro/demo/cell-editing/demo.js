f3efae49e414b56102dd8e04a105b4d23840129e
Sebastian Bochan <sebastian@blacklabel.pl>
2025-03-26 20:06:33 +0100

    Added changelog to cell-editing changes.

--- samples/grid-pro/demo/cell-editing/demo.js
+++ samples/grid-pro/demo/cell-editing/demo.js
@@ -1,9 +1,19 @@
+const changelog = document.querySelector('#changelog');
+
 Grid.grid('container', {
     dataTable: {
         columns: {
             product: ['Apples', 'Pears', 'Plums', 'Bananas'],
             weight: [100, 40, 0.5, 200],
             price: [1.5, 2.53, 5, 4.5]
+        }
+    },
+    events: {
+        cell: {
+            afterEdit: function () {
+                changelog.innerHTML +=
+                    `<strong>${this.column.id}</strong> for <strong>${this.row.data.product}</strong> was updated to ${this.value} <br />`; // eslint-disable-line
+            }
         }
     },
     columnDefaults: {
