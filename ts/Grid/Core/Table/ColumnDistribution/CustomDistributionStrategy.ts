ddc6c10cd013647faf1d3ee7be12e07bc0e6ed7b
Dawid Draguła <dawid.dragula@gmail.com>
2025-04-01 12:22:20 +0200

    Implemented Custom Distribution Strategy.

--- ts/Grid/Core/Table/ColumnDistribution/CustomDistributionStrategy.ts
+++ ts/Grid/Core/Table/ColumnDistribution/CustomDistributionStrategy.ts
@@ -27,6 +27,9 @@ import type ColumnsResizer from '../Actions/ColumnsResizer';
 
 import DistributionStrategy from './ColumnDistributionStrategy.js';
 
+import U from '../../../../Core/Utilities.js';
+const { defined } = U;
+
 
 /* *
  *
@@ -44,6 +47,13 @@ class CustomDistributionStrategy extends DistributionStrategy {
 
     public override readonly type: 'custom' = 'custom';
 
+    /**
+     * Array of units for each column width value. Codified as:
+     * - `0` - px
+     * - `1` - %
+     */
+    private columnWidthUnits: Record<string, number> = {};
+
 
     /* *
      *
@@ -52,15 +62,96 @@ class CustomDistributionStrategy extends DistributionStrategy {
      * */
 
     public override loadColumn(column: Column): void {
+        const raw = column.options.width;
+        if (!raw) {
+            return;
+        }
 
+        let value: number;
+        let unitCode: number;
+
+        if (typeof raw === 'number') {
+            unitCode = 0;
+            value = raw;
+        } else {
+            const match = raw.match(/^(-?\d*\.?\d+)([a-z%]*)$/);
+            if (!match) {
+                return;
+            }
+
+            value = parseFloat(match[1]);
+            unitCode = match[2] === '%' ? 1 : 0;
+        }
+
+        this.columnWidthUnits[column.id] = unitCode;
+        this.columnWidths[column.id] = value;
     }
 
     public override getColumnWidth(column: Column): number {
-        return 1;
+        const vp = this.viewport;
+        const widthValue = this.columnWidths[column.id];
+        const minWidth = 20; // temp
+
+        if (!defined(widthValue)) {
+            const freeWidth =
+                vp.tbodyElement.clientWidth - this.calculateOccupiedWidth();
+            const freeColumns =
+                (vp.grid.enabledColumns?.length || 0) - 
+                Object.keys(this.columnWidths).length;
+    
+            // If undefined width:
+            return Math.max(freeWidth / freeColumns, minWidth);
+        }
+
+        if (this.columnWidthUnits[column.id] === 0) {
+            // If px:
+            return widthValue;
+        }
+
+        // If %:
+        return Math.max(vp.getWidthFromRatio(widthValue / 100), minWidth);
     }
 
     public override resize(resizer: ColumnsResizer, diff: number): void {
-        
+        const column = resizer.draggedColumn;
+        if (!column) {
+            return;
+        }
+
+        const colW = resizer.columnStartWidth ?? 0;
+        const minWidth = 20; // temp
+
+        let newW = colW + diff;
+        if (newW < minWidth) {
+            newW = minWidth;
+        }
+
+        this.columnWidths[column.id] = newW;
+        this.columnWidthUnits[column.id] = 0; // Always save in px
+    }
+
+    /**
+     * Calculates defined (px and %) widths of all defined columns in the grid.
+     * Total in px.
+     */
+    private calculateOccupiedWidth(): number {
+        const vp = this.viewport;
+        let occupiedWidth = 0;
+        let unit: number, width: number;
+
+        for (const columnId in this.columnWidths) {
+            unit = this.columnWidthUnits[columnId];
+
+            if (unit === 0) {
+                occupiedWidth += this.columnWidths[columnId];
+                continue;
+            }
+
+            width = this.columnWidths[columnId];
+            occupiedWidth += vp.getWidthFromRatio(width / 100);
+        }
+
+        return occupiedWidth;
     }
 
 }
