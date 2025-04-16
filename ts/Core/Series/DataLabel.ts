e599edaca15a150ac6aa2236d73ce38975b5b8c1
Markus Knutson Barstad <markus.knutson.barstad@highsoft.com>
2025-04-16 12:18:56 +0200

    Datalabels

--- ts/Core/Series/DataLabel.ts
+++ ts/Core/Series/DataLabel.ts
@@ -626,7 +626,7 @@ namespace DataLabel {
                                     point.formatPrefix + 'Formatter'
                                 ] ||
                                 labelOptions.formatter
-                            ).call(point, labelOptions);
+                            ).call(point, labelOptions, point);
 
                         rotation = labelOptions.rotation;
 
