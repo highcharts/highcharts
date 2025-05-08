76938db1753490fb8614a3c85818ea556fd1f122
skrulling <mikkeleb@gmail.com>
2025-05-08 14:48:06 +0200

    Demo that uses Grid to show data for Chart

---
+++ samples/grid-pro/demo/chart-show-table-grid/demo.js
@@ -0,0 +1,136 @@
+const gridElement = document.getElementById('grid');
+
+function toggleGrid() {
+    if (gridElement.classList.contains('hide')) {
+        gridElement.classList.remove('hide');
+        gridElement.classList.add('show');
+    } else {
+        gridElement.classList.remove('show');
+        gridElement.classList.add('hide');
+    }
+}
+
+const series = [{
+    name: 'Installation & Developers',
+    data: [
+        43934, 48656, 65165, 81827, 112143, 142383,
+        171533, 165174, 155157, 161454, 154610, 168960, 171558
+    ]
+}, {
+    name: 'Manufacturing',
+    data: [
+        24916, 37941, 29742, 29851, 32490, 30282,
+        38121, 36885, 33726, 34243, 31050, 33099, 33473
+    ]
+}, {
+    name: 'Sales & Distribution',
+    data: [
+        11744, 30000, 16005, 19771, 20185, 24377,
+        32147, 30912, 29243, 29213, 25663, 28978, 30618
+    ]
+}, {
+    name: 'Operations & Maintenance',
+    data: [
+        null, null, null, null, null, null, null,
+        null, 11164, 11218, 10077, 12530, 16585
+    ]
+}, {
+    name: 'Other',
+    data: [
+        21908, 5548, 8105, 11248, 8989, 11816, 18274,
+        17300, 13053, 11906, 10073, 11471, 11648
+    ]
+}];
+
+const chart = Highcharts.chart('chart', {
+
+    title: {
+        text: 'U.S Solar Employment Growth',
+        align: 'left'
+    },
+
+    subtitle: {
+        text: 'By Job Category. Source: <a href="https://irecusa.org/programs/solar-jobs-census/" target="_blank">IREC</a>.',
+        align: 'left'
+    },
+
+    yAxis: {
+        title: {
+            text: 'Number of Employees'
+        }
+    },
+
+    xAxis: {
+        accessibility: {
+            rangeDescription: 'Range: 2010 to 2022'
+        }
+    },
+
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+
+    plotOptions: {
+        series: {
+            label: {
+                connectorAllowed: false
+            },
+            pointStart: 2010
+        }
+    },
+
+    exporting: {
+        menuItemDefinitions: {
+            grid: {
+                onclick: function () {
+                    toggleGrid();
+                },
+                text: 'Toggle Grid view'
+            }
+        },
+        buttons: {
+            contextButton: {
+                menuItems: ['viewData', 'grid']
+            }
+        }
+    },
+
+    series,
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
+
+});
+
+const xAxis = chart.xAxis[0];
+const years = xAxis.tickPositions;
+
+let cols = series.reduce((acc, s) => {
+    acc[s.name] = s.data;
+    return acc;
+}, {});
+
+cols = {
+    Year: years,
+    ...cols
+};
+
+Grid.grid('grid', {
+    dataTable: {
+        columns: cols
+    }
+});
