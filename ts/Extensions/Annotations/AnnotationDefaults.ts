c4be58e197a46daa76c7350ecef5f063f158a5f2
Torstein Hønsi <torstein@highsoft.com>
2025-05-19 17:47:01 +0200

    Annotations setOptions: Docs

--- ts/Extensions/Annotations/AnnotationDefaults.ts
+++ ts/Extensions/Annotations/AnnotationDefaults.ts
@@ -19,20 +19,23 @@ const { defined } = U;
  * */
 
 /**
- * A basic type of an annotation. It allows to add custom labels
- * or shapes. The items can be tied to points, axis coordinates
- * or chart pixel coordinates.
+ * A collection of annotations to add to the chart. The basic annotation allows
+ * adding custom labels or shapes. The items can be tied to points, axis
+ * coordinates or chart pixel coordinates.
  *
- * @sample highcharts/annotations/basic/
- *         Basic annotations
- * @sample highcharts/demo/annotations/
- *         Advanced annotations
- * @sample highcharts/css/annotations
- *         Styled mode
- * @sample highcharts/annotations-advanced/controllable
- *         Controllable items
- * @sample {highstock} stock/annotations/fibonacci-retracements
- *         Custom annotation, Fibonacci retracement
+ * General options for all annotations can be set using the
+ * `Highcharts.setOptions` function. In this case only single objects are
+ * supported, because it alters the defaults for all items. For initialization
+ * in the chart constructors however, arrays of annotations are supported.
+ *
+ * See more in the [general docs](https://www.highcharts.com/docs/advanced-chart-features/annotations).
+ *
+ * @sample highcharts/annotations/basic/ Basic annotations
+ * @sample highcharts/demo/annotations/ Annotated chart
+ * @sample highcharts/css/annotations Styled mode
+ * @sample highcharts/annotations-advanced/controllable Controllable items
+ * @sample {highstock} stock/annotations/fibonacci-retracements Custom
+ *         annotation, Fibonacci retracement
  *
  * @type         {Array<*>}
  * @since        6.0.0
