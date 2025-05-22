37a57746ed947f6fc2d646f1245a55486cdb81ce
skrulling <mikkeleb@gmail.com>
2025-05-22 11:19:28 +0200

    Use fixed series for x axis

--- samples/grid-lite/demo/view-chart-data-using-grid/demo.js
+++ samples/grid-lite/demo/view-chart-data-using-grid/demo.js
@@ -1,38 +1,46 @@
-const series = [{
-    name: 'Installation & Developers',
-    data: [
-        43934, 48656, 65165, 81827, 112143, 142383,
-        171533, 165174, 155157, 161454, 154610, 168960, 171558
-    ]
-}, {
-    name: 'Manufacturing',
-    data: [
-        24916, 37941, 29742, 29851, 32490, 30282,
-        38121, 36885, 33726, 34243, 31050, 33099, 33473
-    ]
-}, {
-    name: 'Sales & Distribution',
-    data: [
-        11744, 30000, 16005, 19771, 20185, 24377,
-        32147, 30912, 29243, 29213, 25663, 28978, 30618
-    ]
-}, {
-    name: 'Operations & Maintenance',
-    data: [
-        null, null, null, null, null, null, null,
-        null, 11164, 11218, 10077, 12530, 16585
-    ]
-}, {
-    name: 'Other',
-    data: [
-        21908, 5548, 8105, 11248, 8989, 11816, 18274,
-        17300, 13053, 11906, 10073, 11471, 11648
-    ]
-}];
+const series = [
+    {
+        name: 'Year',
+        data: [
+            2010, 2011, 2012, 2013, 2014, 2015,
+            2016, 2017, 2018, 2019, 2020, 2021, 2022
+        ],
+        xAxis: 0
+    }, {
+        name: 'Installation & Developers',
+        data: [
+            43934, 48656, 65165, 81827, 112143, 142383,
+            171533, 165174, 155157, 161454, 154610, 168960, 171558
+        ]
+    }, {
+        name: 'Manufacturing',
+        data: [
+            24916, 37941, 29742, 29851, 32490, 30282,
+            38121, 36885, 33726, 34243, 31050, 33099, 33473
+        ]
+    }, {
+        name: 'Sales & Distribution',
+        data: [
+            11744, 30000, 16005, 19771, 20185, 24377,
+            32147, 30912, 29243, 29213, 25663, 28978, 30618
+        ]
+    }, {
+        name: 'Operations & Maintenance',
+        data: [
+            null, null, null, null, null, null, null,
+            null, 11164, 11218, 10077, 12530, 16585
+        ]
+    }, {
+        name: 'Other',
+        data: [
+            21908, 5548, 8105, 11248, 8989, 11816, 18274,
+            17300, 13053, 11906, 10073, 11471, 11648
+        ]
+    }];
 
 let grid = null;
 
-const chart = Highcharts.chart('chart', {
+Highcharts.chart('chart', {
 
     title: {
         text: 'U.S Solar Employment Growth',
@@ -116,10 +124,7 @@ const chart = Highcharts.chart('chart', {
 
 });
 
-const years = chart.xAxis[0].tickPositions;
-
 const cols = {
-    Year: years,
     ...Object.fromEntries(series.map(s => [s.name, s.data]))
 };
 
