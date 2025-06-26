3be6fc88b2ad44d3f1bf01898a73e2025cd8419f
Dawid Draguła <dawid.dragula@gmail.com>
2025-06-26 13:04:09 +0200

    Added more data structures to sparklines demo.

--- samples/grid-pro/demo/sparkline/demo.js
+++ samples/grid-pro/demo/sparkline/demo.js
@@ -1,16 +1,18 @@
+/* eslint-disable max-len */
 Grid.grid('container', {
     dataTable: {
         columns: {
-            // Data for the sparkline charts is stored in the Data Table
-            // as JSON strings. The sparkline renderer will parse the JSON
-            // and render the chart based on the data.
+            // Data for the sparkline charts can be provided in various ways.
+            // The simplest way is to provide it as a string of comma-separated
+            // numbers, which will be parsed by the sparkline renderer.
             line: [
-                '[1, 5, 2, 4, 3, 6]',
-                '[5, 2, 3, 4, 7, 2]',
-                '[1, 5, 2, 3, 3, 4]',
-                '[7, 5, 4, 3, 4, 6]',
-                '[1, 5, 2, 6, 3, 2]'
+                '1, 5, 2, 4, 3, 6',
+                '5, 2, 3, 4, 7, 2',
+                '1, 5, 2, 3, 3, 4',
+                '7, 5, 4, 3, 4, 6',
+                '1, 5, 2, 6, 3, 2'
             ],
+            // Data can be provided as a JSON 1D array of numbers.
             area: [
                 '[5, 2, 4, 3, 5, 2]',
                 '[1, 2, 3, 2, 4, 6]',
@@ -18,19 +20,21 @@ Grid.grid('container', {
                 '[2, 4, 3, 7, 3, 3]',
                 '[10, 9, 2, 6, 5, 7]'
             ],
+            // But it can also be provided as JSON array of objects.
             bar: [
-                '[1, 4, 2]',
-                '[1, 1, 1]',
-                '[0, 1, 5]',
-                '[0, 5, 6]',
-                '[10, 1, 10]'
+                '[{ "y": 1, "color": "#91C8E4" }, { "y": 4, "color": "#749BC2" }, { "y": 2, "color": "#4682A9" }]',
+                '[{ "y": 1, "color": "#91C8E4" }, { "y": 1, "color": "#749BC2" }, { "y": 1, "color": "#4682A9" }]',
+                '[{ "y": 2, "color": "#91C8E4" }, { "y": 1, "color": "#749BC2" }, { "y": 5, "color": "#4682A9" }]',
+                '[{ "y": 10, "color": "#91C8E4" }, { "y": 1, "color": "#749BC2" }, { "y": 10, "color": "#4682A9" }]',
+                '[{ "y": 10, "color": "#91C8E4" }, { "y": 1, "color": "#749BC2" }, { "y": 10, "color": "#4682A9" }]'
             ],
+            // Or 2D arrays, which can be interpreted using series.keys.
             column: [
-                '[1, 1, 2, 2, 3, 3, 4, 4]',
-                '[5, 3, 4, 5, 5, 4, 3, 5]',
-                '[1, 1, 6, 2, 1, 1, 5, 2]',
-                '[5, 2, 3, 5, 6, 4, 5, 5]',
-                '[1, 2, 1, 3, 2, 4, 5, 2]'
+                '[[1, "#EAA64D"], [1, "#0D5EA6"], [2, "#EAA64D"], [2, "#0D5EA6"], [3, "#EAA64D"], [3, "#0D5EA6"], [4, "#EAA64D"], [4, "#0D5EA6"]]',
+                '[[5, "#EAA64D"], [3, "#0D5EA6"], [4, "#EAA64D"], [5, "#0D5EA6"], [5, "#EAA64D"], [4, "#0D5EA6"], [3, "#EAA64D"], [5, "#0D5EA6"]]',
+                '[[1, "#EAA64D"], [1, "#0D5EA6"], [6, "#EAA64D"], [2, "#0D5EA6"], [1, "#EAA64D"], [1, "#0D5EA6"], [5, "#EAA64D"], [2, "#0D5EA6"]]',
+                '[[5, "#EAA64D"], [2, "#0D5EA6"], [3, "#EAA64D"], [5, "#0D5EA6"], [6, "#EAA64D"], [4, "#0D5EA6"], [5, "#EAA64D"], [5, "#0D5EA6"]]',
+                '[[1, "#EAA64D"], [2, "#0D5EA6"], [1, "#EAA64D"], [3, "#0D5EA6"], [2, "#EAA64D"], [4, "#0D5EA6"], [5, "#EAA64D"], [2, "#0D5EA6"]]'
             ],
             // Data can be provided differently, even in separate columns, but
             // then it needs to be handled correctly in the renderer.
@@ -95,6 +99,11 @@ Grid.grid('container', {
                 chartOptions: {
                     chart: {
                         type: 'column'
+                    },
+                    plotOptions: {
+                        column: {
+                            keys: ['y', 'color']
+                        }
                     }
                 }
             }
@@ -107,9 +116,9 @@ Grid.grid('container', {
         cells: {
             renderer: {
                 type: 'sparkline',
-                // chartOptions can be an object, or a function that returns the options.
-                // The context of the function is the cell, the first argument
-                // is the raw cell value (not used in this case).
+                // chartOptions can be an object, or a function that returns the
+                // options. The context of the function is the cell, the first
+                // argument is the raw cell value (not used in this case).
                 chartOptions: function () {
                     const rowData = this.row.data;
                     return {
@@ -170,15 +179,34 @@ Grid.grid('container', {
         }
     }]
 }, true).then(grid => {
-// The following is for demo purposes only.
+    // The following is for demo purposes only.
     // Schedule updates for the specific columns' cells. The updaters are
     // functions that modify the cell value and update its content.
     const updaters = {
-        singleBar: cell => {
-            // For the single bar chart, we just set a random value
-            // between -10 and 10. `setValue` is a method that updates the
-            // cell value and re-renders the cell content at once.
-            cell.setValue(Math.round(Math.random() * 20 - 10));
+        line: cell => {
+            // For the line charts, we just set a random value in the
+            // comma-separated string, so the updater function will have to
+            // parse it differently.
+            const arr = cell.value.split(',').map(Number);
+            arr.shift();
+            arr.push(Math.round(Math.random() * 10));
+            cell.setValue(arr.join(','));
+        },
+        default: cell => {
+            const arr = JSON.parse(cell.value);
+            const rnd = Math.round(Math.random() * 10);
+            let cachedPoint = arr.shift();
+
+            if (typeof cachedPoint === 'number') {
+                cachedPoint = rnd;
+            } else if (Array.isArray(cachedPoint)) {
+                cachedPoint[0] = rnd;
+            } else if (Object.hasOwn(cachedPoint, 'y')) {
+                cachedPoint.y = rnd;
+            }
+
+            arr.push(cachedPoint);
+            cell.setValue(JSON.stringify(arr));
         },
         pieA: cell => {
             // For the pie charts that are composed of data from multiple
@@ -194,11 +222,11 @@ Grid.grid('container', {
             // to reflect the changes in the pie chart.
             cell.content.update();
         },
-        default: cell => {
-            const arr = JSON.parse(cell.value);
-            arr.shift();
-            arr.push(Math.round(Math.random() * 10));
-            cell.setValue(JSON.stringify(arr));
+        singleBar: cell => {
+            // For the single bar chart, we just set a random value
+            // between -10 and 10. `setValue` is a method that updates the
+            // cell value and re-renders the cell content at once.
+            cell.setValue(Math.round(Math.random() * 20 - 10));
         }
     };
 
