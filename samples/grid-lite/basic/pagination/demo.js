185c31275eea3068b39e53ef29769a0930278b7b
Sebastian Bochan <sebastian@blacklabel.pl>
2025-07-09 11:52:52 +0200

    Basic demo test case.

---
+++ samples/grid-lite/basic/pagination/demo.js
@@ -0,0 +1,39 @@
+function generateRandomData(rows) {
+    const names = ['John', 'Jane', 'Alex', 'Chris', 'Katie', 'Michael'];
+    const departments = ['HR', 'Engineering', 'Sales', 'Marketing', 'Finance'];
+    const columns = {
+        ID: [],
+        Name: [],
+        Department: []
+    };
+
+    for (let i = 0; i < rows; i++) {
+        const nameIndex = Math.floor(Math.random() * names.length);
+        const departmentIndex = Math.floor(Math.random() * departments.length);
+        const id = i + 1;
+
+        columns.ID.push(id);
+        columns.Name.push(names[nameIndex]);
+        columns.Department.push(departments[departmentIndex]);
+    }
+
+    return columns;
+}
+
+Grid.grid('container', {
+    dataTable: {
+        columns: generateRandomData(100)
+    },
+    pagination: {
+        enabled: true,
+        itemsPerPage: 5,
+        events: {
+            beforePageChange: function () {
+                console.log('beforePageChange');
+            },
+            afterPageChange: function () {
+                console.log('afterPageChange');
+            }
+        }
+    }
+});
