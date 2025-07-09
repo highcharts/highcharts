391ed83d46586b216b3d6bfb89666ff807f35d3a
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2025-07-09 15:26:05 +0200

    Added setup step

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,18 +16,25 @@ export default defineConfig({
 
   projects: [
     {
+      name: 'setup',
+      testMatch: 'setup.ts',
+    },
+    {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
+      dependencies: ['setup'],
     },
 
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
+      dependencies: ['setup'],
     },
 
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
+      dependencies: ['setup'],
     },
   ],
 });
---
+++ tests/setup.ts
@@ -0,0 +1,4 @@
+import { run } from '../tools/gulptasks/lib/gulp.js';
+import '../tools/gulptasks/scripts.js';
+
+run('scripts');
