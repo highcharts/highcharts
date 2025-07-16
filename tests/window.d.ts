5049cf74e08cdb0ed65432f5f6687f93a98197b6
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2025-07-16 14:50:26 +0200

    Used esm files for Highcharts type

--- tests/window.d.ts
+++ tests/window.d.ts
@@ -1,10 +1,16 @@
-import type HCType from '../ts/Dashboards/highcharts.d.ts';
-import type Options from '../ts/Core/Options.d.ts';
+import type { default as Chart } from '../code/esm/highcharts.src.js';
+import type { default as StockChart } from '../code/esm/highstock.src.js';
+import type { default as GanttChart } from '../code/esm/highcharts-gantt.src.js';
+import type { default as MapsChart } from '../code/esm/highmaps.src.js';
 
 import type GridType from '../ts/Grid/Core/Grid.ts';
 import type DashboardsType from '../ts/Dashboards/Board.ts';
 
-type HighchartsType = typeof HCType.Chart & Options;
+type HighchartsType =
+    typeof Chart |
+    typeof StockChart |
+    typeof GanttChart |
+    typeof MapsChart;
 
 declare global {
     const Highcharts: HighchartsType;
