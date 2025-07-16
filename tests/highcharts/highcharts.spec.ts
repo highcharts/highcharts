af8459ca48b3623224138769abfe26a2d74404fb
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2025-07-16 09:26:36 +0200

    Added example highcharts and grid tests, types and eslint fixes

---
+++ tests/highcharts/highcharts.spec.ts
@@ -0,0 +1,181 @@
+import type Chart from '../../ts/Core/Chart/Chart.ts';
+import type ChartOptions from '../../ts/Core/Chart/ChartOptions.d.ts';
+
+import { test, expect } from '../fixtures.ts';
+
+test.describe('unit-tests/chart/renderto equivalent', () => {
+    test.beforeEach(async ({ page }) => {
+        const css = `
+        #container {
+            display: none;
+            height: 300px;
+            width: 300px;
+        }
+
+        #outer {
+            visibility: hidden;
+        }
+
+        #outer-outer {
+            visibility: hidden;
+            display: none;
+        }
+        `;
+
+        await page.setContent(
+            `<html>
+                <head>
+                    <style>${css}</style>
+
+                    <script src="https://code.highcharts.com/highcharts.js"></script>
+                    <script src="https://code.highcharts.com/highcharts-3d.js"></script>
+                </head>
+                <body>
+                    <div id="outer-outer">
+                        <div id="outer">
+                            <div id="container"></div>
+                        </div>
+                    </div>
+                </body>
+            </html>`,
+            { waitUntil: 'networkidle' }
+        );
+    });
+
+    test('Container initially hidden (#6693)', async ({ page }) => {
+        const height = await page.evaluate(() => {
+            var outerOuter = document.createElement('div');
+            outerOuter.style.visibility = 'hidden';
+            outerOuter.style.display = 'none';
+            document.body.appendChild(outerOuter);
+
+            var outer = document.createElement('div');
+            outer.style.visibility = 'hidden';
+            outerOuter.appendChild(outer);
+
+            var container = document.createElement('div');
+            container.style.display = 'none';
+            container.style.width = '300px';
+            container.style.height = '300px';
+            outer.appendChild(container);
+
+            var chart = Highcharts.chart(container, {
+                series: [
+                    {
+                        type: 'column',
+                        data: [1, 3, 2, 4]
+                    }
+                ]
+            });
+
+            container.style.display = 'block';
+            outer.style.display = 'block';
+            outer.style.visibility = 'visible';
+            outerOuter.style.display = 'block';
+            outerOuter.style.visibility = 'visible';
+
+            return chart.chartHeight;
+        });
+
+        expect(height).toStrictEqual(300);
+    });
+
+    test('Container originally detached (#5783)', async ({ page }) => {
+        const chart = await page.evaluateHandle(() => {
+            const c = document.createElement('div');
+            document.getElementById('container')?.appendChild(c);
+
+            c.style.width = '200px';
+            c.style.height = '200px';
+
+            return Highcharts.chart({
+                chart: {
+                    renderTo: c
+                } as unknown as ChartOptions,
+                title: {
+                    text: 'The height of the chart is set to 200px'
+                } as Chart.TitleOptions,
+                xAxis: {
+                    categories: [
+                        'Jan',
+                        'Feb',
+                        'Mar',
+                        'Apr',
+                        'May',
+                        'Jun',
+                        'Jul',
+                        'Aug',
+                        'Sep',
+                        'Oct',
+                        'Nov',
+                        'Dec'
+                    ]
+                },
+                series: [
+                    {
+                        data: [
+                            29.9,
+                            71.5,
+                            106.4,
+                            129.2,
+                            144.0,
+                            176.0,
+                            135.6,
+                            148.5,
+                            216.4,
+                            194.1,
+                            95.6,
+                            54.4
+                        ]
+                    }
+                ]
+            });
+        });
+
+        expect(await chart.evaluate(c => c.chartWidth)).toBe(200);
+
+        await chart.evaluate(prevChart => (
+            Highcharts.chart({
+                chart: { renderTo: prevChart.container } as ChartOptions,
+                title: {
+                    text: 'The second chart in the same container'
+                } as Chart.TitleOptions,
+                xAxis: {
+                    categories: [
+                        'Jan',
+                        'Feb',
+                        'Mar',
+                        'Apr',
+                        'May',
+                        'Jun',
+                        'Jul',
+                        'Aug',
+                        'Sep',
+                        'Oct',
+                        'Nov',
+                        'Dec'
+                    ]
+                },
+                series: [
+                    {
+                        data: [
+                            29.9,
+                            71.5,
+                            106.4,
+                            129.2,
+                            144.0,
+                            176.0,
+                            135.6,
+                            148.5,
+                            216.4,
+                            194.1,
+                            95.6,
+                            54.4
+                        ],
+                        type: 'column'
+                    }
+                ]
+            })
+        ));
+    });
+});
--- tests/window.d.ts
+++ tests/window.d.ts
@@ -1,8 +1,18 @@
-import HighchartsType from '../ts/Dashboards/highcharts';
-import Defaults from '../ts/Core/Defaults';
+import type HCType from '../ts/Dashboards/highcharts.d.ts';
+import type Options from '../ts/Core/Options.d.ts';
+
+import type GridType from '../ts/Grid/Core/Grid.ts';
+import type DashboardsType from '../ts/Dashboards/Board.ts';
+
+type HighchartsType = typeof HCType.Chart & Options;
 
 declare global {
+    const Highcharts: HighchartsType;
+    const Grid: typeof GridType;
+    const Dashboards: typeof DashboardsType;
+
     interface Window {
-        Highcharts: typeof HighchartsType.Chart & typeof Defaults;
+        Highcharts: HighchartsType;
+        Grid: typeof GridType;
     }
 }
