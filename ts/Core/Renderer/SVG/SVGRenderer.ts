e7432567c2dbdb9dee73cdd4f6fd80376a7e8d0a
bre1470 <40056287+bre1470@users.noreply.github.com>
2025-08-06 11:29:26 +0200

    Fixed #22807, SVGRenderer.fontMetrics throw exception with number input.

--- ts/Core/Renderer/SVG/SVGRenderer.ts
+++ ts/Core/Renderer/SVG/SVGRenderer.ts
@@ -1726,18 +1726,21 @@ class SVGRenderer implements SVGRendererLike {
      *
      * @function Highcharts.SVGRenderer#fontMetrics
      *
-     * @param {Highcharts.SVGElement|Highcharts.SVGDOMElement|number} [element]
-     *        The element to inspect for a current font size. If a number is
-     *        given, it's used as a fall back for direct font size in pixels.
+     * @param {Highcharts.SVGElement|Highcharts.SVGDOMElement|number|string} [ref]
+     *        The element to inspect for a current font size. If a number or
+     *        string is given, it's used as a fall back for direct font size in
+     *        pixels.
      *
      * @return {Highcharts.FontMetricsObject}
      *         The font metrics.
      */
     public fontMetrics(
-        element: (DOMElementType|SVGElement)
+        ref: (DOMElementType|SVGElement|number|string)
     ): FontMetricsObject {
         const f = pInt(
-            SVGElement.prototype.getStyle.call(element, 'font-size') || 0
+            isObject(ref) ?
+                (SVGElement.prototype.getStyle.call(ref, 'font-size') || 0) :
+                ref
         );
 
         // Empirical values found by comparing font size and bounding box
