115f83661e6d22a46d06c15ffb9955c06d31bbd0
MarkusBarstad <mkb@Markuss-MacBook-Pro.local>
2025-08-08 10:53:30 +0200

    Reverted test

--- samples/unit-tests/series-bubble/members/demo.js
+++ samples/unit-tests/series-bubble/members/demo.js
@@ -2,7 +2,7 @@ QUnit.test('directTouch', function (assert) {
     var bubbleSeries = Highcharts.Series.types.bubble;
     assert.strictEqual(
         bubbleSeries.prototype.noSharedTooltip,
-        false,
-        'noSharedTooltip should default to false.'
+        true,
+        'noSharedTooltip should default to true.'
     );
 });
