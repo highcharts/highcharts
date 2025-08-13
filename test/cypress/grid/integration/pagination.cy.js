e7f48a83041e67bf69fef9855a4292511c63832f
Sebastian Bochan <sebastian@blacklabel.pl>
2025-08-13 12:41:34 +0200

    Added cypress test cases, updated used sample.

--- test/cypress/grid/integration/pagination.cy.js
+++ test/cypress/grid/integration/pagination.cy.js
@@ -9,7 +9,7 @@ describe('Pagination.', () => {
 
         // Check page info is displayed with correct initial page size
         cy.get('.hcg-pagination-info').should('be.visible');
-        cy.get('.hcg-pagination-info').should('contain', 'Showing');
+        cy.get('.hcg-pagination-info').should('contain', 'Showing 1');
 
         // Check controls buttons exist
         cy.get('.hcg-pagination-controls').should('be.visible');
@@ -28,80 +28,149 @@ describe('Pagination.', () => {
         cy.get('table tbody tr').should('have.length', 21);
     });
 
-    // it('Events.', () => {
-    //     // Click on the last button
-    //     cy.get('.hcg-pagination-wrapper button').eq(1).click();
+    it('Next/previous button.', () => {
+        // Click next page button
+        cy.get('.hcg-pagination-btn[title="Next page"]').click();
 
-    //     // Check the input values
-    //     cy.get('#beforePageChange').should('have.value', '1');
-    //     cy.get('#afterPageChange').should('have.value', '2');
-        
-    //     // Click on the first button
-    //     cy.get('.hcg-pagination-wrapper button').eq(0).click();
+        // Check page info updates
+        cy.get('.hcg-pagination-info').should('contain', 'Showing 23');
 
-    //     cy.get('#beforePageChange').should('have.value', '2');
-    //     cy.get('#afterPageChange').should('have.value', '1');
-    // });
+        // Check active page button updates
+        cy.get('.hcg-pagination-page-active').should('contain', '2');
 
-    // it('Buttons states.', () => {
-    //     // First button is disabled
-    //     cy.get('.hcg-pagination-wrapper button').eq(0)
-    //         .should('be.disabled');
+        // Check data rows update
+        cy.get('table tbody tr').should('have.length', 21);
 
-    //     // Last button is enabled
-    //     cy.get('.hcg-pagination-wrapper button').eq(1)
-    //         .should('not.be.disabled')
-    //         .invoke('click')
-    //         .should('be.disabled');
+        // Check event logging
+        cy.get('#beforePageChange').should('have.value', '1');
+        cy.get('#afterPageChange').should('have.value', '2');
 
-    //     // First button is enabled
-    //     cy.get('.hcg-pagination-wrapper button').eq(0)
-    //         .should('not.be.disabled')
-    //         .invoke('click');
-    // });
+        // Click previous button
+        cy.get('.hcg-pagination-btn[title="Previous page"]').click();
 
-    // it('Sorted pagination.', () => {
-    //     // Click on the name column heade to sort
-    //     cy.get('table th[data-column-id="Name"]')
-    //         .eq(0)
-    //         .invoke('click');
+        // Check we're back on page 1
+        cy.get('.hcg-pagination-info').should('contain', 'Showing 1');
+        cy.get('.hcg-pagination-page-active').should('contain', '1');
 
-    //     // First row does not contain Michael (should be on the second page)
-    //     cy.get('table tbody tr')
-    //         .eq(0)
-    //         .should('not.contain', 'Michael');
+        // Check event logging
+        cy.get('#beforePageChange').should('have.value', '2');
+        cy.get('#afterPageChange').should('have.value', '1');
+    });
 
-    //     // Click on the next pagination button
-    //     cy.get('.hcg-pagination-wrapper button').eq(1).click();
+    it('First/last button', () => {
+        // Click last button to go to last page
+        cy.get('.hcg-pagination-btn[title="Last page"]').click();
 
-    //     cy.get('table tbody tr')
-    //         .eq(0)
-    //         .should('contain', 'Michael');
-    // });
+        // Check we're on the last page
+        cy.get('.hcg-pagination-info').should('contain', 'Showing 243 - 254 of 254');
 
-    // it('Update pagination.', () => {
-    //     cy.window().its('Grid').then((grid) => {
-    //         // Disable pagination
-    //         grid.grids[0].update({
-    //             pagination: {
-    //                 enabled: false
-    //             }
-    //         });
+        // Check button states - last button should be disabled
+        cy.get('.hcg-pagination-btn[title="Last page"]').should('be.disabled');
+        cy.get('.hcg-pagination-btn[title="Next page"]').should('be.disabled');
+        cy.get('.hcg-pagination-btn[title="First page"]').should('not.be.disabled');
+        cy.get('.hcg-pagination-btn[title="Previous page"]').should('not.be.disabled');
 
-    //         cy.get('.hcg-pagination-wrapper').should('not.exist');
-    //         cy.get('table tbody tr').should('have.length', 10);
-    //     });
+        // // Click first button to go back to first page
+        cy.get('.hcg-pagination-btn[title="First page"]').click();
+        cy.get('.hcg-pagination-btn[title="Last page"]').should('not.be.disabled');
+        cy.get('.hcg-pagination-btn[title="Next page"]').should('not.be.disabled');
 
-    //     cy.window().its('Grid').then((grid) => {
-    //         // Enable pagination
-    //         grid.grids[0].update({
-    //             pagination: {
-    //                 enabled: true
-    //             }
-    //         });
+        // // Check we're back on first page
+        cy.get('.hcg-pagination-info').should('contain', 'Showing 1 - 22 of 254');
+    });
 
-    //         cy.get('.hcg-pagination-wrapper').should('exist');
-    //         cy.get('table tbody tr').should('have.length', 6);
-    //     });
-    // });
+    it('Direct page number.', () => {
+        // Click on page number
+        cy.get('.hcg-pagination-page').contains('3').click();
+
+        // Check we're on page
+        cy.get('.hcg-pagination-info').should('contain', 'Showing 45 - 66 of 254');
+
+        // Check event logging
+        cy.get('#beforePageChange').should('have.value', '1');
+        cy.get('#afterPageChange').should('have.value', '3');
+    });
+
+    it('Page size', () => {
+        // Change page size to 10
+        cy.get('.hcg-pagination-page-size-select').eq(0).select('20');
+
+        // Check page info updates
+        cy.get('.hcg-pagination-info').should('contain', 'Showing 1 - 20');
+
+        // Check data rows update
+        cy.get('table tbody tr').should('have.length', 20);
+
+        // // Check event logging
+        cy.get('#beforePageSizeChange').should('have.value', '22');
+        cy.get('#afterPageSizeChange').should('have.value', '20');
+    });
+
+    it('Update pagination.', () => {
+        cy.window().its('Grid').then((grid) => {
+            // Disable pagination
+            grid.grids[0].update({
+                pagination: {
+                    enabled: false
+                }
+            });
+
+            cy.get('.hcg-pagination-wrapper').should('not.exist');
+        });
+
+        cy.window().its('Grid').then((grid) => {
+            // Enable pagination
+            grid.grids[0].update({
+                pagination: {
+                    enabled: true
+                }
+            });
+
+            cy.get('.hcg-pagination-wrapper').should('exist');
+            cy.get('table tbody tr').should('have.length', 20);
+        });
+    });
+
+    it('Sorted pagination.', () => {
+        cy.window().its('Grid').then((grid) => {
+            // Disable pagination
+            grid.grids[0].update({
+                pagination: {
+                    pageSize: 5
+                }
+            });
+
+            // Click on the name column heade to sort
+            cy.get('table th[data-column-id="Name"]')
+                .eq(0)
+                .invoke('click');
+
+            // First row does not contain Michael
+            cy.get('table tbody tr')
+                .eq(0)
+                .should('not.contain', 'Michael');
+        });
+    });
+
+    it('Lang support.', () => {
+        cy.window().its('Grid').then((grid) => {
+            // Disable pagination
+            grid.grids[0].update({
+                lang: {
+                    pagination: {
+                        pageInfo: 'Total pages {total}',
+                        pageSizeLabel: 'Items per page',
+                    }
+                }
+            });
+
+            cy.get('.hcg-pagination-info')
+                .eq(0)
+                .should('contain', 'Total pages');
+
+            cy.get('.hcg-pagination-page-size-container')
+                .eq(0)
+                .should('contain', 'Items per page');
+        });
+    });
 });
