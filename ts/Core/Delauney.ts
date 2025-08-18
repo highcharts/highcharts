ba5b2550733e8994c2d4c1c2780288b3a66d3a2e
MarkusBarstad <mkb@Markuss-MacBook-Pro.local>
2025-08-18 16:02:52 +0200

    Shortening a bunch

--- ts/Core/Delauney.ts
+++ ts/Core/Delauney.ts
@@ -1,280 +1,8 @@
 // === Inline 'robust-predicates' ===
 const EPSILON = 1.1102230246251565e-16;
-const splitter = 134217729;
-const resulterrbound = (3 + 8 * EPSILON) * EPSILON;
 
-/**
- *
- */
-function sum(
-    elen: number,
-    e: Float64Array<ArrayBuffer>,
-    flen: number,
-    f: Float64Array<ArrayBuffer>,
-    h: Float64Array<ArrayBuffer>
-): number {
-    let Q, Qnew, hh, bvirt,
-        enow = e[0],
-        fnow = f[0],
-        eindex = 0,
-        findex = 0,
-        hindex = 0;
 
-    if (fnow > enow === fnow > -enow) {
-        Q = enow;
-        enow = e[++eindex];
-    } else {
-        Q = fnow;
-        fnow = f[++findex];
-    }
-
-    if (eindex < elen && findex < flen) {
-        if (fnow > enow === fnow > -enow) {
-            Qnew = enow + Q;
-            hh = Q - (Qnew - enow);
-            enow = e[++eindex];
-        } else {
-            Qnew = fnow + Q;
-            hh = Q - (Qnew - fnow);
-            fnow = f[++findex];
-        }
-        Q = Qnew;
-        if (hh !== 0) {
-            h[hindex++] = hh;
-        }
-        while (eindex < elen && findex < flen) {
-            if (fnow > enow === fnow > -enow) {
-                Qnew = Q + enow;
-                bvirt = Qnew - Q;
-                hh = Q - (Qnew - bvirt) + (enow - bvirt);
-                enow = e[++eindex];
-            } else {
-                Qnew = Q + fnow;
-                bvirt = Qnew - Q;
-                hh = Q - (Qnew - bvirt) + (fnow - bvirt);
-                fnow = f[++findex];
-            }
-            Q = Qnew;
-            if (hh !== 0) {
-                h[hindex++] = hh;
-            }
-        }
-    }
-    while (eindex < elen) {
-        Qnew = Q + enow;
-        bvirt = Qnew - Q;
-        hh = Q - (Qnew - bvirt) + (enow - bvirt);
-        enow = e[++eindex];
-        Q = Qnew;
-        if (hh !== 0) {
-            h[hindex++] = hh;
-        }
-    }
-    while (findex < flen) {
-        Qnew = Q + fnow;
-        bvirt = Qnew - Q;
-        hh = Q - (Qnew - bvirt) + (fnow - bvirt);
-        fnow = f[++findex];
-        Q = Qnew;
-        if (hh !== 0) {
-            h[hindex++] = hh;
-        }
-    }
-    if (Q !== 0 || hindex === 0) {
-        h[hindex++] = Q;
-    }
-    return hindex;
-}
-
-/**
- *
- */
-function estimate(
-    elen: number,
-    e: Float64Array<ArrayBuffer>
-): number {
-    let Q = e[0];
-    for (let i = 1; i < elen; i++) {
-        Q += e[i];
-    }
-    return Q;
-}
-/**
- *
- */
-function vec(n: number): Float64Array {
-    return new Float64Array(n);
-}
-
-const ccwerrboundA = (3 + 16 * EPSILON) * EPSILON,
-    ccwerrboundB = (2 + 12 * EPSILON) * EPSILON,
-    ccwerrboundC = (9 + 64 * EPSILON) * EPSILON * EPSILON,
-    B = vec(4),
-    C1 = vec(8),
-    C2 = vec(12),
-    D = vec(16),
-    u = vec(4);
-
-/**
- *
- */
-function orient2dadapt(
-    ax: number,
-    ay: number,
-    bx: number,
-    by: number,
-    cx: number,
-    cy: number,
-    detsum: number
-): number {
-    // Const acxtail, acytail, bcxtail, bcytail;
-    let bvirt, c, ahi, alo, bhi, blo, _i, _j, _0, s1, s0, t1, t0, u32;
-    const acx = ax - cx,
-        bcx = bx - cx,
-        acy = ay - cy,
-        bcy = by - cy;
-    s1 = acx * bcy;
-    c = splitter * acx;
-    ahi = c - (c - acx);
-    alo = acx - ahi;
-    c = splitter * bcy;
-    bhi = c - (c - bcy);
-    blo = bcy - bhi;
-    s0 = alo * blo - (s1 - ahi * bhi - alo * bhi - ahi * blo);
-    t1 = acy * bcx;
-    c = splitter * acy;
-    ahi = c - (c - acy);
-    alo = acy - ahi;
-    c = splitter * bcx;
-    bhi = c - (c - bcx);
-    blo = bcx - bhi;
-    t0 = alo * blo - (t1 - ahi * bhi - alo * bhi - ahi * blo);
-    _i = s0 - t0;
-    bvirt = s0 - _i;
-    B[0] = s0 - (_i + bvirt) + (bvirt - t0);
-    _j = s1 + _i;
-    bvirt = _j - s1;
-    _0 = s1 - (_j - bvirt) + (_i - bvirt);
-    _i = _0 - t1;
-    bvirt = _0 - _i;
-    B[1] = _0 - (_i + bvirt) + (bvirt - t1);
-    u32 = _j + _i;
-    bvirt = u32 - _j;
-    B[2] = _j - (u32 - bvirt) + (_i - bvirt);
-    B[3] = u32;
-    let det = estimate(4, B);
-    let errbound = ccwerrboundB * detsum;
-    if (det >= errbound || -det >= errbound) {
-        return det;
-    }
-    bvirt = ax - acx;
-    const acxtail = ax - (acx + bvirt) + (bvirt - cx);
-    bvirt = bx - bcx;
-    const bcxtail = bx - (bcx + bvirt) + (bvirt - cx);
-    bvirt = ay - acy;
-    const acytail = ay - (acy + bvirt) + (bvirt - cy);
-    bvirt = by - bcy;
-    const bcytail = by - (bcy + bvirt) + (bvirt - cy);
-    if (acxtail === 0 && acytail === 0 && bcxtail === 0 && bcytail === 0) {
-        return det;
-    }
-    errbound = ccwerrboundC * detsum + resulterrbound * Math.abs(det);
-    det += acx * bcytail + bcy * acxtail - (acy * bcxtail + bcx * acytail);
-    if (det >= errbound || -det >= errbound) {
-        return det;
-    }
-    s1 = acxtail * bcy;
-    c = splitter * acxtail;
-    ahi = c - (c - acxtail);
-    alo = acxtail - ahi;
-    c = splitter * bcy;
-    bhi = c - (c - bcy);
-    blo = bcy - bhi;
-    s0 = alo * blo - (s1 - ahi * bhi - alo * bhi - ahi * blo);
-    t1 = acytail * bcx;
-    c = splitter * acytail;
-    ahi = c - (c - acytail);
-    alo = acytail - ahi;
-    c = splitter * bcx;
-    bhi = c - (c - bcx);
-    blo = bcx - bhi;
-    t0 = alo * blo - (t1 - ahi * bhi - alo * bhi - ahi * blo);
-    _i = s0 - t0;
-    bvirt = s0 - _i;
-    u[0] = s0 - (_i + bvirt) + (bvirt - t0);
-    _j = s1 + _i;
-    bvirt = _j - s1;
-    _0 = s1 - (_j - bvirt) + (_i - bvirt);
-    _i = _0 - t1;
-    bvirt = _0 - _i;
-    u[1] = _0 - (_i + bvirt) + (bvirt - t1);
-    u32 = _j + _i;
-    bvirt = u32 - _j;
-    u[2] = _j - (u32 - bvirt) + (_i - bvirt);
-    u[3] = u32;
-    const C1len = sum(4, B, 4, u, C1);
-    s1 = acx * bcytail;
-    c = splitter * acx;
-    ahi = c - (c - acx);
-    alo = acx - ahi;
-    c = splitter * bcytail;
-    bhi = c - (c - bcytail);
-    blo = bcytail - bhi;
-    s0 = alo * blo - (s1 - ahi * bhi - alo * bhi - ahi * blo);
-    t1 = acy * bcxtail;
-    c = splitter * acy;
-    ahi = c - (c - acy);
-    alo = acy - ahi;
-    c = splitter * bcxtail;
-    bhi = c - (c - bcxtail);
-    blo = bcxtail - bhi;
-    t0 = alo * blo - (t1 - ahi * bhi - alo * bhi - ahi * blo);
-    _i = s0 - t0;
-    bvirt = s0 - _i;
-    u[0] = s0 - (_i + bvirt) + (bvirt - t0);
-    _j = s1 + _i;
-    bvirt = _j - s1;
-    _0 = s1 - (_j - bvirt) + (_i - bvirt);
-    _i = _0 - t1;
-    bvirt = _0 - _i;
-    u[1] = _0 - (_i + bvirt) + (bvirt - t1);
-    u32 = _j + _i;
-    bvirt = u32 - _j;
-    u[2] = _j - (u32 - bvirt) + (_i - bvirt);
-    u[3] = u32;
-    const C2len = sum(C1len, C1, 4, u, C2);
-    s1 = acxtail * bcytail;
-    c = splitter * acxtail;
-    ahi = c - (c - acxtail);
-    alo = acxtail - ahi;
-    c = splitter * bcytail;
-    bhi = c - (c - bcytail);
-    blo = bcytail - bhi;
-    s0 = alo * blo - (s1 - ahi * bhi - alo * bhi - ahi * blo);
-    t1 = acytail * bcxtail;
-    c = splitter * acytail;
-    ahi = c - (c - acytail);
-    alo = acytail - ahi;
-    c = splitter * bcxtail;
-    bhi = c - (c - bcxtail);
-    blo = bcxtail - bhi;
-    t0 = alo * blo - (t1 - ahi * bhi - alo * bhi - ahi * blo);
-    _i = s0 - t0;
-    bvirt = s0 - _i;
-    u[0] = s0 - (_i + bvirt) + (bvirt - t0);
-    _j = s1 + _i;
-    bvirt = _j - s1;
-    _0 = s1 - (_j - bvirt) + (_i - bvirt);
-    _i = _0 - t1;
-    bvirt = _0 - _i;
-    u[1] = _0 - (_i + bvirt) + (bvirt - t1);
-    u32 = _j + _i;
-    bvirt = u32 - _j;
-    u[2] = _j - (u32 - bvirt) + (_i - bvirt);
-    u[3] = u32;
-    const Dlen = sum(C2len, C2, 4, u, D);
-    return D[Dlen - 1];
-}
+const ccwerrboundA = (3 + 16 * EPSILON) * EPSILON;
 
 /**
  *
@@ -294,12 +22,9 @@ function orient2d(
     if (Math.abs(det) >= ccwerrboundA * detsum) {
         return det;
     }
-    return -orient2dadapt(ax, ay, bx, by, cx, cy, detsum);
+    return 0;
 }
 
-// === Delanautor
-
-// const EPSILON = Math.pow(2, -52);
 const EDGE_STACK = new Uint32Array(512);
 
 export default class Delaunator {
@@ -509,9 +234,9 @@ export default class Delaunator {
                     d0 = d;
                 }
             }
-            this.hull = hull.subarray(0, j);
+            // This.hull = hull.subarray(0, j);
             this.triangles = new Uint32Array(0);
-            this.halfedges = new Int32Array(0);
+            // This.halfedges = new Int32Array(0);
             return;
         }
 
@@ -1041,18 +766,3 @@ function defaultGetX(p: Float64Array): number {
 function defaultGetY(p: Float64Array): number {
     return p[1];
 }
-/* === Temp for testing
-const coords = [
-    377, 479, 453, 434, 326, 387, 444, 359, 511, 389,
-    586, 429, 470, 315, 622, 493, 627, 367, 570, 314
-];
-
-const delaunay = new Delaunator(new Float64Array(coords));
-console.log(delaunay.triangles);
-
-console.log(
-    orient2d(0, 0, 1, 1, 0, 1), // Clockwise
-    orient2d(0, 0, 0, 1, 1, 1), // Counterclockwise
-    orient2d(0, 0, 0.5, 0.5, 1, 1) // Collinear
-);
-*/
