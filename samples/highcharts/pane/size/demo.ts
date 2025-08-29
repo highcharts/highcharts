a5ef1a3755f6fb4ba56cc4c58737d2558aa9d4d4
Torstein Hønsi <torstein@highsoft.com>
2025-08-29 07:51:36 +0200

    Pane auto size: Resize handle on sample

--- samples/highcharts/pane/size/demo.ts
+++ samples/highcharts/pane/size/demo.ts
@@ -1,87 +1,103 @@
-const runChart = (): void => {
+const getPane = () => {
     const endAngle = parseInt(
             document.querySelector<HTMLInputElement>('#end-angle').value,
             10
         ),
         startAngle = endAngle * -1;
-
-    Highcharts.chart('container', {
-
-        chart: {
-            plotBorderWidth: 1,
-            width: parseInt(
-                document.querySelector<HTMLInputElement>('#chart-width').value,
-                10
-            ),
-            height: parseInt(
-                document.querySelector<HTMLInputElement>('#chart-height').value,
-                10
-            )
-        },
-
-        title: {
-            text: 'Responsive pane size'
-        },
-
-        subtitle: {
-            text: `startAngle: ${startAngle}, endAngle: ${endAngle}`
-        },
-        pane: {
-            startAngle,
-            endAngle,
-            background: [{
-                outerRadius: '100%',
-                innerRadius: '60%',
-                shape: 'arc'
-            }],
-            size: undefined,
-            center: ['50%', undefined]
-        },
-
-        yAxis: {
-            min: 0,
-            max: 100,
-            labels: {
-                distance: 15
-            }
-        },
-
-        plotOptions: {
-            series: {
-                animation: false
-            },
-            gauge: {
-                dataLabels: {
-                    y: 25
-                }
-            },
-            solidgauge: {
-                dataLabels: {
-                    verticalAlign: 'middle'
-                }
-            }
-        },
-
-        series: [{
-            name: 'Speed',
-            type: 'gauge',
-            data: [80],
-            dataLabels: {
-                borderWidth: 0,
-                style: {
-                    fontSize: '1.4em'
-                },
-                backgroundColor: '#ddd8'
-            },
-            tooltip: {
-                valueSuffix: ' km/h'
-            }
-        }]
-    });
+    return { startAngle, endAngle };
 };
 
-runChart();
+const chart = Highcharts.chart('container', {
+
+    chart: {
+        backgroundColor: 'transparent',
+        plotBackgroundColor: '#fff'
+    },
+
+    title: {
+        text: 'Responsive pane size'
+    },
+
+    subtitle: {
+        text: `startAngle: ${getPane().startAngle},
+            endAngle: ${getPane().endAngle}`
+    },
+    pane: {
+        ...getPane(),
+        background: [{
+            outerRadius: '100%',
+            innerRadius: '60%',
+            shape: 'arc'
+        }],
+        size: undefined,
+        center: ['50%', undefined]
+    },
+
+    yAxis: {
+        min: 0,
+        max: 200,
+        labels: {
+            distance: 15
+        }
+    },
+
+    plotOptions: {
+        gauge: {
+            dataLabels: {
+                y: 25
+            }
+        },
+        solidgauge: {
+            dataLabels: {
+                verticalAlign: 'middle'
+            }
+        }
+    },
+
+    series: [{
+        name: 'Speed',
+        type: 'gauge',
+        data: [80],
+        dataLabels: {
+            borderWidth: 0,
+            style: {
+                fontSize: '1.4em'
+            },
+            backgroundColor: '#ddd8'
+        },
+        tooltip: {
+            valueSuffix: ' km/h'
+        }
+    }]
+});
 
 document.querySelectorAll('input[type="range"]').forEach(
-    input => input.addEventListener('input', runChart)
+    input => input.addEventListener('input', () => {
+        const pane = getPane();
+        chart.update({
+            pane,
+            subtitle: {
+                text: `startAngle: ${pane.startAngle},
+                    endAngle: ${pane.endAngle}`
+            }
+        }, undefined, undefined, false);
+    })
 );
+
+type ResizableOptions = {
+    resize: (this: HTMLElement) => void;
+}
+
+type JQueryUI = {
+    resizable: (options?: ResizableOptions) => void;
+}
+
+($('#resizer') as unknown as JQueryUI).resizable({
+    resize: function () {
+        Highcharts.charts[0].setSize(
+            this.offsetWidth - 20,
+            this.offsetHeight - 20,
+            false
+        );
+    }
+});
