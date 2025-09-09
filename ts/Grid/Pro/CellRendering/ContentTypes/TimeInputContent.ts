66d459f5e25ddc3c38ddb60449b63874ae0a7cb4
Dawid Draguła <dawid.dragula@gmail.com>
2025-09-09 18:46:58 +0200

    Overrided TimeInput options type.

--- ts/Grid/Pro/CellRendering/ContentTypes/TimeInputContent.ts
+++ ts/Grid/Pro/CellRendering/ContentTypes/TimeInputContent.ts
@@ -22,6 +22,7 @@
  * */
 
 import type { EditModeContent } from '../../CellEditing/CellEditMode.js';
+import type TimeInputRenderer from '../Renderers/TimeInputRenderer.js';
 
 import DateInputContentBase from './DateInputContentBase.js';
 
@@ -36,6 +37,8 @@ import DateInputContentBase from './DateInputContentBase.js';
  * Represents a time input type of cell content.
  */
 class TimeInputContent extends DateInputContentBase implements EditModeContent {
+
+    public override options!: TimeInputRenderer.Options;
 
     protected override getInputType(): 'time' {
         return 'time';
