f79bc5547222929d112c822737aea99b80e4d183
Nancy Dillon <nanc.dillon@gmail.com>
2025-09-10 11:37:23 -0400

    update js file

--- samples/highcharts/blog/boost-color-by-point/demo.js
+++ samples/highcharts/blog/boost-color-by-point/demo.js
@@ -1,4 +1,10 @@
 Highcharts.chart('container', {
+    title: {
+        text: 'Boosted series colored by point'
+    },
+    legend: {
+        enabled: false
+    },
     series: [{
         type: 'scatter',
         colorByPoint: true,
