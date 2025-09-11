be013b38bc38eaba4e76a75c269398685e49d158
Dawid Draguła <dawid.dragula@gmail.com>
2025-09-11 13:56:31 +0200

    Removed redundant code from benchmark.

--- test/ts-node-unit-tests/benchmarks/Shared/Delaunay.bench.ts
+++ test/ts-node-unit-tests/benchmarks/Shared/Delaunay.bench.ts
@@ -1,7 +1,6 @@
 import type { BenchmarkContext, BenchmarkResult } from '../../benchmark';
 import { performance } from 'node:perf_hooks';
 import { join } from 'node:path';
-import { generateColumnData } from '../../data-generators';
 
 export const config = {
     sizes: [10_000, 100_000, 500_000, 1_000_000]
@@ -10,27 +9,22 @@ export const config = {
 export function before(size: number) {
     return {
         fileName: `${size}-points.json`,
-        func: () => generateColumnData(size, 1)
+        func: () => new Float64Array(
+            Array.from({ length: size * 2 }, () => Math.random() * 100 - 50)
+        )
     };
 }
 
 export default async function benchmarkTest(
     {
-        size,
         CODE_PATH,
         data
     }: BenchmarkContext
 ): Promise<BenchmarkResult> {
-
     const Delaunay = require(join(CODE_PATH, '/es-modules/Shared/Delaunay.js')).default;
-    const pts = new Float32Array(
-        Array.from({ length: size * 2 }, () => Math.random() * 100 - 50)
-    );
 
     performance.mark('Start');
-
-    new Delaunay(pts);
-
+    new Delaunay(data);
     performance.mark('End');
 
     return performance.measure('Start to Now', 'Start', 'End').duration;
