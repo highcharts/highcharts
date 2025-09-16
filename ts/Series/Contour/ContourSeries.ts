d9b63ce2c4676fb50651cbc7b733decbdced1fc1
MarkusBarstad <mkb@Markuss-MacBook-Pro.local>
2025-09-16 15:32:19 +0200

    Cleaned up comments, dead funcs

--- ts/Series/Contour/ContourSeries.ts
+++ ts/Series/Contour/ContourSeries.ts
@@ -43,26 +43,6 @@ class ContourSeries extends ScatterSeries {
     private smoothColoringUniformBuffer?: GPUBuffer;
     private showContourLinesUniformBuffer?: GPUBuffer;
 
-
-    // Dummy func for test for now
-    public d(): boolean {
-        const coords = new Delaunator(new Float64Array([
-            377, 479, 453, 434, 326, 387, 444, 359, 511, 389,
-            586, 429, 470, 315, 622, 493, 627, 367, 570, 314
-        ])).triangles;
-        const tris = [
-            4, 3, 1, 4, 6, 3, 1, 5, 4, 4, 9, 6, 2, 0, 1, 1, 7, 5,
-            5, 9, 4, 6, 2, 3, 3, 2, 1, 5, 8, 9, 0, 7, 1, 5, 7, 8
-        ];
-
-        for (let i = 0; i < coords.length; i++) {
-            if (tris[i] !== coords[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public triangulateData(): any {
         const points2d: Float64Array = new Float64Array(this.points.length * 2);
 
@@ -108,63 +88,14 @@ class ContourSeries extends ScatterSeries {
 
 
     public drawPoints(): void {
-        /*
-        Const points2d: Float32Array = new Float32Array(this.points.length * 2);
-        const { xAxis, yAxis } = this;
-        const extremes = [
-            xAxis.toValue(0, true), // XMin
-            xAxis.toValue(xAxis.len, true), // XMax
-            yAxis.toValue(yAxis.len, true), // YMin
-            yAxis.toValue(0, true) // YMax
-        ];
-        let xDivider = 1,
-            yDivider = 1;
-        if (Math.abs(extremes[0]) > 10e6) {
-            xDivider = 10e6;
-        }
-        if (Math.abs(extremes[2]) > 10e6) {
-            yDivider = 10e6;
-        }
+        const series = this,
+            canvas = series.canvas = document.createElement('canvas');
 
-        this.points.forEach((point, i): void => {
-            points2d[i * 2] = point.x / xDivider;
-            points2d[i * 2 + 1] = (point?.y || 0) / yDivider;
-        });
+        // Canvas.classList.add('contourmap-canvas');
 
-        const result = new Delaunator(points2d);
-        console.log(result);
-
-
-        */
-        const series = this;
-        const canvas = series.canvas = document.createElement('canvas');
-        canvas.classList.add('contourmap-canvas');
-        // Series.chart.container.appendChild(canvas);
-
-        // const ctx = canvas.getContext('2d');
-        // if (ctx) {
-        //     // === Just to test canvas ===
-        //     ctx.fillStyle = 'blue';
-        //     ctx.fillRect(0, 0, canvas.width, canvas.height);
-        //
-        //     this.image = this.chart.renderer.image(
-        //         canvas.toDataURL('image/png', 1)
-        //     ).attr({
-        //         width: this.xAxis.len,
-        //         height: this.yAxis.len
-        //     }).add(this.group);
-        // }
-        // this.setCanvasSize();
         this.setExtremes();
-
-        // Canvas.width = this.chart.chartWidth * window.devicePixelRatio;
-        // canvas.height = this.chart.chartHeight * window.devicePixelRatio;
-
-
-        // Canvas.height = canvas.clientHeight * window.devicePixelRatio;
         this.context = canvas.getContext('webgpu');
         this.run();
-        // This.context = canvas.getContext('webgpu');
     }
 
 
@@ -191,24 +122,23 @@ class ContourSeries extends ScatterSeries {
                     format: canvasFormat
                 });
 
-                const vertices = this.get3DData();
-                const indices = this.triangulateData().triangles;
-                // Const indices = new Delaunator(new Float64Array([
-                //     377, 479, 453, 434, 326, 387, 444, 359, 511, 389,
-                //     586, 429, 470, 315, 622, 493, 627, 367, 570, 314
-                // ])).triangles;
-                //
-                const extremesUniform = this.extremesUniform = new Float32Array(
-                    this.getWebGPUExtremes()
-                );
-                const valueExtremesUniform = (
-                    this.valueExtremesUniform = new Float32Array(
-                        this.getDataExtremes()
-                    )
-                );
+                const vertices = this.get3DData(),
+                    indices = this.triangulateData().triangles,
+                    extremesUniform = this.extremesUniform = (
+                        new Float32Array(
+                            this.getWebGPUExtremes()
+                        )
+                    ),
+                    valueExtremesUniform = (
+                        this.valueExtremesUniform = (
+                            new Float32Array(
+                                this.getDataExtremes()
+                            )
+                        )
+                    ),
+                    colorAxisStops = this.getColorAxisStopsData();
 
-                const colorAxisStops = this.getColorAxisStopsData();
-
+                // WebGPU Buffers
                 const colorAxisStopsBuffer = device.createBuffer({
                     size: colorAxisStops.array.byteLength,
                     usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
