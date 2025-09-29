0bf0a6a47692de01f152cd6cc5333ab911fec9db
Sebastian Bochan <sebastian@blacklabel.pl>
2025-09-29 14:01:41 +0200

    Added Grid benchmarks.

---
+++ test/ts-node-unit-tests/benchmarks/Grid/Grid-render.bench.ts
@@ -0,0 +1,49 @@
+import type { BenchmarkContext, BenchmarkResult } from '../../benchmark';
+import { performance } from 'node:perf_hooks';
+import { join } from 'node:path';
+import { setupDOM } from '../../test-utils';
+import { generateGridData } from '../../data-generators';
+
+export const config = {
+    sizes: [1_000, 10_000, 50_000, 100_000]
+};
+
+export function before(size: number) {
+    return {
+        fileName: `${size}-rows.json`,
+        func: generateGridData.bind(undefined, size, 5) // 5 columns
+    };
+}
+
+export default async function benchmarkTest(
+    {
+        size,
+        CODE_PATH,
+        data
+    }: BenchmarkContext
+): Promise<BenchmarkResult> {
+
+    const { win, doc, el } = setupDOM();
+
+    global.window = win;
+    global.document = doc;
+    
+    // Load Grid module
+    let Grid = require(join(CODE_PATH, '/grid/grid-lite.src.js'));
+    if (typeof Grid === 'function') {
+        Grid = Grid(win); // old UMD pattern
+    } else if (!Grid.win) {
+        Grid.win = win;
+    }
+
+    performance.mark('Start');
+
+    // Create and render the Grid
+    Grid.grid(el, {
+        dataTable: data
+    });
+
+    performance.mark('End');
+
+    return performance.measure('Start to Now', 'Start', 'End').duration;
+}
