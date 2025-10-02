c16762837cb59e0249f31f50748d3c7c71b7b677
skrulling <mikkeleb@gmail.com>
2025-10-02 14:09:31 +0200

    More datagrid to grid pro

--- test/cypress/grid/integration/grid-pro/pagination.cy.js
+++ test/cypress/grid/integration/grid-pro/pagination.cy.js
@@ -5,21 +5,21 @@ describe('Pagination.', () => {
 
     it('beforePageChange / afterPageChange.', () => {
         // Click next page button
-        cy.get('.highcharts-datagrid-pagination-btn[title="Next page"]').click();
+        cy.get('.hcg-pagination-btn[title="Next page"]').click();
 
         // Check event logging
         cy.get('#beforePageChange').should('have.value', '1');
         cy.get('#afterPageChange').should('have.value', '2');
 
         // Click previous button
-        cy.get('.highcharts-datagrid-pagination-btn[title="Previous page"]').click();
+        cy.get('.hcg-pagination-btn[title="Previous page"]').click();
 
         // Check event logging
         cy.get('#beforePageChange').should('have.value', '2');
         cy.get('#afterPageChange').should('have.value', '1');
 
         // Click on page number
-        cy.get('.highcharts-datagrid-pagination-page').contains('3').click();
+        cy.get('.hcg-pagination-page').contains('3').click();
 
         // Check event logging
         cy.get('#beforePageChange').should('have.value', '1');
@@ -28,7 +28,7 @@ describe('Pagination.', () => {
 
     it('beforePageSizeChange / afterPageSizeChange.', () => {
         // Change page size to 10
-        cy.get('.highcharts-datagrid-pagination-page-size-select').eq(0).select('20');
+        cy.get('.hcg-pagination-page-size-select').eq(0).select('20');
 
         // // Check event logging
         cy.get('#beforePageSizeChange').should('have.value', '22');
