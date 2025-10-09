8654950303a051475bd0534f6b9d02f34ccb74e2
Dawid Draguła <dawid.dragula@gmail.com>
2025-10-09 13:36:23 +0200

    Extracted selector to const in cy tests.

--- test/cypress/grid/integration/column-header-toolbar.cy.js
+++ test/cypress/grid/integration/column-header-toolbar.cy.js
@@ -1,4 +1,7 @@
 describe('Column Header Toolbar', () => {
+
+    const activeButtonSelector = '.highcharts-datagrid-button.active';
+
     before(() => {
         cy.viewport(1900, 600);
         cy.visit('grid-lite/cypress/filtering');
@@ -13,12 +16,12 @@ describe('Column Header Toolbar', () => {
 
     it('One active button is present.', () => {
         cy.viewport(1900, 600);
-        cy.get('.highcharts-datagrid-button.active').should('have.length', 1);
+        cy.get(activeButtonSelector).should('have.length', 1);
     });
 
     it('Clicking filter button opens menu.', () => {
         cy.viewport(1900, 600);
-        cy.get('.highcharts-datagrid-button.active').first().click();
+        cy.get(activeButtonSelector).first().click();
         cy.get('.highcharts-datagrid-popup-content').should('have.length', 1);
     })
 
@@ -26,7 +29,7 @@ describe('Column Header Toolbar', () => {
         cy.viewport(1900, 600);
         cy.get('.highcharts-datagrid-popup-content input')
             .type('{backspace}{backspace}{backspace}{backspace}');
-        cy.get('.highcharts-datagrid-button.active').should('have.length', 0);
+        cy.get(activeButtonSelector).should('have.length', 0);
         cy.get('#container').click();
         cy.get('.highcharts-datagrid-popup-content').should('not.exist');
     });
@@ -36,18 +39,18 @@ describe('Column Header Toolbar', () => {
         cy.grid().then(grid => {
             grid.viewport.getColumn('date').sorting.setOrder('desc');
         });
-        cy.get('.highcharts-datagrid-button.active').should('have.length', 1);
+        cy.get(activeButtonSelector).should('have.length', 1);
     });
 
     it('Shrinking window minifies toolbar.', () => {
         cy.viewport(800, 600);
-        cy.get('.highcharts-datagrid-button.active')
-            .first().parent().should('have.class', 'highcharts-datagrid-header-cell-menu-icon');
+        cy.get(activeButtonSelector).first().parent()
+            .should('have.class', 'highcharts-datagrid-header-cell-menu-icon');
     });
 
     it('Clicking menu icon opens menu.', () => {
         cy.viewport(800, 600);
-        cy.get('.highcharts-datagrid-button.active').click();
+        cy.get(activeButtonSelector).click();
         cy.get('.highcharts-datagrid-popup').should('have.length', 1);
     });
 
