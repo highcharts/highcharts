30be58df03ffdf563e2de42162294e0a5d016310
MarkusBarstad <markus.knutson.barstad@highsoft.com>
2025-10-14 11:29:49 +0200

    TT defaults

--- ts/Series/Contour/ContourSeries.ts
+++ ts/Series/Contour/ContourSeries.ts
@@ -29,8 +29,6 @@ import U from '../../Core/Utilities.js';
 import ContourSeriesOptions from './ContourSeriesOptions';
 import ColorType from '../../Core/Color/ColorType.js';
 import Chart from '../../Core/Chart/Chart.js';
-import Tooltip from '../../Core/Tooltip.js';
-import { GradientColorStop } from '../../Core/Color/GradientColor.js';
 import ContourSeriesDefaults from './ContourSeriesDefaults.js';
 
 const { extend, merge } = U;
@@ -78,79 +76,10 @@ export default class ContourSeries extends ScatterSeries {
 
 
     public init(chart: Chart, options: ContourSeriesOptions): void {
-        const series = this;
-        const stops = series.colorAxis?.stops ? series.colorAxis.stops.map(
-            (stop: GradientColorStop): number[] => series.colorToArray(stop[1])
-        ) :
-            [
-                [0, 0, 0, 0],
-                [1, 1, 1, 1]
-            ];
-
-        function lerpVectors(
-            v1: number[],
-            v2: number[],
-            t: number
-        ): number[] {
-            const [v1A, v1B, v1C] = v1;
-            const [v2A, v2B, v2C] = v2;
-            const lerpColorDec = (
-                a: number,
-                b: number,
-                t: number
-            ): number => 255 * (a * (1 - t) + b * t);
-
-            return [
-                lerpColorDec(v1A, v2A, t),
-                lerpColorDec(v1B, v2B, t),
-                lerpColorDec(v1C, v2C, t)
-            ];
-        }
-
-        chart.options = merge({
-            tooltip: {
-                formatter: function (tt: Tooltip): string {
-                    const value = (
-                        (tt.chart.hoverPoint as ContourPoint).value || 0
-                    );
-                    // Should be refactored to class prop, updated with data
-                    const MAXVAL = ((): number => {
-                        let maxVal = 0;
-                        for (const p of series.points) {
-
-                            if ((p.value || 0) > maxVal) {
-                                maxVal = p.value || 0;
-                            }
-                        }
-                        return maxVal;
-                    })();
-                    const normVal = value / MAXVAL;
-
-                    let color = [1, 0, 1];
-                    for (let i = 0; i < 1; i++) {
-                        if (normVal < stops[i + 1][0]) {
-                            const t = (
-                                (normVal - stops[i][0]) /
-                                (stops[i + 1][0] - stops[i][0])
-                            );
-                            color = lerpVectors(
-                                stops[i].slice(1),
-                                stops[i + 1].slice(1),
-                                t
-                            );
-                        }
-                    }
-
-                    return `<span style="color: rgb(${
-                        color[0]
-                    }, ${
-                        color[1]
-                    }, ${
-                        color[2]
-                    });"> ● </span> Val: ${value}`;
-                }
-            }
-        }, chart.options || {});
+        chart.options = merge(
+            ContourSeries.defaultOptions,
+            chart.options || {}
+        );
 
         super.init.apply(this, [chart, options]);
 
