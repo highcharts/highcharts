64def140f163e20a0dadb179c567266d9cec12fa
Sebastian Bochan <sebastian@blacklabel.pl>
2025-10-16 13:29:41 +0200

    Replaced id with numbers.

--- samples/grid-lite/demo/grid-column-filtering/demo.js
+++ samples/grid-lite/demo/grid-column-filtering/demo.js
@@ -2,8 +2,8 @@ const grid = Grid.grid('container', {
     dataTable: {
         columns: {
             id: [
-                '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
-                '13', '14', '15', '16', '17', '18', '19', '20'
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+                19, 20
             ],
             active: [
                 true, null, false, true, true, false, null, true, false, true,
