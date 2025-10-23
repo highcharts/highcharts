261b29e3f83f026031e29976427bd81472839b7a
Kamil Musiałowski <kamil.musialowski@blacklabel.net>
2025-10-23 18:50:28 +0200

    Added controls in playground demo.

--- samples/highcharts/studies/higher-lower-ranks/demo.js
+++ samples/highcharts/studies/higher-lower-ranks/demo.js
@@ -1,13 +1,15 @@
-const data = Array.from({ length: 50 }, () => Math.random() * 10);
+function generateData(length) {
+    return Array.from({ length }, () => Math.random() * 10);
+}
 
-Highcharts.stockChart('container', {
+const chart1 = Highcharts.stockChart('container', {
     chart: {
         width: 800
     },
     series: [{
         id: 'main',
         type: 'line',
-        data,
+        data: generateData(50),
         pointInterval: 10000
     }],
     xAxis: {
@@ -19,14 +21,14 @@ Highcharts.stockChart('container', {
     }
 });
 
-Highcharts.stockChart('container1', {
+const chart2 = Highcharts.stockChart('container1', {
     chart: {
         width: 800
     },
     series: [{
         id: 'main',
         type: 'line',
-        data,
+        data: generateData(50),
         pointInterval: 2600000000
     }],
     xAxis: {
@@ -37,4 +39,32 @@ Highcharts.stockChart('container1', {
             }
         }
     }
+});
+
+const slider1 = document.getElementById('slider1');
+const slider2 = document.getElementById('slider2');
+const pointAmount1 = document.getElementById('pointAmount1');
+const pointAmount2 = document.getElementById('pointAmount2');
+slider1.addEventListener('input', function () {
+    chart1.series[0].update({
+        pointInterval: parseInt(slider1.value, 10)
+    });
+});
+
+slider2.addEventListener('input', function () {
+    chart2.series[0].update({
+        pointInterval: parseInt(slider2.value, 10)
+    });
+});
+
+pointAmount1.addEventListener('input', function () {
+    chart1.series[0].update({
+        data: generateData(parseInt(pointAmount1.value, 10))
+    });
+});
+
+pointAmount2.addEventListener('input', function () {
+    chart2.series[0].update({
+        data: generateData(parseInt(pointAmount2.value, 10))
+    });
 });
