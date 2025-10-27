6f1a34eafc46cb608fc9f43246b47442a3683659
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2025-10-27 08:17:52 +0100

    Added CodeSwitchable

--- tools/docs-preview/src/theme/MDXComponents/index.tsx
+++ tools/docs-preview/src/theme/MDXComponents/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import GridProBadge from '../../components/GridProBadge';
 import GridProBanner from '../../components/GridProBanner';
+import CodeSwitchable from '../../components/CodeSwitchable';
 
 export default {
     ...MDXComponents,
     'grid-pro-badge': GridProBadge,
     'grid-pro-banner': GridProBanner,
+    'CodeSwitchable': CodeSwitchable
 };
