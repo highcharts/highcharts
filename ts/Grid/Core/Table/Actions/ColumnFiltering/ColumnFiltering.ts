12fe383a151b88aab84ae22e4ec55a1683a67981
Dawid Draguła <dawid.dragula@gmail.com>
2025-10-28 09:53:19 +0100

    Restored proper datetime input value assigning.

--- ts/Grid/Core/Table/Actions/ColumnFiltering/ColumnFiltering.ts
+++ ts/Grid/Core/Table/Actions/ColumnFiltering/ColumnFiltering.ts
@@ -326,11 +326,14 @@ class ColumnFiltering {
         }
 
         // Assign the default input value.
-        {
-            const { value } = this.column.options.filtering ?? {};
-            if (value) {
-                this.filterInput.value = value.toString();
-            }
+        const { value } = this.column.options.filtering ?? {};
+        if (value || value === 0) {
+            this.filterInput.value = columnType === 'datetime' ?
+                column.viewport.grid.time.dateFormat(
+                    '%Y-%m-%d',
+                    Number(value)
+                ) :
+                value.toString();
         }
 
         if (this.filterSelect) {
