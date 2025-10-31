bd80b61989594cce9b0d0eaf729bc411bc172182
magdalena-gut <magdalena@blacklabel.com>
2025-10-31 12:55:24 +0100

    Reverted templating changes and added polyfill.

--- ts/masters-es5/polyfills.ts
+++ ts/masters-es5/polyfills.ts
@@ -130,3 +130,48 @@ if (typeof ElementPrototype.closest !== 'function') {
 
     window.CustomEvent = CustomEvent as any;
 })();
+
+// Replace \p{L} with language ranges and remove 'u' flag, #23462.
+(function (): void {
+    const languages =
+        'a-zA-Z' + // ASCII
+        '\u00C0-\u017F' + // Latin Extended
+        '\u0370-\u03FF' + // Greek
+        '\u0400-\u04FF\u0500-\u052F' + // Cyrillic
+        '\u0590-\u05FF' + // Hebrew
+        '\u0600-\u06FF\u0750-\u077F' + // Arabic + Extended
+        '\u0900-\u097F' + // Devanagari
+        '\u3040-\u30FF' + // Hiragana + Katakana
+        '\u3130-\u318F\uAC00-\uD7AF' + // Hangul Jamo + Syllables
+        '\u4E00-\u9FFF'; // CJK Unified Ideographs
+
+    const OriginalRegExp = RegExp;
+
+    (window as any).RegExp = function (
+        pattern: string | RegExp,
+        flags?: string
+    ): RegExp {
+        let source = typeof pattern === 'string' ? pattern : pattern.source,
+            finalFlags =
+            flags !== undefined
+                ? flags
+                : typeof pattern !== 'string'
+                    ? pattern.flags
+                    : '';
+        
+        if (source.indexOf('\\p{L}') !== -1) {
+            source = source.replace(/\\p\{L\}/g, languages);
+            if (finalFlags) {
+                finalFlags = finalFlags.replace('u', '');
+            }
+        }
+
+        if (typeof finalFlags !== 'string') {
+            finalFlags = '';
+        }
+
+        return new OriginalRegExp(source, finalFlags);
+    };
+
+    (window as any).RegExp.prototype = (OriginalRegExp as any).prototype;
+})();
