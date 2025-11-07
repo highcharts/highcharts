509a2a71792afa5de79c8d5dbb4b801eda5f9e28
Dawid Draguła <dawid.dragula@gmail.com>
2025-11-07 11:20:32 +0100

    Added cypress tests.

---
+++ samples/grid-pro/cypress/cell-after-edit/demo.js
@@ -0,0 +1,37 @@
+const viewRenderer = document.getElementById('view-renderer');
+const editModeRenderer = document.getElementById('edit-mode-renderer');
+
+Grid.grid('container', {
+    dataTable: {
+        columns: {
+            product: ['Apples', 'Pears', 'Plums', 'Bananas'],
+            cbx: [true, false, false, true],
+            price: [1.5, 2.53, 5, 4.5]
+        }
+    },
+    columns: [{
+        id: 'cbx',
+        cells: {
+            renderer: {
+                type: 'checkbox'
+            },
+            events: {
+                afterEdit: function () {
+                    viewRenderer.value = 'afterEdit';
+                }
+            }
+        }
+    }, {
+        id: 'price',
+        cells: {
+            editMode: {
+                enabled: true
+            },
+            events: {
+                afterEdit: function () {
+                    editModeRenderer.value = 'afterEdit';
+                }
+            }
+        }
+    }]
+});
