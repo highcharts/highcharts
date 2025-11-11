1130ef4ddf7ef5d7496f511c066f4591bc4840db
Markus Knutson Barstad <markusknutsonbarstad@Markuss-MacBook-Pro.local>
2025-11-11 15:19:31 +0100

    First docs

--- ts/Series/Contour/ContourSeriesDefaults.ts
+++ ts/Series/Contour/ContourSeriesDefaults.ts
@@ -1,8 +1,31 @@
-import type { PlotOptionsOf } from '../../Core/Series/SeriesOptions';
+import type ContourSeriesOptions from './ContourSeriesOptions';
 import ContourPoint from './ContourPoint';
-import type ContourSeries from './ContourSeries';
 
-const ContourSeriesDefaults: PlotOptionsOf<ContourSeries> = {
+/**
+ * A contour plot is a graphical representation of three-dimensional data
+ * in two dimensions using contour lines or color-coded regions.
+ *
+ * @productdesc {highcharts}
+ * Requires `modules/contour`.
+ *
+ * @sample highcharts/demo/contour/
+ *         Simple contour
+ *
+ * @extends      plotOptions.scatter
+ * @excluding    animationLimit, cluster, connectEnds, connectNulls,
+ *               cropThreshold, dashStyle, dragDrop, findNearestPointBy,
+ *               getExtremesFromAll, jitter, legendSymbolColor, linecap,
+ *               lineWidth, pointInterval, pointIntervalUnit, pointRange,
+ *               pointStart, shadow, softThreshold, stacking, step, threshold,
+ *               boostBlending, boostThreshold, crisp, clip, colorIndex,
+ *               inactiveOtherPoints, negativeColor, color, turboThreshold,
+ *
+ *
+ * @product      highcharts highmaps
+ * @optionparent plotOptions.contour
+ */
+
+const ContourSeriesDefaults: ContourSeriesOptions = {
     colorKey: 'value',
     marker: {
         symbol: 'cross',
@@ -30,5 +53,6 @@ const ContourSeriesDefaults: PlotOptionsOf<ContourSeries> = {
         }
     }
 };
+''; // Keeps doclets above separate
 
 export default ContourSeriesDefaults;
