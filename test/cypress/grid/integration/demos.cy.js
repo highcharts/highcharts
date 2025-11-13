384746c2f4747f5d913d76f6a2000569aa77d61b
KamilKubik <kamilkubik2904@gmail.com>
2025-11-13 08:42:00 +0100

    Added Grid demos CSS styles checks.

--- test/cypress/grid/integration/demos.cy.js
+++ test/cypress/grid/integration/demos.cy.js
@@ -2,6 +2,50 @@ const gridLiteDir = '/grid-lite/';
 const gridProDir = '/grid-pro/';
 const demoPaths = Cypress.env('demoPaths');
 
+const expectedBodyStyles = `body {
+    font-family:
+        -apple-system,
+        BlinkMacSystemFont,
+        "Segoe UI",
+        Roboto,
+        Helvetica,
+        Arial,
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        sans-serif;
+    background: var(--highcharts-background-color);
+    color: var(--highcharts-neutral-color-100);
+}`;
+
+const expectedDemoStyles = `.demo {
+    padding: 8px 12px;
+}`;
+
+const expectedDescriptionStyles = `.highcharts-description {
+    padding: 0 8px;
+}`;
+
+const validateStyles = (cssPath) => {
+    it('should contain proper body styles', () => {
+        cy.readFile(cssPath).then((cssContent) => {
+            expect(cssContent).to.include(expectedBodyStyles);
+        });
+    });
+
+    it('should contain proper .demo styles', () => {
+        cy.readFile(cssPath).then((cssContent) => {
+            expect(cssContent).to.include(expectedDemoStyles);
+        });
+    });
+
+    it('should contain proper .highcharts-description styles', () => {
+        cy.readFile(cssPath).then((cssContent) => {
+            expect(cssContent).to.include(expectedDescriptionStyles);
+        });
+    });
+};
+
 if (demoPaths && demoPaths.gridLitePaths && demoPaths.gridProPaths) {
     describe('Grid Lite demos', () => {
         demoPaths.gridLitePaths.forEach((demoPath) => {
@@ -25,6 +69,10 @@ if (demoPaths && demoPaths.gridLitePaths && demoPaths.gridProPaths) {
                         `Console errors in ${demoPath}`
                     ).to.be.empty;
                 });
+            });
+
+            describe(demoPath, () => {
+                validateStyles(gridLiteDir + 'demo/' + demoPath + '/demo.css');
             });
         });
     });
@@ -51,6 +99,10 @@ if (demoPaths && demoPaths.gridLitePaths && demoPaths.gridProPaths) {
                         `Console errors in ${demoPath}`
                     ).to.be.empty;
                 });
+            });
+
+            describe(demoPath, () => {
+                validateStyles(gridProDir + 'demo/' + demoPath + '/demo.css');
             });
         });
     });
