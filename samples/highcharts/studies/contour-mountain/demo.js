682eff0d7e4333459eb9b33bb529a47dd22778bc
Dawid Draguła <dawid.dragula@gmail.com>
2025-11-15 18:16:43 +0100

    Added dense data sample.

---
+++ samples/highcharts/studies/contour-mountain/demo.js
@@ -0,0 +1,74 @@
+const data = (() => {
+    const rawData = JSON.parse(document.getElementById('data').textContent);
+
+    const latMin = 49.05829266067379;
+    const latMax = 49.12906842129129;
+    const lonMin = 22.76165556276483;
+    const lonMax = 22.82816857111412;
+
+    const res = [];
+    for (let i = 0; i < 100; ++i) {
+        for (let j = 0; j < 100; ++j) {
+            const lat = latMin + (latMax - latMin) * i / 100;
+            const lon = lonMin + (lonMax - lonMin) * j / 100;
+            res.push([lon, lat, rawData[i * 100 + j]]);
+        }
+    }
+    return res;
+})();
+
+Highcharts.chart('container', {
+    title: {
+        text: 'Mountain elevation'
+    },
+    xAxis: {
+        tickInterval: 0.01,
+        gridLineWidth: 1,
+        title: {
+            text: 'longitude'
+        }
+    },
+    yAxis: {
+        tickInterval: 0.01,
+        gridLineWidth: 1,
+        title: {
+            text: 'latitude'
+        }
+    },
+    colorAxis: {
+        stops: [
+            [0, '#3060cf'],
+            [0.5, '#fffbbc'],
+            [0.9, '#c4463a']
+        ]
+    },
+    tooltip: {
+        pointFormat: `
+            lat: <strong>{point.x:.2f}</strong>,<br/>
+            lon: <strong>{point.y:.2f}</strong>,<br/>
+            elevation: <strong>{point.value} m</strong>
+        `
+    },
+    series: [{
+        type: 'contour',
+        name: 'Elevation',
+        contourInterval: 50,
+        data
+    }, {
+        type: 'scatter',
+        name: 'Peaks',
+        keys: ['x', 'y', 'value', 'name'],
+        data: [
+            [22.7688, 49.072, 1333, 'Halicz'],
+            [22.7702, 49.062, 1280, 'Rozsypaniec']
+        ],
+        dataLabels: {
+            enabled: true,
+            format: '{point.name}'
+        },
+        marker: {
+            symbol: 'triangle',
+            fillColor: '#000'
+        }
+    }]
+});
