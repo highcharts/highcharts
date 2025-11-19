5caf9f0566bf2adebc157c1b8c7f19b5e4afa3e7
Nancy Dillon <nanc.dillon@gmail.com>
2025-11-19 09:39:06 -0500

    Updated map demo
    Changed map demo to flow map

--- samples/highcharts/website/static-map/demo.js
+++ samples/highcharts/website/static-map/demo.js
@@ -1,201 +1,162 @@
-Math.easeInSine = function (pos) {
-    return -Math.cos(pos * (Math.PI / 2)) + 1;
-};
+(async () => {
+    const topology = await fetch(
+        'https://code.highcharts.com/mapdata/custom/europe.topo.json'
+    ).then(response => response.json());
 
-Math.easeOutQuint = function (pos) {
-    return (Math.pow((pos - 1), 5) + 1);
-};
-Math.easeOutBounce = pos => {
-    if ((pos) < (1 / 2.75)) {
-        return (7.5625 * pos * pos);
-    }
-    if (pos < (2 / 2.75)) {
-        return (7.5625 * (pos -= (1.5 / 2.75)) * pos + 0.75);
-    }
-    if (pos < (2.5 / 2.75)) {
-        return (7.5625 * (pos -= (2.25 / 2.75)) * pos + 0.9375);
-    }
-    return (7.5625 * (pos -= (2.625 / 2.75)) * pos + 0.984375);
-};
+    Highcharts.mapChart('maps', {
+        chart: {
+            map: topology
+        },
 
+        title: {
+            text: ''
+        },
 
-const finalMap = function () {
-    (async () => {
+        // subtitle: {
+        //     text: 'Highcharts Maps flow map'
+        // },
 
-        // Load the dataset
-        const data = await fetch(
-            'https://www.highcharts.com/samples/data/world-population-density.json'
-        ).then(response => response.json());
-
-        // Assign id's
-        data.forEach(function (p) {
-            p.id = p.code;
-        });
-        // Initialize the chart
-        Highcharts.mapChart('maps', {
-            chart: {
-                styledMode: true,
-                animation: {
-
-                    duration: 1000
-
-                },
-                events: {
-                    load: function () {
-                        const mapSeries = document.querySelector(
-                            '.highcharts-map-series'
-                        );
-                        const title = document.querySelector(
-                            '.highcharts-title'
-                        );
-                        const subtitle = document.querySelector(
-                            '.highcharts-subtitle'
-                        );
-                        mapSeries.style.opacity = 0;
-                        setTimeout(function () {
-                            mapSeries.style.opacity = 0;
-                            title.classList.add('fade-in');
-                            subtitle.classList.add('fade-in');
-                        }, 200);
-
-                        setTimeout(function () {
-                            mapSeries.classList.add('fade-in');
-                        }, 500);
-                    },
-                    redraw: function () {
-                        const mapSeries = document.querySelector(
-                            '.highcharts-map-series'
-                        );
-                        mapSeries.classList.add('show');
-
-                    }
-                }
-            },
-            credits: {
-                enabled: false
-            },
-            title: {
-                text: 'World Population Density',
-                style: {
-                    fontFamily: 'IBM Plex Sans',
-                    color: 'var(--text-primary)'
-                }
-            },
-            exporting: {
-                enabled: false
-            },
-            legend: {
-                title: {
-                    text: 'Population density per km²'
-                },
-                labelStyle: {
-                    color: 'var(--text-primary)'
-
-                },
-                floating: true,
-                y: 20
-            },
-            colorAxis: {
-                min: 1,
-                max: 1000,
-                type: 'logarithmic',
-                maxColor: 'var(--illo-brand-700)'
-            },
-            mapNavigation: {
-                enabled: true,
-                buttonOptions: {
-                    verticalAlign: 'bottom',
-                    padding: 10,
-                    x: 5
-                }
-            },
-            // mapView: {
-            //     center: [4100, 8280], // In terms of pre-projected units
-            //     zoom: 0.1
-            // },
-            tooltip: {
-                useHTML: true,
-                distance: -15,
-                formatter: function () {
-                    const htmlString =
-                        `<div class="tip-grid">
-                        <div class="tip-content">
-                            <div class="dot"></div>${this.point.name}
-                        </div>
-                        <i class="fas fa-caret-down tip-point"></i>
-                        </div>
-                        `;
-                    return htmlString;
-                },
-                valueSuffix: '/km²'
-            },
-            lang: {
-                accessibility: {
-                    chartContainerLabel: '',
-                    screenReaderSection: {
-                        beforeRegionLabel: '',
-                        endOfChartMarker: ''
-                    }
-                }
-            },
-            accessibility: {
-                screenReaderSection: {
-                    beforeChartFormat: '<h1>Population density</h1><p>' +
-                        'Interactive map showing population density of the ' +
-                        'world\'s countries.</p>'
-                }
-            },
-            series: [{
-                data: data,
-                accessibility: {
-                    keyboardNavigation: {
-                        enabled: false
-                    },
-                    exposeAsGroupOnly: true
-                },
-                mapData: Highcharts.maps['custom/world-highres'],
-                joinBy: ['iso-a2', 'code'],
-                name: 'Population density',
-                allowPointSelect: true,
-                cursor: 'pointer',
-                events: {
-                    click: function (e) {
-                        e.point.zoomTo();
-                    }
-                }
-
-            }],
-            responsive: {
-                rules: [{
-                    condition: {
-                        maxWidth: 400
-                    },
-                    chartOptions: {
-                        subtitle: {
-                            text: ''
-                        },
-                        chart: {
-                            margin: [40, 1, 65, 0]
-                        }
-                    }
-                },
-                {
-                    condition: {
-                        minWidth: 401
-                    },
-                    chartOptions: {
-                        subtitle: {
-                            text: 'Click a country to zoom to it.'
-                        },
-                        chart: {
-                            margin: [60, 1, 65, 0]
-                        }
-
-                    }
-                }]
+        mapNavigation: {
+            enabled: true,
+            buttonOptions: {
+                verticalAlign: 'bottom'
             }
-        });
+        },
 
-    })();
-};
-finalMap();
+        legend: {
+            enabled: false
+        },
+
+        accessibility: {
+            point: {
+                valueDescriptionFormat: '{xDescription}.'
+            }
+        },
+
+        plotOptions: {
+            mappoint: {
+                tooltip: {
+                    headerFormat: '{point.point.id}<br>',
+                    pointFormat: 'Lat: {point.lat} Lon: {point.lon}'
+                }
+            }
+        },
+
+        series: [{
+            name: 'Basemap',
+            showInLegend: false,
+            states: {
+                inactive: {
+                    enabled: false
+                }
+            }
+        }, {
+            type: 'mappoint',
+            id: 'europe',
+            name: 'Cities',
+            dataLabels: {
+                format: '{point.id}'
+            },
+            data: [{
+                id: 'Oslo',
+                lat: 59.91,
+                lon: 10.76
+            }, {
+                id: 'Warszawa',
+                lat: 52.17,
+                lon: 20.97
+            }, {
+                id: 'Paris',
+                lat: 48.73,
+                lon: 2.37
+            }, {
+                id: 'Roma',
+                lat: 41.8,
+                lon: 12.6
+            }, {
+                id: 'Madrid',
+                lat: 40.47,
+                lon: -3.57
+            }, {
+                id: 'Dublin',
+                lat: 53.43,
+                lon: -6.24
+            }, {
+                id: 'Helsinki',
+                lat: 60.32,
+                lon: 24.97
+            }, {
+                id: 'Budapest',
+                lat: 47.43,
+                lon: 19.26
+            }, {
+                id: 'Sofia',
+                lat: 42.69,
+                lon: 23.4
+            }]
+        }, {
+            type: 'flowmap',
+            name: 'Flow route',
+            accessibility: {
+                description:
+                'This is a demonstration of the flowmap using weighted links.'
+            },
+            linkedTo: ':previous',
+            minWidth: 5,
+            maxWidth: 15,
+            growTowards: true,
+            markerEnd: {
+                width: '50%',
+                height: '50%'
+            },
+            fillColor: '#31c2cc',
+            fillOpacity: 0.2,
+            color: '#2caffe',
+            data: [{
+                from: 'Oslo',
+                to: 'Helsinki',
+                weight: 20,
+                markerEnd: {
+                    width: '70%',
+                    height: '70%'
+                }
+            }, {
+                from: 'Oslo',
+                to: 'Dublin',
+                weight: 70,
+                fillOpacity: 0.7
+            }, {
+                from: 'Warszawa',
+                to: 'Helsinki',
+                weight: 10
+            }, {
+                from: 'Warszawa',
+                to: 'Paris',
+                weight: 5
+            }, {
+                from: 'Warszawa',
+                to: 'Madrid',
+                weight: 20
+            }, {
+                from: 'Warszawa',
+                to: 'Budapest',
+                weight: 0.1
+            }, {
+                from: 'Warszawa',
+                to: 'Sofia',
+                weight: 60,
+                growTowards: true,
+                fillColor: '#1cd635',
+                fillOpacity: 0.4
+            }, {
+                from: 'Warszawa',
+                to: 'Roma',
+                weight: 2,
+                markerEnd: {
+                    width: 14,
+                    height: 24
+                }
+            }]
+        }]
+    });
+})();
