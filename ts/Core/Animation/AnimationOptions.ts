e73a173945d8ebd1faefa8ca71172a2209ba80b4
bre1470 <40056287+bre1470@users.noreply.github.com>
2025-11-25 12:56:07 +0100

    Fixed DTS for AnimationOptions.

--- ts/Core/Animation/AnimationOptions.ts
+++ ts/Core/Animation/AnimationOptions.ts
@@ -22,12 +22,43 @@ import type SVGElement from '../Renderer/SVG/SVGElement';
  *
  * */
 
+/**
+ * An animation configuration. Animation configurations can also be defined as
+ * booleans, where `false` turns off animation and `true` defaults to a duration
+ * of 500ms and defer of 0ms.
+ */
 export interface AnimationOptions {
+    /**
+     * A callback function to execute when the animation finishes.
+     * @name Highcharts.AnimationOptionsObject#complete
+     * @type {Function|undefined}
+     */
     complete?: Function;
+    /** @internal */
     curAnim?: Record<string, boolean>;
+    /**
+     * The animation defer in milliseconds.
+     * @name Highcharts.AnimationOptionsObject#defer
+     * @type {number|undefined}
+     */
     defer: number;
+    /**
+     * The animation duration in milliseconds.
+     * @name Highcharts.AnimationOptionsObject#duration
+     * @type {number|undefined}
+     */
     duration: number;
+    /**
+     * The name of an easing function as defined on the `Math` object.
+     * @name Highcharts.AnimationOptionsObject#easing
+     * @type {string|Function|undefined}
+     */
     easing?: (string|Function);
+    /**
+     * A callback function to execute on each step of each attribute or CSS
+     * property that's being animated. The first argument contains information
+     * about the animation and progress.
+     */
     step?: AnimationStepCallbackFunction;
 }
 
