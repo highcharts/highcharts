63c93e2982dabfaba2aca8626843e76c2aff6724
MKB <markusknutsonbarstad@Markuss-MacBook-Pro.local>
2025-11-26 09:59:09 +0100

    Docs & removed broken unit-test

--- ts/Series/Contour/ContourSeriesOptions.d.ts
+++ ts/Series/Contour/ContourSeriesOptions.d.ts
@@ -26,10 +26,46 @@ import type ScatterSeriesOptions from '../Scatter/ScatterSeriesOptions';
  * */
 
 export default interface ContourSeriesOptions extends ScatterSeriesOptions {
+    /**
+     * Whether to use smooth color transitions.
+     *
+     * @sample {highcharts} highcharts/demo/contour/
+     *         Contour plot with smooth coloring
+     */
     smoothColoring?: boolean;
+
+    /**
+     * Whether to display contour lines.
+     *
+     * @sample {highcharts} highcharts/demo/contour/
+     *         Contour plot with lines
+     */
     showContourLines?: boolean;
+
+    /**
+     * Whether to render the series on the background, so that it is visible
+     * behind axes and grid lines. It will be also visible behind series that
+     * are rendered before it.
+     *
+     * @sample {highcharts} highcharts/demo/contour/
+     *         Contour plot rendered on background
+     */
     renderOnBackground?: boolean;
+
+    /**
+     * The interval between contour lines.
+     *
+     * @sample {highcharts} highcharts/demo/contour/
+     *         Contour plot with lines
+     */
     contourInterval?: number;
+
+    /**
+     * The offset of the contour lines.
+     *
+     * @sample {highcharts} highcharts/demo/contour/
+     *         Contour plot with lines
+     */
     contourOffset?: number;
     lineColor?: ColorType; // Kept because we might want alternate docs
 }
