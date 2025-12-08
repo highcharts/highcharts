e393b985adbbbc87edc5075afc593d3972b0ab6c
hubertkozik <hubertkozik13@gmail.com>
2025-12-08 09:56:18 +0100

    Removed files from dist script and changed to peer deps.

--- tools/gulptasks/dist-release.js
+++ tools/gulptasks/dist-release.js
@@ -187,6 +187,25 @@ function updateJSONFiles(version, files, productName) {
                         json.main.replace(/\.js$/u, '.d.ts') :
                         'highcharts.d.ts'
                 );
+
+                if (json.dependencies) {
+                    delete json.dependencies.jspdf;
+                    delete json.dependencies['svg2pdf.js'];
+                }
+
+                json.peerDependencies = Object.assign({}, json.peerDependencies, {
+                    jspdf: '^3.0.0',
+                    'svg2pdf.js': '^2.6.0'
+                });
+
+                json.peerDependenciesMeta = Object.assign(
+                    {},
+                    json.peerDependenciesMeta,
+                    {
+                        jspdf: { optional: true },
+                        'svg2pdf.js': { optional: true }
+                    }
+                );
             }
             json.version = version;
             const outputJson = JSON.stringify(json, null, '  ');
@@ -214,10 +233,10 @@ function copyFiles() {
 
     const files = {
         // 'vendor/canvg.js': join(pathToDistRepo, 'lib/canvg.js'),
-        'vendor/jspdf.js': join(pathToDistRepo, 'lib/jspdf.js'),
-        'vendor/jspdf.src.js': join(pathToDistRepo, 'lib/jspdf.src.js'),
-        'vendor/svg2pdf.js': join(pathToDistRepo, 'lib/svg2pdf.js'),
-        'vendor/svg2pdf.src.js': join(pathToDistRepo, 'lib/svg2pdf.src.js')
+        // 'vendor/jspdf.js': join(pathToDistRepo, 'lib/jspdf.js'),
+        // 'vendor/jspdf.src.js': join(pathToDistRepo, 'lib/jspdf.src.js'),
+        // 'vendor/svg2pdf.js': join(pathToDistRepo, 'lib/svg2pdf.js'),
+        // 'vendor/svg2pdf.src.js': join(pathToDistRepo, 'lib/svg2pdf.src.js')
     };
 
     const filesToIgnore = [
