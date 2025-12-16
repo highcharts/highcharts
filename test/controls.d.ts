81a4904ef27a4b6ba1481ae41618edfb22dff351
Torstein Hønsi <torstein@highsoft.com>
2025-12-16 09:13:13 +0100

    Controls: definition file

--- test/controls.d.ts
+++ test/controls.d.ts
@@ -3,33 +3,8 @@
  *  Copyright (c) Highsoft AS. All rights reserved.
  *
  *!*/
-/**
- * DemoKit class, temporarily declared literally for samples.
- */
-declare class HighchartsControls {
-    static setupBooleanHandler(
-        path: string,
-        rid: string,
-        overrideValue?: boolean
-    ): void;
-    static setupColorHandler(
-        path: string,
-        colorId: string,
-        opacityId: string,
-        valueId: string,
-        overrideValue?: string
-    ): void;
-    static setupNumberHandler(
-        path: string,
-        rid: string,
-        valueRid: string,
-        overrideValue?: number
-    ): void;
-    static setupArrayOfStringHandler(
-        path: string,
-        rid: string,
-        valueRid: string,
-        overrideValue?: string[]
-    ): void;
-    static updateOptionsPreview(): void;
+import * as Controls from '../ts/Controls/Controls';
+
+declare global {
+    const HighchartsControls: typeof Controls;
 }
