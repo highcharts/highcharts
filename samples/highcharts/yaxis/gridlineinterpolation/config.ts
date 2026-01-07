73064854c6761fb0886e3f1763c2ad4d4c69ffaf
Torstein Hønsi <torstein@highsoft.com>
2026-01-07 08:55:23 +0100

    Lint

--- samples/highcharts/yaxis/gridlineinterpolation/config.ts
+++ samples/highcharts/yaxis/gridlineinterpolation/config.ts
@@ -28,7 +28,7 @@ export default {
             tickInterval: 45
         },
         series: [{
-            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5],
+            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5]
         }]
     }
 } satisfies SampleGeneratorConfig;
