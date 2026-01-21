5162d275535f5c9b09397b6a789643aa32500251
Torstein Hønsi <torstein@highsoft.com>
2026-01-21 08:46:28 +0100

    Palette: updated samples

--- samples/highcharts/palette/general/demo.ts
+++ samples/highcharts/palette/general/demo.ts
@@ -17,7 +17,10 @@ Highcharts.chart('container', {
     palette: {
         colorScheme: 'light dark',
         light: {
-            colors: ['#2caffe', '#544fc5']
+            backgroundColor: '#ffffff',
+            colors: ['#2caffe', '#544fc5'],
+            highlightColor: '#0022ff',
+            neutralColor: '#000000'
         }
     },
     series: [{
@@ -25,4 +28,4 @@ Highcharts.chart('container', {
     }, {
         data: [5, 3, 4, 2]
     }]
-});
+} satisfies Highcharts.Options);
