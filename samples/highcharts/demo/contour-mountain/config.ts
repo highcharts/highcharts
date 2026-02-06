0badb1673c52877bbc70e0d5107455fff8a25ffb
Markus Knutson Barstad <markus@Markuss-MacBook-Air.local>
2026-02-06 09:28:40 +0100

    HC Controls

--- samples/highcharts/demo/contour-mountain/config.ts
+++ samples/highcharts/demo/contour-mountain/config.ts
@@ -4,13 +4,130 @@ import type {
 
 export default {
     controls: [{
-        path: 'contour.smoothColoring',
+        path: 'series[0].smoothColoring',
         value: false
     }, {
-        path: 'contour.lineWidth',
-        value: false
+        path: 'series[0].lineWidth',
+        value: 1,
+        min: 0,
+        max: 2,
+        step: 1
+    }, {
+        path: 'series[0].contourInterval',
+        value: 50,
+        min: 0,
+        max: 100,
+        step: 1
+    }, {
+        path: 'series[0].contourOffset',
+        value: 0,
+        min: 0,
+        max: 50,
+        step: 1
     }],
+    modules: ['modules/contour'],
     dataFile: 'contour-mountain-data.json',
     templates: [],
-    factory: 'chart'
+    factory: 'chart',
+    details: {
+        name: 'Mountain elevation with contour series',
+        authors: ['Dawid Dragula', 'Markus Knutson Barstad'],
+        tags: ['Highcharts demo'],
+        categories: [{
+            key: 'Heat and tree maps',
+            priority: 1
+        }]
+    },
+    chartOptionsExtra:{
+        chart: {
+            height: 500,
+            zooming: {
+                type: 'xy'
+            }
+        },
+        title: {
+            text: 'Mountain elevation'
+        },
+        xAxis: {
+            tickInterval: 0.01,
+            gridLineWidth: 1,
+            title: {
+                text: 'longitude'
+            },
+            gridLineColor: '#fff4',
+            gridZIndex: 4,
+            minPadding: 0,
+            maxPadding: 0,
+            endOnTick: false,
+            startOnTick: false,
+            tickWidth: 1,
+            lineWidth: 1
+        },
+        yAxis: {
+            tickInterval: 0.01,
+            gridLineWidth: 1,
+            title: {
+                text: 'latitude'
+            },
+            gridLineColor: '#fff4',
+            gridZIndex: 4,
+            minPadding: 0,
+            maxPadding: 0,
+            endOnTick: false,
+            startOnTick: false,
+            tickWidth: 1,
+            lineWidth: 1
+        },
+        colorAxis: {
+            stops: [
+                [0, '#3060cf'],
+                [0.5, '#fffbbc'],
+                [0.9, '#c4463a']
+            ]
+        },
+        tooltip: {
+            pointFormat: `
+                lat: <strong>{point.x:.2f}</strong>,<br/>
+                lon: <strong>{point.y:.2f}</strong>,<br/>
+                elevation: <strong>{point.value} m</strong>
+            `
+        },
+        series: [{
+            contourInterval: 50,
+            clip: true,
+            type: 'contour',
+            name: 'Elevation',
+            marker: {
+                states: {
+                    hover: {
+                        lineColor: 'white',
+                        lineWidth: 2
+                    }
+                }
+            },
+            states: {
+                hover: {
+                    opacity: 1
+                }
+            }
+        }, {
+            type: 'scatter',
+            name: 'Peaks',
+            keys: ['x', 'y', 'value', 'name'],
+            colorAxis: false,
+            showInLegend: false,
+            data: [
+                [22.7688, 49.072, 1333, 'Halicz'],
+                [22.7702, 49.062, 1280, 'Rozsypaniec']
+            ],
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            },
+            marker: {
+                symbol: 'triangle',
+                fillColor: '#000'
+            }
+        }]
+    }
 } satisfies SampleGeneratorConfig;
--- samples/highcharts/demo/contour-mountain/demo.ts
+++ samples/highcharts/demo/contour-mountain/demo.ts
@@ -6,6 +6,7 @@
 
     Highcharts.chart('container', {
         chart: {
+            height: 500,
             zooming: {
                 type: 'xy'
             }
@@ -14,55 +15,57 @@
             text: 'Mountain elevation'
         },
         xAxis: {
-            tickInterval: 0.01,
-            gridLineWidth: 1,
             title: {
                 text: 'longitude'
             },
-            gridLineColor: '#fff4',
-            gridZIndex: 4,
-            minPadding: 0,
-            maxPadding: 0,
             endOnTick: false,
+            gridLineColor: '#fff4',
+            gridLineWidth: 1,
+            gridZIndex: 4,
+            lineWidth: 1,
+            maxPadding: 0,
+            minPadding: 0,
             startOnTick: false,
-            tickWidth: 1,
-            lineWidth: 1
+            tickInterval: 0.01,
+            tickWidth: 1
         },
         yAxis: {
-            tickInterval: 0.01,
-            gridLineWidth: 1,
             title: {
                 text: 'latitude'
             },
-            gridLineColor: '#fff4',
-            gridZIndex: 4,
-            minPadding: 0,
-            maxPadding: 0,
             endOnTick: false,
+            gridLineColor: '#fff4',
+            gridLineWidth: 1,
+            gridZIndex: 4,
+            lineWidth: 1,
+            maxPadding: 0,
+            minPadding: 0,
             startOnTick: false,
-            tickWidth: 1,
-            lineWidth: 1
+            tickInterval: 0.01,
+            tickWidth: 1
         },
         colorAxis: {
             stops: [
-                [0, '#3060cf'],
-                [0.5, '#fffbbc'],
-                [0.9, '#c4463a']
+                [
+                    0,
+                    '#3060cf'
+                ],
+                [
+                    0.5,
+                    '#fffbbc'
+                ],
+                [
+                    0.9,
+                    '#c4463a'
+                ]
             ]
         },
-        tooltip: {
-            pointFormat: `
-                lat: <strong>{point.x:.2f}</strong>,<br/>
-                lon: <strong>{point.y:.2f}</strong>,<br/>
-                elevation: <strong>{point.value} m</strong>
-            `
-        },
         series: [{
-            contourInterval: 50,
             clip: true,
-            type: 'contour',
-            name: 'Elevation',
-            data,
+            contourInterval: 50,
+            contourOffset: 0,
+            data: data,
+            lineWidth: 1,
             marker: {
                 states: {
                     hover: {
@@ -71,31 +74,94 @@
                     }
                 }
             },
+            name: 'Elevation',
+            smoothColoring: false,
             states: {
                 hover: {
                     opacity: 1
                 }
-            }
+            },
+            type: 'contour'
         }, {
-            type: 'scatter',
-            name: 'Peaks',
-            keys: ['x', 'y', 'value', 'name'],
             colorAxis: false,
-            showInLegend: false,
             data: [
-                [22.7688, 49.072, 1333, 'Halicz'],
-                [22.7702, 49.062, 1280, 'Rozsypaniec']
+                [
+                    22.7688,
+                    49.072,
+                    1333,
+                    'Halicz'
+                ],
+                [
+                    22.7702,
+                    49.062,
+                    1280,
+                    'Rozsypaniec'
+                ]
             ],
             dataLabels: {
                 enabled: true,
                 format: '{point.name}'
             },
+            keys: ['x', 'y', 'value', 'name'],
             marker: {
-                symbol: 'triangle',
-                fillColor: '#000'
-            }
-        }]
+                fillColor: '#000',
+                symbol: 'triangle'
+            },
+            name: 'Peaks',
+            showInLegend: false,
+            type: 'scatter'
+        }],
+        tooltip: {
+            pointFormat: '\n                lat: ' +
+                   '<strong>{point.x:.2f}</strong>,<br/>\n ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   'lon: ' +
+                   '<strong>{point.y:.2f}</strong>,<br/>\n ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   'elevation: ' +
+                   '<strong>{point.value} ' +
+                   'm</strong>\n ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ' ' +
+                   ''
+        }
     });
-
 
 })();
