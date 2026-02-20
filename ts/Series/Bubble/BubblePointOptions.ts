66355d0aeae5a067f7386ae0b071a61c64edbd7b
magdalena-gut <magdalena.gut@blacklabel.net>
2026-02-20 22:03:11 +0100

    Modified Bubble.

--- ts/Series/Bubble/BubblePointOptions.ts
+++ ts/Series/Bubble/BubblePointOptions.ts
@@ -25,10 +25,20 @@ import type ScatterPointOptions from '../Scatter/ScatterPointOptions';
  * */
 
 export interface BubblePointMarkerOptions extends PointMarkerOptions {
+    /**
+     * The fill opacity of the bubble markers.
+     */
     fillOpacity?: number;
 }
 
 export interface BubblePointOptions extends ScatterPointOptions {
+    /**
+     * The size value for each bubble. The bubbles' diameters are computed
+     * based on the `z`, and controlled by series options like `minSize`,
+     * `maxSize`, `sizeBy`, `zMin` and `zMax`.
+     *
+     * @product highcharts
+     */
     z?: (number|null);
 }
 
