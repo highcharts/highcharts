05c2b0e5cbe4d04259b69e286959ea55fa319081
Kamil Musiałowski <kamil.musialowski@blacklabel.net>
2026-02-24 16:44:21 +0100

    Added requested changes.

--- ts/Series/Area/AreaSeries.ts
+++ ts/Series/Area/AreaSeries.ts
@@ -476,7 +476,7 @@ class AreaSeries extends LineSeries {
                     }
                     y = pick(y, 0);
                     if (yAxis.logarithmic && y <= 0) {
-                        y = yAxis.getThreshold(0);
+                        y = yAxis.getThreshold(series.options.threshold ?? 0);
                     } else {
                         y = yAxis.translate(// #6272
                             y, 0 as any, 1 as any, 0 as any, 1 as any
