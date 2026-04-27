f274b1d87dc850e5e5bd84b794c14c0f4aeb8426
Torstein Hønsi <torstein@highsoft.com>
2026-04-27 12:47:35 +0200

    Gauge defaults: tooltip position should heed overshoot

--- ts/Series/Gauge/GaugeSeries.ts
+++ ts/Series/Gauge/GaugeSeries.ts
@@ -501,12 +501,6 @@ class GaugeSeries extends Series {
                     point.y, void 0, void 0, void 0, true
                 );
 
-            // Positions for the tooltip
-            point.tooltipPos = [
-                center[0] + Math.cos(rotation) * radius,
-                center[1] + Math.sin(rotation) * radius
-            ];
-
             // Handle the wrap and overshoot options
             if (isNumber(overshoot) || !wrap) {
                 overshoot = isNumber(overshoot) ?
@@ -517,6 +511,12 @@ class GaugeSeries extends Series {
                     yAxis.endAngleRad + overshoot
                 );
             }
+
+            // Positions for the tooltip
+            point.tooltipPos = [
+                center[0] + Math.cos(rotation) * radius,
+                center[1] + Math.sin(rotation) * radius
+            ];
 
             rotation = rotation * 180 / Math.PI;
 
