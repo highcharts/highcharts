4cadec6d5f15b1af691b4292c97af35d92ce3b13
Torstein Hønsi <torstein@highsoft.com>
2026-05-02 09:04:38 +0200

    Basic demos: Partly bar-chart New design was too text-heavy at the bottom, while top right had large open space

--- samples/highcharts/demo/bar-chart/demo.js
+++ samples/highcharts/demo/bar-chart/demo.js
@@ -2,52 +2,59 @@ Highcharts.chart('container', {
     chart: {
         type: 'bar'
     },
-
     title: {
-        text: 'Historic World Population by Region',
-        align: 'left'
+        text: 'Historic World Population by Region'
     },
-
     subtitle: {
         text: 'Source: <a ' +
             'href="https://en.wikipedia.org/wiki/List_of_continents_and_continental_subregions_by_population"' +
-            'target="_blank">Wikipedia.org</a>',
-        align: 'left'
+            'target="_blank">Wikipedia.org</a>'
     },
-
     xAxis: {
         categories: ['Africa', 'America', 'Asia', 'Europe'],
+        title: {
+            text: null
+        },
         gridLineWidth: 1,
         lineWidth: 0
     },
-
     yAxis: {
+        min: 0,
         title: {
-            text: ''
+            text: 'Population (millions)',
+            align: 'high'
         },
         labels: {
-            format: '{value} M'
+            overflow: 'justify'
         },
-        crosshair: true,
-        gridLineWidth: 0,
-        maxPadding: 0.05,
-        endOnTick: false
+        gridLineWidth: 0
     },
-
     tooltip: {
         valueSuffix: ' millions'
     },
-
     plotOptions: {
         bar: {
+            borderRadius: '50%',
             dataLabels: {
                 enabled: true
             },
-            pointPadding: 0.05,
             groupPadding: 0.1
         }
     },
-
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'top',
+        x: -40,
+        y: 80,
+        floating: true,
+        borderRadius: 4,
+        borderWidth: 1,
+        backgroundColor: 'var(--highcharts-background-color, #ffffff)'
+    },
+    credits: {
+        enabled: false
+    },
     series: [{
         name: 'Year 1990',
         data: [632, 727, 3202, 721]
