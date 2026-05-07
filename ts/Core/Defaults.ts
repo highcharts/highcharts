af6c8b4863f40fdbe26c3f987611041769411847
ania <anna.cherry17@gmail.com>
2026-05-07 13:32:21 +0200

    Fixed #24353, wrong API sample for legend.itemDistance.

--- ts/Core/Defaults.ts
+++ ts/Core/Defaults.ts
@@ -1195,10 +1195,10 @@ const defaultOptions: DefaultOptions = {
          * In a legend with horizontal layout, the itemDistance defines the
          * pixel distance between each item.
          *
-         * @sample {highcharts} highcharts/legend/layout-horizontal/
-         *         50px item distance
-         * @sample {highstock} highcharts/legend/layout-horizontal/
-         *         50px item distance
+         * @sample {highcharts} highcharts/legend/itemwidth-default/
+         *         40px item distance
+         * @sample {highstock} highcharts/legend/itemwidth-default/
+         *         40px item distance
          *
          * @type      {number}
          * @default   {highcharts} 20
