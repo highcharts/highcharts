8b29e0f470c728ec5601d00e9c00e59ffb5716b1
Torstein Hønsi <torstein@highsoft.com>
2026-05-08 10:15:40 +0200

    Table in tooltip: regression with custom header

--- ts/Core/Tooltip.ts
+++ ts/Core/Tooltip.ts
@@ -351,14 +351,20 @@ class Tooltip {
         this: Point,
         tooltip: Tooltip
     ): (string|Array<string>) {
-        const hoverPoints = this.points || splat(this);
-        let s: (string|Array<string>);
+        const hoverPoints = this.points || splat(this),
+            body = tooltip.bodyFormatter(hoverPoints);
+        let s: (string|Array<string>),
+            header = tooltip.headerFooterFormatter(hoverPoints[0]);
 
-        // Build the header
-        s = [tooltip.headerFooterFormatter(hoverPoints[0])];
+        // Build the header. When a pre-v13 header is used with the post v13
+        // table-based body, make sure there is a table tab in the header.
+        if (body[0]?.indexOf('<tr') === 0 && header.indexOf('<table') === -1) {
+            header += '<table>';
+        }
+        s = [header];
 
         // Build the values
-        s = s.concat(tooltip.bodyFormatter(hoverPoints));
+        s = s.concat(body);
 
         // Footer
         s.push(tooltip.headerFooterFormatter(hoverPoints[0], true));
