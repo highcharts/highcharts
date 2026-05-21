078d204d2b3266168a3465d47b9223c7f558aa12
Kamil Musiałowski <kamil.musialowski@blacklabel.net>
2026-05-21 09:31:05 +0200

    Test if setInterval removal helps.

--- samples/stock/plotoptions/series-datagrouping-last-anchor/demo.js
+++ samples/stock/plotoptions/series-datagrouping-last-anchor/demo.js
@@ -1,61 +1,47 @@
-// Highcharts.stockChart('container', {
-//     chart: {
-//         height: 600,
-//         events: {
-//             load: function () {
-//                 const chart = this,
-//                     rawSeries = chart.series[0],
-//                     groupedSeries = chart.series[1];
-
-//                 setInterval(function () {
-//                     const randomNumber = Math.random() * 5;
-
-//                     rawSeries.addPoint(randomNumber, false);
-//                     groupedSeries.addPoint(randomNumber);
-//                 }, 1000);
-//             }
-//         }
-//     },
-//     yAxis: [{
-//         height: '50%',
-//         offset: 0,
-//         min: 0,
-//         max: 5,
-//         title: {
-//             text: 'Raw data'
-//         }
-//     }, {
-//         height: '50%',
-//         top: '50%',
-//         offset: 0,
-//         min: 0,
-//         max: 5,
-//         title: {
-//             text: 'Last anchor set to last point'
-//         }
-//     }],
-//     plotOptions: {
-//         series: {
-//             pointStart: '2025-01-01',
-//             pointInterval: 24 * 36e5
-//         }
-//     },
-//     series: [{
-//         data: [5, 4, 4, 3, 5, 3, 2, 1, 2, 3, 1, 2, 2, 1, 3],
-//         dataGrouping: {
-//             enabled: false
-//         }
-//     }, {
-//         data: [4, 3, 4, 3, 5, 3, 2, 1, 2, 3, 1, 2, 2, 1, 3],
-//         yAxis: 1,
-//         dataGrouping: {
-//             approximation: 'average',
-//             enabled: true,
-//             forced: true,
-//             units: [
-//                 ['week', [1]]
-//             ],
-//             lastAnchor: 'lastPoint'
-//         }
-//     }]
-// });
+Highcharts.stockChart('container', {
+    chart: {
+        height: 600
+    },
+    yAxis: [{
+        height: '50%',
+        offset: 0,
+        min: 0,
+        max: 5,
+        title: {
+            text: 'Raw data'
+        }
+    }, {
+        height: '50%',
+        top: '50%',
+        offset: 0,
+        min: 0,
+        max: 5,
+        title: {
+            text: 'Last anchor set to last point'
+        }
+    }],
+    plotOptions: {
+        series: {
+            pointStart: '2025-01-01',
+            pointInterval: 24 * 36e5
+        }
+    },
+    series: [{
+        data: [5, 4, 4, 3, 5, 3, 2, 1, 2, 3, 1, 2, 2, 1, 3],
+        dataGrouping: {
+            enabled: false
+        }
+    }, {
+        data: [4, 3, 4, 3, 5, 3, 2, 1, 2, 3, 1, 2, 2, 1, 3],
+        yAxis: 1,
+        dataGrouping: {
+            approximation: 'average',
+            enabled: true,
+            forced: true,
+            units: [
+                ['week', [1]]
+            ],
+            lastAnchor: 'lastPoint'
+        }
+    }]
+});
