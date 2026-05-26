8ce9d33e6d749f0fcccf7fe0655de96242369935
Markus Knutson Barstad <mkb@Markus-sin-MacBook-Pro.local>
2026-05-26 13:53:14 +0200

    Preserve tt formats

--- samples/unit-tests/pointer/members/demo.js
+++ samples/unit-tests/pointer/members/demo.js
@@ -344,3 +344,122 @@ QUnit.test(
         });
     }
 );
+
+QUnit.test(
+    'Scatter-like series use grouped tooltip defaults for shared and split ' +
+    'tooltips without overriding explicit formats',
+    function (assert) {
+        var sharedHeaderFormat =
+                Highcharts.defaultOptions.tooltip.headerFormat,
+            sharedScatterPointFormat =
+                '<span style="color:{point.color}">\u25CF</span> ' +
+                '{series.name}: <b>{point.y}</b><br/>',
+            sharedBubblePointFormat =
+                '<span style="color:{point.color}">\u25CF</span> ' +
+                '{series.name}: <b>{point.y}</b>, Size: <b>{point.z}</b>' +
+                '<br/>',
+            scatterHeaderFormat =
+                '<span style="color:{point.color}">\u25CF</span> ' +
+                '<span style="font-size: 0.8em"> {series.name}</span><br/>',
+            scatterPointFormat =
+                'x: <b>{point.x}</b><br/>y: <b>{point.y}</b><br/>',
+            cases = [
+                {
+                    type: 'scatter',
+                    tooltip: { shared: true },
+                    expectedPointFormat: sharedScatterPointFormat
+                },
+                {
+                    type: 'scatter',
+                    tooltip: { split: true },
+                    expectedPointFormat: sharedScatterPointFormat
+                },
+                {
+                    type: 'bubble',
+                    tooltip: { shared: true },
+                    expectedPointFormat: sharedBubblePointFormat
+                },
+                {
+                    type: 'bubble',
+                    tooltip: { split: true },
+                    expectedPointFormat: sharedBubblePointFormat
+                }
+            ];
+
+        cases.forEach(function (testCase) {
+            var chart = Highcharts.chart('container', {
+                    tooltip: testCase.tooltip,
+                    series: [{
+                        type: testCase.type,
+                        data: testCase.type === 'bubble' ?
+                            [[0, 2, 3], [1, 4, 5]] :
+                            [[0, 2], [1, 4]]
+                    }]
+                }),
+                series = chart.series[0],
+                mode = Object.keys(testCase.tooltip)[0];
+
+            assert.strictEqual(
+                series.tooltipOptions.headerFormat,
+                sharedHeaderFormat,
+                testCase.type + ' should use the grouped header in ' + mode +
+                ' mode'
+            );
+            assert.strictEqual(
+                series.tooltipOptions.pointFormat,
+                testCase.expectedPointFormat,
+                testCase.type + ' should use grouped point formatting in ' +
+                mode + ' mode'
+            );
+
+            chart.destroy();
+        });
+
+        var singleScatterChart = Highcharts.chart('container', {
+            series: [{
+                type: 'scatter',
+                data: [[0, 2], [1, 4]]
+            }]
+        });
+
+        assert.strictEqual(
+            singleScatterChart.series[0].tooltipOptions.headerFormat,
+            scatterHeaderFormat,
+            'Scatter should keep its single-point header by default'
+        );
+        assert.strictEqual(
+            singleScatterChart.series[0].tooltipOptions.pointFormat,
+            scatterPointFormat,
+            'Scatter should keep its single-point body by default'
+        );
+
+        singleScatterChart.destroy();
+
+        var sharedScatterChart = Highcharts.chart('container', {
+            tooltip: {
+                shared: true
+            },
+            series: [{
+                type: 'scatter',
+                data: [[0, 2], [1, 4]],
+                tooltip: {
+                    headerFormat: 'custom header',
+                    pointFormat: 'custom point'
+                }
+            }]
+        });
+
+        assert.strictEqual(
+            sharedScatterChart.series[0].tooltipOptions.headerFormat,
+            'custom header',
+            'Scatter should preserve explicit grouped header formats'
+        );
+        assert.strictEqual(
+            sharedScatterChart.series[0].tooltipOptions.pointFormat,
+            'custom point',
+            'Scatter should preserve explicit grouped point formats'
+        );
+
+        sharedScatterChart.destroy();
+    }
+);
