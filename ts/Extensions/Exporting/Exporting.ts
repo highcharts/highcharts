850fb9e3cdcacc526ca866904bb2213216e45608
Kamil Musiałowski <kamil.musialowski@blacklabel.net>
2026-06-24 20:26:38 +0200

    Comment.

--- ts/Extensions/Exporting/Exporting.ts
+++ ts/Extensions/Exporting/Exporting.ts
@@ -831,7 +831,7 @@ export class Exporting {
         options?: Options
     ): string {
         // Remove any HTML added to the container after the SVG, like the
-        // Stock Tools GUI wrapper
+        // Stock Tools GUI wrapper, #24754
         const split = svg.indexOf('</svg>');
         if (split > -1) {
             svg = svg.substr(0, split + 6);
