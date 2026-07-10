ea64f4947e240de6c71e2295699e21d7304501f0
LBoge <99801154+LB0ge@users.noreply.github.com>
2026-07-10 12:39:00 +0200

    Fixed node/no-extraneous-import lint failure in heatmap demo.

--- samples/highcharts/demo/heatmap/react/types.ts
+++
@@ -1,5 +0,0 @@
-import type Highcharts from 'highcharts/es-modules/masters/highcharts.src.js';
-
-export type HighchartsWithTemplating = typeof Highcharts & {
-    Templating: { helpers: Record<string, (...args: unknown[]) => unknown> };
-};
