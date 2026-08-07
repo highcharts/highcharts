highcharts/highcharts

commit 370f361efcbe70960a48f1ac09efcdf4b1c0f2b4

Feature: animate without owner element

ts/Core/Animation/AnimationUtilities.ts
@@ -151,6 +151,11 @@ export function getDeferredAnimation(
 /**
  * The global animate method, which uses Fx to create individual animators.
  *
+ * @sample highcharts/members/renderer-basic
+ *         SVG elements with animation
+ * @sample highcharts/members/animate
+ *         Animation without an owner element
+ *
  * @function Highcharts.animate
  *
  * @param {Highcharts.HTMLDOMElement|Highcharts.SVGElement} el
@@ -167,22 +172,15 @@ export function getDeferredAnimation(
  * @return {void}
  */
 export function animate(
-    el: (HTMLDOMElement|SVGElement),
-    params: (CSSObject|SVGAttributes),
+    el?: (HTMLDOMElement|SVGElement),
+    params: (CSSObject|SVGAttributes|{ pos: number }) = { pos: 1 },
     opt?: boolean|Partial<AnimationOptions>
 ): void {
-    let start,
-        unit = '',
-        end,
-        fx,
-        args;
-
     if (!isObject(opt)) { // Number or undefined/null
-        args = arguments;
         opt = {
-            duration: args[2],
-            easing: args[3],
-            complete: args[4]
+            duration: arguments[2],
+            easing: arguments[3],
+            complete: arguments[4]
         };
     }
     if (!isNumber(opt.duration)) {
@@ -193,26 +191,31 @@ export function animate(
         (Math[opt.easing as keyof Math] || Math.easeInOutSine) as any;
     opt.curAnim = merge(params) as any;
 
-    objectEach(params, function (val, prop): void {
+    objectEach(params, (val: string|number, prop: string): void => {
         // Stop current running animation of this property
-        stop(el as any, prop);
+        if (el) {
+            stop(el, prop);
+        }
 
-        fx = new Fx(el as any, opt as any, prop);
-        end = void 0;
+        const fx = new Fx(el, opt, prop),
+            d = (params as SVGAttributes).d;
 
-        if ((prop as any) === 'd' && isArray((params as any).d)) {
+        let start: number|string = 0,
+            end: number|string|undefined = void 0,
+            unit = '';
+
+        if (prop === 'd' && isArray(d)) {
             fx.paths = fx.initPath(
-                el as any,
-                (el as any).pathArray,
-                (params as any).d
+                el as SVGElement,
+                (el as SVGElement).pathArray,
+                d
             );
-            fx.toD = (params as any).d;
-            start = 0;
+            fx.toD = d;
             end = 1;
-        } else if ((el as any).attr) {
-            start = (el as any).attr(prop);
-        } else {
-            start = parseFloat(getStyle(el as any, prop) as any) || 0;
+        } else if ((el as SVGElement)?.attr) {
+            start = (el as SVGElement).attr(prop);
+        } else if (el) {
+            start = +(getStyle(el as HTMLElement, prop) || 0);
             if (prop !== 'opacity') {
                 unit = 'px';
             }
@@ -249,16 +252,12 @@ export function animate(
  * improvement in all cases where we stop the animation from .attr. Instead of
  * stopping everything, we can just stop the actual attributes we're setting.
  */
-export function stop(el: SVGElement|HTMLElement, prop?: string): void {
-    let i = Fx.timers.length;
-
-    // Remove timers related to this element (#4519)
-    while (i--) {
-        if (Fx.timers[i].elem === el && (!prop || prop === Fx.timers[i].prop)) {
-            Fx.timers[i].stopped = true; // #4667
+export const stop = (el: SVGElement|HTMLElement, prop?: string): void =>
+    Fx.timers.forEach((timer): void => {
+        if (timer.elem === el && (!prop || prop === timer.prop)) {
+            timer.stopped = true; // #4667
         }
-    }
-}
+    });
 
 /* *
  *

samples/highcharts/members/animate/demo.css
@@ -1,0 +1,30 @@
+body {
+    font-family:
+        -apple-system,
+        BlinkMacSystemFont,
+        "Segoe UI",
+        Roboto,
+        Helvetica,
+        Arial,
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        sans-serif;
+}
+
+#container {
+    width: 320px;
+    height: 320px;
+    margin: 2em auto;
+    font-size: 5em;
+}
+
+.highcharts-description {
+    background: light-dark(#f2f6f2, #323232);
+    border-radius: 0 4px 4px 0;
+    border-left: 2px solid light-dark(#1d6aff, #fff);
+    font-size: 0.8rem;
+    font-style: italic;
+    padding: 8px 16px;
+    margin: 2em 10px 1em;
+}

samples/highcharts/members/animate/demo.details
@@ -1,0 +1,6 @@
+---
+ name: Free animation
+ authors:
+   - Torstein Hønsi
+ js_wrap: b
+...

samples/highcharts/members/animate/demo.html
@@ -1,0 +1,8 @@
+<script src="https://code.highcharts.com/highcharts.js"></script>
+
+<div id="container">0</div>
+
+<p class="highcharts-description">
+    This demo shows how to run a custom animation without a HTML- or SVG
+    element, through the step callback.
+</p>

samples/highcharts/members/animate/demo.js
@@ -1,0 +1,9 @@
+// Custom animation without an owner element
+Highcharts.animate(undefined, undefined, {
+    // The step function. The `pos` argument animates from 0 to 1.
+    step: pos => {
+        document.getElementById('container').innerText =
+            `${(pos * 100).toFixed(1)}%`;
+    },
+    duration: 2000
+});

samples/unit-tests/svgrenderer/animate/demo.js
@@ -971,3 +971,34 @@ QUnit.test('#14351: Tweening NaN path', assert => {
         'It should not attempt to tween NaN values'
     );
 });
+
+QUnit.test('Animation without an owner element', function (assert) {
+    const clock = TestUtilities.lolexInstall();
+
+    let currentNow = 0;
+
+    try {
+        Highcharts.animate(
+            undefined,
+            undefined,
+            {
+                duration: 100,
+                step: now => {
+                    currentNow = now;
+                }
+            }
+        );
+
+        setTimeout(() => {
+            assert.ok(
+                currentNow > 0.4 && currentNow < 0.6,
+                'Animation should run without an owner element'
+            );
+        }, 50);
+
+        // Reset animation
+        TestUtilities.lolexRunAndUninstall(clock);
+    } finally {
+        TestUtilities.lolexUninstall(clock);
+    }
+});

ts/Core/Animation/AnimationOptions.ts
@@ -15,6 +15,7 @@
  *
  * */
 
+import type { HTMLDOMElement } from '../Renderer/DOMElementType';
 import type SVGElement from '../Renderer/SVG/SVGElement';
 
 /* *
@@ -64,7 +65,7 @@ export interface AnimationOptions {
 }
 
 export interface AnimationStepCallbackFunction {
-    (this: SVGElement, ...args: Array<any>): void;
+    (this: HTMLDOMElement|SVGElement|undefined, ...args: Array<any>): void;
 }
 
 /* *

ts/Core/Animation/Fx.ts
@@ -21,7 +21,6 @@
 import type AnimationOptions from './AnimationOptions';
 import type FxBase from './FxBase';
 import type { HTMLDOMElement } from '../Renderer/DOMElementType';
-import type HTMLElement from '../Renderer/HTML/HTMLElement';
 import type SVGElement from '../Renderer/SVG/SVGElement';
 import type SVGPath from '../Renderer/SVG/SVGPath';
 
@@ -79,7 +78,7 @@ class Fx {
      * */
 
     public constructor(
-        elem: (HTMLElement|SVGElement),
+        elem: HTMLDOMElement|SVGElement|undefined,
         options: Partial<AnimationOptions>,
         prop: string
     ) {
@@ -98,7 +97,7 @@ class Fx {
      * The element to animate.
      * @internal
      */
-    public elem: (HTMLElement|SVGElement);
+    public elem?: HTMLDOMElement|SVGElement;
 
     /**
      * The end value, value to land on.
@@ -226,7 +225,7 @@ class Fx {
             path = end;
         }
 
-        this.elem.attr('d', path, void 0, true);
+        (this.elem as SVGElement).attr('d', path, void 0, true);
     }
 
     /**
@@ -238,7 +237,7 @@ class Fx {
     public update(): void {
         const elem = this.elem,
             prop = this.prop, // If destroyed, it is null
-            now: number = this.now as any,
+            now: number = this.now ?? 1,
             step = this.options.step;
 
         // Animation setter defined from outside
@@ -246,19 +245,17 @@ class Fx {
             (this as any)[prop + 'Setter']();
 
         // Other animations on SVGElement
-        } else if (elem.attr) {
-            if (elem.element) {
-                elem.attr(prop, now, null as any, true);
+        } else if (elem && (elem as SVGElement).attr) {
+            if ((elem as SVGElement).element) {
+                (elem as SVGElement).attr(prop, now, void 0, true);
             }
 
         // HTML styles, raw HTML content like container size
-        } else {
-            elem.style[prop as any] = now + (this.unit as any);
+        } else if (elem) {
+            elem.style[prop] = now + (this.unit || '');
         }
 
-        if (step) {
-            step.call(elem, now, this);
-        }
+        step?.call(elem, now, this);
 
     }
 
@@ -278,17 +275,16 @@ class Fx {
      *
      */
     public run(from: number, to: number, unit: string): void {
-        const self = this,
-            options = self.options,
-            timer: Fx.Timer = function (gotoEnd?: boolean): boolean {
-                return timer.stopped ? false : self.step(gotoEnd);
-            },
+        const { elem, options } = this,
+            { complete, curAnim = {} } = options,
+            timer: Fx.Timer = (gotoEnd?: boolean): boolean =>
+                (timer.stopped ? false : this.step(gotoEnd)),
             requestAnimationFrame =
                 win.requestAnimationFrame ||
                 function (step: Function): void {
                     setTimeout(step, 13);
                 },
-            step = function (): void {
+            step = (): void => {
                 for (let i = 0; i < Fx.timers.length; i++) {
                     if (!Fx.timers[i]()) {
                         Fx.timers.splice(i--, 1);
@@ -300,13 +296,13 @@ class Fx {
                 }
             };
 
-        if (from === to && !this.elem['forceAnimate:' + this.prop]) {
-            delete (options.curAnim as any)[this.prop];
+        if (from === to && !(elem as SVGElement)['forceAnimate:' + this.prop]) {
+            delete curAnim[this.prop];
             if (
-                options.complete &&
-                Object.keys(options.curAnim as any).length === 0
+                complete &&
+                Object.keys(curAnim).length === 0
             ) {
-                options.complete.call(this.elem);
+                complete.call(elem);
             }
         } else { // #7166
             this.startTime = +new Date();
@@ -316,7 +312,7 @@ class Fx {
             this.now = this.start;
             this.pos = 0;
 
-            timer.elem = this.elem;
+            timer.elem = elem;
             timer.prop = this.prop;
 
             if (timer() && Fx.timers.push(timer) === 1) {
@@ -346,7 +342,8 @@ class Fx {
         let ret,
             done;
 
-        if (!!elem.attr && !elem.element) { // #2616, element is destroyed
+        // #2616, element is destroyed
+        if ((elem as SVGElement)?.attr && !(elem as SVGElement).element) {
             ret = false;
 
         } else if (gotoEnd || t >= duration + (this.startTime as any)) {
@@ -570,7 +567,7 @@ class Fx {
      *
      */
     public strokeSetter(): void {
-        this.elem.attr(
+        (this.elem as SVGElement).attr(
             this.prop,
             color(this.start as any).tweenTo(
                 color(this.end as any),

commit 3d767cff21e876d07f5d3ed65dd5c1e79fe89f39

Fixed regression in animation since 370f361ef

ts/Core/Animation/AnimationUtilities.ts
@@ -227,7 +227,11 @@ export function animate(
         if (typeof end === 'string' && end.match('px')) {
             end = end.replace(/px/g, ''); // #4351
         }
-        fx.run(start as any, end as any, unit);
+
+        // Empty dashstyle animation crashes treemap on hover
+        if (defined(end)) {
+            fx.run(start as any, end as any, unit);
+        }
     });
 }
 