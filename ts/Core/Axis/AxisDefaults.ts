a0b40805a14ded05059bf420da80ef8501fc530a
Kamil Musiałowski <kamil.musialowski@blacklabel.net>
2026-08-18 19:09:16 +0200

    Text formatting.

--- ts/Core/Axis/AxisDefaults.ts
+++ ts/Core/Axis/AxisDefaults.ts
@@ -1329,7 +1329,7 @@ namespace AxisDefaults {
          * Index or [id](#xAxis.id) of another axis that this axis is linked to.
          * When an axis is linked to a master axis, it will take the same
          * extremes as the master, but as assigned by min or max or by
-         * setExtremes. It can be used to show additional info, or to ease
+         * `setExtremes`. It can be used to show additional info, or to ease
          * reading the chart by duplicating the scales.
          *
          * @sample {highcharts} highcharts/xaxis/linkedto/
--- ts/Core/Axis/AxisOptions.ts
+++ ts/Core/Axis/AxisOptions.ts
@@ -1093,9 +1093,9 @@ export interface AxisOptions {
     /**
      * Index or [id](#xAxis.id) of another axis that this axis is linked to.
      * When an axis is linked to a master axis, it will take the same extremes
-     * as the master, but as assigned by min or max or by setExtremes. It can be
-     * used to show additional info, or to ease reading the chart by duplicating
-     * the scales.
+     * as the master, but as assigned by min or max or by `setExtremes`. It can
+     * be used to show additional info, or to ease reading the chart by
+     * duplicating the scales.
      *
      * @sample {highcharts} highcharts/xaxis/linkedto/
      *         Different string formats of the same date
