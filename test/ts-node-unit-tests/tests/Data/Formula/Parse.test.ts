59a99c96d3ff460bb2377a51c399ed1945226517
Oskar Eichler <oskar@songstats.com>
2026-08-30 06:37:01 +0200

    Fixed relative R1C1 formula parsing

--- test/ts-node-unit-tests/tests/Data/Formula/Parse.test.ts
+++ test/ts-node-unit-tests/tests/Data/Formula/Parse.test.ts
@@ -39,4 +39,40 @@ describe('Formula.parseFormula', () => {
             'Processing should result in a value of -10.'
         );
     });
+
+    it('should parse signed relative R1C1 references', () => {
+        deepStrictEqual(
+            Formula.parseFormula('R[-2]C[3]', false),
+            [{
+                column: 3,
+                columnRelative: true,
+                row: -2,
+                rowRelative: true,
+                type: 'reference'
+            }],
+            'Parsing should retain signed relative row and column offsets.'
+        );
+    });
+
+    it('should parse relative R1C1 ranges', () => {
+        deepStrictEqual(
+            Formula.parseFormula('SUM(R[-1]C[1]:R[2]C[-3])', false),
+            [{
+                args: [{
+                    beginColumn: 1,
+                    beginColumnRelative: true,
+                    beginRow: -1,
+                    beginRowRelative: true,
+                    endColumn: -3,
+                    endColumnRelative: true,
+                    endRow: 2,
+                    endRowRelative: true,
+                    type: 'range'
+                }],
+                name: 'SUM',
+                type: 'function'
+            }],
+            'Parsing should retain every signed relative range offset.'
+        );
+    });
 });
