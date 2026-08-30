a602abda58e42015d37fb20b6369c7147b4957d7
Oskar Eichler <oskar@songstats.com>
2026-08-30 11:21:06 +0200

    Fixed built-in-named treemap parents

--- samples/unit-tests/series-sunburst/data-id/demo.js
+++ samples/unit-tests/series-sunburst/data-id/demo.js
@@ -51,3 +51,46 @@ QUnit.test('series.data.id: custom id', function (assert) {
         'series.points[1] has a property id with value of "2"'
     );
 });
+
+QUnit.test('series.data.id: built-in object property names', function (assert) {
+    let duplicateIdWarnings = 0;
+    const removeErrorEvent = Highcharts.addEvent(
+        Highcharts,
+        'displayError',
+        function (event) {
+            if (event.code === 31) {
+                duplicateIdWarnings++;
+                event.preventDefault();
+            }
+        }
+    );
+
+    try {
+        const chart = Highcharts.chart('container', {
+                series: [{
+                    type: 'sunburst',
+                    data: [{
+                        id: 'toString',
+                        value: 1
+                    }, {
+                        id: '__proto__',
+                        value: 1
+                    }]
+                }]
+            }),
+            protoId = '__proto__';
+
+        assert.ok(
+            chart.series[0].nodeMap.toString &&
+                chart.series[0].nodeMap[protoId],
+            'Both built-in-named nodes should be available'
+        );
+        assert.strictEqual(
+            duplicateIdWarnings,
+            0,
+            'Distinct built-in-named nodes should not report duplicate IDs'
+        );
+    } finally {
+        removeErrorEvent();
+    }
+});
