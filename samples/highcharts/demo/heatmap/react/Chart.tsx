2f4bfb5481eda29319d01599f06cf9fa7e0a41b4
LBoge <99801154+LB0ge@users.noreply.github.com>
2026-09-25 10:50:01 +0200

    Removed the duplicate heatmap module import from the heatmap React demo.

--- samples/highcharts/demo/heatmap/react/Chart.tsx
+++ samples/highcharts/demo/heatmap/react/Chart.tsx
@@ -12,7 +12,6 @@ import { HeatmapSeries } from '@highcharts/react/series/Heatmap';
 import type { HeatmapSeriesProps } from '@highcharts/react/series/Heatmap';
 import { Exporting } from '@highcharts/react/modules/Exporting';
 import { Accessibility } from '@highcharts/react/modules/Accessibility';
-import 'highcharts/es-modules/masters/modules/heatmap.src.js';
 
 // Substring template helper for the responsive labels
 Highcharts.Templating.helpers.substr = (
